f41ae7a717ee6e88145bb27589f64034cb5265b2
Wouter Born <github@maindrain.net>
2019-02-23 17:46:17 +0100

    [habpanel] Fix SAT findings (#19)
    Signed-off-by: Wouter Born <eclipse@maindrain.net>

--- org.openhab.ui.habpanel/src/main/java/org/openhab/ui/habpanel/internal/rest/HABPanelResource.java
+++ org.openhab.ui.habpanel/src/main/java/org/openhab/ui/habpanel/internal/rest/HABPanelResource.java
@@ -1,10 +1,14 @@
 /**
- * Copyright (c) 2015-2017 by the respective copyright holders.
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
  */
 package org.openhab.ui.habpanel.internal.rest;
 
@@ -61,7 +65,6 @@ public class HABPanelResource implements RESTResource {
     public Response getGalleryWidgetList(
             @PathParam("galleryName") @ApiParam(value = "gallery name e.g. 'community'", required = true) String galleryName)
             throws Exception {
-
         GalleryWidgetProvider galleryProvider = GalleryProviderFactory.getWidgetGalleryProvider(galleryName);
 
         if (galleryProvider == null) {
@@ -83,7 +86,6 @@ public class HABPanelResource implements RESTResource {
     public Response getGalleryWidgetsItem(
             @PathParam("galleryName") @ApiParam(value = "gallery name e.g. 'community'", required = true) String galleryName,
             @PathParam("id") @ApiParam(value = "id within the gallery", required = true) String id) throws Exception {
-
         GalleryWidgetProvider galleryProvider = GalleryProviderFactory.getWidgetGalleryProvider(galleryName);
 
         if (galleryProvider == null) {
