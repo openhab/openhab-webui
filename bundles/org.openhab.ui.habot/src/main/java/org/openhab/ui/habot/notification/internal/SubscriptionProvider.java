fdd03ad0c332a6953542068fab39481cb0c681ca
Markus Rathgeb <maggu2810@gmail.com>
2019-05-02 21:17:22 +0200

    fix code for new storage service handling
    Related to: https://github.com/openhab/openhab-core/pull/787
    
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- bundles/org.openhab.ui.habot/src/main/java/org/openhab/ui/habot/notification/internal/SubscriptionProvider.java
+++ bundles/org.openhab.ui.habot/src/main/java/org/openhab/ui/habot/notification/internal/SubscriptionProvider.java
@@ -18,9 +18,9 @@ import org.eclipse.smarthome.core.common.registry.ManagedProvider;
 import org.eclipse.smarthome.core.storage.StorageService;
 import org.openhab.ui.habot.notification.internal.webpush.Subscription;
 import org.openhab.ui.habot.notification.internal.webpush.Subscription.Keys;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferencePolicy;
 
 /**
  * The @link {@link ManagedProvider} for {@link Subscription} elements
@@ -29,6 +29,11 @@ import org.osgi.service.component.annotations.ReferencePolicy;
  */
 @Component(service = SubscriptionProvider.class, immediate = true)
 public class SubscriptionProvider extends DefaultAbstractManagedProvider<Subscription, Keys> {
+
+    @Activate
+    public SubscriptionProvider(final @Reference StorageService storageService) {
+        super(storageService);
+    }
 
     @Override
     protected String getStorageName() {
@@ -39,17 +44,6 @@ public class SubscriptionProvider extends DefaultAbstractManagedProvider<Subscri
     @Override
     protected @NonNull String keyToString(@NonNull Keys key) {
         return String.format("%s.%s", key.p256dh, key.auth);
-    }
-
-    @Reference(policy = ReferencePolicy.DYNAMIC)
-    @Override
-    protected void setStorageService(StorageService storageService) {
-        super.setStorageService(storageService);
-    }
-
-    @Override
-    protected void unsetStorageService(StorageService storageService) {
-        super.unsetStorageService(storageService);
     }
 
 }
