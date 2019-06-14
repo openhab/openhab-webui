732eb92418d4c62ee0fb5dcb538fd20729f53143
Wouter Born <github@maindrain.net>
2019-06-14 09:44:53 +0200

    [paper] Sort serial port parameter options (#80)
    When you have many serial ports its easier to find them in a sorted list.
    
    Signed-off-by: Wouter Born <github@maindrain.net>

--- bundles/org.openhab.ui.paper/web-src/js/configuration/directive.configDescription.js
+++ bundles/org.openhab.ui.paper/web-src/js/configuration/directive.configDescription.js
@@ -18,12 +18,22 @@
         var ctrl = this;
 
         this.getName = getName;
+        this.getParameterOptions = getParameterOptions;
 
         function getName(parameter, option) {
             if (!option) {
                 return undefined;
             }
             return option.name ? option.name : parameter.context == 'thing' ? option.UID : parameter.context == 'channel' ? option.id : undefined;
+        }
+
+        function getParameterOptions(parameter) {
+            if (parameter.context == 'serial-port') {
+                return parameter.options.sort(function(o1, o2) {
+                    return o1.label.localeCompare(o2.label);
+                });
+            }
+            return parameter.options;
         }
     }
 
