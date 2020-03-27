bbb97ff6a60b2b3994089abc05607babf388f980
Yannick Schaus <github@schaus.net>
2020-03-27 16:23:22 +0100

    Add some aggregation functions for calendar series
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- bundles/org.openhab.ui/web/src/components/widgets/chart/series/oh-calendar-series.js
+++ bundles/org.openhab.ui/web/src/components/widgets/chart/series/oh-calendar-series.js
@@ -2,7 +2,7 @@ import * as dayjs from 'dayjs'
 
 export default {
   get (component, points) {
-    const data = points.data.reduce((acc, p) => {
+    const groups = points.data.reduce((acc, p) => {
       let day = dayjs(p.time).startOf('day')
       if (acc.length && acc[acc.length - 1][0].isSame(day)) {
         acc[acc.length - 1][1].push(p.state)
@@ -10,13 +10,41 @@ export default {
         acc.push([day, [p.state]])
       }
       return acc
-    }, []).map((arr) => {
+    }, [])
+
+    console.debug(groups)
+
+    const data = groups.map((arr, idx, days) => {
       const aggregationFunction = component.config.aggregationFunction || 'average'
       let aggregate
       switch (aggregationFunction) {
-        default:
-          // average
+        case 'sum':
+          aggregate = arr[1].reduce((sum, state) => sum + parseFloat(state), 0)
+          break
+        case 'min':
+          aggregate = arr[1].reduce((min, state) => parseFloat(state) < min ? parseFloat(state) : min, +Infinity)
+          break
+        case 'max':
+          aggregate = arr[1].reduce((max, state) => parseFloat(state) > max ? parseFloat(state) : max, -Infinity)
+          break
+        case 'first':
+          aggregate = arr[1][0]
+          break
+        case 'last':
+          aggregate = arr[1][arr[1].length - 1]
+          break
+        case 'diff_first':
+          aggregate = idx < 1 ? NaN : arr[1][0] - days[idx - 1][1][0]
+          break
+        case 'diff_last':
+          aggregate = idx < 1 ? NaN : arr[1][arr[1].length - 1] - days[idx - 1][1][days[idx - 1][1].length - 1]
+          break
+        case 'average':
           aggregate = arr[1].reduce((sum, state) => sum + parseFloat(state), 0) / arr[1].length
+          break
+        default:
+          aggregate = arr[1][0]
+          break
       }
       return [arr[0].toDate(), aggregate]
     })
