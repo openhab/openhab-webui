9e29948dbbdd449d5e0119bef95fc024b5b83d98
Yannick Schaus <github@schaus.net>
2020-05-27 11:44:01 +0200

    Restyle theme/dark mode pickers
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- bundles/org.openhab.ui/web/src/components/theme-switcher.vue
+++ bundles/org.openhab.ui/web/src/components/theme-switcher.vue
@@ -1,18 +1,39 @@
 <template>
   <f7-block>
     <f7-block-title class="padding-left">Theme</f7-block-title>
-    <f7-segmented>
-      <f7-button round color="blue" raised :fill="theme === 'auto'" @click="switchTheme('auto')">Auto</f7-button>
-      <f7-button round color="blue" raised :fill="theme === 'md'" @click="switchTheme('md')">MD</f7-button>
-      <f7-button round color="blue" raised :fill="theme === 'ios'" @click="switchTheme('ios')">iOS</f7-button>
-      <f7-button round color="blue" raised :fill="theme === 'aurora'" @click="switchTheme('aurora')">Aurora</f7-button>
-    </f7-segmented>
+    <f7-row>
+      <f7-col width="25" class="theme-picker auto" @click="switchTheme('auto')">
+        <span class="text-color-gray">Auto</span>
+        <f7-checkbox checked disabled v-if="theme === 'auto'" />
+      </f7-col>
+      <f7-col width="25" class="theme-picker" @click="switchTheme('md')">
+        <span><f7-icon f7="logo_android" size="20" color="gray" /></span>
+        <f7-checkbox checked disabled v-if="theme === 'md'" />
+      </f7-col>
+      <f7-col width="25" class="theme-picker" @click="switchTheme('ios')">
+        <span><f7-icon f7="logo_ios" size="25" color="gray" /></span>
+        <f7-checkbox checked disabled v-if="theme === 'ios'" />
+      </f7-col>
+      <f7-col width="25" class="theme-picker" @click="switchTheme('aurora')">
+        <span><f7-icon f7="desktopcomputer" size="20" color="gray" /></span>
+        <f7-checkbox checked disabled v-if="theme === 'aurora'" />
+      </f7-col>
+    </f7-row>
     <f7-block-title>Dark mode</f7-block-title>
-    <f7-segmented>
-      <f7-button round color="blue" raised :fill="darkMode === 'auto'" @click="setThemeDark('auto')">Auto</f7-button>
-      <f7-button round color="blue" raised :fill="darkMode === 'light'" @click="setThemeDark('light')">Light</f7-button>
-      <f7-button round color="blue" raised :fill="darkMode === 'dark'" @click="setThemeDark('dark')">Dark</f7-button>
-    </f7-segmented>
+    <f7-row>
+      <f7-col width="33" class="theme-picker auto" @click="setThemeDark('auto')">
+        <span class="text-color-gray">Auto</span>
+        <f7-checkbox checked disabled v-if="darkMode === 'auto'" />
+      </f7-col>
+      <f7-col width="33" class="bg-color-white theme-picker" @click="setThemeDark('light')">
+        <span class="text-color-gray">Light</span>
+        <f7-checkbox checked disabled v-if="darkMode === 'light'" />
+      </f7-col>
+      <f7-col width="33" class="bg-color-black theme-picker" @click="setThemeDark('dark')">
+        <span class="text-color-gray">Dark</span>
+        <f7-checkbox checked disabled v-if="darkMode === 'dark'" />
+      </f7-col>
+    </f7-row>
     <f7-block-title>Navigation bars style</f7-block-title>
     <f7-row>
       <f7-col width="50" class="nav-bars-picker nav-bars-picker-fill" @click="setBarsStyle('filled')">
@@ -114,64 +135,87 @@ export default {
 }
 </script>
 <style lang="stylus">
+.theme-picker
+  cursor pointer
+  height 100px
+  padding 40px 20px
+  border-radius 10px
+  box-shadow 0px 5px 20px rgba(0,0,0,0.1)
+  border 1px solid rgba(255,255,255,0.2)
+  box-sizing border-box
+  position relative
+  display flex
+  align-content center
+  font-size 16px
+  span
+    width 100%
+    text-align center
+  i.f7-icons
+    width 100%
+    text-align center
+  .checkbox
+    position absolute
+    left 10px
+    bottom 10px
+
 .nav-bars-picker
-  height 200px;
-  border-radius 10px;
-  box-shadow 0px 5px 20px rgba(0,0,0,0.1);
-  cursor pointer;
-  position relative;
-  overflow hidden;
-  background var(--f7-page-bg-color);
-  border 1px solid rgba(255,255,255,0.2);
+  height 200px
+  border-radius 10px
+  box-shadow 0px 5px 20px rgba(0,0,0,0.1)
+  cursor pointer
+  position relative
+  overflow hidden
+  background var(--f7-page-bg-color)
+  border 1px solid rgba(255,255,255,0.2)
 
 .nav-bars-picker .checkbox
-  position absolute;
-  left 10px;
-  bottom 10px;
+  position absolute
+  left 10px
+  bottom 10px
 
 .nav-bars-picker .demo-navbar
-  position absolute;
-  left 0;
-  width 100%;
-  height 30px;
-  top 0;
-  border-bottom 1px solid transparent;
+  position absolute
+  left 0
+  width 100%
+  height 30px
+  top 0
+  border-bottom 1px solid transparent
 
 .nav-bars-picker .demo-navbar:before
-  content '';
-  position absolute;
-  left 10px;
-  width 20px;
-  height 10px;
-  top 50%;
-  margin-top -5px;
+  content ''
+  position absolute
+  left 10px
+  width 20px
+  height 10px
+  top 50%
+  margin-top -5px
 
 .nav-bars-picker .demo-navbar:after
-  content '';
-  position absolute;
-  right 10px;
-  width 20px;
-  height 10px;
-  top 50%;
-  margin-top -5px;
+  content ''
+  position absolute
+  right 10px
+  width 20px
+  height 10px
+  top 50%
+  margin-top -5px
 
 .nav-bars-picker-empty .demo-navbar
-  background #f7f7f8;
-  border-color rgba(0,0,0,0.1);
+  background #f7f7f8
+  border-color rgba(0,0,0,0.1)
 
 .theme-dark .nav-bars-picker-empty .demo-navbar
-  background #1b1b1b;
-  border-color #282829;
+  background #1b1b1b
+  border-color #282829
 
 .nav-bars-picker-empty .demo-navbar:before,
 .nav-bars-picker-empty .demo-navbar:after
-  background var(--f7-theme-color);
+  background var(--f7-theme-color)
 
 .nav-bars-picker-fill .demo-navbar
-  background var(--f7-theme-color);
+  background var(--f7-theme-color)
 
 .nav-bars-picker-fill .demo-navbar:before,
 .nav-bars-picker-fill .demo-navbar:after
-  background #fff;
+  background #fff
 
 </style>
