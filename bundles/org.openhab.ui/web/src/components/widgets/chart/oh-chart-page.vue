270a631215662c0a3844c2958193267276589374
Yannick Schaus <github@schaus.net>
2020-05-27 10:40:38 +0200

    Remove bottom safe area for charts
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- bundles/org.openhab.ui/web/src/components/widgets/chart/oh-chart-page.vue
+++ bundles/org.openhab.ui/web/src/components/widgets/chart/oh-chart-page.vue
@@ -32,11 +32,11 @@
   width 100%
   height calc(100% - var(--f7-safe-area-top) - var(--f7-navbar-height)) !important
   &.with-tabbar
-    height calc(100% - var(--f7-safe-area-top) - var(--f7-navbar-height) - var(--f7-tabbar-labels-height)) !important
+    height calc(100% - var(--f7-safe-area-top) - var(--f7-safe-area-bottom) - var(--f7-navbar-height) - var(--f7-tabbar-labels-height)) !important
   &.with-toolbar
-    height calc(100% - var(--f7-safe-area-top) - var(--f7-navbar-height) - var(--f7-toolbar-height)) !important
+    height calc(100% - var(--f7-safe-area-top) - var(--f7-safe-area-bottom) - var(--f7-navbar-height) - var(--f7-toolbar-height)) !important
   &.with-sheet
-    height calc(100% - var(--f7-safe-area-top) - var(--f7-navbar-height) - var(--f7-sheet-height)) !important
+    height calc(100% - var(--f7-safe-area-top) - var(--f7-safe-area-bottom) - var(--f7-navbar-height) - var(--f7-sheet-height)) !important
   .echarts
     width calc(100% - 20px)
     height 100%
