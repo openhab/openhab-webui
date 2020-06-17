openhab/openhab-webui

commit 238a0aeceefab262e770f94c87fb10006980f5cd

Add Items from Thing: use model picker for parent group (#275)
Use a treeview-based semantic picker to select a parent
location or equipment group item when using the
Add Equipment to Model or Add Points to Model functions
in a Thing's Channels tab.
Replaces the current flat list of group items.

Add functionality to the model picker popup to support this
use case and adjust some texts and controls on the page.

Signed-off-by: Yannick Schaus <github@schaus.net>

bundles/org.openhab.ui/web/src/pages/settings/model/add-from-thing.vue
@@ -16,13 +16,26 @@
           </ul>
         </f7-list>
         <f7-block-footer v-if="thingId" class="padding-left padding-right">
-          Select a parent group, preferably part of the semantic model like a Location or Equipment group, under which the new items will be inserted (optional, but recommended).
+          Select the parent Location or Equipment group in the semantic model, under which the new items will be inserted (optional, but recommended).
         </f7-block-footer>
         <f7-list v-if="thingId">
-          <item-picker title="Create Items Under" name="parent-group" :value="parentGroup" @input="(value) => parentGroup = value" :multiple="false" filterType="Group"></item-picker>
+          <ul v-if="parentGroup">
+            <item :item="parentGroup" :link="true" @click="modelPickerOpened = true" />
+          </ul>
+          <f7-list-item v-else title="Pick From Model" link @click="modelPickerOpened = true" />
+          <model-picker-popup :value="(parentGroup) ? parentGroup.name : null" popup-title="Parent Group"
+            :allow-empty="true" :groups-only="true" :opened="modelPickerOpened"
+            @input="(item) => parentGroup = item" @closed="modelPickerOpened = false" />
+        </f7-list>
+        <f7-block-title v-if="selectedThing.statusInfo">Source Thing</f7-block-title>
+        <f7-list v-if="selectedThing.statusInfo" media-list>
+          <f7-list-item
+              :title="selectedThing.label"
+              :footer="selectedThing.UID"
+              :badge="selectedThing.statusInfo.status"
+              :badge-color="selectedThing.statusInfo.status === 'ONLINE' ? 'green' : 'red'" />
         </f7-list>
         <f7-block-title v-if="createEquipment">Equipment</f7-block-title>
-        <f7-block-title v-else-if="!thingId">Source Thing</f7-block-title>
         <f7-block-footer v-if="createEquipment && !thingId" class="padding-left padding-right">
           Select the Thing you wish to create as a new Equipment group in the model. It will be placed under the parent group above, if any.
           You can alter the new group's details and change its equipment class.
@@ -61,7 +74,7 @@
 
 <script>
 import ThingPicker from '@/components/config/controls/thing-picker.vue'
-import ItemPicker from '@/components/config/controls/item-picker.vue'
+import ModelPickerPopup from '@/components/model/model-picker-popup.vue'
 import ChannelList from '@/components/thing/channel-list.vue'
 import ItemForm from '@/components/item/item-form.vue'
 
@@ -70,7 +83,7 @@ import Item from '@/components/item/item.vue'
 export default {
   components: {
     Item,
-    ItemPicker,
+    ModelPickerPopup,
     ThingPicker,
     ChannelList,
     ItemForm
@@ -85,7 +98,8 @@ export default {
       selectedThingType: {},
       selectedThingChannelTypes: {},
       newEquipmentItem: {},
-      newPointItems: []
+      newPointItems: [],
+      modelPickerOpened: false
     }
   },
   methods: {
@@ -113,14 +127,14 @@ export default {
 
       let valid = true
       if (this.parentGroup && this.createEquipment) {
-        this.newEquipmentItem.groupNames = [this.parentGroup]
+        this.newEquipmentItem.groupNames = [this.parentGroup.name]
       }
       this.newPointItems.forEach((p) => {
         if (!p.name) valid = false
         if (this.createEquipment) {
           p.groupNames = [this.newEquipmentItem.name]
         } else {
-          p.groupNames = (this.parent) ? [this.parent.item.name] : (this.parentGroup) ? [this.parentGroup] : []
+          p.groupNames = (this.parent) ? [this.parent.item.name] : (this.parentGroup) ? [this.parentGroup.name] : []
         }
       })
 

bundles/org.openhab.ui/web/src/components/item/item.vue
@@ -7,6 +7,7 @@
   :footer="(item.label) ? item.name : '\xa0'"
   :subtitle="getItemTypeAndMetaLabel(item)"
   :after="state"
+  v-on="$listeners"
 >
   <oh-icon v-if="item.category" slot="media" :icon="item.category" height="32" width="32" />
   <span v-else slot="media" class="item-initial">{{item.name[0]}}</span>

bundles/org.openhab.ui/web/src/components/model/model-picker-popup.vue
@@ -6,9 +6,9 @@
         <f7-nav-left>
           <f7-link icon-ios="f7:arrow_left" icon-md="material:arrow_back" icon-aurora="f7:arrow_left" popup-close></f7-link>
         </f7-nav-left>
-        <f7-nav-title>Add from Model</f7-nav-title>
+        <f7-nav-title>{{popupTitle || 'Pick from Model'}}</f7-nav-title>
         <f7-nav-right>
-          <f7-link v-if="ready && ((multiple && checkedItems.length > 0) || selectedItem)" @click="pickItems">{{actionLabel || 'Pick'}}<span v-if="multiple && checkedItems.length > 0">&nbsp;{{checkedItems.length}}</span></f7-link>
+          <f7-link v-if="ready && ((multiple && checkedItems.length > 0) || selectedItem || allowEmpty)" @click="pickItems">{{actionLabel || 'Pick'}}<span v-if="multiple && checkedItems.length > 0">&nbsp;{{checkedItems.length}}</span></f7-link>
         </f7-nav-right>
       </f7-navbar>
       <f7-subnavbar :inner="false" v-show="initSearchbar">
@@ -49,7 +49,7 @@ import ModelTreeview from '@/components/model/model-treeview.vue'
 import MetadataNamespaces from '@/assets/definitions/metadata/namespaces.js'
 
 export default {
-  props: ['opened', 'multiple', 'actionLabel'],
+  props: ['value', 'opened', 'multiple', 'semanticOnly', 'groupsOnly', 'allowEmpty', 'popupTitle', 'actionLabel'],
   components: {
     ModelTreeview
   },
@@ -69,12 +69,17 @@ export default {
       rootGroups: [],
       rootItems: [],
       selectedItem: null,
+      previousSelection: null,
       checkedItems: []
     }
   },
   computed: {
     rootNodes () {
-      return [this.rootLocations, this.rootEquipments, this.rootPoints, this.rootGroups, this.rootItems].flat()
+      if (this.semanticOnly) {
+        return [this.rootLocations, this.rootEquipments, (!this.groupsOnly) ? this.rootPoints : []]
+      } else {
+        return [this.rootLocations, this.rootEquipments, (!this.groupsOnly) ? this.rootPoints : [], this.rootGroups, (!this.groupsOnly) ? this.rootItems : []].flat()
+      }
     }
   },
   methods: {
@@ -92,7 +97,7 @@ export default {
       if (this.multiple) {
         this.$emit('input', this.checkedItems.map((i) => i.item))
       } else {
-        this.$emit('input', this.selectedItem.item)
+        this.$emit('input', (this.selectedItem) ? this.selectedItem.item : null)
       }
       this.onClose()
     },
@@ -121,6 +126,9 @@ export default {
       }
 
       modelItem.checkable = this.multiple
+      if (!this.multiple && this.value === item.name) {
+        this.selectItem(modelItem)
+      }
 
       return modelItem
     },
@@ -153,7 +161,7 @@ export default {
           .filter((i) => !i.metadata.semantics.config || (!i.metadata.semantics.config.isPointOf && !i.metadata.semantics.config.hasLocation))
           .map(this.modelItem)
 
-        if (this.includeNonSemantic) {
+        if (this.includeNonSemantic && !this.semanticOnly) {
           this.rootGroups = this.items
             .filter((i) => i.type === 'Group' && (!i.metadata || !i.metadata.semantics) && i.groupNames.length === 0)
             .map(this.modelItem)
@@ -192,18 +200,22 @@ export default {
           .map(this.modelItem)
         parent.children.equipments.forEach(this.getChildren)
 
-        parent.children.points = this.points
-          .filter((i) => i.metadata.semantics.config && i.metadata.semantics.config.hasLocation === parent.item.name)
-          .map(this.modelItem)
+        if (!this.groupsOnly) {
+          parent.children.points = this.points
+            .filter((i) => i.metadata.semantics.config && i.metadata.semantics.config.hasLocation === parent.item.name)
+            .map(this.modelItem)
+        }
       } else {
         parent.children.equipments = this.equipments
           .filter((i) => i.metadata.semantics.config && i.metadata.semantics.config.isPartOf === parent.item.name)
           .map(this.modelItem)
         parent.children.equipments.forEach(this.getChildren)
 
-        parent.children.points = this.points
-          .filter((i) => i.metadata.semantics.config && i.metadata.semantics.config.isPointOf === parent.item.name)
-          .map(this.modelItem)
+        if (!this.groupsOnly) {
+          parent.children.points = this.points
+            .filter((i) => i.metadata.semantics.config && i.metadata.semantics.config.isPointOf === parent.item.name)
+            .map(this.modelItem)
+        }
       }
 
       if (this.includeNonSemantic) {
@@ -216,9 +228,11 @@ export default {
             .filter((i) => i.type !== 'Group' && (!i.metadata) && i.groupNames.indexOf(parent.item.name) >= 0)
             .map(this.modelItem)
         } else {
-          parent.children.items = this.items
-            .filter((i) => i.type !== 'Group' && i.groupNames.indexOf(parent.item.name) >= 0)
-            .map(this.modelItem)
+          if (!this.groupsOnly) {
+            parent.children.items = this.items
+              .filter((i) => i.type !== 'Group' && i.groupNames.indexOf(parent.item.name) >= 0)
+              .map(this.modelItem)
+          }
         }
       }
     },

commit 2b4baba0c44c7c927c665c7433fef33d8e871ef4

Fix points names in Add Points to Model scenario
Fix a bug introduced with #275 when points being added to the model without an
equipment would not have their default name properly computed.

Signed-off-by: Yannick Schaus <github@schaus.net>

bundles/org.openhab.ui/web/src/pages/settings/model/add-from-thing.vue
@@ -64,7 +64,7 @@
               (you will be able to configure the links individually later if needed).
             </f7-block-footer>
             <channel-list :thing="selectedThing" :thingType="selectedThingType" :channelTypes="selectedThingChannelTypes"
-              :multiple-links-mode="true" :new-items-prefix="(createEquipment) ? newEquipmentItem.name : (parentGroup) ? parentGroup : ''"
+              :multiple-links-mode="true" :new-items-prefix="(createEquipment) ? newEquipmentItem.name : (parentGroup) ? parentGroup.name : ''"
               @selected="(channel) => toggleSelect(channel)" :new-items="newPointItems" />
         </div>
       </f7-col>