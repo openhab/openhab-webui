0fcfd5e4bca2591148296a7d634265a58f32fccf
Yannick Schaus <github@schaus.net>
2020-10-16 15:14:24 +0200

    Channel list improvements
    Add `searchbar-ignore` class to link skeletons to prevent
    the height of the accordion to be initialized at 0px.
    Should fix #401.
    
    Add filters to display only linked or unlinked channels.
    Closes #395.
    
    Hide item linking controls for trigger channels.
    Closes #390.
    
    Add option to select/unselect all checkboxes when in
    multiple links mode. This will take the linked/unlinked
    filter into account, and the advanced toggle, but not
    the name filter.
    Closes #377.
    
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- bundles/org.openhab.ui/web/src/components/thing/channel-group.vue
+++ bundles/org.openhab.ui/web/src/components/thing/channel-group.vue
@@ -5,13 +5,15 @@
       :description="group.description"
       :footer="group.description" />
     <f7-list-item
+      v-for="c in group.channels"
+      :key="c.channel.id" :title="c.channel.label || c.channelType.label"
+      v-show="!(pickerMode || multipleLinksMode) || c.channelType.kind !== 'TRIGGER'"
       :accordion-item="!pickerMode && !multipleLinksMode"
       :radio="pickerMode"
       :checkbox="multipleLinksMode"
+      :checked="isSelected(c.channel)"
       name="channel-picker"
       media-item class="channel-item"
-      v-for="c in group.channels"
-      :key="c.channel.id" :title="c.channel.label || c.channelType.label"
       :footer="c.channelType.description"
       :subtitle="c.channel.id + ' (' + getItemType(c.channelType) + ')'"
       :badge="getLinkedItems(c.channel).length || ''" badge-color="blue"
@@ -22,7 +24,7 @@
       <oh-icon v-if="!c.extensible && c.channelType.category" slot="media" :icon="c.channelType.category" height="32" width="32" />
       <span v-else-if="c.extensible && c.channel.label" slot="media" class="item-initial">{{c.channel.label[0]}}</span>
       <span v-else-if="!c.extensible && c.channelType.label" slot="media" class="item-initial">{{c.channelType.label[0]}}</span>
-      <f7-accordion-content v-if="!pickerMode">
+      <f7-accordion-content v-if="!pickerMode" class="searchbar-ignore">
         <slot :channelType="c.channelType" :channelId="c.channel.id" :channel="c.channel" :extensible="c.extensible"></slot>
       </f7-accordion-content>
       <div v-if="multipleLinksMode" slot="root-end">
@@ -41,7 +43,8 @@ export default {
     'thing',
     'pickerMode',
     'multipleLinksMode',
-    'itemTypeFilter'
+    'itemTypeFilter',
+    'selection'
   ],
   data () {
     return {
@@ -72,6 +75,13 @@ export default {
     isItemTypeCompatible (channelType) {
       if (!this.pickerMode || !this.itemTypeFilter) return true
       return this.getItemType(channelType) === this.itemTypeFilter
+    },
+    isSelected (channel) {
+      if (Array.isArray(this.selection)) {
+        return this.selection.indexOf(channel) >= 0
+      } else {
+        return this.selection === channel
+      }
     }
   }
 }
