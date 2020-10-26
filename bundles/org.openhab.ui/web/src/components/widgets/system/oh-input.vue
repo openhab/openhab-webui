cfa2824690d68bb93cbe189196eedf568ac71aad
Yannick Schaus <github@schaus.net>
2020-10-26 19:50:51 +0100

    Fixes for oh-stepper & oh-input (#457)
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- bundles/org.openhab.ui/web/src/components/widgets/system/oh-input.vue
+++ bundles/org.openhab.ui/web/src/components/widgets/system/oh-input.vue
@@ -1,12 +1,8 @@
 <template>
-  <f7-input v-if="!config.item || !config.sendButton" class="input-field" ref="input" v-bind="config" :value="value" @input="$evt => updated($evt.target.value)" :change="updated" @calendar:change="updated" @texteditor:change="updated" @colorpicker:change="updated" />
+  <f7-input v-if="!config.item || !config.sendButton" class="input-field" ref="input" v-bind="config" :value="(config.type.indexOf('date') === 0) ? valueForDatepicker : value" @input="$evt => updated($evt.target.value)" :change="updated" @calendar:change="updated" @texteditor:change="updated" @colorpicker:change="updated" />
   <f7-row no-gap v-else class="oh-input-with-send-button">
-    <f7-col width="90">
-      <f7-input class="input-field" ref="input" v-bind="config" :value="value" @input="$evt => updated($evt.target.value)" :change="updated" @calendar:change="updated" @texteditor:change="updated" @colorpicker:change="updated" />
-    </f7-col>
-    <f7-col width="10">
-      <f7-button class="send-button" v-if="this.config.sendButton" @click="clicked" v-bind="config.sendButtonConfig || { iconMaterial: 'done', iconColor: 'gray' }" />
-    </f7-col>
+    <f7-input class="input-field col-90" ref="input" v-bind="config" :value="(config.type.indexOf('date') === 0) ? valueForDatepicker : value" @input="$evt => updated($evt.target.value)" :change="updated" @calendar:change="updated" @texteditor:change="updated" @colorpicker:change="updated" />
+    <f7-button class="send-button col-10" v-if="this.config.sendButton" @click="sendButtonClicked" v-bind="config.sendButtonConfig || { iconMaterial: 'done', iconColor: 'gray' }" />
   </f7-row>
 </template>
 
@@ -21,37 +17,67 @@
 </style>
 
 <script>
+import dayjs from 'dayjs'
+
 import mixin from '../widget-mixin'
 import { OhInputDefinition } from '@/assets/definitions/widgets/system'
 
 export default {
   mixins: [mixin],
   widget: OhInputDefinition,
-  mounted () {
-    delete this.config.value
+  data () {
+    return {
+      pendingUpdate: null
+    }
   },
   computed: {
     value () {
       if (this.config.variable && this.context.vars[this.config.variable] !== undefined) {
         return this.context.vars[this.config.variable]
-      } else if (this.config.showOK && this.context.vars[this.config.value] !== undefined) {
-        return this.context.vars[this.config.value]
+      } else if (this.config.sendButton && this.pendingUpdate !== null) {
+        return this.pendingUpdate
+      } else if (this.config.item && this.context.store[this.config.item].state !== 'NULL' && this.context.store[this.config.item].state !== 'UNDEF') {
+        return this.context.store[this.config.item].state
       }
-      return (this.config.item) ? this.context.store[this.config.item].state : this.config.defaultValue
+      return this.config.defaultValue
+    },
+    valueForDatepicker () {
+      const value = this.value
+      switch (this.config.type) {
+        case 'datepicker':
+          if (Array.isArray(value)) return value
+          const date = new Date(value)
+          if (isNaN(date)) return null
+          return [date]
+        case 'date':
+          return (Date.parse(value) > 0) ? value.split('T')[0] : null
+        default:
+          return null
+      }
     }
   },
   methods: {
     updated (value) {
-      if (this.config.showOK) {
-        this.$set(this.context.vars, this.config.value, value)
+      if (this.config.type === 'texteditor') {
+        value = this.$$(this.$refs.input.$el).find('.text-editor-content')[0].innerHTML
+        if (value === this.value) return
+      }
+      if (this.config.type === 'datepicker' && Array.isArray(value) && this.valueForDatepicker[0].getTime() === value[0].getTime()) return
+      if (this.config.sendButton) {
+        this.$set(this, 'pendingUpdate', value)
       }
       if (this.config.variable) {
         this.$set(this.context.vars, this.config.variable, value)
       }
     },
-    clicked () {
-      if (this.config.item) {
-        this.$store.dispatch('sendCommand', { itemName: this.config.item, cmd: this.context.vars[this.config.value] })
+    sendButtonClicked () {
+      if (this.config.item && this.pendingUpdate) {
+        let cmd = this.pendingUpdate
+        if (this.config.type === 'datepicker' && Array.isArray(cmd)) {
+          cmd = dayjs(cmd[0]).format()
+        }
+        this.$store.dispatch('sendCommand', { itemName: this.config.item, cmd })
+        this.$set(this, 'pendingUpdate', null)
       }
     }
   }
