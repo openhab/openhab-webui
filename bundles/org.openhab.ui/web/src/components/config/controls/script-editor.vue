openhab/openhab-webui

commit 81791a14ee0a7a98c5d5d9c20c7e81679459ed46

Script-only rules, improve scripting UX (#469)
This adds "Scripts" as a top-level object. Scripts are regular
rules, tagged "Script", without triggers or conditions and
having only a single action module with the id "script".

The main settings menu has been modified to group "Rules",
"Scripts" and "Schedule" under a new "Automation" group.
"Configuration & Automation" has been renamed to "Configuration".

Scripts can be added and edited in a dedicated, full-screen
list and editor saving the hassle of having to configure
modules manually.

The new script editor is also used on the "shortcut" button of
regular rules' script actions & conditions. During script
editing, several keyboard shortcuts are available:

- Ctrl-D disables or enable the rule;
- Ctrl-R runs the rule - but saves it first;
- Ctrl-S saves the rule.

The script editor now has support for C-like languages (the
DSL language is automatically mapped to text/x-java) and
Groovy.

When editing ECMAScript, a special Tern plugin will offer
item names for completion when the cursor is in these
positions (repesented by `|`):

- `ir.getItem("|")`
- `itemRegistry.getItem("|")`
- `events.postUpdate("|")`
- `events.sendCommand("|")`

Signed-off-by: Yannick Schaus <github@schaus.net>

bundles/org.openhab.ui/web/src/components/config/controls/script-editor.vue
@@ -47,6 +47,8 @@ import 'codemirror/lib/codemirror.css'
 
 // language js
 import 'codemirror/mode/javascript/javascript.js'
+import 'codemirror/mode/clike/clike.js'
+import 'codemirror/mode/groovy/groovy.js'
 import 'codemirror/mode/yaml/yaml.js'
 
 // theme css
@@ -75,6 +77,7 @@ import 'codemirror/addon/lint/lint.css'
 import YAML from 'yaml'
 
 import tern from 'tern'
+import infer from 'tern/lib/infer'
 
 // import 'tern/lib/signal.js'
 // import * as Tern from 'tern/lib/tern.js'
@@ -130,14 +133,15 @@ export default {
   components: {
     codemirror
   },
-  props: ['value', 'mode', 'hintContext'],
+  props: ['value', 'mode', 'hintContext', 'ternAutocompletionHook'],
   data () {
     return {
       code: this.value,
+      itemsCache: [],
       cmOptions: {
         // codemirror options
         tabSize: 4,
-        mode: ((this.mode && this.mode.indexOf('yaml')) ? 'text/x-yaml' : this.mode) || 'text/javascript',
+        mode: this.translateMode(this.mode),
         theme: (this.$f7.data.themeOptions.dark === 'dark') ? 'gruvbox-dark' : 'default',
         lineNumbers: true,
         line: true,
@@ -156,16 +160,69 @@ export default {
     }
   },
   methods: {
+    translateMode (mode) {
+      if (this.mode && this.mode.indexOf('yaml') >= 0) return 'text/x-yaml'
+      if (this.mode && this.mode === 'application/javascript') return 'text/javascript'
+      if (this.mode && this.mode === 'application/vnd.openhab.dsl.rule') return 'text/x-java'
+      if (this.mode && this.mode === 'application/x-groovy') return 'text/x-groovy'
+      return this.mode
+    },
     ternComplete (file, query) {
-      // debugger
+      var pos = tern.resolvePos(file, query.end)
+      var lit = infer.findExpressionAround(file.ast, null, pos, file.scope, 'Literal')
+      if (!lit || !lit.node) return
+      var call = infer.findExpressionAround(file.ast, null, lit.node.start - 2, file.scope)
+      if (!call || !call.node) return
+      if (call.node.type !== 'MemberExpression' || (!call.node.object && !call.node.property)) return
+      if ((call.node.object.name === 'events' && call.node.property.name === 'postUpdate') ||
+      (call.node.object.name === 'events' && call.node.property.name === 'sendCommand') ||
+      (call.node.object.name === 'itemRegistry' && call.node.property.name === 'getItem') ||
+      (call.node.object.name === 'ir' && call.node.property.name === 'getItem')) {
+        console.debug('Completing item names!')
+
+        var before = lit.node.value.slice(0, pos - lit.node.start - 1)
+        var matches = []
+        this.itemsCache.sort((a, b) => a.name.localeCompare(b.name)).forEach((item) => {
+          if (item.name.length > before.length && item.name.toLowerCase().indexOf(before.toLowerCase()) >= 0) {
+            if (query.types || query.docs || query.urls || query.origins) {
+              var rec = {
+                name: JSON.stringify(item.name),
+                displayName: item.name,
+                doc: (item.label ? item.label + ' ' : '') + '[' + item.type + ']'
+              }
+              matches.push(rec)
+              if (query.types) rec.type = 'string'
+              if (query.origins) rec.origin = item.name
+            }
+          }
+        })
+
+        return {
+          start: tern.outputPos(query, file, lit.node.start),
+          end: tern.outputPos(query, file, pos + (file.text.charAt(pos) === file.text.charAt(lit.node.start) ? 1 : 0)),
+          isProperty: false,
+          completions: matches
+        }
+      }
     },
     onCmReady (cm) {
       const self = this
       let extraKeys = {}
-      if (!this.mode) {
+      if (this.mode === 'application/javascript') {
         window.tern = tern
+        if (this.ternAutocompletionHook) {
+          tern.registerPlugin('openhab-tern-hook', (server, options) => {
+            server.mod.completeStrings = {
+              maxLen: (options && options.maxLength) || 15,
+              seen: Object.create(null)
+            }
+            server.on('completion', this.ternComplete)
+          })
+          this.$oh.api.get('/rest/items').then((data) => { this.$set(this, 'itemsCache', data) })
+        }
         const server = new _CodeMirror.TernServer({
           defs: [EcmascriptDefs, OpenhabDefs],
+          plugins: (this.ternAutocompletionHook) ? { 'openhab-tern-hook': {} } : undefined,
           ecmaVersion: 5
         })
         extraKeys = {

bundles/org.openhab.ui/web/src/pages/settings/rules/rule-edit.vue
@@ -92,8 +92,8 @@
                   v-for="mod in rule[section]" :key="mod.id"
                   :link="isEditable && !showModuleControls" @click.native="(ev) => editModule(ev, section, mod)" swipeout>
                 <f7-link slot="media" v-if="isEditable" icon-color="red" icon-aurora="f7:minus_circle_filled" icon-ios="f7:minus_circle_filled" icon-md="material:remove_circle_outline" @click="showSwipeout"></f7-link>
-                <f7-link slot="after" v-if="mod.type === 'script.ScriptAction'" icon-f7="pencil_ellipsis_rectangle" color="gray" @click.native="(ev) => editScriptDirect(ev, mod)" tooltip="Edit script"></f7-link>
-                <f7-link slot="after" v-if="mod.type === 'timer.GenericCronTrigger' && isEditable" icon-f7="calendar" color="gray" @click.native="(ev) => buildCronExpression(ev, mod)" tooltip="Build cron expression"></f7-link>
+                <f7-link slot="after" v-if="!createMode && mod.type && mod.type.indexOf('script') === 0" icon-f7="pencil_ellipsis_rectangle" color="gray" @click.native="(ev) => editScriptDirect(ev, mod)" tooltip="Edit script"></f7-link>
+                <f7-link slot="after" v-if="!createMode && mod.type === 'timer.GenericCronTrigger' && isEditable" icon-f7="calendar" color="gray" @click.native="(ev) => buildCronExpression(ev, mod)" tooltip="Build cron expression"></f7-link>
                 <f7-swipeout-actions right v-if="isEditable">
                   <f7-swipeout-button @click="(ev) => deleteModule(ev, section, mod)" style="background-color: var(--f7-swipeout-delete-button-bg-color)">Delete</f7-swipeout-button>
                 </f7-swipeout-actions>
@@ -160,7 +160,6 @@ import SemanticsPicker from '@/components/tags/semantics-picker.vue'
 import TagInput from '@/components/tags/tag-input.vue'
 
 import RuleModulePopup from './rule-module-popup.vue'
-import ScriptEditorPopup from '@/components/config/controls/script-editor-popup.vue'
 import CronEditor from '@/components/config/controls/cronexpression-editor.vue'
 
 import ModuleDescriptionSuggestions from './module-description-suggestions'
@@ -208,7 +207,6 @@ export default {
   },
   methods: {
     onPageAfterIn () {
-      if (this.ready) return
       if (window) {
         window.addEventListener('keydown', this.keyDown)
       }
@@ -274,16 +272,10 @@ export default {
         this.$f7.dialog.alert('Please give a name to the rule')
         return
       }
-      if (this.codeEditorOpened) {
-        // save the code currently being edited if the dialog is open
-        // this allows to hit ctrl-S to save (and ctrl-R to run the rule) while editing the code
-        // without closing the window
-        this.currentModule.configuration.script = this.$refs.codePopup.code
-      }
       const promise = (this.createMode)
         ? this.$oh.api.postPlain('/rest/rules', JSON.stringify(this.rule), 'text/plain', 'application/json')
         : this.$oh.api.put('/rest/rules/' + this.rule.uid, this.rule)
-      promise.then((data) => {
+      return promise.then((data) => {
         if (this.createMode) {
           this.$f7.toast.create({
             text: 'Rule created',
@@ -509,31 +501,8 @@ export default {
       this.currentModuleType = mod.type
       this.scriptCode = mod.configuration.script
 
-      const popup = {
-        component: ScriptEditorPopup
-      }
-
-      this.$f7router.navigate({
-        url: 'script-edit',
-        route: {
-          path: 'script-edit',
-          popup
-        }
-      }, {
-        props: {
-          title: 'Edit Script',
-          fullscreen: false,
-          value: this.scriptCode
-        }
-      })
-
-      this.$f7.once('scriptEditorUpdate', this.updateScript)
-      this.$f7.once('scriptEditorClosed', () => {
-        this.$f7.off('scriptEditorUpdate', this.updateScript)
-        this.$nextTick(() => {
-          this.currentModule = null
-          this.currentModuleType = null
-        })
+      this.save().then(() => {
+        this.$f7router.navigate('script/' + mod.id, { transition: this.$theme.aurora ? 'f7-cover-v' : '' })
       })
     },
     buildCronExpression (ev, mod) {

bundles/org.openhab.ui/web/src/components/app.vue
@@ -55,6 +55,10 @@
               :class="{ currentsection: currentUrl.indexOf('/settings/rules') >= 0 }">
             <f7-icon slot="media" f7="wand_rays" color="gray"></f7-icon>
           </f7-list-item>
+          <f7-list-item v-if="$store.getters.apiEndpoint('rules')" link="/settings/scripts/" title="Scripts" view=".view-main" panel-close :animate="false" no-chevron
+              :class="{ currentsection: currentUrl.indexOf('/settings/scripts') >= 0 }">
+            <f7-icon slot="media" f7="doc_plaintext" color="gray"></f7-icon>
+          </f7-list-item>
           <f7-list-item v-if="$store.getters.apiEndpoint('rules')" link="/settings/schedule/" title="Schedule" view=".view-main" panel-close :animate="false" no-chevron
               :class="{ currentsection: currentUrl.indexOf('/settings/schedule') >= 0 }">
             <f7-icon slot="media" f7="calendar" color="gray"></f7-icon>

bundles/org.openhab.ui/web/src/js/routes.js
@@ -231,6 +231,58 @@ export default [
                   }
                 } : {})
               })
+            },
+            routes: [
+              {
+                path: 'script/:moduleId',
+                async (routeTo, routeFrom, resolve, reject) {
+                  // dynamic import component; returns promise
+                  const ruleEditComponent = () => import(/* webpackChunkName: "rule-script-edit" */ '../pages/settings/rules/script/script-edit.vue')
+                  // resolve promise
+                  ruleEditComponent().then((vc) => {
+                    // resolve with component
+                    resolve({
+                      component: vc.default
+                    },
+                    (routeTo.params.ruleId === 'add') ? {
+                      props: {
+                        createMode: true
+                      }
+                    } : {})
+                  })
+                }
+              }
+            ]
+          }
+        ]
+      },
+      {
+        path: 'scripts/',
+        component: RulesListPage,
+        keepAlive: true,
+        options: {
+          props: {
+            showScripts: true
+          }
+        },
+        routes: [
+          {
+            path: ':ruleId',
+            async (routeTo, routeFrom, resolve, reject) {
+              // dynamic import component; returns promise
+              const ruleEditComponent = () => import(/* webpackChunkName: "script-edit" */ '../pages/settings/rules/script/script-edit.vue')
+              // resolve promise
+              ruleEditComponent().then((vc) => {
+                // resolve with component
+                resolve({
+                  component: vc.default
+                },
+                (routeTo.params.ruleId === 'add') ? {
+                  props: {
+                    createMode: true
+                  }
+                } : {})
+              })
             }
           }
         ]
@@ -286,7 +338,6 @@ export default [
             path: ':bindingId/config',
             component: AddonsConfigureBindingPage
           }
-
         ]
       },
       {

bundles/org.openhab.ui/web/src/pages/settings/rules/rules-list.vue
@@ -1,6 +1,6 @@
 <template>
   <f7-page @page:afterin="onPageAfterIn" @page:afterout="stopEventSource">
-    <f7-navbar title="Rules" back-link="Settings" back-link-url="/settings/" back-link-force>
+    <f7-navbar :title="(showScripts) ? 'Scripts' : 'Rules'" back-link="Settings" back-link-url="/settings/" back-link-force>
       <f7-nav-right>
         <f7-link icon-md="material:done_all" @click="toggleCheck()"
         :text="(!$theme.md) ? ((showCheckboxes) ? 'Done' : 'Select') : ''"></f7-link>
@@ -63,7 +63,8 @@
         </f7-list>
       </f7-col>
       <f7-col v-else>
-        <f7-block-title class="searchbar-hide-on-search">{{rules.length}} rules</f7-block-title>
+        <f7-block-title v-if="showScripts" class="searchbar-hide-on-search">{{rules.length}} scripts</f7-block-title>
+        <f7-block-title v-else class="searchbar-hide-on-search">{{rules.length}} rules</f7-block-title>
         <f7-list
           v-show="rules.length > 0"
           class="searchbar-found col rules-list"
@@ -87,7 +88,7 @@
               :badge-color="ruleStatusBadgeColor(rule.status)"
             >
               <div slot="footer">
-                <f7-chip v-for="tag in rule.tags" :key="tag" :text="tag" media-bg-color="blue" style="margin-right: 6px">
+                <f7-chip v-for="tag in rule.tags.filter((t) => t !== 'Script')" :key="tag" :text="tag" media-bg-color="blue" style="margin-right: 6px">
                   <f7-icon slot="media" ios="f7:tag_fill" md="material:label" aurora="f7:tag_fill" ></f7-icon>
                 </f7-chip>
               </div>
@@ -99,7 +100,8 @@
       </f7-col>
     </f7-block>
     <f7-block v-if="ready && !noRuleEngine && !rules.length" class="service-config block-narrow">
-      <empty-state-placeholder icon="wand_rays" title="rules.title" text="rules.text" />
+      <empty-state-placeholder v-if="showScripts" icon="doc_plaintext" title="scripts.title" text="scripts.text" />
+      <empty-state-placeholder v-else icon="wand_stars" title="rules.title" text="rules.text" />
     </f7-block>
     <f7-fab v-show="ready && !showCheckboxes" position="right-bottom" slot="fixed" color="blue" href="add">
       <f7-icon ios="f7:plus" md="material:add" aurora="f7:plus"></f7-icon>
@@ -113,6 +115,7 @@ import RuleStatus from '@/components/rule/rule-status-mixin'
 
 export default {
   mixins: [RuleStatus],
+  props: ['showScripts'],
   data () {
     return {
       ready: false,
@@ -127,7 +130,7 @@ export default {
   },
   computed: {
     indexedRules () {
-      return this.rules.reduce((prev, rule, i, rules)=> {
+      return this.rules.reduce((prev, rule, i, rules) => {
         const initial = rule.name.substring(0, 1).toUpperCase()
         if (!prev[initial]) {
           prev[initial] = []
@@ -147,10 +150,14 @@ export default {
       this.loading = true
       this.$set(this, 'selectedItems', [])
       this.showCheckboxes = false
-      this.$oh.api.get('/rest/rules').then(data => {
+      this.$oh.api.get('/rest/rules' + (this.showScripts ? '?tags=Script' : '')).then(data => {
         this.rules = data.sort((a, b) => {
           return a.name.localeCompare(b.name)
         })
+
+        if (!this.showScripts) {
+          this.rules = this.rules.filter((r) => !r.tags || r.tags.indexOf('Script') < 0)
+        }
 
         this.loading = false
         this.ready = true

commit 57a389394f345eb0fe30983b79e48b505c7b1f2c

Fix rule code mode
Regression caused by #469.

Signed-off-by: Yannick Schaus <github@schaus.net>

bundles/org.openhab.ui/web/src/components/config/controls/script-editor.vue
@@ -255,7 +255,7 @@ export default {
           hint (cm, option) {
             if (self.mode.indexOf('application/vnd.openhab.uicomponent') === 0) {
               return componentsHint(cm, option, self.mode)
-            } else if (self.mode === 'application/vnd.openhab.rule') {
+            } else if (self.mode === 'application/vnd.openhab.rule+yaml') {
               return rulesHint(cm, option, self.mode)
             } else if (self.mode === 'application/vnd.openhab.thing+yaml') {
               return thingsHint(cm, option, self.mode)

bundles/org.openhab.ui/web/src/pages/settings/rules/rule-edit.vue
@@ -114,7 +114,7 @@
         </f7-block>
       </f7-tab>
       <f7-tab id="code" @tab:show="() => { this.currentTab = 'code'; toYaml() }" :tab-active="currentTab === 'code'">
-        <editor v-if="currentTab === 'code'" class="rule-code-editor" mode="application/vnd.openhab.rule" :value="ruleYaml" @input="(value) => ruleYaml = value" />
+        <editor v-if="currentTab === 'code'" class="rule-code-editor" mode="application/vnd.openhab.rule+yaml" :value="ruleYaml" @input="(value) => ruleYaml = value" />
         <!-- <pre class="yaml-message padding-horizontal" :class="[yamlError === 'OK' ? 'text-color-green' : 'text-color-red']">{{yamlError}}</pre> -->
       </f7-tab>
     </f7-tabs>