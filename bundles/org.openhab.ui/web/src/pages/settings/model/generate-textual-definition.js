90d37d0dfb19879b7fa5078e99f17892089cf1ed
Yannick Schaus <github@schaus.net>
2020-12-08 14:12:32 +0100

    Add Items from Thing: Expert Mode
    This adds a new "expert mode" to the
    "Create Equipment from Thing"/"Add Equipment to Model"/
    "Create Points from Thing"/"Add Points to Model"
    
    Instead of clicking through the channels and
    filling out items, a textual definition will be
    generated and put it the "Add Items from
    Textual Definition" page. That way, the expert
    user can add their items by editing text with the well-known syntax.
    
    Signed-off-by: Yannick Schaus <github@schaus.net>

---
+++ bundles/org.openhab.ui/web/src/pages/settings/model/generate-textual-definition.js
@@ -0,0 +1,74 @@
+import diacritic from 'diacritic'
+import { Points } from '@/assets/semantics'
+
+/**
+ * Generate a textual definition for the items provided by the "add from thing" page,
+ * for expert users who prefer to edit their items that way
+ */
+export default (thing, channelTypes, newEquipmentItem, parentGroupsForEquipment, parentGroupsForPoints) => {
+  const channelTypesMap = new Map(channelTypes.map(ct => [ct.UID, ct]))
+
+  let def = ''
+  if (newEquipmentItem && newEquipmentItem.name) {
+    def += `// Equipment representing thing:\n// ${thing.UID}\n`
+    def += `// (${thing.label})\n\n`
+
+    def += `Group ${newEquipmentItem.name} "${newEquipmentItem.label}" `
+    if (newEquipmentItem.category) def += `<${newEquipmentItem.category}> `
+    if (parentGroupsForEquipment.length) def += `(${parentGroupsForEquipment.join(', ')}) `
+    if (newEquipmentItem.tags.length) def += `[${newEquipmentItem.tags.map((t) => `"${t}"`).join(', ')}] `
+    def = def.trim() + '\n\n'
+  }
+
+  let lines = []
+  for (const channel of thing.channels) {
+    if (channel.kind !== 'STATE') continue
+    const channelType = channelTypesMap.get(channel.channelTypeUID)
+    let newItemName = (newEquipmentItem) ? newEquipmentItem.name : diacritic.clean(thing.label).replace(/[^0-9a-z]/gi, '')
+    newItemName += '_'
+    let suffix = channel.label || channel.id
+    if (thing.channels.filter((c) => c.label === suffix).length > 1) {
+      suffix = channel.id.replace('#', '_')
+    }
+    newItemName += diacritic.clean(suffix).replace(/[^0-9a-z_]/gi, '')
+    const defaultTags = (channel.defaultTags.length > 0) ? channel.defaultTags : channelType.tags
+    const newItem = {
+      channel: channel,
+      channelType: channelType,
+      name: newItemName,
+      label: channel.label || channelType.label,
+      groupNames: parentGroupsForPoints,
+      category: (channelType) ? channelType.category : '',
+      type: channel.itemType,
+      tags: (defaultTags.find((t) => Points.indexOf(t) >= 0)) ? defaultTags : [...defaultTags, 'Point']
+    }
+
+    let line = []
+    line.push(newItem.type)
+    line.push(newItem.name)
+    line.push(`"${newItem.label}"`)
+    if (channelType.advanced) line[0] = '// ' + line[0] // comment the advanced channels by default
+    line.push((newItem.category) ? `<${newItem.category}>` : '')
+    line.push((newItem.groupNames.length) ? `(${newItem.groupNames.join(', ')})` : '')
+    line.push((newItem.tags.length) ? `[${newItem.tags.map((t) => `"${t}"`).join(', ')}] ` : '')
+    line.push(`{ channel="${channel.uid}" }`)
+    lines.push(line)
+  }
+
+  if (!lines.length) return def
+
+  let columnsWidths = []
+  for (let c = 0; c < lines[0].length; c++) {
+    columnsWidths.push(Math.max(...lines.map((l) => l[c].length)) + 1)
+  }
+
+  def += '// Points:\n\n'
+  lines.forEach((l) => {
+    for (let c = 0; c < l.length; c++) {
+      def += l[c] + ' '.repeat(columnsWidths[c] - l[c].length)
+    }
+    def += '\n'
+  })
+
+  return def
+}
