3e95c345a75e2ecb5a44a4d69f0eb24130bf1255
eugen <freiter@gmail.com>
2020-12-10 23:29:05 +0100

    Add support for QR code context in configuration (#614)
    Also-by: Yannick Schaus <github@schaus.net>
    Signed-off-by: Eugen Freiter <freiter@gmx.de>

---
+++ bundles/org.openhab.ui/web/src/components/config/controls/parameter-qrcode.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <f7-list-item
+      :floating-label="$theme.md"
+      :title="configDescription.label"
+    >
+        <vue-qrcode :value="value" slot="media"/>
+    </f7-list-item>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['configDescription', 'value'],
+  components: {
+    'vue-qrcode': () => import(/* webpackChunkName: "vue-qrcode" */ 'vue-qrcode')
+  }
+}
+</script>
