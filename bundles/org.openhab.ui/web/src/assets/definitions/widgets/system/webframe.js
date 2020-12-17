e037b6d49a8f608c4f44d5102ce89b2360e74f56
Yannick Schaus <github@schaus.net>
2020-12-17 19:22:29 +0100

    Add web frame widget (#661)
    Allows to put a webpage in an iframe.
    
    Requested in https://community.openhab.org/t/oh3-show-html-content-in-an-custom-widget-iframe/110417.
    
    Signed-off-by: Yannick Schaus <github@schaus.net>

---
+++ bundles/org.openhab.ui/web/src/assets/definitions/widgets/system/webframe.js
@@ -0,0 +1,6 @@
+import { pt, pn } from '../helpers'
+
+export default () => [
+  pt('src', 'Source URL', 'URL to show in the frame'),
+  pn('height', 'Height', 'Height of the frame (by default 300px)')
+]
---
+++ bundles/org.openhab.ui/web/src/components/widgets/system/oh-webframe.vue
@@ -0,0 +1,19 @@
+<template>
+  <iframe class="oh-webframe" v-bind="config" :style="{ height: config.height || '300px' }" />
+</template>
+
+<style lang="stylus">
+.oh-webframe
+  width 100%
+  border none
+</style>
+
+<script>
+import mixin from '../widget-mixin'
+import { OhWebframeDefinition } from '@/assets/definitions/widgets/system'
+
+export default {
+  mixins: [mixin],
+  widget: OhWebframeDefinition
+}
+</script>
