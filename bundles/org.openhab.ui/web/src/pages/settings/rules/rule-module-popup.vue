4422535250eccff7781576716c81d779c3c0d338
Yannick Schaus <github@schaus.net>
2020-12-20 14:15:45 +0100

    Remove module type override
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- bundles/org.openhab.ui/web/src/pages/settings/rules/rule-module-popup.vue
+++ bundles/org.openhab.ui/web/src/pages/settings/rules/rule-module-popup.vue
@@ -97,8 +97,7 @@ export default {
   },
   methods: {
     setModuleType (val, clearConfig) {
-      let moduleType = (typeof val === 'string') ? this.moduleTypes[this.currentSection].find((t) => t.uid === val) : val
-      if (val === 'core.ItemStateUpdateAction') moduleType = { uid: 'core.ItemStateUpdateAction', configDescriptions: [] }
+      const moduleType = (typeof val === 'string') ? this.moduleTypes[this.currentSection].find((t) => t.uid === val) : val
       this.ruleModule.type = moduleType.uid
       this.$set(this, 'currentRuleModuleType', moduleType)
       if (clearConfig) this.$set(this.ruleModule, 'configuration', {})
