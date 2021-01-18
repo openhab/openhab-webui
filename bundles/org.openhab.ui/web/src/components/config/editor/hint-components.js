0b209d34a02d4b2bb76fe253609df4be4bc8caec
Yannick Schaus <github@schaus.net>
2021-01-18 16:08:25 +0100

    Allow managed links to non-managed items
    Includes both linking and unlinking.
    Add error messages when trying to remove a non-managed
    link.
    
    Fixes #685.
    Fixes #693.
    Fixes https://github.com/openhab/openhab-core/issues/2130.
    
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- bundles/org.openhab.ui/web/src/components/config/editor/hint-components.js
+++ bundles/org.openhab.ui/web/src/components/config/editor/hint-components.js
@@ -97,6 +97,7 @@ function hintExpression (cm, line) {
         { text: 'items.', displayText: 'items', description: 'Access to item states' },
         { text: 'props.', displayText: 'props', description: 'Access to the props of the parent root component' },
         { text: 'vars.', displayText: 'vars', description: 'Access to context vars' },
+        { text: 'loop.', displayText: 'loop', description: 'Access to oh-repeater loop variables' },
         { text: 'JSON.', displayText: 'JSON', description: 'Access to the JSON object functions' },
         { text: 'Math.', displayText: 'Math', description: 'Access to the Math object functions' },
         { text: 'Number.', displayText: 'Number', description: 'Access to the Number object functions' },
