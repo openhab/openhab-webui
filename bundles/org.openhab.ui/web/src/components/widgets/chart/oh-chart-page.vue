21ddf386877bdd8cdd07bea6bfdd3bdba159b634
Boris Krivonog <boris.krivonog@inova.si>
2021-01-18 21:27:33 +0100

    Re-added handling of tabs, toolbars, sheets for chart page.
    Signed-off-by: Boris Krivonog <boris.krivonog@inova.si>

--- bundles/org.openhab.ui/web/src/components/widgets/chart/oh-chart-page.vue
+++ bundles/org.openhab.ui/web/src/components/widgets/chart/oh-chart-page.vue
@@ -1,11 +1,24 @@
 <template>
-  <oh-chart class="oh-chart-page-chart" :context="this.context" />
+  <oh-chart 
+    class="oh-chart-page-chart" 
+    :class="{ 'with-tabbar': context.tab, 'with-toolbar': context.analyzer }"
+    :context="this.context" />
 </template>
 
 <style lang="stylus">
 .oh-chart-page-chart
+  position absolute !important
   background-color white
-  height: 100% !important
+  overflow-x hidden
+  top calc(var(--f7-safe-area-top) + var(--f7-navbar-height))
+  width 100%
+  height calc(100% - var(--f7-safe-area-top) - var(--f7-navbar-height)) !important
+  &.with-tabbar
+    height calc(100% - var(--f7-safe-area-top) - var(--f7-safe-area-bottom) - var(--f7-navbar-height) - var(--f7-tabbar-labels-height)) !important
+  &.with-toolbar
+    height calc(100% - var(--f7-safe-area-top) - var(--f7-safe-area-bottom) - var(--f7-navbar-height) - var(--f7-toolbar-height)) !important
+  &.sheet-opened
+    height calc(100% - var(--f7-safe-area-top) - var(--f7-safe-area-bottom) - var(--f7-navbar-height) - var(--f7-sheet-height)) !important
 </style>
 
 <script>
