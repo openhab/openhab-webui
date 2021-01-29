23e620c7000aa88345e07d4fb8619046c02e145e
Brian Homeyer <bhomeyer@gmail.com>
2021-01-29 08:51:08 -0500

    Remove imports for logger
    Signed-off-by: Brian Homeyer <bhomeyer@gmail.com>

--- bundles/org.openhab.ui.basic/src/main/java/org/openhab/ui/basic/internal/render/ImageRenderer.java
+++ bundles/org.openhab.ui.basic/src/main/java/org/openhab/ui/basic/internal/render/ImageRenderer.java
@@ -32,8 +32,6 @@ import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * This is an implementation of the {@link WidgetRenderer} interface, which
