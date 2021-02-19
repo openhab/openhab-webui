1f4b21ee88531b17ed149ebd5bfc3f732431fe00
Eiko Wagenknecht <eiko.wagenknecht@web.de>
2021-02-19 16:03:06 +0100

    Add missing link to f7 input docs
    Signed-off-by: Eiko Wagenknecht <eiko.wagenknecht@web.de>

--- bundles/org.openhab.ui/web/src/assets/definitions/widgets/system/input.js
+++ bundles/org.openhab.ui/web/src/assets/definitions/widgets/system/input.js
@@ -2,8 +2,8 @@ import { pi, pb, pt } from '../helpers.js'
 
 export default () => [
   pt('name', 'Name', 'Input name'),
-  pt('type', 'Type', 'Type of input (see f7-input docs)'),
-  pt('inputmode', 'Input Mode', 'Type of data that might be entered: see <a class="external text-color-blue" target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/inputmode">MDN docs</a>'),
+  pt('type', 'Type', 'Type of input (see <a class="external text-color-blue" target="_blank" href="https://framework7.io/docs/inputs.html#supported-inputs">f7 input docs</a>)'),
+  pt('inputmode', 'Input Mode', 'Type of data that might be entered (see <a class="external text-color-blue" target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/inputmode">MDN docs</a>)'),
   pt('placeholder', 'Placeholder', 'Placeholder text'),
   pb('sendButton', 'Send button', 'Display Send button to update the state with a command (needs a configured item)'),
   pb('clearButton', 'Clear button', 'Display input clear button when applicable'),
