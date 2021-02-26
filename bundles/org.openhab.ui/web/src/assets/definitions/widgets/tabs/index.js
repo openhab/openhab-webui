cd72f919aab7e923c406f0aae16397cc3a6b0a40
Eiko Wagenknecht <eiko.wagenknecht@web.de>
2021-02-26 18:11:22 +0100

    Add link to material items (#916)
    Signed-off-by: Eiko Wagenknecht <eiko.wagenknecht@web.de>

--- bundles/org.openhab.ui/web/src/assets/definitions/widgets/tabs/index.js
+++ bundles/org.openhab.ui/web/src/assets/definitions/widgets/tabs/index.js
@@ -4,8 +4,8 @@ import { WidgetDefinition, pt } from '../helpers'
 
 export const OhTabDefinition = () => new WidgetDefinition('oh-tab', 'Tab', 'Displays a widget in a tab')
   .params([
-    pt('title', 'Title', 'The title of the tab'),
-    pt('icon', 'Icon', 'The icon on the tab: use <code>f7:iconName</code> (<a class="external text-color-blue" target="_blank" href="https://framework7.io/icons/">Framework7 icon</a>)'),
-    pt('page', 'Page', 'The page to display').c('page'),
-    pt('pageConfig', 'Page Configuration', 'The parameters (props) to configure the page, if any').c('props')
+    pt('title', 'Title', 'Title of the tab'),
+    pt('icon', 'Icon', 'Use <code>f7:iconName</code> (<a class="external text-color-blue" target="_blank" href="https://framework7.io/icons/">Framework7 icon</a>) or <code>material:iconName</code> (<a class="external text-color-blue" target="_blank" href="https://jossef.github.io/material-design-icons-iconfont/">Material icon</a>)'),
+    pt('page', 'Page', 'Page to display').c('page'),
+    pt('pageConfig', 'Page Configuration', 'Parameters (props) to configure the page, if any').c('props')
   ])
