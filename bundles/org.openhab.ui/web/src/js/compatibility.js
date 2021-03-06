2142c8eae4613909b3051c0b8816257ca97339af
Boris Krivonog <boris.krivonog@inova.si>
2021-03-06 01:14:18 +0100

    Added a fallback Array.prototype.flat implementation (#935)
    Fixes #647.
    
    Signed-off-by: Boris Krivonog <boris.krivonog@inova.si>

---
+++ bundles/org.openhab.ui/web/src/js/compatibility.js
@@ -0,0 +1,9 @@
+
+if (!Array.prototype.flat) {
+  // eslint-disable-next-line no-extend-native
+  Array.prototype.flat = function (depth = 1) {
+    return this.reduce(function (flat, toFlatten) {
+      return flat.concat((Array.isArray(toFlatten) && (depth > 1)) ? toFlatten.flat(depth - 1) : toFlatten)
+    }, [])
+  }
+}
