e9e55060db1b8d31a019be98f089e2a96d68538f
hubsif <hubsif@users.noreply.github.com>
2021-03-27 19:32:15 +0100

    Show copy icon for thing and channel uids, fixes #396, fixes #475 (#980)
    Signed-off-by: Hubert Nusser <hubsif@gmx.de>

---
+++ bundles/org.openhab.ui/web/src/components/util/clipboard-icon.vue
@@ -0,0 +1,29 @@
+<template>
+  <f7-icon class="clipboard-icon" v-bind="$attrs" f7="square_on_square" :size="size ? size : 12" v-clipboard="value" v-clipboard:success="success" />
+</template>
+
+<style lang="stylus">
+.clipboard-icon
+  cursor: copy
+</style>
+
+<script>
+import Vue from 'vue'
+import Clipboard from 'v-clipboard'
+
+Vue.use(Clipboard)
+
+export default {
+  props: ['value', 'size'],
+  methods: {
+    success ({ value, event }) {
+      event.stopPropagation()
+      this.$f7.toast.create({
+        text: 'Copied to clipboard',
+        destroyOnClose: true,
+        closeTimeout: 2000
+      }).open()
+    }
+  }
+}
+</script>
