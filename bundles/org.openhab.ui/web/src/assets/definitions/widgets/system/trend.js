27a13dbe7caa0a5c2cc3b30202bbc3fc9118bf00
dominik2579 <82102912+dominik2579@users.noreply.github.com>
2021-04-07 19:06:00 +0200

    Update trend.js
    Signed-off-by: Dominik Budde <dominik.budde@googlemail.com>

--- bundles/org.openhab.ui/web/src/assets/definitions/widgets/system/trend.js
+++ bundles/org.openhab.ui/web/src/assets/definitions/widgets/system/trend.js
@@ -4,5 +4,6 @@ export default () => [
   pi('trendItem', 'Trend Line Item', 'Item to show as a trend line in the background'),
   pt('trendStrokeWidth', 'Trend Stroke Width', 'Thickness of the trend line').a(),
   pt('trendWidth', 'Trend Line Width', 'Width of the trend line (leave blank to set automatically)').a(),
-  pt('trendGradient', 'Trend Line Gradient', 'Colors of the trend line (see <a target="_blank" class="external text-color-blue" href="https://github.com/QingWei-Li/vue-trend#props">vue-trend</a>)').a()
+  pt('trendGradient', 'Trend Line Gradient', 'Colors of the trend line (see <a target="_blank" class="external text-color-blue" href="https://github.com/QingWei-Li/vue-trend#props">vue-trend</a>)').a(),
+  pt('trendSampling', 'Trend Line Sampling', 'Amount of minutes between each point of the trendline (default: 60). Affected by persistence strategies different from "every minute"').a()
 ]
