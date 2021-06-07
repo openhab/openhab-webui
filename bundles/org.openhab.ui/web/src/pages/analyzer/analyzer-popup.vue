76defda5d525fb91f6721b055423dbe6d94396fe
Yannick Schaus <github@schaus.net>
2021-06-07 18:45:31 +0200

    Fix analyzer back button if another popup was opened
    Framework7 would not consider an async route to be a modal route,
    leading to critical code not being executed and thus causing #1068.
    This regression was introduced when the routes were changed to async in #948.
    
    So introduce a new component and lazy load the analyzer inside it, so
    there's a minimal impact on the app entry point.
    
    Changed some z-indexes to make sure the popup backdrop is displayed between
    the analyzer popup and the item picker popup.
    
    Fixes #1068.
    Fixes #1067.
    
    Signed-off-by: Yannick Schaus <github@schaus.net>

---
+++ bundles/org.openhab.ui/web/src/pages/analyzer/analyzer-popup.vue
@@ -0,0 +1,17 @@
+<template>
+  <f7-popup tablet-fullscreen :backdrop="false" class="analyzer-popup" @popup:close="$refs.analyzer.onClose()">
+    <analyzer ref="analyzer" />
+  </f7-popup>
+</template>
+
+<style lang="stylus" scoped>
+.analyzer-popup
+  z-index 10490
+</style>
+<script>
+export default {
+  components: {
+    'analyzer': () => import(/* webpackChunkName: "analyzer" */ './analyzer.vue')
+  }
+}
+</script>
