1fd4146d4bec0dcefbfc5c8ad3e804a49f346538
Dmitry Belyaev <beldimon@gmail.com>
2021-11-29 16:21:46 +0300

    Update list.js
    Add options to change background, Headline, and subheadline in custom list items.

--- bundles/org.openhab.ui/web/src/assets/definitions/widgets/system/list.js
+++ bundles/org.openhab.ui/web/src/assets/definitions/widgets/system/list.js
@@ -3,5 +3,9 @@ import { pb } from '../helpers.js'
 export default () => [
   pb('simpleList', 'Simple List', 'Use for simple lists'),
   pb('mediaList', 'Media List', 'Use for list with rich list items with icons'),
-  pb('accordionList', 'Accordion List', 'Use for lists with accordion (collapsible) items')
+  pb('accordionList', 'Accordion List', 'Use for lists with accordion (collapsible) items'),
+  pt('background', 'Background', 'Set background of the card'),
+  pt('headline', 'Headline', 'Set headline'),
+  pt('subheadline', 'Subheadline', 'Set subheadline')
+
 ]
