75d0d883ebbf5db4e3c3e657f9281dba7e6dab7e
Gautier Taravella <tarag@mailbox.org>
2022-01-19 20:57:06 +0100

    Moved items ancestors comparison to separate function.
    Signed-off-by: Gautier Taravella <tarag@mailbox.org>

--- bundles/org.openhab.ui/web/src/components/widgets/widget-order.js
+++ bundles/org.openhab.ui/web/src/components/widgets/widget-order.js
@@ -7,10 +7,23 @@ export function compareItems (i1, i2) {
   const order2 = (i2.metadata && i2.metadata.widgetOrder) ? i2.metadata.widgetOrder.value : Infinity
   const widgetOrder = (order1 !== order2) ? ((order1 < order2) ? -1 : 1) : 0
 
-  // When comparing Location Items compare on ancestors first
+  // Unless comparing Location Items simply return the order based on widgetOrder metadata if determined
   if (!isLocationContext && widgetOrder !== 0) return widgetOrder
 
   // Compare ancestors in Model (starting at the top level)
+  const modelOrder = compareParents(i1, i2)
+  if (modelOrder !== 0) return modelOrder
+  if (widgetOrder !== 0) return widgetOrder // For Locations Items, own-item widgetOrder only used to compare Items sharing the same parent location
+
+  // Compare label/name
+  const nameOrLabel1 = i1.label || i1.name
+  const nameOrLabel2 = i2.label || i2.name
+  return nameOrLabel1.localeCompare(nameOrLabel2)
+}
+
+// Compares items based on widgetOrder or lexicographical order of their ancestors in Model (starting at the top level)
+// Returns 0 if both items are siblings.
+export function compareParents (i1, i2) {
   let modelOrder = 0
   if (i1.modelPath && i2.modelPath) {
     const minDepth = Math.min(i1.modelPath.length, i2.modelPath.length) // Compare shortest path
@@ -34,13 +47,6 @@ export function compareItems (i1, i2) {
 
     // A parent compares greater than its children
     if (modelOrder === 0 && (i1.modelPath.length !== i2.modelPath.length)) modelOrder = (i1.modelPath.length < i2.modelPath.length) ? -1 : 1
-
-    if (modelOrder !== 0) return modelOrder
   }
-  if (widgetOrder !== 0) return widgetOrder // For Locations Items, own-item widgetOrder only used to compare Items sharing the same parent location
-
-  // Compare label/name
-  const nameOrLabel1 = i1.label || i1.name
-  const nameOrLabel2 = i2.label || i2.name
-  return nameOrLabel1.localeCompare(nameOrLabel2)
+  return modelOrder
 }
