openhab/openhab-webui

commit 9a19a2549fdf4c26462c5b29c6292fc16948cc8b

Provide another configurable action on oh-label-card and oh-button components upon tap and hold (or contextual menu call on desktop). (#1313)
Signed-off-by: Gautier Taravella <tarag@mailbox.org>

bundles/org.openhab.ui/web/src/assets/definitions/widgets/standard/cards.js
@@ -18,6 +18,7 @@ import TrendParameters from '../system/trend.js'
 export const OhLabelCardDefinition = () => new WidgetDefinition('oh-label-card', 'Label Card', 'Display the state of an item in a card')
   .paramGroup(CardParameterGroup(), CardParameters())
   .paramGroup(actionGroup(), actionParams())
+  .paramGroup(actionGroup('Tap Hold', 'Action performed when tapping and holding card (or calling contextual menu on desktop)', 'taphold'), actionParams(null, 'taphold')).a()
   .paramGroup(pg('label', 'Label', 'Parameters of the label'), [
     pi('item', 'Item', 'Item to display'),
     pt('label', 'Label', 'Display this text (or expression result) instead of the item\'s state'),

bundles/org.openhab.ui/web/src/assets/definitions/widgets/system/index.js
@@ -7,6 +7,7 @@ const ClearVariableParameter = pb('clearVariable', 'Clear Variable After Action'
 import ButtonParameters from './button.js'
 export const OhButtonDefinition = () => new WidgetDefinition('oh-button', 'Button', 'Button performing an action')
   .paramGroup(actionGroup(), actionParams())
+  .paramGroup(actionGroup('Tap Hold', 'Action performed when tapping and holding card (or calling contextual menu on desktop)', 'taphold'), actionParams(null, 'taphold')).a()
   .params([...ButtonParameters(), VariableParameter, ClearVariableParameter])
 
 import ChartParameters from './chart.js'

bundles/org.openhab.ui/web/src/components/widgets/standard/oh-label-card.vue
@@ -3,7 +3,7 @@
     <f7-card-header v-if="config.title">
       <div>{{ config.title }}</div>
     </f7-card-header>
-    <f7-card-content ref="cardContent" @click.native="performAction" class="label-card-content" :style="{ background: config.background }" :class="{ 'vertical-arrangement': config.vertical }">
+    <f7-card-content ref="cardContent" @click.native="performAction" @taphold.native="onTaphold($event)" @contextmenu.native="onContextMenu($event)" class="label-card-content" :style="{ background: config.background }" :class="{ 'vertical-arrangement': config.vertical }">
       <oh-trend v-if="config.trendItem" :key="'trend' + config.item" class="trend" :width="($refs.cardContent) ? $refs.cardContent.$el.clientWidth : 0" :context="context" />
       <f7-list>
         <f7-list-item :link="config.action ? true : false" no-chevron>

bundles/org.openhab.ui/web/src/components/widgets/system/oh-button.vue
@@ -1,5 +1,5 @@
 <template>
-  <f7-button v-bind="config" @click="clicked">
+  <f7-button v-bind="config" @click="clicked" @taphold.native="onTaphold($event)" @contextmenu.native="onContextMenu($event)">
     <template v-if="context.component.slots && context.component.slots.default">
       <generic-widget-component :context="childContext(slotComponent)" v-for="(slotComponent, idx) in context.component.slots.default" :key="'default-' + idx" />
     </template>

bundles/org.openhab.ui/web/src/components/widgets/widget-actions.js
@@ -30,7 +30,7 @@ export const actionsMixin = {
     },
     performAction (evt, prefix) {
       if (!this.context || !this.config) return
-      const actionPropsParameterGroup = this.config[((prefix) ? prefix += '_' : '') + 'actionPropsParameterGroup']
+      const actionPropsParameterGroup = this.config[((prefix) ? prefix + '_' : '') + 'actionPropsParameterGroup']
       const actionConfig = (actionPropsParameterGroup) ? this.evaluateExpression('$props', this.context.props) : this.config
       prefix = (actionPropsParameterGroup) ? actionPropsParameterGroup.replace(/action/gi, '') : prefix
       prefix = (prefix) ? prefix += '_' : ''
@@ -231,6 +231,17 @@ export const actionsMixin = {
           console.log('Invalid action: ' + action)
           break
       }
+      return true
+    },
+    onTaphold (event) {
+      this.performAction(event, 'taphold')
+    },
+    onContextMenu (event) {
+      if (this.performAction(event, 'taphold')) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+      // System contextual menu will be displayed
     }
   }
 }

commit 4fdc1543b74b4a4c3d33d5cab51b8fef4416c98a

Fix incorrect parameter group definition
Bug introduced in #1313 and breaking config sheets in various parts of the UI.

Fixes #1343.
Fixes  https://github.com/openhab/openhab-core/issues/2850.

Signed-off-by: Yannick Schaus <github@schaus.net>

bundles/org.openhab.ui/web/src/assets/definitions/widgets/helpers.js
@@ -173,10 +173,10 @@ export function WidgetDefinition (name, label, description, icon) {
 }
 
 WidgetDefinition.prototype = {
-  paramGroup (group, params) {
+  paramGroup (group, params, advanced) {
     this.props.parameterGroups.push(group)
     if (params) {
-      this.props.parameters.push(...params.map((p) => p.g(group.name)))
+      this.props.parameters.push(...params.map((p) => (advanced) ? p.g(group.name).a() : p.g(group.name)))
     }
     return this
   },

bundles/org.openhab.ui/web/src/assets/definitions/widgets/standard/cards.js
@@ -18,7 +18,7 @@ import TrendParameters from '../system/trend.js'
 export const OhLabelCardDefinition = () => new WidgetDefinition('oh-label-card', 'Label Card', 'Display the state of an item in a card')
   .paramGroup(CardParameterGroup(), CardParameters())
   .paramGroup(actionGroup(), actionParams())
-  .paramGroup(actionGroup('Tap Hold', 'Action performed when tapping and holding card (or calling contextual menu on desktop)', 'taphold'), actionParams(null, 'taphold')).a()
+  .paramGroup(actionGroup('Tap Hold', 'Action performed when tapping and holding card (or calling contextual menu on desktop)', 'taphold'), actionParams(null, 'taphold'), true)
   .paramGroup(pg('label', 'Label', 'Parameters of the label'), [
     pi('item', 'Item', 'Item to display'),
     pt('label', 'Label', 'Display this text (or expression result) instead of the item\'s state'),

bundles/org.openhab.ui/web/src/assets/definitions/widgets/system/index.js
@@ -7,7 +7,7 @@ const ClearVariableParameter = pb('clearVariable', 'Clear Variable After Action'
 import ButtonParameters from './button.js'
 export const OhButtonDefinition = () => new WidgetDefinition('oh-button', 'Button', 'Button performing an action')
   .paramGroup(actionGroup(), actionParams())
-  .paramGroup(actionGroup('Tap Hold', 'Action performed when tapping and holding card (or calling contextual menu on desktop)', 'taphold'), actionParams(null, 'taphold')).a()
+  .paramGroup(actionGroup('Tap Hold', 'Action performed when tapping and holding card (or calling contextual menu on desktop)', 'taphold'), actionParams(null, 'taphold'), true)
   .params([...ButtonParameters(), VariableParameter, ClearVariableParameter])
 
 import ChartParameters from './chart.js'