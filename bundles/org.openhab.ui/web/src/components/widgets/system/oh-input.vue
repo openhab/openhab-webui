5d292e17b4dcb77c985be77942472d191bdcd917
Mark Herwege <mark.herwege@telenet.be>
2022-04-30 22:07:59 +0200

    review feedback
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

--- bundles/org.openhab.ui/web/src/components/widgets/system/oh-input.vue
+++ bundles/org.openhab.ui/web/src/components/widgets/system/oh-input.vue
@@ -37,7 +37,7 @@ export default {
       } else if (this.config.sendButton && this.pendingUpdate !== null) {
         return this.pendingUpdate
       } else if (this.config.item && this.context.store[this.config.item].state !== 'NULL' && this.context.store[this.config.item].state !== 'UNDEF') {
-        if (this.config.displayState) {
+        if (this.config.useDisplayState) {
           return this.context.store[this.config.item].displayState || this.context.store[this.config.item].state
         } else {
           return this.context.store[this.config.item].state
