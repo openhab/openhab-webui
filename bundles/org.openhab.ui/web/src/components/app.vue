4624ff0f88227bd31477e6809266d72a3c6b718d
Wouter Born <github@maindrain.net>
2022-06-22 00:10:49 +0200

    [Main UI] Add support for webaudio
    Fixes #743
    
    Signed-off-by: Wouter Born <github@maindrain.net>

--- bundles/org.openhab.ui/web/src/components/app.vue
+++ bundles/org.openhab.ui/web/src/components/app.vue
@@ -555,6 +555,50 @@ export default {
         ev.stopPropagation()
         ev.preventDefault()
       }
+    },
+    startEventSource () {
+      this.eventSource = this.$oh.sse.connect('/rest/events?topics=openhab/webaudio/playurl', null, (event) => {
+        const topicParts = event.topic.split('/')
+        switch (topicParts[2]) {
+          case 'playurl':
+            this.playAudioUrl(JSON.parse(event.payload))
+            break
+        }
+      }, () => {
+        // in case of error, try reloading to refresh
+        this.stopEventSource()
+        this.startEventSource()
+      })
+    },
+    stopEventSource () {
+      this.$oh.sse.close(this.eventSource)
+      this.eventSource = null
+    },
+    playAudioUrl (audioUrl) {
+      let context
+      try {
+        window.AudioContext = window.AudioContext || window.webkitAudioContext
+        if (typeof (window.AudioContext) !== 'undefined') {
+          context = new AudioContext()
+        }
+        console.log('Playing audio URL: ' + audioUrl)
+        this.$oh.api.getPlain(audioUrl, '', '*/*', 'arraybuffer').then((data) => {
+          context.decodeAudioData(data, function (buffer) {
+            let source = context.createBufferSource()
+            source.buffer = buffer
+            source.connect(context.destination)
+            source.onended = function () {
+              context.close()
+            }
+            source.start(0)
+          })
+        })
+      } catch (e) {
+        console.warn('Error while playing audio URL: ' + e.toString())
+        if (context) {
+          context.close()
+        }
+      }
     }
   },
   created () {
@@ -656,6 +700,8 @@ export default {
       if (window) {
         window.addEventListener('keydown', this.keyDown)
       }
+
+      this.startEventSource()
     })
   }
 }
