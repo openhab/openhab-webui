c5d0521c089793ad623962eb0628fa8c14fbcf17
Yannick Schaus <github@schaus.net>
2022-06-22 15:26:59 +0200

    Adjust to support all modals
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- bundles/org.openhab.ui/web/src/components/widgets/widget-foreground-service.js
+++ bundles/org.openhab.ui/web/src/components/widgets/widget-foreground-service.js
@@ -6,16 +6,19 @@ export default {
     }
   },
   mounted () {
-    // determine the current page - support: https://caniuse.com/?search=closest
+    const isInModal = this.$el.closest('.framework7-modals')
+    if (isInModal) {
+      this.inForeground = true
+      this.startForegroundActivity()
+      return
+    }
+
     this.pageEl = this.$el.closest('.page')
-    const isPopup = this.$el.closest('.popup') !== null
-    // start the foreground activity immediately if the page
-    // is already in the foreground when the widget is mounted
-    // or is being shown within a popup
-    if (this.pageEl.classList.contains('page-current') || isPopup) {
+    if (this.pageEl && this.pageEl.classList.contains('page-current')) {
       this.inForeground = true
       this.startForegroundActivity()
     }
+
     this.$f7.on('pageAfterIn', this.onPageAfterIn)
     this.$f7.on('pageBeforeOut', this.onPageBeforeOut)
   },
@@ -26,10 +29,10 @@ export default {
   },
   methods: {
     startForegroundActivity () {
-      // overide this in your widget
+      // override this in your widget
     },
     stopForegroundActivity () {
-      // overide this in your widget
+      // override this in your widget
     },
     onPageAfterIn (page) {
       if (page.el === this.pageEl) {
