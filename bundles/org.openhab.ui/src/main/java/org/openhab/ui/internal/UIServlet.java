5edec3bc90aaa158a2fa51545cbd681962517d9d
Dan Cunningham <dan@digitaldan.com>
2022-10-13 14:52:49 -0700

    Use the same last-modifed time for all bundle assets
    Signed-off-by: Dan Cunningham <dan@digitaldan.com>

--- bundles/org.openhab.ui/src/main/java/org/openhab/ui/internal/UIServlet.java
+++ bundles/org.openhab.ui/src/main/java/org/openhab/ui/internal/UIServlet.java
@@ -12,14 +12,16 @@
  */
 package org.openhab.ui.internal;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.channels.ReadableByteChannel;
 import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.Map;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.UnavailableException;
 import javax.servlet.http.HttpServletRequest;
@@ -65,6 +67,7 @@ public class UIServlet extends DefaultServlet {
 
     private final HttpContext defaultHttpContext;
     private final HttpService httpService;
+    private long bundleModifiedTime = 0;
     private @Nullable ContextHandler contextHandler;
 
     @Activate
@@ -79,6 +82,7 @@ public class UIServlet extends DefaultServlet {
         try {
             logger.debug("Starting up {} at {}", getClass().getSimpleName(), SERVLET_NAME);
             httpService.registerServlet(SERVLET_NAME, this, new Hashtable<>(), defaultHttpContext);
+            bundleModifiedTime = (System.currentTimeMillis() / 1000) * 1000; // round milliseconds
         } catch (NamespaceException e) {
             logger.error("Error during servlet registration - alias {} already in use", SERVLET_NAME, e);
         } catch (ServletException e) {
@@ -93,6 +97,7 @@ public class UIServlet extends DefaultServlet {
 
     @Override
     public void init() throws UnavailableException {
+
         setInitParameter("acceptRanges", "true");
         setInitParameter("dirAllowed", "false");
         setInitParameter("redirectWelcome", "false");
@@ -103,24 +108,15 @@ public class UIServlet extends DefaultServlet {
     }
 
     @Override
-    protected ContextHandler initContextHandler(@Nullable ServletContext servletContext) {
-        if (servletContext instanceof ContextHandler.Context) {
-            return ((ContextHandler.Context) servletContext).getContextHandler();
-        } else {
-            throw new IllegalArgumentException("The servletContext " + servletContext + " "
-                    + servletContext.getClass().getName() + " is not " + ContextHandler.Context.class.getName());
-        }
-    }
-
-    @Override
     public @Nullable Resource getResource(@NonNullByDefault({}) String name) {
         logger.debug("getResource: {}", name);
         ContextHandler contextHandler = this.contextHandler;
         if (contextHandler == null) {
             return null;
         }
-        URL url = null;
+
         if (name.startsWith(STATIC_PATH) && !name.endsWith("/")) {
+            URL url = null;
             try {
                 url = new java.io.File(STATIC_BASE + name.substring(new String(STATIC_PATH).length())).toURI().toURL();
                 logger.trace("Serving static file from {}", url);
@@ -128,8 +124,15 @@ public class UIServlet extends DefaultServlet {
                 logger.error("Error while serving static content: {}", e.getMessage());
                 url = defaultHttpContext.getResource(APP_BASE + name);
             }
+            try {
+                logger.debug("getResource returning {}", url);
+                return contextHandler.newResource(url);
+            } catch (IOException e) {
+                logger.error("Error while serving content: {}", e.getMessage());
+                return null;
+            }
         } else {
-            url = defaultHttpContext.getResource(APP_BASE + name);
+            URL url = defaultHttpContext.getResource(APP_BASE + name);
             // if we don't find a resource, and its not a check for a compressed version, return Vue.js base for
             // routing
             if (url == null && !Arrays.stream(COMPRESS_EXT).anyMatch(entry -> name.endsWith(entry))) {
@@ -138,13 +141,13 @@ public class UIServlet extends DefaultServlet {
                 // which breaks Vue.js routing when reloading the browser on some pages
                 url = defaultHttpContext.getResource(APP_BASE);
             }
-        }
-        try {
-            logger.debug("getResource returning {}", url);
-            return contextHandler.newResource(url);
-        } catch (IOException e) {
-            logger.error("Error while serving content: {}", e.getMessage());
-            return null;
+            try {
+                logger.debug("getResource returning {}", url);
+                return url == null ? null : new ResourceWrapper(contextHandler.newResource(url));
+            } catch (IOException e) {
+                logger.error("Error while serving content: {}", e.getMessage());
+                return null;
+            }
         }
     }
 
@@ -172,5 +175,95 @@ public class UIServlet extends DefaultServlet {
 
     private void setInitParameter(String name, String value) {
         getServletContext().setInitParameter(CONTEXT_INIT + name, value);
+    }
+
+    /**
+     *
+     * Wraps a resource and returns a consistent time for bundled files
+     *
+     */
+    class ResourceWrapper extends Resource {
+
+        private Resource baseResource;
+
+        public ResourceWrapper(Resource baseResource) {
+            this.baseResource = baseResource;
+        }
+
+        @Override
+        public long lastModified() {
+            return bundleModifiedTime;
+        }
+
+        @Override
+        public boolean isContainedIn(@Nullable Resource r) throws MalformedURLException {
+            return baseResource.isContainedIn(r);
+        }
+
+        @Override
+        public void close() {
+            baseResource.close();
+        }
+
+        @Override
+        public boolean exists() {
+            return baseResource.exists();
+        }
+
+        @Override
+        public boolean isDirectory() {
+            return baseResource.isDirectory();
+        }
+
+        @Override
+        public long length() {
+            return baseResource.length();
+        }
+
+        @Override
+        public URL getURL() {
+            return baseResource.getURL();
+        }
+
+        @Override
+        public File getFile() throws IOException {
+            return baseResource.getFile();
+        }
+
+        @Override
+        public String getName() {
+            return baseResource.getName();
+        }
+
+        @Override
+        public InputStream getInputStream() throws IOException {
+            return baseResource.getInputStream();
+        }
+
+        @Override
+        public ReadableByteChannel getReadableByteChannel() throws IOException {
+            return baseResource.getReadableByteChannel();
+        }
+
+        @Override
+        public boolean delete() throws SecurityException {
+            return baseResource.delete();
+        }
+
+        @Override
+        public boolean renameTo(@Nullable Resource dest) throws SecurityException {
+            return baseResource.renameTo(dest);
+        }
+
+        @Override
+        public String[] list() {
+            return baseResource.list();
+        }
+
+        @Override
+        public Resource addPath(@Nullable String path) throws IOException, MalformedURLException {
+            return baseResource.addPath(path);
+        }
+
     }
 }
