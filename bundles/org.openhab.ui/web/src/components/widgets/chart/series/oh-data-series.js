c179f1b342a1b801c018d16aa21a6b62604585a4
Tobias Bräutigam <tbraeutigam@gmail.com>
2022-11-05 13:52:19 +0100

    Revert "Fix parsing arrays - cache being overwritten. Remove redundant array evaluate. (#1531)"
    This reverts commit e4a98517132656fb1ef4e621b6138b222a7145e1.

--- bundles/org.openhab.ui/web/src/components/widgets/chart/series/oh-data-series.js
+++ bundles/org.openhab.ui/web/src/components/widgets/chart/series/oh-data-series.js
@@ -8,6 +8,22 @@ export default {
     if (!component.config || typeof component.config !== 'object') return {}
 
     const dataSeriesId = ComponentId.get(component)
-    return chart.evaluateExpression(dataSeriesId, component.config)
+    let series = chart.evaluateExpression(dataSeriesId, component.config)
+
+    if (series.data && Array.isArray(series.data)) {
+      series.data = series.data.map((v, index) => {
+        const item = chart.evaluateExpression(dataSeriesId + 'data.' + index, v)
+        return Number.isNaN(item.value) ? {} : item
+      })
+    }
+
+    if (series.axisLine && series.axisLine.lineStyle && series.axisLine.lineStyle.color && Array.isArray(series.axisLine.lineStyle.color)) {
+      series.axisLine.lineStyle.color = series.axisLine.lineStyle.color.map((v, index) => {
+        if (!Array.isArray(v)) return v
+        return v.map((s, subindex) => chart.evaluateExpression(dataSeriesId + 'axisLine.lineStyle.color.' + index + '.' + subindex, s))
+      })
+    }
+
+    return series
   }
 }
