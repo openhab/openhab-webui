6a187af1803512d1dab49f694862b81299798334
Gautier Taravella <tarag@mailbox.org>
2022-11-17 18:26:37 +0100

    Add key in v-for
    Signed-off-by: Gautier Taravella <tarag@mailbox.org>

--- bundles/org.openhab.ui/web/src/components/widgets/system/oh-card-footer.vue
+++ bundles/org.openhab.ui/web/src/components/widgets/system/oh-card-footer.vue
@@ -3,7 +3,7 @@
     {{ texts }}
   </f7-card-footer>
   <f7-card-footer v-else-if="texts && Array.isArray(texts)">
-    <span v-for="text in texts">
+    <span v-for="text in texts" :key="text">
       {{ text }}
     </span>
   </f7-card-footer>
