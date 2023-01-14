8757ea1ddd4fc28db3e5c90401b79b527b0f5793
Boris Krivonog <boris.krivonog@inova.si>
2023-01-14 20:51:18 +0100

    Fixes https://github.com/openhab/openhab-webui/issues/1072
    Signed-off-by: Boris Krivonog <boris.krivonog@inova.si>

--- bundles/org.openhab.ui/web/src/components/widgets/chart/series/oh-mark-area.js
+++ bundles/org.openhab.ui/web/src/components/widgets/chart/series/oh-mark-area.js
@@ -6,14 +6,15 @@ dayjs.extend(LocalizedFormat)
 export default {
   get (component, points, startTime, endTime, chart) {
     let markArea = chart.evaluateExpression(ComponentId.get(component), component.config)
+    const onStates = markArea.onStates || ["ON", "OPEN"]
     const itemPoints = points.find(p => p.name === markArea.item).data
     markArea.data = []
     let rollingState = null
     let currentArea = null
     itemPoints.forEach((p) => {
-      if ((p.state === 'ON' && rollingState !== 'ON') || (p.state === 'OPEN' && rollingState !== 'OPEN')) {
+      if (onStates.includes(p.state) && !onStates.includes(rollingState)) {
         currentArea = [{ name: markArea.name, xAxis: new Date(p.time) }]
-      } else if (((p.state !== 'ON' && rollingState === 'ON') || (p.state !== 'OPEN' && rollingState === 'OPEN')) && currentArea) {
+      } else if ((!onStates.includes(p.state) && onStates.includes(rollingState)) && currentArea) {
         currentArea.push({ xAxis: new Date(p.time) })
         currentArea[0].name = markArea.name || markArea.item
         currentArea[0].value = rollingState
