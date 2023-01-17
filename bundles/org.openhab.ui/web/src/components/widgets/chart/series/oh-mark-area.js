b44f1e6695933af1384d1b79b806adb6017f5eb2
Boris Krivonog <boris.krivonog@inova.si>
2023-01-17 14:30:21 +0100

    Renamed onStates to states
    Signed-off-by: Boris Krivonog <boris.krivonog@inova.si>

--- bundles/org.openhab.ui/web/src/components/widgets/chart/series/oh-mark-area.js
+++ bundles/org.openhab.ui/web/src/components/widgets/chart/series/oh-mark-area.js
@@ -6,17 +6,17 @@ dayjs.extend(LocalizedFormat)
 export default {
   get (component, points, startTime, endTime, chart) {
     let markArea = chart.evaluateExpression(ComponentId.get(component), component.config)
-    const onStates = (markArea.onStates && !Array.isArray(markArea.onStates) ? [markArea.onStates] : markArea.onStates) || ['ON', 'OPEN']
+    const states = (markArea.states && !Array.isArray(markArea.states) ? [markArea.states] : markArea.states) || ['ON', 'OPEN']
     const itemPoints = points.find(p => p.name === markArea.item).data
     markArea.data = []
     let rollingState = null
     let currentArea = null
     itemPoints.forEach((p) => {
       // eslint-disable-next-line eqeqeq
-      if (onStates.findIndex((s) => s == p.state) >= 0 && onStates.findIndex((s) => s == rollingState) < 0) {
+      if (states.findIndex((s) => s == p.state) >= 0 && states.findIndex((s) => s == rollingState) < 0) {
         currentArea = [{ name: markArea.name, xAxis: new Date(p.time) }]
       // eslint-disable-next-line eqeqeq
-      } else if ((onStates.findIndex((s) => s == p.state) < 0 && onStates.findIndex((s) => s == rollingState) >= 0) && currentArea) {
+      } else if ((states.findIndex((s) => s == p.state) < 0 && states.findIndex((s) => s == rollingState) >= 0) && currentArea) {
         currentArea.push({ xAxis: new Date(p.time) })
         currentArea[0].name = markArea.name || markArea.item
         currentArea[0].value = rollingState
