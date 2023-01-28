de99a33f269c5e09cd1d85cc57888521c4f19040
Florian Hotze <florianh_dev@icloud.com>
2023-01-28 19:00:58 +0100

    Update webpack config to bind to all interfaces
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- bundles/org.openhab.ui/web/build/webpack.config.js
+++ bundles/org.openhab.ui/web/build/webpack.config.js
@@ -49,6 +49,7 @@ module.exports = {
     // compress: true,
     contentBase: '/www/',
     disableHostCheck: true,
+    host: '0.0.0.0',
     historyApiFallback: true,
     // watchOptions: {
     //   poll: 1000,
