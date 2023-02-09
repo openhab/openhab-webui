193b01a27eff8e5bb63f0ebbe5b0efeabe774051
Florian Hotze <florianh_dev@icloud.com>
2023-02-09 09:13:29 +0100

    Fixes & Enhancements for channel add & edit pages (#1683)
    * [MainUI] Allow dashes in channel ID
    
    Channel ID in fact allows dashes (it works and the hint in the input field says dashes are allowed), but the check was wrong.
    
    Fixes #1630.
    
    * [MainUI] Refactor channel settings to component
    
    This
    - allows setting and modifying channel description
    - allows modifying channel label after creation
    - unifies design of channel add and channel edit pages
    - brings channel settings design closer to script settings design
    
    Fixes #1346.
    Fixes #1234.
    
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

---
+++ bundles/org.openhab.ui/web/src/pages/settings/things/channel/channel-general-settings.vue
@@ -0,0 +1,32 @@
+<template>
+  <f7-block v-if="ready" class="padding-vertical no-padding-horizontal">
+    <f7-col>
+      <f7-list class="no-margin" inline-labels no-hairlines-md>
+        <f7-list-input v-if="createMode" label="Channel Identifier" type="text" placeholder="Required" :value="channel.id"
+                       info="Note: cannot be changed after the creation"
+                       required validate pattern="[A-Za-z0-9_\-]+" error-message="Required. A-Z,a-z,0-9,_,- only"
+                       @input="channel.id = $event.target.value" />
+        <f7-list-item v-if="!createMode" media-item class="channel-item" title="Channel UID">
+          <div slot="subtitle">
+            {{ channel.uid }}
+            <clipboard-icon :value="channel.uid" tooltip="Copy UID" />
+          </div>
+        </f7-list-item>
+        <f7-list-input label="Label" type="text" placeholder="Required" :value="channel.label" required validate
+                       @input="channel.label = $event.target.value" clear-button />
+        <f7-list-input label="Description" type="text" :value="channel.description"
+                       @input="channel.description = $event.target.value" clear-button />
+      </f7-list>
+    </f7-col>
+  </f7-block>
+</template>
+
+<script>
+import ClipboardIcon from '@/components/util/clipboard-icon.vue'
+export default {
+  props: ['channel', 'createMode', 'ready'],
+  components: {
+    ClipboardIcon
+  }
+}
+</script>
