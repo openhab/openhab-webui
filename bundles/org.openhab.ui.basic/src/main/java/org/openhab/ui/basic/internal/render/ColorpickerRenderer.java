27eeefc72f14d822bbd4833a8c5188b52f4d5047
lolodomo <lg.hc@free.fr>
2023-02-27 19:09:47 +0100

    [basicui] Add support for iconify and Google material icons (#1706)
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>

--- bundles/org.openhab.ui.basic/src/main/java/org/openhab/ui/basic/internal/render/ColorpickerRenderer.java
+++ bundles/org.openhab.ui.basic/src/main/java/org/openhab/ui/basic/internal/render/ColorpickerRenderer.java
@@ -80,7 +80,7 @@ public class ColorpickerRenderer extends AbstractWidgetRenderer {
 
         // Should be called before preprocessSnippet
         snippet = snippet.replace("%state%", hexValue);
-        snippet = snippet.replace("%icon_state%", escapeURL(hexValue));
+        snippet = snippet.replace("%state_in_url%", escapeURL(hexValue));
 
         snippet = preprocessSnippet(snippet, w);
         if (purelabel != null) {
