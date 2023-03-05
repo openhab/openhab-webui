ad30503251547dafba36a40cd7b3dab35ac23435
Christian Rose <christian.rose@chrose.de>
2023-03-05 13:23:50 +0100

    Fixed analyzer chart for group derived states
    States that are derived form group members have different properties than a normal states from Number or Dimmer items. As groups have another attribute (groupType) this needs to be checked a well.
    This should probably fix openhab#1365
    
    Signed-off-by: Christian Rose <christian.rose@chrose.de>

--- bundles/org.openhab.ui/web/src/pages/analyzer/analyzer.vue
+++ bundles/org.openhab.ui/web/src/pages/analyzer/analyzer.vue
@@ -349,7 +349,8 @@ export default {
       seriesOptions.name = item.label || item.name
       seriesOptions.type = 'line'
       seriesOptions.discrete = false
-      if ((item.type.indexOf('Number') !== 0 && item.type.indexOf('Dimmer') !== 0)) seriesOptions.discrete = true
+      if ((item.type.indexOf('Number') !== 0 && item.type.indexOf('Dimmer') !== 0 &&
+         ((item.groupType.indexOf('Number') !== 0 && item.groupType.indexOf('Dimmer') !== 0) || item.groupType === undefined))) seriesOptions.discrete = true
       if (!seriesOptions.discrete && this.coordSystem === 'aggregate' && this.aggregateDimensions === 1) seriesOptions.type = 'bar'
       if (!seriesOptions.discrete && (this.coordSystem === 'calendar' || (this.coordSystem === 'aggregate' && this.aggregateDimensions === 2))) seriesOptions.type = 'heatmap'
       if (seriesOptions.discrete) seriesOptions.type = 'area'
