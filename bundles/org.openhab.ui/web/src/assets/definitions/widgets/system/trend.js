91110967d74f8a866bea549a0d3b19d007163157
petero-dk <2478689+petero-dk@users.noreply.github.com>
2023-03-12 17:39:52 +0100

    Added trendGradientDirection to definition
    Signed-off-by: Peter Obel <peter@ecomerc.com> (github: petero-dk)

--- bundles/org.openhab.ui/web/src/assets/definitions/widgets/system/trend.js
+++ bundles/org.openhab.ui/web/src/assets/definitions/widgets/system/trend.js
@@ -5,5 +5,6 @@ export default () => [
   pt('trendStrokeWidth', 'Trend Stroke Width', 'Thickness of the trend line').a(),
   pt('trendWidth', 'Trend Line Width', 'Width of the trend line (leave blank to set automatically)').a(),
   pt('trendGradient', 'Trend Line Gradient', 'Colors of the trend line (see <a target="_blank" class="external text-color-blue" href="https://github.com/QingWei-Li/vue-trend#props">vue-trend</a>)').a(),
+  pt('trendGradientDirection', 'Trend Line Gradient Direction', 'Direction of the trend line gradient (default: top). Possible values "top", "bottom", "left", "right".').a(),
   pt('trendSampling', 'Trend Line Sampling', 'Amount of minutes between each point of the trendline (default: 60). Affected by persistence strategies different from "every minute"').a()
 ]
