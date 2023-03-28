openhab/openhab-webui

commit 1b5170ef4c07f1f9de9528293e21faf73eb5fc17

Configuration & iPhone style fixes for the `options` widget action (#1734)
- Fixes the style of the actions popup on iPhones in the openHAB iOS
app. It did not respect the safe-area-inlets and therefore large action
popups possibly placed behing the notch or outside of the display.
- Fixes #1210: trim whitespaces when parsing the `actionOptions` to
avoid having whitespaces in commands sent to the server.

--
Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

bundles/org.openhab.ui/web/src/components/widgets/widget-actions.js
@@ -103,12 +103,12 @@ export const actionsMixin = {
           const actionsPromise = new Promise((resolve, reject) => {
             if (actionCommandOptions && typeof actionCommandOptions === 'string') {
               resolve(actionCommandOptions.split(',').map((o) => {
-                const parts = o.split('=')
+                const parts = o.trim().split('=')
                 return {
-                  text: parts[1] || parts[0],
+                  text: parts[1].trim() || parts[0].trim(),
                   color: 'blue',
                   onClick: () => {
-                    this.$store.dispatch('sendCommand', { itemName: actionCommandOptionsItem, cmd: parts[0] })
+                    this.$store.dispatch('sendCommand', { itemName: actionCommandOptionsItem, cmd: parts[0].trim() })
                       .then(() => this.showActionFeedback(prefix, actionConfig))
                   }
                 }

bundles/org.openhab.ui/web/src/css/app.styl
@@ -140,6 +140,10 @@ html
    .delete
      --f7-bars-link-color #ff3b30
 
+// Fix actions-modal/popup is placed behind the notch or outside of the screen on iPhone in openHAB iOS app
+.device-ios .actions-modal
+  max-height: calc(100% - var(--f7-safe-area-top) - var(--f7-safe-area-bottom))
+
 .md .contextual-toolbar
   --f7-theme-color #474747
   --f7-bars-bg-color #474747

commit c43e367f010575497b7cc9e5d0877e0c3448960d

Fix action `options` regression
Fixes #1806, which is regression from #1734.

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

bundles/org.openhab.ui/web/src/components/widgets/widget-actions.js
@@ -105,7 +105,7 @@ export const actionsMixin = {
               resolve(actionCommandOptions.split(',').map((o) => {
                 const parts = o.trim().split('=')
                 return {
-                  text: parts[1].trim() || parts[0].trim(),
+                  text: (parts[1] !== undefined ? parts[1].trim() : parts[1]) || parts[0].trim(),
                   color: 'blue',
                   onClick: () => {
                     this.$store.dispatch('sendCommand', { itemName: actionCommandOptionsItem, cmd: parts[0].trim() })