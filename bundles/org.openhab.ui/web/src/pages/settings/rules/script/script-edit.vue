148d025534000f06c1c9a25cf20834918c9d2edb
Florian Hotze <florianh_dev@icloud.com>
2023-05-14 14:07:00 +0200

    Revert changes to f7-segmented
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- bundles/org.openhab.ui/web/src/pages/settings/rules/script/script-edit.vue
+++ bundles/org.openhab.ui/web/src/pages/settings/rules/script/script-edit.vue
@@ -26,7 +26,7 @@
       </span>
       <span class="display-flex flex-direction-row align-items-center">
         <f7-button v-if="!newScript && isBlockly && !blocklyCodePreview" outline small :active="blocklyShowLabels" icon-f7="square_on_circle" :icon-size="($theme.aurora) ? 20 : 22" class="no-ripple" style="margin-right: 5px" @click="toggleBlocklyItemLabelId" :tooltip="'Toggle to show either Item labels or IDs'" />
-        <f7-segmented>
+        <f7-segmented v-if="!newScript && isBlockly" class="margin-right">
           <f7-button outline small :active="!blocklyCodePreview" icon-f7="ticket" :icon-size="($theme.aurora) ? 20 : 22" class="no-ripple" @click="blocklyCodePreview = false" />
           <f7-button outline small :active="blocklyCodePreview" icon-f7="doc_text" :icon-size="($theme.aurora) ? 20 : 22" class="no-ripple" @click="showBlocklyCode" />
         </f7-segmented>
