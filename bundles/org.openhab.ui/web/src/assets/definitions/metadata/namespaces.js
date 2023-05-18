8d28ea21199b7fb027b879231b5ec6f502f5c33f
Florian Hotze <florianh_dev@icloud.com>
2023-05-18 13:21:47 +0200

    Change label, description & order of `unit` metadata
    Addressing review.
    
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- bundles/org.openhab.ui/web/src/assets/definitions/metadata/namespaces.js
+++ bundles/org.openhab.ui/web/src/assets/definitions/metadata/namespaces.js
@@ -1,6 +1,6 @@
 export default [
+  { name: 'unit', label: 'Unit' },
   { name: 'stateDescription', label: 'State Description' },
-  { name: 'unit', label: 'Internal Unit' },
   { name: 'commandDescription', label: 'Command Options' },
   { name: 'synonyms', label: 'Synonyms' },
   { name: 'widget', label: 'Default Standalone Widget' },
--- bundles/org.openhab.ui/web/src/components/item/metadata/item-metadata-unit.vue
+++ bundles/org.openhab.ui/web/src/components/item/metadata/item-metadata-unit.vue
@@ -5,7 +5,7 @@
     </f7-block-header>
     <f7-list>
       <f7-list-input
-        label="Internal Unit"
+        label="Unit"
         name="value"
         ref="value"
         type="text"
@@ -14,7 +14,13 @@
         @blur="(evt) => metadata.value = evt.target.value" />
     </f7-list>
     <f7-block-footer class="param-description">
-      <small>Unit used internally, e.g. in events and for persistence.<br>The unit of the item is independent of the state description. The state description is used for display purposes only, it can contain any compatible unit and will not affect the internal unit.</small>
+      <small>All processed values are internally normalized to the specified <code>unit</code>. The normalized value is used to propagate the value to external integrations (e.g. persistence, REST API, WebSocket) so these values will always have the specified unit and scale.
+        <br>
+        Changing the <code>unit</code> will change the values that these systems receive.
+        <br>
+        <br>
+        Additionally the <code>unit</code> will be used in UIs if no unit is specified for the state description. The state description is used for display purpose only, it can contain any compatible unit and will not affect the internal <code>unit</code>.
+      </small>
     </f7-block-footer>
   </div>
 </template>
