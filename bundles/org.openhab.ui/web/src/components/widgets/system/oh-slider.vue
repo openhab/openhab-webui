openhab/openhab-webui

commit b5176216c11bbdf2e41b95a00d3b21f3f1f07288

`oh-slider`: Fix command sent on state update due to rounding (#1894)
Fixes https://github.com/openhab/openhab-webui/issues/1862.

When a state update like 40.545 was received, the slider component
commanded the Item to 41 because of the rounding used.
Unfortunately, the slider component has no built-in way to avoid that.
This decouples the slider value from the actual Item value and only send
command when the difference between them is larger or equal than step
size.

This is no perfect solution, because it can be that the slider does not
accept input when the above checks don't pass.
Example: Your Item has a value of 40.545, then the slider does not
accept 40.5 and 41.

I've checked `oh-setpoint` and `oh-knob`, they are not affected by this
bug.

--
Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

bundles/org.openhab.ui/web/src/components/widgets/system/oh-slider.vue
@@ -1,6 +1,6 @@
 <template>
-  <f7-range ref="rangeslider" class="oh-slider" v-bind="config" :value="value" :format-label="formatLabel" :format-scale-label="formatScaleLabel"
-            @range:change="sendCommandDebounced($event)" @click.native.stop="sendCommandDebounced(value, true)" @touchend.native="sendCommandDebounced(value, true)" />
+  <f7-range ref="rangeslider" class="oh-slider" v-bind="config" :value="sliderValue" :format-label="formatLabel" :format-scale-label="formatScaleLabel"
+            @range:change="onChange($event)" @click.native.stop="sendCommandDebounced(sliderValue, true)" @touchend.native="sendCommandDebounced(sliderValue, true)" />
 </template>
 
 <style lang="stylus">
@@ -17,6 +17,18 @@ import { OhSliderDefinition } from '@/assets/definitions/widgets/system'
 export default {
   mixins: [mixin, slideMixin],
   widget: OhSliderDefinition,
+  data () {
+    return {
+      sliderValue: null
+    }
+  },
+  watch: {
+    value (newValue) {
+      if (!isNaN(newValue)) {
+        this.sliderValue = newValue
+      }
+    }
+  },
   mounted () {
     // f7-range inside of masonry can get rendered faulty, as the masonry changes its breakpoint layout after being rendered
     // re-calculate the range slider after masonry is updated
@@ -38,6 +50,16 @@ export default {
       // uses the number of decimals in the step config to round the provided number
       const nbDecimals = this.config.step ? Number(this.config.step).toString().replace(',', '.').split('.')[1] : 0
       return parseFloat(Number(value).toFixed(nbDecimals))
+    },
+    onChange (newValue) {
+      if (isNaN(this.value)) return
+      const tsf = this.toStepFixed(newValue)
+      // Do NOT send command if sliderValue is smaller than real value +-step
+      if (Math.abs(tsf - this.value) < (this.config.step || 1)) {
+        this.$refs.rangeslider.setValue(this.value)
+      } else {
+        this.sendCommandDebounced(tsf)
+      }
     }
   }
 }

commit 4702ee8e5a2963bab62e72308848d9127bd853d6

oh-slider: Fix slider not initialized with value
Minor regression from #1894.

In some cases, a slider was not properly initialized a value when the page was loaded.

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

bundles/org.openhab.ui/web/src/components/widgets/system/oh-slider.vue
@@ -29,6 +29,13 @@ export default {
       }
     }
   },
+  created () {
+    if (!isNaN(this.value)) {
+      this.sliderValue = this.value
+    } else {
+      this.sliderValue = this.config.min || this.config.max || 0
+    }
+  },
   mounted () {
     // f7-range inside of masonry can get rendered faulty, as the masonry changes its breakpoint layout after being rendered
     // re-calculate the range slider after masonry is updated
@@ -41,7 +48,7 @@ export default {
   },
   methods: {
     formatLabel (value) {
-      return this.toStepFixed(value) + (this.config.unit || '')
+      return this.toStepFixed(value) + (this.unit ? ' ' + this.unit : '')
     },
     formatScaleLabel (value) {
       return this.toStepFixed(value)