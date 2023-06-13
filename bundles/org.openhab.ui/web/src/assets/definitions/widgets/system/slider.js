openhab/openhab-webui

commit b5176216c11bbdf2e41b95a00d3b21f3f1f07288

`oh-slider`: Fix command sent on state update due to rounding (#1894)
Fixes https://github.com/openhab/openhab-webui/issues/1862.

When a state update like 40.545 was received, the slider component
commanded the Item to 41 because of the rounding used.
Unfortunately, the slider component has no built-in way to avoid that.
This decouples the slider value from the actual Item value and only send
command when the difference between them is larger or equal than step
size.

This is no perfect solution, because it can be that the slider does not
accept input when the above checks don't pass.
Example: Your Item has a value of 40.545, then the slider does not
accept 40.5 and 41.

I've checked `oh-setpoint` and `oh-knob`, they are not affected by this
bug.

--
Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

bundles/org.openhab.ui/web/src/components/widgets/system/oh-slider.vue
@@ -1,6 +1,6 @@
 <template>
-  <f7-range ref="rangeslider" class="oh-slider" v-bind="config" :value="value" :format-label="formatLabel" :format-scale-label="formatScaleLabel"
-            @range:change="sendCommandDebounced($event)" @click.native.stop="sendCommandDebounced(value, true)" @touchend.native="sendCommandDebounced(value, true)" />
+  <f7-range ref="rangeslider" class="oh-slider" v-bind="config" :value="sliderValue" :format-label="formatLabel" :format-scale-label="formatScaleLabel"
+            @range:change="onChange($event)" @click.native.stop="sendCommandDebounced(sliderValue, true)" @touchend.native="sendCommandDebounced(sliderValue, true)" />
 </template>
 
 <style lang="stylus">
@@ -17,6 +17,18 @@ import { OhSliderDefinition } from '@/assets/definitions/widgets/system'
 export default {
   mixins: [mixin, slideMixin],
   widget: OhSliderDefinition,
+  data () {
+    return {
+      sliderValue: null
+    }
+  },
+  watch: {
+    value (newValue) {
+      if (!isNaN(newValue)) {
+        this.sliderValue = newValue
+      }
+    }
+  },
   mounted () {
     // f7-range inside of masonry can get rendered faulty, as the masonry changes its breakpoint layout after being rendered
     // re-calculate the range slider after masonry is updated
@@ -38,6 +50,16 @@ export default {
       // uses the number of decimals in the step config to round the provided number
       const nbDecimals = this.config.step ? Number(this.config.step).toString().replace(',', '.').split('.')[1] : 0
       return parseFloat(Number(value).toFixed(nbDecimals))
+    },
+    onChange (newValue) {
+      if (isNaN(this.value)) return
+      const tsf = this.toStepFixed(newValue)
+      // Do NOT send command if sliderValue is smaller than real value +-step
+      if (Math.abs(tsf - this.value) < (this.config.step || 1)) {
+        this.$refs.rangeslider.setValue(this.value)
+      } else {
+        this.sendCommandDebounced(tsf)
+      }
     }
   }
 }

commit 7f6e093f0184cf6f8dbd7cee887beaf437906121

`oh-slider` & `oh-knob`: Fix user configured unit (state description) ignored (#1920)
Fixes #1913. Fixes https://github.com/openhab/openhab-core/issues/3643.
Depends on https://github.com/openhab/openhab-core/pull/3647.

While fixing this, I also noticed a minor regression from PR #1894, that
I also fixed.

--
Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

bundles/org.openhab.ui/web/src/assets/definitions/widgets/system/slider.js
@@ -10,7 +10,7 @@ export default () => [
   pb('scale', 'Display Scale', 'Display a scale on the slider'),
   pn('scaleSteps', 'Scale steps', 'Number of (major) scale markers'),
   pn('scaleSubSteps', 'Scale sub-steps', 'Number of scale minor markers between each major marker'),
-  pt('unit', 'Unit', 'Text to append to the label while dragging the cursor'),
+  pt('unit', 'Unit', 'Unit for the command sent and also append to the label while dragging the cursor, leave empty to use Item\'s unit'),
   pb('releaseOnly', 'Send command only on release', 'If enabled, no commands are sent during sliding'),
   pn('commandInterval', 'Command Interval', 'Time to wait between subsequent commands in ms (default 200)'),
   pn('delayStateDisplay', 'Delay State Display', 'Time to wait before switching from displaying user input to displaying item state in ms (default 2000)').a()

bundles/org.openhab.ui/web/src/components/widgets/system/oh-slider.vue
@@ -29,6 +29,13 @@ export default {
       }
     }
   },
+  created () {
+    if (!isNaN(this.value)) {
+      this.sliderValue = this.value
+    } else {
+      this.sliderValue = this.config.min || this.config.max || 0
+    }
+  },
   mounted () {
     // f7-range inside of masonry can get rendered faulty, as the masonry changes its breakpoint layout after being rendered
     // re-calculate the range slider after masonry is updated
@@ -41,7 +48,7 @@ export default {
   },
   methods: {
     formatLabel (value) {
-      return this.toStepFixed(value) + (this.config.unit || '')
+      return this.toStepFixed(value) + (this.unit ? ' ' + this.unit : '')
     },
     formatScaleLabel (value) {
       return this.toStepFixed(value)

bundles/org.openhab.ui/web/src/components/widgets/system/slide-mixin.js
@@ -23,10 +23,15 @@ export default {
         }
       }
       if (this.pendingCommand !== null) return this.pendingCommand // to keep the control reactive when operating
-      const value = this.context.store[this.config.item].state
+      const value = this.context.store[this.config.item].displayState || this.context.store[this.config.item].state
       // use as a brightness control for HSB values
       if (value.split && value.split(',').length === 3) return parseFloat(value.split(',')[2])
       return parseFloat(value)
+    },
+    unit () {
+      if (this.config.unit) return this.config.unit
+      if (this.context.store[this.config.item].displayState && this.context.store[this.config.item].displayState.split(' ').length === 2) return this.context.store[this.config.item].displayState.split(' ')[1]
+      return undefined
     }
   },
   methods: {
@@ -56,8 +61,12 @@ export default {
       }
       if (!this.sendCommandTimer) {
         if (this.displayLockTimer) clearTimeout(this.displayLockTimer)
+        const stateType = this.context.store[this.config.item].type
         this.sendCommandTimer = setTimeout(() => {
-          this.$store.dispatch('sendCommand', { itemName: this.config.item, cmd: this.pendingCommand.toString() })
+          this.$store.dispatch('sendCommand', {
+            itemName: this.config.item,
+            cmd: (this.unit && stateType === 'Quantity') ? this.pendingCommand + ' ' + this.unit : this.pendingCommand.toString()
+          })
           this.lastValueSent = this.pendingCommand
           this.lastDateSent = Date.now()
           this.sendCommandTimer = null