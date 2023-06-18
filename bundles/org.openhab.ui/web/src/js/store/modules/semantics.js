0bca358ad8a82d0ebc7ffc8ef274405d574bf2e6
Florian Hotze <florianh_dev@icloud.com>
2023-06-18 19:53:22 +0200

    Adjust to breaking core changes
    Refs https://github.com/openhab/openhab-core/pull/3646.
    
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- bundles/org.openhab.ui/web/src/js/store/modules/semantics.js
+++ bundles/org.openhab.ui/web/src/js/store/modules/semantics.js
@@ -16,14 +16,14 @@ const getters = {
 
 const mutations = {
   setSemanticClasses (state, { tags }) {
-    state.Locations = tags.Locations.map(t => t.name)
-    state.Equipment = tags.Equipments.map(t => t.name)
-    state.Points = tags.Points.map(t => t.name)
-    state.Properties = tags.Properties.map(t => t.name)
+    state.Locations = tags.filter(t => t.uid.startsWith('Location_')).map(t => t.name)
+    state.Equipment = tags.filter(t => t.uid.startsWith('Equipment_')).map(t => t.name)
+    state.Points = tags.filter(t => t.uid.startsWith('Point_')).map(t => t.name)
+    state.Properties = tags.filter(t => t.uid.startsWith('Property_')).map(t => t.name)
     // Store i18n labels
-    Object.values(tags).forEach(ctg => ctg.forEach(t => {
+    Object.values(tags).forEach(t => {
       if (t.label) state.Labels[t.name] = t.label
-    }))
+    })
     // Save as i18n messages
     i18n.mergeLocaleMessage(this.getters.locale, state.Labels)
   }
