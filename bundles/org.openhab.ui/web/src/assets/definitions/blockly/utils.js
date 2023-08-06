03abcd0cf8d0bcfb27b63ada722d46167b6b3e79
Florian Hotze <florianh_dev@icloud.com>
2023-08-06 18:51:55 +0200

    Minor code improvement
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- bundles/org.openhab.ui/web/src/assets/definitions/blockly/utils.js
+++ bundles/org.openhab.ui/web/src/assets/definitions/blockly/utils.js
@@ -230,8 +230,7 @@ export function blockGetCheckedInputType (block, inputName) {
   // Get the input type checks for this block
   const thisBlock = block.getInput(inputName).connection.getCheck()
   // Get the output type checks for the connected block
-  if (!block.getInput(inputName).connection.targetBlock()) return ''
-  const connectedBlock = block.getInput(inputName).connection.targetBlock().outputConnection.getCheck()
+  const connectedBlock = block.getInput(inputName).connection.targetBlock()?.outputConnection.getCheck()
   // Skip if no checks are available
   if (!thisBlock || !connectedBlock) return ''
   // Find any intersection in the checklist
