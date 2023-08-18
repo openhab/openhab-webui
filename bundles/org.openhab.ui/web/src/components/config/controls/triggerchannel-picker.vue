openhab/openhab-webui

commit 1257a0d4d6738185d537f8135b18e93088314472

Use cacheable lists when possible (#1661)
Closes #1650.
Depends on https://github.com/openhab/openhab-core/pull/3335.

Will fetch items, things and rules with the `staticDataOnly` parameter set when appropriate.

Also-by: Florian Hotze <florianh_dev@icloud.com>
Signed-off-by: Yannick Schaus <github@schaus.net>

bundles/org.openhab.ui/web/src/components/config/controls/triggerchannel-picker.vue
@@ -33,8 +33,7 @@ export default {
   },
   created () {
     this.smartSelectParams.closeOnSelect = !(this.multiple)
-    // TODO use a Vuex store
-    this.$oh.api.get('/rest/things').then((data) => {
+    this.$oh.api.get('/rest/things?staticDataOnly=true').then((data) => {
       this.things = data.sort((a, b) => {
         const labelA = a.label
         const labelB = b.label

bundles/org.openhab.ui/web/src/components/config/controls/item-picker.vue
@@ -49,8 +49,7 @@ export default {
   created () {
     this.smartSelectParams.closeOnSelect = !(this.multiple)
     if (!this.items || !this.items.length) {
-      // TODO use a Vuex store
-      this.$oh.api.get('/rest/items').then((items) => {
+      this.$oh.api.get('/rest/items?staticDataOnly=true').then((items) => {
         this.sortAndFilterItems(items)
       })
     } else {

bundles/org.openhab.ui/web/src/components/config/controls/rule-picker.vue
@@ -46,8 +46,7 @@ export default {
   },
   created () {
     this.smartSelectParams.closeOnSelect = !(this.multiple)
-    // TODO use a Vuex store
-    this.$oh.api.get('/rest/rules?summary=true').then((data) => {
+    this.$oh.api.get('/rest/rules?staticDataOnly=true').then((data) => {
       this.scenes = data.filter((r) => r.tags.indexOf('Scene') >= 0).sort((a, b) => {
         const labelA = a.name
         const labelB = b.name

bundles/org.openhab.ui/web/src/components/config/controls/script-editor.vue
@@ -235,7 +235,7 @@ export default {
             }
             server.on('completion', this.ternComplete)
           })
-          this.$oh.api.get('/rest/items').then((data) => { this.$set(this, 'itemsCache', data) })
+          this.$oh.api.get('/rest/items?staticDataOnly=true').then((data) => { this.$set(this, 'itemsCache', data) })
         }
         const server = new _CodeMirror.TernServer({
           defs: (this.mode.indexOf('version=ECMAScript-5.1') > 0) ? [EcmascriptDefs, NashornDefs] : [EcmascriptDefs, OpenhabJsDefs],

bundles/org.openhab.ui/web/src/components/config/controls/thing-picker.vue
@@ -33,8 +33,7 @@ export default {
   },
   created () {
     this.smartSelectParams.closeOnSelect = !(this.multiple)
-    // TODO use a Vuex store
-    this.$oh.api.get('/rest/things?summary=true').then((data) => {
+    this.$oh.api.get('/rest/things?staticDataOnly=true').then((data) => {
       this.things = data.sort((a, b) => {
         const labelA = a.label || a.UID
         const labelB = b.label || b.UID

bundles/org.openhab.ui/web/src/components/config/editor/hint-components.js
@@ -52,7 +52,7 @@ function getWidgetDefinitions (cm) {
 
 function hintItems (cm, line, replaceAfterColon, addStatePropertySuffix, addQuotes) {
   const cursor = cm.getCursor()
-  const promise = (itemsCache) ? Promise.resolve(itemsCache) : cm.state.$oh.api.get('/rest/items')
+  const promise = (itemsCache) ? Promise.resolve(itemsCache) : cm.state.$oh.api.get('/rest/items?staticDataOnly=true')
   return promise.then((data) => {
     if (!itemsCache) itemsCache = data
     let ret = {

bundles/org.openhab.ui/web/src/components/config/editor/hint-rules.js
@@ -14,7 +14,7 @@ function getModuleTypes (cm, section) {
 function hintItems (cm, line, replaceAfterColon, replaceAfterLastSpace, addColonSuffix) {
   const cursor = cm.getCursor()
   if (!cm.state.$oh) return
-  const promise = (itemsCache) ? Promise.resolve(itemsCache) : cm.state.$oh.api.get('/rest/items')
+  const promise = (itemsCache) ? Promise.resolve(itemsCache) : cm.state.$oh.api.get('/rest/items?staticDataOnly=true')
   return promise.then((data) => {
     if (!itemsCache) itemsCache = data
     let ret = {

bundles/org.openhab.ui/web/src/components/developer/developer-sidebar.vue
@@ -556,8 +556,8 @@ export default {
           Promise.resolve(this.cachedObjects[2]),
           Promise.resolve(this.cachedObjects[3])
         ] : [
-          this.$oh.api.get('/rest/items?metadata=.*'),
-          this.$oh.api.get('/rest/things?summary=true'),
+          this.$oh.api.get('/rest/items?staticDataOnly=true&metadata=.*'),
+          this.$oh.api.get('/rest/things?staticDataOnly=true'),
           this.$oh.api.get('/rest/rules?summary=false'),
           Promise.resolve(this.$store.getters.pages)
         ]

commit 3eff1fa404a8ff7931e812f5c893539cc6bc166f

Fix channel for Thing trigger cannot be chosen (#2026)
Fixes #1996.
Regression from #1661.

Supersedes https://github.com/openhab/openhab-core/pull/3761.

Signed-off-by: Yannick Schaus <github@schaus.net>

bundles/org.openhab.ui/web/src/components/config/controls/triggerchannel-picker.vue
@@ -33,7 +33,7 @@ export default {
   },
   created () {
     this.smartSelectParams.closeOnSelect = !(this.multiple)
-    this.$oh.api.get('/rest/things?staticDataOnly=true').then((data) => {
+    this.$oh.api.get('/rest/things').then((data) => {
       this.things = data.sort((a, b) => {
         const labelA = a.label
         const labelB = b.label