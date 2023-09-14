e3fdec91439824757085079ebc43eebcf6711081
Florian Hotze <florianh_dev@icloud.com>
2023-09-14 18:13:20 +0200

    Use mutation to set semantic tags from within action
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- bundles/org.openhab.ui/web/src/js/store/modules/semantics.js
+++ bundles/org.openhab.ui/web/src/js/store/modules/semantics.js
@@ -15,25 +15,30 @@ const getters = {
   }
 }
 
+const mutations = {
+  setSemantics (state, { tags }) {
+    state.Locations = tags.filter(t => t.uid.startsWith('Location')).map(t => t.name)
+    state.Equipment = tags.filter(t => t.uid.startsWith('Equipment')).map(t => t.name)
+    state.Points = tags.filter(t => t.uid.startsWith('Point')).map(t => t.name)
+    state.Properties = tags.filter(t => t.uid.startsWith('Property')).map(t => t.name)
+    // Store i18n labels
+    state.Labels = {} // Clear existing labels
+    for (const i in tags) {
+      const t = tags[i]
+      state.Labels[t.name] = t.label || t.name
+    }
+    // Save as i18n messages
+    i18n.mergeLocaleMessage(i18n.locale, state.Labels)
+  }
+}
+
 const actions = {
-  loadSemantics () {
+  loadSemantics (context) {
     console.debug('Loading semantic tags ...')
     if (this.getters.apiEndpoint('tags')) {
       return api.get('/rest/tags')
         .then((tags) => {
-          state.Locations = tags.filter(t => t.uid.startsWith('Location')).map(t => t.name)
-          state.Equipment = tags.filter(t => t.uid.startsWith('Equipment')).map(t => t.name)
-          state.Points = tags.filter(t => t.uid.startsWith('Point')).map(t => t.name)
-          state.Properties = tags.filter(t => t.uid.startsWith('Property')).map(t => t.name)
-          // Store i18n labels
-          state.Labels = {} // Clear existing labels
-          for (const i in tags) {
-            const t = tags[i]
-            state.Labels[t.name] = t.label || t.name
-          }
-          // Save as i18n messages
-          i18n.mergeLocaleMessage(i18n.locale, state.Labels)
-
+          context.commit('setSemantics', { tags })
           console.debug('Successfully loaded semantic tags.')
           return Promise.resolve()
         })
@@ -48,11 +53,9 @@ const actions = {
   }
 }
 
-const mutations = {}
-
 export default {
   state,
   getters,
-  actions,
-  mutations
+  mutations,
+  actions
 }
