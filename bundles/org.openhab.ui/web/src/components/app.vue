5d89bda5e2f09c02a4783d8c76f4f678a886b1a1
Florian Hotze <florianh_dev@icloud.com>
2023-09-18 20:53:13 +0200

    Fix SSE failure toast container not removed (#2075)
    Fixes #2069.
    
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>
    (cherry picked from commit cf3b21857187fb288b2db521652d5d7b778bbfd7)

--- bundles/org.openhab.ui/web/src/components/app.vue
+++ bundles/org.openhab.ui/web/src/components/app.vue
@@ -672,7 +672,7 @@ export default {
         text: message,
         closeButton: reloadButton,
         closeButtonText: this.$t('dialogs.reload'),
-        destroyOnClose: autoClose,
+        destroyOnClose: true,
         closeTimeout: (autoClose) ? 5000 : undefined,
         cssClass: 'failure-toast button-outline',
         position: 'bottom',
@@ -788,7 +788,6 @@ export default {
                 this.communicationFailureTimeoutId = setTimeout(() => {
                   if (this.communicationFailureToast !== null) return
                   this.communicationFailureToast = this.displayFailureToast(this.$t('error.communicationFailure'), true, false)
-                  this.communicationFailureToast.open()
                   this.communicationFailureTimeoutId = null
                 }, 1000)
               }
@@ -796,7 +795,6 @@ export default {
               if (this.communicationFailureTimeoutId !== null) clearTimeout(this.communicationFailureTimeoutId)
               if (this.communicationFailureToast !== null) {
                 this.communicationFailureToast.close()
-                this.communicationFailureToast.destroy()
                 this.communicationFailureToast = null
               }
             }
