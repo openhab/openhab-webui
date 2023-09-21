a4052c8cc43aaede2476650f65417fcdf8bb0bff
Florian Hotze <florianh_dev@icloud.com>
2023-09-22 01:55:18 +0200

    Update label
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- bundles/org.openhab.ui/web/src/components/pagedesigner/page-settings.vue
+++ bundles/org.openhab.ui/web/src/components/pagedesigner/page-settings.vue
@@ -27,7 +27,7 @@
           </f7-list>
         </f7-accordion-content>
       </f7-list-item>
-      <f7-list-item accordion-item title="Non-Semantic Tags" :disabled="page.uid === 'overview'">
+      <f7-list-item accordion-item title="Tags" :disabled="page.uid === 'overview'">
         <f7-accordion-content>
           <tag-input :item="page" />
         </f7-accordion-content>
