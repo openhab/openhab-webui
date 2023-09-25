openhab/openhab-webui

commit 52da897b3341c8600d7c7d3f7178ad7845624a7a

`oh-knob` & `oh-stepper`: Add offset functionality (#1875)
Supersedes #1565.

This adds an offset parameter to the knob and stepper components, that
allows to add or subtract a number value to the displayed state of the
component.

This is useful for thermostats which have a fixed temperature of n and
are controlled with an offset to that temperature n, e.g. you have a KNX
HVAC system which has a fixed temperature of 20 °C: -2 would mean the
target temperature is 18 °C, +2 would mean that the target temperature
is 22 °C. By setting the offset parameter to 20, the UI will display the
real target temperature instead of the -2 or 2.

--
Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

bundles/org.openhab.ui/web/src/components/widgets/system/oh-stepper.vue
@@ -1,7 +1,12 @@
 <template>
   <f7-stepper ref="stepper" v-bind="config" :value="value" @stepper:change="onChange" @click.native.stop
-              :manual-input-mode="false" :format-value="formatValue" />
+              :input="config.enableInput === true" :manual-input-mode="false" :format-value="formatValue" />
 </template>
+
+<style lang="stylus">
+.stepper-value
+  margin-top: 0px
+</style>
 
 <script>
 import mixin from '../widget-mixin'
@@ -16,13 +21,14 @@ export default {
   },
   computed: {
     value () {
+      const applyOffset = (value) => (typeof this.config.offset === 'number') ? value + this.config.offset : value
       if (this.config.variable) {
         if (this.config.variableKey) {
-          return this.getLastVariableKeyValue(this.context.vars[this.config.variable], this.config.variableKey)
+          return applyOffset(this.getLastVariableKeyValue(this.context.vars[this.config.variable], this.config.variableKey))
         }
-        return this.context.vars[this.config.variable]
+        return applyOffset(this.context.vars[this.config.variable])
       }
-      let value = this.toStepFixed(parseFloat(this.context.store[this.config.item].state))
+      let value = applyOffset(this.toStepFixed(parseFloat(this.context.store[this.config.item].state)))
       if (this.config.min !== undefined) value = Math.max(value, this.config.min)
       if (this.config.max !== undefined) value = Math.min(value, this.config.max)
       return value
@@ -45,11 +51,12 @@ export default {
       return parseFloat(Number(value).toFixed(nbDecimals))
     },
     onChange (value) {
-      let newValue = this.toStepFixed(value)
+      const applyOffset = (value) => (typeof this.config.offset === 'number') ? value - this.config.offset : value
+      let newValue = applyOffset(this.toStepFixed(value))
       if (newValue === this.value) return
       if (this.config.variable) {
         if (this.config.variableKey) {
-          newValue = this.setVariableKeyValues(this.context.vars[this.config.variable], this.config.variableKey, value)
+          newValue = applyOffset(this.setVariableKeyValues(this.context.vars[this.config.variable], this.config.variableKey, value))
         }
         this.$set(this.context.vars, this.config.variable, newValue)
       } else if (this.config.item) {

bundles/org.openhab.ui/web/src/assets/definitions/widgets/system/knob.js
@@ -6,6 +6,7 @@ export default () => [
   pn('min', 'Min', 'Minimum value (default 0)'),
   pn('max', 'Max', 'Maximum value (default 100)'),
   pd('step', 'Step', 'Minimum interval between values (default 1)'),
+  pd('offset', 'Offset', 'Offset to be applied to the Item\'s state (e.g. Item state = 2; offset = 20; knob/rounded slider behaves as Item state would be 22)'),
   pb('releaseOnly', 'Send command only on release', 'If enabled, no commands are sent during sliding'),
   pn('commandInterval', 'Command Interval', 'Time to wait between subsequent commands in ms (default 200)').a(),
   pn('delayStateDisplay', 'Delay State Display', 'Time to wait before switching from displaying user input to displaying Item state in ms (default 2000)').a(),

bundles/org.openhab.ui/web/src/assets/definitions/widgets/system/stepper.js
@@ -1,11 +1,13 @@
-import { pi, pb, pn, pd } from '../helpers.js'
+import { pi, pb, pd } from '../helpers.js'
 
 export default () => [
   pi('item', 'Item', 'Item to control'),
   pd('min', 'Min', 'Minimum value'),
   pd('max', 'Max', 'Maximum value'),
   pd('step', 'Step', 'Minimum interval between values'),
+  pd('offset', 'Offset', 'Offset to be applied to the Item\'s state (e.g. Item state = 2; offset = 20; stepper behaves as Item state would be 22)'),
   pb('buttonsOnly', 'Buttons Only', 'Display the buttons without the value in the middle'),
+  pb('enableInput', 'Enable Input Field', 'Enables the input field between the buttons. Note that this might not work when min/max is set.'),
   pb('small', 'Small', 'Smaller size'),
   pb('large', 'Large', 'Larger size'),
   pb('fill', 'Fill', 'Fill the buttons with the primary color'),

bundles/org.openhab.ui/web/src/components/widgets/system/oh-knob.vue
@@ -1,6 +1,5 @@
 <template>
-  <round-slider v-bind="resolvedConfig" :value="value" :style="`stroke-dasharray: ${(config.dottedPath) ? config.dottedPath : 0}`" mouseScrollAction="true"
-                @input="sendCommandDebounced($event)" @click.native.stop="sendCommandDebounced(value, true)" @touchend.native.stop="sendCommandDebounced(value, true)" />
+  <round-slider v-bind="resolvedConfig" :value="computedValue" :style="`stroke-dasharray: ${(config.dottedPath) ? config.dottedPath : 0}`" mouseScrollAction="true" @input="onChange" />
 </template>
 
 <script>
@@ -16,6 +15,9 @@ export default {
   },
   widget: OhKnobDefinition,
   computed: {
+    computedValue () {
+      return (typeof this.config.offset === 'number') ? (this.value + this.config.offset) : this.value
+    },
     resolvedConfig () {
       const cfg = this.config
       return {
@@ -29,6 +31,12 @@ export default {
         startAngle: !cfg.circleShape !== undefined ? (cfg.startAngle !== undefined ? cfg.startAngle : -50) : null,
         endAngle: !cfg.circleShape !== undefined ? (cfg.endAngle !== undefined ? cfg.endAngle : -130) : null
       }
+    }
+  },
+  methods: {
+    onChange (newValue) {
+      if (typeof this.config.offset === 'number') newValue -= this.config.offset
+      this.sendCommandDebounced(newValue)
     }
   }
 }

commit 0cc4f27f3d7ebeefdec7ee7b36b655584c491c4d

Fix decimal formatting for oh-stepper (#2090)
Fixes #2089.
Possibly regression from #1875.

Stepper formatValue was calculating displayed number of decimals from the value of the step property, not it's length.

Signed-off-by: Justin Georgi <justin.georgi@gmail.com>

bundles/org.openhab.ui/web/src/components/widgets/system/oh-stepper.vue
@@ -21,14 +21,14 @@ export default {
   },
   computed: {
     value () {
-      const applyOffset = (value) => (typeof this.config.offset === 'number') ? value + this.config.offset : value
+      const applyOffset = (value) => (typeof this.config.offset === 'number') ? Number(this.toStepFixed(value + this.config.offset)) : value
       if (this.config.variable) {
         if (this.config.variableKey) {
           return applyOffset(this.getLastVariableKeyValue(this.context.vars[this.config.variable], this.config.variableKey))
         }
         return applyOffset(this.context.vars[this.config.variable])
       }
-      let value = applyOffset(this.toStepFixed(parseFloat(this.context.store[this.config.item].state)))
+      let value = applyOffset(Number(this.context.store[this.config.item].state))
       if (this.config.min !== undefined) value = Math.max(value, this.config.min)
       if (this.config.max !== undefined) value = Math.min(value, this.config.max)
       return value
@@ -37,7 +37,7 @@ export default {
   watch: {
     value (newValue) {
       if (isNaN(newValue) || !isFinite(newValue)) return
-      this.$refs.stepper.setValue(this.toStepFixed(newValue).toString())
+      this.$refs.stepper.setValue(Number(this.toStepFixed(newValue)))
     }
   },
   methods: {
@@ -48,11 +48,11 @@ export default {
       // uses the number of decimals in the step config to round the provided number
       if (!this.config.step) return value
       const nbDecimals = Number(this.config.step).toString().replace(',', '.').split('.')[1]
-      return parseFloat(Number(value).toFixed(nbDecimals))
+      return parseFloat(Number(value)).toFixed(nbDecimals.length)
     },
     onChange (value) {
-      const applyOffset = (value) => (typeof this.config.offset === 'number') ? value - this.config.offset : value
-      let newValue = applyOffset(this.toStepFixed(value))
+      const applyOffset = (value) => (typeof this.config.offset === 'number') ? Number(this.toStepFixed(value - this.config.offset)) : value
+      let newValue = applyOffset(Number(this.toStepFixed(value)))
       if (newValue === this.value) return
       if (this.config.variable) {
         if (this.config.variableKey) {