b07d3a3ab93f29d4bfd98ab75b7cbe99a3d7103c
Dmitry P. (d51x) <dimonich110@gmail.com>
2023-10-09 23:39:01 +0300

    [MainUI] added filtering rules by multiple tags
    Signed-off-by: Dmitry P. (d51x) <dimonich110@gmail.com>

--- bundles/org.openhab.ui/web/src/pages/settings/rules/rules-list.vue
+++ bundles/org.openhab.ui/web/src/pages/settings/rules/rules-list.vue
@@ -417,7 +417,7 @@ export default {
           searchbar.clear()
           searchbar.search(filterQuery)
         }
-        /*if (groupBy === 'alphabetical')*/ this.$refs.listIndex.update()
+        this.$refs.listIndex.update()
       })
     },
     toggleSearchTag (e, item) {
