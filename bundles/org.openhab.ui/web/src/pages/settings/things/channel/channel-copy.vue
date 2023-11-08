81c2b422974b6d4c21a0b19b2565c76f45358186
d51x <dimonich110@gmail.com>
2023-11-08 20:43:05 +0300

    [MainUI] things: fix cope channel page title
    Signed-off-by: Dmitry P. (d51x) <dimonich110@gmail.com>

--- bundles/org.openhab.ui/web/src/pages/settings/things/channel/channel-copy.vue
+++ bundles/org.openhab.ui/web/src/pages/settings/things/channel/channel-copy.vue
@@ -1,6 +1,6 @@
 <template>
   <f7-page @page:afterin="onPageAfterIn" name="channel-copy">
-    <f7-navbar :title="channel.label" :subtitle="thing.label" back-link="Cancel">
+    <f7-navbar title="Copy channel" :subtitle="thing.label" back-link="Cancel">
       <f7-nav-right>
         <f7-link @click="save()" v-if="$theme.md" icon-md="material:save" icon-only />
         <f7-link @click="save()" v-if="!$theme.md">
