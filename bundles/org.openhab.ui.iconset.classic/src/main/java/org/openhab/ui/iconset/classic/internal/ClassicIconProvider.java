dfbdcf23b34afb1b352c19d88a2cbbb309b5e941
lolodomo <lg.hc@free.fr>
2023-11-13 22:13:07 +0100

    Remove PNG icons from the classic icon set (#2084)
    The docker environment is (unfortunately) kept. It can be used to
    compress SVG files. Conversion from SVG into PNG format has been
    commented in the script .convert.sh.
    
    Closes #2071
    Closes #1768
    
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>

--- bundles/org.openhab.ui.iconset.classic/src/main/java/org/openhab/ui/iconset/classic/internal/ClassicIconProvider.java
+++ bundles/org.openhab.ui.iconset.classic/src/main/java/org/openhab/ui/iconset/classic/internal/ClassicIconProvider.java
@@ -16,7 +16,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Locale;
 import java.util.Set;
 
@@ -60,9 +59,7 @@ public class ClassicIconProvider extends AbstractResourceIconProvider implements
 
     @Override
     public Set<IconSet> getIconSets(@Nullable Locale locale) {
-        Set<Format> formats = new HashSet<>(2);
-        formats.add(Format.PNG);
-        formats.add(Format.SVG);
+        Set<Format> formats = Set.of(Format.SVG);
 
         String label = i18nProvider.getText(context.getBundle(), "iconset.label", DEFAULT_LABEL, locale);
         label = label == null ? DEFAULT_LABEL : label;
