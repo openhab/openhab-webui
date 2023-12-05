1c15a11b7398406c4519a43cabf7506013c19f48
Florian Hotze <florianh_dev@icloud.com>
2023-12-05 22:24:12 +0100

    Item edit: Fix wording of Item name alert (#2200)
    Addresses
    https://github.com/openhab/openhab-webui/pull/2126#pullrequestreview-1765483229.
    
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- bundles/org.openhab.ui/web/src/pages/settings/items/item-edit.vue
+++ bundles/org.openhab.ui/web/src/pages/settings/items/item-edit.vue
@@ -160,7 +160,7 @@ export default {
       if (this.currentTab === 'code') {
         if (!this.fromYaml()) return Promise.reject()
       }
-      if (!this.item.name) return this.$f7.dialog.alert('Please give Item a valid name').open() // user cannot change name
+      if (!this.item.name) return this.$f7.dialog.alert('Please give the Item a valid name').open() // user cannot change name
       if (!this.item.type || !this.types.ItemTypes.includes(this.item.type.split(':')[0])) return this.$f7.dialog.alert('Please give Item a valid type').open()
 
       this.doSave(this.item).then(() => {
