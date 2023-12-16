caf2e8cde9c331116b3f93d8632aec9345b2990f
Miguel Álvarez <miguelwork92@gmail.com>
2023-12-16 14:34:39 +0100

    remove dot from description
    Signed-off-by: Miguel Álvarez <miguelwork92@gmail.com>

--- bundles/org.openhab.ui/web/src/components/item/metadata/item-metadata-voicesystem.vue
+++ bundles/org.openhab.ui/web/src/components/item/metadata/item-metadata-voicesystem.vue
@@ -4,7 +4,7 @@
       <f7-list-input ref="input" type="textarea" :floating-label="$theme.md" :label="'Custom Rules'" name="custom-rules"
                      :value="customRules" @input="updateValue" />
       <f7-block-footer class="param-description" slot="after-list">
-        <small>Enter each rule on a separate line. Available placeholders: $name$, $cmd$ and $*$.</small>
+        <small>Enter each rule on a separate line. Available placeholders: $name$, $cmd$ and $*$</small>
       </f7-block-footer>
     </f7-list>
     <config-sheet :parameterGroups="[]" :parameters="ruleOptionParameters" :configuration="metadata.config" />
