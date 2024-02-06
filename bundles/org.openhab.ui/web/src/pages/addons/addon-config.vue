511268d9c31531add0804da07c271af66dc863b1
Florian Hotze <florianh_dev@icloud.com>
2024-02-06 20:48:41 +0100

    Addon config page: Check for unsaved changes before leave & Add Ctrl-S shortcut for save
    This makes it consistent with the service config page.
    
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- bundles/org.openhab.ui/web/src/pages/addons/addon-config.vue
+++ bundles/org.openhab.ui/web/src/pages/addons/addon-config.vue
@@ -1,10 +1,10 @@
 <template>
-  <f7-page>
+  <f7-page @page:afterin="onPageAfterIn" @page:beforeout="onPageBeforeOut">
     <f7-navbar :title="'Configure ' + addon.label" back-link="Back">
       <f7-nav-right>
         <f7-link @click="save()" v-if="$theme.md" icon-md="material:save" icon-only />
         <f7-link @click="save()" v-if="!$theme.md">
-          Save
+          Save<span v-if="$device.desktop">&nbsp;(Ctrl-S)</span>
         </f7-link>
       </f7-nav-right>
     </f7-navbar>
@@ -76,8 +76,10 @@
 
 <script>
 import ConfigSheet from '@/components/config/config-sheet.vue'
+import DirtyMixin from '@/pages/settings/dirty-mixin'
 
 export default {
+  mixins: [DirtyMixin],
   components: {
     ConfigSheet
   },
@@ -99,6 +101,16 @@ export default {
     },
     name () {
       return this.addonId.split('-')[1]
+    }
+  },
+  watch: {
+    config: {
+      handler: function () {
+        if (!this.loading) {
+          this.dirty = true
+        }
+      },
+      deep: true
     }
   },
   methods: {
@@ -127,8 +139,25 @@ export default {
           closeTimeout: 2000
         }).open()
       })
-
+      this.dirty = false
       this.$f7router.back()
+    },
+    onPageAfterIn () {
+      if (window) {
+        window.addEventListener('keydown', this.keyDown)
+      }
+    },
+    onPageBeforeOut () {
+      if (window) {
+        window.removeEventListener('keydown', this.keyDown)
+      }
+    },
+    keyDown (ev) {
+      if (ev.keyCode === 83 && (ev.ctrlKey || ev.metaKey) && !(ev.altKey || ev.shiftKey)) {
+        this.save()
+        ev.stopPropagation()
+        ev.preventDefault()
+      }
     }
   },
   created () {
