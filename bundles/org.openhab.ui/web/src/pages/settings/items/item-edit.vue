openhab/openhab-webui

commit 75bac51626b5104c819459669f978b3492d1b3c8

Fixes & Improvements to persistence edit, channel details & doc links logic (#2339)
- Improve doc link logic and use Vuex store for the openHAB website url.
- Channel info: Fix clear buttons displayed even if not editable.
- Persistence edit: Fix styling issues.

---------

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

bundles/org.openhab.ui/web/src/pages/settings/items/item-edit.vue
@@ -171,8 +171,9 @@ export default {
       }
     },
     save () {
+      if (!this.item.editable) return
       if (this.currentTab === 'code') {
-        if (!this.fromYaml()) return Promise.reject()
+        if (!this.fromYaml()) return
       }
       if (this.validateItemName(this.item.name) !== '') return this.$f7.dialog.alert('Please give the Item a valid name: ' + this.validateItemName(this.item.name)).open()
       if (!this.item.type || !this.types.ItemTypes.includes(this.item.type.split(':')[0])) return this.$f7.dialog.alert('Please give Item a valid type').open()

bundles/org.openhab.ui/web/src/components/addons/addon-info-table.vue
@@ -129,7 +129,7 @@ export default {
           id: 'documentationLink',
           title: 'Documentation',
           afterIcon: 'question_circle_fill',
-          linkUrl: `https://${this.$store.state.runtimeInfo.buildString === 'Release Build' ? 'www' : 'next'}.openhab.org/addons/${this.addon.type.replace('misc', 'integrations').replace('binding', 'bindings').replace('transformation', 'transformations')}/${this.addon.id}` // this.addon.link
+          linkUrl: `${this.$store.state.websiteUrl}/addons/${this.addon.type.replace('misc', 'integrations').replace('binding', 'bindings').replace('transformation', 'transformations')}/${this.addon.id}` // this.addon.link
         })
 
         let repository

bundles/org.openhab.ui/web/src/components/developer/help-sidebar.vue
@@ -15,7 +15,7 @@
                   <f7-list-item v-for="step in instruct.steps" :key="step.title" :link="step.link" :title="step.title">
                     <div class="item-text" v-html="step.text" />
                   </f7-list-item>
-                  <f7-list-button v-if="instruct.button" :external="true" :title="instruct.button.title" :href="documentationLinkPrefix + instruct.button.link" target="_blank" />
+                  <f7-list-button v-if="instruct.button" :external="true" :title="instruct.button.title" :href="$store.state.websiteUrl + '/' + instruct.button.link" target="_blank" />
                 </f7-list>
               </f7-accordion-content>
             </f7-list-item>
@@ -41,7 +41,7 @@
                   </p>
                   <p v-html="faq.text" />
                   <p v-if="faq.doclink">
-                    <f7-link external target="_blank" :href="documentationLinkPrefix+faq.doclink">
+                    <f7-link external target="_blank" :href="$store.state.websiteUrl + '/' + faq.doclink">
                       Full Help Docs
                     </f7-link>
                   </p>
@@ -60,7 +60,7 @@
         </f7-block>
         <f7-block class="no-margin no-padding">
           <f7-list media-list>
-            <f7-list-item v-for="addon in addons" :key="addon.uid" :link="addon.link.replace('https://www.openhab.org/', documentationLinkPrefix)" :external="true" target="_blank" :title="addon.label.replaceAll(/Binding|Transformation|Persistence/gi,'')" :text="addon.type" />
+            <f7-list-item v-for="addon in addons" :key="addon.uid" :link="addon.link.replace('https://www.openhab.org', $store.state.websiteUrl)" :external="true" target="_blank" :title="addon.label.replaceAll(/Binding|Transformation|Persistence/gi,'')" :text="addon.type" />
           </f7-list>
         </f7-block>
       </div>
@@ -94,8 +94,8 @@
           You can find many more details and help at these resources:
           <ul>
             <li><f7-link external target="_blank" href="https://www.openhab.org/" v-t="'about.homePage'" /></li>
-            <li><f7-link external target="_blank" :href="`${documentationLinkPrefix}link/docs`" v-t="'about.documentation'" /></li>
-            <li><f7-link external :href="`${documentationLinkPrefix}link/tutorial`" target="_blank" v-t="'home.overview.button.tutorial'" /></li>
+            <li><f7-link external target="_blank" :href="`${$store.state.websiteUrl}/link/docs`" v-t="'about.documentation'" /></li>
+            <li><f7-link external :href="`${$store.state.websiteUrl}/link/tutorial`" target="_blank" v-t="'home.overview.button.tutorial'" /></li>
             <li><f7-link external target="_blank" href="https://community.openhab.org/" v-t="'about.communityForum'" /></li>
           </ul>
         </f7-block>
@@ -155,11 +155,6 @@ export default {
       addons: [],
       faqs: require('@/assets/definitions/help/help-faq-defs.json'),
       qstart: require('@/assets/definitions/help/help-qstart-defs.json')
-    }
-  },
-  computed: {
-    documentationLinkPrefix () {
-      return `https://${this.$store.state.runtimeInfo.buildString === 'Release Build' ? 'www' : 'next'}.openhab.org/`
     }
   },
   created () {

bundles/org.openhab.ui/web/src/components/developer/help/context.vue
@@ -24,9 +24,6 @@ export default {
       if (this.$store.state.runtimeInfo.buildString === 'Release Build') return 'final-stable'
       return 'final'
     },
-    docUrl () {
-      return `https://${this.$store.state.runtimeInfo.buildString === 'Release Build' ? 'www' : 'next'}.openhab.org/docs`
-    },
     docSrcUrl () {
       return `https://raw.githubusercontent.com/openhab/openhab-docs/${this.docsBranch}/mainui`
     },
@@ -35,8 +32,8 @@ export default {
       return this.$store.state.pagePath
     },
     documentationLink () {
-      if (this.path.endsWith('index')) return `${this.docUrl}/mainui${this.path.replace('index', '')}`
-      return `${this.docUrl}/mainui${this.path}`
+      if (this.path.endsWith('index')) return `${this.$store.state.websiteUrl}/docs/mainui${this.path.replace('index', '')}`
+      return `${this.$store.state.websiteUrl}/docs/mainui${this.path}`
     }
   },
   watch: {
@@ -91,7 +88,7 @@ export default {
             body = body.replace(/<img src=".*$/gm, '') // Remove images
 
             // Fix {{base}} and /docs anchor href for doc pages
-            body = body.replace(/<a href="(%7B%7Bbase%7D%7D|\/docs)/gm, `<a class="external" target="_blank" href="${this.docUrl}`)
+            body = body.replace(/<a href="(%7B%7Bbase%7D%7D|\/docs)/gm, `<a class="external" target="_blank" href="${this.$store.state.websiteUrl}/docs`)
             // Fix local folder anchor href: Rewrite folder to /folder/
             body = body.replace(/(<a href=")([A-z]+)(")/gm, '$1' + this.localUrl + '$2/$3')
 

bundles/org.openhab.ui/web/src/components/item/metadata/item-metadata-alexa.vue
@@ -87,9 +87,7 @@ export default {
       itemType: this.item.groupType || this.item.type,
       multiple: !!this.metadata.value && this.metadata.value.indexOf(',') > 0,
       classSelectKey: this.$f7.utils.id(),
-      docUrl:
-        `https://${this.$store.state.runtimeInfo.buildString === 'Release Build' ? 'www' : 'next'}.openhab.org` +
-        '/link/alexa',
+      docUrl: `${this.$store.state.websiteUrl}/link/alexa`,
       ready: false
     }
   },

bundles/org.openhab.ui/web/src/components/item/metadata/item-metadata-ga.vue
@@ -28,7 +28,7 @@
       <config-sheet :parameterGroups="[]" :parameters="parameters" :configuration="metadata.config" />
     </div>
     <p class="padding">
-      <f7-link color="blue" external target="_blank" :href="`https://${$store.state.runtimeInfo.buildString === 'Release Build' ? 'www' : 'next'}.openhab.org/link/google-assistant`">
+      <f7-link color="blue" external target="_blank" :href="`${$store.state.websiteUrl}/link/google-assistant`">
         Google Assistant Integration Documentation
       </f7-link>
     </p>

bundles/org.openhab.ui/web/src/components/item/metadata/item-metadata-homekit.vue
@@ -53,7 +53,7 @@
       </f7-block-footer>
     </f7-block>
     <p class="padding">
-      <f7-link color="blue" external target="_blank" :href="`https://${$store.state.runtimeInfo.buildString === 'Release Build' ? 'www' : 'next'}.openhab.org/link/homekit`">
+      <f7-link color="blue" external target="_blank" :href="`${$store.state.websiteUrl}/link/homekit`">
         HomeKit integration documentation
       </f7-link>
     </p>

bundles/org.openhab.ui/web/src/js/store/index.js
@@ -23,6 +23,7 @@ const store = new Vuex.Store({
     apiEndpoints: null,
     locale: null,
     runtimeInfo: null,
+    websiteUrl: null,
     developerDock: false,
     pagePath: null
   },
@@ -35,6 +36,7 @@ const store = new Vuex.Store({
       state.apiVersion = rootResponse.version
       state.runtimeInfo = rootResponse.runtimeInfo
       state.apiEndpoints = rootResponse.links
+      state.websiteUrl = `https://${rootResponse.runtimeInfo?.buildString !== 'Release Build' ? 'next' : 'www'}.openhab.org`
     },
     setLocale (state, locale) {
       state.locale = locale

commit 76aebaf08eb85b72205305921456e84ecba7c510

Fix item-add save (#2346)
Regression from #2339 causing a new item unable to be created / saved.

Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

bundles/org.openhab.ui/web/src/pages/settings/items/item-edit.vue
@@ -171,7 +171,7 @@ export default {
       }
     },
     save () {
-      if (!this.item.editable) return
+      if (!this.editable) return
       if (this.currentTab === 'code') {
         if (!this.fromYaml()) return
       }