openhab/openhab-webui

commit 5a9c0c90c7dd7a321c913b77d7b0bc0299d03f27

Fix alexa item group type parameter support (#2194)
Signed-off-by: jsetton <jeremy.setton@gmail.com>

bundles/org.openhab.ui/web/src/assets/definitions/metadata/alexa/deviceattributes.js
@@ -33,8 +33,8 @@ export default {
   CurrentOpenState: {
     itemTypes: ['Contact', 'Number', 'String', 'Switch'],
     requires: ['TargetOpenState'],
-    parameters: (item) =>
-      item.type === 'Contact' || item.type === 'Switch'
+    parameters: (itemType) =>
+      itemType === 'Contact' || itemType === 'Switch'
         ? [p.inverted()]
         : OPEN_STATES.map((state) => p.valueMapping(state))
   },
@@ -45,13 +45,13 @@ export default {
   },
   PositionState: {
     itemTypes: ['Dimmer', 'Rollershutter'],
-    parameters: (item) => [
-      p.inverted(item.type === 'Rollershutter'),
+    parameters: (itemType, item) => [
+      p.inverted(itemType === 'Rollershutter'),
       p.presets(item.stateDescription, '20=Morning,60=Afternoon,80=Evening:@Setting.Night'),
       p.language(item.settings && item.settings.regional.language),
       p.actionMappings({ default: 'value' }, 'Close=0,Open=100,Lower=0,Raise=100', (config) => {
         const primaryControl = getGroupParameter('primaryControl', item.groups) || 'position'
-        if (item.type === 'Dimmer') {
+        if (itemType === 'Dimmer') {
           return primaryControl === 'position'
             ? config.inverted === true
               ? ['Close=100', 'Open=0', 'Lower=100', 'Raise=0']
@@ -60,7 +60,7 @@ export default {
               ? ['Lower=100', 'Raise=0']
               : ['Lower=0', 'Raise=100']
         }
-        if (item.type === 'Rollershutter') {
+        if (itemType === 'Rollershutter') {
           return primaryControl === 'position'
             ? ['Close=DOWN', 'Open=UP', 'Lower=DOWN', 'Raise=UP', 'Stop=STOP']
             : ['Lower=DOWN', 'Raise=UP']
@@ -70,19 +70,19 @@ export default {
   },
   TiltAngle: {
     itemTypes: ['Dimmer', 'Number', 'Number:Angle', 'Rollershutter'],
-    parameters: (item) => [
-      p.inverted(item.type === 'Rollershutter'),
+    parameters: (itemType, item) => [
+      p.inverted(itemType === 'Rollershutter'),
       p.presets(item.stateDescription, '20=Morning,60=Afternoon,80=Evening:@Setting.Night'),
       p.language(item.settings && item.settings.regional.language),
       ...(getGroupParameter('primaryControl', item.groups) !== 'tilt' ? [] : [
         p.actionMappings({ default: 'value' }, 'Close=0,Open=100', (config) => {
-          if (item.type === 'Dimmer') {
+          if (itemType === 'Dimmer') {
             return config.inverted === true ? ['Close=100', 'Open=0'] : ['Close=0', 'Open=100']
           }
-          if (item.type === 'Number' || item.type === 'Number:Angle') {
+          if (itemType === 'Number' || itemType === 'Number:Angle') {
             return config.inverted === true ? ['Close=90', 'Open=0'] : ['Close=-90', 'Open=0']
           }
-          if (item.type === 'Rollershutter') {
+          if (itemType === 'Rollershutter') {
             return ['Close=DOWN', 'Open=UP', 'Stop=STOP']
           }
         })
@@ -93,9 +93,9 @@ export default {
   // Entertainment Attributes
   Channel: {
     itemTypes: ['Number', 'String'],
-    parameters: (item) => [
-      p.channelMappings(item.type === 'String'),
-      ...(item.type === 'Number' ? [p.channelRange()] : [p.supportsChannelNumber()]),
+    parameters: (itemType) => [
+      p.channelMappings(itemType === 'String'),
+      ...(itemType === 'Number' ? [p.channelRange()] : [p.supportsChannelNumber()]),
       p.retrievable()
     ]
   },
@@ -105,8 +105,8 @@ export default {
   },
   Input: {
     itemTypes: ['Number', 'String'],
-    parameters: (item) => [
-      p.supportedInputs(item.stateDescription, item.type === 'String' ? 'HDMI1=Cable,HDMI2=Kodi' : '1=Cable,2=Kodi'),
+    parameters: (itemType, item) => [
+      p.supportedInputs(item.stateDescription, itemType === 'String' ? 'HDMI1=Cable,HDMI2=Kodi' : '1=Cable,2=Kodi'),
       p.language(item.settings && item.settings.regional.language),
       p.retrievable()
     ]
@@ -129,28 +129,28 @@ export default {
   },
   EqualizerBass: {
     itemTypes: ['Dimmer', 'Number'],
-    parameters: (item) => [
-      p.equalizerRange(item.type === 'Dimmer' ? '0:100' : '-10:10'),
-      p.equalizerDefaultLevel(item.type === 'Dimmer' ? 50 : 0),
-      p.increment(item.type === 'Dimmer' ? 'INCREASE/DECREASE' : 1),
+    parameters: (itemType) => [
+      p.equalizerRange(itemType === 'Dimmer' ? '0:100' : '-10:10'),
+      p.equalizerDefaultLevel(itemType === 'Dimmer' ? 50 : 0),
+      p.increment(itemType === 'Dimmer' ? 'INCREASE/DECREASE' : 1),
       p.retrievable()
     ]
   },
   EqualizerMidrange: {
     itemTypes: ['Dimmer', 'Number'],
-    parameters: (item) => [
-      p.equalizerRange(item.type === 'Dimmer' ? '0:100' : '-10:10'),
-      p.equalizerDefaultLevel(item.type === 'Dimmer' ? 50 : 0),
-      p.increment(item.type === 'Dimmer' ? 'INCREASE/DECREASE' : 1),
+    parameters: (itemType) => [
+      p.equalizerRange(itemType === 'Dimmer' ? '0:100' : '-10:10'),
+      p.equalizerDefaultLevel(itemType === 'Dimmer' ? 50 : 0),
+      p.increment(itemType === 'Dimmer' ? 'INCREASE/DECREASE' : 1),
       p.retrievable()
     ]
   },
   EqualizerTreble: {
     itemTypes: ['Dimmer', 'Number'],
-    parameters: (item) => [
-      p.equalizerRange(item.type === 'Dimmer' ? '0:100' : '-10:10'),
-      p.equalizerDefaultLevel(item.type === 'Dimmer' ? 50 : 0),
-      p.increment(item.type === 'Dimmer' ? 'INCREASE/DECREASE' : 1),
+    parameters: (itemType) => [
+      p.equalizerRange(itemType === 'Dimmer' ? '0:100' : '-10:10'),
+      p.equalizerDefaultLevel(itemType === 'Dimmer' ? 50 : 0),
+      p.increment(itemType === 'Dimmer' ? 'INCREASE/DECREASE' : 1),
       p.retrievable()
     ]
   },
@@ -179,8 +179,8 @@ export default {
   // Fan Attributes
   FanDirection: {
     itemTypes: ['String', 'Switch'],
-    parameters: (item) => [
-      ...(item.type === 'Switch' ? [p.inverted()] : FAN_DIRECTIONS.map((direction) => p.valueMapping(direction))),
+    parameters: (itemType) => [
+      ...(itemType === 'Switch' ? [p.inverted()] : FAN_DIRECTIONS.map((direction) => p.valueMapping(direction))),
       p.retrievable()
     ]
   },
@@ -190,10 +190,10 @@ export default {
   },
   FanSpeed: {
     itemTypes: ['Dimmer', 'Number', 'String'],
-    parameters: (item) => [
-      ...(item.type === 'Dimmer'
+    parameters: (itemType) => [
+      ...(itemType === 'Dimmer'
         ? [p.inverted()]
-        : item.type === 'Number'
+        : itemType === 'Number'
           ? [p.speedLevels()]
           : FAN_SPEEDS.map((speed) => p.valueMapping(speed))),
       p.retrievable()
@@ -211,11 +211,11 @@ export default {
   },
   ColorTemperature: {
     itemTypes: ['Dimmer', 'Number', 'Number:Temperature'],
-    parameters: (item) => [
-      ...(item.type === 'Dimmer' ? [p.colorTemperatureBinding()] : []),
+    parameters: (itemType) => [
+      ...(itemType === 'Dimmer' ? [p.colorTemperatureBinding()] : []),
       p.colorTemperatureRange(),
-      p.increment(item.type === 'Dimmer' ? 'INCREASE/DECREASE' : 500),
-      ...(item.type === 'Number' ? [p.requiresSetColorReset()] : []),
+      p.increment(itemType === 'Dimmer' ? 'INCREASE/DECREASE' : 500),
+      ...(itemType === 'Number' ? [p.requiresSetColorReset()] : []),
       p.retrievable()
     ]
   },
@@ -248,8 +248,8 @@ export default {
   CurrentLockState: {
     itemTypes: ['Contact', 'Number', 'String', 'Switch'],
     requires: ['TargetLockState'],
-    parameters: (item) =>
-      item.type === 'Contact' || item.type === 'Switch'
+    parameters: (itemType) =>
+      itemType === 'Contact' || itemType === 'Switch'
         ? [p.inverted()]
         : LOCK_STATES.map((state) => p.valueMapping(state))
   },
@@ -324,8 +324,8 @@ export default {
   PowerState: {
     itemTypes: ['Color', 'Dimmer', 'Switch'],
     customTypes: ['Number', 'String'],
-    parameters: (item) => [
-      ...(item.type === 'Number' || item.type === 'String'
+    parameters: (itemType) => [
+      ...(itemType === 'Number' || itemType === 'String'
         ? [p.valueMapping('OFF', true), p.valueMapping('ON', true)]
         : []),
       p.retrievable()
@@ -337,7 +337,7 @@ export default {
   },
   Percentage: {
     itemTypes: ['Dimmer', 'Rollershutter'],
-    parameters: (item) => [p.inverted(item.type === 'Rollershutter'), p.retrievable()]
+    parameters: (itemType) => [p.inverted(itemType === 'Rollershutter'), p.retrievable()]
   },
 
   // Thermostat Attributes
@@ -378,15 +378,15 @@ export default {
   ThermostatHold: {
     itemTypes: ['Number', 'String', 'Switch'],
     requires: ['HeatingCoolingMode'],
-    parameters: (item) => [
-      ...(item.type === 'Switch' ? [p.inverted()] : [p.valueMapping('OFF'), p.valueMapping('ON')]),
+    parameters: (itemType) => [
+      ...(itemType === 'Switch' ? [p.inverted()] : [p.valueMapping('OFF'), p.valueMapping('ON')]),
       p.requiresSetpointHold()
     ]
   },
   ThermostatFan: {
     itemTypes: ['String', 'Switch'],
-    parameters: (item) => [
-      ...(item.type === 'Switch' ? [p.inverted()] : THERMOSTAT_FAN_MODES.map((mode) => p.valueMapping(mode))),
+    parameters: (itemType) => [
+      ...(itemType === 'Switch' ? [p.inverted()] : THERMOSTAT_FAN_MODES.map((mode) => p.valueMapping(mode))),
       p.retrievable()
     ]
   },
@@ -401,7 +401,7 @@ export default {
   Mode: {
     itemTypes: ['Number', 'String', 'Switch'],
     supports: ['multiInstance'],
-    parameters: (item, config) => [
+    parameters: (_, item, config) => [
       p.capabilityNames(
         item.groups.length ? item.label : '@Setting.Mode',
         'Wash Temperature,@Setting.WaterTemperature'
@@ -421,19 +421,19 @@ export default {
   RangeValue: {
     itemTypes: ['Dimmer', 'Number', 'Number:*', 'Rollershutter'],
     supports: ['multiInstance'],
-    parameters: (item) => [
+    parameters: (itemType, item) => [
       p.capabilityNames(item.groups.length ? item.label : '@Setting.RangeValue', '@Setting.FanSpeed,Speed'),
-      p.inverted(item.type === 'Rollershutter'),
+      p.inverted(itemType === 'Rollershutter'),
       p.nonControllable(item.stateDescription),
       p.retrievable(),
-      ...(item.type === 'Dimmer'
+      ...(itemType === 'Dimmer'
         ? [p.supportedCommands(['ON', 'OFF', 'INCREASE', 'DECREASE'], 'INCREASE=@Value.Up,DECREASE=@Value.Down')]
-        : item.type === 'Rollershutter'
+        : itemType === 'Rollershutter'
           ? [p.supportedCommands(['UP', 'DOWN', 'MOVE', 'STOP'], 'UP=@Value.Open,DOWN=@Value.Close,STOP=@Value.Stop')]
           : []),
       p.supportedRange(
         item.stateDescription,
-        item.type === 'Dimmer' || item.type === 'Rollershutter' ? '0:100:1' : '0:10:1'
+        itemType === 'Dimmer' || itemType === 'Rollershutter' ? '0:100:1' : '0:10:1'
       ),
       p.presets(item.stateDescription, '1=@Value.Low:Lowest,10=@Value.High:Highest'),
       p.unitOfMeasure(item),
@@ -445,9 +445,9 @@ export default {
   ToggleState: {
     itemTypes: ['Number', 'String', 'Switch'],
     supports: ['multiInstance'],
-    parameters: (item) => [
+    parameters: (itemType, item) => [
       p.capabilityNames(item.groups.length ? item.label : '@Setting.ToggleState', '@Setting.Oscillate,Rotate'),
-      ...(item.type === 'Number' || item.type === 'String'
+      ...(itemType === 'Number' || itemType === 'String'
         ? [p.valueMapping('OFF', true), p.valueMapping('ON', true)]
         : [p.inverted()]),
       p.nonControllable(item.stateDescription),

bundles/org.openhab.ui/web/src/assets/definitions/metadata/alexa/devicetypes.js
@@ -58,20 +58,19 @@ const thermostatAttributes = [
   ...genericAttributes
 ]
 
-const blindParameters = (item) => {
+const blindParameters = (_, item) => {
   const attributes = ['PositionState', 'TiltAngle']
   const metadata = item.members.map((mbr) => mbr.metadata && mbr.metadata.alexa.value).join(',')
   return attributes.every((attr) => metadata.includes(attr)) ? [p.primaryControl()] : []
 }
 
-const networkParameters = (item) => {
+const networkParameters = (_, item) => {
   const deviceTypes = ['NetworkHardware', 'Router']
   const connection = item.groups.find((g) => deviceTypes.includes(g.metadata.alexa.value))
   return connection ? [p.connectedTo(connection.label || connection.name), p.hostname(), p.macAddress()] : []
 }
 
-export const defaultParameters = (item) => {
-  const itemType = item.groupType || item.type
+export const defaultParameters = (itemType, item) => {
   return itemType === 'Group' || !item.groups.length
     ? [p.deviceName(item.label), p.deviceDescription(`${itemType} ${item.name}`)]
     : []
@@ -304,7 +303,7 @@ export default {
   Thermostat: {
     defaultAttributes: ['HeatingCoolingMode'],
     supportedAttributes: thermostatAttributes,
-    groupParameters: (item) => [p.scale(item, true)]
+    groupParameters: (_, item) => [p.scale(item, true)]
   },
   VacuumCleaner: {
     defaultAttributes: ['PowerState', 'VacuumMode'],

bundles/org.openhab.ui/web/src/components/item/metadata/item-metadata-alexa.vue
@@ -126,7 +126,7 @@ export default {
     parameters () {
       return this.classes.reduce((parameters, cl) => {
         const { parameters: params = [] } = this.getDefinition(cl)
-        for (const p of params.map((p) => p(this.item, this.metadata.config)).flat()) {
+        for (const p of params.map((p) => p(this.itemType, this.item, this.metadata.config)).flat()) {
           if (p.description) p.description = p.description.replace('%DOC_URL%', this.docUrl)
           if (!parameters.find((e) => e.name === p.name)) parameters.push(p)
         }

commit 344779acfa8e0feb9cb5984539d2c1ed74b6c633

Fix alexa thermostat attribute parameters (#2364)
Breakage introduced by #2194

Signed-off-by: jsetton <jeremy.setton@gmail.com>

bundles/org.openhab.ui/web/src/assets/definitions/metadata/alexa/deviceattributes.js
@@ -309,7 +309,7 @@ export default {
   },
   CurrentTemperature: {
     itemTypes: ['Number', 'Number:Temperature'],
-    parameters: (item) => [p.scale(item)]
+    parameters: (_, item) => [p.scale(item)]
   },
   ContactDetectionState: {
     itemTypes: ['Contact', 'Switch'],
@@ -343,27 +343,27 @@ export default {
   // Thermostat Attributes
   TargetTemperature: {
     itemTypes: ['Number', 'Number:Temperature'],
-    parameters: (item) => [p.scale(item), p.setpointRange(item), p.retrievable()]
+    parameters: (_, item) => [p.scale(item), p.setpointRange(item), p.retrievable()]
   },
   CoolingSetpoint: {
     itemTypes: ['Number', 'Number:Temperature'],
     requires: ['HeatingSetpoint'],
-    parameters: (item) => [p.scale(item), p.comfortRange(item), p.setpointRange(item), p.retrievable()]
+    parameters: (_, item) => [p.scale(item), p.comfortRange(item), p.setpointRange(item), p.retrievable()]
   },
   HeatingSetpoint: {
     itemTypes: ['Number', 'Number:Temperature'],
     requires: ['CoolingSetpoint'],
-    parameters: (item) => [p.scale(item), p.comfortRange(item), p.setpointRange(item), p.retrievable()]
+    parameters: (_, item) => [p.scale(item), p.comfortRange(item), p.setpointRange(item), p.retrievable()]
   },
   EcoCoolingSetpoint: {
     itemTypes: ['Number', 'Number:Temperature'],
     requires: ['EcoHeatingSetpoint'],
-    parameters: (item) => [p.scale(item), p.comfortRange(item), p.setpointRange(item), p.retrievable()]
+    parameters: (_, item) => [p.scale(item), p.comfortRange(item), p.setpointRange(item), p.retrievable()]
   },
   EcoHeatingSetpoint: {
     itemTypes: ['Number', 'Number:Temperature'],
     requires: ['EcoCoolingSetpoint'],
-    parameters: (item) => [p.scale(item), p.comfortRange(item), p.setpointRange(item), p.retrievable()]
+    parameters: (_, item) => [p.scale(item), p.comfortRange(item), p.setpointRange(item), p.retrievable()]
   },
   HeatingCoolingMode: {
     itemTypes: ['Number', 'String', 'Switch'],

bundles/org.openhab.ui/web/src/assets/definitions/metadata/alexa/helpers.js
@@ -53,6 +53,8 @@ export const getSupportedRange = (item, config, defaultValue) => {
 }
 
 export const getTemperatureScale = (item) => {
+  const scale = getGroupParameter('scale', item.groups)
+  if (scale) return scale
   const itemType = item.groupType || item.type
   const format = (itemType === 'Number:Temperature' && item.unitSymbol) || item.stateDescription?.pattern
   if (format?.endsWith('°C')) return 'CELSIUS'

bundles/org.openhab.ui/web/src/assets/definitions/metadata/alexa/parameters.js
@@ -10,7 +10,6 @@ import {
 } from './constants.js'
 import {
   docLink,
-  getGroupParameter,
   getOptions,
   getSemanticFormat,
   getSupportedRange,
@@ -106,11 +105,7 @@ export default {
     label: 'Comfort Range',
     type: 'INTEGER',
     min: 1,
-    default: (config) => {
-      const scale = config.scale || getGroupParameter('scale', item.groups) || getTemperatureScale(item)
-      if (scale === 'CELSIUS') return 1
-      if (scale === 'FAHRENHEIT') return 2
-    }
+    default: (config) => (config.scale || getTemperatureScale(item)) === 'FAHRENHEIT' ? 2 : 1
   }),
   connectedTo: (value) => ({
     name: 'connectedTo',
@@ -286,7 +281,7 @@ export default {
     name: 'scale',
     label: 'Scale',
     type: 'TEXT',
-    default: getGroupParameter('scale', item.groups) || getTemperatureScale(item),
+    default: getTemperatureScale(item) === 'FAHRENHEIT' ? 'FAHRENHEIT' : 'CELSIUS',
     options: getOptions(TEMPERATURE_SCALES),
     limitToOptions: true,
     advanced
@@ -296,11 +291,7 @@ export default {
     label: 'Setpoint Range',
     description: 'Formatted as <code>minValue:maxValue</code>',
     type: 'TEXT',
-    default: (config) => {
-      const scale = config.scale || getGroupParameter('scale', item.groups) || getTemperatureScale(item)
-      if (scale === 'CELSIUS') return '4:32'
-      if (scale === 'FAHRENHEIT') return '40:90'
-    },
+    default: (config) => (config.scale || getTemperatureScale(item)) === 'FAHRENHEIT' ? '40:90' : '4:32',
     pattern: '[+-]?[0-9]+:[+-]?[0-9]+'
   }),
   speedLevels: () => ({