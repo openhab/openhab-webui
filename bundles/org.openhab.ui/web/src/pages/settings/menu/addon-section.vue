c1d305eaa11d8af5188227fdac5fed105a0c596c
Jimmy Tanagra <jcode@tanagra.id.au>
2024-02-26 00:00:42 +1000

    Settings menu: fix search bar to include system and addons settings
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- bundles/org.openhab.ui/web/src/pages/settings/menu/addon-section.vue
+++ bundles/org.openhab.ui/web/src/pages/settings/menu/addon-section.vue
@@ -8,8 +8,9 @@
         v-for="a in addonsSettings"
         :key="a.uid"
         :link="'addons/' + a.uid"
-        :title="a.label" />
-      <f7-list-button v-if="!expanded && addonsInstalled.length > addonsSettings.length" color="blue" @click="expanded = true">
+        :title="a.label"
+        v-show="!a.hidden" />
+      <f7-list-button v-if="!expanded && addonsSettings.find((a) => a.hidden)" color="blue" @click="$emit('expand')">
         {{ $t('dialogs.showAll') }}
       </f7-list-button>
     </f7-list>
@@ -18,20 +19,15 @@
 
 <script>
 export default {
-  props: ['addonsInstalled', 'addonsServices'],
-  data () {
-    return {
-      ready: false,
-      expanded: false
-    }
-  },
+  props: ['addonsInstalled', 'addonsServices', 'expanded'],
   computed: {
     addonsSettings () {
       if (this.expanded) return this.addonsInstalled
-      return this.addonsInstalled.filter((a) =>
-        a.type === 'persistence' ||
-        this.addonsServices.findIndex((as) => as.configDescriptionURI.split(':')[1] === a.id) > -1
-      )
+      return this.addonsInstalled.map((addon) => {
+        const show = addon.type === 'persistence' ||
+          this.addonsServices.findIndex((as) => as.configDescriptionURI.split(':')[1] === addon.id) > -1
+        return Object.assign({ hidden: !show }, addon)
+      })
     }
   }
 }
