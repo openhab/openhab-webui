007205622eb45a184de1eeaa3eb662239d2afeb4
Florian Hotze <florianh_dev@icloud.com>
2024-02-28 13:17:33 +0100

    Persistence edit: Fix title flickering on slow load
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- bundles/org.openhab.ui/web/src/pages/settings/persistence/persistence-edit.vue
+++ bundles/org.openhab.ui/web/src/pages/settings/persistence/persistence-edit.vue
@@ -1,7 +1,6 @@
 <template>
   <f7-page @page:afterin="onPageAfterIn" @page:beforeout="onPageBeforeOut">
-    <f7-navbar :title="editable ? `${newPersistence ? 'Create' : 'Edit'} ${serviceId} persistence configuration` : `${serviceId} persistence configuration details`"
-               back-link="Back">
+    <f7-navbar :title="pageTitle" back-link="Back">
       <f7-nav-right v-show="ready">
         <f7-link v-if="!editable" icon-f7="lock_fill" icon-only tooltip="This persistence configuration is not editable through the UI" />
         <f7-link v-else-if="$theme.md" icon-md="material:save" icon-only @click="save()" />
@@ -261,6 +260,11 @@ export default {
   computed: {
     editable () {
       return this.newPersistence || (this.persistence && this.persistence.editable === true)
+    },
+    pageTitle () {
+      if (!this.ready) return ''
+      if (!this.editable) return `${this.serviceId} persistence configuration details`
+      return `${this.newPersistence ? 'Create' : 'Edit'} ${this.serviceId} persistence configuration`
     },
     strategies () {
       return this.PredefinedStrategies.concat(this.persistence.cronStrategies.map(cs => cs.name))
