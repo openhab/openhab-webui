openhab/openhab-webui

commit 974e28e9babf865d92297980db7e4ff144ffa566

Script edit: Fix dirty handling (#2508)
Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

bundles/org.openhab.ui/web/src/pages/settings/rules/script/script-edit.vue
@@ -269,12 +269,29 @@ export default {
     }
   },
   methods: {
+    /**
+     * Calculates the value of `this.dirty` from the individual dirty states.
+     */
     calculateDirty () {
       this.dirty = this.scriptDirty || this.modeDirty || this.ruleDirty || this.currentModuleDirty
     },
+    /**
+     * Calls {@link initDirty} and resets `this.dirty` and all individual dirty states to `false`.
+     */
     resetDirty () {
-      this.scriptDirty = this.modeDirty = this.ruleDirty = this.currentModuleDirty = false
-      this.calculateDirty()
+      this.initDirty()
+      this.dirty = this.scriptDirty = this.modeDirty = this.ruleDirty = this.currentModuleDirty = false
+    },
+    /**
+     * Stores the current state of tracked objects as the saved state of those, e.g. `this.rule` is cloned to `this.savedRule`.
+     */
+    initDirty () {
+      this.savedRule = cloneDeep(this.rule)
+      if (this.currentModule) {
+        this.savedCurrentModule = cloneDeep(this.currentModule)
+        this.savedMode = this.mode = this.currentModule.configuration.type
+        this.savedScript = this.script = this.currentModule.configuration.script || ''
+      }
     },
     onPageAfterIn () {
       if (this.ready) return
@@ -379,7 +396,6 @@ export default {
       Promise.all([this.$oh.api.get('/rest/module-types?type=trigger'), this.$oh.api.get('/rest/rules/' + this.ruleId)]).then((data) => {
         this.$set(this.moduleTypes, 'triggers', data[0])
         this.$set(this, 'rule', data[1])
-        this.savedRule = cloneDeep(this.rule)
 
         if (this.moduleId) {
           this.$set(this, 'currentModule', this.rule.actions.concat(this.rule.conditions).find((m) => m.id === this.moduleId))
@@ -388,11 +404,7 @@ export default {
           this.isScriptRule = true
         }
 
-        if (this.currentModule) {
-          this.savedCurrentModule = cloneDeep(this.currentModule)
-          this.savedMode = this.mode = this.currentModule.configuration.type
-          this.savedScript = this.script = this.currentModule.configuration.script || ''
-        }
+        this.initDirty()
 
         if (!this.rule.editable) {
           const commentChar = (this.mode === 'application/x-ruby' ? '#' : '//')

commit 4465a45398898422ea40bb0060b2ee8b7e02c048

Script edit: Fix script creation fails due to exception
Regression from #2508.

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

bundles/org.openhab.ui/web/src/pages/settings/rules/script/script-edit.vue
@@ -276,10 +276,9 @@ export default {
       this.dirty = this.scriptDirty || this.modeDirty || this.ruleDirty || this.currentModuleDirty
     },
     /**
-     * Calls {@link initDirty} and resets `this.dirty` and all individual dirty states to `false`.
+     * Resets `this.dirty` and all individual dirty states to `false`.
      */
     resetDirty () {
-      this.initDirty()
       this.dirty = this.scriptDirty = this.modeDirty = this.ruleDirty = this.currentModuleDirty = false
     },
     /**
@@ -462,6 +461,7 @@ export default {
       this.currentModule.configuration.script = this.script
       this.currentModule.configuration.type = this.mode
       return this.$oh.api.put('/rest/rules/' + this.rule.uid, this.rule).then((data) => {
+        this.initDirty()
         this.resetDirty()
         if (!noToast) {
           this.$f7.toast.create({