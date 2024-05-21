968fec5955c0d839f5dbbc69d69143775bfd3816
Thomas Wunschel <4302898+wuschi@users.noreply.github.com>
2024-05-21 15:46:47 +0200

    - get model from vuex store in model-tab.vue - do not pass model as property anymore in home.vue / home-edit.vue - add tab components for locations-tab / equipment-tab / properties-tab to avoid having to pass type parameter directly to model-tab - allow oh-locations-tab / oh-equipment-tab / oh-properties-tab in yaml of page-vuew.vue
    Signed-off-by: Thomas Wunschel <4302898+wuschi@users.noreply.github.com>

---
+++ bundles/org.openhab.ui/web/src/components/tabs/equipment-tab.vue
@@ -0,0 +1,12 @@
+<template>
+  <model-tab type="equipment" :page="context"  />
+</template>
+
+<script>
+import ModelTab from '@/pages/home/model-tab.vue'
+
+export default {
+  props: ['context'],
+  components: { ModelTab }
+}
+</script>
