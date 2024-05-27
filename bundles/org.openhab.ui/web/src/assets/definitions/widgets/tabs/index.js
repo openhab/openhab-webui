6339c62e6f2886e75904993ec9eba361a604f723
Dan Cunningham <dan@digitaldan.com>
2024-05-27 11:44:19 -0700

    [mainui] Adds optional Badge and Badge Color to tabs
    Signed-off-by: Dan Cunningham <dan@digitaldan.com>

--- bundles/org.openhab.ui/web/src/assets/definitions/widgets/tabs/index.js
+++ bundles/org.openhab.ui/web/src/assets/definitions/widgets/tabs/index.js
@@ -6,6 +6,8 @@ export const OhTabDefinition = () => new WidgetDefinition('oh-tab', 'Tab', 'Disp
   .params([
     pt('title', 'Title', 'Title of the tab'),
     pt('icon', 'Icon', 'Use <code>f7:iconName</code> (<a class="external text-color-blue" target="_blank" href="https://framework7.io/icons/">Framework7 icon</a>) or <code>material:iconName</code> (<a class="external text-color-blue" target="_blank" href="https://jossef.github.io/material-design-icons-iconfont/">Material icon</a>)'),
+    pt('badge', 'Badge', 'Badge text to display').a(),
+    pt('badgeColor', 'Badge Color', 'Color of the badge').a(),
     pt('page', 'Page', 'Page to display').c('page'),
     pt('pageConfig', 'Page Configuration', 'Parameters (props) to configure the page, if any').c('props')
   ])
