b56cd5fe05b46d2ca10aa838a3b31df41437db28
Mark Herwege <mark.herwege@telenet.be>
2024-09-02 10:09:47 +0200

    descriptive method dropdown
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

--- bundles/org.openhab.ui/web/src/assets/definitions/blockly/blocks-http.js
+++ bundles/org.openhab.ui/web/src/assets/definitions/blockly/blocks-http.js
@@ -30,14 +30,12 @@ export default function (f7, isGraalJs) {
         .appendField(imageTimeoutField, 'imgTimeout')
         .appendField(imageHeaderField, 'imgHeader')
         .appendField(imageQueryField, 'imgQuery')
-        .appendField('send', 'methodField')
         .appendField(new Blockly.FieldDropdown([
-          ['HttpGetRequest', 'HttpGetRequest'],
-          ['HttpPostRequest', 'HttpPostRequest'],
-          ['HttpPutRequest', 'HttpPutRequest'],
-          ['HttpDeleteRequest', 'HttpDeleteRequest']
+          ['send HTTP GET request to', 'HttpGetRequest'],
+          ['send HTTP POST request to', 'HttpPostRequest'],
+          ['send HTTP PUT request to', 'HttpPutRequest'],
+          ['send HTTP DELETE request to', 'HttpDeleteRequest']
         ], this.handleRequestTypeSelection.bind(this)), 'requestType')
-        .appendField('to')
 
       this.setInputsInline(false)
       this.setOutput(true, null)
