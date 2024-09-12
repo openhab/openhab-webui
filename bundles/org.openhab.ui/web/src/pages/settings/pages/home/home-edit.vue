openhab/openhab-webui

commit 80a172eab3558952a95a1c5d90fd2aec271af6b7

Home edit: Fix model card settings drop-down not accessible (#2304)
Fixes issue mentioned in
https://github.com/openhab/openhab-webui/pull/2144#issuecomment-1784114001.

---------

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

bundles/org.openhab.ui/web/src/pages/settings/pages/home/home-edit.vue
@@ -32,21 +32,20 @@
           <f7-preloader />
           <div>Loading...</div>
         </f7-block>
-        <!-- <f7-block class="block-narrow" v-if="ready && !previewMode">
-          <page-settings :page="page" :createMode="createMode" />
-        </f7-block> -->
 
-        <f7-block class="block-narrow" style="padding-bottom: 8rem" v-else-if="ready && modelReady && !previewMode">
-          <f7-col>
-            <f7-block-title>Page Configuration</f7-block-title>
-            <config-sheet
-              :parameterGroups="pageWidgetDefinition.props.parameterGroups || []"
-              :parameters="pageWidgetDefinition.props.parameters || []"
-              :configuration="page.config"
-              @updated="dirty = true" />
-          </f7-col>
+        <div v-else-if="!previewMode">
+          <f7-block class="block-narrow no-padding">
+            <f7-col>
+              <f7-block-title>Page Configuration</f7-block-title>
+              <config-sheet
+                :parameterGroups="pageWidgetDefinition.props.parameterGroups || []"
+                :parameters="pageWidgetDefinition.props.parameters || []"
+                :configuration="page.config"
+                @updated="dirty = true" />
+            </f7-col>
+          </f7-block>
 
-          <f7-col>
+          <f7-block>
             <f7-segmented strong tag="p">
               <f7-button v-for="tab in modelTabs" :key="tab.value" @click="showCardControls = false; currentModelTab = tab.value" :active="currentModelTab === tab.value" :text="tab.label" />
             </f7-segmented>
@@ -83,7 +82,9 @@
                 </f7-list-item>
               </f7-list>
             </div>
+          </f7-block>
 
+          <f7-block style="z-index: -1">
             <div v-if="currentModelTab === 'locations'">
               <config-sheet
                 :parameterGroups="locationsTabParameters.props.parameterGroups || []"
@@ -107,9 +108,10 @@
                 :configuration="page.slots.properties[0].config"
                 @updated="dirty = true" />
             </div>
-          </f7-col>
-        </f7-block>
-        <div v-else-if="ready && previewMode && currentTab === 'design'" :context="context" :key="pageKey">
+          </f7-block>
+        </div>
+
+        <div v-else :context="context" :key="pageKey">
           <model-tab style="margin-bottom: 4rem" :context="context" :type="currentModelTab" :model="model" :page="page" />
         </div>
       </f7-tab>

commit 460170e76ac055ba6df12464deb529a39e4bb7ca

Home edit: Fix model tab settings not editable
Fixes #2597.
Regression from #2304.

Also minimizes the gap between the Cards title and the reorder button.

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

bundles/org.openhab.ui/web/src/pages/settings/pages/home/home-edit.vue
@@ -49,12 +49,14 @@
             </f7-col>
           </f7-block>
 
-          <f7-block>
+          <f7-block class="no-margin-bottom">
             <f7-segmented strong tag="p">
               <f7-button v-for="tab in modelTabs" :key="tab.value" @click="showCardControls = false; currentModelTab = tab.value" :active="currentModelTab === tab.value" :text="tab.label" />
             </f7-segmented>
 
-            <f7-block-title>Cards</f7-block-title>
+            <f7-block-title class="no-margin-bottom">
+              Cards
+            </f7-block-title>
             <div>
               <div class="display-block padding">
                 <div class="no-padding float-right">
@@ -88,7 +90,7 @@
             </div>
           </f7-block>
 
-          <f7-block style="z-index: -1">
+          <f7-block class="homecards-settings">
             <div v-if="currentModelTab === 'locations'">
               <config-sheet
                 :parameterGroups="locationsTabParameters.props.parameterGroups || []"
@@ -144,9 +146,17 @@
     top 80%
     white-space pre-wrap
   .homecards-list
+    margin-bottom 0
+    padding-bottom calc(5rem + var(--f7-block-margin-vertical))
     .item-link
       overflow inherit
       z-index inherit !important
+  .homecards-settings
+    margin-top 0
+    .parameter-group
+      margin-top 0
+    .parameter-group-title
+      margin-top 0
 </style>
 
 <script>