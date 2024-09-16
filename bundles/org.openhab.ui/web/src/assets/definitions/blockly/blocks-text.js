1d1737a3efb7dcf2eea50f5b39fc3e29f18aa8fe
Mark Herwege <mark.herwege@telenet.be>
2024-09-16 08:50:33 +0200

    text
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

--- bundles/org.openhab.ui/web/src/assets/definitions/blockly/blocks-text.js
+++ bundles/org.openhab.ui/web/src/assets/definitions/blockly/blocks-text.js
@@ -6,7 +6,7 @@
 import Blockly from 'blockly'
 import { javascriptGenerator } from 'blockly/javascript.js'
 
-export default function (f7, isGraalJs) {
+export default function (f7) {
   /*
   * allows adding a CR/LF in string concatenation
   * Block
