91433867423c7de1e5882010bccc2553ab037b6d
Florian Hotze <florianh_dev@icloud.com>
2024-10-01 14:09:29 +0200

    oh-label-card: Use oh-card as base component
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- bundles/org.openhab.ui/web/src/components/widgets/standard/oh-card.vue
+++ bundles/org.openhab.ui/web/src/components/widgets/standard/oh-card.vue
@@ -5,9 +5,11 @@
         <div>{{ config.title }}</div>
       </f7-card-header>
     </slot>
-    <f7-card-content @click.native="performAction" :style="config.contentStyle" :class="[ ...(Array.isArray(contentClass) ? contentClass : ['padding']), ...(Array.isArray(config.contentClass) ? config.contentClass : []) ]">
-      <slot name="content" />
-    </f7-card-content>
+    <slot name="content-root">
+      <f7-card-content @click.native="performAction" :style="config.contentStyle" :class="[ ...(Array.isArray(contentClass) ? contentClass : ['padding']), ...(Array.isArray(config.contentClass) ? config.contentClass : []) ]">
+        <slot name="content" />
+      </f7-card-content>
+    </slot>
     <slot name="footer">
       <oh-card-footer v-if="config.footer" :texts="config.footer" :style="config.footerStyle" :class="config.footerClass" />
     </slot>
@@ -23,6 +25,6 @@ export default {
   mixins: [mixin, actionsMixin],
   components: { OhCardFooter },
   props: ['context', 'contentStyle', 'contentClass'],
-  slotProps: ['header', 'content', 'footer']
+  slotProps: ['header', 'content', 'content-root', 'footer']
 }
 </script>
