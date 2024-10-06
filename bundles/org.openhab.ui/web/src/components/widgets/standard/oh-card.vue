openhab/openhab-webui

commit 0767c91ffdedb63e591d06f4472f8cd41b5c636c

Fix safe area styling issues with card and card contents (#2801)
This fixes ugly styling issues with too large margin/padding on iOS
devices due to safe areas getting applied to children of f7-card, even
though f7-card already respects the safe areas.

-----

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

bundles/org.openhab.ui/web/src/components/widgets/standard/oh-card.vue
@@ -1,5 +1,5 @@
 <template>
-  <f7-card :no-border="config.noBorder" :no-shadow="config.noShadow" :outline="config.outline" :style="config.style" :class="config.class">
+  <f7-card :no-border="config.noBorder" :no-shadow="config.noShadow" :outline="config.outline" :style="config.style" :class="['oh-card', ...(Array.isArray(config.class) ? config.class : [])]">
     <slot name="header">
       <f7-card-header v-if="config.title" :style="config.headerStyle" :class="config.headerClass">
         <div>{{ config.title }}</div>
@@ -22,6 +22,17 @@
     </slot>
   </f7-card>
 </template>
+
+<style lang="stylus">
+// Fix safe-area issues where oh-card is used inside a block or masonry layout, where the safe areas are already respected by the parent block
+.oh-col >
+.oh-masonry-item >
+  .oh-card
+    --f7-safe-area-left 0px
+    --f7-safe-area-right 0px
+    --f7-safe-area-top 0px
+    --f7-safe-area-bottom 0px
+</style>
 
 <script>
 import mixin from '../widget-mixin'

bundles/org.openhab.ui/web/src/css/app.styl
@@ -309,3 +309,11 @@ html
       padding 0
       margin-left 8px
       color var(--f7-input-text-color)
+
+// Fix safe area issues inside f7-card components, where safe areas are already respected by the f7-card itself
+.card >
+  div
+    --f7-safe-area-left 0px
+    --f7-safe-area-right 0px
+    --f7-safe-area-top 0px
+    --f7-safe-area-bottom 0px

commit f2293756de68793ccaf247e4b9ffd80daeca6aa4

Fix style regressions from previous safe area style fixes
Regression from #2801.

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

bundles/org.openhab.ui/web/src/components/widgets/standard/oh-card.vue
@@ -28,10 +28,8 @@
 .oh-col >
 .oh-masonry-item >
   .oh-card
-    --f7-safe-area-left 0px
-    --f7-safe-area-right 0px
-    --f7-safe-area-top 0px
-    --f7-safe-area-bottom 0px
+    margin-left var(--f7-card-margin-horizontal)
+    margin-right var(--f7-card-margin-horizontal)
 </style>
 
 <script>

bundles/org.openhab.ui/web/src/css/app.styl
@@ -311,9 +311,12 @@ html
       color var(--f7-input-text-color)
 
 // Fix safe area issues inside f7-card components, where safe areas are already respected by the f7-card itself
-.card >
-  div
-    --f7-safe-area-left 0px
-    --f7-safe-area-right 0px
-    --f7-safe-area-top 0px
-    --f7-safe-area-bottom 0px
+.card
+  .block
+    padding-left var(--f7-block-padding-horizontal) !important
+    padding-right var(--f7-block-padding-horizontal) !important
+  .media-item
+    .item-content
+      padding-left var(--f7-list-item-padding-horizontal)
+      .item-inner
+        padding-right var(--f7-list-item-padding-horizontal)