openhab/openhab-webui

commit 521d67b15e663e8ad73279c6f1f3687b3516f28e

Rules list: Store search query when leaving page/reloading & Improve code (#2823)
As rules-list.vue is used by scenes and scripts list, this applies to them as well.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/pages/settings/rules/rules-list.vue
@@ -1,17 +1,17 @@
 <template>
-  <f7-page @page:afterin="onPageAfterIn" @page:afterout="stopEventSource">
+  <f7-page @page:afterin="onPageAfterIn" @page:afterout="onPageBeforeOut">
     <f7-navbar :title="type" back-link="Settings" back-link-url="/settings/" back-link-force>
       <f7-nav-right>
         <developer-dock-icon />
         <f7-link icon-md="material:done_all" @click="toggleCheck()"
                  :text="(!$theme.md) ? ((showCheckboxes) ? 'Done' : 'Select') : ''" />
       </f7-nav-right>
-      <f7-subnavbar :inner="false" v-show="initSearchbar">
+      <f7-subnavbar :inner="false" v-show="ready">
+        <!-- Only render searchbar, if page is ready. Otherwise searchbar is broken after changes to the rules list. -->
         <f7-searchbar
-          v-if="initSearchbar"
+          v-if="ready"
           ref="searchbar"
           class="searchbar-rules"
-          :init="initSearchbar"
           search-container=".rules-list"
           search-item=".rulelist-item"
           search-in=".item-title, .item-text, .item-after, .item-subtitle, .item-header, .item-footer"
@@ -52,11 +52,11 @@
       <f7-list-item title="Nothing found" />
     </f7-list>
 
+    <!-- no rule engine available -->
     <empty-state-placeholder v-if="noRuleEngine" icon="exclamationmark_triangle" title="rules.missingengine.title" text="rules.missingengine.text" />
-
-    <f7-block class="block-narrow" v-show="!noRuleEngine">
-      <!-- skeleton for not ready -->
-      <f7-col v-if="!ready">
+    <!-- rule engine available but not yet ready -->
+    <f7-block v-else-if="!noRuleEngine && !ready" class="block-narrow">
+      <f7-col v-show="!ready">
         <f7-block-title>&nbsp;Loading...</f7-block-title>
         <f7-list contacts-list class="col rules-list">
           <f7-list-group>
@@ -72,8 +72,20 @@
           </f7-list-group>
         </f7-list>
       </f7-col>
+    </f7-block>
+    <!-- rule engine available and ready, but no rules -->
+    <f7-block v-else-if="ready && !rules.length" class="block-narrow">
+      <empty-state-placeholder v-if="showScripts" icon="doc_plaintext" title="scripts.title" text="scripts.text" />
+      <empty-state-placeholder v-else-if="showScenes" icon="film" title="scenes.title" text="scenes.text" />
+      <empty-state-placeholder v-else icon="wand_stars" title="rules.title" text="rules.text" />
+      <f7-row v-if="$f7.width < 1280" class="display-flex justify-content-center">
+        <f7-button large fill color="blue" external :href="`${$store.state.websiteUrl}/link/${type.toLowerCase()}`" target="_blank" v-t="'home.overview.button.documentation'" />
+      </f7-row>
+    </f7-block>
 
-      <f7-col v-else-if="rules.length > 0">
+    <!-- rule engine available and ready and has rules -->
+    <f7-block class="block-narrow" v-show="!noRuleEngine && ready && rules.length > 0">
+      <f7-col>
         <f7-block-title class="searchbar-hide-on-search">
           {{ filteredRules.length }} {{ type.toLowerCase() }} {{ selectedTags.length > 0 ? ' - ' : '' }}
           <f7-link v-if="selectedTags.length > 0" @click="selectedTags = []">
@@ -131,15 +143,6 @@
       </f7-col>
     </f7-block>
 
-    <f7-block v-if="ready && !noRuleEngine && !rules.length" class="block-narrow">
-      <empty-state-placeholder v-if="showScripts" icon="doc_plaintext" title="scripts.title" text="scripts.text" />
-      <empty-state-placeholder v-else-if="showScenes" icon="film" title="scenes.title" text="scenes.text" />
-      <empty-state-placeholder v-else icon="wand_stars" title="rules.title" text="rules.text" />
-      <f7-row v-if="$f7.width < 1280" class="display-flex justify-content-center">
-        <f7-button large fill color="blue" external :href="`${$store.state.websiteUrl}/link/${type.toLowerCase()}`" target="_blank" v-t="'home.overview.button.documentation'" />
-      </f7-row>
-    </f7-block>
-
     <f7-fab v-show="ready && !showCheckboxes" position="right-bottom" slot="fixed" color="blue" href="add">
       <f7-icon ios="f7:plus" md="material:add" aurora="f7:plus" />
       <f7-icon ios="f7:close" md="material:close" aurora="f7:close" />
@@ -165,7 +168,6 @@ export default {
       ruleStatuses: {},
       uniqueTags: [],
       selectedTags: [],
-      initSearchbar: false,
       selectedItems: [],
       showCheckboxes: false,
       eventSource: null
@@ -203,9 +205,17 @@ export default {
     onPageAfterIn () {
       this.load()
     },
+    onPageBeforeOut () {
+      this.stopEventSource()
+      this.$f7.data[`last${this.type}SearchQuery`] = this.$refs.searchbar?.f7Searchbar.query
+    },
     load () {
       if (this.loading) return
       this.loading = true
+
+      if (this.ready) this.$f7.data[`last${this.type}SearchQuery`] = this.$refs.searchbar?.f7Searchbar.query
+      this.ready = false
+
       this.$set(this, 'selectedItems', [])
       this.showCheckboxes = false
       let filter = ''
@@ -243,14 +253,13 @@ export default {
 
         this.loading = false
         this.ready = true
-        setTimeout(() => {
-          this.initSearchbar = true
+
+        this.$nextTick(() => {
           if (this.$refs.listIndex) this.$refs.listIndex.update()
-          this.$nextTick(() => {
-            if (this.$device.desktop && this.$refs.searchbar) {
-              this.$refs.searchbar.f7Searchbar.$inputEl[0].focus()
-            }
-          })
+          if (this.$device.desktop && this.$refs.searchbar) {
+            this.$refs.searchbar.f7Searchbar.$inputEl[0].focus()
+          }
+          this.$refs.searchbar?.f7Searchbar.search(this.$f7.data[`last${this.type}SearchQuery`] || '')
         })
 
         if (!this.eventSource) this.startEventSource()
@@ -275,6 +284,7 @@ export default {
             // skip status updates for RUNNING for performance reasons (can be easily skipped as it was never really shown due to the short execution time of rules)
             if (newStatus.status === 'RUNNING') return
 
+            if (!this.ruleStatuses[uid]) this.ruleStatuses[uid] = {}
             this.ruleStatuses[uid].status = newStatus.status
             this.ruleStatuses[uid].statusDetail = newStatus.statusDetail
         }

commit f574b799d1bf4e2d5f361ce1d60a4e08faac6e28

Fix flickering of list pages when updating
Partly regression from #2823 and #2827.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/pages/settings/items/items-list-vlist.vue
@@ -6,10 +6,10 @@
         <f7-link icon-md="material:done_all" @click="toggleCheck()"
                  :text="(!$theme.md) ? ((showCheckboxes) ? 'Done' : 'Select') : ''" />
       </f7-nav-right>
-      <f7-subnavbar :inner="false" v-show="ready">
+      <f7-subnavbar :inner="false" v-show="initSearchbar">
         <!-- Only render searchbar, if page is ready. Otherwise searchbar is broken after changes to the Items list. -->
         <f7-searchbar
-          v-if="ready"
+          v-if="initSearchbar"
           ref="searchbar"
           class="searchbar-items"
           search-container=".virtual-list"
@@ -145,6 +145,7 @@ export default {
   data () {
     return {
       ready: false,
+      initSearchbar: false,
       loading: false,
       items: [], // [{ label: 'Staircase', name: 'Staircase'}],
       vlData: {
@@ -173,8 +174,8 @@ export default {
       if (this.loading) return
       this.loading = true
 
-      if (this.ready) this.$f7.data.lastItemSearchQuery = this.$refs.searchbar?.f7Searchbar.query
-      this.ready = false
+      if (this.initSearchbar) this.$f7.data.lastItemSearchQuery = this.$refs.searchbar?.f7Searchbar.query
+      this.initSearchbar = false
 
       this.$oh.api.get('/rest/items?metadata=semantics').then(data => {
         this.items = data.sort((a, b) => {
@@ -183,7 +184,11 @@ export default {
           return labelA.localeCompare(labelB)
         })
         this.$refs.itemsList.f7VirtualList.replaceAllItems(this.items)
+        this.initSearchbar = true
+        this.loading = false
+
         if (!this.eventSource) this.startEventSource()
+        this.ready = true
 
         this.$nextTick(() => {
           if (this.$device.desktop) {
@@ -191,9 +196,6 @@ export default {
           }
           this.$refs.searchbar?.f7Searchbar.search(this.$f7.data.lastItemSearchQuery || '')
         })
-
-        this.loading = false
-        this.ready = true
       })
     },
     startEventSource () {

bundles/org.openhab.ui/web/src/pages/settings/pages/pages-list.vue
@@ -6,9 +6,9 @@
         <f7-link icon-md="material:done_all" @click="toggleCheck()"
                  :text="(!$theme.md) ? ((showCheckboxes) ? 'Done' : 'Select') : ''" />
       </f7-nav-right>
-      <f7-subnavbar :inner="false" v-show="ready">
+      <f7-subnavbar :inner="false" v-show="initSearchbar">
         <f7-searchbar
-          v-if="ready"
+          v-if="initSearchbar"
           ref="searchbar"
           class="searchbar-pages"
           search-container=".pages-list"
@@ -158,6 +158,7 @@ export default {
   data () {
     return {
       ready: false,
+      initSearchbar: false,
       loading: false,
       pages: [],
       selectedItems: [],
@@ -218,8 +219,8 @@ export default {
       if (this.loading) return
       this.loading = true
 
-      if (this.ready) this.$f7.data.lastPagesSearchQuery = this.$refs.searchbar?.f7Searchbar.query
-      this.ready = false
+      if (this.initSearchbar) this.$f7.data.lastPagesSearchQuery = this.$refs.searchbar?.f7Searchbar.query
+      this.initSearchbar = false
 
       this.$set(this, 'selectedItems', [])
       this.showCheckboxes = false
@@ -232,6 +233,7 @@ export default {
         this.pages = pagesAndSitemaps.sort((a, b) => {
           return a.config.label.localeCompare(b.config.label)
         })
+        this.initSearchbar = true
 
         this.loading = false
         this.ready = true

bundles/org.openhab.ui/web/src/pages/settings/rules/rules-list.vue
@@ -6,10 +6,10 @@
         <f7-link icon-md="material:done_all" @click="toggleCheck()"
                  :text="(!$theme.md) ? ((showCheckboxes) ? 'Done' : 'Select') : ''" />
       </f7-nav-right>
-      <f7-subnavbar :inner="false" v-show="ready">
+      <f7-subnavbar :inner="false" v-show="initSearchbar">
         <!-- Only render searchbar, if page is ready. Otherwise searchbar is broken after changes to the rules list. -->
         <f7-searchbar
-          v-if="ready"
+          v-if="initSearchbar"
           ref="searchbar"
           class="searchbar-rules"
           search-container=".rules-list"
@@ -162,6 +162,7 @@ export default {
   data () {
     return {
       ready: false,
+      initSearchbar: false,
       loading: false,
       noRuleEngine: false,
       rules: [],
@@ -213,8 +214,8 @@ export default {
       if (this.loading) return
       this.loading = true
 
-      if (this.ready) this.$f7.data[`last${this.type}SearchQuery`] = this.$refs.searchbar?.f7Searchbar.query
-      this.ready = false
+      if (this.initSearchbar) this.$f7.data[`last${this.type}SearchQuery`] = this.$refs.searchbar?.f7Searchbar.query
+      this.initSearchbar = false
 
       this.$set(this, 'selectedItems', [])
       this.showCheckboxes = false
@@ -250,6 +251,7 @@ export default {
         })
 
         this.uniqueTags.sort()
+        this.initSearchbar = true
 
         this.loading = false
         this.ready = true

bundles/org.openhab.ui/web/src/pages/settings/schedule/schedule.vue
@@ -6,9 +6,9 @@
         <f7-link icon-md="material:done_all" @click="toggleCheck()"
                  :text="(!$theme.md) ? ((showCheckboxes) ? 'Done' : 'Select') : ''" />
       </f7-nav-right>
-      <f7-subnavbar :inner="false" v-show="ready">
+      <f7-subnavbar :inner="false" v-show="initSearchbar">
         <f7-searchbar
-          v-if="ready"
+          v-if="initSearchbar"
           ref="searchbar"
           class="searchbar-schedule"
           search-container=".timeline"
@@ -88,6 +88,7 @@ export default {
   data () {
     return {
       ready: false,
+      initSearchbar: false,
       loading: false,
       rules: [],
       noRuleEngine: false,
@@ -112,8 +113,8 @@ export default {
       if (this.loading) return
       this.loading = true
 
-      if (this.ready) this.$f7.data.lastScheduleSearchQuery = this.$refs.searchbar?.f7Searchbar.query
-      this.ready = false
+      if (this.initSearchbar) this.$f7.data.lastScheduleSearchQuery = this.$refs.searchbar?.f7Searchbar.query
+      this.initSearchbar = false
 
       let occurrences = []
 
@@ -123,6 +124,7 @@ export default {
       this.$oh.api.get('/rest/rules/schedule/simulations?from=' + start.toISOString() + '&until=' + limit.toISOString()).then(data => {
         this.rules = data
         this.loading = false
+        this.initSearchbar = true
 
         // map RulesExecutions per time
         this.rules.forEach((rule) => {

bundles/org.openhab.ui/web/src/pages/settings/things/things-list.vue
@@ -6,9 +6,9 @@
         <f7-link icon-md="material:done_all" @click="toggleCheck()"
                  :text="(!$theme.md) ? ((showCheckboxes) ? 'Done' : 'Select') : ''" />
       </f7-nav-right>
-      <f7-subnavbar :inner="false" v-show="ready">
+      <f7-subnavbar :inner="false" v-show="initSeachbar">
         <f7-searchbar
-          v-if="ready"
+          v-if="initSeachbar"
           ref="searchbar"
           class="searchbar-things"
           search-container=".contacts-list"
@@ -170,6 +170,7 @@ export default {
   data () {
     return {
       ready: false,
+      initSeachbar: false,
       loading: false,
       things: [],
       inbox: [],
@@ -252,11 +253,12 @@ export default {
       if (this.loading) return
       this.loading = true
 
-      if (this.ready) this.$f7.data.lastThingsSearchQuery = this.$refs.searchbar?.f7Searchbar.query
-      this.ready = false
+      if (this.initSeachbar) this.$f7.data.lastThingsSearchQuery = this.$refs.searchbar?.f7Searchbar.query
+      this.initSeachbar = false
 
       this.$oh.api.get('/rest/things?summary=true').then((data) => {
         this.things = data.sort((a, b) => (a.label || a.UID).localeCompare(b.label || a.UID))
+        this.initSeachbar = true
         this.loading = false
         this.ready = true
         this.$nextTick(() => {