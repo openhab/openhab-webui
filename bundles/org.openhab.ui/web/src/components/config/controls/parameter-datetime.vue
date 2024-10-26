09a6a1a0f0002297d7c6aca11200c4a0d4175c9e
Florian Hotze <dev@florianhotze.com>
2024-10-26 15:07:35 +0200

    Config parameter: Implement `date` and `datetime` contexts
    Closes #2816.
    Depends on https://github.com/openhab/openhab-core/pull/4428.
    
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

---
+++ bundles/org.openhab.ui/web/src/components/config/controls/parameter-datetime.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <f7-list-input
+      ref="input"
+      type="datetime-local"
+      :floating-label="$theme.md"
+      :label="configDescription.label"
+      :name="configDescription.name"
+      :value="value"
+      :required="configDescription.required" validate
+      @input="updateValue" />
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['configDescription', 'value'],
+  methods: {
+    updateValue (event) {
+      this.$emit('input', event.target.value)
+    }
+  }
+}
+</script>
