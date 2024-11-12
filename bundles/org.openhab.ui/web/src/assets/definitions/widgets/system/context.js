f605b2e539268983335ff21b2ec59ad6037d3bb4
Siegmar Immel <36704892+SiggiFR@users.noreply.github.com>
2024-11-12 18:01:36 +0100

    Update context.js
    Added info about changeability of elements (provided by Justin) to the parameter description.
    
    Signed-off-by: Siegmar Immel <36704892+SiggiFR@users.noreply.github.com>

--- bundles/org.openhab.ui/web/src/assets/definitions/widgets/system/context.js
+++ bundles/org.openhab.ui/web/src/assets/definitions/widgets/system/context.js
@@ -1,7 +1,7 @@
 import { pt, pb, pi, pn } from '../helpers.js'
 
 export default () => [
-  pt('functions', 'Widget Functions', 'Object with key:arrow-function pairs. Functions are available to expressions in all child components via the <code>fn</code> object.'),
-  pt('constants', 'Widget Constants', 'Object with key:constant pairs. Constants are available to expressions in all child components via the <code>const</code> object.'),
-  pt('variables', 'Widget Variables', 'Object with key:variable default value pairs. Variables are available to expressions in all child components via the <code>vars</code> object and take precedence over variables with the same name from higher contexts.')
+  pt('functions', 'Widget Functions', 'Object with key:arrow-function pairs. Functions are available to expressions in all child components via the <code>fn</code> object.<br />Functions are evaluated on every call and re-evaluated when dependent values change.'),
+  pt('constants', 'Widget Constants', 'Object with key:constant pairs. Constants are available to expressions in all child components via the <code>const</code> object.<br />Constants are evaluated only on widget build and cannot be changed.'),
+  pt('variables', 'Widget Variables', 'Object with key:variable default value pairs. Variables are available to expressions in all child components via the <code>vars</code> object and take precedence over variables with the same name from higher contexts.<br />Variables are evaluated on widget build. Their values can only be changed by other component variable actions (e.g. <a class="external text-color-blue" target="_blank" href="https://www.openhab.org/docs/ui/components/oh-button.html#action-variable">oh-button</a>)')
 ]
