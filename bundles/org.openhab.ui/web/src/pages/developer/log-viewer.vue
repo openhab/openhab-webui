6a2df5793355ab92d59a0fba1c2f6d6d3c9cbef6
Yannick Schaus <github@schaus.net>
2024-12-05 21:49:03 +0100

    fix linting
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- bundles/org.openhab.ui/web/src/pages/developer/log-viewer.vue
+++ bundles/org.openhab.ui/web/src/pages/developer/log-viewer.vue
@@ -20,7 +20,7 @@
             <f7-block class="input-with-buttons-container">
               <div class="input-with-buttons searchbar">
                 <input type="text" placeholder="Add custom logger package entry..."
-                       @keyup.enter="handleLogPackageEnter($event)" class="custom-input"></input>
+                       @keyup.enter="handleLogPackageEnter($event)" class="custom-input" />
               </div>
             </f7-block>
             <f7-block style="margin-top: 4px; font-size: 0.85rem; text-align: center;">
@@ -81,7 +81,7 @@
           <div class="page-content">
             <f7-list class="col wide">
               <f7-list-item v-for="(highlightFilter, index) in highlightFilters" :key="index">
-                <input slot="media" type="checkbox" v-model="highlightFilter.active" checked></input>
+                <input slot="media" type="checkbox" v-model="highlightFilter.active" checked />
                 <f7-input slot="title" type="text" placeholder="Enter text to highlight..."
                           :value="highlightFilter.text" @input="updateHighlightText($event, index)" />
 
@@ -114,7 +114,6 @@
     <!-- Main Display -->
     <f7-navbar title="Log Viewer" back-link="Developer Tools" back-link-url="/developer/" back-link-force>
       <f7-nav-right>
-
         <f7-link icon-ios="f7:play_fill" icon-f7="play_fill" icon-md="material:play_arrow" class="margin-left"
                  tooltip="Continue receiving logs" :disabled="stateConnected && stateProcessing"
                  :class="{ 'disabled-link': stateConnected && stateProcessing }" @click="loggingContinue" />
@@ -136,7 +135,7 @@
             {{ logStart }}&nbsp;>&nbsp;{{ logEnd }}
           </f7-badge>
           <f7-badge class="margin-horizontal" :color="countersBadgeColor" tooltip="Log entries filtered/total">
-            {{filterCount}}/{{ tableData.length }}
+            {{ filterCount }}/{{ tableData.length }}
           </f7-badge>
           <!-- <f7-badge color="red" tooltip="Maximum log entries to be buffered">
             {{ maxEntries }}
@@ -149,21 +148,21 @@
       <!-- <f7-link icon-f7="arrow_down_to_line" tooltip="Scroll to latest log entries" :disabled="autoScroll"
                  :class="{ 'disabled-link': autoScroll }" @click="showLatestLogs" /> -->
       <f7-link icon-f7="cloud_download"
-                tooltip="Download filtered log as CSV" :disabled="filterCount == 0"
-                :class="{ 'disabled-link': filterCount == 0 }" @click="downloadCSV" />
+               tooltip="Download filtered log as CSV" :disabled="filterCount == 0"
+               :class="{ 'disabled-link': filterCount == 0 }" @click="downloadCSV" />
       <f7-link icon-f7="rectangle_on_rectangle" tooltip="Copy filtered log to clipboard"
-                :disabled="filterCount == 0" :class="{ 'disabled-link': filterCount == 0 }" @click="copyTableToClipboard" />
+               :disabled="filterCount == 0" :class="{ 'disabled-link': filterCount == 0 }" @click="copyTableToClipboard" />
       <f7-link icon-f7="trash" tooltip="Clear the log buffer"
-                :disabled="tableData.length == 0" :class="{ 'disabled-link': tableData.length == 0 }"
-                @click="clearLog" />
+               :disabled="tableData.length == 0" :class="{ 'disabled-link': tableData.length == 0 }"
+               @click="clearLog" />
       <f7-link @click="toggleErrorDisplay" tooltip="Always show error level logs">
         <f7-icon v-if="showErrors" f7="exclamationmark_triangle_fill" />
         <f7-icon v-else f7="exclamationmark_triangle" />
       </f7-link>
       <f7-link icon-f7="pencil" tooltip="Configure highlights"
-                data-popup=".loghighlights-popup" class="popup-open" />
+               data-popup=".loghighlights-popup" class="popup-open" />
       <f7-link icon-f7="gear" tooltip="Configure logging"
-                data-popup=".logsettings-popup" class="popup-open" />
+               data-popup=".logsettings-popup" class="popup-open" />
     </f7-toolbar>
 
     <f7-block class="no-padding no-margin">
@@ -473,7 +472,7 @@ export default {
     },
     keepAlive () {
       if (this.socket && this.stateConnected) {
-        // this.socket.send('ping') shows a warning in the logs
+        this.socket.send('ping')
         this.keepAliveTimer = setTimeout(this.keepAlive, 9000)
       } else {
         if (this.keepAliveTimer) {
