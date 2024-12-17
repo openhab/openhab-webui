openhab/openhab-webui

commit 66787288e0dcc12793d2c5d1bae5210a219308c7

[blockly] Remove Nashorn support & Check for required automation add-on in script edit (#2743)
This PR removes Nashorn support from Blockly and removes all Nashorn 
specific code from the blocks.

When opening a Blockly script, the UI will tell you if the script needs
to be saved again (because the MIME type is for Nashorn or because newly
generated code is different from the saved code).
It will also warn if the JS Scripting addon is not installed.

The Run Now link from the script editor is grayed out when the required
scripting add-on is not installed, and will show a warning when trying to run.

-----

Also-by: Florian Hotze <florianh_dev@icloud.com>
Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

bundles/org.openhab.ui/web/src/components/rule/module-wizard-mixin.js
@@ -45,6 +45,9 @@ export default {
       }
 
       return ['ON', 'OFF'].map((c) => { return { value: c, label: c } })
+    },
+    isJsAvailable () {
+      return this.isMimeTypeAvailable('application/javascript')
     }
   },
   methods: {
@@ -69,6 +72,9 @@ export default {
       this.$f7.once('modelPickerClosed', () => {
         this.$f7.off('itemsPicked', this.itemPicked)
       })
+    },
+    isMimeTypeAvailable (mimeType) {
+      return this.languages.map(l => l.contentType).includes(mimeType)
     }
   }
 }

bundles/org.openhab.ui/web/src/pages/settings/rules/script/script-edit.vue
@@ -25,9 +25,22 @@
     <template v-if="ready">
       <f7-toolbar v-if="!createMode" position="bottom">
         <span class="display-flex flex-direction-row align-items-center">
-          <f7-link :icon-color="(rule.status.statusDetail === 'DISABLED') ? 'orange' : 'gray'" :tooltip="((rule.status.statusDetail === 'DISABLED') ? 'Enable' : 'Disable') + (($device.desktop) ? ' (Ctrl-D)' : '')" icon-ios="f7:pause_circle" icon-md="f7:pause_circle" icon-aurora="f7:pause_circle" color="orange" @click="toggleDisabled" />
-          <f7-link v-if="!$theme.aurora" :tooltip="'Run Now' + (($device.desktop) ? ' (Ctrl-R)' : '')" icon-ios="f7:play_round" icon-md="f7:play_round" icon-aurora="f7:play_round" :color="(rule.status.status === 'IDLE') ? 'blue' : 'gray'" @click="runNow" />
-          <f7-link v-else class="margin-left" :text="($device.desktop) ? 'Run Now (Ctrl-R)' : ''" icon-ios="f7:play_round" icon-md="f7:play_round" icon-aurora="f7:play_round" :color="(rule.status.status === 'IDLE') ? 'blue' : 'gray'" @click="runNow" />
+          <f7-link :icon-color="(rule.status.statusDetail === 'DISABLED') ? 'orange' : 'gray'"
+                   :tooltip="((rule.status.statusDetail === 'DISABLED') ? 'Enable' : 'Disable') + (($device.desktop) ? ' (Ctrl-D)' : '')"
+                   icon-ios="f7:pause_circle" icon-md="f7:pause_circle" icon-aurora="f7:pause_circle"
+                   color="orange"
+                   @click="toggleDisabled" />
+          <f7-link v-if="!$theme.aurora"
+                   :tooltip="isMimeTypeAvailable(mode) ? ('Run Now' + (($device.desktop) ? ' (Ctrl-R)' : '')) : (isScriptRule ? 'Script' : 'Rule') + ' cannot be run, scripting addon for ' + mimeTypeDescription(mode) + ' is not installed'"
+                   icon-ios="f7:play_round" icon-md="f7:play_round" icon-aurora="f7:play_round"
+                   :color="((rule.status.status === 'IDLE') && isMimeTypeAvailable(mode)) ? 'blue' : 'gray'"
+                   @click="runNow" />
+          <f7-link v-else class="margin-left"
+                   :text="'Run Now' + (($device.desktop) ? ' (Ctrl-R)' : '')"
+                   :tooltip="!isMimeTypeAvailable(mode) ? (isScriptRule ? 'Script' : 'Rule') + ' cannot be run, scripting addon for ' + mimeTypeDescription(mode) + ' is not installed' : undefined"
+                   icon-ios="f7:play_round" icon-md="f7:play_round" icon-aurora="f7:play_round"
+                   :color="(rule.status.status === 'IDLE') && isMimeTypeAvailable(mode) ? 'blue' : 'gray'"
+                   @click="runNow" />
           <f7-chip class="margin-left" v-if="currentModule && currentModule.configuration.script"
                    :text="ruleStatusBadgeText(rule.status)"
                    :color="ruleStatusBadgeColor(rule.status)"
@@ -61,7 +74,7 @@
       <f7-icon v-if="!createMode && (!isBlockly && !editable) || (blocklyCodePreview && isBlockly)" f7="lock" class="float-right margin" style="opacity:0.5; z-index: 4000; user-select: none;" size="50" color="gray"
                :tooltip="(isBlockly) ? 'Cannot edit the code generated by Blockly' : 'This rule is not editable because it has been provisioned from a file'" />
       <editor v-if="!createMode && (!isBlockly || blocklyCodePreview)" class="rule-script-editor" :mode="mode" :value="script" @input="onEditorInput" :read-only="isBlockly || !editable" :tern-autocompletion-hook="true" />
-      <blockly-editor ref="blocklyEditor" v-else-if="!createMode && isBlockly" :blocks="currentModule.configuration.blockSource" :isGraalJs="mode === GRAALJS_MIME_TYPE" @change="scriptDirty = true" @mounted="onBlocklyMounted" />
+      <blockly-editor ref="blocklyEditor" v-else-if="!createMode && isBlockly" :blocks="currentModule.configuration.blockSource" @change="scriptDirty = true" @mounted="onBlocklyMounted" @ready="onBlocklyReady" />
       <script-general-settings v-else-if="createMode" :createMode="true" :rule="rule" />
       <f7-block class="block-narrow" v-if="createMode">
         <f7-col>
@@ -71,7 +84,8 @@
           <f7-list media-list>
             <f7-list-item media-item radio radio-icon="start"
                           title="Design with Blockly"
-                          footer="A beginner-friendly way to build scripts visually by assembling blocks"
+                          text="A beginner-friendly way to build scripts visually by assembling blocks"
+                          :footer="!isJsAvailable ? 'You need to install the JavaScript Scripting addon before you will be able to run' : undefined"
                           :value="'application/javascript+blockly'" :checked="mode === 'application/javascript+blockly'"
                           @change="mode = 'application/javascript+blockly'">
               <img src="@/images/blockly.svg" height="32" width="32" slot="media">
@@ -142,6 +156,7 @@ import RuleStatus from '@/components/rule/rule-status-mixin'
 import ScriptGeneralSettings from './script-general-settings.vue'
 import ModuleDescriptionSuggestions from '../module-description-suggestions'
 import DirtyMixin from '../../dirty-mixin'
+import { AddonNames } from '@/assets/definitions/automation-languages'
 
 export default {
   mixins: [RuleStatus, ModuleDescriptionSuggestions, DirtyMixin],
@@ -215,6 +230,9 @@ export default {
     },
     isBlockly () {
       return this.currentModule && this.currentModule.configuration && this.currentModule.configuration.blockSource
+    },
+    isJsAvailable () {
+      return this.isMimeTypeAvailable(this.GRAALJS_MIME_TYPE)
     }
   },
   watch: {
@@ -349,11 +367,7 @@ export default {
         }
       }
       if (this.mode === 'application/javascript+blockly') {
-        if (this.isMimeTypeAvailable(this.GRAALJS_MIME_TYPE)) {
-          actionModule.configuration.type = this.GRAALJS_MIME_TYPE
-        } else {
-          actionModule.configuration.type = this.NASHORNJS_MIME_TYPE
-        }
+        actionModule.configuration.type = this.GRAALJS_MIME_TYPE
         actionModule.configuration.blockSource = '<xml xmlns="https://developers.google.com/blockly/xml"></xml>'
       }
       this.rule.actions.push(actionModule)
@@ -371,6 +385,9 @@ export default {
     isMimeTypeAvailable (mimeType) {
       return this.languages.map(l => l.contentType).includes(mimeType)
     },
+    mimeTypeDescription (mode) {
+      return AddonNames[mode] || mode
+    },
     /**
      * Load the script module type, i.e. the available script languages
      * @returns {Promise}
@@ -387,7 +404,7 @@ export default {
               version: l.label.split(' (')[1].replace(')', '')
             }
           })
-        if (this.isBlockly) languages = languages.filter((l) => l.contentType.startsWith('application/javascript'))
+        if (this.isBlockly) languages = languages.filter((l) => l.contentType === this.GRAALJS_MIME_TYPE)
         this.$set(this, 'languages', languages)
         return Promise.resolve()
       })
@@ -508,6 +525,13 @@ export default {
     },
     runNow () {
       if (this.createMode) return
+      if (!this.isMimeTypeAvailable(this.mode)) {
+        return this.$f7.toast.create({
+          text: `${this.isScriptRule ? 'Script' : 'Rule'} cannot be run, scripting addon for ${this.mimeTypeDescription(this.mode)} is not installed`,
+          destroyOnClose: true,
+          closeTimeout: 2000
+        }).open()
+      }
       if (this.rule.status.status === 'RUNNING' || this.rule.status.status === 'UNINITIALIZED') {
         return this.$f7.toast.create({
           text: `${this.isScriptRule ? 'Script' : 'Rule'} cannot be run ${(this.rule.status.status === 'RUNNING') ? 'while already running, please wait' : 'if it is uninitialized'}!`,
@@ -566,6 +590,36 @@ export default {
         this.$store.commit('setPagePath', this.$store.state.pagePath + '?blockly')
       }
     },
+    onBlocklyReady () {
+      if (!this.isBlockly) return
+      let message = ''
+
+      // Make sure the MIME type is set correctly for Blockly rules and the saved script is up to date
+      if (this.script) {
+        let oldRule = false
+        if (this.mode !== this.GRAALJS_MIME_TYPE) {
+          this.mode = this.GRAALJS_MIME_TYPE
+          oldRule = true
+        } else {
+          // Get the new code, and if it is different from stored code, it was created with an older version
+          try {
+            const newScript = this.$refs.blocklyEditor.getCode()
+            if (newScript && (this.script !== newScript)) {
+              this.scriptDirty = true
+              oldRule = true
+            }
+          } catch (e) {
+            this.$f7.dialog.alert(e)
+          }
+        }
+        if (oldRule) message += 'Your Blockly script was created with a previous version of openHAB. Please save your script'
+      }
+
+      // Check if JS Scripting is installed
+      if (!this.isJsAvailable) message += (message ? ' and' : 'You do not have JS Scripting installed. Please') + ' install the JS Scripting addon'
+
+      if (message) this.$f7.dialog.alert(message + '.')
+    },
     setBlocklyRenderer (newRenderer) {
       this.blocklyRenderer = newRenderer
       this.$refs.blocklyEditor.changeRenderer(this.blocklyRenderer)
@@ -587,7 +641,7 @@ export default {
       }
     },
     convertToBlockly () {
-      if (this.script || this.isBlockly || this.mode !== 'application/javascript') return
+      if (this.script || this.isBlockly || this.mode !== this.GRAALJS_MIME_TYPE) return
       this.$set(this.currentModule.configuration, 'blockSource', '<xml xmlns="https://developers.google.com/blockly/xml"></xml>')
     },
     onEditorInput (value) {
@@ -644,7 +698,6 @@ export default {
   },
   created () {
     this.GRAALJS_MIME_TYPE = 'application/javascript'
-    this.NASHORNJS_MIME_TYPE = 'application/javascript;version=ECMAScript-5.1'
   }
 }
 </script>

bundles/org.openhab.ui/web/src/assets/definitions/automation-languages.js
@@ -1,0 +1,7 @@
+export const AddonNames = {
+  'application/javascript': 'JavaScript Scripting',
+  'application/javascript;version=ECMAScript-5.1': 'JavaScript Scripting (Nashorn)',
+  'application/x-groovy': 'Groovy Scripting',
+  'application/x-python': 'Jython Scripting',
+  'application/x-ruby': 'JRuby Scripting'
+}

bundles/org.openhab.ui/web/src/assets/definitions/blockly/blocks-audio.js
@@ -14,7 +14,7 @@ import Blockly from 'blockly'
 import { javascriptGenerator } from 'blockly/javascript.js'
 import { FieldSlider } from '@blockly/field-slider'
 
-export default function (f7, isGraalJs, sinks, voices) {
+export default function (f7, sinks, voices) {
   Blockly.Blocks['oh_volumeslider'] = {
     init: function () {
       this.appendDummyInput()
@@ -60,8 +60,7 @@ export default function (f7, isGraalJs, sinks, voices) {
     let fileName = javascriptGenerator.valueToCode(block, 'fileName', javascriptGenerator.ORDER_ATOMIC)
     let sinkName = javascriptGenerator.valueToCode(block, 'sinkName', javascriptGenerator.ORDER_ATOMIC).replace('(', '').replace(/[()]/g, '')
 
-    const audio = (isGraalJs) ? 'actions.Audio' : addAudio()
-    return `${audio}.playSound(${sinkName}, ${fileName});\n`
+    return `actions.Audio.playSound(${sinkName}, ${fileName});\n`
   }
 
   /*
@@ -98,12 +97,7 @@ export default function (f7, isGraalJs, sinks, voices) {
     let sinkName = javascriptGenerator.valueToCode(block, 'sinkName', javascriptGenerator.ORDER_ATOMIC).replace('(', '').replace(/[()]/g, '')
     let volume = javascriptGenerator.valueToCode(block, 'volume', javascriptGenerator.ORDER_ATOMIC).replace(/'/g, '')
 
-    if (isGraalJs) {
-      return `actions.Audio.playSound(${sinkName}, ${fileName}, (${volume}/100));\n`
-    } else {
-      const audio = addAudio()
-      return `${audio}.playSound(${sinkName}, ${fileName}, new PercentType(${volume}));\n`
-    }
+    return `actions.Audio.playSound(${sinkName}, ${fileName}, (${volume}/100));\n`
   }
 
   /*
@@ -135,8 +129,7 @@ export default function (f7, isGraalJs, sinks, voices) {
     let url = javascriptGenerator.valueToCode(block, 'url', javascriptGenerator.ORDER_ATOMIC)
     let sinkName = javascriptGenerator.valueToCode(block, 'sinkName', javascriptGenerator.ORDER_ATOMIC).replace('(', '').replace(/[()]/g, '')
 
-    const audio = (isGraalJs) ? 'actions.Audio' : addAudio()
-    return `${audio}.playStream(${sinkName}, ${url});\n`
+    return `actions.Audio.playStream(${sinkName}, ${url});\n`
   }
 
   /*
@@ -165,8 +158,7 @@ export default function (f7, isGraalJs, sinks, voices) {
     let url = block.getFieldValue('url')
     let sinkName = javascriptGenerator.valueToCode(block, 'sinkName', javascriptGenerator.ORDER_ATOMIC).replace('(', '').replace(/[()]/g, '')
 
-    const audio = (isGraalJs) ? 'actions.Audio' : addAudio()
-    return `${audio}.playStream(${sinkName}, null);\n`
+    return `actions.Audio.playStream(${sinkName}, null);\n`
   }
 
   /*
@@ -201,8 +193,7 @@ export default function (f7, isGraalJs, sinks, voices) {
     const voiceName = javascriptGenerator.valueToCode(block, 'voice', javascriptGenerator.ORDER_ATOMIC).replace('(', '').replace(/[()]/g, '')
     const deviceSink = javascriptGenerator.valueToCode(block, 'deviceSink', javascriptGenerator.ORDER_ATOMIC).replace('(', '').replace(/[()]/g, '')
 
-    const voice = (isGraalJs) ? 'actions.Voice' : addVoice()
-    return `${voice}.say(${textToSay}, ${voiceName}, ${deviceSink});\n`
+    return `actions.Voice.say(${textToSay}, ${voiceName}, ${deviceSink});\n`
   }
 
   /*
@@ -262,17 +253,5 @@ export default function (f7, isGraalJs, sinks, voices) {
   javascriptGenerator.forBlock['oh_voices_dropdown'] = function (block) {
     let voiceName = block.getFieldValue('voiceName')
     return [`'${voiceName}'`, javascriptGenerator.ORDER_NONE]
-  }
-
-  function addAudio () {
-    return javascriptGenerator.provideFunction_(
-      'audio',
-      ['var ' + javascriptGenerator.FUNCTION_NAME_PLACEHOLDER_ + ' = Java.type(\'org.openhab.core.model.script.actions.Audio\');'])
-  }
-
-  function addVoice () {
-    return javascriptGenerator.provideFunction_(
-      'voice',
-      ['var ' + javascriptGenerator.FUNCTION_NAME_PLACEHOLDER_ + ' = Java.type(\'org.openhab.core.model.script.actions.Voice\');'])
   }
 }

bundles/org.openhab.ui/web/src/assets/definitions/blockly/blocks-color.js
@@ -7,7 +7,7 @@ import Blockly from 'blockly'
 import { javascriptGenerator } from 'blockly/javascript.js'
 import { blockGetCheckedInputType } from '@/assets/definitions/blockly/utils.js'
 
-export default function (f7, isGraalJs) {
+export default function (f7) {
   /*
   * converts a hex color string in to an openHAB hue-saturation-brightness string
   * Block
@@ -69,12 +69,7 @@ export default function (f7, isGraalJs) {
     const inputType = blockGetCheckedInputType(block, 'item')
     let attributeName = block.getFieldValue('attributeName')
 
-    let code = ''
-    if (isGraalJs) {
-      code = (inputType === 'oh_item') ? `items.getItem(${theItem}).rawState.get${attributeName}()` : `${theItem}.rawState.get${attributeName}()`
-    } else {
-      code = (inputType === 'oh_item') ? `itemRegistry.getItem(${theItem}).getRawState().get${attributeName}()` : `${theItem}.getRawState().get${attributeName}()`
-    }
+    const code = (inputType === 'oh_item') ? `items.getItem(${theItem}).rawState.get${attributeName}()` : `${theItem}.rawState.get${attributeName}()`
     return [code, 0]
   }
 

bundles/org.openhab.ui/web/src/assets/definitions/blockly/blocks-dateoffsets.js
@@ -4,9 +4,9 @@
 import Blockly from 'blockly'
 import { javascriptGenerator } from 'blockly/javascript.js'
 import { FieldDatePicker } from './fields/date-field.js'
-import { addDateSupport, addDateComparisonSupportNashorn, addDateComparisonSupportGraalVM, addGetZdtComponent, addChrono } from './utils.js'
+import { addDateComparisonSupport } from './utils.js'
 
-export default function (f7, isGraalJs) {
+export default function (f7) {
   /*
   * Typed (DayOffset) block that can be used with the Ephemeris check block
   * Note that the block basically returns a zero day offset for the check
@@ -80,8 +80,7 @@ export default function (f7, isGraalJs) {
   * Code part
   */
   javascriptGenerator.forBlock['oh_zdt_now'] = function (block) {
-    const zdt = (isGraalJs) ? 'time.ZonedDateTime' : addDateSupport()[1]
-    return [`${zdt}.now()`, javascriptGenerator.ORDER_NONE]
+    return ['time.ZonedDateTime.now()', javascriptGenerator.ORDER_NONE]
   }
 
   /*
@@ -117,8 +116,7 @@ export default function (f7, isGraalJs) {
     const plusMinus = block.getFieldValue('plusminus')
     const period = block.getFieldValue('period')
 
-    const zdt = (isGraalJs) ? 'time.ZonedDateTime' : addDateSupport()[1]
-    return [`${zdt}.now().${plusMinus}${period}(${offsetValue})`, javascriptGenerator.ORDER_ATOMIC]
+    return [`time.ZonedDateTime.now().${plusMinus}${period}(${offsetValue})`, javascriptGenerator.ORDER_ATOMIC]
   }
 
   /*
@@ -168,15 +166,8 @@ export default function (f7, isGraalJs) {
     const minute = javascriptGenerator.valueToCode(block, 'minute', javascriptGenerator.ORDER_ATOMIC)
     const second = javascriptGenerator.valueToCode(block, 'second', javascriptGenerator.ORDER_ATOMIC)
 
-    if (isGraalJs) {
-      const code = `time.ZonedDateTime.now().withYear(${year}).withMonth(${month}).withDayOfMonth(${day}).withHour(${hour}).withMinute(${minute}).withSecond(${second}).withNano(0)`
-      return [code, javascriptGenerator.ORDER_ATOMIC]
-    } else {
-      let [dtf, zdt, gzdt, czdt] = addDateSupport()
-      let stringToParse = `${czdt}(${year}, ${month} ,${day}, ${hour}, ${minute}, ${second}, 0, ${zdt}.now().getOffset().getId(), ${zdt}.now().getZone().getId())`
-      let code = `${zdt}.parse(${stringToParse}, ${dtf}.ISO_ZONED_DATE_TIME)`
-      return [code, javascriptGenerator.ORDER_ATOMIC]
-    }
+    const code = `time.ZonedDateTime.now().withYear(${year}).withMonth(${month}).withDayOfMonth(${day}).withHour(${hour}).withMinute(${minute}).withSecond(${second}).withNano(0)`
+    return [code, javascriptGenerator.ORDER_ATOMIC]
   }
 
   /*
@@ -202,8 +193,7 @@ export default function (f7, isGraalJs) {
   */
   javascriptGenerator.forBlock['oh_zdt'] = function (block) {
     const day = block.getFieldValue('day')
-    const getZonedDateTime = (isGraalJs) ? 'time.toZDT' : addDateSupport()[2]
-    return [`${getZonedDateTime}('${day}')`, javascriptGenerator.ORDER_NONE]
+    return [`time.toZDT('${day}')`, javascriptGenerator.ORDER_NONE]
   }
 
   /*
@@ -244,8 +234,7 @@ export default function (f7, isGraalJs) {
   */
   javascriptGenerator.forBlock['oh_zdt_fromText'] = function (block) {
     const day = javascriptGenerator.valueToCode(block, 'day', javascriptGenerator.ORDER_ATOMIC)
-    const getZonedDateTime = (isGraalJs) ? 'time.toZDT' : addDateSupport()[2]
-    return [`${getZonedDateTime}(${day})`, javascriptGenerator.ORDER_NONE]
+    return [`time.toZDT(${day})`, javascriptGenerator.ORDER_NONE]
   }
 
   /*
@@ -271,11 +260,7 @@ export default function (f7, isGraalJs) {
   */
   javascriptGenerator.forBlock['oh_zdt_fromItem'] = function (block) {
     const itemName = javascriptGenerator.valueToCode(block, 'itemName', javascriptGenerator.ORDER_ATOMIC)
-    if (isGraalJs) {
-      return [`time.toZDT(items.getItem(${itemName}))`, javascriptGenerator.ORDER_NONE]
-    } else {
-      return [`itemRegistry.getItem(${itemName}).getState().getZonedDateTime()`, javascriptGenerator.ORDER_NONE]
-    }
+    return [`time.toZDT(items.getItem(${itemName}))`, javascriptGenerator.ORDER_NONE]
   }
 
   const nextImage =
@@ -645,18 +630,17 @@ export default function (f7, isGraalJs) {
   javascriptGenerator.forBlock['oh_zdt_toText'] = function (block) {
     const date = javascriptGenerator.valueToCode(block, 'date', javascriptGenerator.ORDER_ATOMIC)
     const withtime = block.getFieldValue('withtime')
-    const dtf = (isGraalJs) ? 'time.DateTimeFormatter' : addDateSupport()[0]
 
     let code = ''
     if (withtime === 'with') {
-      code = `${date}.format(${dtf}.ofPattern('yyyy-MM-dd HH:mm:ss'))`
+      code = `${date}.format(time.DateTimeFormatter.ofPattern('yyyy-MM-dd HH:mm:ss'))`
     } else if (withtime === 'without') {
-      code = `${date}.format(${dtf}.ofPattern('yyyy-MM-dd'))`
+      code = `${date}.format(time.DateTimeFormatter.ofPattern('yyyy-MM-dd'))`
     } else if (withtime === 'withPattern') {
       const pattern = javascriptGenerator.valueToCode(block, 'pattern', javascriptGenerator.ORDER_ATOMIC)
-      code = `${date}.format(${dtf}.ofPattern(${pattern}))`
+      code = `${date}.format(time.DateTimeFormatter.ofPattern(${pattern}))`
     } else {
-      code = `${date}.format(${dtf}.ofPattern('yyyy-MM-dd\\'T\\'HH:mm:ss.SSSZ'))`
+      code = `${date}.format(time.DateTimeFormatter.ofPattern('yyyy-MM-dd\\'T\\'HH:mm:ss.SSSZ'))`
     }
 
     return [code, javascriptGenerator.ORDER_NONE]
@@ -701,7 +685,7 @@ export default function (f7, isGraalJs) {
     const precision = block.getFieldValue('precision')
     const dateComparison = block.getFieldValue('dateComparison')
 
-    const zdtCompare = (isGraalJs) ? addDateComparisonSupportGraalVM() : addDateComparisonSupportNashorn()
+    const zdtCompare = addDateComparisonSupport()
     return [`${zdtCompare}(${zdtOne}, ${zdtTwo}, '${operation}', '${precision}', '${dateComparison}')`, javascriptGenerator.ORDER_NONE]
   }
 
@@ -737,21 +721,14 @@ export default function (f7, isGraalJs) {
   * Code part
   */
   javascriptGenerator.forBlock['oh_zdt_between'] = function (block) {
-    let zdtCompare = (isGraalJs) ? addDateComparisonSupportGraalVM() : addDateComparisonSupportNashorn()
+    let zdtCompare = addDateComparisonSupport()
     let zdtOne = javascriptGenerator.valueToCode(block, 'zdtOne', javascriptGenerator.ORDER_ATOMIC)
     let zdtTwo = javascriptGenerator.valueToCode(block, 'zdtTwo', javascriptGenerator.ORDER_ATOMIC)
     let zdtThree = javascriptGenerator.valueToCode(block, 'zdtThree', javascriptGenerator.ORDER_ATOMIC)
     let dateComparison = block.getFieldValue('dateComparison')
 
-    if (isGraalJs) {
-      const op = new Map([['dateandtime', 'isBetweenDateTimes'], ['date', 'isBetweenDates'], ['time', 'isBetweenTimes']]).get(dateComparison)
-      return [`${zdtOne}.${op}(${zdtTwo}, ${zdtThree})`, javascriptGenerator.ORDER_NONE]
-    } else {
-      let codeLow = `${zdtCompare}(${zdtTwo}, ${zdtOne}, 'beforeEqual', 'nanos', '${dateComparison}')`
-      let codeHigh = `${zdtCompare}(${zdtOne}, ${zdtThree}, 'beforeEqual', 'nanos', '${dateComparison}')`
-      let code = `(${codeLow} && ${codeHigh})`
-      return [code, javascriptGenerator.ORDER_NONE]
-    }
+    const op = new Map([['dateandtime', 'isBetweenDateTimes'], ['date', 'isBetweenDates'], ['time', 'isBetweenTimes']]).get(dateComparison)
+    return [`${zdtOne}.${op}(${zdtTwo}, ${zdtThree})`, javascriptGenerator.ORDER_NONE]
   }
 
   /*
@@ -781,39 +758,18 @@ export default function (f7, isGraalJs) {
     const zdt = javascriptGenerator.valueToCode(block, 'zdt', javascriptGenerator.ORDER_ATOMIC)
     let temporalPart = block.getFieldValue('temporalPart')
 
-    if (isGraalJs) {
-      const op = new Map([['getYear', 'year'], ['getMonthValue', 'monthValue'], ['getDayOfMonth', 'dayOfMonth'], ['getDayOfWeek', 'dayOfWeek().value'], ['getDayOfYear', 'dayOfYear'], ['getHour', 'hour'], ['getMinute', 'minute'], ['getSecond', 'second'], ['getNano', 'nano']])
-      switch (temporalPart) {
-        case 'getMilli':
-          temporalPart = 'getLong(time.ChronoField.MILLI_OF_SECOND)'
-          break
-        case 'getMicro':
-          temporalPart = 'getLong(time.ChronoField.MICRO_OF_SECOND) % 1000'
-          break
-        default:
-          temporalPart = op.get(temporalPart) + '()'
-      }
-      return [`${zdt}.${temporalPart}`, javascriptGenerator.ORDER_NONE]
-    } else {
-      const getZdtComponent = addGetZdtComponent()
-      const chrono = addChrono()
-      switch (temporalPart) {
-        case 'getMilli':
-          temporalPart = `getLong(${chrono}.MILLI_OF_SECOND)`
-          break
-        case 'getMicro':
-          temporalPart = `getLong(${chrono}.MICRO_OF_SECOND) % 1000`
-          break
-        case 'getNano':
-          temporalPart = `getLong(${chrono}.NANO_OF_SECOND) % 1000`
-          break
-        default:
-          temporalPart += '()'
-      }
-
-      let code = `${getZdtComponent}(${zdt}.${temporalPart})`
-      return [code, javascriptGenerator.ORDER_NONE]
+    const op = new Map([['getYear', 'year'], ['getMonthValue', 'monthValue'], ['getDayOfMonth', 'dayOfMonth'], ['getDayOfWeek', 'dayOfWeek().value'], ['getDayOfYear', 'dayOfYear'], ['getHour', 'hour'], ['getMinute', 'minute'], ['getSecond', 'second'], ['getNano', 'nano']])
+    switch (temporalPart) {
+      case 'getMilli':
+        temporalPart = 'getLong(time.ChronoField.MILLI_OF_SECOND)'
+        break
+      case 'getMicro':
+        temporalPart = 'getLong(time.ChronoField.MICRO_OF_SECOND) % 1000'
+        break
+      default:
+        temporalPart = op.get(temporalPart) + '()'
     }
+    return [`${zdt}.${temporalPart}`, javascriptGenerator.ORDER_NONE]
   }
 
   /*
@@ -847,11 +803,7 @@ export default function (f7, isGraalJs) {
     const zdtOne = javascriptGenerator.valueToCode(block, 'zdtOne', javascriptGenerator.ORDER_ATOMIC)
     const zdtTwo = javascriptGenerator.valueToCode(block, 'zdtTwo', javascriptGenerator.ORDER_ATOMIC)
 
-    const chronoUnit = (isGraalJs) ? 'time.ChronoUnit' : javascriptGenerator.provideFunction_(
-      'chronoUnit',
-      ['var ' + javascriptGenerator.FUNCTION_NAME_PLACEHOLDER_ + ' = Java.type("java.time.temporal.ChronoUnit");'])
-
-    let code = `${chronoUnit}.${temporalPart}.between(${zdtOne},${zdtTwo})`
+    const code = `time.ChronoUnit.${temporalPart}.between(${zdtOne},${zdtTwo})`
     return [code, javascriptGenerator.ORDER_NONE]
   }
 }

bundles/org.openhab.ui/web/src/assets/definitions/blockly/blocks-dicts.js
@@ -5,7 +5,7 @@
 import Blockly from 'blockly'
 import { javascriptGenerator } from 'blockly/javascript.js'
 
-export default function (f7, isGraalJs) {
+export default function (f7) {
   Blockly.Blocks['dicts_create_with'] = {
     /**
      * Block for creating a list with any number of elements of any type.

commit 5359bad82d2010c92cac5869274dc82956877e6f

Fix scripting add-on availability check for old JS Scripting MIME type
Regression from #2743.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/rule/module-wizard-mixin.js
@@ -74,6 +74,7 @@ export default {
       })
     },
     isMimeTypeAvailable (mimeType) {
+      if (mimeType === 'application/javascript;version=ECMAScript-2021') mimeType = 'application/javascript'
       return this.languages.map(l => l.contentType).includes(mimeType)
     }
   }

bundles/org.openhab.ui/web/src/pages/settings/rules/script/script-edit.vue
@@ -392,6 +392,7 @@ export default {
       })
     },
     isMimeTypeAvailable (mimeType) {
+      if (mimeType === 'application/javascript;version=ECMAScript-2021') mimeType = 'application/javascript'
       return this.languages.map(l => l.contentType).includes(mimeType)
     },
     mimeTypeDescription (mode) {