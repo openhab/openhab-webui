openhab/openhab-webui

commit dcbc549245f6c0a024d74cc40e11d8a5ad4ab7d5

Refactor card widgets to use a shared base component (#2781)
Supersedes #1801.

Implements the proposal from
https://github.com/openhab/openhab-webui/pull/1801#issuecomment-1475172733.

-----

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

bundles/org.openhab.ui/web/src/components/widgets/standard/oh-card.vue
@@ -1,0 +1,37 @@
+<template>
+  <f7-card :no-border="config.noBorder" :no-shadow="config.noShadow" :outline="config.outline" :style="config.style" :class="config.class">
+    <slot name="header">
+      <f7-card-header v-if="config.title" :style="config.headerStyle" :class="config.headerClass">
+        <div>{{ config.title }}</div>
+      </f7-card-header>
+    </slot>
+    <slot name="content-root">
+      <f7-card-content @click.native="config.action ? performAction : ''" :style="{ ...contentStyle, ...config.contentStyle}" :class="[ ...(Array.isArray(contentClass) ? contentClass : ['padding']), ...(Array.isArray(config.contentClass) ? config.contentClass : []) ]">
+        <slot name="content" />
+      </f7-card-content>
+    </slot>
+    <slot name="footer">
+      <f7-card-footer v-if="config.footer && !Array.isArray(config.footer)">
+        {{ config.footer }}
+      </f7-card-footer>
+      <f7-card-footer v-else-if="config.footer && Array.isArray(config.footer)">
+        <span v-for="text in config.footer" :key="text">
+          {{ text }}
+        </span>
+      </f7-card-footer>
+    </slot>
+  </f7-card>
+</template>
+
+<script>
+import mixin from '../widget-mixin'
+import { actionsMixin } from '@/components/widgets/widget-actions'
+import { OhCardDefinition } from '@/assets/definitions/widgets/standard/cards'
+
+export default {
+  mixins: [mixin, actionsMixin],
+  widget: OhCardDefinition,
+  props: ['context', 'contentStyle', 'contentClass'],
+  slotProps: ['header', 'content', 'content-root', 'footer']
+}
+</script>

bundles/org.openhab.ui/web/src/assets/definitions/widgets/helpers.js
@@ -159,13 +159,15 @@ export function pt (name, label, description) {
  * @param {string} name the name of the widget (in kebab case)
  * @param {string} label the untranslated (English) name of the widget
  * @param {string} description the untranslated (English) description of the widget
- * @param {string} icon an optional icon to illustrate the widget, used for map/plan markers
+ * @param {string} [icon] an optional icon to illustrate the widget, used for map/plan markers
+ * @param {boolean} [hidden=false] whether the widget is hidden and should not be shown in the widget picker
  */
-export function WidgetDefinition (name, label, description, icon) {
+export function WidgetDefinition (name, label, description, icon, hidden = false) {
   this.name = name
   this.label = label
   this.description = description
   if (icon) this.icon = icon
+  this.hidden = hidden
   this.props = {
     parameterGroups: [],
     parameters: []

bundles/org.openhab.ui/web/src/assets/definitions/widgets/layout/index.js
@@ -85,7 +85,7 @@ export function OhCanvasLayoutDefinition () {
     ])
     .paramGroup(pg('screenSettings', 'Screen Settings'), [
       pn('screenWidth', 'Screen Width', 'Screen width in pixels (default 1280)'),
-      pn('screenHeight', 'Screen Height', 'Screen width in pixels (default 720)'),
+      pn('screenHeight', 'Screen Height', 'Screen height in pixels (default 720)'),
       pb('scale', 'Scaling', 'Scale content to screen width (can lead to unexpected styling issues) (default false)'),
       pt('imageUrl', 'Image URL', 'The URL of the image to display as background').c('url'),
       pt('imageSrcSet', 'Image Source Set', 'The src-set attribute of background image element to take into account multiple device resolutions. For example: "/static/floorplans/floor-0.jpg, /static/floorplans/floor-0@2x.jpg 2x"')

bundles/org.openhab.ui/web/src/assets/definitions/widgets/standard/cards.js
@@ -13,6 +13,12 @@ export const CardParameters = () => [
   pb('outline', 'Outline', 'Show the card outline').a()
 ]
 
+// OhCard
+export const OhCardDefinition = () => new WidgetDefinition('oh-card', 'Card', 'The basic structure of all card widgets, providing title and footer and requiring a content slot', undefined, true)
+  .paramGroup(CardParameterGroup(), CardParameters())
+  .paramGroup(actionGroup(), actionParams())
+  .paramGroup(actionGroup('Tap Hold', 'Action performed when tapping and holding card (or calling contextual menu on desktop)', 'taphold'), actionParams(null, 'taphold'), true)
+
 // OhLabelCard
 import TrendParameters from '../system/trend.js'
 export const OhLabelCardDefinition = () => new WidgetDefinition('oh-label-card', 'Label Card', 'Display the state of an item in a card')

bundles/org.openhab.ui/web/src/assets/definitions/widgets/system/slider.js
@@ -5,7 +5,7 @@ export default () => [
   pd('min', 'Min', 'Minimum value'),
   pd('max', 'Max', 'Maximum value'),
   pd('step', 'Step', 'Minimum interval between values'),
-  pd('vertical', 'Vertical', 'Display the slider vertically'),
+  pb('vertical', 'Vertical', 'Display the slider vertically'),
   pb('label', 'Display Label', 'Display a label above the slider knob while sliding'),
   pb('scale', 'Display Scale', 'Display a scale on the slider'),
   pn('scaleSteps', 'Scale steps', 'Number of (major) scale markers'),

bundles/org.openhab.ui/web/src/components/widgets/generic-widget-component.vue
@@ -8,6 +8,12 @@
       <generic-widget-component v-else :context="childContext(slotComponent)" v-for="(slotComponent, idx) in slotComponents" :slot="slotName" :key="slotName + '-' + idx" @command="onCommand" />
     </template>
   </component>
+  <oh-card v-else-if="componentType && componentType === 'oh-card' && visible" :context="context">
+    <!-- eslint-disable-next-line vue/no-unused-vars -->
+    <template v-for="(slotComponents, slotName) in context.component.slots" #[slotName]>
+      <generic-widget-component :context="childContext(slotComponent)" v-for="(slotComponent, idx) in slotComponents" :slot="slotName" :key="slotName + '-' + idx" @command="onCommand" />
+    </template>
+  </oh-card>
   <generic-widget-component v-else-if="componentType && componentType.startsWith('widget:') && visible" :context="childWidgetContext()" @command="onCommand" />
   <component v-else-if="componentType && componentType.startsWith('oh-') && visible" :is="componentType" :context="context" @command="onCommand" />
   <div v-else-if="componentType && componentType === 'Label' && visible" :class="config.class" :style="config.style">

bundles/org.openhab.ui/web/src/components/widgets/standard/index.js
@@ -1,5 +1,6 @@
 /* Add any new widget to this file - the name of the export should be "OhSomething" */
 
+export { default as OhCard } from './oh-card.vue'
 export { default as OhEquipmentCard } from './oh-equipment-card.vue'
 export { default as OhLabelCard } from './oh-label-card.vue'
 export { default as OhToggleCard } from './oh-toggle-card.vue'

commit a907cc3319339b6abd27c76ec412bbf7be9a5401

`oh-card`: Fix action & tap-hold action not working (#2931)
Regression from #2781.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/standard/oh-card.vue
@@ -6,7 +6,7 @@
       </f7-card-header>
     </slot>
     <slot name="content-root">
-      <f7-card-content @click.native="config.action ? performAction : ''" :style="{ ...contentStyle, ...config.contentStyle}" :class="[ ...(Array.isArray(contentClass) ? contentClass : ['padding']), ...(Array.isArray(config.contentClass) ? config.contentClass : []) ]">
+      <f7-card-content @click.native="performAction" @taphold.native="onTaphold($event)" @contextmenu.native="onContextMenu($event)" :style="{ ...contentStyle, ...config.contentStyle}" :class="[ ...(Array.isArray(contentClass) ? contentClass : ['padding']), ...(Array.isArray(config.contentClass) ? config.contentClass : []) ]">
         <slot name="content" />
       </f7-card-content>
     </slot>