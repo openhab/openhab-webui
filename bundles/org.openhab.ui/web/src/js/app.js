8d8e46ad5bbf5bb3db6f30dcaa43220f96e678ac
Mark Herwege <mherwege@users.noreply.github.com>
2024-12-22 17:20:58 +0100

    Import treeview items locally & Clean-up sitemap page components (#2915)
    See https://github.com/openhab/openhab-webui/pull/2904#issuecomment-2537081847
    and https://github.com/openhab/openhab-webui/pull/2904#issuecomment-2537277268
    
    This moves the import from the treeview items for model and sitemap out
    of app.js and removes the sitemap page elements as this was never used.
    
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

--- bundles/org.openhab.ui/web/src/js/app.js
+++ bundles/org.openhab.ui/web/src/js/app.js
@@ -4,10 +4,7 @@ import './compatibility'
 import Vue from 'vue'
 
 // Import globally registered components
-import SitemapWidgetGeneric from '../components/sitemap/widget-generic.vue'
 import OHIconComponent from '../components/widgets/system/oh-icon.vue'
-import ModelTreeviewItem from '../components/model/treeview-item.vue'
-import SitemapTreeviewItem from '../components/pagedesigner/sitemap/treeview-item.vue'
 import GenericWidgetComponent from '../components/widgets/generic-widget-component.vue'
 import DeveloperDockIcon from '../components/developer/developer-dock-icon.vue'
 
@@ -77,9 +74,6 @@ const app = new Vue({
   }
 })
 
-Vue.component('sitemap-widget-generic', SitemapWidgetGeneric)
 Vue.component('oh-icon', OHIconComponent)
-Vue.component('model-treeview-item', ModelTreeviewItem)
-Vue.component('sitemap-treeview-item', SitemapTreeviewItem)
 Vue.component('generic-widget-component', GenericWidgetComponent)
 Vue.component('developer-dock-icon', DeveloperDockIcon)
--- bundles/org.openhab.ui/web/src/pages/settings/pages/sitemap/sitemap-edit.vue
+++ bundles/org.openhab.ui/web/src/pages/settings/pages/sitemap/sitemap-edit.vue
@@ -282,6 +282,7 @@
 import SitemapCode from '@/components/pagedesigner/sitemap/sitemap-code.vue'
 import WidgetDetails from '@/components/pagedesigner/sitemap/widget-details.vue'
 import AttributeDetails from '@/components/pagedesigner/sitemap/attribute-details.vue'
+import SitemapTreeviewItem from '@/components/pagedesigner/sitemap/treeview-item.vue'
 import SitemapMixin from '@/components/pagedesigner/sitemap/sitemap-mixin'
 import DirtyMixin from '../../dirty-mixin'
 
@@ -290,7 +291,8 @@ export default {
   components: {
     SitemapCode,
     WidgetDetails,
-    AttributeDetails
+    AttributeDetails,
+    SitemapTreeviewItem
   },
   props: ['createMode', 'uid', 'itemsList'],
   data () {
