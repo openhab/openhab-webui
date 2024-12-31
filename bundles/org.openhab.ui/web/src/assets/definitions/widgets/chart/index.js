0706f3a2b72b4477106a6c9a13b67e0d794dcc7a
Nadahar <Nadahar@users.noreply.github.com>
2024-12-31 15:40:32 +0100

    Enable additional chart types for `oh-data-series` (#2946)
    This enables the use of all imported ECharts in (custom) widgets using `oh-data-series`.

--- bundles/org.openhab.ui/web/src/assets/definitions/widgets/chart/index.js
+++ bundles/org.openhab.ui/web/src/assets/definitions/widgets/chart/index.js
@@ -286,7 +286,7 @@ export default {
     props: {
       parameterGroups: [actionGroup()],
       parameters: [
-        seriesTypeParameter('gauge', 'pie'),
+        seriesTypeParameter('line', 'bar', 'heatmap', 'scatter', 'gauge', 'pie'),
         ...actionParams()
       ]
     }
