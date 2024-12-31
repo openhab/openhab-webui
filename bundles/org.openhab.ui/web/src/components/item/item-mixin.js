openhab/openhab-webui

commit 553591d72c63c01a1453e395d68dbf1ac1d13358

item-mixin: Improve Item name validation RegEx (#2658)
`A-z` also matches `[ \ ] ^ _ `, which is not intended.

Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

bundles/org.openhab.ui/web/src/components/item/item-mixin.js
@@ -39,7 +39,7 @@ export default {
      * @returns {string} The error message if the name is invalid, or an empty string if the name is valid.
      */
     validateItemName (name) {
-      if (!/^[A-z][A-z0-9_]*$/.test(name)) {
+      if (!/^[A-Za-z][A-Za-z0-9_]*$/.test(name)) {
         return 'Required. Must not start with a number. A-Z,a-z,0-9,_ only'
       } else if (this.items && this.items.some(item => item.name === name)) {
         return 'An Item with this name already exists'

commit 2028872bfc4eee15657e03732966b560f42f65b8

Allow item names to start with an underscore (#2958)
Reported in
https://community.openhab.org/t/openhab-4-3-item-name-starting-with-underscore/161244

Regression from #2658

Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

bundles/org.openhab.ui/web/src/components/item/item-mixin.js
@@ -44,7 +44,7 @@ export default {
      * @returns {string} The error message if the name is invalid, or an empty string if the name is valid.
      */
     validateItemName (name) {
-      if (!/^[A-Za-z][A-Za-z0-9_]*$/.test(name)) {
+      if (!/^[A-Za-z_][A-Za-z0-9_]*$/.test(name)) {
         return 'Required. Must not start with a number. A-Z,a-z,0-9,_ only'
       } else if (this.items && this.items.some(item => item.name === name)) {
         return 'An Item with this name already exists'