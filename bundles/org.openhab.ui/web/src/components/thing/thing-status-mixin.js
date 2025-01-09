d3b8fda584365b27dd2e48001d7c3e2e998347e3
Jimmy Tanagra <jcode@tanagra.id.au>
2025-01-09 18:20:09 +1000

    Thing status: Make link in Thing status description clickable
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- bundles/org.openhab.ui/web/src/components/thing/thing-status-mixin.js
+++ bundles/org.openhab.ui/web/src/components/thing/thing-status-mixin.js
@@ -1,5 +1,9 @@
 import ThingStatusLabels from '@/assets/i18n/thing-status/en'
 
+// Applies to Thing statusInfo.description containing a pattern of
+// http(s)://[YOUROPENHAB]:[YOURPORT]/path
+const linkRegex = /^(?<pretext>.*)http[^:]*:\/\/[^/]?YOUROPENHAB[^/]?:[^/]?YOURPORT[^/]?\/(?<path>\S+)(?<posttext>.*)$/
+
 export default {
   methods: {
     thingStatusBadgeColor (statusInfo) {
@@ -12,6 +16,19 @@ export default {
     thingStatusBadgeText (statusInfo) {
       if (statusInfo.statusDetail !== 'NONE') return ThingStatusLabels[statusInfo.statusDetail]
       return statusInfo.status
+    },
+    thingStatusDescription (statusInfo) {
+      const description = statusInfo.description
+      if (statusInfo.statusDetail === 'CONFIGURATION_PENDING' && description) {
+        const result = linkRegex.exec(description)
+        if (result) {
+          const { pretext, path, posttext } = result.groups
+          if (!path) return description
+          const root = location.protocol + '//' + location.host
+          return `${pretext}<a href="${root}/${path}" target="_blank" class="link color-blue external">${root}/${path}</a>${posttext}`
+        }
+      }
+      return description
     }
   }
 }
