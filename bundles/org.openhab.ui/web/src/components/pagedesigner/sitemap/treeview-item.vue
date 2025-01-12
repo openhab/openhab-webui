openhab/openhab-webui

commit 15463f894f7b2f8a3a3426fd7076ba03de8ec1ed

Sitemap editor: Add drag & drop (#2904)
This PR implements drag and drop in the sitemap tree, to make it easier
to graphically rearange sitemaps.
To achieve this, [Vue.Draggable](https://github.com/SortableJS/Vue.Draggable) (by itself
based on SortableJS) is included.

Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

bundles/org.openhab.ui/web/src/components/pagedesigner/sitemap/treeview-item.vue
@@ -4,14 +4,21 @@
                     :textColor="iconColor" :color="'blue'"
                     :selected="selected && selected === widget"
                     :opened="!widget.closed"
+                    @treeview:open="setWidgetClosed(false)"
+                    @treeview:close="setWidgetClosed(true)"
                     @click="select">
-    <sitemap-treeview-item class="sitemap-treeview-item" v-for="(childwidget, idx) in children"
-                           :key="idx"
-                           :includeItemName="includeItemName"
-                           :widget="childwidget" :parent-widget="widget"
-                           :itemsList="items"
-                           @selected="(event) => $emit('selected', event)"
-                           :selected="selected" />
+    <draggable v-if="canHaveChildren" :list="children" group="sitemap-treeview" animation="150" fallbackOnBody="true" swapThreshold="0.6"
+               @start="onStart" @change="onChange" @end="onEnd">
+      <sitemap-treeview-item class="sitemap-treeview-item" v-for="(childwidget, idx) in children"
+                             :key="idx"
+                             :includeItemName="includeItemName"
+                             :widget="childwidget" :parentWidget="widget"
+                             :itemsList="items"
+                             @selected="(event) => $emit('selected', event)"
+                             :selected="selected"
+                             :sitemap="localSitemap"
+                             :moveState="localMoveState" />
+    </draggable>
     <div slot="label" class="subtitle">
       {{ subtitle() }}
     </div>
@@ -30,13 +37,22 @@
 
 <script>
 import SitemapMixin from '@/components/pagedesigner/sitemap/sitemap-mixin'
+import Draggable from 'vuedraggable'
+import fastDeepEqual from 'fast-deep-equal/es6'
 
 export default {
   name: 'sitemap-treeview-item',
   mixins: [SitemapMixin],
-  props: ['includeItemName', 'widget', 'parentWidget', 'itemsList', 'selected'],
+  props: ['includeItemName', 'widget', 'parentWidget', 'itemsList', 'selected', 'sitemap', 'moveState'],
   components: {
+    Draggable,
     SitemapTreeviewItem: 'sitemap-treeview-item'
+  },
+  data () {
+    return {
+      localSitemap: this.sitemap ? this.sitemap : this.widget,
+      localMoveState: this.moveState ? this.moveState : {}
+    }
   },
   methods: {
     subtitle () {
@@ -47,6 +63,56 @@ export default {
       let $ = self.$$
       if ($(event.target).is('.treeview-toggle')) return
       this.$emit('selected', [this.widget, this.parentWidget])
+    },
+    onStart (event) {
+      console.debug('Drag start event:', event)
+      this.$set(this.localMoveState, 'widget', this.widget.slots.widgets[event.oldIndex])
+      this.$set(this.localMoveState, 'oldList', this.widget.slots.widgets)
+      this.$set(this.localMoveState, 'oldIndex', event.oldIndex)
+    },
+    onChange (event) {
+      console.debug('Drag change event:', event)
+      if (event.added) {
+        this.validateMove(event.added.newIndex)
+      }
+    },
+    onEnd (event) {
+      console.debug('Drag end event:', event)
+      this.validateMove(event.newIndex)
+    },
+    validateMove (newIndex) {
+      const widget = this.localMoveState.widget
+      const parentWidget = this.findParent(widget, this.localSitemap)
+      const newList = parentWidget.slots.widgets
+      console.debug('New parent:', parentWidget)
+      if (!this.allowedWidgetTypes(parentWidget, newIndex).map(wt => wt.type).includes(widget.component)) {
+        this.$f7.toast.create({
+          text: 'Move invalid',
+          destroyOnClose: true,
+          closeTimeout: 2000
+        }).open()
+        console.debug('Move invalid, restore in original position:', this.localMoveState)
+        newList.splice(newIndex, 1)
+        this.localMoveState.oldList.splice(this.localMoveState.oldIndex, 0, widget)
+      } else {
+        console.debug('Move valid')
+      }
+    },
+    findParent (widget, parentWidget) {
+      if (parentWidget.slots?.widgets) {
+        for (const w of parentWidget.slots.widgets) {
+          if (fastDeepEqual(widget, w)) {
+            return parentWidget
+          } else {
+            const parent = this.findParent(widget, w)
+            if (parent != null) return parent
+          }
+        }
+      }
+      return null
+    },
+    setWidgetClosed (closed) {
+      this.$set(this.widget, 'closed', closed)
     }
   },
   computed: {
@@ -54,8 +120,10 @@ export default {
       return ''
     },
     children () {
-      if (!this.widget.slots || !this.widget.slots.widgets) return []
-      return this.widget.slots.widgets
+      return this.widget.slots?.widgets || []
+    },
+    canHaveChildren () {
+      return this.LINKABLE_WIDGET_TYPES.includes(this.widget.component)
     }
   }
 }

bundles/org.openhab.ui/web/src/components/pagedesigner/sitemap/dslUtil.js
@@ -53,7 +53,7 @@ function writeWidget (widget, indent) {
       }
     }
   }
-  if (widget.slots) {
+  if (widget.slots?.widgets?.length) {
     dsl += ' {\n'
     widget.slots.widgets.forEach((w) => {
       dsl += writeWidget(w, indent + 4)

bundles/org.openhab.ui/web/src/components/pagedesigner/sitemap/sitemap-code.vue
@@ -79,7 +79,6 @@ export default {
         const parser = new Parser(Grammar.fromCompiled(grammar))
         parser.feed(this.sitemapDsl.trim().replace(/\t/g, ' '))
         if (!parser.results.length) return { error: 'Unable to parse, check your input' }
-        // return parser.results[0].map((i) => i.name).join('\n')
         return parser.results[0]
       } catch (e) {
         return { error: e }

bundles/org.openhab.ui/web/src/components/pagedesigner/sitemap/sitemap-mixin.js
@@ -74,6 +74,26 @@ export default {
     }
   },
   methods: {
+    allowedWidgetTypes (parentWidget, excludeWidgetIndex) {
+      let types = this.WIDGET_TYPES.filter(w => w.type !== 'Sitemap')
+      // Button only allowed inside Buttongrid
+      if (parentWidget.component === 'Buttongrid') return types.filter(t => t.type === 'Button')
+      types = types.filter(t => t.type !== 'Button')
+      // No frames in frame
+      if (parentWidget.component === 'Frame') return types.filter(t => t.type !== 'Frame')
+      // Linkable widget types only contain frames or none at all
+      if (this.LINKABLE_WIDGET_TYPES.includes(parentWidget.component)) {
+        if (parentWidget.slots?.widgets?.length > 0) {
+          const widgetList = parentWidget.slots.widgets.filter((widget, index) => index !== excludeWidgetIndex)
+          if (widgetList.find(w => w.component === 'Frame')) {
+            return types.filter(t => t.type === 'Frame')
+          } else {
+            return types.filter(t => t.type !== 'Frame')
+          }
+        }
+      }
+      return types
+    },
     widgetTypeDef (component) {
       const componentType = component ?? this.widget.component
       return this.WIDGET_TYPES.find(w => w.type === componentType)

bundles/org.openhab.ui/web/src/pages/settings/pages/sitemap/sitemap-edit.vue
@@ -338,23 +338,7 @@ export default {
     },
     addableWidgetTypes () {
       if (!this.selectedWidget) return
-      let types = this.WIDGET_TYPES.filter(w => w.type !== 'Sitemap')
-      // Button only allowed inside Buttongrid
-      if (this.selectedWidget.component === 'Buttongrid') return types.filter(w => w.type === 'Button')
-      types = types.filter(w => w.type !== 'Button')
-      // No frames in frame
-      if (this.selectedWidget.component === 'Frame') return types.filter(w => w.type !== 'Frame')
-      // Linkable widget types only contain frames or none at all
-      if (this.LINKABLE_WIDGET_TYPES.includes(this.selectedWidget.component)) {
-        if (this.selectedWidget.slots?.widgets?.length > 0) {
-          if (this.selectedWidget.slots.widgets.find(w => w.component === 'Frame')) {
-            return types.filter(w => w.type === 'Frame')
-          } else {
-            return types.filter(w => w.type !== 'Frame')
-          }
-        }
-      }
-      return types
+      return this.allowedWidgetTypes(this.selectedWidget)
     }
   },
   watch: {
@@ -654,6 +638,7 @@ export default {
       if (widget.component === 'Buttongrid') {
         widget.slots?.widgets?.sort((button1, button2) => ((button1.config?.row ?? 0) - (button2.config?.row ?? 0)) || ((button1.config?.column ?? 0) - (button2.config?.column ?? 0)))
       }
+      this.addEmptySlot(widget)
       widget.slots?.widgets?.forEach(this.cleanConfig)
     },
     preProcessSitemapLoad (sitemap) {
@@ -679,7 +664,19 @@ export default {
           }
         }
       }
+      this.addEmptySlot(widget)
       widget.slots?.widgets?.forEach(this.preProcessWidgetLoad)
+    },
+    addEmptySlot (widget) {
+      // Needed for drag and drop to work into empty slot
+      if (this.LINKABLE_WIDGET_TYPES.includes(widget.component)) {
+        if (!widget.slots) {
+          widget.slots = {}
+        }
+        if (!widget.slots.widgets) {
+          widget.slots.widgets = []
+        }
+      }
     },
     preProcessSitemapSave (sitemap) {
       const processed = JSON.parse(JSON.stringify(sitemap))

commit b331344902f728e091fa7bc0b2afc0bcf299ad6e

Sitemap treeview: Fix arrows/chevrons shown if not expandable
Regression from #2904.
Reported in https://github.com/openhab/openhab-webui/pull/2970#issuecomment-2585663931.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/pagedesigner/sitemap/treeview-item.vue
@@ -66,6 +66,7 @@ export default {
     },
     onStart (event) {
       console.debug('Drag start event:', event)
+      this.$set(this.localMoveState, 'moving', true)
       this.$set(this.localMoveState, 'widget', this.widget.slots.widgets[event.oldIndex])
       this.$set(this.localMoveState, 'oldList', this.widget.slots.widgets)
       this.$set(this.localMoveState, 'oldIndex', event.oldIndex)
@@ -73,11 +74,13 @@ export default {
     onChange (event) {
       console.debug('Drag change event:', event)
       if (event.added) {
+        this.$set(this.localMoveState, 'moving', false)
         this.validateMove(event.added.newIndex)
       }
     },
     onEnd (event) {
       console.debug('Drag end event:', event)
+      this.$set(this.localMoveState, 'moving', false)
       this.validateMove(event.newIndex)
     },
     validateMove (newIndex) {
@@ -123,7 +126,7 @@ export default {
       return this.widget.slots?.widgets || []
     },
     canHaveChildren () {
-      return this.LINKABLE_WIDGET_TYPES.includes(this.widget.component)
+      return this.LINKABLE_WIDGET_TYPES.includes(this.widget.component) && (this.children.length > 0 || this.localMoveState.moving)
     }
   }
 }