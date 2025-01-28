e3c692062c001a8271492692c8193503adbae543
Jeff James <jeff@james-online.com>
2025-01-27 20:54:18 -0800

    Display conditions in script-edit.vue
    Signed-off-by: Jeff James <jeff@james-online.com>

--- bundles/org.openhab.ui/web/src/pages/settings/rules/script/script-edit.vue
+++ bundles/org.openhab.ui/web/src/pages/settings/rules/script/script-edit.vue
@@ -423,9 +423,10 @@ export default {
       if (this.loading) return
       this.loading = true
 
-      Promise.all([this.$oh.api.get('/rest/module-types?type=trigger'), this.$oh.api.get('/rest/rules/' + this.ruleId)]).then((data) => {
+      Promise.all([this.$oh.api.get('/rest/module-types?type=trigger'), this.$oh.api.get('/rest/module-types?type=condition'), this.$oh.api.get('/rest/rules/' + this.ruleId)]).then((data) => {
         this.$set(this.moduleTypes, 'triggers', data[0])
-        this.$set(this, 'rule', data[1])
+        this.$set(this.moduleTypes, 'conditions', data[1])
+        this.$set(this, 'rule', data[2])
 
         if (this.moduleId) {
           this.$set(this, 'currentModule', this.rule.actions.concat(this.rule.conditions).find((m) => m.id === this.moduleId))
@@ -438,7 +439,7 @@ export default {
 
         if (!this.rule.editable) {
           const commentChar = (this.mode === 'application/x-ruby' ? '#' : '//')
-          let triggerDescriptionComments = `${commentChar} Triggers:\n`
+          let preamble = `${commentChar} Triggers:\n`
           for (const trigger of this.rule.triggers) {
             const triggerModuleType = this.moduleTypes.triggers.find((t) => t.uid === trigger.type)
             let description = trigger.label || this.suggestedModuleTitle(trigger, triggerModuleType, 'trigger')
@@ -447,9 +448,20 @@ export default {
             } else {
               description = 'When ' + description
             }
-            triggerDescriptionComments += `${commentChar} - ${description}\n`
+            preamble += `${commentChar} - ${description}\n`
           }
-          this.script = triggerDescriptionComments + '\n' + this.script
+
+          if (this.rule.conditions.length > 0) {
+            preamble += `\n${commentChar} Conditions:\n`
+            for (const condition of this.rule.conditions) {
+              const conditionModuleType = this.moduleTypes.conditions.find((t) => t.uid === condition.type)
+              let description = condition.label || this.suggestedModuleTitle(condition, conditionModuleType, 'condition')
+              description = 'Only If ' + description
+              preamble += `${commentChar} - ${description}\n`
+            }
+          }
+
+          this.script = preamble + '\n' + this.script
         }
 
         this.loadScriptModuleTypes().then(() => {
