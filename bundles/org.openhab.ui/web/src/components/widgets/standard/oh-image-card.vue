openhab/openhab-webui

commit a7cc8f762c41cf646ca05cdac5e470c85e66af19

oh-image-card: Fix inconsistent image size when action is defined (#3046)
Fixes #3002.

Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

bundles/org.openhab.ui/web/src/components/widgets/standard/oh-image-card.vue
@@ -1,7 +1,7 @@
 <template>
   <oh-card :context="context" :content-class="['oh-image-card', 'no-padding']">
     <template #content-root>
-      <f7-card-content :style="config.contentStyle" :class="[ ...(Array.isArray(config.contentClass) ? config.contentClass : []), 'oh-image-card', (config.action ? '' : 'no-padding')]">
+      <f7-card-content :style="config.contentStyle" :class="[ ...(Array.isArray(config.contentClass) ? config.contentClass : []), 'oh-image-card', 'no-padding']">
         <f7-list v-if="config.action" class="image-link">
           <f7-list-item class="oh-image-clickable" link="#" no-chevron @click="performAction">
             <oh-image slot="content-start" :context="childContext(context.component)" />
@@ -29,6 +29,9 @@
       display none
     .oh-image
       margin-bottom 5px
+.card
+  .list
+    margin 0 !important
 </style>
 
 <script>

commit 736f6fc6531d153c68c7a3577eafdeea09ce6818

Fix oh-image-card styling affects all cards
Regression from #3046.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/standard/oh-image-card.vue
@@ -29,7 +29,6 @@
       display none
     .oh-image
       margin-bottom 5px
-.card
   .list
     margin 0 !important
 </style>