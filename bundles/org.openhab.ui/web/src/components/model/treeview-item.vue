2f51acd857732702fc8c29b1b6047dadc71c5ad9
Mark Herwege <mark.herwege@telenet.be>
2025-02-17 09:11:37 +0100

    avoid move when scrolling
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

--- bundles/org.openhab.ui/web/src/components/model/treeview-item.vue
+++ bundles/org.openhab.ui/web/src/components/model/treeview-item.vue
@@ -5,7 +5,7 @@
                     :selected="selected && selected.item.name === model.item.name"
                     :opened="model.opened" :toggle="model.item.type === 'Group'"
                     @click="select">
-    <draggable :disabled="!canDragDrop || !model.item.editable" :list="children" group="model-treeview" animation="150" fallbackOnBody="true" forceFallback="true" scrollSensitivity="200" swapThreshold="0.6"
+    <draggable :disabled="!canDragDrop || !model.item.editable" :list="children" group="model-treeview" animation="150" fallbackOnBody="true" forceFallback="true" scrollSensitivity="200" delay="400" swapThreshold="0.6"
                @start="onDragStart" @change="onDragChange" @end="onDragEnd" :move="onDragMove">
       <model-treeview-item v-for="(node, index) in children"
                            :key="node.item.name + '_' + index"
