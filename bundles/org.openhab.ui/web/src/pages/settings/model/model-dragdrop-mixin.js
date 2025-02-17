8ecdd61d1f3fb664b6ce7e13d491dd5ebb3efb15
Mark Herwege <mark.herwege@telenet.be>
2025-02-17 09:11:37 +0100

    dialog buttons vertical
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

--- bundles/org.openhab.ui/web/src/pages/settings/model/model-dragdrop-mixin.js
+++ bundles/org.openhab.ui/web/src/pages/settings/model/model-dragdrop-mixin.js
@@ -216,6 +216,7 @@ export default {
           text: 'Insert "' + this.itemLabel(node.item) +
             '" into "' + this.itemLabel(parentNode.item) +
             '" as',
+          verticalButtons: true,
           buttons: [
             { text: 'Cancel', color: 'gray', onClick: () => this.restoreModelUpdate() },
             { text: 'Location', onClick: () => this.addLocation(node, parentNode) },
@@ -228,7 +229,8 @@ export default {
           text: 'Insert "' + this.itemLabel(node.item) +
             '" into "' + this.itemLabel(parentNode.item) +
             '" as',
-          buttons: [
+            verticalButtons: true,
+            buttons: [
             { text: 'Cancel', color: 'gray', onClick: () => this.restoreModelUpdate() },
             { text: 'Equipment', onClick: () => this.addEquipment(node, parentNode) },
             { text: 'Point', onClick: () => this.addPoint(node, parentNode) }
@@ -255,7 +257,8 @@ export default {
           text: 'Insert "' + this.itemLabel(node.item) +
             '" into "' + this.itemLabel(parentNode.item) +
             '" as',
-          buttons: [
+            verticalButtons: true,
+            buttons: [
             { text: 'Cancel', color: 'gray', onClick: () => this.restoreModelUpdate() },
             { text: 'Equipment', onClick: () => this.addEquipment(node, parentNode) },
             { text: 'Point', onClick: () => this.addPoint(node, parentNode) }
@@ -287,7 +290,8 @@ export default {
           text: 'Insert "' + this.itemLabel(node.item) +
             '" into "' + this.itemLabel(parentNode.item) +
             '" as',
-          buttons: [
+            verticalButtons: true,
+            buttons: [
             { text: 'Cancel', color: 'gray', onClick: () => this.restoreModelUpdate() },
             { text: 'Location', onClick: () => this.addLocation(node, parentNode) },
             { text: 'Equipment', onClick: () => this.addEquipment(node, parentNode) },
@@ -300,7 +304,8 @@ export default {
           text: 'Insert "' + this.itemLabel(node.item) +
             '" into "' + this.itemLabel(parentNode.item) +
             '" as',
-          buttons: [
+            verticalButtons: true,
+            buttons: [
             { text: 'Cancel', color: 'gray', onClick: () => this.restoreModelUpdate() },
             { text: 'Equipment', onClick: () => this.addEquipment(node, parentNode) },
             { text: 'Point', onClick: () => this.addPoint(node, parentNode) },
@@ -396,7 +401,8 @@ export default {
             '" dragged from group "' + this.itemLabel(parentNode.item) +
             '" into "' + this.itemLabel(this.moveState.newParent.item) +
             '", keep original?',
-          buttons: [
+            verticalButtons: true,
+            buttons: [
             { text: 'Cancel', color: 'gray', onClick: () => this.restoreModelUpdate() },
             { text: 'Keep', onClick: () => this.updateAfterRemove() },
             { text: 'Remove', onClick: () => this.remove(node, parentNode, oldIndex) }
