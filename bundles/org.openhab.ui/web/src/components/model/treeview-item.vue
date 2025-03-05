b1ff391316e9a3dc90bc7da4d6e84b083bf332ec
Mark Herwege <mark.herwege@telenet.be>
2025-03-05 09:29:25 +0100

    lint
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

--- bundles/org.openhab.ui/web/src/components/model/treeview-item.vue
+++ bundles/org.openhab.ui/web/src/components/model/treeview-item.vue
@@ -4,7 +4,7 @@
                     :textColor="iconColor" :color="(model.item.created !== false) ? 'blue' :'orange'"
                     :selected="selected && selected.item.name === model.item.name"
                     :opened="model.opened" :toggle="canHaveChildren"
-                    @click="select" >
+                    @click="select">
     <draggable :disabled="!canDragDrop && !dropAllowed(model)" :list="children" group="model-treeview" animation="150" fallbackOnBody="true" fallbackThreshold="5"
                scrollSensitivity="200" delay="400" delayOnTouchOnly="true" invertSwap="true"
                @start="onDragStart" @change="onDragChange" @end="onDragEnd" :move="onDragMove">
