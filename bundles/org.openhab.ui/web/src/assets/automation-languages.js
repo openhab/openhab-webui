openhab/openhab-webui

commit 792ff7fab8ba8d8e5673d5d28d36f93b75c7b3bd

Script edit: Add support for new python add-on & Refactor automation-languages definition (#3051)
- Added support in script-edit.vue for new/imminent graal python
binding.
- Removed hard-coding of commentChar and pulled from restructured
annotation-language.js definition.
- Refactored the annotation-language.js definition implementation.

---------

Signed-off-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/assets/automation-languages.js
@@ -1,12 +1,28 @@
-export const AddonNames = {
-  'application/javascript': 'JavaScript Scripting',
-  'application/javascript;version=ECMAScript-5.1': 'JavaScript Scripting (Nashorn)',
-  'application/x-groovy': 'Groovy Scripting',
-  'application/x-python': 'Jython Scripting',
-  'application/x-ruby': 'JRuby Scripting'
-}
-
-export const DocumentationLinks = {
-  'application/javascript': '/link/jsscripting-script',
-  'application/x-ruby': '/link/jrubyscripting-script'
+export default {
+  'application/javascript': {
+    name: 'JavaScript Scripting',
+    commentChar: '//',
+    documentationLink: '/link/jsscripting-script'
+  },
+  'application/javascript;version=ECMAScript-5.1': {
+    name: 'JavaScript Scripting (Nashorn)',
+    commentChar: '//'
+  },
+  'application/x-groovy': {
+    name: 'Groovy Scripting',
+    commentChar: '//'
+  },
+  'application/x-python': {
+    name: 'Jython Scripting',
+    commentChar: '#'
+  },
+  'application/x-ruby': {
+    name: 'JRuby Scripting',
+    commentChar: '#',
+    documentationLink: '/link/jrubyscripting-script'
+  },
+  'application/x-python3': {
+    name: 'Python Scripting',
+    commentChar: '#'
+  }
 }

bundles/org.openhab.ui/web/src/pages/settings/rules/script/script-edit.vue
@@ -63,13 +63,13 @@
               <f7-button outline small :active="blocklyCodePreview" icon-f7="doc_text" :icon-size="($theme.aurora) ? 20 : 22" class="no-ripple" @click="showBlocklyCode" tooltip="Show generated code" />
             </f7-segmented>
           </template>
-          <f7-link v-if="DocumentationLinks[mode] && !isBlockly"
+          <f7-link v-if="documentationLink(mode) && !isBlockly"
                    icon-color="blue"
                    :text="$device.desktop ? 'Open Documentation' : 'Docs'"
                    tooltip="Open documentation"
                    icon-ios="f7:question_circle" icon-md="f7:question_circle" icon-aurora="f7:question_circle"
                    color="blue"
-                   :href="$store.state.websiteUrl + DocumentationLinks[mode]" target="_blank" external />
+                   :href="$store.state.websiteUrl + documentationLink(mode)" target="_blank" external />
           <f7-link class="right details-link margin-left padding-right" ref="detailsLink" @click="detailsOpened = true" icon-f7="chevron_up" />
         </span>
       </f7-toolbar>
@@ -162,7 +162,7 @@ import RuleStatus from '@/components/rule/rule-status-mixin'
 import ScriptGeneralSettings from './script-general-settings.vue'
 import ModuleDescriptionSuggestions from '../module-description-suggestions'
 import DirtyMixin from '../../dirty-mixin'
-import { AddonNames, DocumentationLinks } from '@/assets/automation-languages'
+import AUTOMATION_LANGUAGES from '@/assets/automation-languages'
 
 export default {
   mixins: [RuleStatus, ModuleDescriptionSuggestions, DirtyMixin],
@@ -396,7 +396,10 @@ export default {
       return this.languages.map(l => l.contentType).includes(mimeType)
     },
     mimeTypeDescription (mode) {
-      return AddonNames[mode] || mode
+      return AUTOMATION_LANGUAGES[mode].name || mode
+    },
+    documentationLink (mode) {
+      return AUTOMATION_LANGUAGES[mode]['documentationLink']
     },
     /**
      * Load the script module type, i.e. the available script languages
@@ -437,7 +440,7 @@ export default {
         this.initDirty()
 
         if (!this.rule.editable) {
-          const commentChar = (this.mode === 'application/x-ruby' ? '#' : '//')
+          const commentChar = AUTOMATION_LANGUAGES[this.mode].commentChar
           let triggerDescriptionComments = `${commentChar} Triggers:\n`
           for (const trigger of this.rule.triggers) {
             const triggerModuleType = this.moduleTypes.triggers.find((t) => t.uid === trigger.type)
@@ -718,7 +721,6 @@ export default {
   },
   created () {
     this.GRAALJS_MIME_TYPE = 'application/javascript'
-    this.DocumentationLinks = DocumentationLinks
   }
 }
 </script>

commit 4b77cd1c7145c50669e2b6e1adc58241645cc06b

Script Editor: Fix editing RulesDSL scripts (#3087)
Regression from #3051

Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

bundles/org.openhab.ui/web/src/assets/automation-languages.js
@@ -1,4 +1,9 @@
 export default {
+  'application/vnd.openhab.dsl.rule': {
+    name: 'Rules DSL',
+    commentChar: '//',
+    documentationLink: '/docs/configuration/rules-dsl.html'
+  },
   'application/javascript': {
     name: 'JavaScript Scripting',
     commentChar: '//',

bundles/org.openhab.ui/web/src/pages/settings/rules/script/script-edit.vue
@@ -396,10 +396,10 @@ export default {
       return this.languages.map(l => l.contentType).includes(mimeType)
     },
     mimeTypeDescription (mode) {
-      return AUTOMATION_LANGUAGES[mode].name || mode
+      return AUTOMATION_LANGUAGES[mode]?.name || mode
     },
     documentationLink (mode) {
-      return AUTOMATION_LANGUAGES[mode]['documentationLink']
+      return AUTOMATION_LANGUAGES[mode]?.documentationLink
     },
     /**
      * Load the script module type, i.e. the available script languages
@@ -441,7 +441,7 @@ export default {
         this.initDirty()
 
         if (!this.rule.editable) {
-          const commentChar = AUTOMATION_LANGUAGES[this.mode].commentChar
+          const commentChar = AUTOMATION_LANGUAGES[this.mode]?.commentChar
           let preamble = `${commentChar} Triggers:\n`
           for (const trigger of this.rule.triggers) {
             const triggerModuleType = this.moduleTypes.triggers.find((t) => t.uid === trigger.type)