c1f74e6a0bcbe13effc14f73d703febdab7fd205
Mark Herwege <mark.herwege@telenet.be>
2025-03-26 17:04:33 +0100

    improvements
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

--- bundles/org.openhab.ui/web/src/pages/settings/persistence/aliases-popup.vue
+++ bundles/org.openhab.ui/web/src/pages/settings/persistence/aliases-popup.vue
@@ -16,29 +16,28 @@
       </f7-navbar>
       <f7-block class="no-padding">
         <f7-list>
+          <item-picker class="alias-item-picker" title="Select Items" name="items" multiple="true" :value="currentItems" @input="updateItems($event)" />
+        </f7-list>
+        <f7-list>
           <li v-for="(a, idx) in currentAliases" class="swipeout" :key="Object.keys(a)[0]">
-            <span class="alias swipeout-content">
+            <div class="alias swipeout-content">
               <f7-link slot="media" icon-color="red" icon-aurora="f7:minus_circle_filled"
                        icon-ios="f7:minus_circle_filled" icon-md="material:remove_circle_outline"
                        @click="showSwipeout" />
-              <item-picker class="alias-item-picker" :title="Object.keys(a)[0]" :name="'item' + idx" :value="Object.keys(a)[0]" @input="updateAlias(idx, $event, null)" />
-              <f7-input class="alias-input" type="text" placeholder="alias [A-Za-z_][A-Za-z0-9_]*"
-                        validate-on-blur pattern="[A-Za-z_][A-Za-z0-9_]*"
-                        :value="Object.values(a)[0]" @change="updateAlias(idx, null, $event.target.value)" />
-            </span>
+              <f7-list-item class="alias-item" :title="Object.keys(a)[0]" />
+              <f7-input class="alias-input"
+                        type="text"
+                        placeholder="alias"
+                        validate pattern="[A-Za-z_][A-Za-z0-9_]*"
+                        error-message="Required. Must not start with a number. A-Z,a-z,0-9,_ only"
+                        :value="Object.values(a)[0]"
+                        @input="updateAlias(idx, Object.keys(a)[0], $event.target.value)" />
+            </div>
             <f7-swipeout-actions right>
-              <f7-swipeout-button @click="(ev) => deleteAlias(idx)" style="background-color: var(--f7-swipeout-delete-button-bg-color)">
+              <f7-swipeout-button @click="(ev) => deleteAlias(idx, Object.keys(a)[0])" style="background-color: var(--f7-swipeout-delete-button-bg-color)">
                 Delete
               </f7-swipeout-button>
             </f7-swipeout-actions>
-          </li>
-          <li>
-            <span class="alias">
-              <item-picker class="alias-item-picker alias-item-picker-new" title="Select Item" :name="'item' + currentAliases.length" value="" @input="updateAlias(null, $event, null)" />
-              <f7-input class="alias-input" type="text" placeholder="alias [A-Za-z_][A-Za-z0-9_]*"
-                        validate-on-blur pattern="[A-Za-z_][A-Za-z0-9_]*"
-                        value="" @change="updateAlias(null, '', $event.target.value)" />
-            </span>
           </li>
         </f7-list>
       </f7-block>
@@ -47,26 +46,43 @@
 </template>
 
 <style lang="stylus">
-span.alias
+.alias
   display: inline-flex
-  padding-left: 2px
-  width: 100%
+  width: calc(100% - (var(--f7-list-item-padding-horizontal) + var(--f7-safe-area-left)))
   align-items: center
   background: var(--f7-list-bg-color)
-ul.alias-item-picker.alias-item-picker-new.item-picker-container
-  padding-left: 18px
-ul.alias-item-picker.item-picker-container
-  padding-left: 0
-  width: 55%
-  .smart-select .item-after
-    display: none
-.alias-input
-  width: 45%
+  padding-left: calc(var(--f7-list-item-padding-horizontal) + var(--f7-safe-area-left))
+.alias::after
+  content: ''
+  position: absolute
+  background-color: var(--f7-list-item-border-color)
+  display: block
+  z-index: 15
+  top: auto
+  right: auto
+  bottom: 0
+  left: 0
+  height: 1px
+  width: 100%
+  transform-origin: 50% 100%
+  transform: scaleY(calc(1 / var(--f7-device-pixel-ratio)));
+.alias-item
+  width: 50%
   min-height: var(--f7-list-item-min-height)
   align-items: center
   display: flex
-ul.alias-item-picker .item-picker .item-content .item-inner::after
-  display: none
+.alias-input
+  width: 50%
+  min-height: var(--f7-list-item-min-height)
+  align-items: center
+  display: flex
+.alias-item .item-content .item-inner:after
+    display: none !important
+ul.alias-item-picker.item-picker-container
+  padding-left: 0
+  width: 100%
+  .smart-select .item-after
+    display: none
 </style>
 
 <script>
@@ -76,49 +92,41 @@ export default {
   components: { ItemPicker },
   props: ['aliases'],
   emits: ['aliasesConfigUpdate'],
-  data () {
-    return {
-      currentAliases: this.aliases || []
+  computed: {
+    currentItems () {
+      const items = this.currentAliases.map((a) => Object.keys(a)[0])
+      return [...items]
     }
   },
+  data () {
+    return {
+      currentAliases: []
+    }
+  },
+  created () {
+    this.currentAliases = this.aliases
+    this.currentAliases.sort((a, b) => this.aliasCompare(a, b))
+  },
   methods: {
+    updateItems (items) {
+      const oldAliases = this.currentAliases.filter((a) => items.indexOf(Object.keys(a)[0]) >= 0)
+      const newItems = items.filter((i) => this.currentItems.indexOf(i) < 0)
+      const newAliases = newItems.map((i) => {
+        const aliasObj = {}
+        aliasObj[i] = null
+        return aliasObj
+      })
+      const aliases = oldAliases.concat(newAliases)
+      aliases.sort((a, b) => this.aliasCompare(a, b))
+      this.currentAliases = [...aliases]
+    },
     updateAlias (idx, item, alias) {
-      const aliasObj = {}
-      if (idx === null) {
-        aliasObj[item] = alias
-        if (this.validateAlias(idx, item, alias)) {
-          this.currentAliases.push(aliasObj)
-        }
-      } else {
-        if (item) {
-          aliasObj[item] = Object.values(this.currentAliases[idx])[0]
-        } else {
-          aliasObj[Object.keys(this.currentAliases[idx])[0]] = alias
-        }
-        if (this.validateAlias(idx, item, alias)) {
-          this.currentAliases.splice(idx, 1, aliasObj)
-        }
-      }
+      const aliasObj = this.currentAliases[idx]
+      aliasObj[Object.keys(aliasObj)[0]] = alias
     },
-    validateAlias (idx, item, alias) {
-      if (item && item !== '') {
-        const index1 = this.currentAliases.findIndex((a) => Object.keys(a)[0] === item)
-        if (index1 !== -1 && (idx === null || idx !== index1)) {
-          this.$f7.dialog.alert('An alias for the item already exists!')
-          return false
-        }
-      }
-      if (alias && alias !== '') {
-        const index2 = this.currentAliases.findIndex((a) => Object.values(a)[0] === alias)
-        if (index2 !== -1 && (idx === null || idx !== index2)) {
-          this.$f7.dialog.alert('An alias with the same name already exists for another item!')
-          return false
-        }
-      }
-      return true
-    },
-    deleteAlias (idx) {
-      this.currentAliases.splice(idx, 1)
+    deleteAlias (idx, item) {
+      const aliases = this.currentAliases.splice(idx, 1)
+      this.currentAliases = [...aliases]
     },
     showSwipeout (ev) {
       let swipeoutElement = ev.target
@@ -131,16 +139,33 @@ export default {
         this.$f7.swipeout.open(swipeoutElement)
       }
     },
+    aliasCompare (a, b) {
+      const aItem = Object.keys(a)[0]
+      const bItem = Object.keys(b)[0]
+      return aItem < bItem ? -1 : (aItem > bItem ? 1 : 0)
+    },
     updateModuleConfig () {
-      // Ignore when item not set
-      let aliases = this.currentAliases.filter((a) => Object.keys(a)[0] !== '')
+      const aliases = this.currentAliases.map((a) => Object.values(a)[0])
       // Warn when no alias for item
-      const idx = aliases.findIndex((a) => Object.values(a)[0] === null)
+      const idx = this.currentAliases.findIndex((a) => !Object.values(a)[0] || Object.values(a)[0] === '')
       if (idx >= 0) {
-        this.$f7.dialog.alert('Empty alias for item ' + Object.keys(aliases[idx])[0] + '!')
+        this.$f7.dialog.alert('Empty alias for item ' + Object.keys(this.currentAliases[idx])[0] + '!')
         return
       }
-      this.$f7.emit('aliasesConfigUpdate', aliases)
+      // Warn when validation errors remain
+      if (aliases.some((a) => !/^[A-Za-z_][A-Za-z0-9_]*$/.test(a))) {
+        this.$f7.dialog.alert('Validation errors for aliases!')
+        return
+      }
+      // Warn when alias already exists
+      for (let i = 0; i < aliases.length; i++) {
+        const firstIdx = aliases.slice(0,i).indexOf(aliases[i])
+        if (firstIdx >= 0) {
+          this.$f7.dialog.alert('Alias ' + aliases[i] + ' for item ' + Object.keys(this.currentAliases[i])[0] + ' already exists for item ' + Object.keys(this.currentAliases[firstIdx])[0])
+          return
+        }
+      }
+      this.$f7.emit('aliasesConfigUpdate', this.currentAliases)
       this.$refs.modulePopup.close()
     }
   }
