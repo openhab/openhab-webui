c8f579cc373aa65544d902a970d4051398727e15
Florian Hotze <dev@florianhotze.com>
2025-04-05 16:26:52 +0200

    Thing & Channel pickers: Add Thing location/Channel description (#3126)
    Closes #2128.
    Replaces abandonded PR #2129.
    
    Displays the Thing location (if any) in the Thing picker and the channel
    description in the channel picker,
    which improves UX greatly as it allows to distinguish Things easier and
    know the channel functionality without having to lookup documentation.
    Also disables virtual list for the Thing picker to avoid issues with the
    height due to the different entry heights, as the Things page does not
    use a virtual list, we should also be fine without here.
    
    ---------
    
    Also-by: Dmitry P. (d51x) <dimonich110@gmail.com>
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- bundles/org.openhab.ui/web/src/components/config/controls/thing-picker.vue
+++ bundles/org.openhab.ui/web/src/components/config/controls/thing-picker.vue
@@ -26,8 +26,24 @@ export default {
         openIn: 'popup',
         searchbar: true,
         searchbarPlaceholder: this.$t('dialogs.search.things'),
-        virtualList: true,
-        virtualListHeight: (this.$theme.aurora) ? 32 : undefined
+
+        renderItem: (item, index) => {
+          let after = (index > 0) ? this.things[index - 1].location
+            ? this.things[index - 1].location + '<i class="icon f7-icons color-gray" style="width: 16px; height: 16px; font-size: 16px;">placemark</i>'
+            : '' : ''
+          return `
+                <li class="media-item">
+                  <label class="item-${item.radio ? 'radio' : 'checkbox'} item-content">
+                    <input type="${item.radio ? 'radio' : 'checkbox'}" name="${item.inputName}" value="${item.value}" ${item.selected ? 'checked' : ''}>
+                    <i class="icon icon-${item.radio ? 'radio' : 'checkbox'}"></i>
+                    <div class="item-inner">
+                      <div class="item-title" style="font-weight: var(--f7-list-item-title-font-weight); white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${item.text}</div>
+                      <div class="item-footer">${after}</div>
+                    </div>
+                  </label>
+                </li>
+              `
+        }
       }
     }
   },
