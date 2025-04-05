79b465c9b3ed0713611710cd22524f8d908d212c
Florian Hotze <dev@florianhotze.com>
2025-04-05 16:28:04 +0200

    Don't use virtual lists where not necessary
    Don't use virtual lists for:
    
    - persistence config filter picker
    - persistence config strategy picker
    
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- bundles/org.openhab.ui/web/src/pages/settings/persistence/strategy-picker.vue
+++ bundles/org.openhab.ui/web/src/pages/settings/persistence/strategy-picker.vue
@@ -35,9 +35,7 @@ export default {
     return {
       smartSelectParams: {
         view: this.$f7.view.main,
-        openIn: 'popup',
-        virtualList: true,
-        virtualListHeight: (this.$theme.aurora) ? 32 : undefined
+        openIn: 'popup'
       }
     }
   },
