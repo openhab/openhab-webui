faf3e58bc8b32a7c7a8b5d7b1f59363bf53c19e9
Mark Herwege <mark.herwege@telenet.be>
2025-05-28 08:59:39 +0200

    simplify overview
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

--- bundles/org.openhab.ui/web/src/pages/settings/health/health-overview.vue
+++ bundles/org.openhab.ui/web/src/pages/settings/health/health-overview.vue
@@ -16,32 +16,18 @@
       </f7-col>
     </f7-block>
 
-    <f7-block v-if="orphanLinksCount !== '0'" class="block-narrow">
+    <f7-block v-if="healthCount > 0" class="block-narrow">
       <f7-col>
         <f7-list media-list>
-          <f7-list-item
-            media-item
-            link="orphanlinks/"
-            title="Orphan Links"
-            :badge="orphanLinksCount > 0 ? orphanLinksCount : undefined"
-            :after="orphanLinksCount > 0 ? undefined : orphanLinksCount"
-            :badge-color="orphanLinksCount ? 'red' : 'blue'"
+          <f7-list-item v-if="orphanLinksCount > 0"
+            media-item link="orphanlinks/" title="Orphan Links"
+            :badge="orphanLinksCount" badge-color="red"
             :footer="objectsSubtitles.orphanLinks">
             <f7-icon slot="media" f7="link" color="gray" />
           </f7-list-item>
-        </f7-list>
-      </f7-col>
-    </f7-block>
-    <f7-block v-if="semanticsProblemCount !== '0'" class="block-narrow">
-      <f7-col>
-        <f7-list media-list>
-          <f7-list-item
-            media-item
-            link="semantics/"
-            title="Semantics Problems"
-            :badge="semanticsProblemCount > 0 ? semanticsProblemCount : undefined"
-            :after="semanticsProblemCount > 0 ? undefined : semanticsProblemCount"
-            :badge-color="semanticsProblemCount ? 'red' : 'blue'"
+          <f7-list-item v-if="semanticsProblemCount > 0"
+            media-item link="semantics/" title="Semantics Problems"
+            :badge="semanticsProblemCount" badge-color="red"
             :footer="objectsSubtitles.semanticsProblems">
             <f7-icon slot="media" f7="link" color="gray" />
           </f7-list-item>
@@ -59,8 +45,8 @@ export default {
         orphanLinks: 'Items pointing to non-existent thing channels or vica versa',
         semanticsProblems: 'Issues with semantic model setup'
       },
-      orphanLinksCount: '',
-      semanticsProblemCount: '',
+      orphanLinksCount: 0,
+      semanticsProblemCount: 0,
 
       expandedTypes: {
         systemSettings: this.$f7.width >= 1450
@@ -70,6 +56,9 @@ export default {
   computed: {
     apiEndpoints () {
       return this.$store.state.apiEndpoints
+    },
+    healthCount () {
+      return this.orphanLinksCount + this.semanticsProblemCount
     }
   },
   watch: {
@@ -83,12 +72,12 @@ export default {
       if (!this.apiEndpoints) return
       if (this.$store.getters.apiEndpoint('links')) {
         this.$oh.api.get('/rest/links/orphans').then((data) => {
-          self.orphanLinksCount = data.length.toString()
+          self.orphanLinksCount = data.length || 0
         })
       }
       if (this.$store.getters.apiEndpoint('items')) {
         this.$oh.api.get('/rest/items/semantics/health').then((data) => {
-          self.semanticsProblemCount = data.length.toString()
+          self.semanticsProblemCount = data.length || 0
         })
       }
     },
