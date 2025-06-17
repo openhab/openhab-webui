bb62550965e185d2520f4da13a72799084817d2d
Mark Herwege <mark.herwege@telenet.be>
2025-06-17 22:14:06 +0200

    moved to app.js
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

--- bundles/org.openhab.ui/web/src/js/routes.js
+++ bundles/org.openhab.ui/web/src/js/routes.js
@@ -93,40 +93,6 @@ const loadAsync = (page, props) => {
   }
 }
 
-// Avoid excessive console logging, make it configurable from the UI
-const LOG_LEVELS = ['OFF', 'ERROR', 'WARN', 'INFO', 'DEBUG', 'TRACE']
-if (!window._originalConsole) {
-  window._originalConsole = {
-    info: console.info,
-    warn: console.warn,
-    error: console.error,
-    debug: console.debug,
-    trace: console.trace,
-    time: console.time,
-    timeEnd: console.timeEnd,
-    timeLog: console.timeLog
-  }
-}
-window.setLogLevel = function (level) {
-  window.LOG_LEVEL = level
-  localStorage.setItem('logLevel', level)
-
-  const c = window._originalConsole
-  const enabled = LOG_LEVELS.indexOf(level.toUpperCase())
-
-  console.error = enabled >= 1 ? c.error : () => {}
-  console.warn = enabled >= 2 ? c.warn : () => {}
-  console.info = enabled >= 3 ? c.info : () => {}
-  console.log = enabled >= 4 ? c.debug : () => {}
-  console.debug = enabled >= 4 ? c.debug : () => {}
-  console.trace = enabled >= 5 ? c.trace : () => {}
-  console.time = enabled >= 4 ? c.time : () => {}
-  console.timeEnd = enabled >= 4 ? c.timeEnd : () => {}
-  console.timeLog = enabled >= 4 ? c.timeLog : () => {}
-}
-// Initialize from localStorage
-window.setLogLevel(localStorage.getItem('logLevel') || 'OFF')
-
 export default [
   {
     path: '/',
