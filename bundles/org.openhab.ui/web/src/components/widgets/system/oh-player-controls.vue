c551ff27073ccdbdc0640114cc829f7ad17ce2a1
Laurent ARNAL <laurent@clae.net>
2025-06-18 17:36:10 +0200

    change browse icon
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

--- bundles/org.openhab.ui/web/src/components/widgets/system/oh-player-controls.vue
+++ bundles/org.openhab.ui/web/src/components/widgets/system/oh-player-controls.vue
@@ -6,7 +6,7 @@
     <f7-button v-if="this.config.showRewindFFward" color="blue" @click.stop="fastForward()" large icon-material="fast_forward" icon-size="24" icon-color="gray" />
     <f7-button color="blue" @click.stop="skipNext()" large icon-material="skip_next" icon-size="24" icon-color="gray" />
     
-    <f7-button color="blue"   @click.stop="mediaChange()" large icon-material="alarm" icon-size="24" icon-color="gray"  :href="mediaBrowserUri"/>
+    <f7-button color="blue"   @click.stop="mediaChange()" large icon-f7="folder_fill" icon-size="24" icon-color="gray"  :href="mediaBrowserUri"/>
     
   </f7-segmented>
 </template>
