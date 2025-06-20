a9ba9cce59a5b1915eb3d9f48af0cf621e0f03be
Florian Hotze <dev@florianhotze.com>
2025-06-20 13:46:39 +0200

    Make code null-safe & Minor improvements
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- bundles/org.openhab.ui/web/src/components/widgets/system/oh-colorpicker.vue
+++ bundles/org.openhab.ui/web/src/components/widgets/system/oh-colorpicker.vue
@@ -88,12 +88,12 @@ export default {
           }
         }
       }))
-      //Fix sliders at 0% - display: none -> width not known during component mount
+      // fixes color picker sliders at 0% because display width not available on component mount in widgets
       setTimeout(() => {
-        this.colorPicker.hueRangeSlider.calcSize()
-        this.colorPicker.saturationRangeSlider.calcSize()
-        this.colorPicker.brightnessRangeSlider.calcSize()
-      }, 0)
+        this.colorPicker.hueRangeSlider?.calcSize()
+        this.colorPicker.saturationRangeSlider?.calcSize()
+        this.colorPicker.brightnessRangeSlider?.calcSize()
+      })
     },
     sendCommand (hsb) {
       console.debug('oh-colorpicker: Received command ' + hsb)
