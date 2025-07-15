openhab/openhab-webui

commit 46038c4bf4d7d4393b510b7cc80430af9ad0632d

Add treeview semantics picker (#3178)
Closes https://github.com/openhab/openhab-webui/issues/3211.

This PR changes the semantics picker to use a treeview.

This treeview is inherited from
https://github.com/openhab/openhab-webui/pull/3165 and enhanced.
It therefore also introduces some enhancements in the semantic treeview maintenance.

The semantics treeview presents a searchable treeview of the semantic
classes/properties, limited to what is relevant in the context. When
used as a picker, radio buttons are used for picking. It has filter and
expand capabilities, options to show the semantic name and synonyms and
tooltips showing the semantic descriptions if available.


---------

Also-by: Florian Hotze <dev@florianhotze.com>
Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

bundles/org.openhab.ui/web/src/pages/settings/items/item-details.vue
@@ -29,11 +29,11 @@
           <item-state-preview :item="item" :context="context" />
         </f7-col>
       </f7-row>
-      <f7-row v-if="item && item.tags && item.tags.length > 0">
+      <f7-row v-if="nonSemanticTags?.length > 0">
         <f7-col>
-          <f7-block-title>Tags</f7-block-title>
+          <f7-block-title>Non-Semantic Tags</f7-block-title>
           <f7-block strong class="tags-block">
-            <f7-chip v-for="tag in item.tags" :key="tag" :text="tag" media-bg-color="blue">
+            <f7-chip v-for="tag in nonSemanticTags" :key="tag" :text="tag" media-bg-color="blue">
               <f7-icon slot="media" ios="f7:tag_fill" md="material:label" aurora="f7:tag_fill" />
             </f7-chip>
           </f7-block>
@@ -43,10 +43,12 @@
         <f7-col>
           <f7-block-title>Semantic Classification</f7-block-title>
           <f7-list>
-            <f7-list-item title="class" :after="item.metadata.semantics.value" />
+            <f7-list-item v-if="semanticClass" :title="semanticClass" :after="semanticValue" />
+            <f7-list-item v-if="semanticProperty" title="Property" :after="semanticProperty" />
             <f7-list-item
-              v-for="(value, key) in item.metadata.semantics.config"
+              v-for="(value, key) in semanticAttributes"
               :key="key"
+              :link="groupLink(value)"
               :title="key"
               :after="value" />
           </f7-list>
@@ -54,13 +56,13 @@
       </f7-row>
       <f7-row v-if="item && item.groupNames && item.groupNames.length > 0">
         <f7-col>
-          <f7-block-title>Direct Parent Groups</f7-block-title>
+          <f7-block-title>Direct Non-Semantic Parent Groups</f7-block-title>
           <f7-card>
             <f7-list>
               <f7-list-item
-                v-for="group in item.groupNames"
+                v-for="group in nonSemanticGroupNames"
                 :key="group"
-                :link="'/settings/items/' + group"
+                :link="groupLink(group)"
                 :title="group" />
             </f7-list>
           </f7-card>
@@ -189,6 +191,43 @@ export default {
       return {
         store: this.$store.getters.trackedItems
       }
+    },
+    semanticClass () {
+      return this.item?.metadata?.semantics?.value?.split('_')[0]
+    },
+    semanticValue () {
+      const valueArray = this.item?.metadata?.semantics?.value?.split('_')
+      if (!valueArray) return null
+      if (valueArray.length > 1) return valueArray.slice(1).join('->')
+      return valueArray[0]
+    },
+    semanticProperty () {
+      const config = this.item?.metadata?.semantics?.config
+      if (!config) return null
+      const propertyArray = config.relatesTo?.split('_')
+      if (!propertyArray) return null
+      if (propertyArray.length > 1) return propertyArray.slice(1).join('->')
+      return propertyArray[0]
+    },
+    semanticAttributes () {
+      const config = this.item?.metadata?.semantics?.config
+      if (!config) return null
+      const filteredAttributes = {
+        'isPointOf': 'Point Of',
+        'hasLocation': 'In Location',
+        'isPartOf': 'Part Of'
+      }
+      const attributes = {}
+      Object.keys(filteredAttributes).filter((key) => config[key]).forEach((key) => {
+        attributes[filteredAttributes[key]] = config[key]
+      })
+      return attributes
+    },
+    nonSemanticGroupNames () {
+      return this.item.groupNames.filter((g) => !Object.values(this.semanticAttributes).includes(g))
+    },
+    nonSemanticTags () {
+      return this.item?.tags?.filter((tag) => tag !== this.semanticTag(this.semanticValue) && tag !== this.semanticTag(this.semanticProperty))
     }
   },
   methods: {
@@ -234,6 +273,13 @@ export default {
     },
     searchInSidebar () {
       this.$f7.emit('selectDeveloperDock', { 'dock': 'tools', 'toolTab': 'pin', 'searchFor': this.item.name })
+    },
+    groupLink (group) {
+      return '/settings/items/' + group
+    },
+    semanticTag (value) {
+      const valueArray = value.split('->')
+      return valueArray[valueArray.length - 1]
     }
   }
 }

bundles/org.openhab.ui/web/src/components/item/item-form.vue
@@ -61,7 +61,7 @@
         </f7-list-input>
       </f7-list-group>
     </f7-list>
-    <semantics-picker v-if="!hideSemantics" :item="item" :same-class-only="true" :hide-type="true" :hide-none="forceSemantics" :createMode="createMode" :key="'semantics-' + item.tags.toString()" />
+    <semantics-picker v-if="!hideSemantics" :item="item" :createMode="createMode" :hide-none="forceSemantics" />
     <f7-list inline-labels no-hairline-md>
       <tag-input title="Non-Semantic Tags" :disabled="!editable" :item="item" />
     </f7-list>

bundles/org.openhab.ui/web/src/components/tags/semantics-picker-popup.vue
@@ -1,0 +1,176 @@
+<template>
+  <f7-popup closeByBackdropClick closeOnEscape @popup:open="onOpen" @popup:close="onClose">
+    <f7-page>
+      <f7-navbar :title="propertyMode ? 'Semantic Property' : 'Semantic Class'">
+        <f7-nav-right>
+          <f7-link @click="onClose">
+            Close
+          </f7-link>
+        </f7-nav-right>
+      </f7-navbar>
+      <f7-subnavbar :inner="false">
+        <f7-searchbar
+          search-container=".semantics-treeview"
+          search-item=".treeview-item"
+          search-in=".treeview-item-label"
+          :disable-button="!$theme.aurora"
+          @input="showFiltered($event.target.value)" />
+        <div class="expand-button">
+          <f7-button v-if="!expanded" icon-size="24" tooltip="Expand" icon-f7="rectangle_expand_vertical" @click="toggleExpanded()" />
+          <f7-button v-else color="gray" icon-size="24" tooltip="Collapse" icon-f7="rectangle_compress_vertical" @click="toggleExpanded()" />
+        </div>
+      </f7-subnavbar>
+      <f7-toolbar bottom class="toolbar-details">
+        <span />
+        <div class="padding-left padding-right text-align-center" style="font-size: 12px">
+          <div v-if="classMode">
+            <f7-checkbox :checked="!limitToClass" @change="toggleLimitToClass" />
+            <label @click="toggleLimitToClass" class="advanced-label">Show all classes</label>
+          </div>
+          <f7-checkbox :checked="showNames" @change="toggleShowNames" />
+          <label @click="toggleShowNames" class="advanced-label">Show tag names</label>
+          <f7-checkbox style="margin-left: 5px" :checked="showSynonyms" @change="toggleShowSynonyms" />
+          <label @click="toggleShowSynonyms" class="advanced-label">Show synonyms</label>
+        </div>
+        <span />
+      </f7-toolbar>
+      <semantics-treeview class="semantic-classes" :semanticTags="semanticTags" :expandedTags="expandedTags"
+                          @selected="tagSelected" :showNames="showNames" :showSynonyms="showSynonyms"
+                          :selectedTag="selectedTag" :selectedClass="selectedClass" :hideNone="hideNone"
+                          picker="true" :propertyMode="!!propertyMode" :classMode="!!classMode" :limitToClass="!!limitToClass" />
+    </f7-page>
+  </f7-popup>
+</template>
+
+<style lang="stylus">
+.expand-button
+  margin-right 8px
+  text-overflow unset
+  align-self center
+  .icon
+    margin-bottom 2.75px !important
+</style>
+
+<script>
+import SemanticsTreeview from '@/components/tags/semantics-treeview.vue'
+
+export default {
+  components: {
+    SemanticsTreeview
+  },
+  props: ['item', 'propertyMode', 'classMode', 'hideNone', 'semanticClass', 'semanticProperty'],
+  data () {
+    return {
+      semanticClasses: this.$store.getters.semanticClasses,
+      expanded: false,
+      expandedTags: [],
+      showNames: false,
+      showSynonyms: false,
+      filtering: false,
+      expandedBeforeFiltering: false,
+      selectedTag: null,
+      limitToClass: true
+    }
+  },
+  computed: {
+    semanticTags () {
+      return this.semanticClasses.Tags.map((t) => {
+        const tag = {
+          uid: t.uid,
+          name: t.name,
+          label: this.semanticClasses.Labels[t.name],
+          description: t.description,
+          synonyms: this.semanticClasses.Synonyms[t.name],
+          parent: t.parent
+        }
+        return tag
+      })
+    },
+    selectedClass () {
+      const selectedTag = this.semanticTags.find((t) => t.name === (this.semanticClass || this.semanticProperty)) || { uid: 'None', label: 'None' }
+      const tagName = selectedTag?.name
+      if (this.semanticClasses.Locations.indexOf(tagName) >= 0) return 'Location'
+      if (this.semanticClasses.Equipment.indexOf(tagName) >= 0) return 'Equipment'
+      if (this.semanticClasses.Points.indexOf(tagName) >= 0) return 'Point'
+      return ''
+    }
+  },
+  methods: {
+    onOpen () {
+      this.selectedTag = this.semanticTags.find((t) => t.name === (this.semanticClass || this.semanticProperty)) || { uid: 'None', label: 'None' }
+      // expand tree down to current selection
+      this.expandToSelection()
+    },
+    toggleShowNames () {
+      this.showNames = !this.showNames
+    },
+    toggleShowSynonyms () {
+      this.showSynonyms = !this.showSynonyms
+    },
+    toggleLimitToClass () {
+      this.limitToClass = !this.limitToClass
+    },
+    toggleExpanded () {
+      this.expanded = !this.expanded
+      this.semanticTags.forEach((t) => {
+        this.$set(this.expandedTags, t.uid, this.expanded)
+      })
+      this.expandToSelection()
+    },
+    expandToSelection () {
+      this.selectedTag?.parent?.split('_').reduce((prev, p) => {
+        const parent = (prev ? (prev + '_') : '') + p
+        this.$set(this.expandedTags, parent, true)
+        return parent
+      }, '')
+    },
+    showFiltered (filter) {
+      if (filter) {
+        if (!this.filtering) {
+          this.filtering = true
+          this.expandedBeforeFiltering = this.expanded
+          if (!this.expanded) {
+            this.toggleExpanded()
+          }
+        }
+      } else if (this.filtering) {
+        this.filtering = false
+        if (this.expanded && !this.expandedBeforeFiltering) {
+          this.toggleExpanded()
+        }
+      }
+    },
+    tagSelected (tag) {
+      const previousTag = this.selectedTag
+      if (previousTag?.name) {
+        const prevIndex = this.item.tags.indexOf(previousTag.name)
+        this.item.tags.splice(prevIndex, 1)
+      }
+      // Only add tag if note 'None'
+      if (tag.name) {
+        if (this.item.tags) {
+          this.item.tags.push(tag.name)
+        } else {
+          this.$set(this.item, 'tags', [tag.name])
+        }
+      }
+      // If changing tag to 'None', a 'Location' tag or an 'Equipment' tag, remove 'Property' tags
+      if (this.classMode && this.item.tags && (!tag.name || tag.uid.split('_')[0] !== 'Point')) {
+        const tags = [...this.item.tags]
+        tags.forEach((t) => {
+          if (this.semanticClasses.Properties.indexOf(t) >= 0) {
+            const index = this.item.tags.indexOf(t)
+            this.item.tags.splice(index, 1)
+          }
+        })
+      }
+      this.selectedTag = tag
+      this.$emit('changed')
+    },
+    onClose () {
+      this.$f7.popup.close()
+      this.$emit('close')
+    }
+  }
+}
+</script>

bundles/org.openhab.ui/web/src/components/tags/semantics-picker.vue
@@ -1,73 +1,104 @@
 <template>
-  <f7-list inline-labels no-hairlines-md v-if="show">
-    <f7-list-item :disabled="!editable" title="Semantic Class" class="aligned-smart-select" :key="'class-' + semanticClass" smart-select :smart-select-params="{view: $f7.view.main, searchbar: true, openIn: 'popup', closeOnSelect: true, scrollToSelectedItem: true}">
-      <select name="select-semantics-class" @change="update('class', $event.target.value)">
-        <option v-if="!hideNone" value="">
-          None
-        </option>
-        <optgroup label="Location" v-if="!sameClassOnly || semanticClass === '' || (sameClassOnly && currentSemanticType === 'Location')">
-          <option v-for="type in orderedLocations" :key="type" :value="type" :selected="type === semanticClass">
-            {{ type }}
-          </option>
-        </optgroup>
-        <optgroup label="Equipment" v-if="!sameClassOnly || semanticClass === '' || (sameClassOnly && currentSemanticType === 'Equipment')">
-          <option v-for="type in orderedEquipment" :key="type" :value="type" :selected="type === semanticClass">
-            {{ type }}
-          </option>
-        </optgroup>
-        <optgroup label="Point" v-if="!sameClassOnly || semanticClass === '' || (sameClassOnly && currentSemanticType === 'Point')">
-          <option v-for="type in orderedPoints" :key="type" :value="type" :selected="type === semanticClass">
-            {{ type }}
-          </option>
-        </optgroup>
-      </select>
-    </f7-list-item>
-    <f7-list-item v-if="currentSemanticType && !hideType" :disabled="!editable" title="Semantic Type" class="aligned-smart-select" :after="currentSemanticType" />
-    <f7-list-item v-if="currentSemanticType === 'Point'" :disabled="!editable" title="Semantic Property" class="aligned-smart-select" :key="'property-' + semanticProperty" smart-select :smart-select-params="{view: $f7.view.main, searchbar: true, openIn: 'popup', closeOnSelect: true, scrollToSelectedItem: true}">
-      <select name="select-semantics-property" :value="semanticProperty" @change="update('property', $event.target.value)">
-        <option :value="''">
-          None
-        </option>
-        <option v-for="type in orderedProperties" :key="type" :value="type" :selected="type === semanticProperty">
-          {{ type }}
-        </option>
-      </select>
-    </f7-list-item>
+  <f7-list inline-labels no-hairlines-md>
+    <f7-list-item :title="'Semantic ' + semanticValueTitle"
+                  :after="semanticValue || 'None'"
+                  :disabled="!editable"
+                  @click="openPopup('class')"
+                  class="aligned-smart-select" :link="editable" />
+    <f7-list-item v-if="currentSemanticType === 'Point'" title="Semantic Property"
+                  :after="tagWithHierarchy(semanticProperty) || 'None'"
+                  :disabled="!editable"
+                  @click="openPopup('property')"
+                  class="aligned-smart-select" :link="editable" />
+    <semantics-picker-popup
+      ref="classPopup" :key="'semantics-class'"
+      v-if="popupType === 'class'"
+      :item="item"
+      :hideNone="hideNone"
+      classMode="true"
+      :semanticClass="semanticClass"
+      @changed="itemChanged"
+      @close="closePopup" />
+    <semantics-picker-popup
+      ref="propertyPopup" :key="'semantics-property'"
+      v-if="popupType === 'property'"
+      :item="item"
+      :hideNone="hideNone"
+      propertyMode="true"
+      @changed="itemChanged"
+      :semanticProperty="semanticProperty"
+      @close="closePopup" />
   </f7-list>
 </template>
 
 <script>
 import TagMixin from '@/components/tags/tag-mixin'
+import SemanticsPickerPopup from '@/components/tags/semantics-picker-popup.vue'
 
 export default {
   mixins: [TagMixin],
-  props: ['item', 'sameClassOnly', 'hideType', 'hideNone', 'createMode'],
+  props: ['item', 'createMode', 'hideNone'],
+  components: {
+    SemanticsPickerPopup
+  },
   data () {
     return {
       semanticClass: '',
       semanticProperty: '',
-      show: true
+      popupType: null
+    }
+  },
+  computed: {
+    editable () {
+      return this.createMode || (this.item && this.item.editable)
+    },
+    currentSemanticType () {
+      return this.semanticType(this.semanticClass)
+    },
+    semanticValue () {
+      if (!this.semanticClass) return null
+      const value = this.tagWithHierarchy(this.semanticClass)
+      return value || this.currentSemanticType
+    },
+    semanticValueTitle () {
+      if (this.currentSemanticType === 'Location') return 'Location'
+      else if (this.currentSemanticType === 'Equipment') return 'Equipment'
+      else if (this.currentSemanticType === 'Point') return 'Point'
+      else return 'Value'
     }
   },
   methods: {
-    update (type, value) {
-      if (type === 'property') {
-        this.semanticProperty = value
-      } else {
-        this.semanticClass = value
-      }
-      this.item.tags = this.item.tags.filter((t) => !this.semanticType(t) && !this.isSemanticPropertyTag(t))
-      if (this.semanticClass) this.item.tags.push(this.semanticClass)
-      if (this.semanticType(this.semanticClass) === 'Point' && this.semanticProperty.length) {
-        this.item.tags.push(this.semanticProperty)
-      }
+    openPopup (type) {
+      if (!this.editable) return
+      this.popupType = type
+      this.$nextTick(() => {
+        const popupRef = type === 'class' ? 'classPopup' : 'propertyPopup'
+        const popupEl = this.$refs[popupRef]?.$el
+        if (popupEl) this.$f7.popup.open(popupEl)
+      })
     },
-    itemChanged (item) {
-      this.show = false
-      if (!item.tags) return
+    closePopup () {
+      this.popupType = null
+    },
+    tagWithHierarchy (tag) {
+      if (!tag) return null
+      let parentTagId = this.semanticClasses.Tags.find((t) => t.name === tag).parent
+      if (!parentTagId) return null // no parent tag, so this is the root class
+      let value = tag
+      while (parentTagId) {
+        const parentTag = this.semanticClasses.Tags.find((t) => t.uid === parentTagId)
+        parentTagId = parentTag.parent
+        if (parentTagId) {
+          value = parentTag.name + '->' + value
+        }
+      }
+      return value
+    },
+    itemChanged () {
+      if (!this.item.tags) return
       this.semanticClass = ''
       this.semanticProperty = ''
-      item.tags.forEach((t) => {
+      this.item.tags.forEach((t) => {
         if (this.semanticType(t) !== '') {
           this.semanticClass = t
         }
@@ -76,53 +107,18 @@ export default {
         }
       })
       if (this.semanticProperty && !this.semanticClass) {
-        if (item.metadata && item.metadata.semantics) {
-          const classFromMetadata = item.metadata.semantics.value.split('_')[1]
+        if (this.item.metadata && this.item.metadata.semantics) {
+          const valueArray = this.item.metadata.semantics.value.split('_')
+          const classFromMetadata = valueArray[valueArray.length - 1]
           if (classFromMetadata) {
             this.semanticClass = classFromMetadata
           }
         }
       }
-      this.$nextTick(() => {
-        this.show = true
-      })
     }
   },
   mounted () {
-    this.itemChanged(this.item)
-  },
-  watch: {
-    item (val) {
-      this.itemChanged(val)
-    }
-  },
-  computed: {
-    editable () {
-      return this.createMode || (this.item && this.item.editable)
-    },
-    orderedLocations () {
-      return [...this.semanticClasses.Locations].sort((a, b) => {
-        return a.localeCompare(b)
-      })
-    },
-    orderedEquipment () {
-      return [...this.semanticClasses.Equipment].sort((a, b) => {
-        return a.localeCompare(b)
-      })
-    },
-    orderedPoints () {
-      return [...this.semanticClasses.Points].sort((a, b) => {
-        return a.localeCompare(b)
-      })
-    },
-    orderedProperties () {
-      return [...this.semanticClasses.Properties].sort((a, b) => {
-        return a.localeCompare(b)
-      })
-    },
-    currentSemanticType () {
-      return this.semanticType(this.semanticClass)
-    }
+    this.itemChanged()
   }
 }
 </script>

bundles/org.openhab.ui/web/src/components/tags/semantics-treeview.vue
@@ -1,39 +1,67 @@
 <template>
   <f7-treeview class="semantics-treeview">
-    <semantics-treeview-item v-for="childTag in rootTags"
-                             :key="childTag.uid" :tag="childTag"
+    <semantics-treeview-item v-for="(childTag, idx) in rootTags"
+                             :key="idx" :tag="childTag"
                              :semanticTags="semanticTags"
                              :expandedTags="expandedTags"
                              :showNames="showNames"
-                             @selected="tagSelected" :selected="selected" />
+                             :showSynonyms="showSynonyms"
+                             :canDragDrop="!!canDragDrop"
+                             :picker="!!picker"
+                             @selected="tagSelected" :selectedTag="selectedTag" />
   </f7-treeview>
 </template>
 
 <style lang="stylus">
 .semantics-treeview
   --f7-treeview-tag-height 40px
-  .treeview-tag-label
+  .treeview-item-label
     font-size 10pt
     white-space nowrap
     overflow-x hidden
+  .synonyms-class
+    font-size 8pt
+    color var(--f7-list-item-footer-text-color)
 </style>
 
 <script>
 import SemanticsTreeviewItem from '@/components/tags/treeview-item.vue'
 
 export default {
-  props: ['semanticTags', 'expandedTags', 'showNames', 'selected'],
+  props: ['semanticTags', 'expandedTags', 'showNames', 'showSynonyms', 'picker', 'selectedTag', 'selectedClass', 'canDragDrop', 'propertyMode', 'classMode', 'limitToClass', 'hideNone'],
   components: {
     SemanticsTreeviewItem
   },
-  data () {
-    return {
-      rootTags: ['Location', 'Equipment', 'Point', 'Property'].map((cls) => this.semanticTags.find((tag) => tag.uid === cls))
+  computed: {
+    rootTags () {
+      const tags = []
+      if (this.picker && !this.hideNone) {
+        tags.push('None')
+      }
+      if (this.propertyMode) {
+        tags.push('Property')
+      } else if (this.classMode) {
+        if (this.limitToClass && this.selectedClass) {
+          tags.push(this.selectedClass)
+        } else {
+          tags.push('Location', 'Equipment', 'Point')
+        }
+      } else {
+        tags.push('Location', 'Equipment', 'Point', 'Property')
+      }
+      return tags.map((cls) => this.semanticTags.find((tag) => tag.uid === cls) || { uid: 'None', label: 'None' })
     }
   },
   methods: {
     tagSelected (tag) {
       this.$emit('selected', tag)
+    }
+  },
+  mounted () {
+    if (this.propertyMode) {
+      this.$set(this.expandedTags, 'Property', true)
+    } else if (this.classMode && this.selectedClass) {
+      this.$set(this.expandedTags, this.selectedClass, true)
     }
   }
 }

bundles/org.openhab.ui/web/src/components/tags/treeview-item.vue
@@ -1,15 +1,15 @@
 <template>
-  <f7-treeview-item selectable :label="tag.label + (showNames ? ' (' + tag.name + ')': '')"
+  <f7-treeview-item selectable :label="tag.label + (showNames && tag.name ? ' (' + tag.name + ')': '')"
                     :icon-ios="icon('ios')" :icon-aurora="icon('aurora')" :icon-md="icon('md')"
                     :iconColor="iconColor"
                     :textColor="iconColor"
-                    :selected="selected && selected === tag"
+                    :selected="!picker && selected"
                     :opened="expandedTags[tag.uid]"
                     :toggle="canHaveChildren"
                     @treeview:open="setTagOpened(true)"
                     @treeview:close="setTagOpened(false)"
                     @click="select">
-    <draggable :list="children" group="semantic-tags-treeview" filter=".non-draggable" animation="150" fallbackOnBody="true" swapThreshold="0.6"
+    <draggable :disabled="!canDragDrop" :list="children" group="semantic-tags-treeview" filter=".non-draggable" animation="150" fallbackOnBody="true" swapThreshold="0.6"
                @start="onDragStart" @change="onDragChange" @end="onDragEnd" :move="onDragMove">
       <semantics-treeview-item v-for="(childTag, idx) in children"
                                :key="idx"
@@ -17,13 +17,28 @@
                                :semanticTags="semanticTags"
                                :expandedTags="expandedTags"
                                :showNames="showNames"
+                               :showSynonyms="showSynonyms"
+                               :canDragDrop="canDragDrop"
+                               :picker="picker"
                                @selected="(event) => $emit('selected', event)"
-                               :selected="selected"
+                               :selectedTag="selectedTag"
                                :moveState="moveState"
                                :class="{ 'non-draggable': !childTag.editable }" />
     </draggable>
+    <div v-if="showSynonyms" slot="label" class="synonyms-class">
+      {{ synonyms }}
+    </div>
+    <f7-radio slot="content-start" name="semantic-tag-radio" v-if="picker" :checked="selected" @change="select" />
+    <f7-badge v-if="tag.description" slot="content-end" class="semantic-tag-tooltip-badge" :tooltip="tooltip">
+      <f7-icon class="tooltip-icon" f7="info_circle" ios="f7:info_circle" md="material:info" />
+    </f7-badge>
   </f7-treeview-item>
 </template>
+
+<style lang="stylus">
+.semantic-tag-tooltip-badge
+  background: transparent
+</style>
 
 <script>
 import Draggable from 'vuedraggable'
@@ -34,8 +49,11 @@ export default {
     semanticTags: Array,
     expandedTags: Array,
     tag: Object,
+    picker: Boolean,
     showNames: Boolean,
-    selected: Object,
+    showSynonyms: Boolean,
+    canDragDrop: Boolean,
+    selectedTag: Object,
     moveState: {
       type: Object,
       default: () => ({
@@ -52,16 +70,27 @@ export default {
   computed: {
     children () {
       return this.semanticTags.filter((tag) => (tag.parent === this.tag.uid)).sort((t1, t2) => {
-        if (t1.uid > t2.uid) return 1
-        if (t1.uid < t2.uid) return -1
-        return 0
+        return t1.label.localeCompare(t2.label)
       })
     },
     iconColor () {
-      return (this.tag.editable) ? (this.$f7.data.themeOptions.dark === 'dark' ? 'white' : 'black') : 'gray'
+      return (this.tag.editable || this.picker) ? (this.$f7.data.themeOptions.dark === 'dark' ? 'white' : 'black') : 'gray'
     },
     canHaveChildren () {
       return (this.children.length > 0 || this.moveState.moving) === true
+    },
+    synonyms () {
+      return this.tag?.synonyms?.join(', ') || ''
+    },
+    tooltip () {
+      let tooltip = this.tag.description
+      if (this.synonyms) {
+        tooltip = tooltip + '<br>(' + this.synonyms + ')'
+      }
+      return tooltip
+    },
+    selected () {
+      return this.selectedTag && this.selectedTag.uid === this.tag.uid
     }
   },
   methods: {
@@ -73,6 +102,8 @@ export default {
         return (theme === 'md') ? 'material:payments' : 'f7:cube_box'
       } else if (type === 'Point') {
         return (theme === 'md') ? 'material:flash_on' : 'f7:bolt_fill'
+      } else if (type === 'None') {
+        return ''
       } else {
         return 'material:label_outline'
       }
@@ -115,11 +146,10 @@ export default {
       }
       this.moveState.moveTarget = event.relatedContext?.element
       // Open group if not open yet, with a delay so you don't open it if you just drag over it
-      if (!this.moveState.moveTarget?.opened) {
-        const element = event.relatedContext.component
+      if (this.moveState.moveTarget && !this.moveState.moveTarget.opened) {
         this.moveState.moveDelayedOpen = setTimeout(() => {
-          this.setTagOpened(true, element.uid)
-        }, 1000, element)
+          this.setTagOpened(true, this.moveState.moveTarget.uid)
+        }, 1000)
       }
       return true
     },

commit 05bb624775da32f9efba843957f19d0ade109a06

Item details: Fix top-level semantics value breaks view (#3274)
Top-level semantic values like Point, Equipment, Location broke the
item-details view.

Regression from #3178.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/pages/settings/items/item-details.vue
@@ -284,8 +284,16 @@ export default {
     groupLink (group) {
       return '/settings/items/' + group
     },
+    /**
+     * Extracts the semantic tag from the semantic metadata value field.
+     *
+     * @param {string|null} value
+     * @return {*|null}
+     */
     semanticTag (value) {
+      if (!value) return null
       const valueArray = value.split('->')
+      if (valueArray.length === 0) return null
       return valueArray[valueArray.length - 1]
     }
   }