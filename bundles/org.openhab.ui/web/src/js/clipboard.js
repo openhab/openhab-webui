4fa88fd86581e581971c0822e5138b1e1c8a944b
Jimmy Tanagra <jcode@tanagra.id.au>
2025-08-20 22:17:51 +1000

    make the options object optional
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- bundles/org.openhab.ui/web/src/js/clipboard.js
+++ bundles/org.openhab.ui/web/src/js/clipboard.js
@@ -6,14 +6,14 @@ import { f7 } from 'framework7-vue'
  * presents a dialog prompting the user to manually trigger the copy operation.
  *
  * @param {string} data - The value to copy to the clipboard.
- * @param {Object} options - Options for the copy operation and dialog.
- * @param {string} [options.dialogTitle='Copy to Clipboard'] - The title of the fallback dialog.
- * @param {string} [options.dialogText='Click OK to copy data to clipboard'] - The text displayed in the fallback dialog.
+ * @param {Object} [options] - Optional configuration object.
+ * @param {string} [options.dialogTitle='Copy to Clipboard'] - Title for the dialog shown when manual copy is needed.
+ * @param {string} [options.dialogText='Click OK to copy data to clipboard'] - Text for the dialog shown when manual copy is needed.
  * @param {Function} [options.onSuccess] - Callback invoked when the copy operation succeeds.
  * @param {Function} [options.onError] - Callback invoked when the copy operation fails.
  * @returns {void} This function uses callbacks for outcomes. On Cancel the dialog closes and no callback is invoked.
  */
-export default function copyToClipboard (data, { dialogTitle = 'Copy to Clipboard', dialogText = 'Click OK to copy data to clipboard', onSuccess, onError }) {
+export default function copyToClipboard (data, { dialogTitle = 'Copy to Clipboard', dialogText = 'Click OK to copy data to clipboard', onSuccess, onError } = {}) {
   if (Vue.prototype.$clipboard(data)) {
     onSuccess && onSuccess()
   } else {
