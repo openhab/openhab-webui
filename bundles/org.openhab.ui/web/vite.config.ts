974ad50bb4ab1c2074d25252ff1b7c4c9fa37fd9
Florian Hotze <dev@florianhotze.com>
2025-10-13 00:10:29 +0200

    Fix RelativeCI webpack bundle analyzer
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- bundles/org.openhab.ui/web/vite.config.ts
+++ bundles/org.openhab.ui/web/vite.config.ts
@@ -7,15 +7,20 @@ import vitePluginTopLevelAwait from 'vite-plugin-top-level-await'
 import pluginDynamicImport from 'vite-plugin-dynamic-import'
 import { resolve } from 'path'
 import { plugin } from 'typescript-eslint'
+import webpackStatsPlugin from 'rollup-plugin-webpack-stats'
 
 const projectRootDir = resolve(__dirname)
 
 const apiBaseUrl = process.env.OH_APIBASE || 'http://localhost:8080'
 const maven = process.env.MAVEN || false
+const stats = process.env.STATS || false
 if (!maven) {
   console.log(`Using openHAB API base URL: ${apiBaseUrl}`)
 }
 const outPath = maven ? '../target/classes/app' : 'www'
+if (stats) {
+  console.log(`Build will generate webpack stats file: ${outPath}/webpack-stats.json`)
+}
 
 export default defineConfig({
   plugins: [
@@ -44,14 +49,15 @@ export default defineConfig({
     },
     pluginDynamicImport({
       filter (id) {
-        if(id.includes('/node_modules/')) {
+        if (id.includes('/node_modules/')) {
           return true
         }
       }
     }),
     vueDevtools(),
     visualizer({ open: false }),
-    vitePluginTopLevelAwait()
+    vitePluginTopLevelAwait(),
+    stats ? webpackStatsPlugin() : null
   ],
   define: {
     // __VUE_I18N_LEGACY_API__: false // tree-shake legacy mode
