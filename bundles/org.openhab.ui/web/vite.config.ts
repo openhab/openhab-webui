51433df29e5548084f8a7f4bab96fd1d5ffd6a38
Florian Hotze <dev@florianhotze.com>
2025-10-13 00:32:03 +0200

    Enable compression for production build
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- bundles/org.openhab.ui/web/vite.config.ts
+++ bundles/org.openhab.ui/web/vite.config.ts
@@ -1,23 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevtools from 'vite-plugin-vue-devtools'
-import { visualizer } from 'rollup-plugin-visualizer'
-import vitePluginTopLevelAwait from 'vite-plugin-top-level-await'
 // import pluginDynamicImportVars from '@rollup/plugin-dynamic-import-vars'
 import pluginDynamicImport from 'vite-plugin-dynamic-import'
+import vitePluginTopLevelAwait from 'vite-plugin-top-level-await'
+import { compression } from 'vite-plugin-compression2'
 import { resolve } from 'path'
 import { plugin } from 'typescript-eslint'
-import webpackStatsPlugin from 'rollup-plugin-webpack-stats'
+import vueDevtools from 'vite-plugin-vue-devtools'
+import { visualizer } from 'rollup-plugin-visualizer'
+import webpackStats from 'rollup-plugin-webpack-stats'
 
 const projectRootDir = resolve(__dirname)
 
+const production = process.env.NODE_ENV === 'production'
 const apiBaseUrl = process.env.OH_APIBASE || 'http://localhost:8080'
 const maven = process.env.MAVEN || false
 const stats = process.env.STATS || false
-if (!maven) {
+const outPath = maven ? '../target/classes/app' : 'www'
+
+if (production) {
+  console.log('Production build enabled.')
+} else {
   console.log(`Using openHAB API base URL: ${apiBaseUrl}`)
 }
-const outPath = maven ? '../target/classes/app' : 'www'
 if (stats) {
   console.log(`Build will generate webpack stats file: ${outPath}/webpack-stats.json`)
 }
@@ -54,10 +59,19 @@ export default defineConfig({
         }
       }
     }),
-    vueDevtools(),
-    visualizer({ open: false }),
     vitePluginTopLevelAwait(),
-    stats ? webpackStatsPlugin() : null
+    ...(production ? [
+      compression({
+        algorithms: [
+          'gzip',
+          'brotliCompress'
+        ]
+      })
+    ] : [
+      vueDevtools(),
+      visualizer({ open: false })
+    ]),
+    stats ? webpackStats() : null
   ],
   define: {
     // __VUE_I18N_LEGACY_API__: false // tree-shake legacy mode
