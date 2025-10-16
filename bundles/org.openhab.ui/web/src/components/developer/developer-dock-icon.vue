d6c3dacacaaa1606c75200814c8eda931b111870
Florian Hotze <dev@florianhotze.com>
2025-10-16 16:39:54 +0200

    Rewrite developer-dock-icon using TS Composition API
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- bundles/org.openhab.ui/web/src/components/developer/developer-dock-icon.vue
+++ bundles/org.openhab.ui/web/src/components/developer/developer-dock-icon.vue
@@ -1,25 +1,26 @@
 <template>
-  <f7-link v-if="iconVisible && runtimeStore.showDeveloperDock" icon-f7="question_circle_fill" @click="f7.emit('toggleDeveloperDock')" />
-  <f7-link v-else-if="iconVisible" icon-f7="question_circle" @click="f7.emit('selectDeveloperDock', { dock: 'help', helpTab: 'current' })" />
+  <f7-link
+    v-if="iconVisible && runtimeStore.showDeveloperDock"
+    icon-f7="question_circle_fill"
+    @click="f7.emit('toggleDeveloperDock')" />
+  <f7-link
+    v-else-if="iconVisible"
+    icon-f7="question_circle"
+    @click="f7.emit('selectDeveloperDock', { dock: 'help', helpTab: 'current' })" />
 </template>
 
-<script>
+<script setup lang="ts">
+import { computed, getCurrentInstance } from 'vue'
 import { f7 } from 'framework7-vue'
 import { useUserStore } from '@/js/stores/useUserStore'
 import { useRuntimeStore } from '@/js/stores/useRuntimeStore'
-import { mapStores } from 'pinia'
 
-export default {
-  setup () {
-    return {
-      f7
-    }
-  },
-  computed: {
-    iconVisible () {
-      return useUserStore().isAdmin() && this.$f7dim.width >= 1280
-    },
-    ...mapStores(useRuntimeStore)
-  }
-}
+const userStore = useUserStore()
+const runtimeStore = useRuntimeStore()
+
+const { proxy } = getCurrentInstance()
+
+const iconVisible = computed(() => {
+  return userStore.isAdmin() && proxy.$f7dim.width >= 1280
+})
 </script>
