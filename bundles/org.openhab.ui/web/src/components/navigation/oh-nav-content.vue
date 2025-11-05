openhab/openhab-webui

commit c1ff7dcf99e0600f029623c992a7c53eecac9ecc

Initial port to Vue3, vite, Framework7 v7.1.5, pinia stores, codemirror 6, vue-i18n v11, ... (#3350)
Closes #2151.

Fully functioning release.
There are some things left to do, see #3378.

Please report issues in this GitHub discussion:
https://github.com/openhab/openhab-webui/discussions/3381
We will then decided how critical the reported bug issue and depending
on that, fix it right now or put it on the to do lost:
https://github.com/openhab/openhab-webui/issues/3378

---------

Also-by: Florian Hotze <dev@florianhotze.com>
Signed-off-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/components/navigation/oh-nav-content.vue
@@ -1,0 +1,96 @@
+<template>
+  <f7-nav-left v-if="backLink">
+    <f7-link v-if="!theme.md"
+             icon-f7="chevron_left"
+             :href="backLinkUrl"
+             @click="back">
+      {{ backLink }}
+    </f7-link>
+    <f7-link v-else
+             icon-f7="arrow_left_md"
+             :href="backLinkUrl"
+             @click="back" />
+  </f7-nav-left>
+  <component :is="titleComponent">
+    {{ title }}
+    <span v-if="subtitle" class="subtitle">{{ subtitle }}</span>
+  </component>
+  <f7-nav-right>
+    <developer-dock-icon />
+    <f7-link v-if="editable === false"
+             icon-f7="lock_fill"
+             icon-only
+             tooltip="Not editable through the UI" />
+    <template v-if="saveLink && (editable === undefined || editable === true)">
+      <f7-link v-if="theme.md"
+               :href="saveLinkUrl"
+               @click="$emit('save')"
+               icon-md="material:save"
+               icon-only />
+      <f7-link v-if="!theme.md" @click="$emit('save')">
+        {{ saveLink }}
+      </f7-link>
+    </template>
+    <slot name="right" />
+  </f7-nav-right>
+</template>
+
+<script setup lang="ts">
+/*
+ * The oh-nav-content component provides the default content for <f7-navbar>.
+ * It includes a (working - F7 doesn't work properly) back button, a title, and a prefilled <f7-nav-right>:
+ * - a lock icon if editable is false
+ * - a save button if saveLink is provided and editable is not false - a click on this button emits a 'save' event and navigates to the saveLinkUrl if configured
+ * - additional content can be added into <f7-nav-right> through the right slot
+ *
+ * To use it, simply put it as the first component into the f7-navbar.
+ */
+import { f7, theme, f7NavTitle, f7NavTitleLarge } from 'framework7-vue'
+import type { Router } from 'framework7'
+import DeveloperDockIcon from '@/components/developer/developer-dock-icon.vue'
+import { computed } from 'vue'
+
+const props = withDefaults(defineProps<{
+  title: string,
+  subtitle?: string,
+  backLink?: string,
+  backLinkUrl?: string,
+  editable?: boolean,
+  saveLink?: string,
+  saveLinkUrl?: string,
+  large?: boolean,
+  f7router?: object,
+}>(), {
+  backLink: 'Back',
+  editable: undefined,
+  large: false
+})
+
+defineEmits(['save'])
+
+defineSlots<{
+  right: void,
+}>()
+
+const titleComponent = computed(() => {
+  if (props.large) {
+    return f7NavTitleLarge
+  } else {
+    return f7NavTitle
+  }
+})
+
+function back () {
+  if (props.backLinkUrl) return
+  const f7router : Router.Router = props.f7router || f7.views.main.router
+
+  f7router.history.pop()
+  const previousRoute : string | null = f7router.history.pop() ?? null
+  if (previousRoute === null) {
+    console.warn('No previous route found in history, cannot navigate back.')
+    return
+  }
+  console.debug('Navigating back to previous route:', previousRoute)
+  f7router.navigate(previousRoute, { force: true })
+}
+</script>

bundles/org.openhab.ui/web/src/pages/settings/items/item-details.vue
@@ -1,25 +1,18 @@
 <template>
   <f7-page class="item-details-page"
            @page:beforein="onPageBeforeIn"
-           @page:afterin="onPageAfterIn"
            @page:beforeout="onPageBeforeOut">
-    <f7-navbar :title="item.name"
-               :key="item.name"
-               back-link="Back"
-               no-shadow
-               no-hairline
-               class="item-details-navbar">
-      <f7-nav-right v-if="ready">
-        <f7-link v-if="item.editable" icon-md="material:edit" href="edit">
-          {{ $theme.md ? '' : 'Edit' }}
-        </f7-link>
-        <f7-link v-else
-                 icon-f7="lock_fill"
-                 tooltip="This Item is not editable through the UI"
-                 href="edit">
-          Details
-        </f7-link>
-      </f7-nav-right>
+    <f7-navbar>
+      <oh-nav-content v-if="ready"
+                      :title="item.name"
+                      :editable="item.editable"
+                      :f7router>
+        <template v-if="ready" #right>
+          <f7-link v-if="item.editable" icon-md="material:edit" href="edit">
+            {{ theme.md ? '' : 'Edit' }}
+          </f7-link>
+        </template>
+      </oh-nav-content>
       <f7-subnavbar sliding class="item-header">
         <div class="item-icon" v-if="item.name">
           <oh-icon v-if="item.category"
@@ -52,10 +45,9 @@
                      :key="tag"
                      :text="tag"
                      media-bg-color="blue">
-              <f7-icon slot="media"
-                       ios="f7:tag_fill"
-                       md="material:label"
-                       aurora="f7:tag_fill" />
+              <template #media>
+                <f7-icon ios="f7:tag_fill" md="material:label" aurora="f7:tag_fill" />
+              </template>
             </f7-chip>
           </f7-block>
         </f7-col>
@@ -98,13 +90,13 @@
       <f7-row v-if="item.name">
         <f7-col>
           <f7-block-title>Metadata</f7-block-title>
-          <metadata-menu :item="item" />
+          <metadata-menu :item="item" :f7router />
         </f7-col>
       </f7-row>
       <f7-row v-if="item.name && item.type !== 'Group'">
         <f7-col>
           <f7-block-title>Channel Links</f7-block-title>
-          <link-details :item="item" :links="links" />
+          <link-details :item="item" :links="links" :f7router />
         </f7-col>
       </f7-row>
       <f7-row>
@@ -182,13 +174,20 @@
     visibility hidden
 </style>
 
-<script>
-import cloneDeep from 'lodash/cloneDeep'
-
+<script setup>
 import ItemStatePreview from '@/components/item/item-state-preview.vue'
 import LinkDetails from '@/components/model/link-details.vue'
 import GroupMembers from '@/components/item/group-members.vue'
 import MetadataMenu from '@/components/item/metadata/item-metadata-menu.vue'
+</script>
+
+<script>
+import cloneDeep from 'lodash/cloneDeep'
+import { utils } from 'framework7'
+import { f7, theme } from 'framework7-vue'
+
+import { useStatesStore } from '@/js/stores/useStatesStore'
+
 import ItemMixin from '@/components/item/item-mixin'
 import FileDefinition from '@/pages/settings/file-definition-mixin'
 
@@ -198,11 +197,8 @@ export default {
     itemName: String,
     f7router: Object
   },
-  components: {
-    LinkDetails,
-    GroupMembers,
-    ItemStatePreview,
-    MetadataMenu
+  setup () {
+    return { theme, utils }
   },
   data () {
     return {
@@ -214,7 +210,7 @@ export default {
   computed: {
     context () {
       return {
-        store: this.$store.getters.trackedItems
+        store: useStatesStore().trackedItems
       }
     },
     semanticClass () {
@@ -264,27 +260,28 @@ export default {
   },
   methods: {
     onPageBeforeIn () {
-      this.$store.dispatch('startTrackingStates')
       this.load()
     },
-    onPageAfterIn () {
-      this.$oh.api.get('/rest/links?itemName=' + this.itemName).then((data) => {
-        this.links = data
-      })
-    },
     onPageBeforeOut () {
-      this.$store.dispatch('stopTrackingStates')
+      useStatesStore().stopTrackingStates()
     },
-    load () {
-      this.$oh.api.get(`/rest/items/${this.itemName}?metadata=.+`).then((data) => {
-        this.item = data
-        this.ready = true
+    async load () {
+      const promises = [
+        this.$oh.api.get(`/rest/items/${this.itemName}?metadata=.+`),
+        this.$oh.api.get('/rest/links?itemName=' + this.itemName)
+      ]
+
+      Promise.all(promises).then((data) => {
+        this.item = data[0]
+        this.links = data[1]
         this.iconUrl = '/icon/' + this.item.category + '?format=svg'
+        this.ready = true
+        useStatesStore().startTrackingStates()
       })
     },
     duplicateItem () {
       let itemClone = cloneDeep(this.item)
-      this.$f7router.navigate({
+      this.f7router.navigate({
         url: '/settings/items/duplicate'
       }, {
         props: {
@@ -293,18 +290,18 @@ export default {
       })
     },
     deleteItem () {
-      this.$f7.dialog.confirm(
+      f7.dialog.confirm(
         `Are you sure you want to delete ${this.item.label || this.item.name}?`,
         'Delete Item',
         () => {
           this.$oh.api.delete('/rest/items/' + this.item.name).then(() => {
-            this.$f7router.back('/settings/items/', { force: true })
+            this.f7router.back('/settings/items/', { force: true })
           })
         }
       )
     },
     searchInSidebar () {
-      this.$f7.emit('selectDeveloperDock', { 'dock': 'tools', 'toolTab': 'pin', 'searchFor': this.item.name })
+      f7.emit('selectDeveloperDock', { 'dock': 'tools', 'toolTab': 'pin', 'searchFor': this.item.name })
     },
     groupLink (group) {
       return '/settings/items/' + group

bundles/org.openhab.ui/web/src/components/model/item-details.vue
@@ -75,9 +75,10 @@
 </template>
 
 <script>
+import { f7 } from 'framework7-vue'
+
 import Item from '@/components/item/item.vue'
 import ItemForm from '@/components/item/item-form.vue'
-
 import ItemMixin from '@/components/item/item-mixin'
 
 export default {
@@ -127,7 +128,7 @@ export default {
       this.createMode = false
       this.forceSemantics = false
       if (this.model.item.created === false) {
-        this.$set(this, 'editedItem', Object.assign({}, this.model.item))
+        this.editedItem = Object.assign({}, this.model.item)
         this.createMode = true
         if (this.model.item.metadata && this.model.item.metadata.semantics) {
           this.forceSemantics = true
@@ -137,14 +138,14 @@ export default {
     save () {
       this.editMode = false
       this.saveItem(this.editedItem).then(() => {
-        this.$f7.toast.create({
+        f7.toast.create({
           text: 'Item updated',
           destroyOnClose: true,
           closeTimeout: 2000
         }).open()
         this.$emit('item-updated', this.editedItem)
       }).catch((err) => {
-        this.$f7.toast.create({
+        f7.toast.create({
           text: 'Item not saved: ' + err,
           destroyOnClose: true,
           closeTimeout: 2000
@@ -158,18 +159,18 @@ export default {
       if (!this.editedItem.name) return
 
       this.saveItem(this.editedItem).then(() => {
-        this.$f7.toast.create({
+        f7.toast.create({
           text: 'Item created',
           destroyOnClose: true,
           closeTimeout: 2000
         }).open()
-        this.$set(this.model, 'item', this.editedItem)
+        this.model.item = this.editedItem
         this.model.item.created = true
         this.model.item.editable = true
         this.$emit('item-created', this.model.item)
         this.onModelChange()
       }).catch((err) => {
-        this.$f7.toast.create({
+        f7.toast.create({
           text: 'Item not saved: ' + err,
           destroyOnClose: true,
           closeTimeout: 2000
@@ -179,7 +180,7 @@ export default {
     remove () {
       const vm = this
 
-      this.$f7.dialog.confirm(
+      f7.dialog.confirm(
         'Remove ' + this.model.item.name + '?',
         'Remove Item',
         () => {
@@ -191,7 +192,7 @@ export default {
       this.editMode = false
 
       this.$oh.api.delete('/rest/items/' + this.model.item.name).then((data) => {
-        this.$f7.toast.create({
+        f7.toast.create({
           text: 'Item removed',
           destroyOnClose: true,
           closeTimeout: 2000
@@ -201,7 +202,7 @@ export default {
         this.$emit('item-removed', this.model.item)
         this.onModelChange()
       }).catch((err) => {
-        this.$f7.toast.create({
+        f7.toast.create({
           text: 'Item not removed: ' + err,
           destroyOnClose: true,
           closeTimeout: 2000
@@ -210,7 +211,7 @@ export default {
     },
     edit () {
       this.editMode = true
-      this.$set(this, 'editedItem', Object.assign({}, this.model.item))
+      this.editedItem = Object.assign({}, this.model.item)
     },
     cancel () {
       if (this.createMode) {
@@ -218,7 +219,7 @@ export default {
       }
       this.createMode = false
       this.editMode = false
-      this.$set(this, 'editedItem', {})
+      this.editedItem = {}
     }
   },
   watch: {

commit e246f5632dc5ffe8fecb6e5bcc06cbdf1074aadd

Item details page: Fix navigation bar unresponsive on mobile devices (#3430)
Regression from #3350.

Issue discovered on iPhone.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/navigation/oh-nav-content.vue
@@ -38,6 +38,7 @@
     </template>
     <slot name="right" />
   </f7-nav-right>
+  <slot name="after" />
 </template>
 
 <script setup lang="ts">
@@ -74,6 +75,7 @@ defineEmits(['save'])
 
 defineSlots<{
   right: void,
+  after: void,
 }>()
 
 function back () {

bundles/org.openhab.ui/web/src/pages/settings/items/item-details.vue
@@ -12,24 +12,26 @@
             {{ theme.md ? '' : 'Edit' }}
           </f7-link>
         </template>
+        <template #after>
+          <f7-subnavbar class="item-header">
+            <div class="item-icon" v-if="item.name">
+              <oh-icon v-if="item.category"
+                       :icon="item.category"
+                       :state="item.type === 'Image' ? null : (context.store[item.name].state || item.state)"
+                       height="60"
+                       width="60" />
+              <span v-else>
+                {{ item.label ? item.label[0] : item.name[0] }}
+              </span>
+            </div>
+            <h2>{{ item.label }}</h2>
+            <!-- <h4 v-show="item.label">{{item.name}}</h4> -->
+            <h5 v-show="item.type" style="margin-top: 10px; margin-bottom: 15px;">
+              <small>{{ getItemTypeLabel(item) }}</small>
+            </h5>
+          </f7-subnavbar>
+        </template>
       </oh-nav-content>
-      <f7-subnavbar sliding class="item-header">
-        <div class="item-icon" v-if="item.name">
-          <oh-icon v-if="item.category"
-                   :icon="item.category"
-                   :state="item.type === 'Image' ? null : (context.store[item.name].state || item.state)"
-                   height="60"
-                   width="60" />
-          <span v-else>
-            {{ item.label ? item.label[0] : item.name[0] }}
-          </span>
-        </div>
-        <h2>{{ item.label }}</h2>
-        <!-- <h4 v-show="item.label">{{item.name}}</h4> -->
-        <h5 v-show="item.type" style="margin-top: 10px; margin-bottom: 15px;">
-          <small>{{ getItemTypeLabel(item) }}</small>
-        </h5>
-      </f7-subnavbar>
     </f7-navbar>
     <f7-block class="block-narrow after-item-header" v-if="item">
       <f7-row v-if="item.state">