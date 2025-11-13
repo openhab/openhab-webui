c128f3230c6957cba3180853e64cd87e988bcbbf
Florian Hotze <dev@florianhotze.com>
2025-11-13 18:22:03 +0100

    Fix typecheck
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- bundles/org.openhab.ui/web/src/components/config/controls/parameter-month.vue
+++ bundles/org.openhab.ui/web/src/components/config/controls/parameter-month.vue
@@ -13,7 +13,7 @@
         <option v-for="(day, idx) in values"
                 :value="day"
                 :key="day"
-                :selected="isSelected(day) ? true : null">
+                :selected="isSelected(day) ? true : undefined">
           {{ labels[idx] }}
         </option>
       </select>
