openhab/openhab-webui

commit c1ff7dcf99e0600f029623c992a7c53eecac9ecc

Initial port to Vue3, vite, Framework7 v7.1.5, pinia stores, codemirror 6, vue-i18n v11, ... (#3350)
Closes #2151.

Fully functioning release.
There are some things left to do, see #3378.

Please report issues in this GitHub discussion:
https://github.com/openhab/openhab-webui/discussions/3381
We will then decided how critical the reported bug issue and depending
on that, fix it right now or put it on the to do lost:
https://github.com/openhab/openhab-webui/issues/3378

---------

Also-by: Florian Hotze <dev@florianhotze.com>
Signed-off-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/components/widgets/chart/misc/oh-chart-tooltip.js
@@ -2,6 +2,7 @@ import dayjs from 'dayjs'
 import 'dayjs/locale/de'
 import LocalizedFormat from 'dayjs/plugin/localizedFormat'
 import ComponentId from '../../component-id'
+
 dayjs.extend(LocalizedFormat)
 
 export default {

bundles/org.openhab.ui/web/src/components/widgets/chart/series/oh-mark-area.js
@@ -1,21 +1,20 @@
 import dayjs from 'dayjs'
 import LocalizedFormat from 'dayjs/plugin/localizedFormat'
 import ComponentId from '../../component-id'
+
 dayjs.extend(LocalizedFormat)
 
 export default {
   get (component, points, startTime, endTime, chart) {
     let markArea = chart.evaluateExpression(ComponentId.get(component), component.config)
     const states = (markArea.states && !Array.isArray(markArea.states) ? [markArea.states] : markArea.states) || ['ON', 'OPEN']
-    const itemPoints = points.find(p => p.name === markArea.item).data
+    const itemPoints = points.find((p) => p.name === markArea.item).data
     markArea.data = []
     let rollingState = null
     let currentArea = null
     itemPoints.forEach((p) => {
-      // eslint-disable-next-line eqeqeq
       if (states.findIndex((s) => s == p.state) >= 0 && states.findIndex((s) => s == rollingState) < 0) {
         currentArea = [{ name: markArea.name, xAxis: new Date(p.time) }]
-      // eslint-disable-next-line eqeqeq
       } else if ((states.findIndex((s) => s == p.state) < 0 && states.findIndex((s) => s == rollingState) >= 0) && currentArea) {
         currentArea.push({ xAxis: new Date(p.time) })
         currentArea[0].name = markArea.name || markArea.item

bundles/org.openhab.ui/web/src/components/widgets/system/oh-chart-component.vue
@@ -3,12 +3,12 @@
     <chart
       v-if="ready"
       ref="chart"
-      :init-options="initOptions"
+      :initOptions="initOptions"
       :option="options"
       class="oh-chart"
       @click="handleClick"
       :class="{ 'with-tabbar': context.tab, 'with-toolbar': context.analyzer }"
-      :theme="$f7.data.themeOptions.dark === 'dark' ? 'dark' : undefined"
+      :theme="uiOptionsStore.getDarkMode() === 'dark' ? 'dark' : undefined"
       autoresize />
     <f7-menu class="padding float-right" v-if="periodVisible">
       <f7-menu-item @click="earlierPeriod()" icon-f7="chevron_left" />
@@ -42,10 +42,13 @@
 </style>
 
 <script>
+import { f7 } from 'framework7-vue'
+import { nextTick } from 'vue'
+import { mapStores } from 'pinia'
+
 import mixin from '../widget-mixin'
 import chart from '../chart/chart-mixin'
 import { actionsMixin } from '../widget-actions'
-import i18n from '@/js/i18n'
 
 import dayjs from 'dayjs'
 import LocalizedFormat from 'dayjs/plugin/localizedFormat'
@@ -54,34 +57,36 @@ dayjs.extend(LocalizedFormat)
 
 import { use, registerLocale } from 'echarts/core'
 import { CanvasRenderer } from 'echarts/renderers'
+import { useUIOptionsStore } from '@/js/stores/useUIOptionsStore'
+
 import { LineChart, BarChart, GaugeChart, HeatmapChart, PieChart, ScatterChart, CustomChart } from 'echarts/charts'
 import { LabelLayout } from 'echarts/features'
 import {
   TitleComponent, LegendComponent, LegendScrollComponent, GridComponent, SingleAxisComponent, ToolboxComponent, TooltipComponent,
   DataZoomComponent, MarkLineComponent, MarkPointComponent, MarkAreaComponent, VisualMapComponent, CalendarComponent
 } from 'echarts/components'
+
+import 'echarts/theme/dark.js'
+
 import VChart from 'vue-echarts'
 
 use([CanvasRenderer, LineChart, BarChart, GaugeChart, HeatmapChart, PieChart, ScatterChart, CustomChart, TitleComponent,
   LegendComponent, LegendScrollComponent, GridComponent, SingleAxisComponent, ToolboxComponent, TooltipComponent, DataZoomComponent,
   MarkLineComponent, MarkPointComponent, MarkAreaComponent, VisualMapComponent, CalendarComponent, LabelLayout])
 
-let echartsLocale = i18n.locale.split('-')[0].toUpperCase()
-
-import(`echarts/i18n/lang${echartsLocale}-obj`)
-  .then(lang => {
-    console.info(`Registering ECharts locale ${echartsLocale}`)
-    registerLocale(echartsLocale, lang.default)
-  })
-  .catch(() => {
-    console.warn(`No ECharts locale found for ${echartsLocale}`)
-    echartsLocale = undefined
-  })
+import { useRuntimeStore } from '@/js/stores/useRuntimeStore'
 
 export default {
   mixins: [mixin, chart, actionsMixin],
   components: {
     chart: VChart
+  },
+  setup () {
+    let echartsLocale = useRuntimeStore().locale.split('-')[0].toUpperCase()
+    let initOptions = echartsLocale ? {
+      locale: echartsLocale
+    } : null
+    return { echartsLocale, initOptions }
   },
   computed: {
     activeHeight () {
@@ -116,11 +121,7 @@ export default {
           return startTime.format('ll')
       }
     },
-    initOptions () {
-      return echartsLocale ? {
-        locale: echartsLocale
-      } : undefined
-    }
+    ...mapStores(useUIOptionsStore, useRuntimeStore)
   },
   data () {
     return {
@@ -129,9 +130,21 @@ export default {
     }
   },
   mounted () {
-    this.ready = true
+    // echarts localisation for EN and ZH are already included
+    if(['EN', 'ZH'].includes(this.echartsLocale)) {
+      this.ready = true
+    } else {
+      import(`../../../../node_modules/echarts/lib/i18n/lang${this.echartsLocale}.js`).then((lang) => {
+        registerLocale(this.echartsLocale, lang.default)
+        console.log('echart localisation loaded: ', this.echartsLocale)
+      }).catch(() => {
+        console.log('echart localisation loading failed: ', this.echartsLocale)
+      }).finally(() => {
+        this.ready = true
+      })
+    }
   },
-  beforeDestroy () {
+  beforeUnmount () {
     if (this.calendarPicker) this.calendarPicker.destroy()
   },
   methods: {
@@ -146,7 +159,7 @@ export default {
     pickFixedStartDate (evt) {
       const self = this
       const value = this.startTime.toDate()
-      this.calendarPicker = this.$f7.calendar.create({
+      this.calendarPicker = f7.calendar.create({
         inputEl: this.$refs.calendarInput,
         value: [value],
         on: {
@@ -161,7 +174,7 @@ export default {
     },
     forceRerender () {
       this.ready = false
-      this.$nextTick(() => {
+      nextTick(() => {
         this.ready = true
       })
     }

commit 9e3d487a6d7673ea1b3c5bb7f40625a0d9455ab3

Fix global dayjs locale setup (#3488)
Regression from #3350.
Addresses setting locale on initial loading of the app.

---------

Also-by: Florian Hotze <dev@florianhotze.com>
Signed-off-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/App.vue
@@ -522,6 +522,9 @@ import { request } from 'framework7'
 import { f7, f7ready } from 'framework7-vue'
 import { mapStores } from 'pinia'
 
+import dayjs from 'dayjs'
+import dayjsLocales from 'dayjs/locale.json'
+
 import buildInfo from '@/assets/build-info'
 
 import routes from '@/js/routes.js'
@@ -535,8 +538,7 @@ import connectionHealth from '@/components/connection-health-mixin'
 import sseEvents from '@/components/sse-events-mixin'
 
 import { i18n, loadLocaleMessages } from '@/js/i18n'
-import dayjs from 'dayjs'
-import dayjsLocales from 'dayjs/locale.json'
+
 import { useI18n } from 'vue-i18n'
 
 import { AddonIcons, AddonTitles } from '@/assets/addon-store'
@@ -794,14 +796,16 @@ export default {
           let dayjsLocalePromise = Promise.resolve(null)
           // try to resolve the dayjs file to load if it exists
           if (locale) {
-            const dayjsLocale = dayjsLocales.find(
-              (l) => l.key === locale || l.key === locale.split('-')[0]
+            let dayjsLocale = dayjsLocales.find(
+              (l) => l.key === locale || l.key === locale.toLowerCase() || l.key === locale.split('-')[0]
             )
+            // fix for missing definitions in en.js locale, see https://github.com/iamkun/dayjs/blob/dev/src/locale/en.js
+            if (dayjsLocale.key === 'en') dayjsLocale = dayjsLocales.find((l) => l.key === 'en-gb')
 
             dayjsLocalePromise = dayjsLocale
               ? import(`../node_modules/dayjs/esm/locale/${dayjsLocale.key}.js`)
-                .then(() => {
-                  return dayjsLocale
+                .then((data) => {
+                  return data.default
                 }).catch((error) => {
                   console.error('Error fetching dayjs: ', error, dayjsLocale)
                 })
@@ -826,8 +830,10 @@ export default {
             })
           this.updateTitle()
 
-          if (data[2]) dayjs.locale(data[2].key)
-
+          if (data[2]) {
+            dayjs.locale(data[2], null, false)
+            console.log('dayjs locale set to', dayjs.locale())
+          }
           // load & build the semantic model
           useModelStore().loadSemanticModel()
         })

bundles/org.openhab.ui/web/src/components/widgets/chart/misc/oh-chart-tooltip.js
@@ -1,5 +1,4 @@
 import dayjs from 'dayjs'
-import 'dayjs/locale/de'
 import LocalizedFormat from 'dayjs/plugin/localizedFormat'
 import ComponentId from '../../component-id'
 

bundles/org.openhab.ui/web/src/components/widgets/chart/series/oh-mark-area.js
@@ -1,8 +1,4 @@
-import dayjs from 'dayjs'
-import LocalizedFormat from 'dayjs/plugin/localizedFormat'
 import ComponentId from '../../component-id'
-
-dayjs.extend(LocalizedFormat)
 
 export default {
   get (component, points, startTime, endTime, chart) {

bundles/org.openhab.ui/web/src/components/widgets/system/oh-chart-component.vue
@@ -105,20 +105,25 @@ export default {
     fixedPeriodLabel () {
       const startTime = this.startTime
       if (!this.startTime) return ''
-      switch (this.context.component.config.chartType) {
-        case 'hour':
-          return startTime.format('lll')
-        case 'day':
-          return startTime.format('ll')
-        case 'week':
-        case 'isoWeek':
-          return startTime.format('ll')
-        case 'month':
-          return startTime.format('MMM YYYY')
-        case 'year':
-          return startTime.format('YYYY')
-        default:
-          return startTime.format('ll')
+      try {
+        switch (this.context.component.config.chartType) {
+          case 'hour':
+            return startTime.format('lll')
+          case 'day':
+            return startTime.format('ll')
+          case 'week':
+          case 'isoWeek':
+            return startTime.format('ll')
+          case 'month':
+            return startTime.format('MMM YYYY')
+          case 'year':
+            return startTime.format('YYYY')
+          default:
+            return startTime.format('ll')
+        }
+      } catch (e) {
+        console.error('Error formatting fixed period label: ', e)
+        return startTime
       }
     },
     ...mapStores(useUIOptionsStore, useRuntimeStore)