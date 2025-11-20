openhab/openhab-webui

commit 9e3d487a6d7673ea1b3c5bb7f40625a0d9455ab3

Fix global dayjs locale setup (#3488)
Regression from #3350.
Addresses setting locale on initial loading of the app.

---------

Also-by: Florian Hotze <dev@florianhotze.com>
Signed-off-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/App.vue
@@ -522,6 +522,9 @@ import { request } from 'framework7'
 import { f7, f7ready } from 'framework7-vue'
 import { mapStores } from 'pinia'
 
+import dayjs from 'dayjs'
+import dayjsLocales from 'dayjs/locale.json'
+
 import buildInfo from '@/assets/build-info'
 
 import routes from '@/js/routes.js'
@@ -535,8 +538,7 @@ import connectionHealth from '@/components/connection-health-mixin'
 import sseEvents from '@/components/sse-events-mixin'
 
 import { i18n, loadLocaleMessages } from '@/js/i18n'
-import dayjs from 'dayjs'
-import dayjsLocales from 'dayjs/locale.json'
+
 import { useI18n } from 'vue-i18n'
 
 import { AddonIcons, AddonTitles } from '@/assets/addon-store'
@@ -794,14 +796,16 @@ export default {
           let dayjsLocalePromise = Promise.resolve(null)
           // try to resolve the dayjs file to load if it exists
           if (locale) {
-            const dayjsLocale = dayjsLocales.find(
-              (l) => l.key === locale || l.key === locale.split('-')[0]
+            let dayjsLocale = dayjsLocales.find(
+              (l) => l.key === locale || l.key === locale.toLowerCase() || l.key === locale.split('-')[0]
             )
+            // fix for missing definitions in en.js locale, see https://github.com/iamkun/dayjs/blob/dev/src/locale/en.js
+            if (dayjsLocale.key === 'en') dayjsLocale = dayjsLocales.find((l) => l.key === 'en-gb')
 
             dayjsLocalePromise = dayjsLocale
               ? import(`../node_modules/dayjs/esm/locale/${dayjsLocale.key}.js`)
-                .then(() => {
-                  return dayjsLocale
+                .then((data) => {
+                  return data.default
                 }).catch((error) => {
                   console.error('Error fetching dayjs: ', error, dayjsLocale)
                 })
@@ -826,8 +830,10 @@ export default {
             })
           this.updateTitle()
 
-          if (data[2]) dayjs.locale(data[2].key)
-
+          if (data[2]) {
+            dayjs.locale(data[2], null, false)
+            console.log('dayjs locale set to', dayjs.locale())
+          }
           // load & build the semantic model
           useModelStore().loadSemanticModel()
         })

bundles/org.openhab.ui/web/src/components/widgets/chart/misc/oh-chart-tooltip.js
@@ -1,5 +1,4 @@
 import dayjs from 'dayjs'
-import 'dayjs/locale/de'
 import LocalizedFormat from 'dayjs/plugin/localizedFormat'
 import ComponentId from '../../component-id'
 

bundles/org.openhab.ui/web/src/components/widgets/chart/series/oh-mark-area.js
@@ -1,8 +1,4 @@
-import dayjs from 'dayjs'
-import LocalizedFormat from 'dayjs/plugin/localizedFormat'
 import ComponentId from '../../component-id'
-
-dayjs.extend(LocalizedFormat)
 
 export default {
   get (component, points, startTime, endTime, chart) {

bundles/org.openhab.ui/web/src/components/widgets/system/oh-chart-component.vue
@@ -105,20 +105,25 @@ export default {
     fixedPeriodLabel () {
       const startTime = this.startTime
       if (!this.startTime) return ''
-      switch (this.context.component.config.chartType) {
-        case 'hour':
-          return startTime.format('lll')
-        case 'day':
-          return startTime.format('ll')
-        case 'week':
-        case 'isoWeek':
-          return startTime.format('ll')
-        case 'month':
-          return startTime.format('MMM YYYY')
-        case 'year':
-          return startTime.format('YYYY')
-        default:
-          return startTime.format('ll')
+      try {
+        switch (this.context.component.config.chartType) {
+          case 'hour':
+            return startTime.format('lll')
+          case 'day':
+            return startTime.format('ll')
+          case 'week':
+          case 'isoWeek':
+            return startTime.format('ll')
+          case 'month':
+            return startTime.format('MMM YYYY')
+          case 'year':
+            return startTime.format('YYYY')
+          default:
+            return startTime.format('ll')
+        }
+      } catch (e) {
+        console.error('Error formatting fixed period label: ', e)
+        return startTime
       }
     },
     ...mapStores(useUIOptionsStore, useRuntimeStore)

commit 0c09e0ac933ae916e53f1382625d94b2002a87d8

Fix JS error if no dayjs locale found
Regression from #3488.
Fixes https://github.com/openhab/openhab-webui/discussions/3381#discussioncomment-15020025.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/App.vue
@@ -800,7 +800,7 @@ export default {
               (l) => l.key === locale || l.key === locale.toLowerCase() || l.key === locale.split('-')[0]
             )
             // fix for missing definitions in en.js locale, see https://github.com/iamkun/dayjs/blob/dev/src/locale/en.js
-            if (dayjsLocale.key === 'en') dayjsLocale = dayjsLocales.find((l) => l.key === 'en-gb')
+            if (dayjsLocale?.key === 'en') dayjsLocale = dayjsLocales.find((l) => l.key === 'en-gb')
 
             dayjsLocalePromise = dayjsLocale
               ? import(`../node_modules/dayjs/esm/locale/${dayjsLocale.key}.js`)