openhab/openhab-webui

commit c1ff7dcf99e0600f029623c992a7c53eecac9ecc

Initial port to Vue3, vite, Framework7 v7.1.5, pinia stores, codemirror 6, vue-i18n v11, ... (#3350)
Closes #2151.

Fully functioning release.
There are some things left to do, see #3378.

Please report issues in this GitHub discussion:
https://github.com/openhab/openhab-webui/discussions/3381
We will then decided how critical the reported bug issue and depending
on that, fix it right now or put it on the to do lost:
https://github.com/openhab/openhab-webui/issues/3378

---------

Also-by: Florian Hotze <dev@florianhotze.com>
Signed-off-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/css/app.styl
@@ -2,18 +2,85 @@
 html
   overscroll-behavior-y contain
 
-/* Fixes for desktops */
-.device-desktop
+/* Fixes & adjustments for Aurora theme */
+.aurora
+  --f7-label-line-height 1.3
+  --f7-searchbar-input-font-size 13px
+  --f7-button-font-size 14px
+  --f7-chip-height 24px
+  --f7-chip-font-size 13px
+  --f7-menu-font-size 13px
+  --f7-block-margin-vertical 15px
+  --f7-inline-label-font-size 14px
+  --f7-input-font-size 14px
+  --f7-button-height 28px
+  --f7-input-height 24px
+  // list
+  --f7-list-item-header-line-height 1.3
+  --f7-list-item-after-line-height 1.3
+  --f7-list-media-item-padding-vertical 5px
+  --f7-list-font-size 14px
+  --f7-list-margin-vertical 16px
+  --f7-list-item-min-height 32px
+  --f7-list-group-title-height 19px
+  .media-list, li.media-item
+  .list .item-inner
+    --f7-list-item-padding-vertical 5px
+    --f7-typography-margin 0
+    .toggle
+       margin-top 0 !important
+       margin-bottom 0 !important
+  // navbar
   --f7-navbar-title-line-height 1.3
   --f7-navbar-subtitle-line-height 1.3
   --f7-subnavbar-title-line-height 1.3
-  --f7-list-item-header-line-height 1.3
-  --f7-label-line-height 1.3
-  --f7-list-item-after-line-height 1.3
+  --f7-navbar-font-size 15px
+  --f7-navbar-large-title-font-size 26px
+  --f7-navbar-height 38px
+  // tabbar
+  --f7-tabbar-labels-height 44px
+  --f7-tabbar-icon-size 20px
+  --f7-tabbar-label-font-size 12px
+  // toolbar
+  --f7-toolbar-font-size 14px
+  --f7-toolbar-height 44px
+  // icons
+  .f7-icons
+     font-size 20px
 
+/* Fixes for desktop devices */
+.device-desktop
   // disable all searchbar backdrops on desktop devices
   .searchbar-backdrop
     display none
+
+/* Adjustments for iOS & Material Design themes */
+.ios
+.md
+  --f7-list-margin-vertical 20px
+  li.media-item
+    --f7-typography-margin 0
+    --f7-list-item-padding-vertical 10px
+
+/* Adjustments for cards */
+:root
+  --f7-card-margin-vertical 10px
+  --f7-card-margin-horizontal 10px
+
+/* Fixes for navbar title positioning */
+.navbar-inner.navbar-inner-centered-title
+.ios & .navbar-inner.sliding
+  display flex
+  justify-content center !important
+  .left
+    position absolute
+    left var(--f7-navbar-inner-padding-left)
+  .title
+    left 0 !important
+    right 0 !important
+  .right
+    position absolute
+    right var(--f7-navbar-inner-padding-right)
 
 .no-page-transitions
   .smart-select-popup.popup.modal-in, .smart-select-popup.popup.modal-out
@@ -75,7 +142,7 @@ html
   --f7-navbar-subtitle-text-color rgba(255,255,255,0.85)
   --f7-navbar-border-color transparent
   --f7-sheet-border-color transparent
-  .searchbar
+  .searchbar & :not(.dark)
     --f7-searchbar-input-bg-color #fff
     input
       --f7-searchbar-input-text-color #000
@@ -98,9 +165,6 @@ html
   --f7-page-bg-color-rgb rgb(250, 250, 250)
   .list-card
     box-shadow var(--f7-elevation-1)
-
-.home-tabs
-  --f7-theme-color #2196f3
 
 .navbar-large-hidden
   --f7-navbar-large-collapse-progress 1
@@ -125,10 +189,10 @@ html
 .md .title-large-text
   font-weight bold !important
 
-:root .theme-dark
+:root .dark
   color-scheme dark
 
-.theme-dark .home-tabs
+.dark .home-tabs
   --f7-bars-bg-color rgb(32, 32, 33)
   --f7-bars-text-color #fff
   --f7-bars-link-color #fff
@@ -260,11 +324,19 @@ html
   text-align center
   line-height 32px
 
-#framework7-root:not(.theme-dark) .page-home:not(.standard-background),
-#framework7-root:not(.theme-dark) .page-about:not(.standard-background),
-#framework7-root:not(.theme-dark) .page-addon-store:not(.standard-background),
-#framework7-root:not(.theme-dark) .page-addon-details:not(.standard-background),
-//#framework7-root:not(.theme-dark) .page-settings
+// TODO-V3.1 - should be able to be deleted in favor of the below since the dark class is added to the top-level html element now
+#framework7-root:not(.dark) .page-home:not(.standard-background),
+#framework7-root:not(.dark) .page-about:not(.standard-background),
+#framework7-root:not(.dark) .page-addon-store:not(.standard-background),
+#framework7-root:not(.dark) .page-addon-details:not(.standard-background),
+  --f7-page-bg-color #fff
+//#framework7-root:not(.dark) .page-settings
+
+html:not(.dark) .page
+html:not(.dark) .page-home:not(.standard-background),
+html:not(.dark) .page-about:not(.standard-background),
+html:not(.dark) .page-addon-store:not(.standard-background),
+html:not(.dark) .page-addon-details:not(.standard-background),
   --f7-page-bg-color #fff
 
 // disable blur on card backdrop (poor performance - update: seems fixed)
@@ -328,3 +400,20 @@ html
 
 // Additional safe area fixes are in nomini.css due to CssMinimizerPlugin minimizing app.styl and removing the unit px from 0px,
 // which breaks calculcations using the overwritten CSS variables.
+
+// TODO-V3.1: Check if this actually fixes the issue, because e.g. config-parameter.vue needed its own CSS
+// Fix for grouped list indentation
+.list ul .list-group ul
+  --f7-list-in-list-padding-left 0px
+  --f7-list-item-padding-horizontal 0px
+
+.advanced-label
+  font-size var(--f7-toolbar-font-size)
+
+.advanced-label .checkbox
+  padding-left 5px
+  padding-right 5px
+  cursor pointer
+
+.advanced-label + .advanced-label
+  margin-left 5px

bundles/org.openhab.ui/web/src/pages/settings/model/model.vue
@@ -1,17 +1,13 @@
 <template>
   <f7-page name="Model"
-           :stacked="true"
            @page:afterin="onPageAfterIn"
-           @page:beforeout="onPageBeforeOut"
-           @click="selectItem(null)">
-    <f7-navbar title="Semantic Model"
-               back-link="Settings"
-               back-link-url="/settings/"
-               back-link-force>
-      <f7-nav-right>
-        <developer-dock-icon />
-      </f7-nav-right>
-      <f7-subnavbar :inner="false" v-show="initSearchbar">
+           @page:beforeout="onPageBeforeOut">
+    <f7-navbar>
+      <oh-nav-content title="Semantic Model"
+                      back-link="Settings"
+                      back-link-url="/settings/"
+                      :f7router />
+      <f7-subnavbar :inner="false" v-show="initSearchbar" style="height: var(--f7-searchbar-height)">
         <f7-searchbar
           v-if="initSearchbar"
           ref="searchbar"
@@ -19,7 +15,7 @@
           search-item=".treeview-item"
           search-in=".treeview-item-label"
           :placeholder="searchPlaceholder"
-          :disable-button="!$theme.aurora" />
+          :disable-button="!theme.aurora" />
         <div class="expand-button">
           <f7-button v-if="!expanded"
                      icon-size="24"
@@ -37,17 +33,23 @@
     </f7-navbar>
 
     <!-- Toolbar -->
-    <f7-toolbar bottom class="toolbar-details" v-if="$f7.width >= 500">
-      <f7-link :disabled="selectedItem != null" class="left" @click="selectedItem = null">
+    <f7-toolbar v-if="$f7dim.width >= 500" bottom class="toolbar-details">
+      <f7-link class="left" :class="{ disabled: selectedItem == null }" @click="selectedItem = null">
         Clear
       </f7-link>
       <div class="padding-right text-align-right">
-        <f7-checkbox :checked="includeNonSemantic" @change="toggleNonSemantic" />
-        <label @click="toggleNonSemantic" class="advanced-label">Show non-semantic</label>
-        <f7-checkbox style="margin-left: 5px" :checked="includeItemName" @change="toggleItemName" />
-        <label @click="toggleItemName" class="advanced-label">Show name</label>
-        <f7-checkbox style="margin-left: 5px" :checked="includeItemTags" @change="toggleItemTags" />
-        <label @click="toggleItemTags" class="advanced-label">Show tags</label>
+        <label class="advanced-label">
+          <f7-checkbox v-model:checked="includeNonSemantic" @change="changeNonSemantic" />
+          Show non-semantic
+        </label>
+        <label class="advanced-label">
+          <f7-checkbox v-model:checked="includeItemName" />
+          Show name
+        </label>
+        <label class="advanced-label">
+          <f7-checkbox v-model:checked="includeItemTags" />
+          Show tags
+        </label>
       </div>
       <f7-link class="right details-link padding-right"
                ref="detailsLink"
@@ -55,19 +57,25 @@
                icon-f7="chevron_up" />
     </f7-toolbar>
     <f7-toolbar v-else bottom class="toolbar-details">
-      <f7-link :disabled="selectedItem != null" class="left" @click="selectedItem = null">
+      <f7-link :class="{ disabled: selectedItem == null }" @click="selectedItem = null">
         Clear
       </f7-link>
       <div class="padding-left padding-right text-align-center" style="font-size: 12px">
         <div>
-          <f7-checkbox :checked="includeNonSemantic" @change="toggleNonSemantic" />
-          <label @click="toggleNonSemantic" class="advanced-label">Show non-semantic</label>
+          <label class="advanced-label">
+            <f7-checkbox v-model:checked="includeNonSemantic" @change="changeNonSemantic" />
+            Show non-semantic
+          </label>
         </div>
         <div>
-          <f7-checkbox :checked="includeItemName" @change="toggleItemName" />
-          <label @click="toggleItemName" class="advanced-label">Show name</label>
-          <f7-checkbox style="margin-left: 5px" :checked="includeItemTags" @change="toggleItemTags" />
-          <label @click="toggleItemTags" class="advanced-label">Show tags</label>
+          <label class="advanced-label">
+            <f7-checkbox v-model:checked="includeItemName" />
+            Show name
+          </label>
+          <label class="advanced-label">
+            <f7-checkbox v-model:checked="includeItemTags" />
+            Show tags
+          </label>
         </div>
       </div>
       <f7-link class="right details-link padding-right"
@@ -100,7 +108,7 @@
                     strong
                     class="semantic-tree"
                     no-gap
-                    @click.native="clearSelection">
+                    @click="clearSelection">
             <model-treeview :rootNodes="[rootLocations, rootEquipment, rootPoints, rootGroups, rootItems].flat()"
                             :items="items"
                             :includeItemName="includeItemName"
@@ -108,7 +116,8 @@
                             :canDragDrop="true"
                             @selected="selectItem"
                             :selected="selectedItem"
-                            @reload="load" />
+                            @reload="load"
+                            @click.stop />
           </f7-block>
         </f7-col>
         <f7-col width="100" medium="50" class="details-pane">
@@ -118,6 +127,7 @@
                                 :links="links"
                                 :items="items"
                                 :context="context"
+                                :f7router
                                 @item-updated="update"
                                 @item-created="update"
                                 @item-removed="selectItem(null)"
@@ -153,40 +163,41 @@
       </f7-row>
     </f7-block>
 
-    <f7-fab class="add-to-model-fab"
-            position="right-bottom"
-            slot="fixed"
-            color="blue"
-            v-if="!selectedItem || (selectedItem.item.created !== false && selectedItem.item.type === 'Group' && selectedItem.class.indexOf('Point_') < 0)">
-      <f7-icon ios="f7:plus" md="material:add" aurora="f7:plus" />
-      <f7-icon ios="f7:multiply" md="material:close" aurora="f7:multiply" />
-      <f7-fab-buttons position="top">
-        <f7-fab-button v-if="includeNonSemantic"
-                       fab-close
-                       label="Add Item"
-                       @click="addNonSemanticItem(false)">
-          <f7-icon ios="material:label_outline" md="material:label_outline" aurora="material:label_outline" />
-        </f7-fab-button>
-        <f7-fab-button fab-close label="Add Point" @click="addSemanticItem('Point')">
-          <f7-icon ios="f7:bolt_fill" md="material:flash_on" aurora="f7:bolt_fill" />
-        </f7-fab-button>
-        <f7-fab-button fab-close label="Add Equipment" @click="addSemanticItem('Equipment')">
-          <f7-icon ios="f7:cube_box_fill" md="material:payments" aurora="f7:cube_box_fill" />
-        </f7-fab-button>
-        <f7-fab-button fab-close label="Create Points from Thing" @click="addFromThing(false)">
-          <f7-icon ios="f7:layers" md="material:layers" aurora="f7:layers" />
-        </f7-fab-button>
-        <f7-fab-button fab-close label="Create Equipment from Thing" @click="addFromThing(true)">
-          <f7-icon ios="f7:layers_fill" md="material:layers" aurora="f7:layers_fill" />
-        </f7-fab-button>
-        <f7-fab-button v-show="!selectedItem || selectedItem.class.indexOf('Location') === 0"
-                       fab-close
-                       label="Add Location"
-                       @click="addSemanticItem('Location')">
-          <f7-icon ios="f7:placemark_fill" md="material:place" aurora="f7:placemark_fill" />
-        </f7-fab-button>
-      </f7-fab-buttons>
-    </f7-fab>
+    <template #fixed>
+      <f7-fab v-if="!selectedItem || (selectedItem.item.created !== false && selectedItem.item.type === 'Group' && selectedItem.class.indexOf('Point_') < 0)"
+              class="add-to-model-fab"
+              position="right-bottom"
+              color="blue">
+        <f7-icon ios="f7:plus" md="material:add" aurora="f7:plus" />
+        <f7-icon ios="f7:multiply" md="material:close" aurora="f7:multiply" />
+        <f7-fab-buttons position="top">
+          <f7-fab-button v-if="includeNonSemantic"
+                         fab-close
+                         label="Add Item"
+                         @click="addNonSemanticItem(false)">
+            <f7-icon ios="material:label_outline" md="material:label_outline" aurora="material:label_outline" />
+          </f7-fab-button>
+          <f7-fab-button fab-close label="Add Point" @click="addSemanticItem('Point')">
+            <f7-icon ios="f7:bolt_fill" md="material:flash_on" aurora="f7:bolt_fill" />
+          </f7-fab-button>
+          <f7-fab-button fab-close label="Add Equipment" @click="addSemanticItem('Equipment')">
+            <f7-icon ios="f7:cube_box_fill" md="material:payments" aurora="f7:cube_box_fill" />
+          </f7-fab-button>
+          <f7-fab-button fab-close label="Create Points from Thing" @click="addFromThing(false)">
+            <f7-icon ios="f7:layers" md="material:layers" aurora="f7:layers" />
+          </f7-fab-button>
+          <f7-fab-button fab-close label="Create Equipment from Thing" @click="addFromThing(true)">
+            <f7-icon ios="f7:layers_fill" md="material:layers" aurora="f7:layers_fill" />
+          </f7-fab-button>
+          <f7-fab-button v-show="!selectedItem || selectedItem.class.indexOf('Location') === 0"
+                         fab-close
+                         label="Add Location"
+                         @click="addSemanticItem('Location')">
+            <f7-icon ios="f7:placemark_fill" md="material:place" aurora="f7:placemark_fill" />
+          </f7-fab-button>
+        </f7-fab-buttons>
+      </f7-fab>
+    </template>
 
     <f7-sheet class="model-details-sheet"
               :backdrop="false"
@@ -219,13 +230,16 @@
                         :model="selectedItem"
                         :links="links"
                         :items="items"
+                        :context="context"
                         @item-updated="update"
                         @item-created="update"
                         @item-removed="selectItem(null)"
-                        @cancel-create="selectItem(null)"
-                        :context="context" />
-          <metadata-menu v-if="detailsTab === 'meta'" :item="selectedItem.item" />
-          <link-details v-if="detailsTab === 'links'" :item="selectedItem.item" :links="links" />
+                        @cancel-create="selectItem(null)" />
+          <metadata-menu v-if="detailsTab === 'meta'" :item="selectedItem.item" :f7router />
+          <link-details v-if="detailsTab === 'links'"
+                        :item="selectedItem.item"
+                        :links="links"
+                        :f7router />
         </f7-block>
         <f7-block v-else>
           <div class="padding text-align-center">
@@ -295,6 +309,7 @@
     padding-right 0px
 
 .expand-button
+  height unset
   margin-right 8px
   text-overflow unset
   align-self center
@@ -303,11 +318,16 @@
 </style>
 
 <script>
-import ModelDetailsPane from '@/components/model/details-pane.vue'
-import ModelTreeview from '@/components/model/model-treeview.vue'
+import { nextTick } from 'vue'
+import { utils } from 'framework7'
+import { f7, theme } from 'framework7-vue'
+import { mapWritableState } from 'pinia'
+
 import AddFromThing from './add-from-thing.vue'
 import AddFromTemplate from './add-from-template.vue'
 
+import ModelDetailsPane from '@/components/model/details-pane.vue'
+import ModelTreeview from '@/components/model/model-treeview.vue'
 import ItemStatePreview from '@/components/item/item-state-preview.vue'
 import ItemDetails from '@/components/model/item-details.vue'
 import MetadataMenu from '@/components/item/metadata/item-metadata-menu.vue'
@@ -316,7 +336,14 @@ import LinkDetails from '@/components/model/link-details.vue'
 import ModelMixin from '@/pages/settings/model/model-mixin'
 import EmptyStatePlaceholder from '@/components/empty-state-placeholder.vue'
 
+import { useRuntimeStore } from '@/js/stores/useRuntimeStore'
+import { useStatesStore } from '@/js/stores/useStatesStore'
+import { useLastSearchQueryStore } from '@/js/stores/useLastSearchQueryStore'
+
 export default {
+  props: {
+    f7router: Object
+  },
   mixins: [ModelMixin],
   components: {
     EmptyStatePlaceholder,
@@ -327,19 +354,20 @@ export default {
     MetadataMenu,
     LinkDetails
   },
-  data () {
-    if (!this.$f7.data.model) this.$f7.data.model = {}
+  setup () {
     return {
-      includeItemName: this.$f7.data.model.includeItemName || false,
-      includeItemTags: this.$f7.data.model.includeItemTags || false,
-      expanded: this.$f7.data.model.expanded || false,
+      f7, theme
+    }
+  },
+  data () {
+    return {
       newItem: null,
       newItemParent: null,
       initSearchbar: false,
       detailsOpened: false,
       detailsTab: 'state',
       eventSource: null,
-      itemDetailsKey: this.$f7.utils.id()
+      itemDetailsKey: utils.id()
     }
   },
   computed: {
@@ -349,16 +377,22 @@ export default {
     },
     context () {
       return {
-        store: this.$store.getters.trackedItems
+        store: useStatesStore().trackedItems
       }
     },
     searchPlaceholder () {
       return window.innerWidth >= 1280 ? 'Search (for advanced search, use the developer sidebar (Shift+Alt+D))' : 'Search'
-    }
+    },
+    ...mapWritableState(useRuntimeStore, {
+      includeItemName: 'modelPicker.includeItemName',
+      includeItemTags: 'modelPicker.includeItemTags',
+      includeNonSemantic: 'modelPicker.includeNonSemantic',
+      expanded: 'modelPicker.expanded'
+    })
   },
   methods: {
     onPageAfterIn () {
-      this.$store.dispatch('startTrackingStates')
+      useStatesStore().startTrackingStates()
       if (this.selectedItem) {
         this.update()
       } else {
@@ -367,9 +401,9 @@ export default {
     },
     onPageBeforeOut () {
       this.detailsOpened = false
-      this.$store.dispatch('stopTrackingStates')
+      useStatesStore().stopTrackingStates()
       this.stopEventSource()
-      this.$f7.data.lastModelSearchQuery = this.$refs.searchbar?.f7Searchbar.query
+      useLastSearchQueryStore().lastModelSearchQuery = this.$refs.searchbar?.$el.f7Searchbar.query
     },
     modelItem (item) {
       const modelItem = {
@@ -397,16 +431,16 @@ export default {
       return modelItem
     },
     load () {
-      if (this.initSearchbar) this.$f7.data.lastModelSearchQuery = this.$refs.searchbar?.f7Searchbar.query
+      if (this.initSearchbar) useLastSearchQueryStore().lastModelSearchQuery = this.$refs.searchbar?.$el.f7Searchbar.query
       this.initSearchbar = false
 
       this.loadModel().then(() => {
         this.initSearchbar = true
-        this.$nextTick(() => {
+        nextTick(() => {
           if (this.$device.desktop && this.$refs.searchbar) {
-            this.$refs.searchbar.f7Searchbar.$inputEl[0].focus()
+            this.$refs.searchbar.$el.f7Searchbar.$inputEl[0].focus()
           }
-          this.$refs.searchbar?.f7Searchbar.search(this.$f7.data.lastModelSearchQuery || '')
+          this.$refs.searchbar?.$el.f7Searchbar.search(useLastSearchQueryStore().lastModelSearchQuery || '')
           this.restoreExpanded()
         })
         if (!this.eventSource) this.startEventSource()
@@ -454,25 +488,13 @@ export default {
         this.detailsOpened = false
       }
     },
-    toggleNonSemantic () {
+    changeNonSemantic () {
       this.rootGroups = []
       this.rootItems = []
-      this.includeNonSemantic = !this.includeNonSemantic
-      this.load()
-    },
-    toggleItemName () {
-      this.includeItemName = !this.includeItemName
-      this.$f7.data.model.includeItemName = this.includeItemName
-      this.load()
-    },
-    toggleItemTags () {
-      this.includeItemTags = !this.includeItemTags
-      this.$f7.data.model.includeItemTags = this.includeItemTags
       this.load()
     },
     toggleExpanded () {
       this.expanded = !this.expanded
-      this.$f7.data.model.expanded = this.expanded
       this.applyExpandedOption()
     },
     addSemanticItem (semanticType) {
@@ -532,7 +554,7 @@ export default {
       this.load()
     },
     addFromThing (createEquipment) {
-      this.$f7router.navigate({
+      this.f7router.navigate({
         url: 'add-thing',
         route: {
           component: AddFromThing,
@@ -552,7 +574,7 @@ export default {
       })
     },
     addFromLocationTemplate () {
-      this.$f7router.navigate({
+      this.f7router.navigate({
         url: 'add-template',
         route: {
           component: AddFromTemplate,

commit 05aa2f13ea6a038cb43d6776cb70027791c09cac

Set searchbar height in Aurora theme to subnavbar height (#3492)
Regression from #3350.

This fixes CSS issues where the searchbar was higher than the subnavbar,
e.g. on the model page.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/css/app.styl
@@ -6,6 +6,7 @@ html
 .aurora
   --f7-label-line-height 1.3
   --f7-searchbar-input-font-size 13px
+  --f7-searchbar-height var(--f7-subnavbar-height)
   --f7-button-font-size 14px
   --f7-chip-height 24px
   --f7-chip-font-size 13px

bundles/org.openhab.ui/web/src/pages/settings/model/model.vue
@@ -7,7 +7,7 @@
                       back-link="Settings"
                       back-link-url="/settings/"
                       :f7router />
-      <f7-subnavbar :inner="false" v-show="initSearchbar" style="height: var(--f7-searchbar-height)">
+      <f7-subnavbar :inner="false" v-show="initSearchbar">
         <f7-searchbar
           v-if="initSearchbar"
           ref="searchbar"