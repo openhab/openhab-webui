af0475d4fe4e9211e7feb64483846a0e3e08ce9c
Mark Herwege <mark.herwege@telenet.be>
2025-11-21 17:28:09 +0100

    refactor config sheet advanced toggle
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

--- bundles/org.openhab.ui/web/src/components/config/config-sheet.vue
+++ bundles/org.openhab.ui/web/src/components/config/config-sheet.vue
@@ -4,6 +4,13 @@
       <label class="advanced-label">
         <f7-checkbox v-model:checked="showAdvanced" />
         Show advanced
+        <f7-badge v-if="advancedNonDefaultCount"
+                  style="margin-left:2px"
+                  color="blue"
+                  class="count-badge"
+                  tooltip="Non-default advanced parameter">
+          {{ advancedNonDefaultCount }}
+        </f7-badge>
       </label>
     </div>
     <f7-col>
@@ -19,6 +26,7 @@
               :configuration="configurationWithDefaults"
               :read-only="readOnly"
               :status="parameterStatus(parameter)"
+              :showNonDefaultBadge="parameter.advanced && isNonDefault(parameter)"
               :f7router
               @update="(value) => updateParameter(parameter, value)" />
           </f7-col>
@@ -48,6 +56,7 @@
               :configuration="configurationWithDefaults"
               :read-only="readOnly"
               :status="parameterStatus(parameter)"
+              :showNonDefaultBadge="parameter.advanced && isNonDefault(parameter)"
               :f7router
               @update="(value) => updateParameter(parameter, value)" />
           </f7-col>
@@ -129,11 +138,7 @@ export default {
       if (this.showAdvanced) return this.parameterGroups
       return this.parameterGroups.filter((pg) => !pg.advanced)
     },
-    displayedParameters () {
-      function notNullNotUndefined (value) {
-        return value !== null && value !== undefined
-      }
-
+    allParameters () {
       if (!this.parameters.length) return []
       let finalParameters = [...this.parameters]
       if (this.parameterGroups && this.parameterGroups.some((g) => g.context === 'action')) {
@@ -142,14 +147,20 @@ export default {
           finalParameters = [...finalParameters, ...actionParams(g.name, prefix)]
         })
       }
-      if (this.showAdvanced) return finalParameters // show all parameters
-      // exclude advanced parameters:
-      return finalParameters.filter((p) =>
-        // parameter is not advanced: always show
-        !p.advanced ||
-        // parameter is advanced: show only if default is defined and value is different from default
-        (notNullNotUndefined(p.default) && notNullNotUndefined(this.configuration[p.name]) && this.configuration[p.name].toString() !== p.default)
-      )
+      return finalParameters
+    },
+    baseParameters () {
+      return this.allParameters.filter((p) => !p.advanced)
+    },
+    advancedParameters () {
+      return this.allParameters.filter((p) => p.advanced)
+    },
+    advancedNonDefaultCount () {
+      return this.advancedParameters.filter((p) => this.isNonDefault(p)).length
+    },
+    displayedParameters () {
+      if (this.showAdvanced) return this.allParameters // show all parameters
+      return this.baseParameters
     }
   },
   methods: {
@@ -173,6 +184,12 @@ export default {
     parameterStatus (parameter) {
       if (!this.status || !this.status.length) return null
       return this.status.find((ps) => ps.parameterName === parameter.name)
+    },
+    isNonDefault (parameter) {
+      function notNullNotUndefined (value) {
+        return value !== null && value !== undefined
+      }
+      return notNullNotUndefined(parameter.default) && notNullNotUndefined(this.configuration[parameter.name]) && this.configuration[parameter.name].toString() !== parameter.default
     }
   }
 }
