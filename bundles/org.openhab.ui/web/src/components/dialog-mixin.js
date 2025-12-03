0dbb901c14753820839bf64f7aee79b8e951f5f5
Miguel Álvarez <miguelwork92@gmail.com>
2025-12-03 01:16:51 +0100

    update options
    Signed-off-by: Miguel Álvarez <miguelwork92@gmail.com>

--- bundles/org.openhab.ui/web/src/components/dialog-mixin.js
+++ bundles/org.openhab.ui/web/src/components/dialog-mixin.js
@@ -1,5 +1,6 @@
 import { getAccessToken } from '@/js/openhab/auth.js'
 import { useRuntimeStore } from '@/js/stores/useRuntimeStore.js'
+import { useUIOptionsStore } from '@/js/stores/useUIOptionsStore.js'
 
 export default {
   data () {
@@ -12,13 +13,10 @@ export default {
       if (this.audioMain) {
         return
       }
-      const dialogEnabled = localStorage.getItem('openhab.ui:dialog.enabled') === 'true'
+      const { dialogEnabled, dialogIdentifier, dialogLocationItem, dialogListeningItem, dialogConnectOnWindowEvent, dialogTriggerOnConnect } = useUIOptionsStore()
       if (!dialogEnabled) {
         return
       }
-      const identifier = localStorage.getItem('openhab.ui:dialog.id') ?? 'anonymous'
-      const dialogListeningItem = localStorage.getItem('openhab.ui:dialog.listeningItem') ?? ''
-      const dialogLocationItem = localStorage.getItem('openhab.ui:dialog.locationItem') ?? ''
       import('../js/voice/audio-main.js').then(({ AudioMain }) => {
         if (this.audioMain) {
           return
@@ -42,12 +40,17 @@ export default {
           useRuntimeStore().voiceIcon = voiceIcon
         }
         updatePageIcon(false)
-        this.audioMain = new AudioMain(ohURL, getAccessToken, {
+        let forceTrigger = dialogTriggerOnConnect
+        const audioMain = this.audioMain = new AudioMain(ohURL, getAccessToken, {
           onMessage: (...args) => {
             console.debug('Voice: ' + args[0])
           },
           onRunningChange (io) {
             updatePageIcon(io.isRunning(), io.isListening(), io.isSpeaking())
+            if (forceTrigger && io.isRunning()) {
+              forceTrigger = false
+              io.sendSpot()
+            }
           },
           onListeningChange (io) {
             updatePageIcon(io.isRunning(), io.isListening(), io.isSpeaking())
@@ -56,18 +59,27 @@ export default {
             updatePageIcon(io.isRunning(), io.isListening(), io.isSpeaking())
           }
         })
-        const events = ['touchend', 'mousedown', 'keydown']
-        const startAudio = () => {
-          clean()
-          this.audioMain.initialize(identifier, dialogListeningItem, dialogLocationItem)
+        if (dialogConnectOnWindowEvent) {
+          const events = ['touchend', 'mousedown', 'keydown']
+          const startAudio = () => {
+            clean()
+            if (!this.audioMain.isInitialized()) {
+              this.audioMain.initialize(dialogIdentifier, dialogListeningItem, dialogLocationItem)
+            }
+          }
+          const clean = () => events.forEach((e) => document.body.removeEventListener(e, startAudio))
+          events.forEach((e) => document.body.addEventListener(e, startAudio, false))
         }
-        const clean = () => events.forEach((e) => document.body.removeEventListener(e, startAudio))
-        events.forEach((e) => document.body.addEventListener(e, startAudio, false))
       })
     },
     triggerDialog () {
       if (this.audioMain != null) {
-        this.audioMain.sendSpot()
+        if (this.audioMain.isInitialized()) {
+          this.audioMain.sendSpot()
+        } else {
+          const { dialogIdentifier, dialogLocationItem, dialogListeningItem } = useUIOptionsStore()
+          this.audioMain.initialize(dialogIdentifier, dialogListeningItem, dialogLocationItem)
+        }
       }
     }
   }
