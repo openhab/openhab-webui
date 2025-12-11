315f2ad5585e96f5aa78049f67c723c71babb1c4
Florian Hotze <dev@florianhotze.com>
2025-12-12 00:59:15 +0100

    media.js: Refactor to TypeScript
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- bundles/org.openhab.ui/web/src/js/openhab/media.ts
+++ bundles/org.openhab.ui/web/src/js/openhab/media.ts
@@ -1,20 +1,27 @@
 import { getBasicCredentials } from '@/js/openhab/auth'
 
-function blobToDataURL (blob) {
+export type IconType = 'svg' | 'png'
+
+/**
+ * Convert a Blob to a data URL.
+ * @param blob
+ */
+function blobToDataURL (blob: Blob): Promise<string> {
   return new Promise((resolve, reject) => {
     const reader = new FileReader()
     reader.onerror = () => reject(new Error('Failed to read blob as data URL'))
-    reader.onload = () => resolve(reader.result)
+    reader.onload = () => resolve(reader.result as string)
     reader.readAsDataURL(blob)
   })
 }
 
 /**
- * Shared helper to fetch an image if credentials exist,
- * otherwise returns the raw URL.
+ * Fetch a URL with basic authentication if needed and return the result as a data URL.
+ * If basic authentication is not needed, the URL is returned directly.
+ * @param url
  */
-async function fetchWithAuth (url) {
-  const creds = getBasicCredentials()
+async function fetchWithAuth (url: string): Promise<string> {
+  const creds: any = getBasicCredentials()
 
   // If no credentials, return the URL (async function wraps it in a Promise)
   if (!creds) return url
@@ -25,22 +32,21 @@ async function fetchWithAuth (url) {
 
   const resp = await fetch(url, { method: 'GET', headers, credentials: 'include' })
 
-  if (!resp.ok) throw new Error(resp.statusText || resp.status)
+  if (!resp.ok) throw new Error(resp.statusText || resp.status.toString())
 
   const blob = await resp.blob()
   return blobToDataURL(blob)
 }
 
 export default {
-  getIcon: (icon, format = 'svg', state, iconSet) => {
+  getIcon: (icon: string, format: IconType = 'svg', state?: string, iconSet?: string) => {
     const params = new URLSearchParams({ format, anyFormat: 'true' })
     if (state) params.append('state', state)
     if (iconSet) params.append('iconset', iconSet)
 
     return fetchWithAuth(`/icon/${icon}?${params.toString()}`)
   },
-
-  getImage: (url) => {
+  getImage: (url: string) => {
     return fetchWithAuth(url)
   }
 }
