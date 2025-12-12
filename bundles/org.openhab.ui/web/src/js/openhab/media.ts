adb4142872b1cf1bfbf46e26133299b9f496e0d9
Florian Hotze <dev@florianhotze.com>
2025-12-12 15:59:40 +0100

    Refactor `$oh.media` to TypeScript and use fetch() (#3577)
    Also remove extraneous request params from scene-configure-item-popup.vue.
    
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

---
+++ bundles/org.openhab.ui/web/src/js/openhab/media.ts
@@ -0,0 +1,52 @@
+import { getBasicCredentials } from '@/js/openhab/auth'
+
+export type IconType = 'svg' | 'png'
+
+/**
+ * Convert a Blob to a data URL.
+ * @param blob
+ */
+function blobToDataURL (blob: Blob): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.onerror = () => reject(new Error('Failed to read blob as data URL: ' + reader.error))
+    reader.onload = () => resolve(reader.result as string)
+    reader.readAsDataURL(blob)
+  })
+}
+
+/**
+ * Fetch a URL with basic authentication if needed (i.e. behind reverse proxy) and return the result as a data URL.
+ * If basic authentication is not needed, the URL is returned directly, as neither the IconServlet at `/icon` nor Jetty at `/static` require auth.
+ * @param url
+ */
+async function fetchWithAuth (url: string): Promise<string> {
+  const creds: any = getBasicCredentials()
+
+  // If no credentials, return the URL (async function wraps it in a Promise)
+  if (!creds) return url
+
+  const headers = new Headers({
+    Authorization: 'Basic ' + btoa(`${creds.id}:${creds.password}`)
+  })
+
+  const resp = await fetch(url, { method: 'GET', headers, credentials: 'include' })
+
+  if (!resp.ok) throw new Error(`HTTP ${resp.status}: ${resp.statusText || 'Unknown error'}`)
+
+  const blob = await resp.blob()
+  return blobToDataURL(blob)
+}
+
+export default {
+  getIcon: (icon: string, format: IconType = 'svg', state?: string, iconSet?: string) => {
+    const params = new URLSearchParams({ format, anyFormat: 'true' })
+    if (state) params.append('state', state)
+    if (iconSet) params.append('iconset', iconSet)
+
+    return fetchWithAuth(`/icon/${icon}?${params.toString()}`)
+  },
+  getImage: (url: string) => {
+    return fetchWithAuth(url)
+  }
+}
