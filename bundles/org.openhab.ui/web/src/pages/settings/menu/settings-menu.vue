9e703f3f75e67b237ab539851097d00f6ca6852a
Mark Herwege <mherwege@users.noreply.github.com>
2025-12-14 17:48:34 +0100

    Persistence strategies not automatically applied (#3123)
    Depends on: https://github.com/openhab/openhab-core/pull/4682
    
    This PR removes the default strategy from the persistence configuration
    and implements a health check on the persistence configuration.
    
    ---------
    
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

--- bundles/org.openhab.ui/web/src/pages/settings/menu/settings-menu.vue
+++ bundles/org.openhab.ui/web/src/pages/settings/menu/settings-menu.vue
@@ -289,6 +289,7 @@ export default {
 
       orphanLinkCount: 0,
       semanticsProblemCount: 0,
+      persistenceProblemCount: 0,
 
       advancedSystemServices: [
         'org.openhab.storage.json',
@@ -319,7 +320,7 @@ export default {
       })
     },
     healthCount () {
-      const problemCount = this.orphanLinkCount + this.semanticsProblemCount
+      const problemCount = this.orphanLinkCount + this.semanticsProblemCount + this.persistenceProblemCount
       return problemCount.toString()
     },
     ...mapStores(useComponentsStore, useRuntimeStore)
@@ -363,6 +364,8 @@ export default {
         this.$oh.api.get('/rest/links/orphans').then((data) => { this.orphanLinkCount = data.length })
       if (useRuntimeStore().apiEndpoint('items'))
         this.$oh.api.get('/rest/items/semantics/health').then((data) => { this.semanticsProblemCount = data.length })
+      if (this.$store.getters.apiEndpoint('persistence'))
+        this.$oh.api.get('/rest/persistence/persistencehealth').then((data) => { this.persistenceProblemCount = data.length })
       if (useRuntimeStore().apiEndpoint('inbox'))
         this.$oh.api.get('/rest/inbox?includeIgnored=false').then((data) => { this.inboxCount = data.filter((e) => e.flag === 'NEW').length.toString() })
       if (useRuntimeStore().apiEndpoint('things'))
