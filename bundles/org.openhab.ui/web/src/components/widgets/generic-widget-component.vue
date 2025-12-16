openhab/openhab-webui

commit 91c62a9fa4a300d21f569e5fb0014507ec582b24

Fix stylesheet not working for layout grid items (#3595)
Also fixes #1425.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/generic-widget-component.vue
@@ -5,6 +5,7 @@
     <oh-swiper v-if="componentType === 'f7-swiper'"
                v-bind="$attrs"
                :context="context"
+               :class="scopedCssUid"
                ref="component" />
 
     <component v-else-if="componentType && componentType.startsWith('f7-')"
@@ -28,9 +29,10 @@
       </template>
     </component>
     <oh-card v-else-if="componentType && componentType === 'oh-card'"
+             ref="component"
              v-bind="$attrs"
              :context="context"
-             ref="component">
+             :class="scopedCssUid">
       <template v-for="(slotComponents, slotName) in context.component.slots" :key="slotName" #[slotName]>
         <generic-widget-component v-for="(slotComponent, idx) in slotComponents"
                                   :context="childContext(slotComponent)"
@@ -40,19 +42,21 @@
     <generic-widget-component v-else-if="componentType && componentType.startsWith('widget:')"
                               ref="component"
                               v-bind="$attrs"
-                              :context="childWidgetContext()" />
+                              :context="childWidgetContext()"
+                              :class="scopedCssUid" />
     <component v-else-if="componentType && componentType.startsWith('oh-')"
                ref="component"
                v-bind="$attrs"
                :is="componentType"
-               :context="context" />
+               :context="context"
+               :class="scopedCssUid" />
     <!-- Label renders text inside <div> element -->
     <div
       v-else-if="componentType && componentType === 'Label'"
+      ref="component"
       v-bind="$attrs"
-      :class="config.class"
-      :style="config.style"
-      ref="component">
+      :class="[...config.class, scopedCssUid]"
+      :style="config.style">
       {{ config.text }}
     </div>
     <!-- Content renders text without any additional container -->
@@ -62,9 +66,9 @@
     </template>
     <pre v-else-if="componentType && componentType === 'Error'" class="text-color-red" style="white-space: pre-wrap">{{ config.error }}</pre>
     <component v-else
+               ref="component"
                :is="componentType"
-               v-bind="{ ...$attrs, ...config }"
-               ref="component">
+               v-bind="{ ...$attrs, ...config }">
       {{ config.content }}
       <template v-if="context.component.slots && context.component.slots.default">
         <generic-widget-component v-for="(slotComponent, idx) in context.component.slots.default"

bundles/org.openhab.ui/web/src/components/widgets/layout/oh-layout-page.vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="page">
+  <div ref="page" :class="scopedCssUid">
     <template v-if="!config.layoutType || config.layoutType === 'responsive'">
       <oh-block v-for="(component, idx) in context.component.slots.default"
                 v-bind="$attrs"

bundles/org.openhab.ui/web/src/components/widgets/widget-mixin.js
@@ -1,5 +1,3 @@
-// Import into widget components as a mixin!
-
 import { f7 } from 'framework7-vue'
 import { mapStores } from 'pinia'
 
@@ -10,6 +8,14 @@ import { useUserStore } from '@/js/stores/useUserStore'
 import { useComponentsStore } from '@/js/stores/useComponentsStore'
 import { useWidgetExpression } from '@/components/widgets/useWidgetExpression.ts'
 
+/**
+ * The widget-mixin must be imported as a mixin into all widget components!
+ *
+ * A few requirements have to be met by components importing this mixin:
+ * - Widget components have to add a `component` ref to their root.
+ * - Page components have to add a `page` ref to their root.
+ * - All components using this mixin have to add the `scopedCssUid` as a class to their root element.
+ */
 export default {
   props: {
     context: Object
@@ -20,6 +26,7 @@ export default {
       ctxVars: (this.context) ? this.context.ctxVars : {},
       widgetVars: {},
       varScope: null,
+      scopedCssUid: null,
       widgetExpression: useWidgetExpression()
     }
   },
@@ -84,23 +91,16 @@ export default {
   },
   mounted () {
     if (this.context?.component?.config?.stylesheet) {
-      // generic-widget-component .$el or oh-layout-page HTML element
-      const el = this.$refs.component?.$el ?? this.$refs.page
-      if (el) {
-        this.cssUid = 'scoped-' + f7.utils.id()
-
-        el.classList.add(this.cssUid)
-
-        let style = document.createElement('style')
-        style.id = this.cssUid
-        style.innerHTML = scope(this.context.component.config.stylesheet, '.' + this.cssUid)
-        document.head.appendChild(style)
-      }
+      this.scopedCssUid = 'scoped-' + f7.utils.id()
+      let style = document.createElement('style')
+      style.id = this.scopedCssUid
+      style.innerHTML = scope(this.context.component.config.stylesheet, '.' + this.scopedCssUid)
+      document.head.appendChild(style)
     }
   },
   beforeUnmount () {
-    if (this.cssUid) {
-      const scoped_stylesheet = document.getElementById(this.cssUid)
+    if (this.scopedCssUid) {
+      const scoped_stylesheet = document.getElementById(this.scopedCssUid)
       if (scoped_stylesheet) scoped_stylesheet.remove()
     }
   },

commit e06a5c46f0b279b1fbe6526d5501572aace6a81b

generic-widget-component: Fix critical regression for <Label> component
Regression from #3595.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/generic-widget-component.vue
@@ -55,7 +55,7 @@
       v-else-if="componentType && componentType === 'Label'"
       ref="component"
       v-bind="$attrs"
-      :class="[...config.class, scopedCssUid]"
+      :class="[...(Array.isArray(config.class) ? config.class : []), scopedCssUid]"
       :style="config.style">
       {{ config.text }}
     </div>