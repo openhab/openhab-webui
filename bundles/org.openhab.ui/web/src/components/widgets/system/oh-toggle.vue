openhab/openhab-webui

commit d1ce5af03d20410662f8613fd84fd85ee7488256

oh-toggle: Display UNDEF/NULL states distinctively (#3523)
Closes #3516.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/system/oh-toggle.vue
@@ -1,9 +1,25 @@
 <template>
   <f7-toggle v-bind="config"
              :checked="value ? true : null"
+             :class="['oh-toggle', (value === null ? 'unknown-state' : '')]"
              @toggle:change="onChange"
              @click.stop />
 </template>
+
+<style lang="stylus">
+.large-vertical-toggle.oh-toggle.unknown-state
+  .toggle-icon::after
+     transform rotate(90deg)
+
+.oh-toggle.unknown-state
+  .toggle-icon::after
+    content '?'
+    font-weight bold
+    color var(--f7-toggle-inactive-color)
+    display flex
+    justify-content center
+    align-items center
+</style>
 
 <script>
 import mixin from '../widget-mixin'
@@ -34,7 +50,8 @@ export default {
       if (value === 'ON') return true
       if (value === 'OFF') return false
       if (value.split(',').length === 3) return (value.split(',')[2] !== '0')
-      return (['0', 'UNDEF', 'NULL', '-'].indexOf(value.toString()) < 0)
+      if (value === 'UNDEF' || value === 'NULL' || value === '-') return null
+      return false
     }
   },
   methods: {

commit ad1980831e52fc7d3f4e9715b3e9be70c4a65cb2

oh-toggle: Fix regression for Dimmer items
Regression from #3523.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/system/oh-toggle.vue
@@ -51,7 +51,7 @@ export default {
       if (value === 'OFF') return false
       if (value.split(',').length === 3) return (value.split(',')[2] !== '0')
       if (value === 'UNDEF' || value === 'NULL' || value === '-') return null
-      return false
+      return value !== '0'
     }
   },
   methods: {