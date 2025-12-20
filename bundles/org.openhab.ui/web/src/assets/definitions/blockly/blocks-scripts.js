openhab/openhab-webui

commit 20db04f6fbc6ffed667d3b42ddc8bc0e2b64ccba

[blockly] change event attributes, add return block (#3376)
Depends on: https://github.com/openhab/openhab-addons/pull/19443

The event object/attributes in script actions and conditions is now the
same as in JSRule and Rule Builder.
Backwards compatibility will be retained for some time, though it is
advised to adjust, which is done in this PR according to:

https://github.com/openhab/openhab-js/blob/51f164dc9f11a29e6223e24268848aa8f0be168f/src/rules/rules.js#L471

Additionally **a new return block** was added to the "Run & Process" category with return parameter.

1) The block without a return parameter is a nice improvement to easily
leave a script without using non-elegant if/else statements. Bei adding
the return statement the script is immediately ended. In this case the
return parameter is not important.
2) By adding a return parameter (in particular true or false) this can be used for script conditions

---------

Signed-off-by: Stefan Höhn <mail@stefanhoehn.com>

bundles/org.openhab.ui/web/src/assets/definitions/blockly/blocks-scripts.js
@@ -252,22 +252,39 @@ export default function defineOHBlocks_Scripts (f7, transformationServices) {
     }
   }
 
+  // mapping of old event  names to the new ones
+  let attributeMap =
+    {
+      'ruleUID': 'ruleUID',
+      'eventAvailable': 'true', // event always available in new approach
+      'type': 'event.eventName',
+      'itemState': '(event.receivedState ?? event.newState)',
+      'oldItemState': 'event.oldState',
+      'itemName': 'event.itemName',
+      'itemCommand': 'event.receivedCommand',
+      'channel': 'event.channelUID',
+      'event': 'event.receivedEvent'
+    }
+
   javascriptGenerator.forBlock['oh_context_info'] = function (block) {
-    const contextInfo = block.getFieldValue('contextInfo')
+    const oldContextInfoName = block.getFieldValue('contextInfo')
+    // map attributes to the correct event attribute names
+    const contextInfo = attributeMap[oldContextInfoName]
+
     const type = block.getFieldValue('asType')
     if (contextInfo === 'eventAvailable') return ['(event !== undefined)', javascriptGenerator.ORDER_ATOMIC]
     if (contextInfo === 'ruleUID') return ['ctx.ruleUID', javascriptGenerator.ORDER_ATOMIC]
 
-    if (contextInfo === 'itemState' || contextInfo === 'oldItemState' || contextInfo === 'itemCommand') {
+    if (contextInfo === 'itemState' || contextInfo === 'oldState' || contextInfo === 'itemCommand') {
       if (type === 'asNumber') {
-        return [`((event.${contextInfo} !== undefined) ? parseFloat(event.${contextInfo}.toString()) : undefined)`, javascriptGenerator.ORDER_ATOMIC]
+        return [`((${contextInfo} !== undefined) ? parseFloat(${contextInfo}.toString()) : undefined)`, javascriptGenerator.ORDER_ATOMIC]
       } else if (type === 'asQuantity') {
-        return [`((event.${contextInfo} !== undefined) ? Quantity(event.${contextInfo}.toString()) : undefined)`, javascriptGenerator.ORDER_ATOMIC]
+        return [`((${contextInfo} !== undefined) ? Quantity(${contextInfo}.toString()) : undefined)`, javascriptGenerator.ORDER_ATOMIC]
       } else {
-        return [`event.${contextInfo}?.toString()`, javascriptGenerator.ORDER_ATOMIC]
+        return [`${contextInfo}?.toString()`, javascriptGenerator.ORDER_ATOMIC]
       }
     }
-    return [`event.${contextInfo}`, javascriptGenerator.ORDER_ATOMIC]
+    return [`${contextInfo}`, javascriptGenerator.ORDER_ATOMIC]
   }
 
   /*
@@ -350,5 +367,35 @@ export default function defineOHBlocks_Scripts (f7, transformationServices) {
     const enableType = blockGetCheckedInputType(block, 'enable')
     let enable = (enableType === 'Boolean') ? enableValue : (enableValue === '\'true\'' || enableValue === '\'enabled\'')
     return `rules.setEnabled(${ruleUID}, ${enable});\n`
+  }
+
+  /*
+  * Allows quick return from a rule. This is useful to stop further processing of a rule.
+  * Also allows returning a value from a rule if required which allows using blockly for a script condition
+  * Blockly part
+  */
+  Blockly.Blocks['oh_rule_return'] = {
+    init: function () {
+      this.appendValueInput('value')
+        .appendField('return')
+      this.setInputsInline(true)
+      this.setPreviousStatement(true, null)
+      this.setNextStatement(false, null)
+      this.setColour(0)
+      this.setTooltip('Allows to return from the rule')
+
+      this.setHelpUrl('https://www.openhab.org/docs/configuration/blockly/rules-blockly-run-and-process.html#returnRule')
+    }
+  }
+
+  /*
+  * Allows quick return from a rule.
+  * Code part
+  */
+  javascriptGenerator.forBlock['oh_rule_return'] = function (block) {
+    // we need to enable the wrapper to allow returning from the rule
+    javascriptGenerator.provideFunction_('wrapper', ['"use wrapper;"'])
+    const value = javascriptGenerator.valueToCode(block, 'value', javascriptGenerator.ORDER_ATOMIC)
+    return `return ${value} \n`
   }
 }

bundles/org.openhab.ui/web/src/components/config/controls/blockly-editor.vue
@@ -1124,6 +1124,13 @@
               </shadow>
             </value>
           </block>
+          <block type="oh_rule_return">
+            <value name="value">
+              <shadow type="logic_boolean">
+                <field name="BOOL">TRUE</field>
+              </shadow>
+            </value>
+          </block>
         </category>
 
         <category name="Logging &amp; Output">

commit 308e53999aee00b5e7f3036a1062939d0d532030

[blockly] Fix oh_context_info block "as {type}" not working
Regression from #3376.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/assets/definitions/blockly/blocks-scripts.js
@@ -276,13 +276,13 @@ export default function defineOHBlocks_Scripts (f7, transformationServices) {
     if (contextInfo === 'eventAvailable') return ['(event !== undefined)', javascriptGenerator.ORDER_ATOMIC]
     if (contextInfo === 'ruleUID') return ['ctx.ruleUID', javascriptGenerator.ORDER_ATOMIC]
 
-    if (contextInfo === 'itemState' || contextInfo === 'oldState' || contextInfo === 'itemCommand') {
+    if (oldContextInfoName === 'itemState' || oldContextInfoName === 'oldItemState' || oldContextInfoName === 'itemCommand') {
       if (type === 'asNumber') {
-        return [`((${contextInfo} !== undefined) ? parseFloat(${contextInfo}.toString()) : undefined)`, javascriptGenerator.ORDER_ATOMIC]
+        return [`((${contextInfo} !== undefined) ? parseFloat(${contextInfo}) : undefined)`, javascriptGenerator.ORDER_ATOMIC]
       } else if (type === 'asQuantity') {
-        return [`((${contextInfo} !== undefined) ? Quantity(${contextInfo}.toString()) : undefined)`, javascriptGenerator.ORDER_ATOMIC]
+        return [`((${contextInfo} !== undefined) ? Quantity(${contextInfo}) : undefined)`, javascriptGenerator.ORDER_ATOMIC]
       } else {
-        return [`${contextInfo}?.toString()`, javascriptGenerator.ORDER_ATOMIC]
+        return [`${contextInfo}`, javascriptGenerator.ORDER_ATOMIC]
       }
     }
     return [`${contextInfo}`, javascriptGenerator.ORDER_ATOMIC]