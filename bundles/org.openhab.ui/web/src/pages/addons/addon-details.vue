388a5da9103d4cc0961e6d06ed86a5a27997f535
Florian Hotze <dev@florianhotze.com>
2025-12-21 19:17:47 +0100

    Fix lint
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- bundles/org.openhab.ui/web/src/pages/addons/addon-details.vue
+++ bundles/org.openhab.ui/web/src/pages/addons/addon-details.vue
@@ -4,7 +4,10 @@
            ref="addondetails"
            class="page-addon-details">
     <f7-navbar :transparent="true" class="addon-details-nav">
-      <oh-nav-content :title="addon?.label" back-link="Add-on Store" :back-link-url="`/addons/${addonId.split('-')[0]}`" :f7router />
+      <oh-nav-content :title="addon?.label"
+                      back-link="Add-on Store"
+                      :back-link-url="`/addons/${addonId.split('-')[0]}`"
+                      :f7router />
     </f7-navbar>
     <f7-block class="block-narrow addon-details" v-if="ready && addon">
       <f7-row>
