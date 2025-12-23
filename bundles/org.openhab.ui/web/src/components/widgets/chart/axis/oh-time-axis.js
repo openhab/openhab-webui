openhab/openhab-webui

commit 0d34eb0adb7f149a9a30ee8273332a3a6bc10f1a

Analyzer/Charts: Fix week starting on Sunday starts on Monday (#3578)
The `week` chart type should range from Sunday to Sunday, but it has
been rendered similarly to `isoWeek` from Monday to Monday.

This fixes that behaviour by properly calculating the startTime and
removing the alignment logic from oh-time-axis, as startTime and endTime
are already properly aligned by the chart-mixin.

It also makes week starting on Sunday available in the analyzer.

---------

Also-by: Jeff James <jeff@james-online.com>
Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/chart/axis/oh-time-axis.js
@@ -8,19 +8,8 @@ export default {
   get (component, startTime, endTime, chart, chartWidget) {
     let axis = chartWidget.evaluateExpression(ComponentId.get(component), component.config)
     axis.type = 'time'
-    if (chart.config.chartType) {
-      axis.min = (v) => {
-        if(isNaN(v.min)) return startTime.toDate().getTime()
-        return isFinite(v.min) ? dayjs(v.min).startOf(chart.config.chartType).toDate().getTime() : v.min
-      }
-      axis.max = (v) => {
-        if(isNaN(v.min)) return endTime.toDate().getTime()
-        return isFinite(v.min) ? dayjs(v.min).startOf(chart.config.chartType).add(1, chart.config.chartType === 'isoWeek' ? 'week' : chart.config.chartType).toDate().getTime() : v.max
-      }
-    } else {
-      axis.min = startTime.toDate().getTime()
-      axis.max = endTime.toDate().getTime()
-    }
+    axis.min = startTime.toDate().getTime()
+    axis.max = endTime.toDate().getTime()
 
     if (!axis.axisPointer) {
       axis.axisPointer = { show: true, label: { backgroundColor: '#2196f3' } }

bundles/org.openhab.ui/web/src/components/widgets/chart/chart-mixin.js
@@ -51,7 +51,11 @@ export default {
     const chartType = config.chartType
     const future = config.future === true ? 1 : (config.future ?? 0)
     const endTime = (chartType)
-      ? this.addOrSubtractPeriod(dayjs().startOf(chartType), 1 + future)
+      ? chartType === 'week'
+        // Week starting on Sunday
+        ? this.addOrSubtractPeriod(dayjs().startOf(chartType).subtract(1, 'day'), 1 + future)
+        // Week starting on Monday and all other chart types
+        : this.addOrSubtractPeriod(dayjs().startOf(chartType === 'isoWeek' ? 'week' : chartType), 1 + future)
       : this.addOrSubtractPeriod(dayjs(), future)
 
     return {

bundles/org.openhab.ui/web/src/pages/analyzer/analyzer.vue
@@ -167,7 +167,7 @@
                     :text="t('analyzer.coords.period.fixed')" />
                 </f7-segmented>
                 <f7-segmented v-if="coordSettings.chartType !== ChartType.dynamic">
-                  <f7-button v-for="type in [ChartType.day, ChartType.isoWeek, ChartType.month, ChartType.year]"
+                  <f7-button v-for="type in [ChartType.day, ChartType.isoWeek, ChartType.week, ChartType.month, ChartType.year]"
                              :key="type"
                              :disabled="!coordSettings.typeOptions.includes(type)"
                              :active="coordSettings.chartType === type"

bundles/org.openhab.ui/web/src/pages/analyzer/chart-time.ts
@@ -30,7 +30,7 @@ const GRID_CONFIG = {
 
 const timeCoordSystem : CoordSystem = {
   initCoordSystem (coordSettings? : Partial<TimeCoordSettings>) : TimeCoordSettings {
-    const typeOptions : ChartType[] = [ChartType.day, ChartType.isoWeek, ChartType.month, ChartType.year]
+    const typeOptions : ChartType[] = [ChartType.day, ChartType.isoWeek, ChartType.week, ChartType.month, ChartType.year]
     return {
       period: coordSettings?.period || Period.D,
       categoryAxisValues: [],

commit b183d0657c2f1c9b8d91527a1aabe16489cf2187

Charts: Fix oh-time-axis doesn't adjust begin/end for series with offset
Regression from #3578.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/chart/axis/oh-time-axis.js
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs'
 import IsoWeek from 'dayjs/plugin/isoWeek'
 import ComponentId from '../../component-id'
+import { startOf } from '@/components/widgets/chart/utils.ts'
 
 dayjs.extend(IsoWeek)
 
@@ -8,8 +9,23 @@ export default {
   get (component, startTime, endTime, chart, chartWidget) {
     let axis = chartWidget.evaluateExpression(ComponentId.get(component), component.config)
     axis.type = 'time'
-    axis.min = startTime.toDate().getTime()
-    axis.max = endTime.toDate().getTime()
+    const chartType = chart.config.chartType
+    if (chartType) {
+      // adjusts time-axis begin and end timestamps depending on the received data, allowing to display series with offset
+      axis.min = (v) => {
+        if (isNaN(v.min)) return startTime.toDate().getTime()
+        if (!isFinite(v.min)) return v.min
+        return startOf(chartType, v.min).toDate().getTime()
+      }
+      axis.max = (v) => {
+        if (isNaN(v.min)) return endTime.toDate().getTime()
+        if (!isFinite(v.max)) return v.max
+        return startOf(chartType, v.min).add(1, chartType === 'isoWeek' ? 'week' : chartType).toDate().getTime()
+      }
+    } else {
+      axis.min = startTime.toDate().getTime()
+      axis.max = endTime.toDate().getTime()
+    }
 
     if (!axis.axisPointer) {
       axis.axisPointer = { show: true, label: { backgroundColor: '#2196f3' } }

bundles/org.openhab.ui/web/src/components/widgets/chart/chart-mixin.js
@@ -29,6 +29,7 @@ import OhChartTitle from './misc/oh-chart-title'
 import OhChartToolbox from './misc/oh-chart-toolbox'
 
 import { useRuntimeStore } from '@/js/stores/useRuntimeStore'
+import { startOf } from '@/components/widgets/chart/utils.ts'
 
 const DEFAULT_PERIOD = 'D'
 
@@ -56,11 +57,7 @@ export default {
     const chartType = config.chartType
     const future = config.future === true ? 1 : (config.future ?? 0)
     const endTime = (chartType)
-      ? chartType === 'week'
-        // Week starting on Sunday
-        ? this.addOrSubtractPeriod(dayjs().startOf(chartType).subtract(1, 'day'), 1 + future)
-        // Week starting on Monday and all other chart types
-        : this.addOrSubtractPeriod(dayjs().startOf(chartType === 'isoWeek' ? 'week' : chartType), 1 + future)
+      ? this.addOrSubtractPeriod(startOf(chartType), 1 + future)
       : this.addOrSubtractPeriod(dayjs(), future)
 
     return {
@@ -239,7 +236,7 @@ export default {
     setDate (date) {
       const chartType = this.context.component.config.chartType
       const day = dayjs(date)
-      this.endTime = this.addOrSubtractPeriod((chartType) ? day.startOf(chartType) : day, 1)
+      this.endTime = this.addOrSubtractPeriod((chartType) ? startOf(chartType) : day, 1)
     },
     earlierPeriod () {
       this.endTime = this.addOrSubtractPeriod(this.endTime, -1)

bundles/org.openhab.ui/web/src/components/widgets/chart/utils.ts
@@ -1,0 +1,29 @@
+import dayjs, { type ConfigType, Dayjs, type OpUnitType } from 'dayjs'
+import IsoWeek from 'dayjs/plugin/isoWeek'
+import DayDuration from 'dayjs/plugin/duration'
+import { ChartType } from '@/types/components/widgets'
+
+dayjs.extend(IsoWeek)
+dayjs.extend(DayDuration)
+
+/**
+ * Returns the dayjs date/time at the start of the time range of the given chart type.
+ * By default, the beginning is relative to now, i.e. if today is Wednesday and `chartType` is `isoWeek`, Monday 00:00 will be returned.
+ * If an optional `date` is passed, the start depends on that data, i.e. if last week's Wednesday is passed and `chartType` is `isoWeek`, last week's Monday 00:00 will be returned
+ *
+ * @param chartType
+ * @param date
+ */
+export function startOf (chartType: ChartType, date?: any): Dayjs {
+  if (chartType === 'week') {
+    // Week starting on Sunday & passed-in date is on Sunday: pass through to avoid shifting back by one week
+    if (date && dayjs(date).day() === 0) return dayjs(date)
+    // Week starting on Sunday
+    return dayjs(date).startOf(chartType).subtract(1, 'day')
+  }
+  if (chartType === 'isoWeek') {
+    // Week starting on Monday
+    return dayjs(date).startOf('week')
+  }
+  return dayjs(date).startOf(chartType as OpUnitType)
+}