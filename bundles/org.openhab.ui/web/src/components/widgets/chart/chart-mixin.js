openhab/openhab-webui

commit 0560b6377fca44573eb9a6d90a44ba581c16b407

Added support for localization of number formats for echarts (#3587)
Refs #3583.

Also-by: Florian Hotze <dev@florianhotze.com>
Signed-off-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/components/widgets/chart/chart-mixin.js
@@ -28,6 +28,8 @@ import OhChartLegend from './misc/oh-chart-legend'
 import OhChartTitle from './misc/oh-chart-title'
 import OhChartToolbox from './misc/oh-chart-toolbox'
 
+import { useRuntimeStore } from '@/js/stores/useRuntimeStore'
+
 const DEFAULT_PERIOD = 'D'
 
 const axisComponents = {
@@ -46,6 +48,9 @@ const seriesComponents = {
 }
 
 export default {
+  beforeCreate () {
+    this.numberFormatter = new Intl.NumberFormat(useRuntimeStore().locale)
+  },
   data () {
     const config = this.context.component.config || {}
     const chartType = config.chartType
@@ -104,7 +109,7 @@ export default {
     },
     yAxis () {
       if (!this.context.component.slots || !this.context.component.slots.yAxis) return undefined
-      return this.context.component.slots.yAxis.map((a) => axisComponents[a.component].get(a, this.startTime, this.endTime, this.context.component, this, true)) // invert Y axis by default
+      return this.context.component.slots.yAxis.map((a) => axisComponents[a.component].get(a, this.startTime, this.endTime, this.context.component, this, true, this.numberFormatter)) // invert Y axis by default
     },
     calendar () {
       if (!this.context.component.slots || !this.context.component.slots.calendar) return undefined
@@ -116,12 +121,12 @@ export default {
     },
     tooltip () {
       if (!this.context.component.slots || !this.context.component.slots.tooltip) return undefined
-      return this.context.component.slots.tooltip.map((c) => OhChartTooltip.get(c, this.startTime, this.endTime, this, this.$device))
+      return this.context.component.slots.tooltip.map((c) => OhChartTooltip.get(c, this.startTime, this.endTime, this, this.$device, this.numberFormatter))
     },
     visualMap () {
       if (!this.context.component.slots) return undefined
       if (this.context.component.slots.visualMap) {
-        return this.context.component.slots.visualMap.map((c) => OhChartVisualMap.get(c, this.startTime, this.endTime, this, this.$device))
+        return this.context.component.slots.visualMap.map((c) => OhChartVisualMap.get(c, this.startTime, this.endTime, this, this.$device, this.numberFormatter))
       } else if (JSON.stringify(this.context.component.slots.series)?.includes('heatmap')) { // heatmap needs a visualMap, therefore fall back to a default
         const config = {
           calculable: true,
@@ -129,7 +134,7 @@ export default {
           show: false,
           type: 'continuous'
         }
-        return OhChartVisualMap.get({ config }, this.startTime, this.endTime, this, this.$device)
+        return OhChartVisualMap.get({ config }, this.startTime, this.endTime, this, this.$device, this.numberFormatter)
       }
       return undefined
     },
@@ -139,7 +144,7 @@ export default {
     },
     legend () {
       if (!this.context.component.slots || !this.context.component.slots.legend) return undefined
-      return this.context.component.slots.legend.map((c) => OhChartLegend.get(c, this.startTime, this.endTime, this, this.$device))
+      return this.context.component.slots.legend.map((c) => OhChartLegend.get(c, this.startTime, this.endTime, this, this.$device, this.numberFormatter))
     },
     title () {
       if (!this.context.component.slots || !this.context.component.slots.title) return undefined

bundles/org.openhab.ui/web/src/components/widgets/chart/axis/oh-category-axis.js
@@ -18,7 +18,7 @@ const weekdays = {
 }
 
 export default {
-  get (component, startTime, endTime, chart, chartWidget, inverse) {
+  get (component, startTime, endTime, chart, chartWidget, inverse, numberFormatter) {
     const config = component.config || {}
     let axis = chartWidget.evaluateExpression(ComponentId.get(component), component.config)
     axis.type = 'category'

bundles/org.openhab.ui/web/src/components/widgets/chart/axis/oh-value-axis.js
@@ -1,9 +1,15 @@
 import ComponentId from '../../component-id'
 
 export default {
-  get (component, startTime, endTime, chart, chartWidget) {
+  get (component, startTime, endTime, chart, chartWidget, inverse, numberFormatter) {
     let axis = chartWidget.evaluateExpression(ComponentId.get(component), component.config)
     axis.type = 'value'
+
+    axis.axisLabel = axis.axisLabel || {}
+    if (!axis.axisLabel.formatter) {
+      axis.axisLabel.formatter = (value, index) => numberFormatter.format(value)
+    }
+
     return axis
   }
 }

bundles/org.openhab.ui/web/src/components/widgets/chart/misc/oh-chart-tooltip.js
@@ -5,13 +5,20 @@ import ComponentId from '../../component-id'
 dayjs.extend(LocalizedFormat)
 
 export default {
-  get (component, startTime, endTime, chart, device) {
+  get (component, startTime, endTime, chart, device, numberFormatter) {
     let options = chart.evaluateExpression(ComponentId.get(component), component.config)
     if (options.confine === undefined) options.confine = true
+
+    if (!options.valueFormatter) {
+      options.valueFormatter = (value) => numberFormatter.format(value)
+    }
 
     if (options.smartFormatter) {
       options.formatter = (params, ticket, callback) => {
         let tooltip = ''
+        // special tooltip for mark area:
+        // - header: time range (start time to end time) in 'dd DD.MM.YYYY HH:mm:ss'
+        // - content: marker colour, series name and value (if available)
         if (params.componentType === 'markArea') {
           tooltip += `<div>${dayjs(params.data.coord[0][0]).format('llll')}<br />${dayjs(params.data.coord[1][0]).format('llll')}</div>`
           tooltip += params.marker
@@ -20,15 +27,17 @@ export default {
           return tooltip
         }
         if (!params[0] || !params[0].axisType) return
+        // header: x-axis time in 'dd DD.MM.YYYY HH:mm:ss'
         if (params[0].axisType === 'xAxis.time') {
-          tooltip += `<div>${dayjs(params[0].axisValue).format('llll')}</div>` // ('dd DD.MM.YYYY HH:mm:ss')
+          tooltip += `<div>${dayjs(params[0].axisValue).format('llll')}</div>`
         }
+        // content: for each oh-time-series marker colour, series name and formatted value
         params.forEach((s) => {
           if (s.seriesId) {
             const [seriesType, itemName] = s.seriesId.split('#')
             if (seriesType === 'oh-time-series') {
               let item = chart._items[itemName]
-              let state = s.data[1]
+              let state = numberFormatter.format(s.data[1])
               if (item) {
                 const stateDescription = item.stateDescription || {}
                 if (stateDescription.format) {

bundles/org.openhab.ui/web/src/components/widgets/chart/misc/oh-chart-visualmap.js
@@ -7,7 +7,7 @@ const presetPalettes = {
 }
 
 export default {
-  get (component, startTime, endTime, chart, device) {
+  get (component, startTime, endTime, chart, device, numberFormatter) {
     let options = chart.evaluateExpression(ComponentId.get(component), component.config)
     if (options.presetPalette && (!options.inRange || !options.inRange.color)) {
       if (presetPalettes[options.presetPalette]) {
@@ -25,6 +25,11 @@ export default {
     } else if (chart.series && chart.series[0]) {
       options.max = Math.max(...chart.series[0].data.map((p) => p[p.length - 1]))
     }
+
+    if (!options.formatter) {
+      options.formatter = (value1, value2) => { return (value2) ? numberFormatter.format(value1) + ' - ' + numberFormatter.format(value2) : numberFormatter.format(value1) }
+    }
+
     return options
   }
 }

commit ddd7c3ede64c8f3594ea101d5094c9fb91b9e87a

Fixes community reported error https://community.openhab.org/t/echarts-js-error-on-rotate/167700?u=jsjames Fixed regression from #3587
Signed-off-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/components/widgets/chart/chart-mixin.js
@@ -105,7 +105,7 @@ export default {
     },
     xAxis () {
       if (!this.context.component.slots || !this.context.component.slots.xAxis) return undefined
-      return this.context.component.slots.xAxis.map((a) => axisComponents[a.component].get(a, this.startTime, this.endTime, this.context.component, this))
+      return this.context.component.slots.xAxis.map((a) => axisComponents[a.component].get(a, this.startTime, this.endTime, this.context.component, this, true, this.numberFormatter))
     },
     yAxis () {
       if (!this.context.component.slots || !this.context.component.slots.yAxis) return undefined