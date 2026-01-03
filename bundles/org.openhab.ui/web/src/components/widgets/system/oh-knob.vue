openhab/openhab-webui

commit a7e0836535e870b9cab1082e9c8985a049deba34

oh-knob: Fix state not rendered (#3470)
Fixes #3458

Signed-off-by: Dan Cunningham <dan@digitaldan.com>

bundles/org.openhab.ui/web/src/components/widgets/system/oh-knob.vue
@@ -1,6 +1,8 @@
 <template>
   <round-slider v-bind="resolvedConfig"
-                :value="computedValue"
+                :startValue="knobValue"
+                :model-value="knobValue"
+                @update:model-value="onChange"
                 :style="`stroke-dasharray: ${(config.dottedPath) ? config.dottedPath : 0}`"
                 mouseScrollAction="true"
                 @input="onChange"
@@ -22,10 +24,26 @@ export default {
     RoundSlider: defineAsyncComponent(() => import(/* webpackChunkName: "vue-round-slider" */ 'vue-three-round-slider'))
   },
   widget: OhKnobDefinition,
+  data () {
+    return {
+      knobValue: null
+    }
+  },
+  watch: {
+    value (newValue) {
+      if (!isNaN(newValue)) {
+        this.knobValue = this.computeValue(newValue)
+      }
+    }
+  },
+  created () {
+    if (!isNaN(this.value)) {
+      this.knobValue = this.computeValue(this.value)
+    } else {
+      this.knobValue = this.config.min || 0
+    }
+  },
   computed: {
-    computedValue () {
-      return (typeof this.config.offset === 'number') ? (this.value + this.config.offset) : this.value
-    },
     resolvedConfig () {
       const cfg = this.config
       return {
@@ -44,7 +62,11 @@ export default {
     }
   },
   methods: {
+    computeValue (value) {
+      return (typeof this.config.offset === 'number') ? (value + this.config.offset) : value
+    },
     onChange (newValue) {
+      if (isNaN(this.value) || isNaN(newValue)) return
       if (typeof this.config.offset === 'number') newValue -= this.config.offset
       this.sendCommandDebounced(newValue)
     }

commit ead4796bf21837449fcb653e5f27cb5a905ca304

oh-knob: Fix circle not filled
Fixes #3680.
Regression from #3470.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/system/oh-knob.vue
@@ -1,6 +1,5 @@
 <template>
   <round-slider v-bind="resolvedConfig"
-                :startValue="knobValue"
                 :model-value="knobValue"
                 @update:model-value="onChange"
                 :style="`stroke-dasharray: ${(config.dottedPath) ? config.dottedPath : 0}`"