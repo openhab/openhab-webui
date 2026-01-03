openhab/openhab-webui

commit a55c60ba0924ce5bbc15e1911f05381c6b797325

Thing page: Fix Z-Wave network viewer action, thing action caching, action config groups (#3673)
Fixes #3672
Fixes #3671

Signed-off-by: Dan Cunningham <dan@digitaldan.com>

bundles/org.openhab.ui/web/src/pages/settings/things/thing-details.vue
@@ -170,7 +170,7 @@
             <config-sheet
               ref="thingConfiguration"
               :parameter-groups="configDescriptions.parameterGroups"
-              :parameters="configDescriptions.parameters"
+              :parameters="filteredConfigParameters"
               :configuration="thing.configuration"
               :status="configStatusInfo"
               :set-empty-config-as-null="true"
@@ -475,7 +475,30 @@ export default {
         store: useStatesStore().trackedItems
       }
     },
-    ...mapState(useThingEditStore, ['configDirty', 'thingDirty', 'thing', 'thingType', 'channelTypes', 'configDescriptions', 'configStatusInfo', 'thingActions', 'firmwares', 'editable', 'isExtensible', 'hasLinkedItems'])
+    ...mapState(useThingEditStore, ['configDirty', 'thingDirty', 'thing', 'thingType', 'channelTypes', 'configDescriptions', 'configStatusInfo', 'thingActions', 'firmwares', 'editable', 'isExtensible', 'hasLinkedItems']),
+    /**
+     * Returns config parameters with deprecated action parameters filtered out.
+     * Action parameters are BOOLEAN parameters in groups with context='actions' or (name='actions' AND label='Actions').
+     * @deprecated Can be removed once all bindings have migrated from config actions to real Thing actions.
+     */
+    filteredConfigParameters () {
+      if (!this.configDescriptions?.parameters || !this.configDescriptions?.parameterGroups) {
+        return this.configDescriptions?.parameters || []
+      }
+      // Find action groups: first by context, then fall back to name+label heuristic
+      let actionGroupNames = this.configDescriptions.parameterGroups
+        .filter((pg) => pg.context === 'actions')
+        .map((pg) => pg.name)
+      if (actionGroupNames.length === 0) {
+        actionGroupNames = this.configDescriptions.parameterGroups
+          .filter((pg) => pg.name === 'actions' && pg.label === 'Actions')
+          .map((pg) => pg.name)
+      }
+      // Filter out BOOLEAN parameters in action groups (these are rendered as action buttons instead)
+      return this.configDescriptions.parameters.filter(
+        (p) => !(actionGroupNames.includes(p.groupName) && p.type === 'BOOLEAN')
+      )
+    }
   },
   watch: {
     configDirty: function () { this.dirty = this.configDirty || this.thingDirty || this.codeDirty },
@@ -536,6 +559,9 @@ export default {
       this.codeDirty = codeDirty
     },
     load () {
+      this.ready = false
+      this.error = false
+
       const loadingFinished = (success) => {
         if (!success) {
           this.error = true

bundles/org.openhab.ui/web/src/components/thing/zwave/zwave-network.vue
@@ -26,6 +26,7 @@ import { use } from 'echarts/core'
 import { CanvasRenderer } from 'echarts/renderers'
 import { GraphChart } from 'echarts/charts'
 import { TooltipComponent, ToolboxComponent } from 'echarts/components'
+import 'echarts/theme/dark.js'
 import VChart from 'vue-echarts'
 
 import { useUIOptionsStore } from '@/js/stores/useUIOptionsStore'

bundles/org.openhab.ui/web/src/js/stores/useThingEditStore.ts
@@ -7,7 +7,6 @@ import api from '@/js/openhab/api'
 
 import type {
   Channel,
-  ConfigDescriptionParameter,
   ConfigDescriptionResponse,
   Firmware,
   FirmwareResponse,
@@ -73,6 +72,7 @@ export const useThingEditStore = defineStore('thingEditStore', () => {
     } catch (e: any) {
       if (e === 'Not Found' || e === 404) {
         console.log('No actions available for this Thing')
+        thingActions.value = []
         return
       }
       console.error('Error loading thing actions: ' + e)
@@ -84,9 +84,6 @@ export const useThingEditStore = defineStore('thingEditStore', () => {
     try {
       const data: ConfigDescriptionResponse = await api.get('/rest/config-descriptions/thing:' + thingUID)
       configDescriptions.value = data
-
-      // TODO: Can be removed once the config actions have been removed from all add-ons
-      configDescriptions.value.parameters = configDescriptions.value.parameters.filter((p: ConfigDescriptionParameter) => p.groupName !== 'actions')
     } catch (e: any) {
       console.debug('No specific config description available for this thing, using config description from thing type instead.')
       configDescriptions.value = {

commit 53c7a0f0a8efa387f30af33342a282c8e8424fb0

Thing details: Fix code tab emptied on save
Fixes #3699.
Regression from #3673.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/pages/settings/things/thing-details.vue
@@ -575,8 +575,14 @@ export default {
     onCodeChanged (codeDirty) {
       this.codeDirty = codeDirty
     },
-    load () {
-      this.ready = false
+    /**
+     * Load required data from the REST API.
+     * @param {boolean} [stay=false] stay ready: do not reset ready state, only reload data
+     */
+    load (stay = false) {
+      if (!stay) {
+        this.ready = false
+      }
       this.error = false
 
       const loadingFinished = (success) => {
@@ -891,7 +897,7 @@ export default {
                 break
               case 'updated':
                 console.log('Thing updated according to SSE, reloading')
-                this.load()
+                this.load(true)
                 break
             }
             break