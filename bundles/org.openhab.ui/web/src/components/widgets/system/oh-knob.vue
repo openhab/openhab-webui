openhab/openhab-webui

commit a7e0836535e870b9cab1082e9c8985a049deba34

oh-knob: Fix state not rendered (#3470)
Fixes #3458

Signed-off-by: Dan Cunningham <dan@digitaldan.com>

bundles/org.openhab.ui/web/src/components/widgets/system/oh-knob.vue
@@ -1,6 +1,8 @@
 <template>
   <round-slider v-bind="resolvedConfig"
-                :value="computedValue"
+                :startValue="knobValue"
+                :model-value="knobValue"
+                @update:model-value="onChange"
                 :style="`stroke-dasharray: ${(config.dottedPath) ? config.dottedPath : 0}`"
                 mouseScrollAction="true"
                 @input="onChange"
@@ -22,10 +24,26 @@ export default {
     RoundSlider: defineAsyncComponent(() => import(/* webpackChunkName: "vue-round-slider" */ 'vue-three-round-slider'))
   },
   widget: OhKnobDefinition,
+  data () {
+    return {
+      knobValue: null
+    }
+  },
+  watch: {
+    value (newValue) {
+      if (!isNaN(newValue)) {
+        this.knobValue = this.computeValue(newValue)
+      }
+    }
+  },
+  created () {
+    if (!isNaN(this.value)) {
+      this.knobValue = this.computeValue(this.value)
+    } else {
+      this.knobValue = this.config.min || 0
+    }
+  },
   computed: {
-    computedValue () {
-      return (typeof this.config.offset === 'number') ? (this.value + this.config.offset) : this.value
-    },
     resolvedConfig () {
       const cfg = this.config
       return {
@@ -44,7 +62,11 @@ export default {
     }
   },
   methods: {
+    computeValue (value) {
+      return (typeof this.config.offset === 'number') ? (value + this.config.offset) : value
+    },
     onChange (newValue) {
+      if (isNaN(this.value) || isNaN(newValue)) return
       if (typeof this.config.offset === 'number') newValue -= this.config.offset
       this.sendCommandDebounced(newValue)
     }

commit a88ed39f4a2537905fe400c0d5e06edb6e41a382

oh-knob: Can't control Item with NULL/UNDEF state
Regression from #3470.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/system/oh-knob.vue
@@ -65,7 +65,8 @@ export default {
       return (typeof this.config.offset === 'number') ? (value + this.config.offset) : value
     },
     onChange (newValue) {
-      if (isNaN(this.value) || isNaN(newValue)) return
+      if (isNaN(newValue)) return
+      if (!['UNDEF', 'NULL'].includes(this.itemState) && isNaN(this.value)) return
       if (typeof this.config.offset === 'number') newValue -= this.config.offset
       this.sendCommandDebounced(newValue)
     }

bundles/org.openhab.ui/web/src/components/widgets/system/slide-mixin.js
@@ -14,6 +14,9 @@ export default {
     this.delayStateDisplay = this.config.delayStateDisplay ? this.config.delayStateDisplay : 2000
   },
   computed: {
+    itemState () {
+      return (this.config.ignoreDisplayState === true) ? this.context.store[this.config.item].state : (this.context.store[this.config.item].displayState ?? this.context.store[this.config.item].state)
+    },
     value () {
       if (this.config.variable) {
         const variableScope = getVariableScope(this.context.ctxVars, this.context.varScope, this.config.variable)
@@ -25,7 +28,7 @@ export default {
         }
       }
       if (this.pendingCommand !== null) return this.pendingCommand // to keep the control reactive when operating
-      const value = (this.config.ignoreDisplayState === true) ? this.context.store[this.config.item].state : this.context.store[this.config.item].displayState || this.context.store[this.config.item].state
+      const value = this.itemState
       // use as a brightness control for HSB values
       if (value.split && value.split(',').length === 3) {
         return parseFloat(value.split(',')[2])