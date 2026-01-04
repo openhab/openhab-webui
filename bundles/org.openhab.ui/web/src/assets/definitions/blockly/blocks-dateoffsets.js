openhab/openhab-webui

commit a308e64f9ec5506f2f6f013725262fcfa30e3899

[blockly] fix zdt copy_of nano handling (#3361)
Fixes #3188 

The root of the problem is that the underlying time library does not
support to set the nanoseconds only. Nanoseconds also contain the millis
and the micros. As a result, for example changing only microseconds
requires to merge the new microseconds between the millis and the actual
nano seconds. The has now been fixed in the copy-of method that allows
to overwrite them individually. 

As a side effect or natural consequence, _nanoTotal_ was introduced to
retrieve the >whole< nanoseconds including millis an micros, whereas
_nano_ only retrieves the actual nanoseconds.

**Important side note or warning:** 

When retrieving the current datetime with _now_ there is a precision
limitation within javascript that will only return a time with precision
milliseconds! This means that micros and nanos are always set to zero.
This is not an openHAB limitation but one that relates to javascript
only.

Signed-off-by: Stefan Höhn <mail@stefanhoehn.com>

bundles/org.openhab.ui/web/src/assets/definitions/blockly/blocks-dateoffsets.js
@@ -133,22 +133,22 @@ export default function (f7) {
       this.setHelpUrl('https://www.openhab.org/docs/configuration/blockly/rules-blockly-date-handling.html#datetime-with-date-and-time-values')
     },
     updateShape_: function () {
-      let year = this.appendValueInput('year')
+      this.appendValueInput('year')
         .setCheck('Number')
         .appendField('datetime with date')
-      let month = this.appendValueInput('month')
+      this.appendValueInput('month')
         .setCheck('Number')
         .appendField('-')
-      let day = this.appendValueInput('day')
+      this.appendValueInput('day')
         .setCheck('Number')
         .appendField('-')
-      let hour = this.appendValueInput('hour')
+      this.appendValueInput('hour')
         .setCheck('Number')
         .appendField('and time')
-      let minute = this.appendValueInput('minute')
+      this.appendValueInput('minute')
         .setCheck('Number')
         .appendField(':')
-      let second = this.appendValueInput('second')
+      this.appendValueInput('second')
         .setCheck('Number')
         .appendField(':')
     }
@@ -525,6 +525,9 @@ export default function (f7) {
     let millis = 0
     let micros = 0
     let nanos = 0
+    let millisChanged = false
+    let microsChanged = false
+    let nanosChanged = false
 
     let operationBlock = block.getInput('ADD0')
     if (operationBlock) {
@@ -542,12 +545,15 @@ export default function (f7) {
             switch (parseInt(blockType)) {
               case 6:
                 millis = parseInt(temporal)
+                millisChanged = true
                 break
               case 7:
                 micros = parseInt(temporal)
+                microsChanged = true
                 break
               case 8:
                 nanos = parseInt(temporal)
+                nanosChanged = true
                 break
               default:
                 code += `.${operation}${operationUnit}(${temporal})`
@@ -555,11 +561,14 @@ export default function (f7) {
           }
         }
       }
-      let totalNanos = 1000000 * millis + 1000 * micros + nanos
-      if (totalNanos > 0) {
-        let operationUnit = this.blockTypesMethod[operation][8]
-        code += `.${operation}${operationUnit}(${totalNanos})`
+      let nanoCode = ''
+      if (millisChanged || microsChanged || nanosChanged) {
+        nanoCode += (!millisChanged) ? `(${baseZdt}.get(time.ChronoField.NANO_OF_SECOND) / 1000000 | 0) * 1000000  + ` : `${millis}  * 1000000 +`
+        nanoCode += (!microsChanged) ? `((${baseZdt}.get(time.ChronoField.NANO_OF_SECOND) / 1000) % 1000 | 0) * 1000 + ` : `${micros} * 1000 +`
+        nanoCode += (!nanosChanged) ? `(${baseZdt}.get(time.ChronoField.NANO_OF_SECOND) % 1000 | 0)` : nanos
       }
+      let operationUnit = this.blockTypesMethod[operation][8]
+      code += `.${operation}${operationUnit}(${nanoCode})`
     }
     return [code, javascriptGenerator.ORDER_ATOMIC]
   }
@@ -738,7 +747,7 @@ export default function (f7) {
   Blockly.Blocks['oh_get_zdt_part'] = {
     init: function () {
       this.appendDummyInput()
-        .appendField(new Blockly.FieldDropdown([['year', 'getYear'], ['month', 'getMonthValue'], ['day of month', 'getDayOfMonth'], ['day of week', 'getDayOfWeek'], ['day of year', 'getDayOfYear'], ['hour', 'getHour'], ['minute', 'getMinute'], ['second', 'getSecond'], ['milli', 'getMilli'], ['micro', 'getMicro'], ['nano', 'getNano']]), 'temporalPart')
+        .appendField(new Blockly.FieldDropdown([['year', 'getYear'], ['month', 'getMonthValue'], ['day of month', 'getDayOfMonth'], ['day of week', 'getDayOfWeek'], ['day of year', 'getDayOfYear'], ['hour', 'getHour'], ['minute', 'getMinute'], ['second', 'getSecond'], ['milli', 'getMilli'], ['micro', 'getMicro'], ['nano', 'getNano'], ['nanoTotal', 'getNanoTotal']]), 'temporalPart')
         .appendField('of')
       this.appendValueInput('zdt')
         .setCheck('ZonedDateTime')
@@ -758,14 +767,21 @@ export default function (f7) {
     const zdt = javascriptGenerator.valueToCode(block, 'zdt', javascriptGenerator.ORDER_ATOMIC)
     let temporalPart = block.getFieldValue('temporalPart')
 
-    const op = new Map([['getYear', 'year'], ['getMonthValue', 'monthValue'], ['getDayOfMonth', 'dayOfMonth'], ['getDayOfWeek', 'dayOfWeek().value'], ['getDayOfYear', 'dayOfYear'], ['getHour', 'hour'], ['getMinute', 'minute'], ['getSecond', 'second'], ['getNano', 'nano']])
+    const op = new Map([['getYear', 'year'], ['getMonthValue', 'monthValue'], ['getDayOfMonth', 'dayOfMonth'], ['getDayOfWeek', 'dayOfWeek().value'], ['getDayOfYear', 'dayOfYear'], ['getHour', 'hour'], ['getMinute', 'minute'], ['getSecond', 'second'], ['getNanoTotal', 'get(time.ChronoField.NANO_OF_SECOND)']])
     switch (temporalPart) {
       case 'getMilli':
-        temporalPart = 'getLong(time.ChronoField.MILLI_OF_SECOND)'
+        temporalPart = 'get(time.ChronoField.NANO_OF_SECOND) / 1000000 | 0'
         break
       case 'getMicro':
-        temporalPart = 'getLong(time.ChronoField.MICRO_OF_SECOND) % 1000'
+        temporalPart = 'get(time.ChronoField.NANO_OF_SECOND) / 1000 % 1000 | 0'
         break
+      case 'getNano':
+        temporalPart = 'get(time.ChronoField.NANO_OF_SECOND) % 1000 | 0'
+        break
+      case 'getNanoTotal':
+        temporalPart = 'get(time.ChronoField.NANO_OF_SECOND)'
+        break
+
       default:
         temporalPart = op.get(temporalPart) + '()'
     }

commit 718beef4b508859174abd507b547dba3e9b71ffb

[blockly] Fix empty `plusNanos()` code generation (#3706)
Fixes #3694.
Possibly regression from #3361.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/assets/definitions/blockly/blocks-dateoffsets.js
@@ -561,7 +561,7 @@ export default function (f7) {
           }
         }
       }
-      let nanoCode = ''
+      let nanoCode = '0'
       if (millisChanged || microsChanged || nanosChanged) {
         nanoCode += (!millisChanged) ? `(${baseZdt}.get(time.ChronoField.NANO_OF_SECOND) / 1000000 | 0) * 1000000  + ` : `${millis}  * 1000000 +`
         nanoCode += (!microsChanged) ? `((${baseZdt}.get(time.ChronoField.NANO_OF_SECOND) / 1000) % 1000 | 0) * 1000 + ` : `${micros} * 1000 +`