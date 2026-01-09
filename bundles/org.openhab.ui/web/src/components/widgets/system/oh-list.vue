86ebf11e86c2187625bd46ec154d450f3a8ada73
Florian Hotze <dev@florianhotze.com>
2026-01-09 14:16:57 +0100

    oh-list: Fix weird alignment of entries in editmode
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- bundles/org.openhab.ui/web/src/components/widgets/system/oh-list.vue
+++ bundles/org.openhab.ui/web/src/components/widgets/system/oh-list.vue
@@ -15,33 +15,35 @@
     </template>
     <ul v-if="context.component.slots && context.component.slots.default">
       <template v-for="(slotComponent, idx) in context.component.slots.default" :key="idx">
-        <f7-menu v-if="context.editmode" style="float: left">
-          <f7-menu-item icon-f7="list_bullet" class="margin-left configure-layout-menu" dropdown>
-            <f7-menu-dropdown>
-              <f7-menu-dropdown-item @click="context.editmode.configureWidget(context.component.slots.default[idx], context)" href="#" text="Configure Item" />
-              <f7-menu-dropdown-item @click="context.editmode.editWidgetCode(context.component.slots.default[idx], context)" href="#" text="Edit YAML" />
-              <f7-menu-dropdown-item v-if="context.parent.component.config.accordionList"
-                                     @click="context.editmode.editWidgetCode(context.component.slots.default[idx], context, 'accordion')"
-                                     href="#"
-                                     text="Edit Accordion Code" />
-              <f7-menu-dropdown-item divider />
-              <f7-menu-dropdown-item @click="context.editmode.cutWidget(context.component.slots.default[idx], context)" href="#" text="Cut" />
-              <f7-menu-dropdown-item @click="context.editmode.copyWidget(context.component.slots.default[idx], context)" href="#" text="Copy" />
-              <f7-menu-dropdown-item v-if="idx > 0 || idx < context.component.slots.default.length - 1" divider />
-              <f7-menu-dropdown-item v-if="idx > 0"
-                                     @click="context.editmode.moveWidgetUp(context.component.slots.default[idx], context)"
-                                     href="#"
-                                     text="Move Up" />
-              <f7-menu-dropdown-item v-if="idx < context.component.slots.default.length - 1"
-                                     @click="context.editmode.moveWidgetDown(context.component.slots.default[idx], context)"
-                                     href="#"
-                                     text="Move Down" />
-              <f7-menu-dropdown-item divider />
-              <f7-menu-dropdown-item @click="context.editmode.removeWidget(context.component.slots.default[idx], context)" href="#" text="Remove Item" />
-            </f7-menu-dropdown>
-          </f7-menu-item>
-        </f7-menu>
-        <generic-widget-component :context="childContext(slotComponent)" />
+        <div style="display: flex; align-items: center;">
+          <f7-menu v-if="context.editmode">
+            <f7-menu-item icon-f7="list_bullet" class="margin-left configure-layout-menu" dropdown>
+              <f7-menu-dropdown>
+                <f7-menu-dropdown-item @click="context.editmode.configureWidget(context.component.slots.default[idx], context)" href="#" text="Configure Item" />
+                <f7-menu-dropdown-item @click="context.editmode.editWidgetCode(context.component.slots.default[idx], context)" href="#" text="Edit YAML" />
+                <f7-menu-dropdown-item v-if="context.parent.component.config.accordionList"
+                                       @click="context.editmode.editWidgetCode(context.component.slots.default[idx], context, 'accordion')"
+                                       href="#"
+                                       text="Edit Accordion Code" />
+                <f7-menu-dropdown-item divider />
+                <f7-menu-dropdown-item @click="context.editmode.cutWidget(context.component.slots.default[idx], context)" href="#" text="Cut" />
+                <f7-menu-dropdown-item @click="context.editmode.copyWidget(context.component.slots.default[idx], context)" href="#" text="Copy" />
+                <f7-menu-dropdown-item v-if="idx > 0 || idx < context.component.slots.default.length - 1" divider />
+                <f7-menu-dropdown-item v-if="idx > 0"
+                                       @click="context.editmode.moveWidgetUp(context.component.slots.default[idx], context)"
+                                       href="#"
+                                       text="Move Up" />
+                <f7-menu-dropdown-item v-if="idx < context.component.slots.default.length - 1"
+                                       @click="context.editmode.moveWidgetDown(context.component.slots.default[idx], context)"
+                                       href="#"
+                                       text="Move Down" />
+                <f7-menu-dropdown-item divider />
+                <f7-menu-dropdown-item @click="context.editmode.removeWidget(context.component.slots.default[idx], context)" href="#" text="Remove Item" />
+              </f7-menu-dropdown>
+            </f7-menu-item>
+          </f7-menu>
+          <generic-widget-component :context="childContext(slotComponent)" style="flex-grow: 1;" />
+        </div>
       </template>
     </ul>
   </f7-list>
