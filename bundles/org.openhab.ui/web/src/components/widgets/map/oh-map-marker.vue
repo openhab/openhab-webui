openhab/openhab-webui

commit 86f01437bd4b55e1585744915cc936248f11d68f

oh-map/oh-plan: Fix various Leaflet related issues (#3687)
Fixes #3679.

Fixes the following issues:
- map-edit/plan-edit previews behind toolbar.
- Clean-up Leaflet `Icon.Default` code from oh-map-page/oh-plan-page as
it has no effect.
- oh-plan-marker: Fix default icon not displayed.
- oh-plan-page: Fix failure in plan-edit preview.
- oh-plan-page: Fix widget expressions for icons not evaluated in design
tab.

Code enhancements:
- oh-map-marker: Use default icon from Leaflet dist instead of base64
encoded.
- map-edit/plan-edit: Use a single oh-*-page component for the preview,
not one for each tab.

---------

Signed-off-by: Florian Hotze <dev@florianhotze.com>
Co-authored-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/components/widgets/map/oh-map-marker.vue
@@ -7,8 +7,11 @@
     <l-tooltip v-if="config.label">
       {{ config.label }}
     </l-tooltip>
-    <l-icon v-if="icon" :icon-size="[40,40]" :icon-url="icon" />
-    <l-icon v-else :icon-size="[25,41]" :icon-url="DefaultIcon" />
+    <l-icon
+      v-if="icon"
+      :icon-size="icon.iconSize"
+      :icon-url="icon.iconUrl"
+      :shadow-url="icon?.shadowUrl" />
   </l-marker>
 </template>
 
@@ -20,7 +23,8 @@ import { LMarker, LTooltip, LIcon } from '@vue-leaflet/vue-leaflet'
 import { actionsMixin } from '../widget-actions'
 import { OhMapMarkerDefinition } from '@/assets/definitions/widgets/map'
 
-const DefaultIcon = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAABSCAMAAAAhFXfZAAAC91BMVEVMaXEzeak2f7I4g7g3g7cua5gzeKg8hJo3grY4g7c3grU0gLI2frE0daAubJc2gbQwd6QzeKk2gLMtd5sxdKIua5g1frA2f7IydaM0e6w2fq41fK01eqo3grgubJgta5cxdKI1f7AydaQydaMxc6EubJgvbJkwcZ4ubZkwcJwubZgubJcydqUydKIxapgubJctbJcubZcubJcvbJYubJcvbZkubJctbJctbZcubJg2f7AubJcrbZcubJcubJcua5g3grY0fq8ubJcubJdEkdEwhsw6i88vhswuhcsuhMtBjMgthMsrg8srgss6is8qgcs8i9A9iMYtg8spgcoogMo7hcMngMonf8olfso4gr8kfck5iM8jfMk4iM8he8k1fro7itAgesk2hs8eecgzfLcofssdeMg0hc4cd8g2hcsxeLQbdsgZdcgxeLImfcszhM0vda4xgckzhM4xg84wf8Yxgs4udKsvfcQucqhUndROmdM1fK0wcZ8vb5w0eqpQm9MzeKhXoNVcpdYydKNWn9VZotVKltJFjsIwcJ1Rms9OlslLmtH///8+kc9epdYzd6dbo9VHkMM2f7FHmNBClM8ydqVcpNY9hro3gLM9hLczealQmcw3fa46f7A8gLMxc6I3eagyc6FIldJMl9JSnNRSntNNl9JPnNJFi75UnM9ZodVKksg8kM45jc09e6ZHltFBk883gbRBh7pDk9EwcaBzn784g7dKkcY2i81Om9M7j85Llc81is09g7Q4grY/j9A0eqxKmdFFltBEjcXf6fFImdBCiLxJl9FGlNFBi78yiMxVndEvbpo6js74+vx+psPP3+o/ks5HkcpGmNCjwdZCkNDM3ehYoNJEls+lxNkxh8xHks0+jdC1zd5Lg6r+/v/H2ufz9/o3jM3t8/edvdM/k89Th61OiLBSjbZklbaTt9BfptdjmL1AicBHj8hGk9FAgK1dkLNTjLRekrdClc/k7fM0icy0y9tgp9c4jc2NtM9Dlc8zicxeXZn3AAAAQ3RSTlMAHDdTb4yPA+LtnEQmC4L2EmHqB7XA0d0sr478x4/Yd5i1zOfyPkf1sLVq4Nh3FvjxopQ2/STNuFzUwFIwxKaejILpIBEV9wAABhVJREFUeF6s1NdyFEcYBeBeoQIhRAkLlRDGrhIgY3BJL8CVeKzuyXFzzjkn5ZxzzuScg3PO8cKzu70JkO0LfxdTU//pM9vTu7Xgf6KqOVTb9X7toRrVEfBf1HTVjZccrT/2by1VV928Yty9ZbVuucdz90frG8DBjl9pVApbOstvmMuvVgaNXSfAAd6pGxpy6yxf5ph43pS/4f3uoaGm2rdu72S9xzOvMymkZFq/ptDrk90mhW7e4zl7HLzhxGWPR20xmSxJ/VqldG5m9XhaVOA1DadsNh3Pu5L2N6QtPO/32JpqQBVVk20oy/Pi2s23WEvyfHbe1thadVQttvm7Llf65gGmXK67XtupyoM7HQhmXdLS8oGWJNeOJ3C5fG5XCEJnkez3/oFdsvgJ4l2ANZwhrJKk/7OSXa+3Vw2WJMlKnGkobouYk6T0TyX30klOUnTD9HJ5qpckL3EW/w4XF3Xd0FGywXUrstrclVsqz5Pd/sXFYyDnPdrLcQODmGOK47IZb4CmibmMn+MYRzFZ5jg33ZL/EJrWcszHmANy3ARBK/IXtciJy8VsitPSdE3uuHxzougojcUdr8/32atnz/ev3f/K5wtpxUTpcaI45zusVDpYtZi+jg0oU9b3x74h7+n9ABvYEZeKaVq0sh0AtLKsFtqNBdeT0MrSzwwlq9+x6xAO4tgOtSzbCjrNQQiNvQUbUEubvzBUeGw26yDCsRHCoLkTHDa7IdOLIThs/gHvChszh2CimE8peRs47cxANI0lYNB5y1DljpOF0IhzBDPOZnDOqYYbeGKECbPzWnXludPphw5c2YBq5zlwXphIbO4VDCZ0gnPfUO1TwZoYwAs2ExPCedAu9DAjfQUjzITQb3jNj0KG2Sgt6BHaQUdYzWz+XmBktOHwanXjaSTcwwziBcuMOtwBmqPrTOxFQR/DRKKPqyur0aiW6cULYsx6tBm0jXpR/AUWR6HRq9WVW6MRhIq5jLyjbaCTDCijyYJNpCajdyobP/eTw0iexBAKkJ3gA5KcQb2zBXsIBckn+xVv8jkZSaEFHE+jFEleAEfayRU0MouNoBmB/L50Ai/HSLIHxcrpCvnhSQAuakKp2C/YbCylJjXRVy/z3+Kv/RrNcCo+WUzlVEhzKffnTQnxeN9fWF88fiNCUdSTsaufaChKWInHeysygfpIqagoakW+vV20J8uyl6TyNKEZWV4oRSPyCkWpgOLSbkCObT8o2r6tlG58HQquf6O0v50tB7JM7F4EORd2dx/K0w/KHsVkLPaoYrwgP/y7krr3SSMA4zj+OBgmjYkxcdIJQyQRKgg2viX9Hddi9UBb29LrKR7CVVEEEXWojUkXNyfTNDE14W9gbHJNuhjDettN3ZvbOvdOqCD3Jp/9l+/wJE+9PkYGjx/fqkys3S2rMozM/o2106rfMUINo6hVqz+eu/hd1c4xTg0TAfy5kV+4UG6+IthHTU9woWmxuKNbTfuCSfovBCxq7EtHqvYL4Sm6F8GVxsSXHMQ07TOi1DKtZxjWaaIyi4CXWjxPccUw8WVbMYY5wxC1mzEyXMJWkllpRloi+Kkoq69sxBTlElF6aAxYUbjXNlhlDZilDnM4U5SlN5biRsRHnbx3mbeWjEh4mEyiuJDl5XcWVmX5GvNkFgLWZM5qwsop4/AWfLhU1cR7k1VVvcYCWRkOI6Xy5gmnphCYIkvzuNYzHzosq2oNk2RtSs8khfUOfHIDgR6ysYBaMpl4uEgk2U/oJTs9AaTSwma7dT69geAE2ZpEjUsn2ieJNHeKfrI3EcAGJ2ZaNgVuC8EBctCLc57P5u5led6IOBkIYkuQMrmmjChs4VkfOerHqSBkPzZlhe06RslZ3zMjk2sscqKwY0RcjKK+LWbzd7KiHhkncs/siFJ+V5eXxD34B8nVuJEpGJNmxN2gH3vSvp7J70tF+D1Ej8qUJD1TkErAND2GZwTFg/LubvmgiBG3SOvdlsqFQrkEzJCL1rstlnVFROixZoDDSuXQFHESwVGlcuQcMb/b42NgjLowh5MTDFE3vNB5qStRIErdCQEh6pLPR92anSUb/wAIhldAaDMpGgAAAABJRU5ErkJggg=='
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
 
 export default {
   mixins: [mixin, actionsMixin],
@@ -36,9 +40,6 @@ export default {
       markerKey: f7.utils.id()
     }
   },
-  created () {
-    this.DefaultIcon = DefaultIcon
-  },
   computed: {
     coords () {
       if (this.config.item) {
@@ -51,21 +52,26 @@ export default {
         return this.config.location.split(',')
       }
       return null
-    },
-    hasIcon () {
-      return this.config.icon
     }
   },
   asyncComputed: {
-    icon () {
-      if (this.config.icon && this.config.icon.indexOf('oh:') === 0) {
-        return this.$oh.media.getIcon(this.config.icon.substring(3)).then((icon) => {
-          this.markerKey = f7.utils.id()
-          this.$emit('update')
-          return icon
-        })
+    async icon () {
+      if (!this.config.icon?.startsWith('oh:')) {
+        this.markerKey = f7.utils.id()
+        return {
+          iconUrl: markerIcon,
+          shadowUrl: markerShadow,
+          iconSize: null
+        }
       }
-      return null
+
+      const iconData = await this.$oh.media.getIcon(this.config.icon.substring(3))
+      this.markerKey = f7.utils.id()
+      this.$emit('update')
+      return {
+        iconUrl: iconData,
+        iconSize: [40, 40]
+      }
     }
   },
   watch: {

bundles/org.openhab.ui/web/src/components/widgets/plan/oh-plan-marker.vue
@@ -1,18 +1,18 @@
 <template>
-  <l-marker v-if="visible && coords"
-            ref="marker"
-            :key="markerKey"
-            :draggable="context.editmode != undefined"
-            :lat-lng="coords"
-            @update:lat-lng="onMove"
-            @click="onClick">
-    <l-tooltip v-if="tooltip && !config.useTooltipAsLabel"
-               :options="tooltipOptions">
+  <l-marker
+    v-if="visible && coords"
+    ref="marker"
+    :key="markerKey"
+    :draggable="context.editmode !== undefined && context.editmode !== null"
+    :lat-lng="coords"
+    @update:lat-lng="onMove"
+    @click="onClick">
+    <l-tooltip v-if="tooltip && !config.useTooltipAsLabel" :options="tooltipOptions">
       <div style="white-space: nowrap" :style="tooltipStyle">
         {{ tooltip }}
       </div>
     </l-tooltip>
-    <l-icon :icon-size="[config.iconSize || 40, config.iconSize || 40]">
+    <l-icon v-if="hasCustomIcon" :icon-size="iconSize">
       <div v-if="config.useTooltipAsLabel" style="white-space: nowrap" :style="tooltipStyle">
         {{ tooltip }}
       </div>
@@ -24,7 +24,8 @@
                :height="config.iconHeight || config.iconSize || 40"
                :state="config.iconUseState ? state : undefined" />
     </l-icon>
-    <l-popup v-if="context.editmode != null && !dragging">
+    <l-icon v-else :icon-url="DefaultIcon.iconUrl" :shadow-url="DefaultIcon.shadowUrl" />
+    <l-popup v-if="context.editmode && !dragging">
       <div class="display-flex">
         <f7-link href="#"
                  class="text-color-blue display-flex flex-direction-column margin-right"
@@ -63,6 +64,9 @@ import { LMarker, LTooltip, LIcon, LPopup } from '@vue-leaflet/vue-leaflet'
 import { actionsMixin } from '../widget-actions'
 import { OhPlanMarkerDefinition } from '@/assets/definitions/widgets/plan'
 
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
 export default {
   mixins: [mixin, actionsMixin],
   components: {
@@ -79,12 +83,23 @@ export default {
       dragging: false
     }
   },
+  created () {
+    this.DefaultIcon = {
+      iconUrl: markerIcon,
+      shadowUrl: markerShadow
+    }
+  },
   computed: {
     coords () {
       return (this.config.coords) ? this.config.coords.split(',') : [250, 250]
     },
-    hasIcon () {
-      return this.config.icon
+    hasCustomIcon () {
+      return this.config.useTooltipAsLabel || this.config.icon
+    },
+    iconSize () {
+      if (this.hasCustomIcon) return null
+      const iconSize = this.config.iconSize || 40
+      return [iconSize, iconSize]
     },
     tooltipOptions () {
       return {

bundles/org.openhab.ui/web/src/components/config/controls/location-picker.vue
@@ -24,11 +24,15 @@ import 'leaflet/dist/leaflet.css'
 import { useUIOptionsStore } from '@/js/stores/useUIOptionsStore'
 import { mapStores } from 'pinia'
 
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
 delete Icon.Default.prototype._getIconUrl
 Icon.Default.mergeOptions({
-  iconRetinaUrl: import('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: import('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: import('leaflet/dist/images/marker-shadow.png')
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow
 })
 
 export default {

bundles/org.openhab.ui/web/src/components/widgets/map/oh-map-page.vue
@@ -40,7 +40,7 @@ import { nextTick } from 'vue'
 import { useUIOptionsStore } from '@/js/stores/useUIOptionsStore'
 
 import mixin from '../widget-mixin'
-import { tileLayer, latLng, Icon } from 'leaflet'
+import { tileLayer, latLng } from 'leaflet'
 import { LMap, LTileLayer, LFeatureGroup } from '@vue-leaflet/vue-leaflet'
 import 'leaflet/dist/leaflet.css'
 
@@ -50,13 +50,6 @@ import OhMapMarker from './oh-map-marker.vue'
 import OhMapCircleMarker from './oh-map-circle-marker.vue'
 
 import 'leaflet-providers'
-
-delete Icon.Default.prototype._getIconUrl
-Icon.Default.mergeOptions({
-  iconRetinaUrl: import('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: import('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: import('leaflet/dist/images/marker-shadow.png')
-})
 
 export default {
   mixins: [mixin],

bundles/org.openhab.ui/web/src/components/widgets/plan/oh-plan-page.vue
@@ -10,7 +10,6 @@
     :marker-zoom-animation="!config.noMarkerZoomAnimation"
     :max-bounds="bounds"
     :key="mapKey"
-    @update:bounds="ready = true"
     class="oh-plan-page-lmap"
     @ready="fitMapBounds"
     :class="{ 'with-tabbar': context.tab,
@@ -23,15 +22,15 @@
     }"
     @update:center="centerUpdate"
     @update:zoom="zoomUpdate">
-    <l-image-overlay :url="backgroundImageUrl" :bounds="bounds" />
-    <l-feature-group ref="featureGroup" v-if="context.component.slots && ready">
+    <l-image-overlay v-if="backgroundImageUrl" :url="backgroundImageUrl" :bounds="bounds" />
+    <l-feature-group ref="featureGroup" v-if="context.component.slots">
       <component v-for="(marker, idx) in markers"
                  :key="idx"
                  :is="markerComponent(marker)"
                  :context="childContext(marker)"
                  @update="onMarkerUpdate" />
     </l-feature-group>
-    <l-control v-if="context.editmode != null" position="topright">
+    <l-control v-if="context.editmode" position="topright">
       <f7-menu class="padding">
         <f7-menu-item @click="context.editmode.addWidget(context.component, 'oh-plan-marker')" icon-f7="plus" text="Add Marker" />
         <f7-menu-item v-if="context.clipboardtype"
@@ -39,7 +38,7 @@
                       icon-f7="square_on_square" />
       </f7-menu>
     </l-control>
-    <l-control v-if="context.editmode != null" position="bottomleft">
+    <l-control v-if="context.editmode" position="bottomleft">
       <span>Zoom Level: {{ currentZoom.toFixed(2) }}</span>
     </l-control>
   </l-map>
@@ -100,7 +99,6 @@ dark-tooltip()
 // Make tooltips non-interactive so touch events pass through to markers on mobile
 .oh-plan-page-lmap .leaflet-tooltip
   pointer-events none
-
 </style>
 
 <script>
@@ -108,19 +106,12 @@ import { nextTick } from 'vue'
 import { f7 } from 'framework7-vue'
 
 import mixin from '../widget-mixin'
-import { CRS, Icon } from 'leaflet'
+import { CRS } from 'leaflet'
 import { LMap, LImageOverlay, LFeatureGroup, LControl } from '@vue-leaflet/vue-leaflet'
 import 'leaflet/dist/leaflet.css'
 
 import OhPlanMarker from './oh-plan-marker.vue'
 import { OhPlanPageDefinition } from '@/assets/definitions/widgets/plan'
-
-delete Icon.Default.prototype._getIconUrl
-Icon.Default.mergeOptions({
-  iconRetinaUrl: import('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: import('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: import('leaflet/dist/images/marker-shadow.png')
-})
 
 export default {
   mixins: [mixin],
@@ -134,7 +125,6 @@ export default {
   widget: OhPlanPageDefinition,
   data () {
     return {
-      ready: false,
       currentZoom: 13,
       currentCenter: null,
       minZoom: -2,
@@ -189,7 +179,7 @@ export default {
         const isVisibleMax = isNaN(zoomVisibilityMax) || zoomVisibilityMax > this.currentZoom
         return this.context.editmode != null || (isVisibleMin && isVisibleMax)
       })
-      // only update our markers if the list has changed to avoid unessesary rendering
+      // only update our markers if the list has changed to avoid unnecessary rendering
       if (visibleMarkers.length !== this.markers.length ||
         visibleMarkers.every((e) => this.markers.indexOf(e) < 0)) {
         this.markers = visibleMarkers
@@ -204,7 +194,9 @@ export default {
     onMarkerUpdate () {
     },
     fitMapBounds () {
-      if (this.$refs.map) this.$refs.map.leafletObject?.fitBounds(this.bounds)
+      if (this.$refs.map) {
+        this.$refs.map.leafletObject?.fitBounds(this.bounds)
+      }
     },
     refreshMap () {
       this.mapKey = f7.utils.id()

bundles/org.openhab.ui/web/src/pages/settings/pages/map/map-edit.vue
@@ -91,11 +91,6 @@
             </f7-list>
           </f7-col>
         </f7-block>
-
-        <oh-map-page v-else-if="ready && previewMode"
-                     class="map-page"
-                     :context="context"
-                     :key="pageKey" />
       </f7-tab>
 
       <f7-tab id="code" :tab-active="currentTab === 'code'">
@@ -106,13 +101,13 @@
                 :value="pageYaml"
                 @input="onEditorInput" />
         <!-- <pre class="yaml-message padding-horizontal" :class="[yamlError === 'OK' ? 'text-color-green' : 'text-color-red']">{{yamlError}}</pre> -->
-
-        <oh-map-page v-if="ready && previewMode"
-                     class="map-page"
-                     :context="context"
-                     :key="pageKey + '2'" />
       </f7-tab>
     </f7-tabs>
+    <oh-map-page
+      v-if="ready && previewMode"
+      class="map-page"
+      :context="context"
+      :key="pageKey" />
   </f7-page>
 </template>
 
@@ -126,10 +121,9 @@
     position absolute
     top 80%
     white-space pre-wrap
-  .map-editor
-    .oh-map-page-lmap
-      top calc(var(--f7-navbar-height) + var(--f7-toolbar-height)) !important
-      height calc(100% - var(--f7-navbar-height) - 2 * var(--f7-toolbar-height)) !important
+  .oh-map-page-lmap
+    top calc(var(--f7-safe-area-top) + var(--f7-navbar-height) + var(--f7-toolbar-height)) !important
+    height calc(100% - var(--f7-safe-area-top) - var(--f7-navbar-height) - 2 * var(--f7-toolbar-height)) !important
   .markers-list
     .item-link
       overflow inherit

bundles/org.openhab.ui/web/src/pages/settings/pages/plan/plan-edit.vue
@@ -62,8 +62,8 @@
                             link="#"
                             @click="(ev) => configureMarker(ev, marker, context)">
                 <template #media>
-                  <oh-icon v-if="marker.config.icon"
-                           :icon="marker.config.icon"
+                  <oh-icon v-if="markerIcon(marker)"
+                           :icon="markerIcon(marker)"
                            height="32"
                            width="32" />
                 </template>
@@ -97,11 +97,6 @@
             </f7-block-footer>
           </f7-col>
         </f7-block>
-
-        <oh-plan-page v-else-if="ready && previewMode"
-                      class="plan-page"
-                      :context="context"
-                      :key="pageKey" />
       </f7-tab>
 
       <f7-tab id="code" :tab-active="currentTab === 'code'">
@@ -112,13 +107,14 @@
                 :value="pageYaml"
                 @input="onEditorInput" />
         <!-- <pre class="yaml-message padding-horizontal" :class="[yamlError === 'OK' ? 'text-color-green' : 'text-color-red']">{{yamlError}}</pre> -->
-
-        <oh-plan-page v-if="ready && previewMode"
-                      class="plan-page"
-                      :context="context"
-                      :key="pageKey + '2'" />
       </f7-tab>
     </f7-tabs>
+
+    <oh-plan-page
+      v-if="ready && previewMode"
+      class="plan-page"
+      :context="context"
+      :key="pageKey" />
   </f7-page>
 </template>
 
@@ -132,10 +128,9 @@
     position absolute
     top 80%
     white-space pre-wrap
-  .plan-editor
-    .oh-plan-page-lmap
-      top calc(var(--f7-navbar-height) + var(--f7-toolbar-height)) !important
-      height calc(100% - var(--f7-navbar-height) - 2 * var(--f7-toolbar-height)) !important
+  .oh-plan-page-lmap
+    top calc(var(--f7-safe-area-top) + var(--f7-navbar-height) + var(--f7-toolbar-height)) !important
+    height calc(100% - var(--f7-safe-area-top) - var(--f7-navbar-height) - 2 * var(--f7-toolbar-height)) !important
   .markers-list
     .item-link
       overflow inherit
@@ -161,6 +156,7 @@ import PageSettings from '@/components/pagedesigner/page-settings.vue'
 
 import ConfigSheet from '@/components/config/config-sheet.vue'
 import { useViewArea } from '@/composables/useViewArea.ts'
+import { useWidgetExpression } from '@/components/widgets/useWidgetExpression.ts'
 
 export default {
   mixins: [PageDesigner],
@@ -178,8 +174,9 @@ export default {
   },
   setup () {
     useViewArea()
+    const { evaluateExpression } = useWidgetExpression()
 
-    return { theme }
+    return { theme, evaluateExpression }
   },
   data () {
     return {
@@ -195,12 +192,10 @@ export default {
     }
   },
   methods: {
-    markerDefaultIcon (marker) {
-      const widgetDefinition = Object.values(ConfigurableWidgets).find((c) => c.widget && typeof c.widget === 'function' && c.widget().name === marker.component)
-      if (widgetDefinition) {
-        return widgetDefinition.widget().icon
-      }
-      return null
+    markerIcon (marker) {
+      if (!marker?.config?.icon) return null
+      const key = marker.component + '-' + marker.config.coords + ':icon'
+      return this.evaluateExpression(key, marker.config.icon, this.context)
     },
     addWidget (component, widgetType, parentContext, slot) {
       if (!slot) slot = 'default'

commit 25369abbf5a7a63f6576a2277b6be397053f8891

oh-map-/oh-plan-marker: Fix incorrect marker/tooltip positioning
Regression from #3687.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/map/oh-map-marker.vue
@@ -3,7 +3,7 @@
     <l-tooltip v-if="config.label">
       {{ config.label }}
     </l-tooltip>
-    <l-icon v-if="icon" :icon-size="icon.iconSize" :icon-url="icon.iconUrl" :shadow-url="icon?.shadowUrl" />
+    <l-icon v-if="icon" :icon-size="icon.iconSize" :icon-url="icon.iconUrl" />
   </l-marker>
 </template>
 
@@ -16,7 +16,6 @@ import { actionsMixin } from '../widget-actions'
 import { OhMapMarkerDefinition } from '@/assets/definitions/widgets/map'
 
 import markerIcon from 'leaflet/dist/images/marker-icon.png'
-import markerShadow from 'leaflet/dist/images/marker-shadow.png'
 
 export default {
   mixins: [mixin, actionsMixin],
@@ -52,8 +51,7 @@ export default {
         this.markerKey = f7.utils.id()
         return {
           iconUrl: markerIcon,
-          shadowUrl: markerShadow,
-          iconSize: null
+          iconSize: [25,41],
         }
       }
 

bundles/org.openhab.ui/web/src/components/widgets/plan/oh-plan-marker.vue
@@ -25,7 +25,7 @@
         :height="config.iconHeight || config.iconSize || 40"
         :state="config.iconUseState ? state : undefined" />
     </l-icon>
-    <l-icon v-else :icon-url="DefaultIcon.iconUrl" :shadow-url="DefaultIcon.shadowUrl" />
+    <l-icon v-else :icon-url="DefaultIcon.iconUrl" :icon-size="DefaultIcon.iconSize" />
     <l-popup v-if="context.editmode && !dragging">
       <div class="display-flex">
         <f7-link
@@ -70,7 +70,6 @@ import { actionsMixin } from '../widget-actions'
 import { OhPlanMarkerDefinition } from '@/assets/definitions/widgets/plan'
 
 import markerIcon from 'leaflet/dist/images/marker-icon.png'
-import markerShadow from 'leaflet/dist/images/marker-shadow.png'
 
 export default {
   mixins: [mixin, actionsMixin],
@@ -91,7 +90,7 @@ export default {
   created () {
     this.DefaultIcon = {
       iconUrl: markerIcon,
-      shadowUrl: markerShadow
+      iconSize: [25, 41],
     }
   },
   computed: {
@@ -102,7 +101,6 @@ export default {
       return this.config.useTooltipAsLabel || this.config.icon
     },
     iconSize () {
-      if (this.hasCustomIcon) return null
       const iconSize = this.config.iconSize || 40
       return [iconSize, iconSize]
     },