08c989258db35d61ac1f939a286dd2f5e48150fa
Jeff James <jeff@james-online.com>
2026-01-13 10:33:06 -0800

    Script editor: Allow return outside functions/global return (#3656)
    JS Scripting supports global return thanks to the wrapper, but ESLint
    displays a parser error.
    
    Reported on the community:
    https://community.openhab.org/t/openhab-5-1-release-discussion/167620/53
    
    Signed-off-by: Florian Hotze <dev@florianhotze.com>
    
    oh-swiper: Don't set swiper-slide width to 100% (#3653)
    
    Fixes a style regression of #3350.
    
    Reported on the community:
    
    https://community.openhab.org/t/5-1-0-m3-main-ui-shows-blank-screen-on-android/167432/41
    
    Signed-off-by: Florian Hotze <dev@florianhotze.com>
    
    Draft - initial implementation of api using @hey-api
    Additional edits
    
    updated propertiesRequiredByDefault
    
    Updates to openapi spec per proposed changes in core.
    
    import * as api so its easy to identify api calls/types
    
    Updated to have responseStyle: 'data'
    
    Implement optional for ConfigDescriptionParameter and ConfigDescriptionParameterGroup (based on updates to core api)
    
    Signed-off-by: Jeff James <jeff@james-online.com>
    
    WIP
    
    Signed-off-by: Jeff James <jeff@james-online.com>
    
    Ready for review
    
    Signed-off-by: Jeff James <jeff@james-online.com>
    
    oxc-update
    
    run format
    
    Signed-off-by: Jeff James <jeff@james-online.com>

---
+++ bundles/org.openhab.ui/web/build/generate-rest-client.mjs
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+import fetch from 'node-fetch'
+import fs from 'node:fs'
+import { createClient } from '@hey-api/openapi-ts'
+
+// const url = `https://api.swaggerhub.com/apis/wborn/openhab-rest-api/8/swagger.json`
+// const url = `https://demo.openhab.org/rest/spec`
+const url = null
+const file = './build/openhab-api-spec.json'
+
+const outpath = './src/api'
+
+let openApi = null
+
+if (url) {
+  try {
+    openApi = await fetch(url).then(res => res.json())
+  } catch (error) {
+    console.error('Error fetching OpenAPI spec:', error);
+  }
+}
+
+if (file) {
+  console.log('Reading OpenAPI spec from file:', file);
+  try {
+    const data = fs.readFileSync(file, 'utf8')
+    openApi = JSON.parse(data)
+  } catch (error) {
+    console.error('Error reading OpenAPI spec from file:', error);
+  }
+}
+
+try {
+  await createClient({
+    client: "@hey-api/client-fetch",
+    input: openApi,
+    output: outpath,
+    parser: {
+      transforms: {
+        propertiesRequiredByDefault: true
+      }
+    },
+    plugins: [
+      {
+        name: "@hey-api/sdk",
+        responseStyle: 'data',
+        paramsStructure: 'flat'
+      }
+    ]
+  })
+} catch (error) {
+  console.error('Error creating client:', error)
+}
