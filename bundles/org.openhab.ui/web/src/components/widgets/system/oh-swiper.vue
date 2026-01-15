openhab/openhab-webui

commit 2589928667e84ce187aade79a75d5c5faf7ebb7a

oh-swiper: Don't set swiper-slide width to 100% (#3653)
Fixes a style regression of #3350.

Reported on the community:

https://community.openhab.org/t/5-1-0-m3-main-ui-shows-blank-screen-on-android/167432/41

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/system/oh-swiper.vue
@@ -69,6 +69,11 @@
   </f7-swiper>
 </template>
 
+<style lang="stylus">
+.oh-swiper-slide
+  width unset !important
+</style>
+
 <script>
 import { defineAsyncComponent } from 'vue'
 

commit 70362c7abde15157d153723b534d05a649940442

oh-swiper: Only unset width is slidesPerView set to auto
Regression from #3653.
When setting a fixed size per slide, it is not applied anymore as !important overwrites it.
Reported on the community: https://community.openhab.org/t/universal-toggle/115500/29

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/system/oh-swiper.vue
@@ -5,7 +5,7 @@
       v-for="(slide, idx) in slides"
       :key="'editmode-' + idx"
       class="oh-swiper-slide"
-      :class="{ 'edit-mode': context.editmode }">
+      :class="{ 'unset-width': mergedConfig.slidesPerView === 'auto', 'edit-mode': context.editmode }">
       <f7-menu v-if="context.editmode" class="configure-layout-menu padding-horizontal">
         <f7-menu-item style="margin-left: auto" icon-f7="rectangle_on_rectangle" dropdown>
           <f7-menu-dropdown right>
@@ -65,8 +65,8 @@
 </template>
 
 <style lang="stylus">
-.oh-swiper-slide
-  width unset !important
+.oh-swiper-slide.unset-width
+    width unset !important
 </style>
 
 <script>