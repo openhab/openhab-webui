openhab/openhab-webui

commit 959c711bcdcf8d509761268659d484f3c1310d11

oh-list: Fix various issues when editing (#3741)
Reported on the community: https://community.openhab.org/t/openhab-5-1-release-discussion/167620/363

bundles/org.openhab.ui/web/src/pages/settings/pages/layout/layout-edit.vue
@@ -257,11 +257,13 @@ export default {
     f7.off('svgOnclickConfigUpdate', this.onSvgOnClickConfigUpdate)
   },
   methods: {
-    addWidget (component, widgetType, parentContext, slot) {
+    addWidget (component, widgetType, parentContext, slot = 'default') {
       const isList = component.component.indexOf('oh-list') === 0
       const isCells = component.component.indexOf('oh-grid-cells') === 0
-      if (!slot) slot = 'default'
-      if (!component.slots) component.slots = {}
+      if (!component.slots) {
+        console.warn(`slots property is missing on ${component.component}! If adding children fails, add the slots property manually in the code tab:\nslots: {}`)
+        component.slots = {}
+      }
       if (!component.slots[slot]) component.slots[slot] = []
       if (widgetType) {
         component.slots[slot].push({
@@ -275,7 +277,8 @@ export default {
         const doAddWidget = (choice) => {
           component.slots[slot].push({
             component: choice,
-            config: {}
+            config: {},
+            slots: { default: [] }
           })
           nextTick(() => actions.destroy())
           this.forceUpdate()
@@ -323,7 +326,6 @@ export default {
           }
         }).sort((a, b) => a.text.localeCompare(b.text))
         actions = f7.actions.create({
-          // grid: true,
           buttons: [
             [
               {
@@ -351,7 +353,8 @@ export default {
               { color: 'red', 'text': 'Cancel', close: true }
             ]
           ]
-        }).open()
+        })
+        actions.open()
       }
     },
     doAddFromModel (value) {

bundles/org.openhab.ui/web/src/components/widgets/standard/list/default-list-item.js
@@ -165,6 +165,8 @@ export default function itemDefaultListComponent (item, footer) {
   }
   if (!component.config.item) component.config.item = item.name
   if (!component.config.title) component.config.title = item.label || item.name
+  if (!component.slots) component.slots = { default: [] }
+  if (!component.slots.default) component.slots.default = []
   if (item.category && !component.config.icon) component.config.icon = item.category
   // Only enable dynamic icon by default for Item types with good support for dynamic icons and "predictable" states
   const discreteItemTypes = ['Contact', 'Dimmer', 'Rollershutter', 'Switch']

bundles/org.openhab.ui/web/src/components/widgets/system/oh-list.vue
@@ -1,16 +1,15 @@
 <template>
   <f7-list v-bind="config"
-           :title="null"
-           :style="{ 'z-index': context.editmode ? 'inherit' : undefined }">
+           :title="null">
     <template #before-list>
       <f7-menu v-if="context.editmode" class="configure-layout-menu margin-vertical padding-left">
         <f7-menu-item @click="context.editmode.addWidget(context.component)" icon-f7="plus" />
-        <f7-menu-item style="margin-left: auto" icon-f7="square_list" dropdown>
+        <f7-menu-item v-if="context.clipboardtype"
+                      style="margin-left: auto"
+                      icon-f7="square_list"
+                      dropdown>
           <f7-menu-dropdown right>
-            <f7-menu-dropdown-item @click="context.editmode.configureWidget(context.component, context.parent, 'oh-list')" href="#" text="Configure List" />
-            <f7-menu-dropdown-item v-if="context.clipboardtype" divider />
-            <f7-menu-dropdown-item v-if="context.clipboardtype"
-                                   @click="context.editmode.pasteWidget(context.component, context.parent)"
+            <f7-menu-dropdown-item @click="context.editmode.pasteWidget(context.component, context.parent)"
                                    href="#"
                                    text="Paste Item" />
           </f7-menu-dropdown>
@@ -19,33 +18,35 @@
     </template>
     <ul v-if="context.component.slots && context.component.slots.default">
       <template v-for="(slotComponent, idx) in context.component.slots.default" :key="idx">
-        <f7-menu v-if="context.editmode" style="float: left">
-          <f7-menu-item icon-f7="list_bullet" class="margin-left configure-layout-menu" dropdown>
-            <f7-menu-dropdown>
-              <f7-menu-dropdown-item @click="context.editmode.configureWidget(context.component.slots.default[idx], context)" href="#" text="Configure Item" />
-              <f7-menu-dropdown-item @click="context.editmode.editWidgetCode(context.component.slots.default[idx], context)" href="#" text="Edit YAML" />
-              <f7-menu-dropdown-item v-if="context.parent.component.config.accordionList"
-                                     @click="context.editmode.editWidgetCode(context.component.slots.default[idx], context, 'accordion')"
-                                     href="#"
-                                     text="Edit Accordion Code" />
-              <f7-menu-dropdown-item divider />
-              <f7-menu-dropdown-item @click="context.editmode.cutWidget(context.component.slots.default[idx], context)" href="#" text="Cut" />
-              <f7-menu-dropdown-item @click="context.editmode.copyWidget(context.component.slots.default[idx], context)" href="#" text="Copy" />
-              <f7-menu-dropdown-item v-if="idx > 0 || idx < context.component.slots.default.length - 1" divider />
-              <f7-menu-dropdown-item v-if="idx > 0"
-                                     @click="context.editmode.moveWidgetUp(context.component.slots.default[idx], context)"
-                                     href="#"
-                                     text="Move Up" />
-              <f7-menu-dropdown-item v-if="idx < context.component.slots.default.length - 1"
-                                     @click="context.editmode.moveWidgetDown(context.component.slots.default[idx], context)"
-                                     href="#"
-                                     text="Move Down" />
-              <f7-menu-dropdown-item divider />
-              <f7-menu-dropdown-item @click="context.editmode.removeWidget(context.component.slots.default[idx], context)" href="#" text="Remove Item" />
-            </f7-menu-dropdown>
-          </f7-menu-item>
-        </f7-menu>
-        <generic-widget-component :context="childContext(slotComponent)" />
+        <div style="display: flex; align-items: center;">
+          <f7-menu v-if="context.editmode">
+            <f7-menu-item icon-f7="list_bullet" class="margin-left configure-layout-menu" dropdown>
+              <f7-menu-dropdown>
+                <f7-menu-dropdown-item @click="context.editmode.configureWidget(context.component.slots.default[idx], context)" href="#" text="Configure Item" />
+                <f7-menu-dropdown-item @click="context.editmode.editWidgetCode(context.component.slots.default[idx], context)" href="#" text="Edit YAML" />
+                <f7-menu-dropdown-item v-if="context.parent.component.config.accordionList"
+                                       @click="context.editmode.editWidgetCode(context.component.slots.default[idx], context, 'accordion')"
+                                       href="#"
+                                       text="Edit Accordion Code" />
+                <f7-menu-dropdown-item divider />
+                <f7-menu-dropdown-item @click="context.editmode.cutWidget(context.component.slots.default[idx], context)" href="#" text="Cut" />
+                <f7-menu-dropdown-item @click="context.editmode.copyWidget(context.component.slots.default[idx], context)" href="#" text="Copy" />
+                <f7-menu-dropdown-item v-if="idx > 0 || idx < context.component.slots.default.length - 1" divider />
+                <f7-menu-dropdown-item v-if="idx > 0"
+                                       @click="context.editmode.moveWidgetUp(context.component.slots.default[idx], context)"
+                                       href="#"
+                                       text="Move Up" />
+                <f7-menu-dropdown-item v-if="idx < context.component.slots.default.length - 1"
+                                       @click="context.editmode.moveWidgetDown(context.component.slots.default[idx], context)"
+                                       href="#"
+                                       text="Move Down" />
+                <f7-menu-dropdown-item divider />
+                <f7-menu-dropdown-item @click="context.editmode.removeWidget(context.component.slots.default[idx], context)" href="#" text="Remove Item" />
+              </f7-menu-dropdown>
+            </f7-menu-item>
+          </f7-menu>
+          <generic-widget-component :context="childContext(slotComponent)" style="flex-grow: 1;" />
+        </div>
       </template>
     </ul>
   </f7-list>

bundles/org.openhab.ui/web/src/components/widgets/widget-actions.js
@@ -86,11 +86,11 @@ export const actionsMixin = {
       prefix = (actionPropsParameterGroup) ? actionPropsParameterGroup.replace(/action/gi, '') : prefix
       prefix = (prefix) ? prefix += '_' : ''
       const action = actionConfig[prefix + 'action']
+      if (!action) return
       if (context.editmode) {
         this.showActionFeedback(prefix, actionConfig, `Action '${action}' not performed while in edit mode`)
         return
       }
-      if (!action) return
 
       const actionConfirmation = actionConfig[prefix + 'actionConfirmation']
       const confirmationPromise = (actionConfirmation) ? this.requestActionConfirmation(prefix, actionConfig) : Promise.resolve()

commit bc650e980d886d07d3e9ed63883907bde1d33691

Widgets: Fix default slots overwritten with empty array
Regression from #3741.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/js/stores/useComponentsStore.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { readonly, ref } from 'vue'
 
 import api from '@/js/openhab/api'
 
@@ -20,21 +20,23 @@ export const useComponentsStore = defineStore('components', () => {
   const ready = ref<boolean>(false)
 
   // Getters
-  function widget(uid: string) {
-    return _widgets.value.find((widget) => widget.uid === uid)
+  function widget(uid: string): Readonly<Widget> | null {
+    const widget = _widgets.value.find((widget) => widget.uid === uid)
+    return widget ? readonly(widget) : null
   }
 
-  function widgets() {
-    return _widgets.value.sort((a, b) => a.uid.localeCompare(b.uid))
+  function widgets(): Readonly<Widget[]> {
+    return readonly(_widgets.value.sort((a, b) => a.uid.localeCompare(b.uid)))
   }
 
-  function page(uid: string) {
-    return _pages.value.find((page) => page.uid === uid)
+  function page(uid: string): Readonly<Page> | null {
+    const page = _pages.value.find((page) => page.uid === uid)
+    return page ? readonly(page) : null
   }
 
-  function pages(): Page[] {
+  function pages(): Readonly<Page[]> {
     const pages = _pages.value.sort((a, b) => a.uid.localeCompare(b.uid))
-    return pages
+    return readonly(pages)
   }
 
   // Actions

bundles/org.openhab.ui/web/src/pages/settings/pages/layout/layout-edit.vue
@@ -273,7 +273,7 @@ export default {
           component.slots[slot].push({
             component: choice,
             config: {},
-            slots: { default: [] }
+            slots: choice === 'oh-list-card' ? { default: [] } : undefined
           })
           nextTick(() => actions.destroy())
           this.forceUpdate()