eb7fd3b53a2c361e05f3f22fab9ee0700f6ca0de
Nadahar <Nadahar@users.noreply.github.com>
2026-01-30 15:26:30 +0100

    Add failure handing to selected REST endpoints (#3710)
    This is related to https://github.com/openhab/openhab-core/pull/5236.
    I've been trying to test and prepare for the changes to the REST API
    that are being discussed there.
    
    I have some reservations on how I've chosen to handle errors here. I
    don't really know what is the desired reaction, I've made them "Back"
    out where I thought that made sense, and take other actions where I
    thought that made more sense. But, all didn't work like I wanted it to,
    in particular the pages where you have to select a Thing/Item and the
    failure reveals itself after the selection. I tried to get them to
    "reset" the selected element, but it didn't quite work as I wanted it
    to. Neither of them actually cleared the selected text, and neither of
    them stopped the "loading spinner" animation. But, neither of these
    situations are very likely to occur in the first place, and there's a
    lot of missing error handling in MainUI in general, so this is hardly
    worse than what's already there.
    
    If somebody has some suggestions for improved failure handling, please
    let me know and I'll try to adapt.
    
    ---------
    
    Signed-off-by: Ravi Nadahar <nadahar@rediffmail.com>
    Signed-off-by: Jeff James <jeff@james-online.com>
    Co-authored-by: Ravi Nadahar <nadahar@rediffmail.com>
    Co-authored-by: Jeff James <jeff@james-online.com>

--- bundles/org.openhab.ui/web/src/pages/settings/things/channel/channel-add.vue
+++ bundles/org.openhab.ui/web/src/pages/settings/things/channel/channel-add.vue
@@ -81,6 +81,9 @@ export default {
       Promise.all(promises).then((ct) => {
         this.channelTypes = ct
         this.ready = true
+      }).catch((err) => {
+        f7.dialog.alert('Error loading channel type: ' + err)
+        this.f7router.back()
       })
     },
     save () {
