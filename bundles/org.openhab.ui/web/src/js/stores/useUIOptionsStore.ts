openhab/openhab-webui

commit c860ac24c8dc15c1bfaafaf52c8da6ac96de9fa5

useUIOptionsStore: Make darkMode reactive (#3788)
Fixes #3787.
Resolves #3757.

- Made darkMode a computed property so it is now reactive across
  the UI with both a getter and setter
- Changed all references from getDarkMode() to darkMode 
- Changed "Dark Mode" title on About page to "Appearance"
- Centralized all f7 darkMode operations into useUIOptionsStore

---------

Signed-off-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/js/stores/useUIOptionsStore.ts
@@ -1,15 +1,18 @@
 import { defineStore } from 'pinia'
-import { ref, watch } from 'vue'
+import { computed, ref, watch } from 'vue'
 import { Dom7 } from 'framework7'
-import { f7 } from 'framework7-vue'
+import { f7, f7ready } from 'framework7-vue'
+
+type StoredDarkModeType = 'auto' | 'dark' | 'light'
 
 export const useUIOptionsStore = defineStore('uiOptions', () => {
   // States
   // shared with basicUI
   const _storedDarkMode = localStorage.getItem('openhab.ui:theme.dark')
-  const storedDarkMode = ref<'auto' | 'dark' | 'light'>(
-    _storedDarkMode === 'auto' || _storedDarkMode === 'dark' || _storedDarkMode === 'light' ? _storedDarkMode : 'auto'
+  const storedDarkMode = ref<StoredDarkModeType>(
+    ['auto', 'dark', 'light'].includes(_storedDarkMode as any) ? (_storedDarkMode as StoredDarkModeType) : 'auto'
   )
+  const darkModeChange = ref<number>(0) // Used to trigger recomputation of darkMode
 
   const _storedBars = localStorage.getItem('openhab.ui:theme.bars') || 'light'
   const bars = ref<'light' | 'filled'>(['light', 'filled'].includes(_storedBars as any) ? (_storedBars as 'light' | 'filled') : 'light')
@@ -50,33 +53,44 @@ export const useUIOptionsStore = defineStore('uiOptions', () => {
 
   const sitemapShowItemName = ref<boolean>(localStorage.getItem('openhab.ui:sitemap.showItemName') === 'true')
 
-  // Getters
-  function getDarkMode() {
-    if (storedDarkMode.value === 'auto') {
-      return typeof window.OHApp?.preferDarkMode === 'function' ? window.OHApp.preferDarkMode() : f7.darkMode ? 'dark' : 'light'
+  const darkMode = computed({
+    get: (): 'dark' | 'light' => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+      darkModeChange.value // darkModeChange to force re-computation
+      if (storedDarkMode.value === 'auto') {
+        if (typeof window.OHApp?.preferDarkMode === 'function') {
+          return window.OHApp.preferDarkMode() ? 'dark' : 'light'
+        }
+        return f7.darkMode ? 'dark' : 'light'
+      }
+      return storedDarkMode.value
+    },
+    set: (value: StoredDarkModeType) => {
+      storedDarkMode.value = value
+      if (value === 'auto') {
+        f7.enableAutoDarkMode()
+        localStorage.removeItem('openhab.ui:theme.dark')
+      } else {
+        f7.disableAutoDarkMode()
+        localStorage.setItem('openhab.ui:theme.dark', value)
+      }
+
+      bars.value = 'light' // Reset bars to light when dark mode changes
+      updateClasses()
     }
+  })
 
-    return storedDarkMode.value
-  }
+  f7ready(() => {
+    updateClasses()
+    f7.on('darkModeChange', () => {
+      darkModeChange.value++
+      updateClasses()
+    })
+  })
 
+  // Getters
   function isAutoDarkMode() {
     return storedDarkMode.value === 'auto'
-  }
-
-  // Actions
-  function setDarkMode(value: 'auto' | 'dark' | 'light') {
-    storedDarkMode.value = value
-
-    if (value === 'auto') {
-      f7.enableAutoDarkMode()
-      localStorage.removeItem('openhab.ui:theme.dark')
-    } else {
-      f7.disableAutoDarkMode()
-      localStorage.setItem('openhab.ui:theme.dark', value)
-    }
-
-    bars.value = 'light' // Reset bars to light when dark mode changes
-    updateClasses()
   }
 
   watch(bars, (newValue) => {
@@ -140,7 +154,7 @@ export const useUIOptionsStore = defineStore('uiOptions', () => {
   })
 
   function updateClasses() {
-    if (getDarkMode() === 'dark') {
+    if (darkMode.value === 'dark') {
       Dom7('html').addClass('dark')
     } else {
       Dom7('html').removeClass('dark')
@@ -159,7 +173,7 @@ export const useUIOptionsStore = defineStore('uiOptions', () => {
 
   function themeOptions() {
     return {
-      dark: getDarkMode(),
+      dark: darkMode,
       autoDarkMode: isAutoDarkMode(),
       bars: bars.value,
       homeNavBar: homeNavBar.value,
@@ -175,8 +189,7 @@ export const useUIOptionsStore = defineStore('uiOptions', () => {
 
   return {
     storedDarkMode,
-    getDarkMode,
-    setDarkMode,
+    darkMode,
     isAutoDarkMode,
     bars,
     homeNavBar,

bundles/org.openhab.ui/web/src/App.vue
@@ -13,7 +13,7 @@
       <f7-page>
         <f7-link href="/overview/" class="openhab-logo no-ripple" panel-close>
           <div class="logo-inner">
-            <img v-if="uiOptionsStore.getDarkMode() === 'dark'" src="@/images/openhab-logo-white.svg" type="image/svg+xml" width="196px" />
+            <img v-if="uiOptionsStore.darkMode === 'dark'" src="@/images/openhab-logo-white.svg" type="image/svg+xml" width="196px" />
             <img v-else src="@/images/openhab-logo.svg" type="image/svg+xml" width="196px" />
           </div>
         </f7-link>
@@ -843,7 +843,6 @@ export default {
       }
     },
     updateThemeOptions () {
-      useUIOptionsStore().updateClasses()
       if (useUIOptionsStore().visibleBreakpointDisabled) {
         nextTick(() => {
           f7.panel.get('left').disableVisibleBreakpoint()
@@ -1072,10 +1071,6 @@ export default {
 
       f7.on('addonChange', () => {
         this.loadData()
-      })
-
-      f7.on('darkModeChange', () => {
-        this.updateThemeOptions()
       })
 
       f7.on('toggleDeveloperDock', () => {

bundles/org.openhab.ui/web/src/components/addons/addon-card.vue
@@ -32,7 +32,7 @@
           <f7-icon
             v-if="addon.verifiedAuthor"
             size="15"
-            :color="uiOptionsStore.getDarkMode() === 'dark' ? 'white' : 'blue'"
+            :color="uiOptionsStore.darkMode === 'dark' ? 'white' : 'blue'"
             f7="checkmark_seal_fill"
             style="margin-top: -3px;" />
         </div>

bundles/org.openhab.ui/web/src/components/addons/addon-list-item.vue
@@ -6,7 +6,7 @@
         <f7-icon
           v-if="addon.verifiedAuthor"
           size="15"
-          :color="uiOptionsStore.getDarkMode() === 'dark' ? 'white' : 'blue'"
+          :color="uiOptionsStore.darkMode === 'dark' ? 'white' : 'blue'"
           f7="checkmark_seal_fill"
           style="margin-top: -3px" />
       </div>

bundles/org.openhab.ui/web/src/components/config/controls/blockly-editor.vue
@@ -1201,9 +1201,9 @@ export default {
   computed: {
     cssVars () {
       return {
-        '--blockly-ws-search-bg-color': useUIOptionsStore().getDarkMode() === 'dark' ? '#1e1e1e' : 'white',
-        '--blockly-ws-search-border-color': useUIOptionsStore().getDarkMode() === 'dark' ? 'lightgrey' : 'grey',
-        '--blockly-ws-search-text-color': useUIOptionsStore().getDarkMode() === 'dark' ? 'white' : 'black'
+        '--blockly-ws-search-bg-color': useUIOptionsStore().darkMode === 'dark' ? '#1e1e1e' : 'white',
+        '--blockly-ws-search-border-color': useUIOptionsStore().darkMode === 'dark' ? 'lightgrey' : 'grey',
+        '--blockly-ws-search-text-color': useUIOptionsStore().darkMode === 'dark' ? 'white' : 'black'
       }
     },
     ...mapStores(useUIOptionsStore)
@@ -1287,7 +1287,7 @@ export default {
       const options = {
         toolbox: this.$refs.toolbox,
         horizontalLayout: !this.$device.desktop,
-        theme: useUIOptionsStore().getDarkMode() === 'dark' ? DarkTheme : undefined,
+        theme: useUIOptionsStore().darkMode === 'dark' ? DarkTheme : undefined,
         zoom: {
           controls: true,
           wheel: true,

bundles/org.openhab.ui/web/src/components/config/controls/location-picker.vue
@@ -41,7 +41,7 @@ export default {
       zoom: 1,
       center: latLng(48, 6),
       // url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      url: `https://a.basemaps.cartocdn.com/${useUIOptionsStore().getDarkMode()}_all/{z}/{x}/{y}.png`,
+      url: `https://a.basemaps.cartocdn.com/${useUIOptionsStore().darkMode}_all/{z}/{x}/{y}.png`,
       attribution: '&copy; <a class="external" target="_blank" href="http://osm.org/copyright">OpenStreetMap</a>, &copy; <a class="external" target="_blank" href="https://carto.com/attribution/">CARTO</a>',
       marker: null,
       mapOptions: {

bundles/org.openhab.ui/web/src/components/config/controls/script-editor.vue
@@ -344,7 +344,7 @@ export default {
         ...autocompletions,
         linter,
         linter && lintGutter(),
-        useUIOptionsStore().getDarkMode() === 'dark' ? gruvboxDark : null
+        useUIOptionsStore().darkMode === 'dark' ? gruvboxDark : null
       ].filter((ext) => ext)
 
       return extensions

bundles/org.openhab.ui/web/src/components/developer/developer-dock.vue
@@ -1,7 +1,7 @@
 <template>
   <f7-page class="developer-dock">
     <!-- note: title is left aligned in Material theme, this is no bug -->
-    <f7-navbar title="Developer Sidebar" subtitle="(Shift+Alt+D)" :color="uiOptionsStore.getDarkMode() === 'dark' ? '' : 'black'" />
+    <f7-navbar title="Developer Sidebar" subtitle="(Shift+Alt+D)" :color="uiOptionsStore.darkMode === 'dark' ? '' : 'black'" />
     <f7-segmented
       strong
       tag="p"

commit bbf869ad6688602009f1475f4251c72d7259e718

useUiOptionsStore: Fix darkMode on startup (#3825)
Fixes #3824.
Regression from #3788

Signed-off-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/js/stores/useUIOptionsStore.ts
@@ -82,6 +82,7 @@ export const useUIOptionsStore = defineStore('uiOptions', () => {
   })
 
   f7ready(() => {
+    darkModeChange.value++ // trigger computed darkMode now f7 is ready
     updateClasses()
     f7.on('darkModeChange', () => {
       darkModeChange.value++