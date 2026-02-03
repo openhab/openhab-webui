8aa20b83993a879fa95728b6620689f41ac4b551
maniac103 <dannybaumann@web.de>
2026-02-03 16:18:31 +0100

    Fix auto dark mode selection in Android app (#3838)
    The app callback 'preferDarkMode()' doesn't return a boolean, but a
    string ('light' or 'dark'). Since either string evaluates truthy, the UI
    always selected dark mode.
    
    Fixes #3835
    
    ---------
    
    Signed-off-by: Danny Baumann <dannybaumann@web.de>
    Signed-off-by: Jeff James <jeff@james-online.com>
    Co-authored-by: Jeff James <jeff@james-online.com>

--- bundles/org.openhab.ui/web/src/types/OHApp.d.ts
+++ bundles/org.openhab.ui/web/src/types/OHApp.d.ts
@@ -1,8 +1,8 @@
 // Android app: https://github.com/openhab/openhab-android/blob/c7b942c/mobile/src/main/java/org/openhab/habdroid/ui/activity/AbstractWebViewFragment.kt#L401
 // iOS app: https://github.com/openhab/openhab-ios/blob/da9a7e2/openHAB/OpenHABWebViewController.swift#L37
 interface OHApp {
-  preferTheme?: () => string
-  preferDarkMode?: () => string
+  preferTheme?: () => 'md' | 'ios' | 'aurora'
+  preferDarkMode?: () => 'light' | 'dark'
   exitToApp?: () => void
   goFullscreen?: () => void
   pinToHome?: () => void
