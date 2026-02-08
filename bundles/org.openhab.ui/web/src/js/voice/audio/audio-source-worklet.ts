08a80c071224dcf31a2cd98c908470c50a632737
Florian Hotze <dev@florianhotze.com>
2026-02-08 19:52:15 +0100

    Merge multiple input channels instead of stripping
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- bundles/org.openhab.ui/web/src/js/voice/audio/audio-source-worklet.ts
+++ bundles/org.openhab.ui/web/src/js/voice/audio/audio-source-worklet.ts
@@ -1,13 +1,32 @@
 /**
+ * Merges multiple audio channels into a single channel using Quadratic Mean/Equal Power Scaling.
+ * @param input
+ */
+function mergeChannels(input: Float32Array[]): Float32Array {
+  const channels = input.length
+  if (channels === 1) return input[0]!
+  const samples = input[0]!.length
+  const mixRatio = Math.sqrt(channels) / channels
+
+  const output = new Float32Array(samples)
+
+  for (let s = 0; s < samples; s++) {
+    let sum = 0
+    for (let c = 0; c < channels; c++) {
+      sum += input[c]![s]!
+    }
+    output[s] = sum * mixRatio
+  }
+  return output
+}
+
+/**
  * The class {@link AudioSourceWorklet}, is an {@link AudioWorkletProcessor} implementation that sends the incoming audio data through a {@link MessagePort}.
  */
 class AudioSourceWorklet extends AudioWorkletProcessor {
   process(inputs: Float32Array[][]) {
-    if (inputs[0]!.length) {
-      // only supports one channel
-      // TODO: Merge channels instead of stripping
-      this.port.postMessage(new Float32Array(inputs[0]![0]!))
-    }
+    const merged = mergeChannels(inputs[0]!)
+    this.port.postMessage(merged, [merged.buffer])
     return true
   }
 }
