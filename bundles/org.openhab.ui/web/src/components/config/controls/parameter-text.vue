db4a1e4d1a3bf399153994e09dc9d68d68099f79
Andrew Fiddian-Green <software@whitebear.ch>
2026-02-25 15:08:06 +0000

    add support for pure ip-address
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>

--- bundles/org.openhab.ui/web/src/components/config/controls/parameter-text.vue
+++ bundles/org.openhab.ui/web/src/components/config/controls/parameter-text.vue
@@ -107,18 +107,25 @@ export default {
         return `(?:(?:${hex2}[:\\-]){5}${hex2}|(?:${hex4}\\.){2}${hex4})`
       }
 
-      // handle network-address and url as synonyms
-      if (ctx === 'url' || ctx === 'network-address') {
-        // matches both a.b.c.d and 1.2.3.4 so covers ipv4 address pattern as well
-        const hostIpv4 = `(?:[A-Za-z0-9](?:[A-Za-z0-9\\-]{0,61}[A-Za-z0-9])?(?:\\.[A-Za-z0-9](?:[A-Za-z0-9\\-]{0,61}[A-Za-z0-9])?)*)`
-        const ipv6Addr = `(?:\\[(?:(?:[A-Fa-f0-9]{1,4}:){7}[A-Fa-f0-9]{1,4}|(?:[A-Fa-f0-9]{1,4}:){1,7}:|:(?::[A-Fa-f0-9]{1,4}){1,7}|(?:[A-Fa-f0-9]{1,4}:){1,6}:[A-Fa-f0-9]{1,4}|::)\\])`
-        const http = `(?:[Hh][Tt][Tt][Pp][Ss]?:\\/\\/)?`
-        const host = `(?:${hostIpv4}|${ipv6Addr})(?![A-Za-z0-9\\-\\.])`
-        const port = `(?::(?:6553[0-5]|655[0-2]\\d|65[0-4]\\d{2}|6[0-4]\\d{3}|[1-5]\\d{4}|\\d{1,4}))?`
-        const pathQuery = `(?:\\/[A-Za-z0-9\\-\\.\\!\\(\\)\\/\\*\\?+,;=:@%_~$&']*)*`
-        return `${http}${host}${port}${pathQuery}`
+      if (ctx === 'url' || ctx === 'network-address' || ctx === 'ip-address') {
+        const ipv4Addr = `((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])`
+        const ipv6Addr = `(?:(?:[A-Fa-f0-9]{1,4}:){7}[A-Fa-f0-9]{1,4}|(?:[A-Fa-f0-9]{1,4}:){1,7}:|:(?::[A-Fa-f0-9]{1,4}){1,7}|(?:[A-Fa-f0-9]{1,4}:){1,6}:[A-Fa-f0-9]{1,4}|::)`
+
+        if (ctx === 'ip-address') {
+          // handle "pure" ip address cases; ipv4, ipv6, without port suffix
+          return `(?:${ipv4Addr}|${ipv6Addr})(?![A-Za-z0-9\\-\\.])`
+        } else {
+          // handle network-address and url (synonyms)
+          // hostIpv4 matches both a.b.c.d and 1.2.3.4 so covers ipv4 address pattern as well
+          const hostIpv4 = `(?:[A-Za-z0-9](?:[A-Za-z0-9\\-]{0,61}[A-Za-z0-9])?(?:\\.[A-Za-z0-9](?:[A-Za-z0-9\\-]{0,61}[A-Za-z0-9])?)*)`
+          const http = `(?:[Hh][Tt][Tt][Pp][Ss]?:\\/\\/)?`
+          const host = `(?:${hostIpv4}|(?:\\[${ipv6Addr}\\]))(?![A-Za-z0-9\\-\\.])`
+          const port = `(?::(?:6553[0-5]|655[0-2]\\d|65[0-4]\\d{2}|6[0-4]\\d{3}|[1-5]\\d{4}|\\d{1,4}))?`
+          const rest = `(?:\\/[^\\s]*)`
+          return `${http}${host}${port}${rest}`
+        }
+        return this.configDescription.pattern
       }
-      return this.configDescription.pattern
     },
     multiple () {
       return this.configDescription?.multiple
