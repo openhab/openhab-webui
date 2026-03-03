openhab/openhab-webui

commit 4c5420421dbf587aaeabf9d60c68b1ceb6963a86

Refactor widget-mixin to TS composable (#3862)
Fixes #3919.

* Refactors the widget-mixin to a TypeScript composable.
  This is a precondition for refactoring widget components to TypeScript.
  In Composition API, components must be registered globally if referenced
  by name - hence all widgets are registered globally.
  Note, there is no performance impact here.
* Adds TypeScript definitions for widget definitions.
* Removed special title overflow in oh-list-item for divider as CSS
  text-overflow does the same operation and is now part of the F7 styles.

---------

Also-by: Florian Hotze <dev@florianhotze.com>
Signed-off-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/components/widgets/layout/oh-canvas-item.vue
@@ -32,20 +32,20 @@
               href="#"
               text="Configure container" />
             <f7-menu-dropdown-item
-              v-if="context.component.slots.default.length > 0"
-              @click="context.editmode.configureWidget(context.component.slots.default[0], context)"
+              v-if="defaultSlots.length > 0"
+              @click="context.editmode.configureWidget(defaultSlots[0], context)"
               href="#"
               text="Configure Widget" />
             <f7-menu-dropdown-item @click="context.editmode.editWidgetCode(context.component, context.parent)" href="#" text="Edit YAML" />
-            <f7-menu-dropdown-item v-if="context.component.slots.default.length > 0" @click="toggleAutoSize()" href="#">
+            <f7-menu-dropdown-item v-if="defaultSlots.length > 0" @click="toggleAutoSize()" href="#">
               <span>Auto Size</span>
               <f7-icon class="margin-left" :f7="autosize ? 'checkmark_square' : 'square'" />
             </f7-menu-dropdown-item>
-            <f7-menu-dropdown-item v-if="context.component.slots.default.length > 0" @click="toggleShadow()" href="#">
+            <f7-menu-dropdown-item v-if="defaultSlots.length > 0" @click="toggleShadow()" href="#">
               <span>Shadow</span>
               <f7-icon class="margin-left" :f7="shadow ? 'checkmark_square' : 'square'" />
             </f7-menu-dropdown-item>
-            <f7-menu-dropdown-item v-if="context.component.slots.default.length > 0" divider />
+            <f7-menu-dropdown-item v-if="defaultSlots.length > 0" divider />
             <f7-menu-dropdown-item @click="context.editmode.copyWidget(context.component, context.parent)" href="#" text="Copy" />
             <f7-menu-dropdown-item @click="context.editmode.cutWidget(context.component, context.parent)" href="#" text="Cut" />
             <f7-menu-dropdown-item divider />
@@ -72,12 +72,12 @@
       }"
         class="disable-user-select">
         <oh-placeholder-widget
-          v-if="!context.component.slots.default.length"
+          v-if="!defaultSlots.length"
           @click="context.editmode.addWidget(context.component, null, context.parent)"
           class="oh-canvas-item-content" />
         <generic-widget-component
-          v-else-if="context.component.slots.default.length"
-          :context="childContext(context.component.slots.default[0])"
+          v-else-if="defaultSlots.length"
+          :context="childContext(defaultSlots[0])"
           class="oh-canvas-item-content"
           :class="{
             'oh-canvas-item-styled': styled,
@@ -100,7 +100,7 @@
       class="oh-canvas-item oh-canvas-item-runmode">
       <generic-widget-component
         v-if="ready"
-        :context="childContext(context.component.slots.default[0])"
+        :context="childContext(defaultSlots[0])"
         class="oh-canvas-item-content"
         :class="{
           'oh-canvas-item-styled': styled,
@@ -191,24 +191,28 @@ import VueDraggableResizable from 'vue-draggable-resizable'
 </script>
 
 <script>
-import mixin from '../widget-mixin'
+import { useWidgetContext } from '@/components/widgets/useWidgetContext'
 import OhPlaceholderWidget from './oh-placeholder-widget.vue'
 import { OhCanvasItemDefinition } from '@/assets/definitions/widgets/layout'
 
 export default {
-  mixins: [mixin],
   widget: OhCanvasItemDefinition,
   components: {
     VueDraggableResizable,
     OhPlaceholderWidget
   },
   props: {
+    context: Object,
     gridPitch: Number,
     gridEnable: Boolean,
     id: String,
     preventDeactivation: Boolean
   },
   emits: ['oci-selected', 'oci-deselected', 'oci-drag-stop', 'oci-dragged'],
+  setup (props) {
+    const { config, childContext, defaultSlots } = useWidgetContext(props.context)
+    return { config, childContext, defaultSlots }
+  },
   data () {
     return {
       x: 0,
@@ -374,7 +378,7 @@ export default {
     eventControl (ev) {
       // Events are captured before bubbling to prevent undesired widget interaction when a widget has been
       // added but the page is in edit mode.
-      if (this.context.editmode && this.context.component.slots.default.length > 0) {
+      if (this.context.editmode && this.defaultSlots.length > 0) {
         ev.stopPropagation()
         ev.preventDefault()
       }

bundles/org.openhab.ui/web/src/components/widgets/layout/oh-canvas-layer.vue
@@ -21,21 +21,25 @@
 </style>
 
 <script>
-import mixin from '../widget-mixin'
+import { useWidgetContext } from '@/components/widgets/useWidgetContext'
 import OhCanvasItem from './oh-canvas-item.vue'
 import { OhCanvasLayerDefinition } from '@/assets/definitions/widgets/layout'
 
 export default {
-  mixins: [mixin],
   widget: OhCanvasLayerDefinition,
   components: {
     OhCanvasItem
   },
   props: {
+    context: Object,
     gridPitch: Number,
     gridEnable: Boolean,
     id: String,
     preventDeactivation: Boolean
+  },
+  setup (props) {
+    const { config, childContext, visible } = useWidgetContext(props.context)
+    return { config, childContext, visible }
   },
   data () {
     return {

commit 4b9e8e259fc12d5b556755bcf6f7324f6b10a734

oh-canvas: Fix broken oh-canvas-item & Refactor to TS (#3956)
Regression from #3862.

#3862 broke the oh-canvas-item due to lifecylce issues with the <setup
script> and the setup() method in the Options API script.
This fixes that issue by refactoring oh-canvas-item to use only the
Composition API.
Also refactor oh-canvas-layer.

---------

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/layout/oh-canvas-item.vue
@@ -90,7 +90,7 @@
     </vue-draggable-resizable>
     <div
       v-else
-      ref="root"
+      :ref="root"
       :style="{
         left: x + 'px',
         top: y + 'px',
@@ -185,204 +185,228 @@
       width 0px
 </style>
 
-<script setup>
+<script setup lang="ts">
+import { ref, computed, watch, onMounted, onBeforeUnmount, type VNodeRef } from 'vue'
 import 'vue-draggable-resizable/style.css'
 import VueDraggableResizable from 'vue-draggable-resizable'
-</script>
 
-<script>
 import { useWidgetContext } from '@/components/widgets/useWidgetContext'
-import OhPlaceholderWidget from './oh-placeholder-widget.vue'
 import { OhCanvasItemDefinition } from '@/assets/definitions/widgets/layout'
+import type { WidgetContext } from '@/components/widgets/types'
+import GenericWidgetComponent from '@/components/widgets/generic-widget-component.vue'
+import OhPlaceholderWidget from '@/components/widgets/layout/oh-placeholder-widget.vue'
 
-export default {
-  widget: OhCanvasItemDefinition,
-  components: {
-    VueDraggableResizable,
-    OhPlaceholderWidget
-  },
-  props: {
-    context: Object,
-    gridPitch: Number,
-    gridEnable: Boolean,
-    id: String,
-    preventDeactivation: Boolean
-  },
-  emits: ['oci-selected', 'oci-deselected', 'oci-drag-stop', 'oci-dragged'],
-  setup (props) {
-    const { config, childContext, defaultSlots } = useWidgetContext(props.context)
-    return { config, childContext, defaultSlots }
-  },
-  data () {
-    return {
-      x: 0,
-      y: 0,
-      w: 0,
-      h: 0,
-      reloadKey: 0,
-      shadow: true,
-      styled: true,
-      dragging: false,
-      resizing: false,
-      active: false,
-      // run mode only:
-      ready: false,
-      resizeObserver: null
-    }
-  },
-  created () {
-    this.x = this.config.x ?? 20
-    this.y = this.config.y ?? 20
-    this.w = this.config.w ?? 100
-    this.h = this.config.h ?? 100
-    this.shadow = !this.config.noCanvasShadow
-    this.styled = !this.config.notStyled
-  },
-  mounted () {
-    // In Edit Mode: No need to wait for the root element to have actual dimensions
-    if (this.context.editmode) {
-      this.ready = true
-      return
-    }
+defineOptions({ widget: OhCanvasItemDefinition })
 
-    // In Run Mode: Wait for the element to have actual dimensions
-    this.resizeObserver = new ResizeObserver((entries) => {
-      for (const entry of entries) {
-        // Once width/height is non-zero, the layout is stable enough for f7-swiper
-        if (entry.contentRect.width > 0 && entry.contentRect.height > 0) {
-          this.ready = true
-          this.resizeObserver.disconnect()
-        }
+// props & emits
+const props = defineProps<{
+  context: WidgetContext
+  gridPitch: number
+  gridEnable: boolean
+  id: string
+  preventDeactivation: boolean
+}>()
+
+export interface OhCanvasItemEmits {
+  (e: 'oci-selected', id: string): void
+  (e: 'oci-deselected', id: string): void
+  (e: 'oci-drag-stop', id: string): void
+  (e: 'oci-dragged', id: string, x: number, y: number): void
+}
+
+const emit = defineEmits<OhCanvasItemEmits>();
+
+// composables
+const { config, visible, childContext, defaultSlots } = useWidgetContext(props.context)
+
+// data (state)
+const root = ref<VNodeRef | null>(null)
+const reloadKey = ref(0)
+const dragging = ref(false)
+const resizing = ref(false)
+const active = ref(false)
+// run mode only:
+const ready = ref(false)
+const resizeObserver = ref<ResizeObserver | null>(null)
+
+// computed
+// note: direct mutation of props is intentional, parents handle this
+
+const x = computed({
+  get: () => (config.value?.x as number) ?? 20,
+  set: (val) => { props.context.component.config.x = val }
+})
+
+const y = computed({
+  get: () => (config.value?.y as number) ?? 20,
+  set: (val) => { props.context.component.config.y = val }
+})
+
+const w = computed<number | 'auto'>({
+  get: () => (config.value?.w as number | string as 'auto') ?? 100,
+  set: (val: number | 'auto') => { props.context.component.config.w = val }
+})
+
+const h = computed<number | 'auto'>({
+  get: () => (config.value?.h as number | string as 'auto') ?? 100,
+  set: (val: number | 'auto') => { props.context.component.config.h = val }
+})
+
+const shadow = computed({
+  get: () => config.value?.noCanvasShadow === false,
+  set: (val) => { props.context.component.config.noCanvasShadow = val }
+})
+
+const styled = computed(() => config.value?.notStyled === false)
+
+const autosize = computed(() => w.value === 'auto')
+
+const editMessage = computed(() => {
+  if (dragging.value) return `(${x.value}, ${y.value})`
+  if (resizing.value) return `${w.value}x${h.value}`
+  return ''
+})
+
+// watchers
+watch(active, (val) => {
+  if (val) {
+    emit('oci-selected', props.id)
+  } else {
+    emit('oci-deselected', props.id)
+  }
+})
+
+// lifecycle
+onMounted(() => {
+  // In Edit Mode: No need to wait for the root element to have actual dimensions
+  if (props.context.editmode) {
+    ready.value = true
+    return
+  }
+
+  // In Run Mode: Wait for the element to have actual dimensions
+  resizeObserver.value = new ResizeObserver((entries) => {
+    for (const entry of entries) {
+      // Once width/height is non-zero, the layout is stable enough for f7-swiper
+      if (entry.contentRect.width > 0 && entry.contentRect.height > 0) {
+        ready.value = true
+        resizeObserver.value?.disconnect()
       }
-    })
+    }
+  })
 
-    // Start observing the root element of this component
-    if (this.$refs.root) {
-      this.resizeObserver.observe(this.$refs.root)
+  // Start observing the root element of this component
+  if (root.value) {
+    resizeObserver.value.observe(root.value)
+  } else {
+    ready.value = true
+  }
+})
+
+onBeforeUnmount(() => {
+  if (resizeObserver.value) {
+    resizeObserver.value.disconnect()
+  }
+})
+
+// methods
+const toggleAutoSize = () => {
+  if (w.value === 'auto') {
+    const elem = document.getElementById('oh-canvas-item-vdr-' + props.id)
+    if (elem) {
+      w.value = Math.max(10, elem.clientWidth)
+      h.value = Math.max(10, elem.clientHeight)
+    }
+  } else {
+    w.value = 'auto'
+    h.value = 'auto'
+    reloadKey.value += 1
+  }
+}
+
+const toggleShadow = () => {
+  shadow.value = !shadow.value
+}
+
+const onResize = (newX: number, newY: number, width: number, height: number) => {
+  x.value = newX
+  y.value = newY
+  w.value = width
+  h.value = height
+}
+
+const onDrag = (newX: number, newY: number) => {
+  emit('oci-dragged', props.id, newX - x.value, newY - y.value)
+  moveTo(newX, newY)
+}
+
+const moveTo = (newX: number, newY: number) => {
+  x.value = newX
+  y.value = newY
+}
+
+const onResizeStartCallback = (_handle: unknown, _x: number, _y: number, _width: number, _height: number) => {
+  if (w.value === 'auto' || h.value === 'auto') return false
+
+  const posOK = onDragStartCallback(_x, _y)
+  if (props.gridEnable) {
+    const snapW = Math.round((w.value as number) / props.gridPitch) * props.gridPitch
+    const snapH = Math.round((h.value as number) / props.gridPitch) * props.gridPitch
+
+    if (w.value === snapW && h.value === snapH) {
+      // Widget already on grid, can continue to resize
+      resizing.value = posOK
     } else {
-      this.ready = true
+      // Widget was not on grid, snap to grid upon first action
+      onResize(x.value, y.value, snapW, snapH)
+      resizing.value = false
     }
-  },
-  beforeUnmount () {
-    if (this.resizeObserver) {
-      this.resizeObserver.disconnect()
+  } else {
+    resizing.value = true
+  }
+
+  if (resizing.value) dragging.value = false
+  return resizing.value
+}
+
+const onDragStartCallback = (_x: number, _y: number) => {
+  if (!props.context.editmode) return false
+
+  if (props.gridEnable) {
+    const snapX = Math.round(x.value / props.gridPitch) * props.gridPitch
+    const snapY = Math.round(y.value / props.gridPitch) * props.gridPitch
+
+    if (x.value === snapX && y.value === snapY) {
+      // Origin on grid, continue dragging action
+      dragging.value = true
+    } else {
+      // Snap to grid and stop action
+      onDrag(snapX, snapY)
+      dragging.value = true
     }
-  },
-  computed: {
-    autosize () {
-      return this.w === 'auto'
-    },
-    editMessage () {
-      if (this.dragging) {
-        return `(${this.x}, ${this.y})`
-      } else if (this.resizing) {
-        return `${this.w}x${this.h}`
-      } else {
-        return ''
-      }
-    }
-  },
-  watch: {
-    active (val) {
-      if (val) this.$emit('oci-selected', this)
-      else this.$emit('oci-deselected', this)
-    }
-  },
-  methods: {
-    toggleAutoSize () {
-      if (this.w === 'auto') {
-        const elem = document.getElementById('oh-canvas-item-vdr-' + this.id)
-        this.w = this.context.component.config.w = Math.max(10, elem.clientWidth)
-        this.h = this.context.component.config.h = Math.max(10, elem.clientHeight)
-      } else {
-        this.w = this.context.component.config.w = 'auto'
-        this.h = this.context.component.config.h = 'auto'
-        this.reloadKey += 1
-      }
-    },
-    toggleShadow () {
-      this.shadow = !this.shadow
-      this.context.component.config.noCanvasShadow = !this.shadow
-    },
-    onResize (x, y, width, height) {
-      this.x = this.context.component.config.x = x
-      this.y = this.context.component.config.y = y
-      this.w = this.context.component.config.w = width
-      this.h = this.context.component.config.h = height
-    },
-    onDrag (x, y) {
-      this.$emit('oci-dragged', this, x - this.x, y - this.y)
-      this.moveTo(x, y)
-    },
-    moveTo (x, y) {
-      this.x = this.context.component.config.x = x
-      this.y = this.context.component.config.y = y
-    },
-    onResizeStartCallback (ev) {
-      if (this.w === 'auto' || this.h === 'auto') {
-        return false
-      }
+  } else {
+    dragging.value = true
+  }
 
-      const posOK = this.onDragStartCallback(ev)
-      if (this.gridEnable) {
-        const snapW = Math.round(this.w / this.gridPitch) * this.gridPitch
-        const snapH = Math.round(this.h / this.gridPitch) * this.gridPitch
+  if (dragging.value) resizing.value = false
+  return dragging.value
+}
 
-        if (this.w === snapW && this.h === snapH) {
-          // Widget already on grid, can continue to resize
-          this.resizing = posOK
-        } else {
-          // Widget was not on grid, snap to grid upon first action
-          this.onResize(this.x, this.y, snapW, snapH)
-          this.resizing = false
-        }
-      } else {
-        this.resizing = true
-      }
+const onResizeStop = () => {
+  resizing.value = false
+}
 
-      if (this.resizing) this.dragging = false
-      return this.resizing
-    },
-    onDragStartCallback (ev) {
-      if (!this.context.editmode) return false
+const onDragStop = () => {
+  emit('oci-drag-stop', props.id)
+  dragging.value = false
+}
 
-      if (this.gridEnable) {
-        const snapX = Math.round(this.x / this.gridPitch) * this.gridPitch
-        const snapY = Math.round(this.y / this.gridPitch) * this.gridPitch
-
-        if (this.x === snapX && this.y === snapY) {
-          // Origin on grid, continue dragging action
-          this.dragging = true
-        } else {
-          // First snap to grid component and stop action
-          this.onDrag(snapX, snapY)
-          this.dragging = true
-        }
-      } else {
-        this.dragging = true
-      }
-
-      if (this.dragging) this.resizing = false
-      return this.dragging
-    },
-    onResizeStop () {
-      this.resizing = false
-    },
-    onDragStop () {
-      this.$emit('oci-drag-stop', this)
-      this.stopDrag()
-    },
-    stopDrag () {
-      this.dragging = false
-    },
-    eventControl (ev) {
-      // Events are captured before bubbling to prevent undesired widget interaction when a widget has been
-      // added but the page is in edit mode.
-      if (this.context.editmode && this.defaultSlots.length > 0) {
-        ev.stopPropagation()
-        ev.preventDefault()
-      }
-    }
+const eventControl = (ev: Event) => {
+  // Events are captured before bubbling to prevent undesired widget interaction
+  // when a widget has been added but the page is in edit mode.
+  if (props.context.editmode && defaultSlots.value.length > 0) {
+    ev.stopPropagation()
+    ev.preventDefault()
   }
 }
 </script>

bundles/org.openhab.ui/web/src/components/widgets/layout/oh-canvas-layer.vue
@@ -8,7 +8,11 @@
       :grid-enable="gridEnable"
       :grid-pitch="gridPitch"
       :prevent-deactivation="preventDeactivation"
-      :context="childContext(obj.item)" />
+      :context="childContext(obj.item)"
+      @oci-selected="(cid) => emit('oci-selected', cid)"
+      @oci-deselected="(cid) => emit('oci-deselected', cid)"
+      @oci-drag-stop="(cid) => emit('oci-drag-stop', cid)"
+      @oci-dragged="(cid, x, y) => emit('oci-dragged', cid, x, y)" />
   </div>
 </template>
 
@@ -20,64 +24,49 @@
   pointer-events none
 </style>
 
-<script>
+<script setup lang="ts">
+import { computed } from 'vue'
 import { useWidgetContext } from '@/components/widgets/useWidgetContext'
 import OhCanvasItem from './oh-canvas-item.vue'
 import { OhCanvasLayerDefinition } from '@/assets/definitions/widgets/layout'
+import type { WidgetContext } from '@/components/widgets/types'
+import type { UiComponent } from '@/api'
+import type { OhCanvasItemEmits } from '@/components/widgets/layout/oh-canvas-item.vue'
 
-export default {
-  widget: OhCanvasLayerDefinition,
-  components: {
-    OhCanvasItem
-  },
-  props: {
-    context: Object,
-    gridPitch: Number,
-    gridEnable: Boolean,
-    id: String,
-    preventDeactivation: Boolean
-  },
-  setup (props) {
-    const { config, childContext, visible } = useWidgetContext(props.context)
-    return { config, childContext, visible }
-  },
-  data () {
-    return {
-      layer: []
-    }
-  },
-  created () {
-    this.computeLayer()
-  },
-  computed: {
-    layerPreload () {
-      return this.config?.preload === true
-    },
-    layerVisible () {
-      return (!this.context.editmode && this.visible) || (this.context.editmode && this.editVisible)
-    },
-    editVisible () {
-      return !(this.config && (this.config.editVisible === false))
-    }
-  },
-  methods: {
-    computeLayer () {
-      let layer = []
-      if (this.context.component.slots) {
-        this.context.component.slots?.default.forEach((item) => {
-          if (item.component === 'oh-canvas-item') {
-            layer.push({
-              item,
-              selected: false,
-              id: Math.random().toString(36).substring(2)
-            })
-          } else {
-            console.log('Wrong component type in canvas layer: ' + item.component)
-          }
-        })
-      }
-      this.layer = layer
-    }
-  }
+defineOptions({ widget: OhCanvasLayerDefinition })
+
+// props & emits
+const props = defineProps<{
+  context: WidgetContext,
+  gridPitch: number,
+  gridEnable: boolean,
+  id: string,
+  preventDeactivation: boolean
+}>()
+
+const emit = defineEmits<OhCanvasItemEmits>()
+
+// composables
+const { config, defaultSlots, childContext, visible } = useWidgetContext(props.context)
+
+// computed
+const layerPreload = computed(() => config.value?.preload === true)
+const layerVisible = computed(() => (!props.context.editmode && visible.value) || (props.context.editmode && editVisible.value))
+const editVisible = computed(() => !(config.value && (config.value.editVisible === false)))
+
+interface Layer {
+  item: UiComponent,
+  selected: boolean,
+  id: string
 }
+
+const layer = computed<Layer[]>(() => {
+  return defaultSlots.value
+    .filter((item: UiComponent) => item.component === 'oh-canvas-item')
+    .map((item: UiComponent) => ({
+      item,
+      selected: false,
+      id: Math.random().toString(36).substring(2)
+    }))
+})
 </script>

bundles/org.openhab.ui/web/src/components/widgets/useWidgetContext.ts
@@ -14,8 +14,6 @@ import * as api from '@/api'
  * The useWidgetContext must be used as a composable in all widget components.
  *
  * A few requirements have to be met by components importing this composable:
- * - Widget components have to add a `component` ref to their root.
- * - Page components have to add a `page` ref to their root.
  * - All components using this composable have to add the `scopedCssUid` as a class to their root element.
  */
 export function useWidgetContext(context: WidgetContext) {
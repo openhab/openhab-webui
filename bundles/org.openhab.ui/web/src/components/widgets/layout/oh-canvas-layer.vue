4b9e8e259fc12d5b556755bcf6f7324f6b10a734
Florian Hotze <dev@florianhotze.com>
2026-03-03 19:21:14 +0100

    oh-canvas: Fix broken oh-canvas-item & Refactor to TS (#3956)
    Regression from #3862.
    
    #3862 broke the oh-canvas-item due to lifecylce issues with the <setup
    script> and the setup() method in the Options API script.
    This fixes that issue by refactoring oh-canvas-item to use only the
    Composition API.
    Also refactor oh-canvas-layer.
    
    ---------
    
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- bundles/org.openhab.ui/web/src/components/widgets/layout/oh-canvas-layer.vue
+++ bundles/org.openhab.ui/web/src/components/widgets/layout/oh-canvas-layer.vue
@@ -8,7 +8,11 @@
       :grid-enable="gridEnable"
       :grid-pitch="gridPitch"
       :prevent-deactivation="preventDeactivation"
-      :context="childContext(obj.item)" />
+      :context="childContext(obj.item)"
+      @oci-selected="(cid) => emit('oci-selected', cid)"
+      @oci-deselected="(cid) => emit('oci-deselected', cid)"
+      @oci-drag-stop="(cid) => emit('oci-drag-stop', cid)"
+      @oci-dragged="(cid, x, y) => emit('oci-dragged', cid, x, y)" />
   </div>
 </template>
 
@@ -20,64 +24,49 @@
   pointer-events none
 </style>
 
-<script>
+<script setup lang="ts">
+import { computed } from 'vue'
 import { useWidgetContext } from '@/components/widgets/useWidgetContext'
 import OhCanvasItem from './oh-canvas-item.vue'
 import { OhCanvasLayerDefinition } from '@/assets/definitions/widgets/layout'
+import type { WidgetContext } from '@/components/widgets/types'
+import type { UiComponent } from '@/api'
+import type { OhCanvasItemEmits } from '@/components/widgets/layout/oh-canvas-item.vue'
 
-export default {
-  widget: OhCanvasLayerDefinition,
-  components: {
-    OhCanvasItem
-  },
-  props: {
-    context: Object,
-    gridPitch: Number,
-    gridEnable: Boolean,
-    id: String,
-    preventDeactivation: Boolean
-  },
-  setup (props) {
-    const { config, childContext, visible } = useWidgetContext(props.context)
-    return { config, childContext, visible }
-  },
-  data () {
-    return {
-      layer: []
-    }
-  },
-  created () {
-    this.computeLayer()
-  },
-  computed: {
-    layerPreload () {
-      return this.config?.preload === true
-    },
-    layerVisible () {
-      return (!this.context.editmode && this.visible) || (this.context.editmode && this.editVisible)
-    },
-    editVisible () {
-      return !(this.config && (this.config.editVisible === false))
-    }
-  },
-  methods: {
-    computeLayer () {
-      let layer = []
-      if (this.context.component.slots) {
-        this.context.component.slots?.default.forEach((item) => {
-          if (item.component === 'oh-canvas-item') {
-            layer.push({
-              item,
-              selected: false,
-              id: Math.random().toString(36).substring(2)
-            })
-          } else {
-            console.log('Wrong component type in canvas layer: ' + item.component)
-          }
-        })
-      }
-      this.layer = layer
-    }
-  }
+defineOptions({ widget: OhCanvasLayerDefinition })
+
+// props & emits
+const props = defineProps<{
+  context: WidgetContext,
+  gridPitch: number,
+  gridEnable: boolean,
+  id: string,
+  preventDeactivation: boolean
+}>()
+
+const emit = defineEmits<OhCanvasItemEmits>()
+
+// composables
+const { config, defaultSlots, childContext, visible } = useWidgetContext(props.context)
+
+// computed
+const layerPreload = computed(() => config.value?.preload === true)
+const layerVisible = computed(() => (!props.context.editmode && visible.value) || (props.context.editmode && editVisible.value))
+const editVisible = computed(() => !(config.value && (config.value.editVisible === false)))
+
+interface Layer {
+  item: UiComponent,
+  selected: boolean,
+  id: string
 }
+
+const layer = computed<Layer[]>(() => {
+  return defaultSlots.value
+    .filter((item: UiComponent) => item.component === 'oh-canvas-item')
+    .map((item: UiComponent) => ({
+      item,
+      selected: false,
+      id: Math.random().toString(36).substring(2)
+    }))
+})
 </script>
