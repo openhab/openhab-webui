openhab/openhab-webui

commit 12497cda66a71325aa8d3cff6e4ba6dfa8bf1111

Charts: Add support for mixed past/future charts (#3337)
Modifies the future parameter to allow configuring the proportion of the
period extending into the future.
Backwards compatibility with the old boolean parameter is kept.

---------

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/chart/chart-mixin.js
@@ -1,7 +1,9 @@
 import dayjs from 'dayjs'
 import IsoWeek from 'dayjs/plugin/isoWeek'
+import Duration from 'dayjs/plugin/duration'
 
 dayjs.extend(IsoWeek)
+dayjs.extend(Duration)
 
 import { useUIOptionsStore } from '@/js/stores/useUIOptionsStore'
 
@@ -47,13 +49,15 @@ export default {
   data () {
     const config = this.context.component.config || {}
     const chartType = config.chartType
+    const future = config.future === true ? 1 : (config.future ?? 0)
     const endTime = (chartType)
-      ? this.addOrSubtractPeriod(dayjs().startOf(chartType), 1 + (config.future ? 1 : 0))
-      : this.addOrSubtractPeriod(dayjs(), config.future ? 1 : 0)
+      ? this.addOrSubtractPeriod(dayjs().startOf(chartType), 1 + future)
+      : this.addOrSubtractPeriod(dayjs(), future)
 
     return {
       items: {},
       speriod: config.period || DEFAULT_PERIOD,
+      future,
       endTime,
       orient: null
     }
@@ -203,7 +207,7 @@ export default {
     },
     setPeriod (period) {
       this.speriod = period
-      this.endTime = this.addOrSubtractPeriod(dayjs(), this.config.future ? 1 : 0)
+      this.endTime = this.addOrSubtractPeriod(dayjs(), this.future)
     },
     setDate (date) {
       const chartType = this.context.component.config.chartType
@@ -221,15 +225,16 @@ export default {
       if (direction === 0) return day
       const fn = (direction < 0) ? day.subtract : day.add
       const chartType = this.context.component.config.chartType
-      for (let i = 0; i < Math.abs(direction); i++) {
-        if (chartType) {
-          day = fn.apply(day, [1, chartType === 'isoWeek' ? 'week' : chartType])
-        } else {
-          const period = this.period || this.context.component.config.period || DEFAULT_PERIOD
-          const span = period.match(/^([\d]*)([smhdDwWMQyY])$/)
-          if (span) {
-            day = fn.apply(day, [parseInt(span[1]) || 1, span[2].replace(/[DWY]/, (x) => x.toLowerCase())])
-          }
+      direction = Math.abs(direction)
+      if (chartType) {
+        const millis = dayjs.duration({ [chartType === 'isoWeek' ? 'week' : chartType]: 1 }).asMilliseconds() * direction
+        day = fn.apply(day, [millis, 'millisecond'])
+      } else {
+        const period = this.period || this.context.component.config.period || DEFAULT_PERIOD
+        const span = period.match(/^([\d]*)([smhdDwWMQyY])$/)
+        if (span) {
+          const millis = dayjs.duration({ [span[2].replace(/[DWY]/, (x) => x.toLowerCase())]: parseInt(span[1]) || 1 }).asMilliseconds() * direction
+          day = fn.apply(day, [millis, 'millisecond'])
         }
       }
 

bundles/org.openhab.ui/doc/components/src/generate.js
@@ -43,7 +43,7 @@ const replaceBetweenComments = (commentTag, text, value) => {
 
 const escapeQuotes = (text) => {
   if (text !== undefined) {
-    return text.replace(/"/g, "&quot;");
+    return text.toString().replace(/"/g, "&quot;");
   }
   return text
 }

bundles/org.openhab.ui/web/src/assets/definitions/widgets/system/chart.js
@@ -1,4 +1,4 @@
-import { po, pb } from '../helpers.js'
+import { pd, po } from '../helpers.js'
 
 export default () => [
   po('chartType', 'Chart Type', 'Define a fixed period for the chart, aligned at the beginning of the period, e.g. January 1st at midnight for a year chart. If not set (or set to dynamic), the length of the period will be configurable but certain combinations like aggregated series might not work', [
@@ -30,5 +30,11 @@ export default () => [
   ]).v((value, configuration, configDescription, parameters) => {
     return !configuration.chartType
   }),
-  pb('future', 'Future Period', 'Whether the period should extend to the future instead of the past')
+  pd('future', 'Future Proportion', 'The proportion of the period that should extend into the future').o([
+    { value: 0, label: '0% - Past only' },
+    { value: 0.25, label: '25% - 3/4 past, 1/4 future' },
+    { value: 0.50, label: '50% - 1/2 past, 1/2 future' },
+    { value: 0.75, label: '75% - 1/4 past, 3/4 future' },
+    { value: 1, label: '100% - Future only' }
+  ])
 ]

bundles/org.openhab.ui/web/src/components/config/controls/parameter-options.vue
@@ -91,21 +91,22 @@ export default {
   methods: {
     updateValue (evt) {
       let value = (this.inlineList) ? evt : this.$refs.item.$el.children[0].f7SmartSelect.getValue()
-      if (!this.configDescription.multiple && this.configDescription.type === 'INTEGER') {
-        value = parseInt(value)
+      if (!this.configDescription.multiple) {
+        if (this.configDescription.type === 'INTEGER') value = parseInt(value)
+        if (this.configDescription.type === 'DECIMAL') value = parseFloat(value)
       }
       this.$emit('input', value)
     },
     isSelected (option) {
+      let optVal = option.value
+      if (this.configDescription.type === 'INTEGER') optVal = parseInt(optVal)
+      if (this.configDescription.type === 'DECIMAL') optVal = parseFloat(optVal)
+
       if (this.value === null || this.value === undefined) return
       if (!this.configDescription.multiple) {
-        return this.value.toString() === option.value
+        return this.value === optVal
       } else {
-        if (this.configDescription.type === 'INTEGER') {
-          return this.value && this.value.indexOf(parseInt(option.value)) >= 0
-        } else {
-          return this.value && this.value.indexOf(option.value) >= 0
-        }
+        return this.value && this.value.indexOf(optVal) >= 0
       }
     }
   }

commit fa40fba066527c2714bf98ccee38cdc626b552cd

Charts: Fix add/subtract period doesn't account for different month lenghts (#3958)
Regression from #3337.

Signed-off-by: Florian Hotze <dev@florianhotze.com>
Co-authored-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/components/widgets/chart/chart-mixin.js
@@ -272,18 +272,30 @@ export default {
       if (direction === 0) return day
       const fn = direction < 0 ? day.subtract : day.add
       const chartType = this.context.component.config.chartType
-      direction = Math.abs(direction)
-      if (chartType) {
-        const millis = dayjs.duration({ [chartType === 'isoWeek' ? 'week' : chartType]: 1 }).asMilliseconds() * direction
-        day = fn.apply(day, [millis, 'millisecond'])
-      } else {
-        const period = this.period || this.context.component.config.period || DEFAULT_PERIOD
-        const span = period.match(/^([\d]*)([smhdDwWMQyY])$/)
-        if (span) {
-          const millis =
-            dayjs.duration({ [span[2].replace(/[DWY]/, (x) => x.toLowerCase())]: parseInt(span[1]) || 1 }).asMilliseconds() * direction
-          day = fn.apply(day, [millis, 'millisecond'])
+      const absDirection = Math.abs(direction)
+      // Handle full direction units
+      for (let i = 0; i < Math.floor(absDirection); i++) {
+        if (chartType) {
+          day = fn.apply(day, [1, chartType === 'isoWeek' ? 'week' : chartType])
+        } else {
+          const period = this.period || this.context.component.config.period || DEFAULT_PERIOD
+          const span = period.match(/^([\d]*)([smhdDwWMQyY])$/)
+          if (span) {
+            day = fn.apply(day, [parseInt(span[1]) || 1, span[2].replace(/[DWY]/, (x) => x.toLowerCase())])
+          }
         }
+      }
+      // Handle fractional direction
+      const fraction = absDirection % 1
+      if (fraction > 0) {
+        const unit = chartType
+          ? chartType === 'isoWeek'
+            ? 'week'
+            : chartType
+          : (this.period || this.context.component.config.period || DEFAULT_PERIOD).match(/[smhdDwWMQyY]$/)[0].toLowerCase()
+        const nextFullUnit = fn.apply(day, [1, unit])
+        const diff = nextFullUnit.diff(day)
+        day = day.add(diff * fraction, 'ms')
       }
 
       return day