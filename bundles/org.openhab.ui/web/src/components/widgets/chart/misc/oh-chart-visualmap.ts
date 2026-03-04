9622aee7a0254bf1edb8f9e284a75a8307c8236e
Florian Hotze <dev@florianhotze.com>
2026-03-04 17:03:23 +0100

    Fix lint
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- bundles/org.openhab.ui/web/src/components/widgets/chart/misc/oh-chart-visualmap.ts
+++ bundles/org.openhab.ui/web/src/components/widgets/chart/misc/oh-chart-visualmap.ts
@@ -26,12 +26,12 @@ const chartVisualMap: MiscChartComponent = {
     if (options.min !== undefined) {
       ;(options as VisualMapComponentOption).min = parseFloat(options.min)
     } else if (context.series && context.series[0]) {
-      ;(options as VisualMapComponentOption).min = Math.min(...context.series[0].data.map((p) => p[p.length - 1] as number))
+      ;(options as VisualMapComponentOption).min = Math.min(...(context.series[0].data as number[][]).map((p) => p[p.length - 1]!))
     }
     if (options.max !== undefined) {
       ;(options as VisualMapComponentOption).max = parseFloat(options.max)
     } else if (context.series && context.series[0]) {
-      ;(options as VisualMapComponentOption).max = Math.max(...context.series[0].data.map((p) => p[p.length - 1] as number))
+      ;(options as VisualMapComponentOption).max = Math.max(...(context.series[0].data as number[][]).map((p) => p[p.length - 1] as number))
     }
 
     if (!options.formatter && context.numberFormatter) {
