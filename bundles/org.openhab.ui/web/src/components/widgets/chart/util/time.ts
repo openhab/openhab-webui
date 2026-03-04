40e4f3ed40b36c14cff82eb2a9ff61f12e431382
Florian Hotze <dev@florianhotze.com>
2026-03-04 15:13:56 +0100

    util: Add addOrSubtractPeriod to time util
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- bundles/org.openhab.ui/web/src/components/widgets/chart/util/time.ts
+++ bundles/org.openhab.ui/web/src/components/widgets/chart/util/time.ts
@@ -1,7 +1,7 @@
 import dayjs, { Dayjs, type OpUnitType } from 'dayjs'
 import IsoWeek from 'dayjs/plugin/isoWeek'
 import DayDuration from 'dayjs/plugin/duration'
-import { ChartType } from '@/types/components/widgets'
+import { ChartType, Period } from '@/types/components/widgets'
 
 dayjs.extend(IsoWeek)
 dayjs.extend(DayDuration)
@@ -26,4 +26,46 @@ export function startOf(chartType: ChartType, date?: string | number | dayjs.Day
     return dayjs(date).startOf('week')
   }
   return dayjs(date).startOf(chartType as OpUnitType)
+}
+
+/**
+ * Adds (`direction > 0`) or subtracts (`direction < 0`) `Math.abs(direction)` units from the given date.
+ * For {@link ChartType#dynamic}, the unit depends on the `period`, else the unit depends on the `chartType`.
+ *
+ * @param chartType
+ * @param period
+ * @param date
+ * @param direction
+ */
+export function addOrSubtractPeriod(chartType: ChartType, period: Period, date: dayjs.Dayjs, direction: number) {
+  if (direction === 0) return date
+  const fn =
+    direction < 0 ? (v: number, d: dayjs.ManipulateType) => date.subtract(v, d) : (v: number, d: dayjs.ManipulateType) => date.add(v, d)
+  const absDirection = Math.abs(direction)
+  // Handle full direction units
+  for (let i = 0; i < Math.floor(absDirection); i++) {
+    if (chartType !== ChartType.dynamic) {
+      date = fn(1, chartType === ChartType.isoWeek ? ChartType.week : chartType)
+    } else {
+      const span = period.match(/^([\d]*)([smhdDwWMQyY])$/)!
+      if (span) {
+        date = fn(parseInt(span[1]!) || 1, span[2]!.replace(/[DWY]/, (x) => x.toLowerCase()) as unknown as dayjs.ManipulateType)
+      }
+    }
+  }
+  // Handle fractional direction
+  const fraction = absDirection % 1
+  if (fraction > 0) {
+    const unit =
+      chartType !== ChartType.dynamic
+        ? chartType === ChartType.isoWeek
+          ? ChartType.week
+          : chartType
+        : (period.match(/[smhdDwWMQyY]$/)![0].toLowerCase() as unknown as dayjs.ManipulateType)
+    const nextFullUnit = fn(1, unit)
+    const diff = nextFullUnit.diff(date)
+    date = date.add(diff * fraction, 'ms')
+  }
+
+  return date
 }
