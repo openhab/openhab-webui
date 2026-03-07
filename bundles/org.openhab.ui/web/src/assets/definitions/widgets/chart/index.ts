8a822623d87400c6a339a43009acf14e4268700f
Florian Hotze <dev@florianhotze.com>
2026-03-07 17:17:45 +0100

    Fix format
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- bundles/org.openhab.ui/web/src/assets/definitions/widgets/chart/index.ts
+++ bundles/org.openhab.ui/web/src/assets/definitions/widgets/chart/index.ts
@@ -400,8 +400,6 @@ const chartComponentsList: WidgetDefinition[] = [
     ])
 ]
 
-const chartComponents: Record<string, WidgetDefinition> = Object.fromEntries(
-  chartComponentsList.map((c) => [c.name, c])
-)
+const chartComponents: Record<string, WidgetDefinition> = Object.fromEntries(chartComponentsList.map((c) => [c.name, c]))
 
 export default chartComponents
