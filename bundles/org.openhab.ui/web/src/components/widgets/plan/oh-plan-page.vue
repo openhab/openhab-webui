openhab/openhab-webui

commit 4c5420421dbf587aaeabf9d60c68b1ceb6963a86

Refactor widget-mixin to TS composable (#3862)
Fixes #3919.

* Refactors the widget-mixin to a TypeScript composable.
  This is a precondition for refactoring widget components to TypeScript.
  In Composition API, components must be registered globally if referenced
  by name - hence all widgets are registered globally.
  Note, there is no performance impact here.
* Adds TypeScript definitions for widget definitions.
* Removed special title overflow in oh-list-item for divider as CSS
  text-overflow does the same operation and is now part of the F7 styles.

---------

Also-by: Florian Hotze <dev@florianhotze.com>
Signed-off-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/components/widgets/plan/oh-plan-page.vue
@@ -106,7 +106,7 @@ dark-tooltip()
 import { nextTick } from 'vue'
 import { f7 } from 'framework7-vue'
 
-import mixin from '../widget-mixin'
+import { useWidgetContext } from '@/components/widgets/useWidgetContext'
 import { Icon, CRS } from 'leaflet'
 import { LMap, LImageOverlay, LFeatureGroup, LControl } from '@vue-leaflet/vue-leaflet'
 import 'leaflet/dist/leaflet.css'
@@ -123,7 +123,9 @@ Icon.Default.mergeOptions({
 })
 
 export default {
-  mixins: [mixin],
+  props: {
+    context: Object
+  },
   components: {
     LMap,
     LImageOverlay,
@@ -132,6 +134,10 @@ export default {
     OhPlanMarker
   },
   widget: OhPlanPageDefinition,
+  setup(props) {
+    const { config, scopedCssUid, childContext, defaultSlots } = useWidgetContext(props.context)
+    return { config, scopedCssUid, childContext, defaultSlots }
+  },
   data () {
     return {
       currentZoom: 13,
@@ -180,7 +186,7 @@ export default {
   methods: {
     zoomUpdate (zoom) {
       this.currentZoom = zoom
-      const allMarkers = this.context.component.slots.default
+      const allMarkers = this.defaultSlots
       const visibleMarkers = allMarkers.filter((e) => {
         const zoomVisibilityMin = parseFloat(e.config.zoomVisibilityMin)
         const zoomVisibilityMax = parseFloat(e.config.zoomVisibilityMax)

commit a5dd027cdceb18a1454df5d8bc48baafadb30082

oh-plan-page: Fix floor plan marker visibility (#3993)
Regression from #3862

---------

Signed-off-by: Dan Cunningham <dan@digitaldan.com>
Signed-off-by: Jeff James <jeff@james-online.com>
Co-authored-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/components/widgets/plan/oh-plan-page.vue
@@ -25,12 +25,13 @@
       @update:zoom="zoomUpdate">
       <l-image-overlay v-if="backgroundImageUrl" :url="backgroundImageUrl" :bounds="bounds" />
       <l-feature-group v-if="context.component.slots" ref="featureGroup">
-        <component
-          :is="markerComponent(marker)"
-          v-for="(marker, idx) in markers"
-          :key="idx"
-          :context="childContext(marker)"
-          @update="onMarkerUpdate" />
+        <template v-for="(marker, idx) in defaultSlots" :key="idx">
+          <component
+            :is="markerComponent(marker)"
+            v-if="isMarkerVisible(marker)"
+            :context="childContext(marker)"
+            @update="onMarkerUpdate" />
+        </template>
       </l-feature-group>
       <l-control v-if="context.editmode" position="topright">
         <f7-menu class="padding">
@@ -146,8 +147,7 @@ export default {
       zoom: -0.5,
       crs: CRS.Simple,
       showMap: false,
-      mapKey: f7.utils.id(),
-      markers: []
+      mapKey: f7.utils.id()
     }
   },
   computed: {
@@ -155,6 +155,21 @@ export default {
       const lat = this.config.imageHeight || 1000
       const lng = this.config.imageWidth || 1000
       return [[0, 0], [lat, lng]]
+    },
+    markerComponent() {
+      return (marker) => {
+        return OhPlanMarker
+      }
+    },
+    isMarkerVisible() {
+      return (marker) => {
+        if (this.context.editmode != null) return true
+        const zoomVisibilityMin = parseFloat(marker.config.zoomVisibilityMin)
+        const zoomVisibilityMax = parseFloat(marker.config.zoomVisibilityMax)
+        const isVisibleMin = isNaN(zoomVisibilityMin) || zoomVisibilityMin < this.currentZoom
+        const isVisibleMax = isNaN(zoomVisibilityMax) || zoomVisibilityMax > this.currentZoom
+        return isVisibleMin && isVisibleMax
+      }
     },
     mapOptions () {
       return Object.assign({
@@ -186,25 +201,9 @@ export default {
   methods: {
     zoomUpdate (zoom) {
       this.currentZoom = zoom
-      const allMarkers = this.defaultSlots
-      const visibleMarkers = allMarkers.filter((e) => {
-        const zoomVisibilityMin = parseFloat(e.config.zoomVisibilityMin)
-        const zoomVisibilityMax = parseFloat(e.config.zoomVisibilityMax)
-        const isVisibleMin = isNaN(zoomVisibilityMin) || zoomVisibilityMin < this.currentZoom
-        const isVisibleMax = isNaN(zoomVisibilityMax) || zoomVisibilityMax > this.currentZoom
-        return this.context.editmode != null || (isVisibleMin && isVisibleMax)
-      })
-      // only update our markers if the list has changed to avoid unnecessary rendering
-      if (visibleMarkers.length !== this.markers.length ||
-        visibleMarkers.every((e) => this.markers.indexOf(e) < 0)) {
-        this.markers = visibleMarkers
-      }
     },
     centerUpdate (center) {
       this.currentCenter = center
-    },
-    markerComponent (marker) {
-      return OhPlanMarker
     },
     onMarkerUpdate () {
     },