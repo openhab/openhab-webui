openhab/openhab-webui

commit 9cc2cd6d17c89f86e054bb5fe1e04eff9576808a

Fix oh-chart/other components not loading in production build (#4010)
`oh-chart`, `oh-knob` and possibly other components as well weren't
loading in the prod build.
Additionally, widget edit also wasn't working anymore. 
Reported on the community:
https://community.openhab.org/t/openhab-5-2-snapshot-discussion/167691/13

This fixes the issue by globally registering asyncComponents for widgets
only, affecting import resolution.
Pages and tabs are explicitly imported where needed, but in most cases
async as well.

---------

Signed-off-by: Jeff James <jeff@james-online.com>
Signed-off-by: Florian Hotze <dev@florianhotze.com>
Co-authored-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/pages/page/page-view.vue
@@ -59,7 +59,7 @@
       </f7-tab>
     </f7-tabs>
 
-    <component :is="widgetRegistry.page(page.component)" v-else-if="page && visibleToCurrentUser" :context="context" :f7router="f7router" />
+    <component :is="pageComponent(page)" v-else-if="page && visibleToCurrentUser" :context="context" :f7router="f7router" />
 
     <empty-state-placeholder
       v-if="!visibleToCurrentUser"
@@ -86,9 +86,9 @@
 </style>
 
 <script>
+import { defineAsyncComponent } from 'vue'
 import { f7, theme } from 'framework7-vue'
 
-import * as widgetRegistry from '@/components/oh-component-registry.ts'
 import EmptyStatePlaceholder from '@/components/empty-state-placeholder.vue'
 
 import { useStatesStore } from '@/js/stores/useStatesStore'
@@ -97,6 +97,15 @@ import { useUserStore } from '@/js/stores/useUserStore'
 import { useUIOptionsStore } from '@/js/stores/useUIOptionsStore'
 import { useWidgetExpression } from '@/components/widgets/useWidgetExpression.ts'
 import { useViewArea } from '@/composables/useViewArea.ts'
+
+import OhLayoutPage from '@/components/widgets/layout/oh-layout-page.vue'
+const OhMapPage = defineAsyncComponent(() => import('@/components/widgets/map/oh-map-page.vue'))
+const OhPlanPage = defineAsyncComponent(() => import('@/components/widgets/plan/oh-plan-page.vue'))
+const OhChartPage = defineAsyncComponent(() => import('@/components/widgets/chart/oh-chart-page.vue'))
+
+const OhLocationsTab = defineAsyncComponent(() => import('@/components/tabs/locations-tab.vue'))
+const OhEquipmentTab = defineAsyncComponent(() => import('@/components/tabs/equipment-tab.vue'))
+const OhPropertiesTab = defineAsyncComponent(() => import('@/components/tabs/properties-tab.vue'))
 
 export default {
   components: {
@@ -113,7 +122,7 @@ export default {
     useViewArea()
 
     const { evaluateExpression } = useWidgetExpression()
-    return { theme, evaluateExpression, widgetRegistry }
+    return { theme, evaluateExpression }
   },
   data () {
     return {
@@ -245,16 +254,32 @@ export default {
       }
       return context
     },
+    pageComponent(page) {
+      if (!page.component) return null
+      switch (page.component) {
+        case 'oh-layout-page':
+          return OhLayoutPage
+        case 'oh-map-page':
+          return OhMapPage
+        case 'oh-plan-page':
+          return OhPlanPage
+        case 'oh-chart-page':
+          return OhChartPage
+      }
+      return null
+    },
     tabComponent (tab) {
       switch (tab) {
         case 'oh-locations-tab':
+          return OhLocationsTab
         case 'oh-equipment-tab':
+          return OhEquipmentTab
         case 'oh-properties-tab':
-          return widgetRegistry.tab(tab.component)
+          return OhPropertiesTab
       }
 
       const page = useComponentsStore().page(tab.config.page.replace('page:', ''))
-      return widgetRegistry.page(page.component)
+      return page.component
     },
     tabEvaluateExpression (tab, idx, key) {
       const ctx = this.tabContext(tab)

bundles/org.openhab.ui/web/src/components/oh-component-registry.ts
@@ -1,50 +1,39 @@
-import { type Component, defineAsyncComponent } from 'vue'
+import { type Component, defineAsyncComponent, type App } from 'vue'
 
-const pages = new Map<string, Component>()
-const tabs = new Map<string, Component>()
-const widgets = new Map<string, Component>()
+type Loader = () => Promise<{ default: Component }>
 
-const widgetFiles: Record<string, unknown> = import.meta.glob('@/components/widgets/**/*.vue')
-Object.entries(widgetFiles).forEach(([path, loader]) => {
-  const componentName = path.split('/').pop()?.replace('.vue', '')
-  if (componentName) {
-    const asyncComponent = defineAsyncComponent(loader as () => Promise<{ default: Component }>)
-    if (componentName.endsWith('-page')) {
-      pages.set(componentName, asyncComponent)
-    } else {
-      widgets.set(componentName, asyncComponent)
+function registerSystemWidgets(app: App) {
+  const files = import.meta.glob<Loader>('@/components/widgets/system/*.vue')
+  Object.entries(files).forEach(([path, loader]) => {
+    const componentName = path.split('/').pop()?.replace('.vue', '')
+    if (componentName) {
+      app.component(componentName, defineAsyncComponent(loader))
     }
-  }
-})
-
-const tabsFiles: Record<string, unknown> = import.meta.glob('@/components/tabs/**/*.vue')
-Object.entries(tabsFiles).forEach(([path, loader]) => {
-  const componentName = path.split('/').pop()?.replace('.vue', '')
-  if (componentName) {
-    const asyncComponent = defineAsyncComponent(loader as () => Promise<{ default: Component }>)
-    tabs.set(componentName, asyncComponent)
-  }
-})
-
-console.info(`oh-component-registry: Registered ${pages.size} page, ${tabs.size} tab and ${widgets.size} widgets components`)
-console.debug('oh-component-registry pages', pages.keys())
-console.debug('oh-component-registry tabs', tabs.keys())
-console.debug('oh-component-registry widgets', widgets.keys())
-
-export function page(name: string) {
-  const p = pages.get(name)
-  if (!p) console.error("oh-component-registry: Didn't find page component ", name)
-  return p
+  })
 }
 
-export function tab(name: string) {
-  const t = tabs.get(name)
-  if (!t) console.error("oh-component-registry: Didn't find tab component ", name)
-  return t
+function registerStandardWidgets(app: App) {
+  const files = import.meta.glob<Loader>('@/components/widgets/standard/**/*.vue')
+  Object.entries(files).forEach(([path, loader]) => {
+    const componentName = path.split('/').pop()?.replace('.vue', '')
+    if (componentName) {
+      app.component(componentName, defineAsyncComponent(loader))
+    }
+  })
 }
 
-export function widget(name: string, suppressLog = false) {
-  const w = widgets.get(name)
-  if (!w && !suppressLog) console.error("oh-component-registry: Didn't find widget component ", name)
-  return w
+function registerLayoutWidgets(app: App) {
+  const files = import.meta.glob<Loader>('@/components/widgets/layout/*.vue')
+  Object.entries(files).forEach(([path, loader]) => {
+    const componentName = path.split('/').pop()?.replace('.vue', '')
+    if (componentName) {
+      app.component(componentName, defineAsyncComponent(loader))
+    }
+  })
+}
+
+export function registerWidgets(app: App) {
+  registerSystemWidgets(app)
+  registerStandardWidgets(app)
+  registerLayoutWidgets(app)
 }

bundles/org.openhab.ui/web/src/components/widgets/chart/oh-chart-page.vue
@@ -37,7 +37,6 @@
 
 <script>
 import { useWidgetContext } from '@/components/widgets/useWidgetContext'
-import OhChart from '../system/oh-chart.vue'
 import { OhChartPageDefinition } from '@/assets/definitions/widgets/chart/page'
 import { mapStores } from 'pinia'
 
@@ -46,9 +45,6 @@ import { useUIOptionsStore } from '@/js/stores/useUIOptionsStore'
 export default {
   props: {
     context: Object
-  },
-  components: {
-    OhChart
   },
   widget: OhChartPageDefinition,
   setup (props) {

bundles/org.openhab.ui/web/src/components/widgets/generic-widget-component.vue
@@ -51,7 +51,7 @@
     </template>
     <component
       v-bind="$attrs"
-      :is="widgetRegistry.widget(componentType)"
+      :is="componentType"
       v-else-if="componentType && componentType.startsWith('oh-')"
       ref="component"
       :context="context"
@@ -74,12 +74,7 @@
       style="white-space: pre-wrap"
       >{{ config.error }}</pre
     >
-    <component
-      :is="widgetRegistry.widget(componentType, true) ?? componentType"
-      v-else
-      ref="component"
-      v-bind="{ ...$attrs, ...config }"
-      :class="scopedCssUid">
+    <component :is="componentType" v-else ref="component" v-bind="{ ...$attrs, ...config }" :class="scopedCssUid">
       {{ config.content }}
       <template v-if="defaultSlots.length > 0">
         <generic-widget-component
@@ -94,7 +89,6 @@
 <script setup lang="ts">
 import { useWidgetContext } from '@/components/widgets/useWidgetContext'
 import type { WidgetContext } from '@/components/widgets/types'
-import * as widgetRegistry from '@/components/oh-component-registry.ts'
 import Label from '@/components/widgets/Label.vue'
 import { defineAsyncComponent } from 'vue'
 

bundles/org.openhab.ui/web/src/components/widgets/layout/oh-block.vue
@@ -34,11 +34,7 @@
         </f7-menu-dropdown>
       </f7-menu-item>
     </f7-menu>
-    <component
-      :is="widgetRegistry.widget(component.component, true) ?? component.component"
-      v-for="(component, idx) in defaultSlots"
-      :key="idx"
-      :context="childContext(component)" />
+    <component :is="component.component" v-for="(component, idx) in defaultSlots" :key="idx" :context="childContext(component)" />
   </f7-block>
 </template>
 
@@ -51,7 +47,6 @@
 import { useWidgetContext } from '@/components/widgets/useWidgetContext'
 import type { WidgetContext } from '@/components/widgets/types'
 import { OhBlockDescription } from '@/assets/definitions/widgets/layout'
-import * as widgetRegistry from '@/components/oh-component-registry.ts'
 import * as api from '@/api'
 
 defineOptions({

bundles/org.openhab.ui/web/src/components/widgets/modals/modal-mixin.js
@@ -1,8 +1,28 @@
-import * as widgetRegistry from '@/components/oh-component-registry.ts'
+import { defineAsyncComponent } from 'vue'
 
 import { useUserStore } from '@/js/stores/useUserStore'
 import { useComponentsStore } from '@/js/stores/useComponentsStore'
 import { useStatesStore } from '@/js/stores/useStatesStore'
+
+import OhLayoutPage from '@/components/widgets/layout/oh-layout-page.vue'
+const OhMapPage = defineAsyncComponent(() => import('@/components/widgets/map/oh-map-page.vue'))
+const OhPlanPage = defineAsyncComponent(() => import('@/components/widgets/plan/oh-plan-page.vue'))
+const OhChartPage = defineAsyncComponent(() => import('@/components/widgets/chart/oh-chart-page.vue'))
+
+function pageComponent(page) {
+  if (!page.component) return null
+  switch (page.component) {
+    case 'oh-layout-page':
+      return OhLayoutPage
+    case 'oh-map-page':
+      return OhMapPage
+    case 'oh-plan-page':
+      return OhPlanPage
+    case 'oh-chart-page':
+      return OhChartPage
+  }
+  return null
+}
 
 export default {
   props: {
@@ -54,7 +74,7 @@ export default {
     },
     componentType() {
       if (this.page) {
-        return widgetRegistry.page(this.page.component)
+        return pageComponent(this.page)
       } else if (this.widget || this.standard) {
         return 'generic-widget-component'
       }
@@ -88,7 +108,7 @@ export default {
     },
     tabComponent(tab) {
       const page = useComponentsStore().page(tab.config.page.replace('page:', ''))
-      return widgetRegistry.page(page.component)
+      return pageComponent(page)
     }
   }
 }

bundles/org.openhab.ui/web/src/main.ts
@@ -13,6 +13,8 @@ import DeveloperDockIcon from './components/developer/developer-dock-icon.vue'
 import OhIconComponent from './components/widgets/system/oh-icon.vue'
 import GenericWidgetComponent from './components/widgets/generic-widget-component.vue'
 
+import { registerWidgets } from '@/components/oh-component-registry'
+
 // Import Framework7
 import Framework7 from 'framework7/lite-bundle'
 import Framework7Vue, { registerComponents } from 'framework7-vue/bundle'
@@ -71,6 +73,8 @@ app.component('OhNavContent', OhNavContent)
 app.component('DeveloperDockIcon', DeveloperDockIcon)
 app.component('OhIcon', OhIconComponent)
 app.component('GenericWidgetComponent', GenericWidgetComponent)
+
+registerWidgets(app)
 
 app.mount('#app')
 performance.mark('app-mounted')

bundles/org.openhab.ui/web/src/pages/home/overview-tab.vue
@@ -17,13 +17,7 @@
     </f7-block>
 
     <template v-else>
-      <component
-        :is="widgetRegistry.page(overviewPage.component)"
-        v-if="overviewPage"
-        v-show="!inChatSession"
-        :context="overviewPageContext"
-        :class="{ notready: !ready }"
-        :f7router />
+      <oh-layout-page v-if="overviewPage" v-show="!inChatSession" :context="overviewPageContext" :class="{ notready: !ready }" :f7router />
       <div v-else-if="!inChatSession" class="empty-overview">
         <empty-state-placeholder icon="house" title="overview.title" text="overview.text" />
         <f7-row v-if="!userStore.isAdmin() || $f7dim.width < 1280" class="display-flex justify-content-center">
@@ -78,8 +72,8 @@ import { defineAsyncComponent } from 'vue'
 import { f7 } from 'framework7-vue'
 import { mapStores } from 'pinia'
 
-import * as widgetRegistry from '@/components/oh-component-registry.ts'
 import EmptyStatePlaceholder from '@/components/empty-state-placeholder.vue'
+import OhLayoutPage from '@/components/widgets/layout/oh-layout-page.vue'
 
 import { useStatesStore } from '@/js/stores/useStatesStore'
 import { useUserStore } from '@/js/stores/useUserStore'
@@ -94,11 +88,12 @@ export default {
     f7router: Object
   },
   components: {
-    'empty-state-placeholder': EmptyStatePlaceholder,
+    OhLayoutPage,
+    EmptyStatePlaceholder,
     habot: defineAsyncComponent(() => import(/* webpackChunkName: "habot" */ '../../components/home/habot.vue'))
   },
   setup () {
-    return { f7, widgetRegistry }
+    return { f7 }
   },
   data () {
     return {

commit 5cbdaf5e5899c2dada80acdf64c87c08547be27a

Fix page-view doesn't render tabbed pages correctly
Regression from #4010.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/pages/page/page-view.vue
@@ -279,7 +279,7 @@ export default {
       }
 
       const page = useComponentsStore().page(tab.config.page.replace('page:', ''))
-      return page.component
+      return this.pageComponent(page)
     },
     tabEvaluateExpression (tab, idx, key) {
       const ctx = this.tabContext(tab)