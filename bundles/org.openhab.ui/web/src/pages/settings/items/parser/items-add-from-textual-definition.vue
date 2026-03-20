openhab/openhab-webui

commit 701daa1d8c3b20bc8c0ef70b46c51eebd983f3e0

Code editor: Migrate to TS & Add Vim keybindings support (#3884)
- Migrates script-editor to typescript
- Refactors language support to a separate file
- Refactors to use Codemirror 6 compartments to streamline changes vs.
  reloading the full component on changes
- Adds Vim mode using async loading to ensure no performance
  degradation
- Fixes mode for yaml+thing and yaml+item
- Refactors hints to TypeScript
- Introduces a new local developer settings section to developer tools

---------

Also-by: Florian Hotze <dev@florianhotze.com>
Signed-off-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/pages/settings/items/parser/items-add-from-textual-definition.vue
@@ -7,7 +7,7 @@
     <f7-block class="items-add-from-textual-definition">
       <div v-if="ready" class="row items-parser resizable">
         <div class="col">
-          <editor class="editor" :value="code" @input="(value: string) => code = value" :mode="mediaType" />
+          <editor class="editor" :value="code" @input="(value: string) => code = value" :mode="mediaType" @save="add()" />
         </div>
         <span class="resize-handler" />
       </div>

bundles/org.openhab.ui/web/src/assets/categories.ts
@@ -1,4 +1,4 @@
-export const Categories = [
+export const Categories: string[] = [
   'attic',
   'bath',
   'bedroom',

bundles/org.openhab.ui/web/src/assets/definitions/blockly/libraries-components.d.ts
@@ -1,0 +1,14 @@
+import type { WidgetDefinition } from '../index'
+
+export declare const BlockLibrariesComponentDefinitions: {
+  BlockLibraryDefinition: () => WidgetDefinition
+  BlockTypeDefinition: () => WidgetDefinition
+  BlockCodeTemplateDefinition: () => WidgetDefinition
+  PresetInputDefinition: () => WidgetDefinition
+  PresetFieldDefinition: () => WidgetDefinition
+  BlockAssemblyDefinition: () => WidgetDefinition
+  SeparatorDefinition: () => WidgetDefinition
+  UtilityFunction: () => WidgetDefinition
+  UtilityJavaType: () => WidgetDefinition
+  UtilityFrameworkService: () => WidgetDefinition
+}

bundles/org.openhab.ui/web/src/assets/definitions/media-types.ts
@@ -2,9 +2,16 @@ export type CodeEditorType = 'YAML' | 'DSL'
 
 export enum MediaType {
   YAML = 'application/yaml',
+  JSON = 'application/json',
+  JAVASCRIPT = 'application/javascript',
+  // openHAB vendor specific:
+  THING_YAML = 'application/yaml+thing',
+  ITEM_YAML = 'application/yaml+item',
   THING_DSL = 'text/vnd.openhab.dsl.thing',
   ITEM_DSL = 'text/vnd.openhab.dsl.item',
-  JSON = 'application/json'
+  RULE_YAML = 'application/vnd.openhab.rule+yaml',
+  RULE_DSL = 'application/vnd.openhab.dsl.rule',
+  UI_COMPONENT = 'application/vnd.openhab.uicomponent'
 }
 
 /**
@@ -19,11 +26,11 @@ export const DefaultMediaTypes = {
  */
 export const SupportedMediaTypes = {
   items: {
-    YAML: MediaType.YAML,
+    YAML: MediaType.ITEM_YAML,
     DSL: MediaType.ITEM_DSL
   },
   things: {
-    YAML: MediaType.YAML,
+    YAML: MediaType.THING_YAML,
     DSL: MediaType.THING_DSL
   }
 }

bundles/org.openhab.ui/web/src/assets/definitions/metadata/namespaces.d.ts
@@ -1,0 +1,7 @@
+export interface Namespace {
+  name: string
+  label: string
+}
+
+declare const namespaces: Namespace[]
+export default namespaces

bundles/org.openhab.ui/web/src/assets/item-types.ts
@@ -1,4 +1,4 @@
-const SharedTypes = [
+const SharedTypes: string[] = [
   'Call',
   'Color',
   'Contact',

bundles/org.openhab.ui/web/src/components/config/controls/code-editor.vue
@@ -151,7 +151,8 @@ export default {
     generateCode (codeType, onSuccessCallback) {
       codeType ||= this.uiOptionsStore.codeEditorType
       const sourceMediaType = MediaType.JSON
-      const targetMediaType = this.mediaTypes[codeType]
+      let targetMediaType = this.mediaTypes[codeType]
+      targetMediaType = targetMediaType.split('+')[0] // remove the +thing or +item suffix, if present
       const payload = {}
       payload[this.objectType] = [this.object]
       this.$oh.api.postPlain('/rest/file-format/create', JSON.stringify(payload), null, sourceMediaType, { accept: targetMediaType })
@@ -179,7 +180,8 @@ export default {
      * @param {function} onFailureCallback - Optional. A callback function to call when parsing fails or no object is found
      */
     parseCode (onSuccessCallback, onFailureCallback) {
-      const sourceMediaType = this.mediaTypes[this.uiOptionsStore.codeEditorType]
+      let sourceMediaType = this.mediaTypes[this.uiOptionsStore.codeEditorType]
+      sourceMediaType = sourceMediaType.split('+')[0] // remove the +thing or +item suffix, if present
       const targetMediaType = MediaType.JSON
       this.$oh.api.request({
         method: 'POST',

bundles/org.openhab.ui/web/src/components/config/controls/script-editor.vue
@@ -65,50 +65,28 @@
       width unset
 </style>
 
-<script>
+<script setup lang="ts">
+import { computed, shallowRef, watch, type ShallowRef } from 'vue'
 import { useUIOptionsStore } from '@/js/stores/useUIOptionsStore'
-import { mapStores } from 'pinia'
 
 // codemirror core
 import { Codemirror } from 'vue-codemirror'
-import { keymap, tooltips } from '@codemirror/view'
-import { EditorState, EditorSelection } from '@codemirror/state'
+import { keymap, tooltips, EditorView, type KeyBinding } from '@codemirror/view'
+import { EditorState, EditorSelection, Prec, Compartment, type Extension } from '@codemirror/state'
 import { defaultKeymap, historyKeymap, indentMore } from '@codemirror/commands'
-import { StreamLanguage, getIndentUnit, codeFolding } from '@codemirror/language'
-import { autocompletion, closeBrackets } from '@codemirror/autocomplete'
+import { codeFolding, getIndentUnit } from '@codemirror/language'
+import { closeBrackets } from '@codemirror/autocomplete'
 
-// for linting
-import { linter, lintGutter } from '@codemirror/lint'
-import YAML from 'yaml'
-import globals from 'globals'
-
-// dynamic import for better chunking
-const eslint = await import('eslint-linter-browserify')
+import { languageCompartmentExtension } from '../editor/editor-languages'
+import type { ExtendedEditorView, HintContext } from '../editor/types'
 
 // other extensions
 import { indentationMarkers } from '@replit/codemirror-indentation-markers'
 import { gruvboxDark } from '@uiw/codemirror-theme-gruvbox-dark'
 
-// languages
-import { javascript, esLint } from '@codemirror/lang-javascript'
-import { python } from '@codemirror/lang-python'
-import { groovy } from '@codemirror/legacy-modes/mode/groovy'
-import { ruby } from '@codemirror/legacy-modes/mode/ruby'
-import { java } from '@codemirror/lang-java'
-import { xml } from '@codemirror/lang-xml'
-import { yaml } from '@codemirror/lang-yaml'
-import { jinja2 } from '@codemirror/legacy-modes/mode/jinja2'
-import { properties } from '@codemirror/legacy-modes/mode/properties'
-import { shell } from '@codemirror/legacy-modes/mode/shell'
+const uiOptionsStore = useUIOptionsStore()
 
-// OH-specific CompletionSources
-import javascriptAutocompletions from '../editor/hint-javascript'
-import componentsHint from '../editor/hint-components'
-import rulesHint from '../editor/hint-rules'
-import thingsHint from '../editor/hint-things'
-import itemsHint from '../editor/hint-items'
-
-const KEYMAP = [
+const KEYMAP : KeyBinding[] = [
   {
     // The default indentWithTab will indent the line regardless of the cursor position.
     // This overrides this behavior so when you're at the beginning of the line, it would indent the line
@@ -143,7 +121,37 @@ const KEYMAP = [
   }
 ]
 
-const STANDARD_EXTENSIONS = [
+const props = defineProps<{
+  value: string | undefined
+  mode: string
+  hintContext?: HintContext
+  readOnly?: boolean
+}>()
+
+const emit = defineEmits<{
+  input: [newCode: string]
+  save: []
+}>()
+
+const cmView : ShallowRef<ExtendedEditorView | null> = shallowRef(null)
+
+const dynamicCompartment = new Compartment()
+const languageCompartment = new Compartment()
+const asyncCompartment = new Compartment()
+
+const dynamicExtensions = computed((): Extension[] => {
+  const extensions : Extension[] = []
+
+  if (uiOptionsStore.darkMode === 'dark') {
+    extensions.push(gruvboxDark)
+  }
+
+  extensions.push(EditorState.readOnly.of(props.readOnly ?? false))
+
+  return extensions
+})
+
+const extensions = [
   keymap.of([...defaultKeymap, ...historyKeymap, ...KEYMAP]),
   closeBrackets(),
   codeFolding(),
@@ -154,207 +162,57 @@ const STANDARD_EXTENSIONS = [
   tooltips({
     // This prevents the lint tooltip from going outside the editor and getting clipped
     tooltipSpace: (view) => view.contentDOM.getBoundingClientRect()
-  })
+  }),
+  dynamicCompartment.of(dynamicExtensions.value),
+  languageCompartment.of(languageCompartmentExtension(props.mode || '', !props.readOnly)),
+  asyncCompartment.of([])
 ]
 
-export default {
-  components: {
-    Codemirror
-  },
-  props: {
-    value: String,
-    mode: String,
-    hintContext: Object,
-    ternAutocompletionHook: Boolean,
-    readOnly: Boolean
-  },
-  emits: ['input'],
-  data () {
-    return {
-      code: this.value,
-      autocompletion: null
-    }
-  },
-  beforeUnmount () {
-    if (this.codemirror && this.codemirror.closeHint) {
-      this.codemirror.closeHint()
-    }
-  },
-  methods: {
-    /**
-     * Provides the CodeMirror language for the given mode.
-     * Thereby, this method handles the mapping of the mode to the language for scripts, transformations, etc.
-     *
-     * @param {string} mode
-     * @return {StreamLanguage<unknown>|LanguageSupport|null}
-     */
-    languageExtension (mode) {
-      switch (true) {
-        case mode.includes('yaml'):
-          return yaml()
+watch(dynamicExtensions, (newExtensions) => {
+  if (cmView.value) {
+    cmView.value.dispatch({
+      effects: dynamicCompartment.reconfigure(newExtensions)
+    })
+  }
+})
 
-        case mode === 'dsl':
-        case mode === 'application/vnd.openhab.dsl.rule':
-          return java()
+watch(() => props.mode, (newMode) => {
+  if (!cmView.value || !newMode) return
+  const extensions = languageCompartmentExtension(newMode)
+  cmView.value.dispatch({
+    effects: languageCompartment.reconfigure(extensions)
+    })
+})
 
-        case mode === 'text/vnd.openhab.dsl.thing':
-        case mode === 'text/vnd.openhab.dsl.item':
-          return javascript()
+function onCmReady ({ view }: { view: EditorView }) {
+  const extendedView = view as ExtendedEditorView
+  extendedView.originalMode = props.mode
 
-        case mode === 'js':
-        case mode.startsWith('application/javascript'):
-          return javascript()
+  if (props.hintContext) extendedView.hintContext = Object.assign({}, props.hintContext)
+  cmView.value = extendedView
 
-        case mode === 'py':
-        case mode === 'py2':
-        case mode === 'py3':
-        case mode.startsWith('application/x-python'):
-          return python()
+  loadAsyncCompartmentExtensions(view)
+}
 
-        case mode === 'rb':
-        case mode === 'application/x-ruby':
-          return StreamLanguage.define(ruby)
+function onCmCodeChange (newCode : string) {
+  emit('input', newCode)
+}
 
-        case mode === 'groovy':
-        case mode === 'application/x-groovy':
-          return StreamLanguage.define(groovy)
+async function loadAsyncCompartmentExtensions (cmView: EditorView) {
+  if (uiOptionsStore.codeMirrorSettings.vimMode) {
+    const { Vim, vim } = await import('@replit/codemirror-vim').catch(() => {
+      console.error('Failed to load Vim mode for CodeMirror')
+      return { Vim: null, vim: null }
+    })
+    if (!Vim || !vim) return
 
-        case mode === 'map':
-        case mode === 'scale':
-          return StreamLanguage.define(properties)
+    Vim.defineEx('write', 'w', () => {
+      emit('save')
+    })
 
-        case mode === 'exec':
-          return StreamLanguage.define(shell)
-
-        case mode === 'jinja':
-          return StreamLanguage.define(jinja2)
-
-        case mode === 'xslt':
-          return xml()
-
-        default:
-          console.debug('Unsupported editor mode:', mode)
-          return null
-      }
-    },
-    autocompletionExtension (mode) {
-      if (!mode) {
-        return null
-      }
-
-      const activateOnCompletion = () => true
-
-      switch (true) {
-        case mode.startsWith('application/vnd.openhab.uicomponent'):
-          return autocompletion({ activateOnCompletion, override: [ componentsHint ] })
-
-        case mode === 'application/vnd.openhab.rule+yaml':
-          return autocompletion({ activateOnCompletion, override: [ rulesHint ] })
-
-        case mode === 'application/vnd.openhab.thing+yaml':
-          return autocompletion({ activateOnCompletion, override: [ thingsHint ] })
-
-        case mode === 'application/vnd.openhab.item+yaml':
-          return autocompletion({ activateOnCompletion, override: [ itemsHint ] })
-
-        case mode === 'js':
-        case mode.startsWith('application/javascript'):
-          return javascriptAutocompletions(mode)
-
-        // CodeMirror supports autocompletion for python by default
-        default:
-          return autocompletion()
-      }
-    },
-    linterExtension (mode) {
-      if (this.readOnly) return null
-      switch (true) {
-        case mode.includes('yaml'):
-          return linter((view) => {
-            const parsed = YAML.parseDocument(view.state.doc.toString())
-            return parsed.errors.map((e) => {
-              return {
-                from: e.pos[0],
-                to: e.pos[1],
-                message: e.message,
-                severity: e.name === 'YAMLParseError' ? 'error' : 'warning'
-              }
-            })
-          })
-
-        case mode === 'js':
-        case mode.startsWith('application/javascript'):
-          const config = {
-            // eslint configuration
-            languageOptions: {
-              globals: {
-                ...globals.node,
-                // context:
-                ctx: 'readonly',
-                event: 'readonly',
-                ruleUID: 'readonly',
-                // openhab-js namespaces:
-                Java: 'readonly',
-                actions: 'readonly',
-                cache: 'readonly',
-                environment: 'readonly',
-                items: 'readonly',
-                rules: 'readonly',
-                things: 'readonly',
-                time: 'readonly',
-                utils: 'readonly',
-                Quantity: 'readonly'
-              },
-              sourceType: 'commonjs',
-              parserOptions: {
-                ecmaVersion: 2024,
-                ecmaFeatures: {
-                  globalReturn: true // allow return outside functions
-                }
-              }
-            },
-            rules: {
-              semi: 'off', // allow both with and without semicolons
-              'no-undef': 'error',
-              'no-const-assign': 'error',
-              'no-unreachable': 'warn'
-            }
-          }
-          return linter(esLint(new eslint.Linter(), config))
-      }
-    },
-    onCmReady (cm) {
-      cm.view.$oh = this.$oh
-      cm.view.originalMode = this.mode
-      if (this.hintContext) cm.view.hintContext = Object.assign({}, this.hintContext)
-    },
-    onCmCodeChange (newCode) {
-      this.$emit('input', newCode)
-    }
-  },
-  computed: {
-    extensions () {
-      const linter = this.linterExtension(this.mode)
-
-      let autocompletions = this.autocompletionExtension(this.mode)
-      autocompletions = Array.isArray(autocompletions) ? autocompletions : [autocompletions]
-
-      const extensions = [
-        ...STANDARD_EXTENSIONS,
-        EditorState.readOnly.of(this.readOnly),
-        this.languageExtension(this.mode),
-        ...autocompletions,
-        linter,
-        linter && lintGutter(),
-        useUIOptionsStore().darkMode === 'dark' ? gruvboxDark : null
-      ].filter((ext) => ext)
-
-      return extensions
-    },
-    codemirror () {
-      return this.$refs.cm.codemirror
-    },
-    ...mapStores(useUIOptionsStore)
+    cmView.dispatch({
+      effects: asyncCompartment.reconfigure([Prec.high(vim())])
+    })
   }
 }
 </script>

commit f87815b71e1100fdfc47c81bc3bda6ed5eea019c

Add Items from YAML: Fix parsing not working (#4007)
Fixes #3985
Regression from #3884.
Refs #3819.

mediaType for server is generically `application/yaml`, however for
proper processing in the UI,
we add the `+item` suffix - this has to be stripped off when using the
server parse API.

---------

Also-by: Florian Hotze <dev@florianhotze.com>
Signed-off-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/pages/settings/items/parser/items-add-from-textual-definition.vue
@@ -158,8 +158,9 @@ import debounce from 'debounce'
 import { useUIOptionsStore } from '@/js/stores/useUIOptionsStore.ts'
 import EmptyStatePlaceholder from '@/components/empty-state-placeholder.vue'
 import * as api from '@/api'
-import { type CodeEditorType, MediaType, SupportedMediaTypes } from '@/assets/definitions/media-types.ts'
+import { type CodeEditorType, SupportedMediaTypes } from '@/assets/definitions/media-types.ts'
 import { storeToRefs } from 'pinia'
+import { ApiError } from '@/js/hey-api.ts'
 
 const uiOptionsStore = useUIOptionsStore()
 
@@ -216,11 +217,13 @@ const load = async () => {
 }
 
 const parseItems = () => {
+  const serverMediaType = mediaType.value.split('+')[0]
+
   api.parse({
     body: code.value
   }, {
     headers: {
-      'Content-Type': mediaType.value,
+      'Content-Type': serverMediaType,
       accept: 'application/json'
     }
   }).then((data) => {
@@ -236,7 +239,11 @@ const parseItems = () => {
       return extendedItem
     })
   }).catch(async (err) => {
-    parseError.value = await err.response.text()
+    if (err instanceof ApiError) {
+      parseError.value = await err.response.text()
+      return
+    }
+    console.error('Unexpected error while parsing input', err)
   })
 }
 