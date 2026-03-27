b3cb81e569bb49e8c4ff40b4636461239351cfa3
Jeff James <jeff@james-online.com>
2026-03-27 06:51:00 -0700

    oh-repeater: Fix various issues, improve runtime error handling & refactor to TS (#4035)
    - Refactors oh-repeater to TypeScript
    - Improved runtime error checking in configuration
    - Use hey-api REST client
    - Fixes #2181 - for the getRules endpoint, the tags
    need to be broken out to individual ?tags=xxx parameters vs passing a
    comma concatenated string.
    - Fixes #2532 - when the childrenContexts changed, vue reused the existing
    components vs. re-rendering. Fixed by ensuring each element has a unique
    key so Vue know which component to reuse.
    - Don't pass configTags to the REST API for itemsInGroup source,
    as REST doesn't support tag filtering there.
    
    ---------
    
    Signed-off-by: Jeff James <jeff@james-online.com>

--- bundles/org.openhab.ui/web/src/components/widgets/widget-order.ts
+++ bundles/org.openhab.ui/web/src/components/widgets/widget-order.ts
@@ -1,15 +1,23 @@
-export function compareItems(i1, i2) {
+import * as api from '@/api'
+
+interface ItemWithModelPath extends api.EnrichedItem {
+  modelPath: ItemWithModelPath[]
+}
+
+export function compareItems(i1: api.EnrichedItem | ItemWithModelPath, i2: api.EnrichedItem | ItemWithModelPath) {
   const isLocationContext =
     i1.metadata &&
     i1.metadata.semantics &&
+    typeof i1.metadata.semantics.value === 'string' &&
     i1.metadata.semantics.value.indexOf('Location') === 0 &&
     i2.metadata &&
     i2.metadata.semantics &&
+    typeof i2.metadata.semantics.value === 'string' &&
     i2.metadata.semantics.value.indexOf('Location') === 0
 
   // Compare widgetOrder
-  const order1 = i1.metadata && i1.metadata.widgetOrder ? parseFloat(i1.metadata.widgetOrder.value) : Infinity
-  const order2 = i2.metadata && i2.metadata.widgetOrder ? parseFloat(i2.metadata.widgetOrder.value) : Infinity
+  const order1 = i1.metadata && i1.metadata.widgetOrder ? parseFloat(i1.metadata.widgetOrder.value as string) : Infinity
+  const order2 = i2.metadata && i2.metadata.widgetOrder ? parseFloat(i2.metadata.widgetOrder.value as string) : Infinity
   const widgetOrder = order1 !== order2 ? (order1 < order2 ? -1 : 1) : 0
 
   // Unless comparing Location Items simply return the order based on widgetOrder metadata if determined
@@ -28,26 +36,30 @@ export function compareItems(i1, i2) {
 
 // Compares items based on widgetOrder or lexicographical order of their ancestors in Model (starting at the top level)
 // Returns 0 if both items are siblings.
-export function compareParents(i1, i2) {
+export function compareParents(i1: api.EnrichedItem | ItemWithModelPath, i2: api.EnrichedItem | ItemWithModelPath) {
   let modelOrder = 0
-  if (i1.modelPath && i2.modelPath) {
+  if ('modelPath' in i1 && 'modelPath' in i2) {
     const minDepth = Math.min(i1.modelPath.length, i2.modelPath.length) // Compare shortest path
     for (let d = 0; d < minDepth; d++) {
       if (i1.modelPath[d] === i2.modelPath[d]) continue
 
       // widgetOrder comparison...
       const order1 =
-        i1.modelPath[d].metadata && i1.modelPath[d].metadata.widgetOrder ? i1.modelPath[d].metadata.widgetOrder.value : Infinity
+        i1.modelPath[d]!.metadata && i1.modelPath[d]!.metadata!.widgetOrder
+          ? (i1.modelPath[d]!.metadata!.widgetOrder!.value as number)
+          : Infinity
       const order2 =
-        i2.modelPath[d].metadata && i2.modelPath[d].metadata.widgetOrder ? i2.modelPath[d].metadata.widgetOrder.value : Infinity
+        i2.modelPath[d]!.metadata && i2.modelPath[d]!.metadata!.widgetOrder
+          ? (i2.modelPath[d]!.metadata!.widgetOrder!.value as number)
+          : Infinity
       if (order1 !== order2) {
         modelOrder = order1 < order2 ? -1 : 1
         break
       }
 
       // ... or lexicographical comparison
-      const nameOrLabel1 = i1.modelPath[d].label || i1.modelPath[d].name
-      const nameOrLabel2 = i2.modelPath[d].label || i2.modelPath[d].name
+      const nameOrLabel1 = i1.modelPath[d]!.label || i1.modelPath[d]!.name
+      const nameOrLabel2 = i2.modelPath[d]!.label || i2.modelPath[d]!.name
       modelOrder = nameOrLabel1.localeCompare(nameOrLabel2)
       if (modelOrder !== 0) break
     }
@@ -58,6 +70,7 @@ export function compareParents(i1, i2) {
   return modelOrder
 }
 
-export function compareRules(r1, r2) {
+export function compareRules(r1: api.EnrichedRule, r2: api.EnrichedRule) {
+  if (!r1.name || !r2.name) return r2.name ? 1 : 0
   return r1.name.localeCompare(r2.name)
 }
