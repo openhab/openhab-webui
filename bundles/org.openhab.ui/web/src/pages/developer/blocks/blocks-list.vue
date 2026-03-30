openhab/openhab-webui

commit 3d964429a682124afa69c1e1aafd29f9f8b31926

list navigation: Fix middle-mouse button navigation (#4036)
Fixes #2463

Uses default link navigation which enables use of middle mouse button to open new tab.

Signed-off-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/pages/developer/blocks/blocks-list.vue
@@ -67,10 +67,11 @@
             class="blockslist-item"
             :checkbox="showCheckboxes"
             :checked="isChecked(b.uid) ? true : null"
-            @click.ctrl="(e) => ctrlClick(e, b)"
-            @click.meta="(e) => ctrlClick(e, b)"
-            @click.exact="(e) => click(e, b)"
-            link=""
+            :prevent-router="showCheckboxes"
+            @click.ctrl="ctrlClick($event, b)"
+            @click.meta="ctrlClick($event, b)"
+            @click.exact="click($event, b)"
+            :link="`/settings/blocks/${b.uid}`"
             :title="b.uid">
             <template #subtitle>
               <div>
@@ -152,8 +153,6 @@ export default {
     click (event, item) {
       if (this.showCheckboxes) {
         this.toggleItemCheck(event, item.uid, item)
-      } else {
-        this.f7router.navigate(item.uid, { animate: false })
       }
     },
     ctrlClick (event, item) {

bundles/org.openhab.ui/web/src/js/routes.js
@@ -545,7 +545,10 @@ export default [
             path: ':uid',
             beforeEnter: [enforceAdminForRoute],
             beforeLeave: [checkDirtyBeforeLeave],
-            async: loadAsync(WidgetEditPage, ({ to }) => (to.params.uid === 'add' ? { createMode: true } : {}))
+            async: loadAsync(WidgetEditPage, ({ to }) => (to.params.uid === 'add' ? { createMode: true } : {})),
+            options: {
+              animate: false
+            }
           }
         ]
       },

bundles/org.openhab.ui/web/src/pages/developer/widgets/widget-list.vue
@@ -68,10 +68,11 @@
             class="widgetlist-item"
             :checkbox="showCheckboxes"
             :checked="isChecked(widget.uid)"
-            @click.ctrl="(e) => ctrlClick(e, widget)"
-            @click.meta="(e) => ctrlClick(e, widget)"
-            @click.exact="(e) => click(e, widget)"
-            link=""
+            :prevent-router="showCheckboxes"
+            @click.ctrl="ctrlClick($event, widget)"
+            @click.meta="ctrlClick($event, widget)"
+            @click.exact="click($event, widget)"
+            :link="`/developer/widgets/${widget.uid}`"
             :title="widget.uid">
             <template #subtitle>
               <div>
@@ -153,8 +154,6 @@ export default {
     click (event, item) {
       if (this.showCheckboxes) {
         this.toggleItemCheck(event, item.uid, item)
-      } else {
-        this.f7router.navigate(item.uid, { animate: false })
       }
     },
     ctrlClick (event, item) {

bundles/org.openhab.ui/web/src/pages/settings/items/items-list-vlist.vue
@@ -107,10 +107,11 @@
               class="itemlist-item"
               :checkbox="showCheckboxes"
               :checked="isChecked(item.name)"
-              @click.ctrl="(e) => ctrlClick(e, item)"
-              @click.meta="(e) => ctrlClick(e, item)"
-              @click.exact="(e) => click(e, item)"
-              link=""
+              :prevent-router="showCheckboxes"
+              @click.ctrl="ctrlClick($event, item)"
+              @click.meta="ctrlClick($event, item)"
+              @click.exact="click($event, item)"
+              :link="`/settings/items/${item.name}`"
               :title="(item.label) ? item.label : item.name"
               :footer="(item.label) ? item.name : '\xa0'"
               :subtitle="getItemTypeAndMetaLabel(item)"
@@ -332,8 +333,6 @@ export default {
     click (event, item) {
       if (this.showCheckboxes) {
         this.toggleItemCheck(event, item.name, item)
-      } else {
-        this.f7router.navigate(item.name)
       }
     },
     ctrlClick (event, item) {

bundles/org.openhab.ui/web/src/pages/settings/pages/pages-list.vue
@@ -92,10 +92,11 @@
               :checkbox="showCheckboxes && page.uid !== 'overview'"
               :checked="isChecked(((page.component === 'Sitemap') ? 'system:sitemap:' : 'ui:page:') + page.uid)"
               :disabled="showCheckboxes && page.uid === 'overview' ? true : null"
-              @click.ctrl="(e) => ctrlClick(e, page)"
-              @click.meta="(e) => ctrlClick(e, page)"
-              @click.exact="(e) => click(e, page)"
-              link=""
+              :prevent-router="showCheckboxes && page.uid !== 'overview'"
+              @click.ctrl="ctrlClick($event, page)"
+              @click.meta="ctrlClick($event, page)"
+              @click.exact="click($event, page)"
+              :link="`/settings/pages/${getPageType(page).type}/${page.uid}`"
               :title="page.config.label"
               :subtitle="getPageType(page).label"
               :footer="page.uid"
@@ -291,8 +292,6 @@ export default {
     click (event, item) {
       if (this.showCheckboxes) {
         this.toggleItemCheck(event, item.uid, item)
-      } else {
-        this.f7router.navigate(this.getPageType(item).type + '/' + item.uid)
       }
     },
     ctrlClick (event, item) {

bundles/org.openhab.ui/web/src/pages/settings/rules/rules-list.vue
@@ -173,10 +173,11 @@
               class="rulelist-item"
               :checkbox="showCheckboxes ? true : null"
               :checked="isChecked(rule.uid) ? true : null"
-              @click.ctrl="(e) => ctrlClick(e, rule)"
-              @click.meta="(e) => ctrlClick(e, rule)"
-              @click.exact="(e) => click(e, rule)"
-              link=""
+              :prevent-router="showCheckboxes"
+              @click.ctrl="ctrlClick($event, rule)"
+              @click.meta="ctrlClick($event, rule)"
+              @click.exact="click($event, rule)"
+              :link="`/settings/rules/${rule.uid}`"
               :title="rule.name"
               :text="rule.uid"
               :footer="rule.description"
@@ -516,8 +517,6 @@ export default {
     click (event, item) {
       if (this.showCheckboxes) {
         this.toggleItemCheck(event, item.uid, item)
-      } else {
-        this.f7router.navigate(item.uid)
       }
     },
     ctrlClick (event, item) {

bundles/org.openhab.ui/web/src/pages/settings/things/things-list.vue
@@ -154,10 +154,11 @@
               :checkbox="showCheckboxes"
               :checked="isChecked(thing.UID) ? true : null"
               :value="thing.UID"
-              @click.ctrl="(e) => ctrlClick(e, thing)"
-              @click.meta="(e) => ctrlClick(e, thing)"
-              @click.exact="(e) => click(e, thing)"
-              link=""
+              :prevent-router="showCheckboxes"
+              @click.ctrl="ctrlClick($event, thing)"
+              @click.meta="ctrlClick($event, thing)"
+              @click.exact="click($event, thing)"
+              :link="`/settings/things/${thing.UID}`"
               :title="thing.label || thing.UID">
               <template #footer>
                 <div>
@@ -474,8 +475,6 @@ export default {
     click (event, item) {
       if (this.showCheckboxes) {
         this.toggleItemCheck(event, item.UID, item)
-      } else {
-        this.f7router.navigate(item.UID)
       }
     },
     ctrlClick (event, item) {

commit cfe3db5d7d26ea49557fe1cbc5c86210eb1d11eb

Blocks: Fix links in block-list (#4060)
Fixes #4059 
Regression from #4036

Signed-off-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/pages/developer/blocks/blocks-list.vue
@@ -71,7 +71,7 @@
             @click.ctrl="ctrlClick($event, b)"
             @click.meta="ctrlClick($event, b)"
             @click.exact="click($event, b)"
-            :link="`/settings/blocks/${b.uid}`"
+            :link="`/developer/blocks/${b.uid}`"
             :title="b.uid">
             <template #subtitle>
               <div>