openhab/openhab-webui

commit e4270409cb111d7a68b321e4bae66177e6724ad3

Charts: Refactor to TypeScript, Composable & Composition API (#3977)
- Refactor `oh-*-axis`, `oh-*-series` and misc `oh-* chart` components
to TypeScript.
- Refactor chart-mixin to TS composable.
- Refactor oh-chart-component to TS Composition API.

---------

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/chart/useChart.ts
@@ -1,0 +1,351 @@
+import { ref, computed, watch, shallowRef, type ComputedRef, readonly } from 'vue'
+import dayjs, { type Dayjs } from 'dayjs'
+import IsoWeek from 'dayjs/plugin/isoWeek'
+import DayDuration from 'dayjs/plugin/duration'
+
+dayjs.extend(IsoWeek)
+dayjs.extend(DayDuration)
+
+import { useUIOptionsStore } from '@/js/stores/useUIOptionsStore'
+import { useRuntimeStore } from '@/js/stores/useRuntimeStore'
+import ComponentId from '../component-id'
+import * as api from '@/api'
+import { startOf, addOrSubtractPeriod as addOrSubtractPeriodUtil } from './util/time.ts'
+
+// Axis components
+import OhTimeAxis from './axis/oh-time-axis'
+import OhValueAxis from './axis/oh-value-axis'
+import OhCalendarAxis from './axis/oh-calendar-axis'
+import OhCategoryAxis from './axis/oh-category-axis'
+
+// Series components
+import OhDataSeries from './series/oh-data-series'
+import OhTimeSeries from './series/oh-time-series'
+import OhAggregateSeries from './series/oh-aggregate-series'
+import OhCalendarSeries from './series/oh-calendar-series'
+import OhStateSeries from './series/oh-state-series'
+
+// Other components
+import OhChartTooltip from './misc/oh-chart-tooltip'
+import OhChartVisualMap from './misc/oh-chart-visualmap'
+import OhChartDataZoom from './misc/oh-chart-datazoom'
+import OhChartLegend from './misc/oh-chart-legend'
+import OhChartTitle from './misc/oh-chart-title'
+import OhChartToolbox from './misc/oh-chart-toolbox'
+
+// Types
+import type { EChartsOption } from 'echarts'
+import { ChartType, type Period, type OhChart, PeriodType } from '@/types/components/widgets'
+import type { WidgetContext } from '../types'
+import type { ChartContext, SeriesOption, AxisComponent, SeriesComponent, ChartEvaluateExpressionFn, SeriesConfig } from './types'
+import type { ComponentOption } from 'echarts/types/dist/shared'
+
+const DEFAULT_PERIOD = PeriodType.D
+
+const axisComponents: Record<string, AxisComponent> = {
+  'oh-time-axis': OhTimeAxis,
+  'oh-value-axis': OhValueAxis,
+  'oh-calendar-axis': OhCalendarAxis,
+  'oh-category-axis': OhCategoryAxis
+}
+
+const seriesComponents: Record<string, SeriesComponent> = {
+  'oh-data-series': OhDataSeries,
+  'oh-time-series': OhTimeSeries,
+  'oh-aggregate-series': OhAggregateSeries,
+  'oh-calendar-series': OhCalendarSeries,
+  'oh-state-series': OhStateSeries
+}
+
+export function useChart(
+  context: WidgetContext,
+  config: ComputedRef<OhChart.Config>,
+  slots: ComputedRef<Record<string, api.UiComponent[]>>,
+  evaluateExpression: ChartEvaluateExpressionFn
+) {
+  const uiOptionsStore = useUIOptionsStore()
+  const runtimeStore = useRuntimeStore()
+
+  const speriod = ref<Period>(config.value.period || DEFAULT_PERIOD)
+  // future as boolean allows for backwards compatibility
+  const future = ref<number>((config.value.future as unknown as boolean) === true ? 1 : ((config.value.future as number) ?? 0))
+  const orient = ref<string | null>(null)
+
+  const addOrSubtractPeriod = (day: Dayjs, direction: number): Dayjs => {
+    if (!config.value) return day
+    const chartType = config.value.chartType || ChartType.dynamic
+    const p = evaluateExpression('.period', speriod.value) || (config.value.period as Period) || DEFAULT_PERIOD
+    return addOrSubtractPeriodUtil(chartType, p, day, direction)
+  }
+
+  const initialEndTime = (): Dayjs => {
+    const chartType = config.value.chartType
+    if (chartType) {
+      return addOrSubtractPeriod(startOf(chartType), 1 + future.value)
+    } else {
+      return addOrSubtractPeriod(dayjs(), future.value)
+    }
+  }
+
+  const endTime = ref<Dayjs>(initialEndTime())
+
+  // computed
+  const numberFormatter = computed(() => new Intl.NumberFormat(runtimeStore.locale))
+
+  const startTime = computed(() => addOrSubtractPeriod(endTime.value, -1))
+
+  const period = computed(() => evaluateExpression('.period', speriod.value))
+
+  const chartContext = computed<ChartContext>(() => ({
+    chart: {
+      ...(context.component as api.UiComponent),
+      config: config.value
+    },
+    evaluateExpression,
+    numberFormatter: numberFormatter.value
+  }))
+
+  const grid = computed(() => {
+    if (!slots.value?.grid) return []
+    return slots.value.grid.map((g) => g.config)
+  })
+
+  const xAxis = computed(() => {
+    if (!slots.value?.xAxis) return []
+    return slots.value.xAxis.map((a) => axisComponents[a.component]!.get(chartContext.value, a, startTime.value, endTime.value))
+  })
+
+  const yAxis = computed(() => {
+    if (!slots.value || !slots.value.yAxis) return []
+    return slots.value.yAxis.map((a) => axisComponents[a.component]!.get(chartContext.value, a, startTime.value, endTime.value, true))
+  })
+
+  const calendar = computed(() => {
+    if (!slots.value?.calendar) return []
+    return slots.value.calendar.map((a) =>
+      axisComponents[a.component]!.get(chartContext.value, a, startTime.value, endTime.value, orient.value === 'vertical')
+    )
+  })
+
+  const singleAxis = computed(() => {
+    if (!slots.value?.singleAxis) return []
+    return slots.value.singleAxis.map((a) => axisComponents[a.component]!.get(chartContext.value, a, startTime.value, endTime.value))
+  })
+
+  const tooltip = computed<ComponentOption[]>(() => {
+    if (!slots.value?.tooltip) return []
+    return slots.value.tooltip.map((c) => OhChartTooltip.get(chartContext.value, c, startTime.value, endTime.value))
+  })
+
+  const visualMap = computed<ComponentOption[]>(() => {
+    if (!slots.value) return []
+    const visualMapContext = {
+      ...chartContext.value,
+      series: series.value
+    }
+    if (slots.value.visualMap) {
+      return slots.value.visualMap.map((c) => OhChartVisualMap.get(visualMapContext, c, startTime.value, endTime.value))
+    } else if (JSON.stringify(slots.value.series)?.includes('heatmap')) {
+      const heatmapConfig = {
+        calculable: true,
+        presetPalette: 'bluered',
+        show: false,
+        type: 'continuous'
+      }
+      return [
+        OhChartVisualMap.get(
+          visualMapContext,
+          { component: 'oh-chart-visualmap', config: heatmapConfig } satisfies api.UiComponent,
+          startTime.value,
+          endTime.value
+        )
+      ]
+    }
+    return []
+  })
+
+  const dataZoom = computed(() => {
+    if (!slots.value?.dataZoom) return []
+    return slots.value.dataZoom.map((c) => OhChartDataZoom.get(chartContext.value, c, startTime.value, endTime.value))
+  })
+
+  const legend = computed(() => {
+    if (!slots.value?.legend) return []
+    return slots.value.legend.map((c) => OhChartLegend.get(chartContext.value, c, startTime.value, endTime.value))
+  })
+
+  const title = computed(() => {
+    if (!slots.value?.title) return []
+    return slots.value.title.map((c) => OhChartTitle.get(chartContext.value, c, startTime.value, endTime.value))
+  })
+
+  const toolbox = computed(() => {
+    if (!slots.value?.toolbox) return []
+    return slots.value.toolbox.map((c) => OhChartToolbox.get(chartContext.value, c, startTime.value, endTime.value))
+  })
+
+  // async computed
+  const series = shallowRef<SeriesOption[]>([])
+
+  const _items: Record<string, api.EnrichedItem> = {}
+  const _itemPromises: Record<string, Promise<api.EnrichedItem>> = {}
+  const _persistencePromises: Record<string, Promise<api.ItemHistory>> = {}
+
+  const getSeriesPromises = async (component: api.UiComponent): Promise<SeriesOption> => {
+    const config = evaluateExpression<SeriesConfig>(ComponentId.get(component)!, component.config)
+
+    const getter = (data: [api.EnrichedItem, api.ItemHistory][]): SeriesOption =>
+      seriesComponents[component.component]!.get(
+        chartContext.value,
+        component,
+        data.map((d) => d[1]),
+        startTime.value,
+        endTime.value
+      )
+
+    const neededItems = seriesComponents[component.component]!.neededItems(chartContext.value, component).filter((i) => !!i)
+    if (neededItems.length === 0) {
+      return getter([])
+    }
+
+    const now = dayjs()
+    const isBetweenStartAndEnd =
+      dayjs(startTime.value).subtract(5, 'minutes').isBefore(now) && dayjs(endTime.value).add(5, 'minutes').isAfter(now)
+
+    let boundary = seriesComponents[component.component]!.includeBoundary?.(chartContext.value, component) ?? isBetweenStartAndEnd
+    if (config.noBoundary === true) boundary = false
+
+    let itemState = seriesComponents[component.component]!.includeItemState?.(chartContext.value, component) ?? isBetweenStartAndEnd
+    if (config.noItemState === true) itemState = false
+
+    neededItems.forEach((neededItem) => {
+      if (_itemPromises[neededItem]) {
+        // do nothing
+      } else if (_items[neededItem]) {
+        _itemPromises[neededItem] = Promise.resolve(_items[neededItem])
+      } else {
+        _itemPromises[neededItem] = api.getItemByName({ itemName: neededItem }).then((item) => {
+          _items[neededItem] = item!
+          delete _itemPromises[neededItem]
+          return item!
+        })
+      }
+    })
+
+    const combinedPromises = neededItems.map(async (neededItem) => {
+      let seriesStartTime = startTime.value
+      let seriesEndTime = endTime.value
+      if (config.offsetAmount && config.offsetUnit) {
+        seriesStartTime = seriesStartTime.subtract(config.offsetAmount, config.offsetUnit as dayjs.ManipulateType)
+        seriesEndTime = seriesEndTime.subtract(config.offsetAmount, config.offsetUnit as dayjs.ManipulateType)
+      }
+      const query = {
+        itemName: neededItem,
+        serviceId: config.service,
+        starttime: seriesStartTime.toISOString(),
+        endtime: seriesEndTime.subtract(1, 'millisecond').toISOString(),
+        boundary,
+        itemState
+      }
+      const key = `${neededItem}-${query.serviceId ?? 'default'}-${query.starttime}-${query.endtime}-${boundary.toString()}-${itemState.toString()}`
+      if (!_persistencePromises[key]) {
+        _persistencePromises[key] = api.getItemDataFromPersistenceService(query).then((result) => {
+          delete _persistencePromises[key]
+          return result!
+        })
+      }
+
+      return await Promise.all([_itemPromises[neededItem] as Promise<api.EnrichedItem>, _persistencePromises[key]])
+    })
+
+    return Promise.all(combinedPromises).then(getter)
+  }
+
+  watch(
+    [slots, startTime, endTime, chartContext],
+    (_newVal, _oldVal, onCleanup) => {
+      let isStale = false
+
+      // invoked when the watch is re-triggered or stopped
+      onCleanup(() => {
+        isStale = true
+      })
+
+      const updateSeries = async (): Promise<void> => {
+        if (!slots.value || !slots.value.series) {
+          series.value = []
+          return
+        }
+        if (!isStale) {
+          // only request series if the watch is not stale, i.e., the most-recently triggered instance
+          const result = await Promise.all(slots.value.series.map(async (s) => await getSeriesPromises(s)))
+          if (!isStale) {
+            // only update series if the watch is not stale, i.e., the most-recently triggered instance
+            series.value = result
+          }
+        }
+      }
+
+      void updateSeries()
+    },
+    { immediate: true }
+  )
+
+  const options = computed<EChartsOption>(() => {
+    if (!config.value) return {}
+    const chartConfig = ((config.value as { options?: EChartsOption }).options as EChartsOption) || {}
+    if (!chartConfig.backgroundColor && uiOptionsStore.darkMode === 'dark') {
+      chartConfig.backgroundColor = '#121212'
+    }
+
+    const cast = <T>(val: unknown): T => val as T
+
+    return {
+      ...chartConfig,
+      grid: cast(grid.value),
+      xAxis: cast(xAxis.value),
+      yAxis: cast(yAxis.value),
+      calendar: cast(calendar.value),
+      singleAxis: cast(singleAxis.value),
+      tooltip: cast(tooltip.value),
+      visualMap: cast(visualMap.value),
+      dataZoom: cast(dataZoom.value),
+      legend: cast(legend.value),
+      title: cast(title.value),
+      toolbox: cast(toolbox.value),
+      series: cast(series.value)
+    }
+  })
+
+  // methods
+  const setPeriod = (periodValue: Period) => {
+    speriod.value = periodValue
+    endTime.value = addOrSubtractPeriod(dayjs(), future.value)
+  }
+
+  const setDate = (date: Date | string | number | Dayjs) => {
+    const chartType = config.value.chartType
+    const day = dayjs(date)
+    endTime.value = addOrSubtractPeriod(chartType ? startOf(chartType, day) : day, 1)
+  }
+
+  const earlierPeriod = () => {
+    endTime.value = addOrSubtractPeriod(endTime.value, -1)
+  }
+
+  const laterPeriod = () => {
+    endTime.value = addOrSubtractPeriod(endTime.value, 1)
+  }
+
+  return {
+    endTime: readonly(endTime),
+    startTime,
+    period,
+    options,
+
+    setPeriod,
+    setDate,
+    earlierPeriod,
+    laterPeriod
+  }
+}

bundles/org.openhab.ui/web/build/generate-widget-component-ts.config.js
@@ -1,6 +1,6 @@
 export default {
     defaultExportAs: 'enum',
-    '*': {      // global modifer applied to all components
+    '*': { // global modifier applied to all components
         modifier: (e) => e
           .replace('actionAnalyzerItems: string', 'actionAnalyzerItems?: string | string[]') // Fix array type
           .replace('taphold_actionAnalyzerItems: string', 'taphold_actionAnalyzerItems?: string | string[]') // Fix array type
@@ -9,6 +9,9 @@ export default {
     },
     // modifiers to apply to all components in common.ts
     _Common: {
+        '*': {
+            modifier: (e) => e.replace('export enum Period {', 'export type Period = `${number}${PeriodType}` | PeriodType\n\nexport enum PeriodType {')
+        },
         ChartType: {
             modifier: (e) => e.replace('none', 'dynamic')
         },

bundles/org.openhab.ui/web/build/generate-widget-component-ts.mjs
@@ -180,6 +180,11 @@ function generateCommonTS(mapCommonOptions) {
       content += optionStr
     }
   })
+
+  if(configCommon['*'] && typeof configCommon['*'].modifier === 'function') {
+    content = configCommon['*'].modifier(content)
+  }
+
   fs.writeFileSync(`${outDir}/common.gen.ts`, content)
 }
 
@@ -342,12 +347,12 @@ function generateComponentTS(mapCommonOptions) {
       let postamble = ''
       if (commonComponents.length > 0) {
         preamble += 'import {\n'
-        preamble += commonComponents.map(name => `  ${name}`).join(',\n')
+        preamble += commonComponents.map(name => name === 'Period' ? `  type ${name}` : `  ${name}`).join(',\n')
         const depth = widgetDir.split('/').length
         preamble += `\n} from '${'../'.repeat(depth)}common.gen.ts'\n\n`
 
         postamble += '\nexport {\n'
-        postamble += commonComponents.map(name => `  ${name}`).join(',\n')
+        postamble += commonComponents.map(name => name === 'Period' ? `  type ${name}` : `  ${name}`).join(',\n')
         postamble += '\n}\n'
       }
 

bundles/org.openhab.ui/web/patches/echarts+5.6.0.patch
@@ -1,0 +1,67 @@
+diff --git a/node_modules/echarts/types/dist/shared.d.ts b/node_modules/echarts/types/dist/shared.d.ts
+index 8f54be7..716d36e 100644
+--- a/node_modules/echarts/types/dist/shared.d.ts
++++ b/node_modules/echarts/types/dist/shared.d.ts
+@@ -2375,7 +2375,7 @@ declare const AXIS_TYPES: {
+     readonly log: 1;
+ };
+ declare type OptionAxisType = keyof typeof AXIS_TYPES;
+-interface AxisBaseOptionCommon extends ComponentOption, AnimationOptionMixin {
++export interface AxisBaseOptionCommon extends ComponentOption, AnimationOptionMixin {
+     type?: OptionAxisType;
+     show?: boolean;
+     inverse?: boolean;
+@@ -2424,7 +2424,7 @@ interface AxisBaseOptionCommon extends ComponentOption, AnimationOptionMixin {
+     }) => ScaleDataValue);
+     startValue?: number;
+ }
+-interface NumericAxisBaseOptionCommon extends AxisBaseOptionCommon {
++export interface NumericAxisBaseOptionCommon extends AxisBaseOptionCommon {
+     boundaryGap?: [number | string, number | string];
+     /**
+      * AxisTick and axisLabel and splitLine are calculated based on splitNumber.
+@@ -2450,7 +2450,7 @@ interface NumericAxisBaseOptionCommon extends AxisBaseOptionCommon {
+      */
+     alignTicks?: boolean;
+ }
+-interface CategoryAxisBaseOption extends AxisBaseOptionCommon {
++export interface CategoryAxisBaseOption extends AxisBaseOptionCommon {
+     type?: 'category';
+     boundaryGap?: boolean;
+     axisLabel?: AxisLabelOption<'category'> & {
+@@ -2466,7 +2466,7 @@ interface CategoryAxisBaseOption extends AxisBaseOptionCommon {
+         interval?: 'auto' | number | ((index: number, value: string) => boolean);
+     };
+ }
+-interface ValueAxisBaseOption extends NumericAxisBaseOptionCommon {
++export interface ValueAxisBaseOption extends NumericAxisBaseOptionCommon {
+     type?: 'value';
+     axisLabel?: AxisLabelOption<'value'>;
+     /**
+@@ -2481,7 +2481,7 @@ interface LogAxisBaseOption extends NumericAxisBaseOptionCommon {
+     axisLabel?: AxisLabelOption<'log'>;
+     logBase?: number;
+ }
+-interface TimeAxisBaseOption extends NumericAxisBaseOptionCommon {
++export interface TimeAxisBaseOption extends NumericAxisBaseOptionCommon {
+     type?: 'time';
+     axisLabel?: AxisLabelOption<'time'>;
+ }
+@@ -2571,7 +2571,7 @@ interface SplitAreaOption {
+     interval?: 'auto' | number | ((index: number, value: string) => boolean);
+     areaStyle?: AreaStyleOption<ZRColor[]>;
+ }
+-declare type AxisBaseOption = ValueAxisBaseOption | LogAxisBaseOption | CategoryAxisBaseOption | TimeAxisBaseOption | AxisBaseOptionCommon;
++export type AxisBaseOption = ValueAxisBaseOption | LogAxisBaseOption | CategoryAxisBaseOption | TimeAxisBaseOption | AxisBaseOptionCommon;
+ 
+ interface AxisModelCommonMixin<Opt extends AxisBaseOption> extends Pick<Model<Opt>, 'option'> {
+     axis: Axis;
+@@ -7173,7 +7173,7 @@ interface CommonAxisPointerOption {
+         dataIndexInside: number;
+     }[];
+ }
+-interface ComponentOption {
++export interface ComponentOption {
+     mainType?: string;
+     type?: string;
+     id?: OptionId;

bundles/org.openhab.ui/web/src/assets/definitions/widgets/chart/index.ts
@@ -151,6 +151,14 @@ const seriesTypeParameter = (...types: string[]): WidgetDefinitionParameter => {
   )
 }
 
+const lineSeriesSymbolParameter = pb(
+  'showSymbol',
+  'Show Symbol',
+  'Whether to always show the datapoint symbol. It will always be shown during tooltip hover.'
+).v((_value, configuration) => {
+  return configuration.type === 'line'
+})
+
 const aggregationFunctionParameter = pt(
   'aggregationFunction',
   'Aggregation Function',
@@ -234,7 +242,7 @@ const chartComponentsList: WidgetDefinition[] = [
   new WidgetDefinition('oh-data-series', 'Data Series', '')
     .doc('https://echarts.apache.org/en/option.html#series')
     .paramGroup(actionGroup())
-    .params([seriesTypeParameter('line', 'bar', 'heatmap', 'scatter', 'gauge', 'pie'), ...actionParams()]),
+    .params([seriesTypeParameter('line', 'bar', 'heatmap', 'scatter', 'gauge', 'pie'), lineSeriesSymbolParameter, ...actionParams()]),
 
   new WidgetDefinition('oh-time-series', 'Time Series', '')
     .doc('https://echarts.apache.org/en/option.html#series')
@@ -244,6 +252,7 @@ const chartComponentsList: WidgetDefinition[] = [
       ...seriesParameters,
       markersParameter(true),
       seriesTypeParameter('line', 'bar', 'heatmap', 'scatter'),
+      lineSeriesSymbolParameter,
       xAxisIndexParameter,
       yAxisIndexParameter,
       ...actionParams()
@@ -273,6 +282,7 @@ const chartComponentsList: WidgetDefinition[] = [
       ...seriesParameters,
       markersParameter(),
       seriesTypeParameter('line', 'bar', 'heatmap', 'scatter'),
+      lineSeriesSymbolParameter,
       po(
         'dimension1',
         'First Dimension',
@@ -299,6 +309,7 @@ const chartComponentsList: WidgetDefinition[] = [
     .params([
       ...seriesParameters,
       seriesTypeParameter('heatmap', 'scatter'),
+      lineSeriesSymbolParameter,
       aggregationFunctionParameter,
       calendarIndexParameter,
       ...actionParams()
@@ -306,7 +317,16 @@ const chartComponentsList: WidgetDefinition[] = [
 
   new WidgetDefinition('oh-chart-tooltip', 'Tooltip', '')
     .doc('https://echarts.apache.org/en/option.html#tooltip')
-    .params([showParameter(), orientParameter(), pb('confine', 'Confine', 'Keep the tooltip within the chart bounds')]),
+    .params([
+      showParameter(),
+      orientParameter(),
+      pb('confine', 'Confine', 'Keep the tooltip within the chart bounds'),
+      pb(
+        'smartFormatter',
+        'Smart Formatter',
+        'Automatically format numbers according to local configuration (e.g., decimal places) & Display markArea information'
+      ).a()
+    ]),
 
   new WidgetDefinition('oh-chart-visualmap', 'Visual Map', '')
     .doc('https://echarts.apache.org/en/option.html#visualMap')

bundles/org.openhab.ui/web/src/components/widgets/chart/axis/oh-calendar-axis.js
@@ -1,37 +1,0 @@
-import ComponentId from '../../component-id'
-
-import { useUIOptionsStore } from '@/js/stores/useUIOptionsStore'
-
-export default {
-  get(component, startTime, endTime, chart, chartWidget, inverse, numberFormatter) {
-    let calendar = chartWidget.evaluateExpression(ComponentId.get(component), component.config)
-    calendar.range = [startTime.toDate(), endTime.subtract(1, 'day').toDate()]
-    if (inverse) calendar.orient = inverse
-    if (!calendar.dayLabel) calendar.dayLabel = {}
-    if (calendar.dayLabel.firstDay === undefined) calendar.dayLabel.firstDay = 1
-    if (calendar.dayLabel.margin === undefined) calendar.dayLabel.margin = 5
-    if (!calendar.monthName) calendar.monthName = {}
-    if (calendar.monthName.margin === undefined) calendar.monthName.margin = 5
-
-    if (!calendar.top) calendar.top = 100
-    if (!calendar.bottom) calendar.bottom = 50
-    if (!calendar.left) calendar.left = 60
-    if (!calendar.right) calendar.right = 50
-
-    if (useUIOptionsStore().darkMode === 'dark') {
-      if (!calendar.itemStyle) calendar.itemStyle = {}
-      if (!calendar.itemStyle.color) calendar.itemStyle.color = '#333'
-      if (!calendar.itemStyle.borderColor) calendar.itemStyle.borderColor = '#555'
-      if (!calendar.itemStyle) calendar.itemStyle = {}
-      if (!calendar.dayLabel) calendar.dayLabel = {}
-      if (!calendar.dayLabel.color) calendar.dayLabel.color = '#aaa'
-      if (!calendar.monthLabel) calendar.monthLabel = {}
-      if (!calendar.monthLabel.color) calendar.monthLabel.color = '#aaa'
-      if (!calendar.splitLine) calendar.splitLine = {}
-      if (!calendar.splitLine.lineStyle) calendar.splitLine.lineStyle = {}
-      if (!calendar.splitLine.lineStyle.color) calendar.splitLine.lineStyle.color = '#aaa'
-    }
-
-    return calendar
-  }
-}

bundles/org.openhab.ui/web/src/components/widgets/chart/axis/oh-calendar-axis.ts
@@ -1,0 +1,38 @@
+import ComponentId from '../../component-id'
+import { useUIOptionsStore } from '@/js/stores/useUIOptionsStore'
+import type { AxisComponent } from '../types'
+import type { CalendarOption } from 'echarts/types/dist/shared'
+
+const calendarAxis: AxisComponent = {
+  get(context, component, startTime, endTime) {
+    const calendar = context.evaluateExpression<CalendarOption>(ComponentId.get(component)!, component.config)
+    calendar.range = [startTime.toDate(), endTime.subtract(1, 'day').toDate()]
+    if (!calendar.dayLabel) calendar.dayLabel = ({} as CalendarOption['dayLabel'])!
+    if (calendar.dayLabel.firstDay === undefined) calendar.dayLabel.firstDay = 1
+    if (calendar.dayLabel.margin === undefined) calendar.dayLabel.margin = 5
+    if (!calendar.monthLabel) calendar.monthLabel = ({} as CalendarOption['monthLabel'])!
+    if (calendar.monthLabel.margin === undefined) calendar.monthLabel.margin = 5
+
+    if (!calendar.top) calendar.top = 100
+    if (!calendar.bottom) calendar.bottom = 50
+    if (!calendar.left) calendar.left = 60
+    if (!calendar.right) calendar.right = 50
+
+    if (useUIOptionsStore().darkMode === 'dark') {
+      if (!calendar.itemStyle) calendar.itemStyle = ({} as CalendarOption['itemStyle'])!
+      if (!calendar.itemStyle.color) calendar.itemStyle.color = '#333'
+      if (!calendar.itemStyle.borderColor) calendar.itemStyle.borderColor = '#555'
+      if (!calendar.dayLabel) calendar.dayLabel = ({} as CalendarOption['dayLabel'])!
+      if (!calendar.dayLabel.color) calendar.dayLabel.color = '#aaa'
+      if (!calendar.monthLabel) calendar.monthLabel = ({} as CalendarOption['monthLabel'])!
+      if (!calendar.monthLabel.color) calendar.monthLabel.color = '#aaa'
+      if (!calendar.splitLine) calendar.splitLine = ({} as CalendarOption['splitLine'])!
+      if (!calendar.splitLine.lineStyle) calendar.splitLine.lineStyle = ({} as { color: string })!
+      if (!calendar.splitLine.lineStyle.color) calendar.splitLine.lineStyle.color = '#aaa'
+    }
+
+    return calendar
+  }
+}
+
+export default calendarAxis

commit 57a881cbdf076de1e655f196a70af7dce5833aa2

useChart: Fix faulty async computed workaround, use proper computedAsync instead
Regression from #3977.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/chart/useChart.ts
@@ -1,4 +1,5 @@
-import { ref, computed, watch, shallowRef, type ComputedRef, readonly } from 'vue'
+import { ref, computed, type ComputedRef, readonly } from 'vue'
+import { computedAsync } from '@vueuse/core'
 import dayjs, { type Dayjs } from 'dayjs'
 import IsoWeek from 'dayjs/plugin/isoWeek'
 import DayDuration from 'dayjs/plugin/duration'
@@ -188,13 +189,9 @@ export function useChart(
     return slots.value.toolbox.map((c) => OhChartToolbox.get(chartContext.value, c, startTime.value, endTime.value))
   })
 
-  // async computed
-  const series = shallowRef<SeriesOption[]>([])
-
   const _items: Record<string, api.EnrichedItem> = {}
   const _itemPromises: Record<string, Promise<api.EnrichedItem>> = {}
   const _persistencePromises: Record<string, Promise<api.ItemHistory>> = {}
-
   const getSeriesPromises = async (component: api.UiComponent): Promise<SeriesOption> => {
     const config = evaluateExpression<SeriesConfig>(ComponentId.get(component)!, component.config)
 
@@ -268,35 +265,12 @@ export function useChart(
     return Promise.all(combinedPromises).then(getter)
   }
 
-  watch(
-    [slots, startTime, endTime, chartContext],
-    (_newVal, _oldVal, onCleanup) => {
-      let isStale = false
-
-      // invoked when the watch is re-triggered or stopped
-      onCleanup(() => {
-        isStale = true
-      })
-
-      const updateSeries = async (): Promise<void> => {
-        if (!slots.value || !slots.value.series) {
-          series.value = []
-          return
-        }
-        if (!isStale) {
-          // only request series if the watch is not stale, i.e., the most-recently triggered instance
-          const result = await Promise.all(slots.value.series.map(async (s) => await getSeriesPromises(s)))
-          if (!isStale) {
-            // only update series if the watch is not stale, i.e., the most-recently triggered instance
-            series.value = result
-          }
-        }
-      }
-
-      void updateSeries()
-    },
-    { immediate: true }
-  )
+  const series = computedAsync<SeriesOption[]>(async () => {
+    if (!slots.value || !slots.value.series) {
+      return []
+    }
+    return await Promise.all(slots.value.series.map(async (s) => await getSeriesPromises(s)))
+  })
 
   const options = computed<EChartsOption>(() => {
     if (!config.value) return {}