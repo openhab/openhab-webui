4563d4fcb4f77c4e82530c61afafaedf3eeae4b8
Mark Herwege <mark.herwege@telenet.be>
2026-04-01 13:41:40 +0200

    extra info steps and refactoring
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

---
+++ bundles/org.openhab.ui/web/src/pages/wizards/setup-wizard-info.vue
@@ -0,0 +1,53 @@
+<template>
+    <tab-header
+        :icon="icon"
+        :title="t('setupwizard.' + step + '.title')"
+        :step="step"
+        :t="t" />
+    <f7-login-screen-title>
+        <div class="padding">
+        <img style="width: 85%" :src="image" />
+        </div>
+    </f7-login-screen-title>
+    <f7-block>
+        {{ body }}
+        <br /><br />
+        <a class="text-color-blue external" target="_blank" :href="link">
+        {{ t('setupwizard.documentationLink') }}</a
+        >
+        <br /><br />
+        {{ t('setupwizard.' + step + '.nextDescription') }}
+    </f7-block>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default {
+  props: {
+    step: String,
+    icon: String,
+    image: String,
+    link: String,
+    t: Function
+  },
+  components: {
+    'tab-header': defineAsyncComponent(() => import('./setup-wizard-tab-header.vue'))
+  },
+  computed: {
+    body () {
+      if (!this.step) return null
+      let body = ''
+      let index = 1
+      while (true) {
+        const line = this.t('setupwizard.' + this.step + '.body' + index)
+        const hasLine = line !== 'setupwizard.' + this.step + '.body' + index
+        if (!hasLine) break
+        body += line
+        index++
+      }
+      return body
+    },
+  }
+}
+</script>
