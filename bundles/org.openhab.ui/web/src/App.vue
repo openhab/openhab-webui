738a7cc34196ed343194dddc2eb9c37ee12539e3
Jimmy Tanagra <jcode@tanagra.id.au>
2026-04-21 21:34:41 +1000

    fix inconsistent left-panel breakpoint
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- bundles/org.openhab.ui/web/src/App.vue
+++ bundles/org.openhab.ui/web/src/App.vue
@@ -6,7 +6,7 @@
       visibility: userStore.user || componentsStore.page('overview') || communicationFailureMsg ? '' : 'hidden'
     }">
     <!-- Left Panel -->
-    <f7-panel v-show="ready" left :cover="showSidebar ? true : null" class="sidebar" :visible-breakpoint="1024">
+    <f7-panel v-show="ready" left :cover="showSidebar ? true : null" class="sidebar" :visible-breakpoint="960">
       <f7-page>
         <f7-link href="/overview/" class="openhab-logo no-ripple" panel-close>
           <div class="logo-inner">
