d9a9feafec36e3ee9508a8aaafcf64785160e1a1
Florian Hotze <dev@florianhotze.com>
2026-05-04 16:59:02 +0200

    ´generate-rest-client´ script: Add support for downloading OpenAPI spec from URL
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- bundles/org.openhab.ui/web/build/generate-rest-client.mjs
+++ bundles/org.openhab.ui/web/build/generate-rest-client.mjs
@@ -2,34 +2,58 @@
 
 import fetch from 'node-fetch'
 import fs from 'node:fs'
+import { parseArgs } from 'node:util'
 import { createClient } from '@hey-api/openapi-ts'
+import { execSync } from 'node:child_process'
 
-// const url = `https://api.swaggerhub.com/apis/wborn/openhab-rest-api/8/swagger.json`
-// const url = `https://demo.openhab.org/rest/spec`
-const url = null
-const file = './build/openhab-api-spec.json'
+const options = {
+  url: { type: 'string' },
+  file: { type: 'string' }
+}
+
+const { values } = parseArgs({ options })
+
+const url = values.url ? new URL(values.url) : null
+const file = values.file || './build/openhab-api-spec.json'
 
 const outpath = './src/api'
 
 let openApi = null
 
 if (url) {
-  console.log('Fetching OpenAPI spec from URL:', url)
+  process.stdout.write(`Fetching OpenAPI spec from URL ${url.href} ... `)
+
   try {
     openApi = await fetch(url).then((res) => res.json())
   } catch (error) {
-    console.error('Error fetching OpenAPI spec:', error)
+    process.stderr.write('ERROR fetching/saving OpenAPI spec:', error)
+    process.exit(1)
+  }
+  process.stdout.write('DONE\n')
+
+  process.stdout.write(`Saving OpenAPI spec to file ${file} ... `)
+  fs.writeFileSync(file, JSON.stringify(openApi, null, 2))
+  process.stdout.write('DONE\n')
+
+  try {
+    process.stdout.write(`Applying oxfmt to ${file} ... `)
+    execSync(`oxfmt ${file}`, { stdio: 'ignore' })
+    process.stdout.write('DONE\n')
+  } catch (fmtError) {
+    process.stdout.write('WARN: oxfmt failed or is not installed. Continuing with client generation.\n')
   }
 }
 
-if (file) {
-  console.log('Reading OpenAPI spec from file:', file)
+if (file && !openApi) {
+  process.stdout.write(`Reading OpenAPI spec from file ${file} ... `)
   try {
     const data = fs.readFileSync(file, 'utf8')
     openApi = JSON.parse(data)
   } catch (error) {
-    console.error('Error reading OpenAPI spec from file:', error)
+    process.stderr.write('ERROR reading OpenAPI spec from file:', error)
+    process.exit(1)
   }
+  process.stdout.write('DONE\n')
 }
 
 try {
