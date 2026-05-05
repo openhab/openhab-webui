ba75114371eff3fc34e350f615e701e79c884c77
Florian Hotze <dev@florianhotze.com>
2026-05-05 15:15:09 +0200

    useChart: Use hash of query as "cache" key
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- bundles/org.openhab.ui/web/src/components/widgets/chart/useChart.ts
+++ bundles/org.openhab.ui/web/src/components/widgets/chart/useChart.ts
@@ -53,6 +53,7 @@ import { applyParameterDefaults } from '@/components/widgets/helpers.ts'
 import cloneDeep from 'lodash/cloneDeep'
 import type { EvaluateExpressionFn } from '@/components/widgets/useWidgetExpression.ts'
 import type { WidgetDefinition } from '@/assets/definitions/widgets/helpers.ts'
+import { simpleHash } from '@/js/openhab/utils.ts'
 
 dayjs.extend(IsoWeek)
 dayjs.extend(DayDuration)
@@ -316,15 +317,15 @@ export function useChart(
         itemState,
         displayState: 'displayState' in config ? config.displayState : undefined
       }
-      const key = `${neededItem}-${query.serviceId ?? 'default'}-${query.starttime}-${query.endtime}-${boundary.toString()}-${itemState.toString()}`
-      if (_persistencePromises[key] === undefined) {
-        _persistencePromises[key] = api.getItemDataFromPersistenceService(query).then((result) => {
-          delete _persistencePromises[key]
+      const hash = simpleHash(query)
+      if (_persistencePromises[hash] === undefined) {
+        _persistencePromises[hash] = api.getItemDataFromPersistenceService(query).then((result) => {
+          delete _persistencePromises[hash]
           return result!
         })
       }
 
-      return await Promise.all([_itemPromises[neededItem], _persistencePromises[key]])
+      return await Promise.all([_itemPromises[neededItem], _persistencePromises[hash]])
     })
 
     return Promise.all(combinedPromises).then(getter)
