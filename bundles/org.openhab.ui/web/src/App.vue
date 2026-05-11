3115e936d96bb708d3422678ffd2a61fa3552e06
Florian Hotze <dev@florianhotze.com>
2026-05-11 16:52:44 +0200

    Log viewer: Avoid log dock overflow when reducing screen height
    Instead of letting the log dock flow over on the top, reduce its height, so the controls are still reachable.
    
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- bundles/org.openhab.ui/web/src/App.vue
+++ bundles/org.openhab.ui/web/src/App.vue
@@ -950,6 +950,17 @@ export default {
     toggleLogDockFullscreen() {
       if (!useRuntimeStore().showLogDock) this.setLogDockVisible(true)
       this.logDockFullscreen = !this.logDockFullscreen
+      if (!this.logDockFullscreen) {
+        this.$nextTick(() => this.avoidLogDockOverflow())
+      }
+    },
+    avoidLogDockOverflow() {
+      const maxHeight = window.innerHeight
+      const currentHeight =
+        this.logDockHeight || parseInt(getComputedStyle(document.documentElement).getPropertyValue('--log-dock-height')) || 300
+      if (currentHeight > maxHeight) {
+        this.logDockHeight = Math.round(maxHeight)
+      }
     },
     startDockResize(ev) {
       const startY = ev.clientY
@@ -1229,6 +1240,7 @@ export default {
 
       if (window) {
         window.addEventListener('keydown', this.keyDown)
+        window.addEventListener('resize', this.avoidLogDockOverflow)
       }
 
       this.startEventSource()
@@ -1238,6 +1250,7 @@ export default {
   beforeUnmount() {
     if (window) {
       window.removeEventListener('keydown', this.keyDown)
+      window.removeEventListener('resize', this.avoidLogDockOverflow)
       if (this._logDockResizeHandlers) {
         const { onMove, onUp, onCancel } = this._logDockResizeHandlers
         window.removeEventListener('pointermove', onMove)
