openhab/openhab-webui

commit 7096bb66df1e2355cc2938f73f7e352327d5d502

Add readonly mode for file-based pages and widgets (#4115)
Refs https://github.com/openhab/openhab-core/pull/5493.

Note that this changes the YAML syntax in the Code tab:
- remove special keys for the slots. Instead, the slots are displayed in the YAML as is.
- always show the `component:` key.
This is so that the yaml code is compatible with the file-based syntax,
making it copy-pasteable to/from file.

---------

Also-by: Florian Hotze <dev@florianhotze.com>
Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

bundles/org.openhab.ui/web/src/components/config/controls/code-editor.vue
@@ -1,13 +1,4 @@
 <template>
-  <f7-icon
-    v-if="readOnly"
-    f7="lock"
-    class="float-right margin"
-    style="opacity: 0.5; z-index: 4000; user-select: none"
-    size="50"
-    color="gray"
-    :tooltip="readOnlyMsg" />
-
   <div class="code-editor">
     <editor
       ref="editor"
@@ -114,8 +105,7 @@ export default {
     objectType: String, // the type of the object, e.g. 'items', 'things' which corresponds to the yaml element name.
     objectId: String,
     hintContext: Object,
-    readOnly: Boolean,
-    readOnlyMsg: String
+    readOnly: Boolean
   },
   // @parsed  event is emitted when the code has been parsed back into an object
   //          as a result of calling the parseCode() method

bundles/org.openhab.ui/web/src/components/config/controls/script-editor.vue
@@ -1,5 +1,13 @@
 <template>
-  <codemirror ref="cm" class="code-editor-fit" :model-value="value" :extensions="extensions" @ready="onCmReady" @change="onCmCodeChange" />
+  <div class="code-editor-fit">
+    <f7-icon
+      v-if="readOnly"
+      f7="lock"
+      size="50"
+      class="code-readonly-icon"
+      :tooltip="readOnlyMsg || 'Read-only: provisioned from a file'" />
+    <codemirror ref="cm" :model-value="value" :extensions="extensions" @ready="onCmReady" @change="onCmCodeChange" />
+  </div>
 </template>
 
 <style lang="stylus">
@@ -13,6 +21,20 @@
   width 100%
   height 100%
   display flex !important
+
+  .code-readonly-icon
+    position absolute
+    top 0
+    right 0
+    margin var(--f7-typography-margin) !important
+    color var(--f7-color-gray)
+    opacity 0.5
+    z-index 4000
+    user-select none
+
+  .v-codemirror
+    flex 1
+    min-width 0
 
   .cm-editor
     height 100%
@@ -131,6 +153,7 @@ const props = defineProps<{
   mode: string
   hintContext?: HintContext
   readOnly?: boolean
+  readOnlyMsg?: string
 }>()
 
 const emit = defineEmits<{

bundles/org.openhab.ui/web/src/pages/settings/items/item-edit.vue
@@ -18,7 +18,7 @@
       <f7-tab id="design" :tab-active="currentTab === 'design'">
         <f7-block v-if="item.name || item.created === false" class="block-narrow">
           <f7-col v-if="!editable">
-            <div class="padding-left">Note: {{ notEditableMsg }}</div>
+            <not-editable-notice subject="Item" />
           </f7-col>
           <f7-col>
             <item-form ref="itemForm" :item="item" :items="items" :createMode="createMode" />
@@ -56,7 +56,6 @@
           :object="item"
           :object-id="item.name"
           :read-only="!editable"
-          :read-only-msg="notEditableMsg"
           @save="save()"
           @parsed="updateItem"
           @changed="onCodeChanged" />
@@ -88,6 +87,7 @@ import fastDeepEqual from 'fast-deep-equal/es6'
 import * as Types from '@/assets/item-types.js'
 
 import ItemForm from '@/components/item/item-form.vue'
+import NotEditableNotice from '@/components/util/not-editable-notice.vue'
 
 import ItemMixin from '@/components/item/item-mixin'
 import { showToast } from '@/js/dialog-promises'
@@ -104,6 +104,7 @@ export default {
   },
   components: {
     ItemForm,
+    NotEditableNotice,
     CodeEditor: defineAsyncComponent(() => import(/* webpackChunkName: "code-editor" */ '@/components/config/controls/code-editor.vue'))
   },
   setup() {
@@ -124,8 +125,7 @@ export default {
       semanticClass: '',
       semanticProperty: '',
       pendingTag: '',
-      currentTab: 'design',
-      notEditableMsg: 'This Item is not editable because it has been provisioned from a file.'
+      currentTab: 'design'
     }
   },
   computed: {

bundles/org.openhab.ui/web/src/pages/settings/things/thing-details.vue
@@ -70,9 +70,7 @@
             <f7-block-title v-if="thingType && thingType.UID" medium style="margin-bottom: var(--f7-list-margin-vertical)">
               Information
             </f7-block-title>
-            <f7-block-footer v-if="!editable" class="no-margin padding-left">
-              <f7-icon f7="lock_fill" size="12" color="gray" />&nbsp;Note: {{ notEditableMsg }}
-            </f7-block-footer>
+            <not-editable-notice v-if="!editable" subject="Thing" />
             <f7-list accordion-opposite>
               <f7-list-item v-if="thingType" accordion-item title="Thing Type" :after="thingType.label">
                 <f7-accordion-content class="thing-type-description">
@@ -315,7 +313,6 @@
           :object="thing"
           :object-id="thing.UID"
           :read-only="!editable"
-          :read-only-msg="notEditableMsg"
           :hint-context="{ thingType: thingType, channelTypes: channelTypes }"
           @save="save()"
           @parsed="updateThing"
@@ -427,6 +424,7 @@ import ConfigSheet from '@/components/config/config-sheet.vue'
 
 import ChannelList from '@/components/thing/channel-list.vue'
 import ThingGeneralSettings from '@/components/thing/thing-general-settings.vue'
+import NotEditableNotice from '@/components/util/not-editable-notice.vue'
 
 import NetworkPopup from '@/pages/settings/things/network/network-popup.vue'
 
@@ -451,6 +449,7 @@ export default {
     ConfigSheet,
     ChannelList,
     ThingGeneralSettings,
+    NotEditableNotice,
     CodeEditor: defineAsyncComponent(() => import(/* webpackChunkName: "code-editor" */ '@/components/config/controls/code-editor.vue'))
   },
   props: {
@@ -479,7 +478,6 @@ export default {
        */
       configActionsByGroup: [],
       eventSource: null,
-      notEditableMsg: 'This Thing is not editable because it has been provisioned from a file.',
       propertyTruncation: {}
     }
   },

bundles/org.openhab.ui/web/src/components/pagedesigner/chart/chart-designer.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <f7-block class="block-narrow margin-bottom" inset>
+    <f7-block v-if="context.editmode?.isEditable" class="block-narrow margin-bottom" inset>
       <f7-block-title>Coordinate Systems</f7-block-title>
       <f7-row class="margin-bottom">
         <f7-col class="elevation-2 elevation-hover-6 elevation-pressed-1 chartdesigner-big-button" width="50">
@@ -34,7 +34,7 @@
               :configureLabel="'Configure Y Axis'"
               :removeLabel="'Remove Axis'" />
           </span>
-          <f7-menu-item icon-f7="plus" dropdown>
+          <f7-menu-item v-if="context.editmode.isEditable" icon-f7="plus" dropdown>
             <f7-menu-dropdown left>
               <f7-menu-dropdown-item @click="addAxis(gridIdx, 'yAxis', 'oh-value-axis')" href="#" text="Add value axis" />
               <f7-menu-dropdown-item @click="addAxis(gridIdx, 'yAxis', 'oh-category-axis')" href="#" text="Add category axis" />
@@ -85,9 +85,11 @@
                   </div>
                 </template>
               </f7-list-item>
-              <f7-list-button color="blue" @click="addSeries('oh-time-series', gridIdx)"> Add Time Series </f7-list-button>
-              <f7-list-button color="blue" @click="addSeries('oh-aggregate-series', gridIdx)"> Add Aggregate Series </f7-list-button>
-              <f7-list-button color="blue" @click="addSeries('oh-state-series', gridIdx)"> Add State Series </f7-list-button>
+              <template v-if="context.editmode?.isEditable">
+                <f7-list-button color="blue" @click="addSeries('oh-time-series', gridIdx)"> Add Time Series </f7-list-button>
+                <f7-list-button color="blue" @click="addSeries('oh-aggregate-series', gridIdx)"> Add Aggregate Series </f7-list-button>
+                <f7-list-button color="blue" @click="addSeries('oh-state-series', gridIdx)"> Add State Series </f7-list-button>
+              </template>
             </f7-list>
           </f7-card>
         </div>
@@ -111,7 +113,11 @@
               :removeLabel="'Remove Axis'" />
           </span>
 
-          <f7-menu-item :style="{ marginLeft: context.component.slots.xAxis.length === 0 ? 'auto' : undefined }" icon-f7="plus" dropdown>
+          <f7-menu-item
+            v-if="context.editmode.isEditable"
+            :style="{ marginLeft: context.component.slots.xAxis.length === 0 ? 'auto' : undefined }"
+            icon-f7="plus"
+            dropdown>
             <f7-menu-dropdown right>
               <f7-menu-dropdown-item @click="addAxis(gridIdx, 'xAxis', 'oh-time-axis')" href="#" text="Add time axis" />
               <f7-menu-dropdown-item @click="addAxis(gridIdx, 'xAxis', 'oh-category-axis')" href="#" text="Add category axis" />
@@ -172,7 +178,10 @@
                   <img v-else src="./line.svg" width="32px" />
                 </template>
               </f7-list-item>
-              <f7-list-button color="blue" @click="addCalendarSeries('oh-calendar-series', calendarIdx)">
+              <f7-list-button
+                v-if="context.editmode?.isEditable"
+                color="blue"
+                @click="addCalendarSeries('oh-calendar-series', calendarIdx)">
                 Add Calendar Series
               </f7-list-button>
             </f7-list>
@@ -187,7 +196,7 @@
       <f7-block-title>Other Components</f7-block-title>
       <f7-row class="margin-bottom">
         <f7-col class="elevation-2 elevation-hover-6 elevation-pressed-1 chartdesigner-big-button" width="33">
-          <f7-link color="blue" class="display-flex flex-direction-column padding" @click="configureSlot('tooltip')">
+          <f7-link color="blue" class="display-flex flex-direction-column padding" @click="configureSlotByName('tooltip')">
             <f7-badge v-if="context.component.slots.tooltip" color="blue" class="count-badge">
               {{ context.component.slots.tooltip.length }}
             </f7-badge>
@@ -196,7 +205,7 @@
           </f7-link>
         </f7-col>
         <f7-col class="elevation-2 elevation-hover-6 elevation-pressed-1 chartdesigner-big-button" width="33">
-          <f7-link color="blue" class="display-flex flex-direction-column padding" @click="configureSlot('visualMap')">
+          <f7-link color="blue" class="display-flex flex-direction-column padding" @click="configureSlotByName('visualMap')">
             <f7-badge v-if="context.component.slots.visualMap" color="blue" class="count-badge">
               {{ context.component.slots.visualMap.length }}
             </f7-badge>
@@ -216,7 +225,7 @@
       </f7-row>
       <f7-row class="margin-bottom">
         <f7-col class="elevation-2 elevation-hover-6 elevation-pressed-1 chartdesigner-big-button" width="33">
-          <f7-link color="blue" class="display-flex flex-direction-column padding" @click="configureSlot('legend')">
+          <f7-link color="blue" class="display-flex flex-direction-column padding" @click="configureSlotByName('legend')">
             <f7-badge v-if="context.component.slots.legend" color="blue" class="count-badge">
               {{ context.component.slots.legend.length }}
             </f7-badge>
@@ -225,7 +234,7 @@
           </f7-link>
         </f7-col>
         <f7-col class="elevation-2 elevation-hover-6 elevation-pressed-1 chartdesigner-big-button" width="33">
-          <f7-link color="blue" class="display-flex flex-direction-column padding" @click="configureSlot('title')">
+          <f7-link color="blue" class="display-flex flex-direction-column padding" @click="configureSlotByName('title')">
             <f7-badge v-if="context.component.slots.title" color="blue" class="count-badge">
               {{ context.component.slots.title.length }}
             </f7-badge>
@@ -234,7 +243,7 @@
           </f7-link>
         </f7-col>
         <f7-col class="elevation-2 elevation-hover-6 elevation-pressed-1 chartdesigner-big-button" width="33">
-          <f7-link color="blue" class="display-flex flex-direction-column padding" @click="configureSlot('toolbox')">
+          <f7-link color="blue" class="display-flex flex-direction-column padding" @click="configureSlotByName('toolbox')">
             <f7-badge v-if="context.component.slots.toolbox" color="blue" class="count-badge">
               {{ context.component.slots.toolbox.length }}
             </f7-badge>
@@ -353,7 +362,9 @@ const defaultSlotComponentType = {
 
 export default {
   props: {
-    context: Object
+    context: Object,
+    configureWidget: Function,
+    configureSlot: Function
   },
   components: {
     'chart-skeleton': VChart,
@@ -584,10 +595,10 @@ export default {
         if (el && el.classList.contains('menu')) return
         el = el.parentElement
       }
-      this.context.editmode.configureWidget(series, context)
+      this.configureWidget(series, context)
     },
-    configureSlot(slotName) {
-      this.context.editmode.configureSlot(this.context.component, slotName, defaultSlotComponentType[slotName])
+    configureSlotByName(slotName) {
+      this.configureSlot(this.context.component, slotName, defaultSlotComponentType[slotName])
     }
   }
 }

bundles/org.openhab.ui/web/src/components/pagedesigner/edit-menu.vue
@@ -1,19 +1,27 @@
 <template>
   <f7-menu-item :text="text" :icon-f7="iconF7" dropdown>
     <f7-menu-dropdown :right="right">
-      <f7-menu-dropdown-item @click="context.editmode.configureWidget(component, context)" href="#" :text="configureLabel || 'Configure'" />
-      <f7-menu-dropdown-item @click="context.editmode.editWidgetCode(component, context)" href="#" text="Edit YAML" />
-      <f7-menu-dropdown-item divider />
-      <f7-menu-dropdown-item @click="context.editmode.cutWidget(component, context, parentSlot)" href="#" text="Cut" />
-      <f7-menu-dropdown-item @click="context.editmode.copyWidget(component, context, parentSlot)" href="#" text="Copy" />
-      <f7-menu-dropdown-item divider />
-      <f7-menu-dropdown-item @click="context.editmode.moveWidgetUp(component, context, parentSlot)" href="#" text="Move Up" />
-      <f7-menu-dropdown-item @click="context.editmode.moveWidgetDown(component, context, parentSlot)" href="#" text="Move Down" />
-      <f7-menu-dropdown-item divider />
       <f7-menu-dropdown-item
-        @click="context.editmode.removeWidget(component, context, parentSlot)"
+        @click="context.editmode.configureWidget(component, context)"
         href="#"
-        :text="removeLabel || 'Remove'" />
+        :text="(context.editmode.isEditable && configureLabel) || 'Settings'" />
+      <f7-menu-dropdown-item
+        @click="context.editmode.editWidgetCode(component, context)"
+        href="#"
+        :text="context.editmode.isEditable ? 'Edit YAML' : 'View YAML'" />
+      <template v-if="context.editmode.isEditable">
+        <f7-menu-dropdown-item divider />
+        <f7-menu-dropdown-item @click="context.editmode.cutWidget(component, context, parentSlot)" href="#" text="Cut" />
+        <f7-menu-dropdown-item @click="context.editmode.copyWidget(component, context, parentSlot)" href="#" text="Copy" />
+        <f7-menu-dropdown-item divider />
+        <f7-menu-dropdown-item @click="context.editmode.moveWidgetUp(component, context, parentSlot)" href="#" text="Move Up" />
+        <f7-menu-dropdown-item @click="context.editmode.moveWidgetDown(component, context, parentSlot)" href="#" text="Move Down" />
+        <f7-menu-dropdown-item divider />
+        <f7-menu-dropdown-item
+          @click="context.editmode.removeWidget(component, context, parentSlot)"
+          href="#"
+          :text="removeLabel || 'Remove'" />
+      </template>
     </f7-menu-dropdown>
   </f7-menu-item>
 </template>

bundles/org.openhab.ui/web/src/components/pagedesigner/page-settings.vue
@@ -8,14 +8,14 @@
         placeholder="A unique identifier for the page"
         :value="page.uid"
         @input="page.uid = $event.target.value"
-        :clear-button="createMode"
+        :clear-button="createMode && !readOnly"
         :info="createMode ? 'Required. Note: cannot be changed after the creation' : ''"
         input-id="input"
         required
         validate
         pattern="[A-Za-z0-9_]+"
         error-message="Required. A-Z,a-z,0-9,_ only"
-        :disabled="!createMode ? true : null">
+        :disabled="readOnly || !createMode">
         <template #inner>
           <f7-link
             v-if="createMode && $refs.pageId?.state?.inputInvalid && page.uid.trim()"
@@ -34,10 +34,16 @@
         @input="page.config.label = $event.target.value"
         required
         validate
-        clear-button />
+        :disabled="readOnly"
+        :clear-button="!readOnly" />
       <f7-list-item v-if="page.uid !== 'overview'" accordion-item title="Sidebar &amp; Visibility">
         <f7-accordion-content>
-          <f7-list-item ref="pageVisibility" title="Visible only to" smart-select :smart-select-params="{ openIn: 'popover' }">
+          <f7-list-item
+            ref="pageVisibility"
+            title="Visible only to"
+            smart-select
+            :smart-select-params="{ openIn: 'popover' }"
+            :disabled="readOnly">
             <select name="pagevisibility" multiple @change="updatePageVisibility">
               <optgroup label="Roles">
                 <option value="role:administrator" :selected="isVisibleTo('role:administrator') ? true : null">Administrators</option>
@@ -48,7 +54,10 @@
           <f7-list inline-labels no-hairline-md>
             <f7-list-item title="Show on sidebar">
               <template #after>
-                <f7-toggle :checked="page.config.sidebar ? true : null" @toggle:change="page.config.sidebar = $event" />
+                <f7-toggle
+                  :checked="page.config.sidebar ? true : null"
+                  @toggle:change="page.config.sidebar = $event"
+                  :disabled="readOnly" />
               </template>
             </f7-list-item>
             <f7-list-input
@@ -57,21 +66,24 @@
               placeholder="Assign order index to rearrange pages on sidebar"
               :value="page.config.order"
               @input="page.config.order = $event.target.value"
-              clear-button />
+              :disabled="readOnly ? true : null"
+              :clear-button="!readOnly" />
             <f7-list-input
               label="Icon"
               type="text"
               placeholder="Assign a custom icon"
               :value="page.config.icon"
               @input="page.config.icon = $event.target.value"
-              clear-button />
+              :disabled="readOnly ? true : null"
+              :clear-button="!readOnly" />
             <f7-list-input
               label="Browser Title"
               type="text"
               placeholder="A custom browser title instead of the label"
               :value="page.config.browserTitle"
               @input="page.config.browserTitle = $event.target.value"
-              clear-button />
+              :disabled="readOnly ? true : null"
+              :clear-button="!readOnly" />
           </f7-list>
         </f7-accordion-content>
       </f7-list-item>
@@ -79,12 +91,12 @@
     <template v-if="page.uid !== 'overview'">
       <f7-list inline-labels no-hairline-md>
         <div>
-          <tag-input :item="page" />
+          <tag-input :item="page" :disabled="readOnly" />
         </div>
       </f7-list>
       <f7-list v-if="!createMode" inline-labels no-hairline-md>
         <f7-list-button color="blue" @click="duplicatePage"> Duplicate Page </f7-list-button>
-        <f7-list-button color="red" @click="deletePage"> Remove Page </f7-list-button>
+        <f7-list-button v-if="!readOnly" color="red" @click="deletePage"> Remove Page </f7-list-button>
       </f7-list>
     </template>
   </f7-col>
@@ -104,6 +116,7 @@ export default {
   props: {
     page: Object,
     createMode: Boolean,
+    readOnly: Boolean,
     f7router: Object
   },
   data() {
@@ -126,6 +139,7 @@ export default {
       const pageClone = cloneDeep(this.page)
       const pageType = pageClone.component.replace(/^oh-|-page$/g, '')
       pageClone.uid = pageClone.uid + '_copy'
+      delete pageClone.editable
       this.f7router.navigate(`/settings/pages/${pageType}/duplicate`, { props: { pageCopy: pageClone } })
     },
     deletePage() {

bundles/org.openhab.ui/web/src/components/pagedesigner/sitemap/sitemap-code.vue
@@ -2,7 +2,7 @@
   <f7-block class="sitemap-code">
     <div class="row sitemap-parser resizable">
       <div class="col">
-        <editor :value="sitemapDsl" @input="updateSitemap" mode="application/vnd.openhab.sitemap+dsl" />
+        <editor :value="sitemapDsl" @input="updateSitemap" mode="application/vnd.openhab.sitemap+dsl" :readOnly="readOnly" />
       </div>
       <span class="resize-handler" />
     </div>
@@ -58,7 +58,8 @@ export default {
     editor: defineAsyncComponent(() => import(/* webpackChunkName: "script-editor" */ '@/components/config/controls/script-editor.vue'))
   },
   props: {
-    sitemap: Object
+    sitemap: Object,
+    readOnly: Boolean
   },
   emits: ['updated'],
   data() {

commit 209f3b12391742f067915530d03c47c1071eb001

Restore readOnlyMsg prop on code-editor (#4188)
Regression from #4115

Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

bundles/org.openhab.ui/web/src/components/config/controls/code-editor.vue
@@ -6,6 +6,7 @@
       :value="code"
       :hint-context="hintContext"
       :read-only="readOnly"
+      :read-only-msg="readOnlyMsg"
       @input="onEditorInput"
       @save="$emit('save')" />
   </div>
@@ -105,7 +106,8 @@ export default {
     objectType: String, // the type of the object, e.g. 'items', 'things' which corresponds to the yaml element name.
     objectId: String,
     hintContext: Object,
-    readOnly: Boolean
+    readOnly: Boolean,
+    readOnlyMsg: String
   },
   // @parsed  event is emitted when the code has been parsed back into an object
   //          as a result of calling the parseCode() method

bundles/org.openhab.ui/web/src/components/config/controls/script-editor.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="code-editor-fit">
-    <f7-icon
-      v-if="readOnly"
-      f7="lock"
-      size="50"
-      class="code-readonly-icon"
-      :tooltip="readOnlyMsg || 'Read-only: provisioned from a file'" />
+    <f7-icon v-if="readOnly" f7="lock" size="50" class="code-readonly-icon" :tooltip="readOnlyMsg || 'Not editable'" />
     <codemirror ref="cm" :model-value="value" :extensions="extensions" @ready="onCmReady" @change="onCmCodeChange" />
   </div>
 </template>

bundles/org.openhab.ui/web/src/pages/settings/items/item-edit.vue
@@ -56,6 +56,7 @@
           :object="item"
           :object-id="item.name"
           :read-only="!editable"
+          read-only-msg="This Item is not editable because it has been provisioned from a file."
           @save="save()"
           @parsed="updateItem"
           @changed="onCodeChanged" />

bundles/org.openhab.ui/web/src/pages/settings/things/thing-details.vue
@@ -313,6 +313,7 @@
           :object="thing"
           :object-id="thing.UID"
           :read-only="!editable"
+          read-only-msg="This Thing is not editable because it has been provisioned from a file."
           :hint-context="{ thingType: thingType, channelTypes: channelTypes }"
           @save="save()"
           @parsed="updateThing"