267f7f66fc7ed49c677276bb430555702bf4f65c
Jimmy Tanagra <jcode@tanagra.id.au>
2026-05-14 15:50:50 +1000

    fix initial load bug for highlighting submenu
    On the initial load, the submenu ref was null inside isMainMenuHighlighted causing the main menu being highlighted as well as the submenu item.
    
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- bundles/org.openhab.ui/web/src/App.vue
+++ bundles/org.openhab.ui/web/src/App.vue
@@ -63,7 +63,7 @@
           <li>
             <transition name="submenu">
               <sidebar-admin-submenu
-                v-if="isOpen('settings')"
+                v-show="isOpen('settings')"
                 ref="settingsSubmenu"
                 :key="`settings-${uiOptionsStore.showSidebarSubmenuEditor}`"
                 section="settings"
@@ -91,7 +91,7 @@
           </f7-list-item>
           <li>
             <transition name="submenu">
-              <ul v-if="isOpen('addons') && runtimeStore.apiEndpoint('addons')" class="menu-sublinks">
+              <ul v-show="isOpen('addons') && runtimeStore.apiEndpoint('addons')" class="menu-sublinks">
                 <f7-list-item
                   v-for="section in Object.keys(AddonTitles)"
                   :key="section"
@@ -129,7 +129,7 @@
           <li>
             <transition name="submenu">
               <sidebar-admin-submenu
-                v-if="isOpen('developer')"
+                v-show="isOpen('developer')"
                 ref="developerSubmenu"
                 :key="`developer-${uiOptionsStore.showSidebarSubmenuEditor}`"
                 section="developer"
