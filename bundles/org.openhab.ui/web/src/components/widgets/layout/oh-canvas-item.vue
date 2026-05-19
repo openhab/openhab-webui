openhab/openhab-webui

commit dd4114c161318dd00c8f63c9bc821e26d5963707

oh-canvas-item: Make content use 100% height and align vertical centered (#3859)
Related to
https://community.openhab.org/t/vertical-alignment-of-standard-library-widgets-in-fixed-canvas-layout/168211.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/layout/oh-canvas-item.vue
@@ -128,12 +128,39 @@
   pointer-events auto
 
   .oh-canvas-item-content
-    width 100%
-    height 100%
-    margin 0
+    width 100%                // use full width and
+    height 100%               // height of item
+    margin 0                  // without any margin
+    box-sizing border-box     // include padding
 
-  .oh-canvas-item-styled        // override background obscuring styles from system widgets
-    &.card                    // apply to card items
+    &.card
+      display flex            // use flexbox to distribute card elements
+      flex-direction column   // from top to bottom
+      .card-content           // with content centered
+        margin-top auto       // independent of presence
+        margin-bottom auto    // of header or footer
+
+        // oh-swiper-card
+        .swiper-container .placeholder-widget
+          margin 0
+          height 100%
+          width 100%
+        .oh-swiper-slide.edit-mode
+          min-height 0
+
+      // oh-image-card
+      .oh-image-card, .oh-gauge-card
+        flex 1
+        overflow hidden
+        *
+          height 100%
+
+        .oh-image
+          object-fit contain
+          height calc(100% - 10px)
+
+  .oh-canvas-item-styled  // override background obscuring styles from system widgets
+    &.card                // apply to card items
       box-shadow none
       background none
       .card-content, .card-footer

commit bdcd1038d68624180176b1e9c3dfdac96452f3fa

oh-canvas-item: Fix regression from #3859 affecting absolute positioning
Regression from #3859.
Fixes #4200.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/layout/oh-canvas-item.vue
@@ -132,17 +132,22 @@
   pointer-events auto
 
   .oh-canvas-item-content
-    width 100%                // use full width and
-    height 100%               // height of item
-    margin 0                  // without any margin
-    box-sizing border-box     // include padding
+    position relative
+    width 100%
+    height 100%
+    margin 0
+    box-sizing border-box // include padding
 
     &.card
-      display flex            // use flexbox to distribute card elements
-      flex-direction column   // from top to bottom
-      .card-content           // with content centered
-        margin-top auto       // independent of presence
-        margin-bottom auto    // of header or footer
+      // use flexbox to distribute card elements from top to bottom
+      display flex
+      flex-direction column
+      .card-content
+        // use flexbox to vertically center card contents
+        display flex
+        flex 1
+        flex-direction column
+        justify-content center
 
         // oh-swiper-card
         .swiper-container .placeholder-widget