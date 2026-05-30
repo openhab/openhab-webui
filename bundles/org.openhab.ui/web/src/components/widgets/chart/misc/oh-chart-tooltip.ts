openhab/openhab-webui

commit 73a4acfaf229c0734063362cb7be29dff82834f6

Charts: Add option for showing display instead of raw state (#4169)
UI part of https://github.com/openhab/openhab-core/issues/4697.

- Add a new parameter to chart series to request the display instead of
the raw state.
- Use the new `unit` field in the REST response instead of the fixed
`Item::unitSymbol` to display the unit in the tooltip.
- Refactor `$oh.utils` to TypeScript.
- Extract `simpleHash` from `oh-repeater` to `$oh.utils`.

---------

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/chart/misc/oh-chart-tooltip.ts
@@ -28,14 +28,14 @@ const chartTooltip: MiscChartComponent = {
         let tooltip = ''
         if (!Array.isArray(params)) {
           if (!params.seriesId) return ''
-          const [_seriesType, itemName, _id] = params.seriesId.split('#')
-          const item = context.items?.[itemName]
           // tooltip for aggregate & calendar series:
           // - header: series name
           // - content: category label and formatted value
           if (params.componentType === 'series') {
-            let state = context.numberFormatter!.format((params.data as number[])[1])
-            if (item && item.unitSymbol) state += ' ' + item.unitSymbol
+            if (!Array.isArray(params.data)) return ''
+            let state = context.numberFormatter!.format(params.data[1] as number)
+            const unit = params.data[params.data.length - 1] as string | undefined
+            if (unit) state += ' ' + unit
             tooltip += `<div>${params.seriesName}</div>`
             if (params.name) {
               // aggregate series
@@ -51,6 +51,8 @@ const chartTooltip: MiscChartComponent = {
           // - header: time range (start time to end time) in 'dd DD.MM.YYYY HH:mm - HH:mm'
           // - content: marker colour, series name and value (if available)
           if (params.componentType === 'markArea') {
+            const [_seriesType, itemName, _id] = params.seriesId.split('#')
+            const item = context.items?.[itemName]
             const state = item
               ? item.stateDescription?.options.find((o) => o.value === (params.value as string))?.label
               : (params.value as string)
@@ -67,17 +69,18 @@ const chartTooltip: MiscChartComponent = {
         // - header: x-axis time in 'dd DD.MM.YYYY HH:mm'
         // - content: for each oh-time-series marker color, series name and formatted value
         if (Array.isArray(params)) {
-          if (!params[0] || !('axisType' in params[0])) return ''
-          if (params[0].axisType === 'xAxis.time' && 'axisValue' in params[0]) {
-            tooltip += `<div>${dayjs(params[0].axisValue as string).format('llll')}</div>`
+          const param = params[0]
+          if (!param || !('axisType' in param)) return ''
+          if (param.axisType === 'xAxis.time' && 'axisValue' in param) {
+            tooltip += `<div>${dayjs(param.axisValue as string).format('llll')}</div>`
           }
           params.forEach((p) => {
             if (p.seriesId) {
-              const [seriesType, itemName, _id, markArea] = p.seriesId.split('#')
-              const item = context.items?.[itemName]
+              const [seriesType, _itemName, _id, markArea] = p.seriesId.split('#')
+              const unit = Array.isArray(p.value) ? (p.value[p.value.length - 1] as string) : undefined
               if ((seriesType === 'oh-time-series' && !markArea) || seriesType !== 'oh-time-series') {
                 let state = context.numberFormatter!.format((p.data as number[])[1])
-                if (item && item.unitSymbol) state += ' ' + item.unitSymbol
+                if (unit) state += ' ' + unit
                 tooltip += `${p.marker as string} ${p.seriesName}`
                 tooltip += `<span style="float: right; margin-left: 20px"><b style="text-align: right;">${state}</b></span><br/>`
               }

bundles/org.openhab.ui/web/src/components/widgets/chart/misc/oh-chart-visualmap.ts
@@ -27,12 +27,14 @@ const chartVisualMap: MiscChartComponent = {
     if (options.min !== undefined) {
       ;(options as VisualMapComponentOption).min = parseFloat(options.min)
     } else if (context.series && context.series[0]) {
-      ;(options as VisualMapComponentOption).min = Math.min(...(context.series[0].data as number[][]).map((p) => p[p.length - 1]))
+      // second-last element, last element is the unit
+      ;(options as VisualMapComponentOption).min = Math.min(...(context.series[0].data as number[][]).map((p) => p[p.length - 2]))
     }
     if (options.max !== undefined) {
       ;(options as VisualMapComponentOption).max = parseFloat(options.max)
     } else if (context.series && context.series[0]) {
-      ;(options as VisualMapComponentOption).max = Math.max(...(context.series[0].data as number[][]).map((p) => p[p.length - 1]))
+      // second-last element, last element is the unit
+      ;(options as VisualMapComponentOption).max = Math.max(...(context.series[0].data as number[][]).map((p) => p[p.length - 2]))
     }
 
     if (!options.formatter && context.numberFormatter) {

bundles/org.openhab.ui/web/src/components/widgets/chart/series/oh-aggregate-series.ts
@@ -115,7 +115,8 @@ const aggregateSeries: SeriesComponent = {
     const dimension2 = series.dimension2
     const boundary = includeBoundaryAndItemStateFor(component.config)
 
-    const itemPoints = points.find((p) => p.name === series.item)?.data ?? []
+    const itemSeries = points.find((p) => p.name === series.item)
+    const itemPoints = itemSeries?.data ?? []
 
     // we'll suppose dimension2 always more granular than dimension1
     // e.g. if dimension1=day, dimension2 can be hour but not month
@@ -158,13 +159,14 @@ const aggregateSeries: SeriesComponent = {
         return [
           dimensionFromDate(chartType, startTime, endTime, arr[0], axisX),
           dimensionFromDate(chartType, startTime, endTime, arr[0], axisY, true),
-          formatter.format(value)
+          formatter.format(value),
+          itemSeries?.unit
         ]
       } else {
         if (series.transpose) {
-          return [formatter.format(value), dimensionFromDate(chartType, startTime, endTime, arr[0], dimension1, true)]
+          return [formatter.format(value), dimensionFromDate(chartType, startTime, endTime, arr[0], dimension1, true), itemSeries?.unit]
         } else {
-          return [dimensionFromDate(chartType, startTime, endTime, arr[0], dimension1), formatter.format(value)]
+          return [dimensionFromDate(chartType, startTime, endTime, arr[0], dimension1), formatter.format(value), itemSeries?.unit]
         }
       }
     })
@@ -174,7 +176,8 @@ const aggregateSeries: SeriesComponent = {
     if (series.type === OhAggregateSeries.Type.scatter) {
       const scatterSeries = series as ScatterSeriesOption & { scatterSymbolSizeFactor?: number }
       scatterSeries.symbolSize = (v: number[]) => {
-        return v.pop()! * (scatterSeries.scatterSymbolSizeFactor ?? 1)
+        const state = v[v.length - 2] // second-last element, last element is the unit
+        return state * (scatterSeries.scatterSymbolSizeFactor ?? 1)
       }
     }
 

bundles/org.openhab.ui/web/src/components/widgets/chart/series/oh-calendar-series.ts
@@ -23,7 +23,8 @@ const calendarSeries: SeriesComponent = {
       component.config,
       OhCalendarSeriesDefinition
     )
-    const itemPoints = points.find((p) => p.name === series.item)?.data ?? []
+    const itemSeries = points.find((p) => p.name === series.item)
+    const itemPoints = itemSeries?.data ?? []
 
     type Group = [Dayjs, string[]]
     const groups: Group[] = itemPoints.reduce((acc: Group[], p) => {
@@ -42,7 +43,7 @@ const calendarSeries: SeriesComponent = {
     const data = groups.map((arr, idx, days) => {
       const aggregationFunction = series.aggregationFunction || OhCalendarSeries.AggregationFunction.average
       let value = aggregate(aggregationFunction, arr, idx, days)
-      return [arr[0].toDate(), parseFloat(formatter.format(value))]
+      return [arr[0].toDate(), parseFloat(formatter.format(value)), itemSeries?.unit]
     })
 
     if (!series.type) (series.type as unknown as string) = OhCalendarSeries.Type.heatmap
@@ -51,7 +52,8 @@ const calendarSeries: SeriesComponent = {
     if (series.type === OhCalendarSeries.Type.scatter) {
       const scatterSeries = series as ScatterSeriesOption & { scatterSymbolSizeFactor?: number }
       scatterSeries.symbolSize = (v: number[]) => {
-        return v.pop()! * (scatterSeries.scatterSymbolSizeFactor ?? 1)
+        const state = v[v.length - 2] // second-last element, last element is the unit
+        return state * (scatterSeries.scatterSymbolSizeFactor ?? 1)
       }
     }
 

bundles/org.openhab.ui/web/src/components/widgets/chart/series/oh-time-series.ts
@@ -29,11 +29,12 @@ const timeSeries: SeriesComponent = {
     series.data = []
 
     if (series.item) {
-      const itemPoints = points.find((p) => p.name === series.item)?.data ?? []
+      const itemSeries = points.find((p) => p.name === series.item)
+      const itemPoints = itemSeries?.data ?? []
 
       const formatter = new Intl.NumberFormat('en', { useGrouping: false, maximumFractionDigits: 3 })
       series.data = itemPoints.map((p) => {
-        return [new Date(p.time), formatter.format(Number(p.state))]
+        return [new Date(p.time), formatter.format(Number(p.state)), itemSeries?.unit]
       })
       series.id = `oh-time-series#${series.item}#${f7.utils.id()}`
     }

bundles/org.openhab.ui/web/src/assets/definitions/widgets/chart/index.ts
@@ -92,6 +92,12 @@ const itemStateParameter = pb(
   'Do not add the current Item state into the requested period (the item state will be before or at the end time)'
 ).a()
 
+const displayStateParameter = pb(
+  'displayState',
+  'Use Display State',
+  'If set to true, formatting from the state description is applied to the values. For QuantityType states, the unit from the state description is respected, but no formatting is applied.'
+)
+
 const offsetAmountParameter = pn(
   'offsetAmount',
   'Offset Amount',
@@ -143,6 +149,7 @@ const baseSeriesParameter = [
   persistenceServiceParameter,
   boundaryParameter,
   itemStateParameter,
+  displayStateParameter,
   offsetAmountParameter,
   offsetUnitParameter
 ]

bundles/org.openhab.ui/web/src/components/widgets/chart/useChart.ts
@@ -53,6 +53,7 @@ import { applyParameterDefaults } from '@/components/widgets/helpers.ts'
 import cloneDeep from 'lodash/cloneDeep'
 import type { EvaluateExpressionFn } from '@/components/widgets/useWidgetExpression.ts'
 import type { WidgetDefinition } from '@/assets/definitions/widgets/helpers.ts'
+import { simpleHash } from '@/js/openhab/utils.ts'
 
 dayjs.extend(IsoWeek)
 dayjs.extend(DayDuration)
@@ -313,17 +314,18 @@ export function useChart(
         starttime: seriesStartTime.toISOString(),
         endtime: seriesEndTime.subtract(1, 'millisecond').toISOString(),
         boundary,
-        itemState
+        itemState,
+        displayState: 'displayState' in config ? config.displayState : undefined
       }
-      const key = `${neededItem}-${query.serviceId ?? 'default'}-${query.starttime}-${query.endtime}-${boundary.toString()}-${itemState.toString()}`
-      if (_persistencePromises[key] === undefined) {
-        _persistencePromises[key] = api.getItemDataFromPersistenceService(query).then((result) => {
-          delete _persistencePromises[key]
+      const hash = simpleHash(query)
+      if (_persistencePromises[hash] === undefined) {
+        _persistencePromises[hash] = api.getItemDataFromPersistenceService(query).then((result) => {
+          delete _persistencePromises[hash]
           return result!
         })
       }
 
-      return await Promise.all([_itemPromises[neededItem], _persistencePromises[key]])
+      return await Promise.all([_itemPromises[neededItem], _persistencePromises[hash]])
     })
 
     return Promise.all(combinedPromises).then(getter)

bundles/org.openhab.ui/web/src/components/widgets/system/oh-repeater.vue
@@ -33,6 +33,7 @@ import { OhRepeater } from '@/types/components/widgets'
 
 import * as api from '@/api'
 import { ApiError } from '@/js/hey-api'
+import { simpleHash } from '@/js/openhab/utils'
 
 type SourceTypeElement = api.EnrichedItem | api.EnrichedGroupItem | api.EnrichedRule | api.StateOption | api.CommandOption | string | number
 type SourceArray = SourceTypeElement[] | null
@@ -174,18 +175,6 @@ const source = computedAsync(async (): Promise<SourceArray> => {
 })
 
 // Methods
-function simpleHash(obj: object | string): string {
-  const str = JSON.stringify(obj)
-  let hash = 0
-  if (str.length === 0) return hash.toString()
-  for (let i = 0; i < str.length; i++) {
-    const chr = str.charCodeAt(i)
-    hash = (hash << 5) - hash + chr
-    hash |= 0 // Convert to 32bit integer
-  }
-  return (hash >>> 0).toString(36)
-}
-
 function getKey(el: SourceTypeElement): string {
   let key = 'repeater-'
   switch (sourceType.value) {

commit 5ec0ce46b773e21c77ff467acc5d7ce88bce821c

Charts: Fix visual map regression
Fix visual map doesn't render colors.
Regression from #4169.

This fix reverts the passing of the unit with each datapoint from #4169.
Instead, the unit is passed through the series id, which is more robust and should require less memory as well.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/chart/misc/oh-chart-tooltip.ts
@@ -28,13 +28,13 @@ const chartTooltip: MiscChartComponent = {
         let tooltip = ''
         if (!Array.isArray(params)) {
           if (!params.seriesId) return ''
+          const [_seriesType, _itemName, unit, _id] = params.seriesId.split('#')
           // tooltip for aggregate & calendar series:
           // - header: series name
           // - content: category label and formatted value
           if (params.componentType === 'series') {
             if (!Array.isArray(params.data)) return ''
             let state = context.numberFormatter!.format(params.data[1] as number)
-            const unit = params.data[params.data.length - 1] as string | undefined
             if (unit) state += ' ' + unit
             tooltip += `<div>${params.seriesName}</div>`
             if (params.name) {
@@ -76,8 +76,7 @@ const chartTooltip: MiscChartComponent = {
           }
           params.forEach((p) => {
             if (p.seriesId) {
-              const [seriesType, _itemName, _id, markArea] = p.seriesId.split('#')
-              const unit = Array.isArray(p.value) ? (p.value[p.value.length - 1] as string) : undefined
+              const [seriesType, _itemName, unit, _id, markArea] = p.seriesId.split('#')
               if ((seriesType === 'oh-time-series' && !markArea) || seriesType !== 'oh-time-series') {
                 let state = context.numberFormatter!.format((p.data as number[])[1])
                 if (unit) state += ' ' + unit

bundles/org.openhab.ui/web/src/components/widgets/chart/misc/oh-chart-visualmap.ts
@@ -27,14 +27,12 @@ const chartVisualMap: MiscChartComponent = {
     if (options.min !== undefined) {
       ;(options as VisualMapComponentOption).min = parseFloat(options.min)
     } else if (context.series && context.series[0]) {
-      // second-last element, last element is the unit
-      ;(options as VisualMapComponentOption).min = Math.min(...(context.series[0].data as number[][]).map((p) => p[p.length - 2]))
+      ;(options as VisualMapComponentOption).min = Math.min(...(context.series[0].data as number[][]).map((p) => p[p.length - 1]))
     }
     if (options.max !== undefined) {
       ;(options as VisualMapComponentOption).max = parseFloat(options.max)
     } else if (context.series && context.series[0]) {
-      // second-last element, last element is the unit
-      ;(options as VisualMapComponentOption).max = Math.max(...(context.series[0].data as number[][]).map((p) => p[p.length - 2]))
+      ;(options as VisualMapComponentOption).max = Math.max(...(context.series[0].data as number[][]).map((p) => p[p.length - 1]))
     }
 
     if (!options.formatter && context.numberFormatter) {

bundles/org.openhab.ui/web/src/components/widgets/chart/series/oh-aggregate-series.ts
@@ -159,14 +159,13 @@ const aggregateSeries: SeriesComponent = {
         return [
           dimensionFromDate(chartType, startTime, endTime, arr[0], axisX),
           dimensionFromDate(chartType, startTime, endTime, arr[0], axisY, true),
-          formatter.format(value),
-          itemSeries?.unit
+          formatter.format(value)
         ]
       } else {
         if (series.transpose) {
-          return [formatter.format(value), dimensionFromDate(chartType, startTime, endTime, arr[0], dimension1, true), itemSeries?.unit]
+          return [formatter.format(value), dimensionFromDate(chartType, startTime, endTime, arr[0], dimension1, true)]
         } else {
-          return [dimensionFromDate(chartType, startTime, endTime, arr[0], dimension1), formatter.format(value), itemSeries?.unit]
+          return [dimensionFromDate(chartType, startTime, endTime, arr[0], dimension1), formatter.format(value)]
         }
       }
     })
@@ -176,13 +175,12 @@ const aggregateSeries: SeriesComponent = {
     if (series.type === OhAggregateSeries.Type.scatter) {
       const scatterSeries = series as ScatterSeriesOption & { scatterSymbolSizeFactor?: number }
       scatterSeries.symbolSize = (v: number[]) => {
-        const state = v[v.length - 2] // second-last element, last element is the unit
-        return state * (scatterSeries.scatterSymbolSizeFactor ?? 1)
+        return v.pop()! * (scatterSeries.scatterSymbolSizeFactor ?? 1)
       }
     }
 
     if (series.item) {
-      series.id = `oh-aggregate-series#${series.item}#${f7.utils.id()}`
+      series.id = `oh-aggregate-series#${series.item}#${itemSeries?.unit ?? ''}#${f7.utils.id()}`
     }
 
     series.data = data

bundles/org.openhab.ui/web/src/components/widgets/chart/series/oh-calendar-series.ts
@@ -43,7 +43,7 @@ const calendarSeries: SeriesComponent = {
     const data = groups.map((arr, idx, days) => {
       const aggregationFunction = series.aggregationFunction || OhCalendarSeries.AggregationFunction.average
       let value = aggregate(aggregationFunction, arr, idx, days)
-      return [arr[0].toDate(), parseFloat(formatter.format(value)), itemSeries?.unit]
+      return [arr[0].toDate(), parseFloat(formatter.format(value))]
     })
 
     if (!series.type) (series.type as unknown as string) = OhCalendarSeries.Type.heatmap
@@ -52,13 +52,12 @@ const calendarSeries: SeriesComponent = {
     if (series.type === OhCalendarSeries.Type.scatter) {
       const scatterSeries = series as ScatterSeriesOption & { scatterSymbolSizeFactor?: number }
       scatterSeries.symbolSize = (v: number[]) => {
-        const state = v[v.length - 2] // second-last element, last element is the unit
-        return state * (scatterSeries.scatterSymbolSizeFactor ?? 1)
+        return v.pop()! * (scatterSeries.scatterSymbolSizeFactor ?? 1)
       }
     }
 
     if (series.item) {
-      series.id = `oh-calendar-series#${series.item}#${f7.utils.id()}`
+      series.id = `oh-calendar-series#${series.item}#${itemSeries?.unit ?? ''}#${f7.utils.id()}`
     }
 
     series.data = data

bundles/org.openhab.ui/web/src/components/widgets/chart/series/oh-time-series.ts
@@ -34,9 +34,9 @@ const timeSeries: SeriesComponent = {
 
       const formatter = new Intl.NumberFormat('en', { useGrouping: false, maximumFractionDigits: 3 })
       series.data = itemPoints.map((p) => {
-        return [new Date(p.time), formatter.format(Number(p.state)), itemSeries?.unit]
+        return [new Date(p.time), formatter.format(Number(p.state))]
       })
-      series.id = `oh-time-series#${series.item}#${f7.utils.id()}`
+      series.id = `oh-time-series#${series.item}#${itemSeries?.unit ?? ''}#${f7.utils.id()}`
     }
 
     // other things