openhab/openhab-webui

commit 3a511299203bdedbd8f191a3d611d8c2174aa244

Widget edit: Don't cache expressions in edit mode & throttle rerender on input (#4134)
Fixes #4092

Signed-off-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/pages/developer/widgets/widget-edit.vue
@@ -121,6 +121,7 @@ import { useStatesStore } from '@/js/stores/useStatesStore'
 import { useViewArea } from '@/js/composables/useViewArea'
 import { transformParameterDefaults } from '@/components/widgets/helpers.ts'
 import { showToast } from '@/js/dialog-promises'
+import { useThrottleFn } from '@vueuse/core'
 
 const toStringOptions = { toStringDefaults: { lineWidth: 0 } }
 
@@ -178,7 +179,8 @@ export default {
         store: useStatesStore().trackedItems,
         props: this.props,
         vars: this.vars,
-        ctxVars: this.ctxVars
+        ctxVars: this.ctxVars,
+        editmode: true
       }
     },
     widget() {
@@ -204,12 +206,12 @@ export default {
       }
       useStatesStore().stopTrackingStates()
     },
-    onEditorInput(value) {
+    onEditorInput: useThrottleFn(function (value) {
       this.widgetDefinition = value
       if (!this.loading) {
         this.dirty = true
       }
-    },
+    }, 300),
     keyDown(ev) {
       if ((ev.ctrlKey || ev.metaKey) && !(ev.altKey || ev.shiftKey)) {
         switch (ev.keyCode) {

commit 9a1ac5fc463c75cd5251a1c3ffc28505023b89ab

Widget edit: Fix widget actions not available anymore
Regression from #4134.
While expressions shouldn't be cached in the widget editor,
editmode was never activated for the widget preview.
Revert the activation of editmode, instead add a new flag to the WidgetContext to disable the AST-cache.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/types.d.ts
@@ -65,6 +65,11 @@ export interface WidgetContext {
    * Whether the widget is displayed in a tab.
    */
   tab?: boolean
+  /**
+   * Disables the widget expression AST-cache.
+   * Useful when expressions change, but `editmode` is not intended.
+   */
+  noExpressionCache?: boolean
 
   parent?: WidgetContext
 }

bundles/org.openhab.ui/web/src/components/widgets/useWidgetContext.ts
@@ -134,6 +134,7 @@ export function useWidgetContext(context: Ref<WidgetContext>) {
       config: context.value.config,
       editmode: context.value.editmode,
       clipboardtype: context.value.clipboardtype,
+      noExpressionCache: context.value.noExpressionCache,
       parent: context.value.parent
     } satisfies WidgetContext
   })
@@ -157,6 +158,7 @@ export function useWidgetContext(context: Ref<WidgetContext>) {
       config: context.value.config,
       editmode: context.value.editmode,
       clipboardtype: context.value.clipboardtype,
+      noExpressionCache: context.value.noExpressionCache,
       parent: context.value
     }
   }

bundles/org.openhab.ui/web/src/components/widgets/useWidgetExpression.ts
@@ -163,7 +163,7 @@ export function useWidgetExpression(properties: { context?: WidgetContext; props
       try {
         // we cache the parsed abstract tree to prevent it from being parsed again at runtime
         // if we are in edit-mode according to the context, do not cache because the expression is subject to change
-        if (!exprAst[key] || ctx.editmode) {
+        if (!exprAst[key] || ctx.editmode || ctx.noExpressionCache) {
           exprAst[key] = parse(value.substring(1))
         }
         return evaluate(exprAst[key], {

bundles/org.openhab.ui/web/src/pages/developer/widgets/widget-edit.vue
@@ -169,23 +169,6 @@ export default {
     }
   },
   computed: {
-    editmode() {
-      return {
-        isEditable: this.isEditable,
-        addWidget: this.noopEditmodeAction,
-        configureWidget: this.noopEditmodeAction,
-        configureSlot: this.noopEditmodeAction,
-        editWidgetCode: this.noopEditmodeAction,
-        cutWidget: this.noopEditmodeAction,
-        copyWidget: this.noopEditmodeAction,
-        pasteWidget: this.noopEditmodeAction,
-        moveWidgetUp: this.noopEditmodeAction,
-        moveWidgetDown: this.noopEditmodeAction,
-        sendWidgetToBack: this.noopEditmodeAction,
-        bringWidgetToFront: this.noopEditmodeAction,
-        removeWidget: this.noopEditmodeAction
-      }
-    },
     context() {
       return {
         component:
@@ -207,7 +190,7 @@ export default {
         props: this.props,
         vars: this.vars,
         ctxVars: this.ctxVars,
-        editmode: this.editmode
+        noExpressionCache: true
       }
     },
     isEditable() {
@@ -224,7 +207,6 @@ export default {
     }
   },
   methods: {
-    noopEditmodeAction() {},
     onPageAfterIn() {
       if (window) {
         window.addEventListener('keydown', this.keyDown)