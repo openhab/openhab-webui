openhab/openhab-webui

commit ece3b920c6a49bb9a2525210965bc5062832e66f

Sitemap editor: Various improvements & replace UI DSL parsing/generation with REST calls (#4117)
Refs https://github.com/openhab/openhab-webui/pull/4103
and https://github.com/openhab/openhab-core/issues/5007.

This:

- shows unmanged sitemaps in the sitemap editor (all fields
  read only), including conversion between (DSL, YAML).
- replaced all DSL parsing and generation in the UI code by
  calls to the sitemap REST API.
- remove all use of nearley from the code base (sitemaps
  were the last element using browser code DSL parsing).
- refactors and improves the sitemap editor UI, e.g. better
  editors for rules and mappings, separating the format string from the
  label and editor with validation for the format string, item picker
  where an item is expected, the ability to duplicate a complete
  sitemap...
- transformation service and transformation pickers to help setting the
  label format.

Also fixes a bug in code editor where switching language was not preserving edits.

---------

Also-by: Florian Hotze <dev@florianhotze.com>
Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

bundles/org.openhab.ui/web/src/components/config/controls/item-picker.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="item-picker-container">
     <f7-list-item
-      :title="label || 'Item'"
+      :title="showTitle ? label || 'Item' : undefined"
       :after="displayValue"
       link
       :disabled="disabled ? true : null"
       :textColor="textColor"
-      class="item-picker"
-      :no-chevron="false"
-      @click="openPopup">
+      :class="{ 'item-picker': true, 'item-picker-empty': showTitle && isEmptySelection }"
+      :no-chevron="disabled"
+      @click="!disabled && openPopup()"
+      @keydown.enter.prevent="!disabled && openPopup()"
+      @keydown.space.prevent="!disabled && openPopup()">
       <template #media>
         <f7-button
           v-if="!noModelPicker"
+          tabindex="-1"
           :icon-color="color"
           :icon-aurora="aurora"
           :icon-ios="ios"
@@ -22,7 +25,7 @@
       </template>
     </f7-list-item>
 
-    <f7-popup v-model:opened="popupOpen" @popup:opened="onPopupOpened">
+    <f7-popup class="item-picker-popup" v-model:opened="popupOpen" @popup:opened="onPopupOpened">
       <f7-page>
         <f7-navbar :title="label || $t('dialogs.itemPicker.popup.title')">
           <f7-nav-right>
@@ -78,6 +81,13 @@
     padding 0
   .item-inner:after
     display none
+  .item-picker-empty
+    .item-title
+      color var(--f7-input-placeholder-color, var(--f7-list-item-footer-text-color))
+  .item-picker:focus-within .item-content
+    outline 2px solid var(--f7-theme-color)
+    outline-offset -2px
+    border-radius var(--f7-list-inset-border-radius)
 </style>
 
 <script>
@@ -113,7 +123,11 @@ export default {
     iosIcon: String,
     mdIcon: String,
     textColor: String,
-    hideIcon: Boolean
+    hideIcon: Boolean,
+    showTitle: {
+      type: Boolean,
+      default: true
+    }
   },
   emits: ['input', 'item-selected'],
   data() {
@@ -130,6 +144,12 @@ export default {
     }
   },
   computed: {
+    isEmptySelection() {
+      if (this.multiple) {
+        return !Array.isArray(this.value) || this.value.length === 0
+      }
+      return this.value === null || this.value === undefined || this.value === ''
+    },
     displayValue() {
       if (!this.setValueText) return ''
       if (this.multiple) {
@@ -238,9 +258,9 @@ export default {
         this.$emit('input', this.selectedValue)
       } else {
         this.selectedValue = item ? item.name : null
+        this.closePopup()
         this.$emit('input', this.selectedValue)
         this.$emit('item-selected', item)
-        this.closePopup()
       }
     },
     updateFromModelPicker(value) {

bundles/org.openhab.ui/web/build/compile-grammar.mjs
@@ -1,17 +1,0 @@
-import fs from 'node:fs'
-import path from 'node:path'
-import { execSync } from 'node:child_process'
-import { fileURLToPath } from 'node:url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-const dir = path.join(__dirname, '../src/assets')
-const files = fs.readdirSync(dir).filter((f) => f.endsWith('.nearley'))
-
-files.forEach((file) => {
-  const input = path.join(dir, file)
-  const output = input + '.js'
-  console.log(`Compiling ${file} -> ${path.basename(output)}`)
-  execSync(`npx nearleyc "${input}" -o "${output}"`, { stdio: 'inherit' })
-})

bundles/org.openhab.ui/web/eslint.config.js
@@ -218,6 +218,6 @@ export default defineConfig([
       'no-undef': 'off'
     }
   },
-  globalIgnores(['dist', 'build', 'public', '**/*.nearley.js', 'src/api/**']),
+  globalIgnores(['dist', 'build', 'public', 'src/api/**']),
   eslintConfigPrettier // Disables all ESLint rules that conflict with Prettier/oxfmt
 ])

bundles/org.openhab.ui/web/jest.config.js
@@ -52,8 +52,7 @@ module.exports = {
   },
   transform: {
     '.*\\.(vue)$': '@vue/vue2-jest',
-    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-    '^.+\\.nearley$': 'jest-transform-nearley'
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub'
   },
   transformIgnorePatterns: [
     '/node_modules/(?!framework7|framework7-vue|template7|dom7/)'

bundles/org.openhab.ui/web/src/assets/definitions/media-types.ts
@@ -9,6 +9,8 @@ export enum MediaType {
   ITEM_YAML = 'application/yaml+item',
   THING_DSL = 'text/vnd.openhab.dsl.thing',
   ITEM_DSL = 'text/vnd.openhab.dsl.item',
+  SITEMAP_YAML = 'application/yaml+sitemap',
+  SITEMAP_DSL = 'text/vnd.openhab.dsl.sitemap',
   RULE_YAML = 'application/vnd.openhab.rule+yaml',
   RULE_DSL = 'application/vnd.openhab.dsl.rule',
   UI_COMPONENT = 'application/vnd.openhab.uicomponent'
@@ -32,5 +34,9 @@ export const SupportedMediaTypes = {
   things: {
     YAML: MediaType.THING_YAML,
     DSL: MediaType.THING_DSL
+  },
+  sitemaps: {
+    YAML: MediaType.SITEMAP_YAML,
+    DSL: MediaType.SITEMAP_DSL
   }
 }

bundles/org.openhab.ui/web/src/assets/sitemap-lexer.nearley
@@ -1,228 +1,0 @@
-@preprocessor esmodule
-
-@{%
-  import moo from 'moo'
-
-  let lexer = moo.compile({
-    WS:               /[ \t]+/,
-    sitemap:          'sitemap ',
-    name:             'name=',
-    label:            'label=',
-    item:             'item=',
-    staticIcon:       'staticIcon=',
-    icon:             'icon=',
-    widgetattr:       ['url=', 'refresh=', 'service=', 'height=', 'minValue=', 'maxValue=', 'step=', 'encoding=', 'yAxisDecimalPattern=', 'inputHint=', 'columns=', 'row=', 'column=', 'interpolation='],
-    widgetboolattr:   ['legend='],
-    widgetfrcitmattr: 'forceasitem=',
-    widgetmapattr:    'mappings=',
-    widgetbuttonsattr:'buttons=',
-    widgetvisiattr:   'visibility=',
-    widgetcolorattr:  ['labelcolor=', 'valuecolor=', 'iconcolor='],
-    widgetswitchattr: 'switchSupport',
-    widgetronlyattr:  'releaseOnly',
-    widgetstatelattr: 'stateless',
-    widgetclickattr:  'click=',
-    widgetreleaseattr:'release=',
-    widgetperiodattr: 'period=',
-    nlwidget:         ['Switch ', 'Selection ', 'Slider ', 'Setpoint ', 'Input ', 'Video ', 'Chart ', 'Webview ', 'Colorpicker ', 'Colortemperaturepicker', 'Mapview ', 'Button ', 'Default '],
-    lwidget:          ['Text ', 'Group ', 'Image ', 'Frame ', 'Buttongrid '],
-    lparen:           '(',
-    rparen:           ')',
-    lbrace:           '{',
-    rbrace:           '}',
-    lbracket:         '[',
-    rbracket:         ']',
-    eq:               '==',
-    noteq:            '!=',
-    lteq:             '<=',
-    gteq:             '>=',
-    lt:               '<',
-    gt:               '>',
-    and:              'AND',
-    equals:           '=',
-    NL:               { match: /\n/, lineBreaks: true },
-    SL_COMMENT:       /\/\/.*$/,
-    ML_COMMENT:       /\/\*[\s\S]*?\*\//,
-    boolean:          /(?:true)|(?:false)/,
-    identifier:       /(?:[A-Za-z_][A-Za-z0-9_]*)|(?:[0-9]+[A-Za-z_][A-Za-z0-9_]*)/,
-    comma:            ',',
-    colon:            ':',
-    hyphen:           '-',
-    plus:             '+',
-    dot:              '.',
-    unsignedint:      /[0-9]+/,
-    string:           { match: /"(?:\\["\\]|[^\n"\\])*"/, value: x => x.slice(1, -1) }
-  })
-  const requiresItem = ['Group', 'Chart', 'Switch', 'Mapview', 'Slider', 'Selection', 'Setpoint', 'Input ', 'Colorpicker', 'Colortemperaturepicker', 'Button', 'Default']
-
-  function getSitemap(d) {
-    return {
-      "uid": d[2][0].text,
-      "component": "Sitemap",
-      "config": d[4],
-      "slots": {
-        "widgets": d[8]
-      }
-    }
-  }
-
-  function getWidget(d, l, reject) {
-    let widget = {
-      component: d[0].text.trim(),
-      config: {}
-    }
-    if (d[2] && d[2][0]) {
-      for (let a of d[2][0]) {
-        widget.config[a[0].replace('=', '')] = a[1]
-      }
-    }
-    if (d[6]) {
-      widget.slots = {
-        widgets: d[6]
-      }
-    }
-
-    // if icon exists remove staticIcon, if not set icon to staticIcon and make staticIcon=true
-    if (widget.config.icon) {
-     delete widget.config.staticIcon
-    }
-    if (widget.config.staticIcon) {
-      widget.config.icon = widget.config.staticIcon
-      widget.config.staticIcon = true
-    }
-    return widget
-  }
-
-  // helper function to join values if array, pass through if string
-  function joinValue(val) {
-    return Array.isArray(val) ? val.join("") : val;
-  }
-%}
-
-@lexer lexer
-
-Main -> _ Sitemap _                                                               {% (d) => d[1] %}
-Sitemap -> %sitemap _ SitemapName __ SitemapLabel __ %lbrace _ Widgets _ %rbrace  {% getSitemap %}
-
-SitemapName -> %identifier
-SitemapLabel -> null                                                              {% (d) => { return {} } %}
-  | %label _ %string                                                              {% (d) => { return { 'label': d[2].value } } %}
-
-Widgets -> Widget                                                                 {% (d) => [d[0]] %}
-  | Widgets _ Widget                                                              {% (d) => d[0].concat([d[2]]) %}
-
-Widget -> %nlwidget _ WidgetAttrs:*                                               {% getWidget %}
-  | %lwidget _ WidgetAttrs:*                                                      {% getWidget %}
-  | %lwidget _ WidgetAttrs:* _ %lbrace _ Widgets _ %rbrace                        {% getWidget %}
-
-WidgetAttrs -> WidgetAttr                                                         {% (d) => [d[0]] %}
-  | WidgetAttrs _ WidgetAttr                                                      {% (d) => d[0].concat([d[2]]) %}
-WidgetAttr -> %widgetswitchattr                                                   {% (d) => ['switchEnabled', true] %}
-  | %widgetronlyattr                                                              {% (d) => ['releaseOnly', true] %}
-  | %widgetstatelattr                                                             {% (d) => ['stateless', true] %}
-  | %widgetfrcitmattr _ WidgetBooleanAttrValue                                    {% (d) => ['forceAsItem', d[2]] %}
-  | %widgetboolattr _ WidgetBooleanAttrValue                                      {% (d) => [d[0].value, d[2]] %}
-  | %widgetperiodattr _ WidgetPeriodAttrValue                                     {% (d) => ['period', d[2]] %}
-  | %widgetclickattr _ WidgetAttrValue                                            {% (d) => ['cmd', d[2]] %}
-  | %widgetreleaseattr _ WidgetAttrValue                                          {% (d) => ['releaseCmd', d[2]] %}
-  | %icon _ WidgetIconRulesAttrValue                                              {% (d) => ['iconrules', d[2]] %}
-  | %icon _ WidgetIconAttrValue                                                   {% (d) => [d[0].value, joinValue(d[2])] %}
-  | %staticIcon _ WidgetIconAttrValue                                             {% (d) => [d[0].value, joinValue(d[2])] %}
-  | WidgetAttrName _ WidgetAttrValue                                              {% (d) => [d[0][0].value, d[2]] %}
-  | WidgetMappingsAttrName WidgetMappingsAttrValue                                {% (d) => [d[0][0].value, d[1]] %}
-  | WidgetButtonsAttrName WidgetButtonsAttrValue                                  {% (d) => [d[0][0].value, d[1]] %}
-  | WidgetVisibilityAttrName WidgetVisibilityAttrValue                            {% (d) => [d[0][0].value, d[1]] %}
-  | WidgetColorAttrName WidgetColorAttrValue                                      {% (d) => [d[0][0].value, d[1]] %}
-WidgetAttrName -> %item | %label | %widgetattr
-WidgetBooleanAttrValue -> %boolean                                                {% (d) => (d[0].value === 'true') %}
-  | %string                                                                       {% (d) => (d[0].value === 'true') %}
-WidgetIconAttrValue -> %string                                                    {% (d) => d[0].value %}
-  | WidgetIconAttrPart
-  | WidgetIconAttrPart %colon WidgetIconAttrPart
-  | WidgetIconAttrPart %colon WidgetIconAttrPart %colon WidgetIconAttrPart
-WidgetIconAttrPart -> %identifier                                                 {% (d) => d[0].value %}
-  | %identifier %hyphen WidgetIconAttrPart                                        {% (d) => d[0].value + "-" + d[2] %}
-WidgetIconRulesAttrValue -> %lbracket _ IconRules _ %rbracket                     {% (d) => d[2] %}
-WidgetPeriodAttrValue -> %identifier %hyphen %identifier                          {% (d) => d[0].value + "-" + d[2].value %}
-  | %hyphen %identifier                                                           {% (d) => "-" + d[1].value %}
-  | %identifier                                                                   {% (d) => d[0].value %}
-  | %string                                                                       {% (d) => d[0].value %}
-WidgetAttrValue -> Number                                                         {% (d) => d[0] %}
-  | %identifier                                                                   {% (d) => d[0].value %}
-  | %string                                                                       {% (d) => d[0].value %}
-WidgetMappingsAttrName -> %widgetmapattr
-WidgetMappingsAttrValue -> %lbracket _ Mappings _ %rbracket                       {% (d) => d[2] %}
-WidgetButtonsAttrName -> %widgetbuttonsattr
-WidgetButtonsAttrValue -> %lbracket _ Buttons _ %rbracket                         {% (d) => d[2] %}
-WidgetVisibilityAttrName -> %widgetvisiattr
-WidgetVisibilityAttrValue -> %lbracket _ Visibilities _ %rbracket                 {% (d) => d[2] %}
-WidgetColorAttrName -> %widgetcolorattr
-WidgetColorAttrValue -> %lbracket _ Colors _ %rbracket                            {% (d) => d[2] %}
-
-Mappings -> Mapping                                                               {% (d) => [d[0]] %}
-  | Mappings _ %comma _ Mapping                                                   {% (d) => d[0].concat([d[4]]) %}
-Mapping -> Command _ %colon _ Command _ %equals _ Label                           {% (d) => d[0] + ':' + d[4] + '=' + d[8] %}
-  | Command _ %equals _ Label                                                     {% (d) => d[0] + '=' + d[4] %}
-  | Command _ %colon _ Command _ %equals _ Label _ %equals _ WidgetIconAttrValue  {% (d) => d[0] + ':' + d[4] + '=' + d[8] + '=' + joinValue(d[12]) %}
-  | Command _ %equals _ Label _ %equals _ WidgetIconAttrValue                     {% (d) => d[0] + '=' + d[4] + '=' + joinValue(d[8]) %}
-
-Buttons -> ButtonDef                                                              {% (d) => [d[0]] %}
-  | Buttons _ %comma _ ButtonDef                                                  {% (d) => d[0].concat([d[4]]) %}
-ButtonDef -> %unsignedint _ %colon _ %unsignedint _ %colon _ ButtonValue          {% (d) => { return { 'row':  parseInt(d[0].value), 'column': parseInt(d[4].value), 'command': d[8] } } %}
-ButtonValue -> Command _ %equals _ Label                                          {% (d) => d[0] + '=' + d[4] %}
-  | Command _ %equals _ Label _ %equals _ WidgetIconAttrValue                     {% (d) => d[0] + '=' + d[4] + '=' + joinValue(d[8]) %}
-
-Command -> %unsignedint | %identifier                                             {% (d) => d[0].value %}
-  | %string                                                                       {% (d) => '"' + d[0].value + '"' %}
-Label -> %identifier                                                              {% (d) => d[0].value %}
-  | %string                                                                       {% (d) => '"' + d[0].value + '"' %}
-
-Visibilities -> Conditions                                                        {% (d) => [d[0]] %}
-  | Visibilities _ %comma _ Conditions                                            {% (d) => d[0].concat(d[4]) %}
-
-Colors -> Color                                                                   {% (d) => [d[0]] %}
-  | Colors _ %comma _ Color                                                       {% (d) => d[0].concat([d[4]]) %}
-Color -> Conditions _ %equals _ ColorName                                         {% (d) => d[0] + '=' + d[4][0].value %}
-  | ColorName                                                                     {% (d) => d[0][0].value %}
-ColorName ->  %identifier | %string
-
-IconRules -> IconRule                                                             {% (d) => [d[0]] %}
-  | IconRules _ %comma _ IconRule                                                 {% (d) => d[0].concat([d[4]]) %}
-IconRule -> Conditions _ %equals _ WidgetIconAttrValue                            {% (d) => d[0] + '=' + joinValue(d[4]) %}
-  | WidgetIconAttrValue                                                           {% (d) => joinValue(d[0]) %}
-
-Conditions -> Condition                                                           {% (d) => d[0] %}
-  | Conditions _ %and _ Condition                                                 {% (d) => d[0] + ' AND ' + d[4] %}
-Condition -> ConditionCommand _ ConditionComparator _ ConditionValue              {% (d) => d[0][0].value + d[2][0].value + d[4] %}
-  | ConditionComparator _ ConditionValue                                          {% (d) => d[0][0].value + d[2] %}
-  | ConditionValue                                                                {% (d) => d[0] %}
-ConditionCommand -> %identifier
-ConditionComparator -> %eq | %noteq | %lteq | %gteq | %lt | %gt
-ConditionValue -> XState                                                          {% (d) => d[0] %}
-  | Sign XState                                                                   {% (d) => d[0][0].value + d[1] %}
-XState -> %unsignedint %dot %unsignedint                                          {% (d) => parseFloat(d[0].value + '.' + d[2].value) %}
-  | %unsignedint                                                                  {% (d) => parseInt(d[0].value) %}
-  | %identifier                                                                   {% (d) => d[0].value %}
-  | %string                                                                       {% (d) => '"' + d[0].value + '"' %}
-
-Sign -> %plus
-  | %hyphen
-
-Number -> %hyphen %unsignedint %dot %unsignedint                                  {% (d) => parseFloat('-' + d[1].value + '.' + d[2].value) %}
-  | %hyphen %unsignedint                                                          {% (d) => parseInt('-' + d[1].value) %}
-  | %unsignedint %dot %unsignedint                                                {% (d) => parseFloat(d[0].value + '.' + d[2].value) %}
-  | %unsignedint                                                                  {% (d) => parseInt(d[0].value) %}
-
-_ -> null               {% () => null %}
-	| __                  {% () => null %}
-
-__ -> %WS               {% () => null %}
-	| %NL                 {% () => null %}
-	| Comment 		        {% () => null %}
-	| __ %WS    	        {% () => null %}
-	| __ %NL    	        {% () => null %}
-	| __ Comment 	        {% () => null %}
-
-Comment -> %SL_COMMENT  {% () => null %}
-  | %ML_COMMENT         {% () => null %}
-

bundles/org.openhab.ui/web/src/components/config/controls/code-editor.vue
@@ -94,7 +94,7 @@ import Editor from '@/components/config/controls/script-editor.vue'
 import MovablePopup from '@/pages/settings/movable-popup-mixin'
 import copyToClipboard from '@/js/clipboard'
 import { DefaultMediaTypes, MediaType, SupportedMediaTypes } from '@/assets/definitions/media-types.ts'
-import { showToast } from '@/js/dialog-promises'
+import { showAlertDialog, showToast } from '@/js/dialog-promises'
 
 export default {
   mixins: [MovablePopup],
@@ -127,7 +127,7 @@ export default {
   },
   computed: {
     editorMode() {
-      return this.mediaTypes[this.uiOptionsStore.codeEditorType]
+      return this.mediaTypes[this.uiOptionsStore.codeEditorType] || this.mediaTypes[Object.keys(this.mediaTypes)[0]]
     },
     mediaTypes() {
       return SupportedMediaTypes[this.objectType] || DefaultMediaTypes
@@ -147,14 +147,18 @@ export default {
      *
      * @param {string} codeType - Optional. The type of code to generate (e.g. YAML, DSL)
      * @param {function} onSuccessCallback - Optional. A callback function to call when the code has been generated
+     * @param {object} sourceObject - Optional. The object to generate code from. Defaults to the current prop value.
      */
-    generateCode(codeType, onSuccessCallback) {
+    generateCode(codeType, onSuccessCallback, sourceObject = this.object) {
       codeType ||= this.uiOptionsStore.codeEditorType
+      if (!this.mediaTypes[codeType]) {
+        codeType = Object.keys(this.mediaTypes)[0]
+      }
       const sourceMediaType = MediaType.JSON
       let targetMediaType = this.mediaTypes[codeType]
       targetMediaType = targetMediaType.split('+')[0] // remove the +thing or +item suffix, if present
       const payload = {}
-      payload[this.objectType] = [this.object]
+      payload[this.objectType] = [sourceObject]
       this.$oh.api
         .postPlain('/rest/file-format/create', JSON.stringify(payload), null, sourceMediaType, { accept: targetMediaType })
         .then((code) => {
@@ -162,13 +166,14 @@ export default {
           // therefore normalize before loading in editor.
           this.code = code.replaceAll('\r\n', '\n').replaceAll('\r', '\n')
           this.originalCode = this.code
+          this.dirty = false
           this.uiOptionsStore.codeEditorType = codeType
           if (onSuccessCallback) {
             onSuccessCallback()
           }
         })
         .catch((err) => {
-          f7.dialog.alert(`Error creating ${codeType}: ${err}`).open()
+          showAlertDialog(`Error creating ${codeType}: ${err}`)
         })
     },
     /**
@@ -177,7 +182,7 @@ export default {
      * Called from the parent component to update the object from code.
      * The resulting object is emitted in a `parsed` event.
      *
-     * @param {function} onSuccessCallback - Optional. A callback function to call when the code has been parsed
+     * @param {function} onSuccessCallback - Optional. A callback function to call when the code has been parsed. Receives the parsed object.
      * @param {function} onFailureCallback - Optional. A callback function to call when parsing fails or no object is found
      */
     parseCode(onSuccessCallback, onFailureCallback) {
@@ -195,17 +200,21 @@ export default {
         })
         .then((data) => {
           let object = JSON.parse(data.data)
+          const warnings = object.warnings
+          if (warnings && warnings.length > 0) {
+            showAlertDialog(`Code parsed with warnings:\n${warnings.join('\n')}`)
+          }
           object = object[this.objectType]
           if (object?.length > 0) {
             this.$emit('parsed', object[0])
             if (onSuccessCallback) {
-              onSuccessCallback()
+              onSuccessCallback(object[0])
             }
           } else {
             if (onFailureCallback) {
               onFailureCallback()
             }
-            f7.dialog.alert(`Error parsing ${this.uiOptionsStore.codeEditorType}: no ${this.objectType} found`).open()
+            showAlertDialog(`Error parsing ${this.uiOptionsStore.codeEditorType}: no ${this.objectType} found`)
           }
         })
         .catch((err) => {
@@ -236,7 +245,7 @@ export default {
               f7.popup.open(this.$refs.errors.$el)
             }
           } else {
-            f7.dialog.alert(`Error parsing ${this.uiOptionsStore.codeEditorType}: ${err.message || err.status}`).open()
+            showAlertDialog(`Error parsing ${this.uiOptionsStore.codeEditorType}: ${err.message || err.status}`)
           }
         })
     },
@@ -251,8 +260,8 @@ export default {
       if (this.readOnly || !this.dirty) {
         this.generateCode(type)
       } else {
-        this.parseCode(() => {
-          this.generateCode(type)
+        this.parseCode((parsedObject) => {
+          this.generateCode(type, null, parsedObject)
         })
       }
     },

commit 57724115a2efac89b6cd7fd75c9f7d9b9825c825

Item picker: Fix grayed out like disabled if empty
Regression from #4117.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/config/controls/item-picker.vue
@@ -81,9 +81,6 @@
     padding 0
   .item-inner:after
     display none
-  .item-picker-empty
-    .item-title
-      color var(--f7-input-placeholder-color, var(--f7-list-item-footer-text-color))
   .item-picker:focus-within .item-content
     outline 2px solid var(--f7-theme-color)
     outline-offset -2px