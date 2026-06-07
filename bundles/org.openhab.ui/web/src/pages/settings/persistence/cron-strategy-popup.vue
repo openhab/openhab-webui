4a9d1738cb7783553d7278616054d073bee005b5
Jeff James <jeff@james-online.com>
2026-06-07 08:24:18 -0700

    Enhance UI support for configuring persistence (#3986)
    Based on work by @mherwege in PR #3766:
    
    - Amends the UI to make a distinction between real persistence configuration
    (item, strategy and filter selections that define what will be persisted + aliases)
    and definitions for cron strategies and filters that can afterwards be selected
    - Definitions are considered to be on an extra level and not presented to the
    user immediately in the same screen.
    
    Additional changes to #3766:
    - Remove the usePersistenceEdit Pinia store. This was not needed since all
    persistence edits were done on the single page and creating a separate Pinia
    store is not required for tracking change. Also, once in memory, Pinia stores stay in memory.
    - Migrate the entire persistence configuration code to TypeScript Composition API.
    - Use v-model interfaces and consistently use local copies of data throughout the
    hierarchy as standard mechanisms for updates.
    
    ------
    
    Also-by: Florian Hotze <dev@florianhotze.com>
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>
    Signed-off-by: Jeff James <jeff@james-online.com>

--- bundles/org.openhab.ui/web/src/pages/settings/persistence/cron-strategy-popup.vue
+++ bundles/org.openhab.ui/web/src/pages/settings/persistence/cron-strategy-popup.vue
@@ -1,13 +1,15 @@
 <template>
-  <f7-popup ref="modulePopup" class="moduleconfig-popup">
-    <f7-page>
+  <f7-popup :opened="opened" @popup:opened="onOpened" class="cron-strategy-popup moduleconfig-popup">
+    <f7-page v-if="opened">
       <f7-navbar>
         <f7-nav-left>
-          <f7-link icon-ios="f7:arrow_left" icon-md="material:arrow_back" icon-aurora="f7:arrow_left" popup-close />
+          <f7-link icon-ios="f7:arrow_left" icon-md="material:arrow_back" icon-aurora="f7:arrow_left" @click="closePopup" />
         </f7-nav-left>
-        <f7-nav-title> Configure cron strategy </f7-nav-title>
+        <f7-nav-title> {{ createMode ? 'Add Cron Strategy' : 'Edit Cron Strategy' }} </f7-nav-title>
         <f7-nav-right>
-          <f7-link v-show="currentCronStrategy.name && currentCronStrategy.cronExpression" @click="updateModuleConfig"> Done </f7-link>
+          <f7-link v-show="localCronStrategy.name && localCronStrategy.cronExpression" @click="onDone">
+            {{ createMode ? 'Add' : 'Done' }}
+          </f7-link>
         </f7-nav-right>
       </f7-navbar>
       <f7-block class="no-margin no-padding">
@@ -18,25 +20,24 @@
               label="Name"
               type="text"
               placeholder="Required"
-              :value="currentCronStrategy.name"
-              @input="currentCronStrategy.name = $event.target.value"
+              v-model:value="localCronStrategy.name"
               :disabled="!createMode ? true : null"
               :info="createMode ? 'Note: cannot be changed after the creation' : ''"
               required
               validate
               pattern="[A-Za-z0-9_]+"
-              error-message="Required. A-Z,a-z only" />
+              :error-message="nameErrorMessage"
+              :error-message-force="createMode && isDuplicateName" />
           </f7-list>
         </f7-col>
         <f7-col>
-          <f7-block-title medium class="padding-bottom"> Configuration </f7-block-title>
-          <f7-list>
+          <f7-block-title medium class="padding-bottom">Configuration</f7-block-title>
+          <f7-list style="--f7-list-in-list-padding-left: 0">
             <f7-list-group>
               <parameter-cronexpression
                 ref="cronExpression"
                 :configDescription="cronExpressionConfigDescription"
-                :value="currentCronStrategy.cronExpression"
-                @input="currentCronStrategy.cronExpression = $event" />
+                v-model:value="localCronStrategy.cronExpression" />
             </f7-list-group>
           </f7-list>
         </f7-col>
@@ -45,43 +46,72 @@
   </f7-popup>
 </template>
 
-<script>
+<script setup lang="ts">
+import { ref, computed, useTemplateRef } from 'vue'
 import { f7 } from 'framework7-vue'
-
 import ParameterCronexpression from '@/components/config/controls/parameter-cronexpression.vue'
 
-export default {
-  components: {
-    ParameterCronexpression
-  },
-  props: {
-    cronStrategy: Object
-  },
-  emits: ['cronStrategyConfigUpdate'],
-  data() {
-    return {
-      createMode: !this.cronStrategy,
-      currentCronStrategy: this.cronStrategy || {
-        name: null,
-        cronExpression: null
-      },
+import type * as api from '@/api'
 
-      cronExpressionConfigDescription: {
-        label: 'Cron Expression',
-        name: 'cronExpression',
-        required: true
-      }
-    }
-  },
-  methods: {
-    updateModuleConfig() {
-      if (!f7.input.validateInputs(this.$refs.name.$el) && !f7.input.validateInputs(this.$refs.cronExpression.$el)) {
-        f7.dialog.alert('Please review the configuration and correct validation errors')
-        return
-      }
-      f7.emit('cronStrategyConfigUpdate', this.currentCronStrategy)
-      this.$refs.modulePopup.$el.f7Modal.close()
-    }
+const nameRef = useTemplateRef('name')
+const cronExpressionRef = useTemplateRef('cronExpression')
+// Non-null assertion: this popup is only rendered within persistence-edit which always provides the key
+
+// Props and emits
+const opened = defineModel<boolean>('opened')
+const cronStrategy = defineModel<api.PersistenceCronStrategy | null>('cronStrategy', { required: true })
+const emits = defineEmits<{
+  (e: 'add:cronStrategy', cronStrategy: api.PersistenceCronStrategy): void
+}>()
+const props = defineProps<{ cronNameList: string[] }>()
+
+const newCronStrategy = {
+  name: '',
+  cronExpression: ''
+} satisfies api.PersistenceCronStrategy
+
+const localCronStrategy = ref<api.PersistenceCronStrategy>({} as api.PersistenceCronStrategy)
+
+const cronExpressionConfigDescription = {
+  label: 'Cron Expression',
+  name: 'cronExpression',
+  required: true
+}
+
+// Computed properties
+const createMode = computed(() => !cronStrategy.value)
+const isDuplicateName = computed(() => createMode.value && props.cronNameList.includes(localCronStrategy.value.name))
+const nameErrorMessage = computed(() =>
+  isDuplicateName.value ? 'A (cron) strategy with the same name already exists!' : 'Required. Use only letters, numbers, and _.'
+)
+
+// Event handlers
+function onOpened() {
+  localCronStrategy.value = cronStrategy.value ? { ...cronStrategy.value } : { ...newCronStrategy }
+}
+
+function onDone() {
+  if (!f7.input.validateInputs(nameRef.value.$el) || !f7.input.validateInputs(cronExpressionRef.value.$el)) {
+    f7.dialog.alert('Please review the configuration and correct validation errors')
+    return
   }
+
+  // Check for duplicates (unless editing existing)
+  if (isDuplicateName.value) {
+    f7.dialog.alert('A (cron) strategy with the same name already exists!')
+    return
+  }
+
+  if (createMode.value) {
+    emits('add:cronStrategy', { ...localCronStrategy.value })
+  } else {
+    cronStrategy.value = { ...localCronStrategy.value }
+  }
+  closePopup()
+}
+
+// Methods
+function closePopup() {
+  opened.value = false
 }
 </script>
--- bundles/org.openhab.ui/web/src/pages/settings/persistence/filter-popup.vue
+++ bundles/org.openhab.ui/web/src/pages/settings/persistence/filter-popup.vue
@@ -1,16 +1,30 @@
 <template>
-  <f7-popup ref="modulePopup" class="moduleconfig-popup">
+  <f7-popup :opened="opened" @popup:opened="onOpen" class="filter-popup">
     <f7-page>
       <f7-navbar>
         <f7-nav-left>
-          <f7-link icon-ios="f7:arrow_left" icon-md="material:arrow_back" icon-aurora="f7:arrow_left" popup-close />
+          <f7-link icon-ios="f7:arrow_left" icon-md="material:arrow_back" icon-aurora="f7:arrow_left" @click="closePopup" />
         </f7-nav-left>
-        <f7-nav-title> Configure {{ filterType.label.toLowerCase() }} filter </f7-nav-title>
+        <f7-nav-title>{{ title }}</f7-nav-title>
         <f7-nav-right>
-          <f7-link v-show="currentFilter.name" @click="updateModuleConfig"> Done </f7-link>
+          <f7-link v-show="localFilter.filter.name != ''" @click="onDone">{{ createMode ? 'Add' : 'Done' }}</f7-link>
         </f7-nav-right>
       </f7-navbar>
-      <f7-block class="no-margin no-padding">
+      <f7-block v-if="!localFilter.filterTypeName">
+        <f7-row v-for="(ftRow, rowIndex) in filterTypesMatrix" :key="rowIndex" class="margin-bottom">
+          <f7-col
+            v-for="ft in ftRow"
+            :key="ft.name"
+            class="elevation-2 elevation-hover-6 elevation-pressed-1 persistence-filter-big-button"
+            width="50">
+            <f7-link class="display-flex flex-direction-column no-ripple" no-ripple @click="setFilterType(ft.name as FilterTypeName)">
+              <f7-icon size="35" :f7="ft.icon" class="margin" />
+              {{ ft.label }}<br />Filter
+            </f7-link>
+          </f7-col>
+        </f7-row>
+      </f7-block>
+      <f7-block v-else class="no-margin no-padding">
         <f7-col>
           <f7-list>
             <f7-list-input
@@ -18,65 +32,146 @@
               label="Name"
               type="text"
               placeholder="Required"
-              :value="currentFilter.name"
-              @input="currentFilter.name = $event.target.value"
+              :value="localFilter.filter.name"
+              @input="localFilter.filter.name = $event.target.value"
               :disabled="!createMode ? true : null"
               :info="createMode ? 'Note: cannot be changed after the creation' : ''"
               required
               validate
               pattern="[A-Za-z0-9_]+"
-              error-message="Required. A-Z,a-z only" />
+              :error-message="nameErrorMessage"
+              :error-message-force="createMode && isDuplicateName" />
           </f7-list>
         </f7-col>
         <f7-col>
-          <f7-block-title medium> Configuration </f7-block-title>
+          <f7-block-title medium>Configuration</f7-block-title>
           <config-sheet
             ref="config-sheet"
             :parameter-groups="[]"
             :parameters="filterConfigDescriptionParameters"
-            :configuration="currentFilter" />
+            :configuration="localFilter.filter" />
         </f7-col>
       </f7-block>
     </f7-page>
   </f7-popup>
 </template>
 
-<script>
+<style lang="stylus">
+.persistence-filter-big-button
+  background var(--f7-card-bg-color)
+  text-align center
+  height 7.5rem
+  .link
+    color var(--f7-text-color)
+</style>
+
+<script setup lang="ts">
+import { ref, computed, useTemplateRef } from 'vue'
 import { f7 } from 'framework7-vue'
-
 import ConfigSheet from '@/components/config/config-sheet.vue'
+import { FilterTypeName, FilterTypes, type Filter } from '@/assets/definitions/persistence'
 
-export default {
-  components: { ConfigSheet },
-  props: {
-    filter: Object,
-    filterType: Object,
-    filterConfigDescriptionParameters: Array
-  },
-  emits: ['filterUpdate'],
-  data() {
-    return {
-      createMode: !this.filter,
-      currentFilter: this.filter || {
-        name: null
-      }
-    }
-  },
-  methods: {
-    updateModuleConfig() {
-      if (!this.$refs['config-sheet'].isValid()) {
-        f7.dialog.alert('Please review the configuration and correct validation errors')
-        return
-      }
-      if (this.filterType.name === 'includeFilters') {
-        if (this.currentFilter.upper <= this.currentFilter.lower) {
-          f7.dialog.alert('The lower bound value must be less than the upper bound value')
-          return
-        }
-      }
-      f7.emit('filterUpdate', this.currentFilter, this.filterType.name)
-      this.$refs.modulePopup.$el.f7Modal.close()
+import cloneDeep from 'lodash/cloneDeep'
+
+// Props and emits
+const opened = defineModel<boolean>('opened')
+const filter = defineModel<Filter | null>('filter', { required: true })
+const emits = defineEmits<{
+  (e: 'add:filter', filter: Filter): void
+}>()
+const props = defineProps<{ filterNameList: string[] }>()
+
+// Local variables
+const filterTypesMatrix = createFilterTypesMatrix()
+
+// Refs and reactive data
+const configSheetRef = useTemplateRef('config-sheet')
+const localFilter = ref<Filter>({ filter: { name: '' } } as Filter) // props.filter will be copied to this localFilter on open
+
+// computed properties
+const createMode = computed(() => !filter.value)
+const filterConfigDescriptionParameters = computed(() => filterType.value?.configDescriptionParameters || [])
+const filterTypeLabel = computed(() => filterType.value?.label?.toLowerCase() || '')
+const filterType = computed(() => (localFilter.value.filterTypeName ? FilterTypes[localFilter.value.filterTypeName] : null))
+const title = computed(() => (localFilter.value.filterTypeName ? `Configure ${filterTypeLabel.value} Filter` : 'Select Filter Type'))
+const isDuplicateName = computed(() => createMode.value && props.filterNameList.includes(localFilter.value.filter.name))
+const nameErrorMessage = computed(() =>
+  isDuplicateName.value ? 'A filter with the same name already exists!' : 'Required. Use only letters, numbers, and _.'
+)
+
+// methods
+function createFilterTypesMatrix() {
+  const matrix = []
+  const columns = 2
+  const filterTypesList = Object.values(FilterTypes)
+
+  for (let i = 0; i < filterTypesList.length; i += columns) {
+    matrix.push(filterTypesList.slice(i, i + columns))
+  }
+  return matrix
+}
+
+function setFilterType(filterTypeName: FilterTypeName) {
+  localFilter.value.filterTypeName = filterTypeName
+  localFilter.value.filter = { name: filterTypeName }
+  if (localFilter.value.filterTypeName === FilterTypeName.EqualsFilters) {
+    if (!localFilter.value.filter.values) {
+      localFilter.value.filter.values = ['']
     }
   }
+}
+
+function closePopup() {
+  opened.value = false
+}
+
+// Events
+function onOpen() {
+  localFilter.value = filter.value ? cloneDeep(filter.value) : ({ filter: { name: '' } } as Filter)
+  if (localFilter.value.filterTypeName === FilterTypeName.EqualsFilters && !filter.value) {
+    localFilter.value.filter.values = ['']
+  }
+}
+
+function onDone() {
+  if (!localFilter.value.filterTypeName) {
+    f7.dialog.alert('Please select a filter type')
+    return
+  }
+
+  if (!configSheetRef.value?.isValid()) {
+    f7.dialog.alert('Please review the configuration and correct validation errors')
+    return
+  }
+
+  if (localFilter.value.filterTypeName === FilterTypeName.IncludeFilters) {
+    if (localFilter.value.filter.lower == null || localFilter.value.filter.upper == null) {
+      f7.dialog.alert('Please provide both lower and upper bound values')
+      return
+    }
+    if (localFilter.value.filter.upper <= localFilter.value.filter.lower) {
+      f7.dialog.alert('The lower bound value must be less than the upper bound value')
+      return
+    }
+  }
+
+  if (isDuplicateName.value) {
+    f7.dialog.alert('A filter with the same name already exists!')
+    return
+  }
+
+  // Handle comma-separated values for equalsFilters
+  let filterToSave = cloneDeep(localFilter.value)
+  if (localFilter.value.filterTypeName === FilterTypeName.EqualsFilters && typeof filterToSave.filter.values === 'string') {
+    filterToSave.filter.values = (filterToSave.filter.values as string).split(',').map((v) => v.trim())
+  }
+
+  if (createMode.value) {
+    emits('add:filter', filterToSave)
+  } else {
+    filter.value = filterToSave
+  }
+
+  closePopup()
 }
 </script>
