openhab/openhab-webui

commit 4a0de997bd1f4e55d0bbe90496ae07253406d826

Semantic tag editor use conversion REST API (#4234)
This enhances the semantic tag editor to use the new YAML conversion API
introduced in: https://github.com/openhab/openhab-core/pull/5573

The logic in the treeview has been enhanced to only allow adding child
tags inside a default tag or a managed tag to avoid issues of parent
tags not being available when loading in core.

Not done in this PR, but a potential future enhancement: if tags are
edited in the code editor, check the the parent-child relationships are
still correct before accepting the change.

Here are some screenshots:

Show and edit editable (=managed) tags:

<img width="748" height="944" alt="image"
src="https://github.com/user-attachments/assets/1b6052c8-6f25-4227-87be-f9d464f6abab"
/>

Show all custom tags (managed and unmanaged). If there are no unmanaged
tags (nothing defined in files), this will be editable, if not this will
be read-only:

<img width="752" height="942" alt="image"
src="https://github.com/user-attachments/assets/bdbff550-3b5c-474d-bf88-b409093cd899"
/>

Show all tags, including the default tags. This will always be
read-only:

<img width="750" height="938" alt="image"
src="https://github.com/user-attachments/assets/52a84070-378f-4316-9a18-79681dba4c0f"
/>

---------

Signed-off-by: Mark Herwege <mark.herwege@telenet.be>
Signed-off-by: Jeff James <jeff@james-online.com>
Co-authored-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/pages/developer/semantics/semantic-tags-edit.vue
@@ -9,9 +9,9 @@
         @save="save()"
         :f7router />
     </f7-navbar>
-    <f7-toolbar tabbar position="top">
-      <f7-link @click="switchTab('tree')" :tab-link-active="currentTab === 'tree'" tab-link="#tree"> Design </f7-link>
-      <f7-link @click="switchTab('code')" :tab-link-active="currentTab === 'code'" tab-link="#code"> Code </f7-link>
+    <f7-toolbar tabbar position="top" class="semantics-editor-tabbar">
+      <f7-link @click="switchTab('tree', switchTabTree)" :tab-link-active="currentTab === 'tree'"> Design </f7-link>
+      <f7-link @click="switchTab('code', switchTabCode)" :tab-link-active="currentTab === 'code'"> Code </f7-link>
     </f7-toolbar>
     <f7-toolbar v-if="currentTab === 'tree'" bottom class="toolbar-details">
       <f7-link class="left" :class="{ disabled: selectedTag == null }" @click="selectTag(null)"> Clear </f7-link>
@@ -139,7 +139,7 @@
                         :disabled="!selectedTag.editable"
                         :clear-button="selectedTag.editable"
                         placeholder="synonym"
-                        @change="updateSynonyms($event, index)" />
+                        @change="updateSynonym($event, index)" />
                       <f7-list-input
                         :value="newSynonym"
                         :disabled="!selectedTag.editable"
@@ -151,7 +151,7 @@
                   </f7-card-content>
                 </f7-card>
               </f7-block>
-              <f7-block v-if="selectedTag">
+              <f7-block v-if="selectedTag && (selectedTag.editable || selectedTag.defaultTag)">
                 <div><f7-block-title>Add Child Tag</f7-block-title></div>
                 <f7-card>
                   <f7-card-content>
@@ -169,13 +169,51 @@
         </f7-block>
       </f7-tab>
       <f7-tab id="code" :tab-active="currentTab === 'code'">
-        <editor
+        <code-editor
           v-if="ready"
-          class="semantic-tag-code-editor"
-          mode="application/vnd.openhab.tag+yaml"
-          :value="editingTagsYaml"
-          @input="onEditorInput"
-          @save="save()" />
+          ref="codeEditor"
+          object-type="tags"
+          :read-only="editorReadOnly"
+          :read-only-msg="
+            showCodeTags === 'custom'
+              ? 'Showing non-editable tags, switch to editable to edit'
+              : showCodeTags === 'all'
+                ? 'Showing default tags, switch to editable to edit'
+                : ''
+          "
+          :object="codeEditorTags"
+          @parsed="update"
+          @changed="onCodeChanged"
+          @save="save">
+          <template #additional-panel-controls>
+            <f7-segmented>
+              <f7-button
+                outline
+                small
+                :active="showCodeTags === 'editable'"
+                tooltip="Show all editable tags"
+                @click="switchCodeTags('editable')">
+                Editable
+              </f7-button>
+              <f7-button
+                outline
+                small
+                :active="showCodeTags === 'custom'"
+                tooltip="Show all user defined tags"
+                @click="switchCodeTags('custom')">
+                Custom
+              </f7-button>
+              <f7-button
+                outline
+                small
+                :active="showCodeTags === 'all'"
+                tooltip="Show all tags, including default tags"
+                @click="switchCodeTags('all')">
+                All
+              </f7-button>
+            </f7-segmented>
+          </template>
+        </code-editor>
       </f7-tab>
     </f7-tabs>
 
@@ -245,7 +283,7 @@
               :disabled="!selectedTag.editable ? true : null"
               :clear-button="selectedTag.editable"
               placeholder="synonym"
-              @change="updateSynonyms($event, index)" />
+              @change="updateSynonym($event, index)" />
             <f7-list-input
               :value="newSynonym"
               :disabled="!selectedTag.editable ? true : null"
@@ -278,6 +316,12 @@
     --f7-grid-gap 0px
     overflow auto
 
+.semantics-editor-tabbar
+  .link
+    border-bottom 2px solid transparent
+  .link.tab-link-active
+    border-bottom-color var(--f7-tabbar-link-active-border-color)
+
 .semantics-tree-wrapper
   padding 0
   margin-bottom 0
@@ -288,7 +332,6 @@
 
 .semantics-tree
   padding 0
-  border-right 1px solid var(--f7-block-strong-border-color)
   --f7-theme-color var(--f7-color-blue)
   --f7-theme-color-rgb var(--f7-color-blue-rgb)
   .treeview
@@ -317,6 +360,7 @@
         width 50% /* manually set column width because of https://github.com/openhab/openhab-webui/issues/2574 */
         height 100%
         overflow auto
+        border-right 1px solid var(--f7-block-strong-border-color)
         .semantics-tree
           margin 0
           height auto
@@ -350,45 +394,48 @@
 </style>
 
 <script>
-import { defineAsyncComponent } from 'vue'
+import { nextTick, defineAsyncComponent } from 'vue'
 import { f7, theme } from 'framework7-vue'
 import { mapStores } from 'pinia'
 
-import YAML from 'yaml'
 import fastDeepEqual from 'fast-deep-equal/es6'
 import SemanticsTreeview from '@/components/tags/semantics-treeview.vue'
 import TagMixin from '@/components/tags/tag-mixin'
 import { useDirty } from '@/pages/useDirty'
+import { useTabs } from '@/pages/useTabs'
 
 import { i18n } from '@/js/i18n'
 
 import { useSemanticsStore } from '@/js/stores/useSemanticsStore'
-import { showToast } from '@/js/dialog-promises'
+import { showToast, showAlertDialog } from '@/js/dialog-promises'
 
 export default {
   mixins: [TagMixin],
   components: {
     SemanticsTreeview,
-    editor: defineAsyncComponent(() => import(/* webpackChunkName: "script-editor" */ '@/components/config/controls/script-editor.vue'))
+    CodeEditor: defineAsyncComponent(() => import(/* webpackChunkName: "code-editor" */ '@/components/config/controls/code-editor.vue'))
   },
   props: {
     f7router: Object
   },
   setup() {
-    const { dirty, dirtyIndicator } = useDirty('semantic-tags-edit-page')
+    const { currentTab, switchTab } = useTabs('tree')
+    const { dirty, dirtyIndicator, setupDirtyWatch } = useDirty('semantic-tags-edit-page')
     return {
       theme,
       dirty,
-      dirtyIndicator
+      dirtyIndicator,
+      setupDirtyWatch,
+      currentTab,
+      switchTab
     }
   },
   data() {
     return {
       semanticTags: [],
-      selectedTag: null,
-      expandedTags: [],
+      selectedTagUid: null,
+      expandedTags: {},
       newSynonym: '',
-      currentTab: 'tree',
       detailsTab: 'tag',
       detailsOpened: false,
       loading: false,
@@ -397,24 +444,48 @@ export default {
       expanded: false,
       filtering: false,
       expandedBeforeFiltering: false,
-      editableSemanticTagsYaml: null,
-      editingTagsYaml: null,
-      nonCodeDirty: false, // When editing code, keeps track if it was already dirty before switching to code tab
-      ready: false
+      ready: false,
+      tagsDirty: false,
+      codeDirty: false,
+      showCodeTags: 'editable'
     }
   },
   computed: {
-    ...mapStores(useSemanticsStore)
+    ...mapStores(useSemanticsStore),
+    selectedTag() {
+      return this.semanticTags.find((t) => t.uid === this.selectedTagUid) || null
+    },
+    editableTags() {
+      return this.semanticTags.filter((t) => t.editable).sort((a, b) => a.uid.localeCompare(b.uid))
+    },
+    customTags() {
+      return this.semanticTags.filter((t) => !t.defaultTag)
+    },
+    codeTagsToShow() {
+      if (this.showCodeTags === 'all') return this.semanticTags
+      if (this.showCodeTags === 'custom') return this.customTags
+      if (this.showCodeTags === 'editable') return this.editableTags
+      return []
+    },
+    codeEditorTags() {
+      return this.codeTagsToShow
+        .map((t) => {
+          const uid = (t.parent ? t.parent + '_' : '') + t.name
+          return {
+            uid: uid,
+            label: t.label,
+            description: t.description,
+            synonyms: [...t.synonyms]
+          }
+        })
+        .sort((a, b) => a.uid.localeCompare(b.uid))
+    },
+    editorReadOnly() {
+      if (this.showCodeTags === 'editable') return false
+      return this.codeTagsToShow.some((t) => !t.editable)
+    }
   },
   watch: {
-    semanticTags: {
-      handler: function () {
-        if (!this.loading) {
-          this.dirty = true
-        }
-      },
-      deep: true
-    },
     'semanticsStore.ready': {
       handler: function (newValue, oldValue) {
         if (newValue) {
@@ -422,6 +493,11 @@ export default {
         }
       },
       immediate: true
+    },
+    currentTab(newTab, oldTab) {
+      if (oldTab === 'tree') {
+        this.$refs.detailsSheet?.$el?.f7Modal?.close?.()
+      }
     }
   },
   methods: {
@@ -429,6 +505,7 @@ export default {
       if (window) {
         window.addEventListener('keydown', this.keyDown)
       }
+      this.setupDirtyWatch(() => this.editableTags)
     },
     onPageBeforeOut() {
       if (window) {
@@ -441,40 +518,56 @@ export default {
         useSemanticsStore().loadSemantics(i18n)
       }
     },
-    onEditorInput(value) {
-      if (value !== this.editableSemanticTagsYaml) {
-        this.dirty = this.nonCodeDirty || true
-      } else {
-        this.dirty = this.nonCodeDirty || false
-      }
-      this.editingTagsYaml = value
-    },
-    switchTab(tab) {
-      if (this.currentTab === tab) return
-      // avoid error with existing details sheet when switching tabs
-      const sheet = this.$refs['details-sheet']?.$el.f7Modal
-      if (sheet?.opened) {
-        sheet.close()
-      }
-      if (tab === 'code') {
-        this.currentTab = tab
-        this.nonCodeDirty = this.dirty
-        this.selectTag(null)
-        this.editableSemanticTagsYaml = this.toYaml()
-        this.editingTagsYaml = this.editableSemanticTagsYaml
-      } else {
-        if (!this.fromYaml()) {
-          f7.dialog.alert('Error parsing YAML')
-          return
-        }
-        this.currentTab = tab
-      }
-    },
     keyDown(ev) {
       if (ev.keyCode === 83 && (ev.ctrlKey || ev.metaKey) && !(ev.altKey || ev.shiftKey)) {
         this.save()
         ev.stopPropagation()
         ev.preventDefault()
+      }
+    },
+    async switchTabTree() {
+      if (!this.codeDirty) return true
+
+      // Delay switching to the tree tab until parsing succeeds.
+      return new Promise((resolve) => {
+        this.$refs.codeEditor.parseCode(
+          () => {
+            resolve(true)
+          },
+          () => {
+            resolve(false)
+          },
+          true
+        )
+      })
+    },
+    switchTabCode() {
+      // Switching to code tab: set immediately, then generate
+      this.codeDirty = false
+      nextTick(() => {
+        this.$refs.codeEditor?.generateCode?.()
+      })
+    },
+    switchCodeTags(showCodeTags) {
+      if (this.showCodeTags === showCodeTags) return
+
+      if (!this.codeDirty) {
+        this.showCodeTags = showCodeTags
+        this.$nextTick(() => {
+          this.$refs.codeEditor?.generateCode?.()
+        })
+      } else {
+        this.codeDirty = false
+        this.$refs.codeEditor?.parseCode(
+          () => {
+            this.showCodeTags = showCodeTags
+            this.$nextTick(() => {
+              this.$refs.codeEditor?.generateCode?.()
+            })
+          },
+          undefined,
+          true
+        )
       }
     },
     load() {
@@ -487,41 +580,56 @@ export default {
           name: t.name,
           label: useSemanticsStore().Labels[t.name],
           description: t.description,
-          synonyms: useSemanticsStore().Synonyms[t.name],
+          synonyms: [...useSemanticsStore().Synonyms[t.name]],
           editable: t.editable,
+          defaultTag: !!t.defaultTag,
           parent: t.parent
         }
       })
       this.semanticTags = tags
       this.$nextTick(() => {
+        this.tagsDirty = this.codeDirty = false
         this.dirty = false
         this.loading = false
         this.ready = true
       })
     },
     async save() {
-      if (!this.dirty) return
-      if (this.currentTab === 'code') {
-        if (!this.fromYaml()) {
-          f7.dialog.alert('Error parsing YAML, cannot save')
+      if (this.currentTab === 'code' && this.codeDirty) {
+        const parsedSuccessfully = await new Promise((resolve) => {
+          this.$refs.codeEditor.parseCode(
+            () => {
+              resolve(true)
+            },
+            () => {
+              showAlertDialog('Error parsing YAML, cannot save')
+              resolve(false)
+            },
+            true
+          )
+        })
+
+        if (!parsedSuccessfully) {
           return
         }
-      }
 
-      const editableTags = this.semanticTags.filter((t) => t.editable)
-      const addedTags = editableTags.filter((t) => !useSemanticsStore().Tags.find((c) => c.uid === t.uid))
-      const modifiedTags = editableTags.filter((t) => useSemanticsStore().Tags.find((c) => c.uid === t.uid && !fastDeepEqual(c, t)))
+        await this.$nextTick()
+      }
+      if (!this.dirty) return
+
+      const addedTags = this.editableTags.filter((t) => !useSemanticsStore().Tags.find((c) => c.uid === t.uid))
+      const modifiedTags = this.editableTags.filter((t) => useSemanticsStore().Tags.find((c) => c.uid === t.uid && !fastDeepEqual(c, t)))
       const removedTags = useSemanticsStore().Tags.filter((c) => !this.semanticTags.find((t) => t.uid === c.uid))
       console.debug('Added: ', addedTags, 'Removed: ', removedTags, 'Modified: ', modifiedTags)
 
       if (
         addedTags.some((t) => {
           if (!t.name || !t.label || modifiedTags.some((t) => !t.name || !t.label)) {
-            f7.dialog.alert(`${t.name}: Tag name and label required`)
+            showAlertDialog(`${t.name}: Tag name and label required`)
             return true
           }
           if (useSemanticsStore().Tags.find((c) => c.name === t.name) && !removedTags.find((r) => r.name === t.name)) {
-            f7.dialog.alert(`${t.name}: Tag names must be unique`)
+            showAlertDialog(`${t.name}: Tag names must be unique`)
             return true
           }
           return false
@@ -534,6 +642,7 @@ export default {
       const changeTasks = modifiedTags.map((t) => () => this.$oh.api.put('/rest/tags/' + t.uid, t))
       const removeTasks = removedTags.map((t) => () => this.$oh.api.delete('/rest/tags/' + t.uid))
       if (addTasks.length <= 0 && changeTasks.length <= 0 && removeTasks.length <= 0) {
+        this.tagsDirty = this.codeDirty = false
         this.dirty = false
         return
       }
@@ -556,15 +665,11 @@ export default {
           console.debug('Successfully modified tags')
           showToast((changeTasks.length === 1 ? 'Tag' : 'Tags') + ' modified')
         }
-        this.dirty = false
-        useSemanticsStore()
-          .loadSemantics(i18n)
-          .then(() => {
-            this.load()
-          })
+        await useSemanticsStore().loadSemantics(i18n)
+        this.load()
       } catch (error) {
         console.error(error)
-        f7.dialog.alert('Error saving: ' + error)
+        showAlertDialog('Error saving: ' + error)
       }
     },
     toggleExpanded() {
@@ -599,14 +704,14 @@ export default {
       }
     },
     selectTag(tag) {
-      if (this.selectedTag === tag) return
-      this.selectedTag = null
+      if (this.selectedTagUid === tag?.uid) return
       if (!tag) {
+        this.selectedTagUid = null
         this.detailsOpened = false
         return
       }
       this.$nextTick(() => {
-        this.selectedTag = tag
+        this.selectedTagUid = tag.uid
         this.detailsTab = 'tag'
         this.$nextTick(() => {
           const detailsLink = this.$refs.detailsLink
@@ -655,7 +760,7 @@ export default {
       }
       this.newSynonym = ''
     },
-    updateSynonyms(event, index) {
+    updateSynonym(event, index) {
       const newValue = event.target.value.trim()
       if (newValue) {
         this.selectedTag.synonyms.splice(index, 1, newValue)
@@ -668,40 +773,24 @@ export default {
         this.selectTag(null)
       }
     },
-    toYaml() {
-      const tags = this.semanticTags
-        .filter((t) => t.editable)
-        .map((t) => {
-          return {
-            name: t.name,
-            label: t.label,
-            description: t.description,
-            synonyms: [...t.synonyms],
-            parent: t.parent
-          }
-        })
-      return YAML.stringify(tags)
-    },
-    fromYaml() {
+    update(value) {
+      if (this.editorReadOnly) return
+
       const tags = [...this.semanticTags].filter((t) => !t.editable)
-      const editableTags = this.semanticTags.filter((t) => t.editable)
-      try {
-        const updatedTags = YAML.parse(this.editingTagsYaml).map((t) => {
-          const tag = t
-          tag.editable = true
-          tag.uid = t.parent + '_' + t.name
-          return tag
-        })
-        if (fastDeepEqual(updatedTags, editableTags)) {
-          return true
-        }
-        tags.push(...updatedTags)
-      } catch (error) {
-        console.warn('Error parsing YAML')
-        return false
-      }
+      const updatedTags = value.map((t) => {
+        const tag = t
+        tag.name = tag.uid.split('_').slice(-1)[0]
+        tag.parent = tag.uid.split('_').slice(0, -1).join('_')
+        tag.editable = true
+        tag.defaultTag = false
+        return tag
+      })
+      tags.push(...updatedTags)
       this.semanticTags = tags
-      return true
+    },
+    onCodeChanged(codeDirty) {
+      this.dirty = this.tagsDirty || codeDirty
+      this.codeDirty = codeDirty
     }
   }
 }

bundles/org.openhab.ui/web/src/assets/definitions/media-types.ts
@@ -5,6 +5,7 @@ export enum MediaType {
   JSON = 'application/json',
   JAVASCRIPT = 'application/javascript',
   // openHAB vendor specific:
+  TAG_YAML = 'application/yaml+tag',
   THING_YAML = 'application/yaml+thing',
   ITEM_YAML = 'application/yaml+item',
   THING_DSL = 'text/vnd.openhab.dsl.thing',
@@ -27,6 +28,9 @@ export const DefaultMediaTypes = {
  * Specific media types supported additionally to the {@link DefaultMediaTypes} for specific configuration.
  */
 export const SupportedMediaTypes = {
+  tags: {
+    YAML: MediaType.TAG_YAML
+  },
   items: {
     YAML: MediaType.ITEM_YAML,
     DSL: MediaType.ITEM_DSL

bundles/org.openhab.ui/web/src/components/config/controls/code-editor.vue
@@ -23,13 +23,15 @@
         {{ type }}
       </f7-button>
     </f7-segmented>
+    <slot name="additional-panel-controls" />
     <f7-button
       @click="copy"
       icon-ios="f7:square_on_square"
       icon-aurora="f7:square_on_square"
       color="blue"
+      tooltip="Copy code to clipboard"
       class="copy display-flex flex-direction-row">
-      &nbsp;Copy
+      <span class="button-label">Copy</span>
     </f7-button>
     <f7-button
       v-if="!readOnly"
@@ -38,8 +40,9 @@
       icon-ios="f7:arrow_2_circlepath"
       icon-aurora="f7:arrow_2_circlepath"
       color="red"
+      tooltip="Revert local changes to the code"
       class="reset display-flex flex-direction-row">
-      &nbsp;Revert
+      <span class="button-label">Revert</span>
     </f7-button>
   </f7-toolbar>
 
@@ -75,9 +78,38 @@
   position absolute
   .toolbar-inner
     padding-left 8px
+    .toolbar-options
+      align-items center
+      gap 8px
+      .opt-show-all
+        flex-wrap nowrap
+        align-items center
+        flex-direction row
+        span
+          white-space nowrap
+          padding-left 4px
   .segmented
     .button
-      width 5em
+      width auto
+  .button-label
+    margin-left 4px
+
+@media (max-width: 475px)
+  .code-editor-toolbar
+    --f7-toolbar-height var(--f7-tabbar-labels-height)
+    font-size var(--f7-tabbar-label-font-size)
+    .button-label
+      display none
+    .toolbar-inner
+      padding-left 5px
+      .toolbar-options
+        gap 6px
+        .opt-show-all
+          flex-wrap nowrap
+          align-items center
+          flex-direction column
+          span
+            padding-left 0
 
 .code-editor-errors
   .item-title
@@ -102,7 +134,7 @@ export default {
     Editor
   },
   props: {
-    object: Object,
+    object: [Object, Array],
     objectType: String, // the type of the object, e.g. 'items', 'things' which corresponds to the yaml element name.
     objectId: String,
     hintContext: Object,
@@ -120,7 +152,6 @@ export default {
     return {
       code: null,
       originalCode: null,
-      displayCodeSwitcher: false,
       dirty: false,
       errors: null
     }
@@ -147,18 +178,21 @@ export default {
      *
      * @param {string} codeType - Optional. The type of code to generate (e.g. YAML, DSL)
      * @param {function} onSuccessCallback - Optional. A callback function to call when the code has been generated
-     * @param {object} sourceObject - Optional. The object to generate code from. Defaults to the current prop value.
      */
-    generateCode(codeType, onSuccessCallback, sourceObject = this.object) {
+    generateCode(codeType, onSuccessCallback) {
       codeType ||= this.uiOptionsStore.codeEditorType
       if (!this.mediaTypes[codeType]) {
         codeType = Object.keys(this.mediaTypes)[0]
       }
       const sourceMediaType = MediaType.JSON
       let targetMediaType = this.mediaTypes[codeType]
-      targetMediaType = targetMediaType.split('+')[0] // remove the +thing or +item suffix, if present
+      targetMediaType = targetMediaType.split('+')[0] // remove the +tag, +thing or +item suffix, if present
       const payload = {}
-      payload[this.objectType] = [sourceObject]
+      if (Array.isArray(this.object)) {
+        payload[this.objectType] = this.object
+      } else {
+        payload[this.objectType] = [this.object]
+      }
       this.$oh.api
         .postPlain('/rest/file-format/create', JSON.stringify(payload), null, sourceMediaType, { accept: targetMediaType })
         .then((code) => {
@@ -184,10 +218,11 @@ export default {
      *
      * @param {function} onSuccessCallback - Optional. A callback function to call when the code has been parsed. Receives the parsed object.
      * @param {function} onFailureCallback - Optional. A callback function to call when parsing fails or no object is found
+     * @param {boolean} parseAll - Optional. If true, all parsed objects will be returned as an array. By default, only the first object is returned for backward compatibility with existing code that expects a single object.
      */
-    parseCode(onSuccessCallback, onFailureCallback) {
+    parseCode(onSuccessCallback, onFailureCallback, parseAll = false) {
       let sourceMediaType = this.mediaTypes[this.uiOptionsStore.codeEditorType]
-      sourceMediaType = sourceMediaType.split('+')[0] // remove the +thing or +item suffix, if present
+      sourceMediaType = sourceMediaType.split('+')[0] // remove the +tag, +thing or +item suffix, if present
       const targetMediaType = MediaType.JSON
       this.$oh.api
         .request({
@@ -206,9 +241,16 @@ export default {
           }
           object = object[this.objectType]
           if (object?.length > 0) {
-            this.$emit('parsed', object[0])
-            if (onSuccessCallback) {
-              onSuccessCallback(object[0])
+            if (parseAll) {
+              this.$emit('parsed', object)
+              if (onSuccessCallback) {
+                onSuccessCallback(object)
+              }
+            } else {
+              this.$emit('parsed', object[0])
+              if (onSuccessCallback) {
+                onSuccessCallback(object[0])
+              }
             }
           } else {
             if (onFailureCallback) {
@@ -257,11 +299,13 @@ export default {
     switchCodeType(type) {
       if (this.uiOptionsStore.codeEditorType === type) return
 
-      if (this.readOnly || !this.dirty) {
+      if (!this.dirty) {
         this.generateCode(type)
       } else {
-        this.parseCode((parsedObject) => {
-          this.generateCode(type, null, parsedObject)
+        this.parseCode(() => {
+          this.$nextTick(() => {
+            this.generateCode(type)
+          })
         })
       }
     },

commit 189a5b4996950d48c09fee3537439b2612ddcd8a

Semantic Tag Editor: Fix adding tag in treeview (#4277)
Regression from #4234.

Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

bundles/org.openhab.ui/web/src/pages/developer/semantics/semantic-tags-edit.vue
@@ -762,10 +762,12 @@ export default {
     },
     updateName(ev) {
       const name = ev.target.value
-      this.selectedTag.name = name
-      const oldUid = this.selectedTag.uid
-      const newUid = this.selectedTag.parent + '_' + name
-      this.selectedTag.uid = newUid
+      const tag = this.selectedTag
+      tag.name = name
+      const oldUid = tag.uid
+      const newUid = tag.parent + '_' + name
+      tag.uid = newUid
+      this.selectedTagUid = newUid
       this.expandedTags[newUid] = this.expandedTags[oldUid]
     },
     addSynonym(event) {