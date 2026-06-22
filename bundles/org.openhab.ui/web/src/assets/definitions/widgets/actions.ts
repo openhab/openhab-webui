openhab/openhab-webui

commit 2e4047c2652d093fac66bbf3084da73aa62f3bda

Refactor widget definitions to TypeScript (#3959)
Signed-off-by: Florian Hotze <dev@florianhotze.com>
Co-authored-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/assets/definitions/widgets/actions.ts
@@ -1,0 +1,229 @@
+// parameter group & parameters definitions for actions
+import type { ConfigDescriptionParameterGroup } from '@/api'
+
+import { po, pt, pi, pb } from './helpers.ts'
+import { aggregationTypeOptions } from './chart/options.ts'
+
+export const actionGroup = (groupPrefix?: string, label?: string, description?: string): ConfigDescriptionParameterGroup => {
+  groupPrefix = groupPrefix ? (groupPrefix += '_') : ''
+  return {
+    name: groupPrefix + 'actions',
+    label: label || 'Action',
+    description: description || 'Action to perform when the element is clicked',
+    advanced: false
+  }
+}
+
+export const actionParams = (paramPrefix?: string, groupName?: string) => {
+  paramPrefix = paramPrefix ? (paramPrefix += '_') : ''
+  if (!groupName) groupName = 'actions'
+  return [
+    po(paramPrefix + 'action', 'Action', 'Type of action to perform', [
+      { value: 'navigate', label: 'Navigate to page' },
+      { value: 'command', label: 'Send command' },
+      { value: 'toggle', label: 'Toggle Item' },
+      { value: 'options', label: 'Command options' },
+      { value: 'rule', label: 'Run scene, script or rule' },
+      { value: 'popup', label: 'Open popup' },
+      { value: 'popover', label: 'Open popover' },
+      { value: 'sheet', label: 'Open sheet' },
+      { value: 'photos', label: 'Open photo browser' },
+      { value: 'group', label: 'Group details' },
+      { value: 'analyzer', label: 'Analyze item(s)' },
+      { value: 'url', label: 'Navigate to external URL' },
+      { value: 'http', label: 'Send HTTP request' },
+      { value: 'variable', label: 'Set Variable' }
+    ]),
+    pt(paramPrefix + 'actionUrl', 'Action URL', 'URL to navigate to or to send HTTP request to')
+      .c('url')
+      .v((_value, configuration) => {
+        return ['url', 'http'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+      }),
+    pb(
+      paramPrefix + 'actionUrlSameWindow',
+      'Open in same tab/window',
+      'Open the URL in the same tab/window instead of a new one. This will exit the app.'
+    ).v((_value, configuration) => {
+      return ['url'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+    }),
+    po(paramPrefix + 'actionHttpMethod', 'HTTP Method', 'HTTP method to use for the request', [
+      { value: 'GET', label: 'GET' },
+      { value: 'POST', label: 'POST' },
+      { value: 'PUT', label: 'PUT' },
+      { value: 'DELETE', label: 'DELETE' }
+    ]).v((_value, configuration) => {
+      return ['http'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+    }),
+    pt(paramPrefix + 'actionHttpBody', 'HTTP Body', 'Body to send with the request').v((_value, configuration) => {
+      return ['http'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+    }),
+    pi(paramPrefix + 'actionItem', 'Action Item', 'Item to perform the action on').v((_value, configuration) => {
+      return ['command', 'toggle', 'options'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+    }),
+    pt(
+      paramPrefix + 'actionCommand',
+      'Action Command',
+      'Command to send to the Item. If "Toggle Item" is selected as the action, only send the command when the state is different'
+    ).v((_value, configuration) => {
+      return ['command', 'toggle'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+    }),
+    pt(
+      paramPrefix + 'actionCommandAlt',
+      'Action Toggle Command',
+      'Command to send to the Item when "Toggle Item" is selected as the action, and the Item\'s state is equal to the command above'
+    ).v((_value, configuration) => {
+      return ['toggle'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+    }),
+    pt(
+      paramPrefix + 'actionOptions',
+      'Command Options',
+      'Comma-separated list of options; if omitted, retrieve the command options from the Item dynamically. Use <code>value=label</code> format to provide a label different than the option.'
+    ).v((_value, configuration) => {
+      return ['options'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+    }),
+    pt(paramPrefix + 'actionRule', 'Scene, Script or Rule', 'Scene, Script or Rule to run')
+      .c('rule')
+      .v((_value, configuration) => {
+        return ['rule'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+      }),
+    pt(
+      paramPrefix + 'actionRuleContext',
+      'Context',
+      'Object representing the optional context to pass. Edit in YAML or provide a JSON object, e.g. <code>{ "param1": "value1", "param2": { "subkey1": "testing", "subkey2": 123 } }</code>.'
+    )
+      .c('script')
+      .v((_value, configuration) => {
+        return ['rule'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+      }),
+    pt(paramPrefix + 'actionPage', 'Page', 'Page to navigate to')
+      .c('page')
+      .v((_value, configuration) => {
+        return ['navigate'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+      }),
+    po(
+      paramPrefix + 'actionPageTransition',
+      'Transition Effect',
+      'Use a specific <a class="external text-color-blue" target="_blank" href="https://framework7.io/docs/view.html#custom-page-transitions">page transition animation</a>',
+      [
+        { value: 'f7-circle', label: 'Circle' },
+        { value: 'f7-cover', label: 'Cover' },
+        { value: 'f7-cover-v', label: 'Cover from bottom' },
+        { value: 'f7-dive', label: 'Dive' },
+        { value: 'f7-fade', label: 'Fade' },
+        { value: 'f7-flip', label: 'Flip' },
+        { value: 'f7-parallax', label: 'Parallax' },
+        { value: 'f7-push', label: 'Push' }
+      ]
+    ).v((_value, configuration) => {
+      return ['navigate'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+    }),
+    pt(paramPrefix + 'actionModal', 'Modal Page or Widget', 'Page or widget to display in the modal')
+      .c('pagewidget')
+      .v((_value, configuration) => {
+        return ['popup', 'popover', 'sheet'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+      }),
+    pt(
+      paramPrefix + 'actionModalConfig',
+      'Modal component configuration',
+      'Configuration (prop values) for the target modal page or widget'
+    )
+      .c('props')
+      .v((_value, configuration) => {
+        return ['navigate', 'popup', 'popover', 'sheet'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+      }),
+    pt(
+      paramPrefix + 'actionPhotos',
+      'Images to show',
+      'Array of URLs or objects representing the images. Auto-refresh is not supported.<br />Edit in YAML, e.g.<br /><code><pre>- item: ImageItem1<br />  caption: Camera</pre></code>or provide a JSON array, e.g.<br /><code>[ "url1", { "item": "ImageItem1", "caption": "Camera" } ]</code><br />Objects are in the <a class="external text-color-blue" target="_blank" href="https://framework7.io/docs/photo-browser.html#photos-array">photos array format</a> with an additional <code>item</code> property to specify an item to view.'
+    ).v((_value, configuration) => {
+      return ['photos'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+    }),
+    pt(
+      paramPrefix + 'actionPhotoBrowserConfig',
+      'Photo browser configuration',
+      'Configuration for the photo browser.<br />Edit in YAML or provide a JSON object, e.g.<br /><code>{ "exposition": false, "type": "popup", "theme": "dark" }</code><br /> See <a class="external text-color-blue" target="_blank" href="https://framework7.io/docs/photo-browser.html#photo-browser-parameters">photo browser parameters</a> (not all are supported).'
+    ).v((_value, configuration) => {
+      return ['photos'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+    }),
+    pi(paramPrefix + 'actionGroupPopupItem', 'Group Popup Item', 'Group Item whose members to show in a popup').v(
+      (_value, configuration) => {
+        return ['group'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+      }
+    ),
+    pi(paramPrefix + 'actionAnalyzerItems', 'Item(s) to Analyze', 'Start analyzing with the specified (set of) Item(s)')
+      .m()
+      .r()
+      .v((_value, configuration) => {
+        return ['analyzer'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+      }),
+    po(
+      paramPrefix + 'actionAnalyzerChartType',
+      'Chart Type',
+      'The initial analyzing period - dynamic or a predefined fixed period: day, week, month or year',
+      [
+        { value: '', label: 'Dynamic' },
+        { value: 'day', label: 'Day' },
+        { value: 'isoWeek', label: 'Week (starting on Mondays)' },
+        { value: 'month', label: 'Month' },
+        { value: 'year', label: 'Year' }
+      ]
+    ).v((_value, configuration) => {
+      return ['analyzer'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+    }),
+    po(
+      paramPrefix + 'actionAnalyzerCoordSystem',
+      'Initial Coordinate System',
+      'The initial coordinate system of the analyzer - time, aggregate or calendar (only time is supported for dynamic periods)',
+      [
+        { value: 'time', label: 'Time' },
+        { value: 'aggregate', label: 'Aggregate' },
+        { value: 'calendar', label: 'Calendar' }
+      ]
+    ).v((_value, configuration) => {
+      return ['analyzer'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+    }),
+    po(
+      paramPrefix + 'actionAnalyzerAggregation',
+      'Initial Aggregation',
+      'The initial aggregation of the analyzer - ',
+      aggregationTypeOptions
+    ).v((_value, configuration) => {
+      return (
+        ['analyzer'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0 &&
+        configuration[paramPrefix + 'actionAnalyzerCoordSystem'] === 'aggregate'
+      )
+    }),
+    pt(
+      paramPrefix + 'actionConfirmation',
+      'Action Confirmation',
+      'Shows a dialog or sheet to ask for confirmation before the action is executed. Can either be a text to show in the dialog or a JSON object <code>{ type: "dialog", title: "Confirm", text: "Are you sure?" }</code> or <code>{ type: "sheet", text: "Confirm", color: "green" }</code>'
+    ).a(),
+    pt(
+      paramPrefix + 'actionFeedback',
+      'Action Feedback',
+      'Shows a toast popup when the action has been executed. Can either be a text to show or a JSON object including some of the <a class="external text-color-blue" target="_blank" href="https://framework7.io/docs/toast.html#toast-parameters">supported parameters</a>'
+    )
+      .a()
+      .v((_value, configuration) => {
+        return ['command', 'toggle', 'options', 'rule', 'http'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+      }),
+    pt(paramPrefix + 'actionVariable', 'Variable', 'The variable name to set').v((_value, configuration) => {
+      return ['variable'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+    }),
+    pt(paramPrefix + 'actionVariableValue', 'Variable Value', 'The value to set the variable to').v((_value, configuration) => {
+      return ['variable'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+    }),
+    pt(
+      paramPrefix + 'actionVariableKey',
+      'Variable Key',
+      'Consider the variable value is an object and set the corresponding deep property within that object using a key syntax. Examples: <code>user.name</code>, <code>user[0].address[1].street</code>, <code>[0]</code>, <code>[0].label</code>. The inner property and its parent hierarchy will be created if missing.'
+    )
+      .a()
+      .v((_value, configuration) => {
+        return ['variable'].indexOf(configuration[paramPrefix + 'action'] as string) >= 0
+      })
+  ].map((p) => {
+    p.groupName = groupName
+    return p
+  })
+}

commit 4b1d366912064d4f6157320e94f4b9aa1fadfc90

Fix widget action regression from #3959
Signed-off-by: Ravi Nadahar <nadahar@rediffmail.com>

bundles/org.openhab.ui/web/src/assets/definitions/widgets/actions.ts
@@ -28,7 +28,7 @@ export const actionGroup = (groupPrefix?: string, label?: string, description?: 
   }
 }
 
-export const actionParams = (paramPrefix?: string, groupName?: string) => {
+export const actionParams = (groupName?: string, paramPrefix?: string) => {
   paramPrefix = paramPrefix ? (paramPrefix += '_') : ''
   if (!groupName) groupName = 'actions'
   const actionKey = paramPrefix + 'action'