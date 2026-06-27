openhab/openhab-webui

commit 4c5420421dbf587aaeabf9d60c68b1ceb6963a86

Refactor widget-mixin to TS composable (#3862)
Fixes #3919.

* Refactors the widget-mixin to a TypeScript composable.
  This is a precondition for refactoring widget components to TypeScript.
  In Composition API, components must be registered globally if referenced
  by name - hence all widgets are registered globally.
  Note, there is no performance impact here.
* Adds TypeScript definitions for widget definitions.
* Removed special title overflow in oh-list-item for divider as CSS
  text-overflow does the same operation and is now part of the F7 styles.

---------

Also-by: Florian Hotze <dev@florianhotze.com>
Signed-off-by: Jeff James <jeff@james-online.com>

bundles/org.openhab.ui/web/src/components/widgets/system/oh-context.vue
@@ -1,6 +1,6 @@
 <template>
   <generic-widget-component
-    v-for="(slotComponent, idx) in children"
+    v-for="(slotComponent, idx) in defaultSlots"
     v-bind="$attrs"
     :key="'default-' + idx"
     :context="childrenContext(slotComponent)" />
@@ -9,23 +9,25 @@
 <script>
 import { f7 } from 'framework7-vue'
 
-import mixin from '../widget-mixin'
+import { useWidgetContext } from '@/components/widgets/useWidgetContext'
 import { OhContextDefinition } from '@/assets/definitions/widgets/system'
 
 export default {
   inheritAttrs: false,
-  mixins: [mixin],
+  props: {
+    context: Object
+  },
   widget: OhContextDefinition,
+  setup (props) {
+    const { childContext, evaluateExpression, defaultSlots } = useWidgetContext(props.context)
+    return { childContext, evaluateExpression, defaultSlots }
+  },
   data () {
     return {
       varScope: (this.context.varScope || 'varScope') + '-' + f7.utils.id()
     }
   },
   computed: {
-    children () {
-      if (!this.context?.component?.slots?.default) return []
-      return this.context.component.slots.default
-    },
     fn () {
       if (!this.context?.component?.config) return {}
       let evalFunc = {}

bundles/org.openhab.ui/web/src/components/widgets/useWidgetContext.ts
@@ -1,0 +1,224 @@
+import { f7 } from 'framework7-vue'
+
+import scope from 'scope-css'
+
+import { useUserStore } from '@/js/stores/useUserStore'
+import { useComponentsStore } from '@/js/stores/useComponentsStore'
+import { useWidgetExpression } from '@/components/widgets/useWidgetExpression.ts'
+
+import type { WidgetContext } from './types'
+import { computed, onBeforeUnmount, onMounted, ref } from 'vue'
+import * as api from '@/api'
+
+/**
+ * The useWidgetContext must be used as a composable in all widget components.
+ *
+ * A few requirements have to be met by components importing this composable:
+ * - Widget components have to add a `component` ref to their root.
+ * - Page components have to add a `page` ref to their root.
+ * - All components using this composable have to add the `scopedCssUid` as a class to their root element.
+ */
+export function useWidgetContext(context: WidgetContext) {
+  /* eslint-disable-next-line @typescript-eslint/no-unsafe-return */
+  if (!context) return {} as any
+
+  const widgetExpression = useWidgetExpression()
+  const _evaluateExpression = widgetExpression.evaluateExpression
+  const componentsStore = useComponentsStore()
+  const userStore = useUserStore()
+
+  // state
+  const vars = ref<Record<string, unknown>>(context.vars ?? {})
+  const ctxVars = ref<Record<string, unknown>>(context.ctxVars ?? {})
+  const widgetVars = ref<Record<string, unknown>>({})
+  const varScope = ref<string | null>(null)
+  const scopedCssUid = ref<string | null>(null)
+
+  // computed
+  const componentType = computed<string | null>(() => {
+    if (!context.component?.component) return null
+    return (evaluateExpression('type', context.component.component) as string) || null
+  })
+
+  const childWidgetComponentType = computed<string | null>(() => {
+    if (!componentType.value) return null
+    if (!componentType.value.startsWith('widget:')) return null
+    const widget = componentsStore.widget(componentType.value.substring(7))
+    if (!widget) {
+      console.warn('widget not found, cannot render: ' + componentType.value)
+      return null
+    }
+    return widget.component
+  })
+
+  const config = computed<Record<string, unknown>>(() => {
+    if (!context.component) return {}
+    let evalConfig: Record<string, unknown> = {}
+    // Fallback to modelConfig for oh- components to allow configuring them in modals
+    const sourceConfig: Record<string, unknown> =
+      context.component.config || (componentType.value?.startsWith('oh-') ? context.modalConfig : {})
+    if (sourceConfig) {
+      if (typeof sourceConfig !== 'object') return {}
+      for (const [key, value] of Object.entries(sourceConfig)) {
+        if (key === 'visible' || key === 'visibleTo' || key === 'stylesheet' || key === 'constants') continue
+        evalConfig[key] = evaluateExpression(key, value)
+      }
+    }
+    return evalConfig
+  })
+
+  const props = computed<Record<string, unknown>>(() => {
+    if (!context.component) return {}
+    if ('props' in context.component && context.component.props.parameters) {
+      let defaultValues: Record<string, unknown> = {}
+      // Note: api.ConfigDescriptionParameter uses 'defaultValue', but UI widgets just use 'default'
+      context.component.props.parameters.forEach((p: { name: string; default?: unknown }) => {
+        if (p.default !== undefined) {
+          defaultValues[p.name] = p.default
+        }
+      })
+      return Object.assign({}, defaultValues, context.props || {})
+    } else {
+      return context.props || {}
+    }
+  })
+
+  const hasAction = computed<boolean>(() => {
+    return !!(config.value && (config.value.action || config.value.actionPropsParameterGroup))
+  })
+
+  const visible = computed<boolean>(() => {
+    if (context.editmode || !context.component?.config) return true
+    const visible = evaluateExpression('visible', context.component.config.visible)
+    const visibleTo = context.component.config.visibleTo as string | undefined
+    if (visible === undefined && visibleTo === undefined) return true
+    if (visible === false || visible === 'false') return false
+    if (visibleTo) {
+      const user = userStore.user
+      if (!user) return false
+      if (user.roles && user.roles.some((r) => visibleTo.indexOf('role:' + r) >= 0)) return true
+      return visibleTo.indexOf('user:' + user.name) >= 0
+    }
+    return true
+  })
+
+  const slots = computed<Record<string, api.UiComponent[]>>(() => {
+    if ('slots' in context.component) return context.component.slots
+    return {}
+  })
+
+  const defaultSlots = computed<api.UiComponent[]>(() => {
+    if ('slots' in context.component && context.component.slots.default) {
+      return context.component.slots.default
+    }
+    return []
+  })
+
+  const childWidgetContext = computed((): WidgetContext | null => {
+    if (!componentType.value?.startsWith('widget:')) return null
+    let widget = componentsStore.widget(componentType.value.substring(7))
+    if (!widget) {
+      console.warn('widget not found, cannot render: ' + componentType.value)
+      return null
+    }
+    if (context.vars) {
+      for (const varKey in context.vars) {
+        widgetVars.value[varKey] = context.vars[varKey]
+      }
+    }
+    const extendedWidget =
+      context.component && 'slots' in context.component ? { ...widget, slots: { ...widget.slots, ...context.component.slots } } : widget
+    return {
+      component: extendedWidget,
+      props: config.value,
+      fn: context.fn,
+      const: context.const,
+      vars: widgetVars.value,
+      varScope: varScope.value || context.varScope,
+      ctxVars: context.ctxVars,
+      store: context.store,
+      config: context.config,
+      editmode: context.editmode,
+      clipboardtype: context.clipboardtype,
+      parent: context.parent
+    } satisfies WidgetContext
+  })
+
+  // methods
+  function evaluateExpression(key: string, value: any, _context?: WidgetContext, _props?: Record<string, unknown>): unknown {
+    return _evaluateExpression(key, value, _context || context, _props || props.value)
+  }
+
+  function childContext(component: api.UiComponent): WidgetContext {
+    return {
+      component: component,
+      props: props.value,
+      fn: context.fn,
+      const: context.const,
+      vars: context.vars,
+      varScope: varScope.value || context.varScope,
+      ctxVars: context.ctxVars,
+      loop: context.loop,
+      store: context.store,
+      config: context.config,
+      editmode: context.editmode,
+      clipboardtype: context.clipboardtype,
+      parent: context
+    }
+  }
+
+  function cardChildContext(component: api.RootUiComponent): WidgetContext {
+    // clone object to avoid mutating the original object
+    const cmp = { ...component }
+    if (cmp.config?.style) {
+      cmp.config = { ...cmp.config }
+      // remove style config as it should only apply to top-level element and not children
+      delete cmp.config.style
+    }
+    return childContext(cmp)
+  }
+
+  // lifecycle
+  onMounted(() => {
+    const stylesheet = context.component?.config?.stylesheet as string | undefined
+    if (stylesheet) {
+      scopedCssUid.value = 'scoped-' + f7.utils.id()
+      let style = document.createElement('style')
+      style.id = scopedCssUid.value
+      style.innerHTML = scope(stylesheet, '.' + scopedCssUid.value)
+      document.head.appendChild(style)
+    }
+  })
+
+  onBeforeUnmount(() => {
+    if (scopedCssUid.value) {
+      const scoped_stylesheet = document.getElementById(scopedCssUid.value)
+      if (scoped_stylesheet) scoped_stylesheet.remove()
+    }
+  })
+
+  return {
+    // state
+    vars,
+    ctxVars,
+    widgetVars,
+    varScope,
+    scopedCssUid,
+
+    // computed
+    componentType,
+    childWidgetComponentType,
+    config,
+    props,
+    hasAction,
+    visible,
+    slots,
+    defaultSlots,
+    childWidgetContext,
+
+    // methods
+    evaluateExpression,
+    childContext,
+    cardChildContext
+  }
+}

commit 00fb59ee46383ef809dcad87b71295620ce13f8a

Fix oh-context variables no longer defined or scoped
Fixes #4294.
Regression from #3862.

Signed-off-by: Florian Hotze <dev@florianhotze.com>

bundles/org.openhab.ui/web/src/components/widgets/system/oh-context.vue
@@ -20,13 +20,9 @@ export default {
   },
   widget: OhContextDefinition,
   setup(props) {
-    const { childContext, evaluateExpression, defaultSlots } = useWidgetContext(computed(() => props.context))
-    return { childContext, evaluateExpression, defaultSlots }
-  },
-  data() {
-    return {
-      varScope: (this.context.varScope || 'varScope') + '-' + f7.utils.id()
-    }
+    const { varScope, childContext, evaluateExpression, defaultSlots } = useWidgetContext(computed(() => props.context))
+    varScope.value = (props.context.varScope || 'varScope') + '-' + f7.utils.id()
+    return { varScope, childContext, evaluateExpression, defaultSlots }
   },
   computed: {
     fn() {
@@ -64,7 +60,7 @@ export default {
       ctx.const = ctxConstants
 
       if (typeof ctx.ctxVars !== 'object') ctx.ctxVars = {}
-      ctx.ctxVars[this.varScope] = this.ctxVars
+      ctx.ctxVars[this.varScope] = this.localCtxVars
 
       return ctx
     }
@@ -82,12 +78,12 @@ export default {
         }
       }
 
-      this.ctxVars = {}
+      this.localCtxVars = {}
       const sourceCtxVars = this.context.component.config.variables || {}
       if (sourceCtxVars) {
         if (typeof sourceCtxVars !== 'object') return
         for (const key in sourceCtxVars) {
-          this.ctxVars[key] = this.evaluateExpression(key, sourceCtxVars[key])
+          this.localCtxVars[key] = this.evaluateExpression(key, sourceCtxVars[key])
         }
       }
     }

bundles/org.openhab.ui/web/src/components/widgets/useWidgetContext.ts
@@ -113,9 +113,9 @@ export function useWidgetContext(context: Ref<WidgetContext>) {
       console.warn('widget not found, cannot render: ' + componentType.value)
       return null
     }
-    if (context.value.vars) {
-      for (const varKey in context.value.vars) {
-        widgetVars.value[varKey] = context.value.vars[varKey]
+    if (vars.value) {
+      for (const varKey in vars.value) {
+        widgetVars.value[varKey] = vars.value[varKey]
       }
     }
     const extendedWidget =
@@ -129,7 +129,7 @@ export function useWidgetContext(context: Ref<WidgetContext>) {
       const: context.value.const,
       vars: widgetVars.value,
       varScope: varScope.value || context.value.varScope,
-      ctxVars: context.value.ctxVars,
+      ctxVars: ctxVars.value,
       store: context.value.store,
       config: context.value.config,
       editmode: context.value.editmode,
@@ -150,9 +150,9 @@ export function useWidgetContext(context: Ref<WidgetContext>) {
       props: props.value,
       fn: context.value.fn,
       const: context.value.const,
-      vars: context.value.vars,
+      vars: vars.value,
       varScope: varScope.value || context.value.varScope,
-      ctxVars: context.value.ctxVars,
+      ctxVars: ctxVars.value,
       loop: context.value.loop,
       store: context.value.store,
       config: context.value.config,