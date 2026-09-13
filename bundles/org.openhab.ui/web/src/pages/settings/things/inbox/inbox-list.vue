2b5cff29164357e95b14a1333edb9284ca212954
jimtng <2554958+jimtng@users.noreply.github.com>
2026-09-13 20:06:16 +1000

    Inbox List: Fix truncation of dialog button texts (#4505)
    Regression from #4498.
    
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- bundles/org.openhab.ui/web/src/pages/settings/things/inbox/inbox-list.vue
+++ bundles/org.openhab.ui/web/src/pages/settings/things/inbox/inbox-list.vue
@@ -210,6 +210,10 @@
   @media (min-width 960px)
     padding-left 0 !important
     padding-right 0 !important
+</style>
+
+/* Unscoped styles for root-level dynamic dialogs */
+<style lang="stylus">
 .thing-inbox-approve-dialog
   width auto
 </style>
